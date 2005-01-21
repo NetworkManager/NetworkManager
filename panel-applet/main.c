e94b108e85307ea0aeb09f240578c791914eceb9
Dan Williams <dcbw@redhat.com>
2005-01-21 18:43:25 +0000

    Make sure all files use config.h
    git-svn-id: http://svn-archive.gnome.org/svn/NetworkManager/trunk@387 4912f4e0-d625-0410-9fb7-b9a5a253dbdc

--- panel-applet/main.c
+++ panel-applet/main.c
@@ -1,3 +1,30 @@
+/* NetworkManager Wireless Applet -- Display wireless access points and allow user control
+ *
+ * Dan Williams <dcbw@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This applet used the GNOME Wireless Applet as a skeleton to build from.
+ *
+ * (C) Copyright 2005 Red Hat, Inc.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <gtk/gtk.h>
 #include <libgnomeui/libgnomeui.h>
 #include "NMWirelessApplet.h"
