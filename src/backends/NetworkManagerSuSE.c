f5bd3c97d792125b40a787cea4c2e80c159a1072
Robert Love <rlove@rlove.org>
2006-03-14 16:51:53 +0000

    Add back policy enforcement.
    git-svn-id: http://svn-archive.gnome.org/svn/NetworkManager/trunk@1600 4912f4e0-d625-0410-9fb7-b9a5a253dbdc

--- src/backends/NetworkManagerSuSE.c
+++ src/backends/NetworkManagerSuSE.c
@@ -633,6 +633,9 @@ found:
 
 		nm_debug ("Adding '%s' to the list of trusted networks", buf);
 
+		/* Ensure all devices get new information copied into their device lists */
+		nm_policy_schedule_device_ap_lists_update_from_allowed (app_data);
+
 		free (key);
 		free (mode);
 		free (buf);
