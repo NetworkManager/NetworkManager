5bf591af74c2bdefdc7346b41c555ac1369811fc
Antony Mee <eemynotna@gmail.com>
2006-08-05 10:25:36 +0000

    * Moved the pppd headers to a directory based on the pppd version     and included some automake stuff to give a -I option for     the correct directory.
    git-svn-id: http://svn-archive.gnome.org/svn/NetworkManager/trunk@1927 4912f4e0-d625-0410-9fb7-b9a5a253dbdc

--- vpn-daemons/pptp/src/nm-pppd-plugin.c
+++ vpn-daemons/pptp/src/nm-pppd-plugin.c
@@ -17,10 +17,10 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
  */
-#include "pppd/pppd.h"
+#include "pppd.h"
 
-#include "pppd/fsm.h"
-#include "pppd/ipcp.h"
+#include "fsm.h"
+#include "ipcp.h"
 
 #include <sys/types.h>
 #include <unistd.h>
