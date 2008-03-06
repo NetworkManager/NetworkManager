2806222cb07984ea074a3e6badac81732e1a287f
Tambet Ingo <tambet@gmail.com>
2008-03-06 19:59:41 +0000

    2008-03-06  Tambet Ingo  <tambet@gmail.com>
            * src/backends/shvar.c (unescape): Fix a bug where empty strings
            * ('') get
            unescaped to a single quote (').
    
    
    
    git-svn-id: http://svn-archive.gnome.org/svn/NetworkManager/branches/NETWORKMANAGER_0_6_0_RELEASE@3377 4912f4e0-d625-0410-9fb7-b9a5a253dbdc

--- src/backends/shvar.c
+++ src/backends/shvar.c
@@ -121,9 +121,13 @@ unescape(char *s) {
     len = strlen(s);
     if ((s[0] == '"' || s[0] == '\'') && s[0] == s[len-1]) {
 	i = len - 2;
-	memmove(s, s+1, i);
-	s[i+1] = '\0';
-	len = i;
+	if (i == 0)
+	  s[0] = '\0';
+	else {
+	  memmove(s, s+1, i);
+	  s[i+1] = '\0';
+	  len = i;
+	}
     }
     for (i = 0; i < len; i++) {
 	if (s[i] == '\\') {
