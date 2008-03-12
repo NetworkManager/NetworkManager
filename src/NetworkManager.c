ad62f1f68e56bd8b0c520d05ffd2447a5a6e1e5b
Dan Williams <dcbw@redhat.com>
2008-03-12 19:44:26 +0000

    Don't use GNOMELOCALDIR
    git-svn-id: http://svn-archive.gnome.org/svn/NetworkManager/trunk@3439 4912f4e0-d625-0410-9fb7-b9a5a253dbdc

--- src/NetworkManager.c
+++ src/NetworkManager.c
@@ -234,7 +234,7 @@ main (int argc, char *argv[])
 		exit (1);
 	}
 
-	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+	bindtextdomain (GETTEXT_PACKAGE, NMLOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
 
