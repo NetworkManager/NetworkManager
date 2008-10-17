7cfd2ea57b31696a6f1ef4ad6bc9819dcc83f6fc
Dan Williams <dcbw@redhat.com>
2008-10-17 11:06:37 +0000

    2008-10-17  Dan Williams  <dcbw@redhat.com>
    	* properties/auth-helpers.c
    		- (tls_default_filter, tls_file_chooser_filter_new): accept ".cer"
    			extension too
    
    
    
    git-svn-id: http://svn-archive.gnome.org/svn/NetworkManager/trunk@4188 4912f4e0-d625-0410-9fb7-b9a5a253dbdc

--- vpn-daemons/openvpn/properties/auth-helpers.c
+++ vpn-daemons/openvpn/properties/auth-helpers.c
@@ -611,7 +611,7 @@ tls_default_filter (const GtkFileFilterInfo *filter_info, gpointer data)
 	ext = g_ascii_strdown (p, -1);
 	if (!ext)
 		return FALSE;
-	if (strcmp (ext, ".pem") && strcmp (ext, ".crt") && strcmp (ext, ".key")) {
+	if (strcmp (ext, ".pem") && strcmp (ext, ".crt") && strcmp (ext, ".key") && strcmp (ext, ".cer")) {
 		g_free (ext);
 		return FALSE;
 	}
@@ -657,7 +657,7 @@ tls_file_chooser_filter_new (void)
 
 	filter = gtk_file_filter_new ();
 	gtk_file_filter_add_custom (filter, GTK_FILE_FILTER_FILENAME, tls_default_filter, NULL, NULL);
-	gtk_file_filter_set_name (filter, _("PEM certificates (*.pem, *.crt, *.key)"));
+	gtk_file_filter_set_name (filter, _("PEM certificates (*.pem, *.crt, *.key, *.cer)"));
 	return filter;
 }
 
