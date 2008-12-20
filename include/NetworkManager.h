03c11a65b7a6e035fab44461e2eb90ec96813849
Dan Williams <dcbw@redhat.com>
2008-12-20 09:46:41 -0500

    License header and FSF address update

--- include/NetworkManager.h
+++ include/NetworkManager.h
@@ -16,7 +16,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (C) Copyright 2004 Red Hat, Inc.
+ * (C) Copyright 2004 - 2008 Red Hat, Inc.
  */
 
 #ifndef NETWORK_MANAGER_H
