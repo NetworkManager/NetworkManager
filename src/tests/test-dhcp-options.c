e927e44753c820450d4e2eee39ea8af2c960fb47
Dan Williams <dcbw@redhat.com>
2009-04-21 18:29:44 -0400

    core: don't shadow basename (2)

--- src/tests/test-dhcp-options.c
+++ src/tests/test-dhcp-options.c
@@ -583,7 +583,7 @@ int main (int argc, char **argv)
 {
 	GError *error = NULL;
 	DBusGConnection *bus;
-	char *basename;
+	char *base;
 
 	g_type_init ();
 	bus = dbus_g_bus_get (DBUS_BUS_SESSION, NULL);
@@ -602,9 +602,9 @@ int main (int argc, char **argv)
 	test_escaped_domain_searches ();
 	test_invalid_escaped_domain_searches ();
 
-	basename = g_path_get_basename (argv[0]);
-	fprintf (stdout, "%s: SUCCESS\n", basename);
-	g_free (basename);
+	base = g_path_get_basename (argv[0]);
+	fprintf (stdout, "%s: SUCCESS\n", base);
+	g_free (base);
 	return 0;
 }
 
