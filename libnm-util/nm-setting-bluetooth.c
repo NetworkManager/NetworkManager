7be36a7319af2209cc8976978c65e0a8e16c55cb
Bastien Nocera <hadess@hadess.net>
2009-05-18 11:25:04 -0400

    bluetooth: actually set the NMSettingBluetooth name property on init

--- libnm-util/nm-setting-bluetooth.c
+++ libnm-util/nm-setting-bluetooth.c
@@ -178,6 +178,7 @@ verify (NMSetting *setting, GSList *all_settings, GError **error)
 static void
 nm_setting_bluetooth_init (NMSettingBluetooth *setting)
 {
+	g_object_set (setting, NM_SETTING_NAME, NM_SETTING_BLUETOOTH_NAME, NULL);
 }
 
 static void
