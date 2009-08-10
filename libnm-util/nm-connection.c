NetworkManager/NetworkManager

commit ff88cf12c2de43081f1a7c93a2565658119b7894

olpc: add mesh device logic and config setting

libnm-util/nm-connection.c
@@ -41,6 +41,7 @@
 #include "nm-setting-wireless.h"
 #include "nm-setting-wireless-security.h"
 #include "nm-setting-vpn.h"
+#include "nm-setting-olpc-mesh.h"
 
 #include "nm-setting-serial.h"
 #include "nm-setting-gsm.h"
@@ -214,6 +215,11 @@ register_default_settings (void)
 	register_one_setting (NM_SETTING_WIRELESS_SETTING_NAME,
 	                      NM_TYPE_SETTING_WIRELESS,
 	                      NM_SETTING_WIRELESS_ERROR,
+	                      1);
+
+	register_one_setting (NM_SETTING_OLPC_MESH_SETTING_NAME,
+	                      NM_TYPE_SETTING_OLPC_MESH,
+	                      NM_SETTING_OLPC_MESH_ERROR,
 	                      1);
 
 	register_one_setting (NM_SETTING_GSM_SETTING_NAME,

Makefile.am
@@ -27,7 +27,7 @@ EXTRA_DIST = \
 	intltool-merge.in \
 	intltool-update.in		
 
-DISTCHECK_CONFIGURE_FLAGS = --with-tests=yes --with-docs=yes
+DISTCHECK_CONFIGURE_FLAGS = --with-tests=yes --with-docs=yes --with-udev-dir=$$dc_install_base/lib/udev
 
 DISTCLEANFILES = intltool-extract intltool-merge intltool-update
 

callouts/77-nm-olpc-mesh.rules
@@ -1,0 +1,6 @@
+# do not edit this file, it will be overwritten on update
+
+# The fact that this device is driven by libertas is not currently exposed
+# in the sysfs tree..?
+KERNEL=="msh*", SUBSYSTEM=="net", DRIVERS=="usb", ATTRS{idVendor}=="1286", ATTRS{idProduct}=="2001", ENV{ID_NM_OLPC_MESH}="1"
+

callouts/Makefile.am
@@ -62,6 +62,9 @@ nm_dispatcher_action_LDADD = \
 nm-dispatcher-glue.h: nm-dispatcher.xml
 	dbus-binding-tool --prefix=nm_dispatcher --mode=glib-server --output=$@ $<
 
+udevrulesdir = $(UDEV_BASE_DIR)/rules.d
+udevrules_DATA = 77-nm-olpc-mesh.rules
+
 dbusactivationdir = $(datadir)/dbus-1/system-services
 dbusactivation_in_files = org.freedesktop.nm_dispatcher.service.in
 dbusactivation_DATA = $(dbusactivation_in_files:.service.in=.service)
@@ -85,6 +88,7 @@ CLEANFILES = $(BUILT_SOURCES) $(dbusactivation_DATA)
 
 EXTRA_DIST = \
 	$(dbusservice_DATA) \
+	$(udevrules_DATA) \
 	$(dbusactivation_in_files) \
 	nm-dispatcher.xml
 

configure.ac
@@ -193,6 +193,14 @@ PKG_CHECK_MODULES(GUDEV, gudev-1.0)
 AC_SUBST(GUDEV_CFLAGS)
 AC_SUBST(GUDEV_LIBS)
 
+AC_ARG_WITH(udev-dir, AS_HELP_STRING([--with-udev-dir=DIR], [where the udev base directory is]))
+if test -n "$with_udev_dir" ; then
+	UDEV_BASE_DIR="$with_udev_dir"
+else
+	UDEV_BASE_DIR="/lib/udev"
+fi
+AC_SUBST(UDEV_BASE_DIR)
+
 PKG_CHECK_EXISTS(gio-2.0,[have_gio=yes],[have_gio=no])
 if test x"$have_gio" = "xno"; then
 	AC_DEFINE([NO_GIO],[1],[Define if you don't have GIO])

include/NetworkManager.h
@@ -77,7 +77,8 @@ typedef enum NMDeviceType
 	NM_DEVICE_TYPE_WIFI,
 	NM_DEVICE_TYPE_GSM,
 	NM_DEVICE_TYPE_CDMA,
-	NM_DEVICE_TYPE_BT  /* Bluetooth */
+	NM_DEVICE_TYPE_BT,  /* Bluetooth */
+	NM_DEVICE_TYPE_OLPC_MESH
 } NMDeviceType;
 
 /* DEPRECATED TYPE NAMES */

introspection/Makefile.am
@@ -6,6 +6,7 @@ EXTRA_DIST = \
 	nm-access-point.xml \
 	nm-device-bt.xml \
 	nm-device-wifi.xml \
+	nm-device-olpc-mesh.xml \
 	nm-device-ethernet.xml \
 	nm-device-cdma.xml \
 	nm-device-gsm.xml \

introspection/nm-device-olpc-mesh.xml
@@ -1,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+
+<node name="/" xmlns:tp="http://telepathy.freedesktop.org/wiki/DbusSpec#extensions-v0">
+  <interface name="org.freedesktop.NetworkManager.Device.OlpcMesh">
+    <property name="HwAddress" type="s" access="read">
+      <tp:docstring>
+        The hardware address of the device.
+      </tp:docstring>
+    </property>
+    <property name="Companion" type="s" access="read">
+      <tp:docstring>
+        The object path of the companion device.
+      </tp:docstring>
+    </property>
+    <property name="ActiveChannel" type="o" access="read">
+      <tp:docstring>
+        The currently active channel.
+      </tp:docstring>
+    </property>
+
+    <signal name="PropertiesChanged">
+        <arg name="properties" type="a{sv}" tp:type="String_Variant_Map">
+            <tp:docstring>
+                A dictionary containing the FIXME: check changed parameters.
+            </tp:docstring>
+        </arg>
+        <tp:docstring>
+            Emitted when the wireless device's properties changed.
+        </tp:docstring>
+    </signal>
+  </interface>
+</node>

commit 0993ea398951df26eb961e7ca0ac386ed374022d

libnm-util: fix DEFAULT_MAP_SIZE; broken by ff88cf12c2de43

libnm-util/nm-connection.c
@@ -141,7 +141,7 @@ static guint signals[LAST_SIGNAL] = { 0 };
 
 static GHashTable *registered_settings = NULL;
 
-#define DEFAULT_MAP_SIZE 14
+#define DEFAULT_MAP_SIZE 15
 
 static struct SettingInfo {
 	const char *name;