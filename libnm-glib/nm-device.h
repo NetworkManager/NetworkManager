7239863d8fe824250f86f4f9993e58c918309229
Dan Williams <dcbw@redhat.com>
2009-09-18 08:38:29 -0700

    libnm-glib: make nm_device_disconnect() async

--- libnm-glib/nm-device.h
+++ libnm-glib/nm-device.h
@@ -85,7 +85,11 @@ NMDeviceState nm_device_get_state          (NMDevice *device);
 const char *  nm_device_get_product        (NMDevice *device);
 const char *  nm_device_get_vendor         (NMDevice *device);
 
-void  nm_device_disconnect                 (NMDevice *device, GError **error);
+typedef void (*NMDeviceDeactivateFn) (NMDevice *device, GError *error, gpointer user_data);
+
+void          nm_device_disconnect         (NMDevice *device,
+                                            NMDeviceDeactivateFn callback,
+                                            gpointer user_data);
 
 G_END_DECLS
 
