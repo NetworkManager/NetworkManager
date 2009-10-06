NetworkManager/NetworkManager

commit 715ddd2045cf786e6c80e6d3d8617fe70ad24552

core: Make iptables path configurable
On some systems, it's located in different path...

src/nm-activation-request.c
@@ -575,22 +575,24 @@ nm_act_request_set_shared (NMActRequest *req, gboolean shared)
 	for (iter = list; iter; iter = g_slist_next (iter)) {
 		ShareRule *rule = (ShareRule *) iter->data;
 		char *envp[1] = { NULL };
-		char **argv;
+		char *argv[6];
 		char *cmd;
 		int status;
 		GError *error = NULL;
 
+		argv[0] = IPTABLES_PATH;
+		argv[1] = "--table";
+		argv[2] = rule->table;
+
 		if (shared)
-			cmd = g_strdup_printf ("/sbin/iptables --table %s --insert %s", rule->table, rule->rule);
+			argv[3] = "--insert";
 		else
-			cmd = g_strdup_printf ("/sbin/iptables --table %s --delete %s", rule->table, rule->rule);
+			argv[3] = "--delete";
 
-		argv = g_strsplit (cmd, " ", 0);
-		if (!argv || !argv[0]) {
-			continue;
-			g_free (cmd);
-		}
+		argv[4] = rule->rule;
+		argv[5] = NULL;
 
+		cmd = g_strjoinv (" ", argv);
 		nm_info ("Executing: %s", cmd);
 		g_free (cmd);
 
@@ -602,8 +604,6 @@ nm_act_request_set_shared (NMActRequest *req, gboolean shared)
 				g_error_free (error);
 		} else if (WEXITSTATUS (status))
 			nm_info ("** Command returned exit status %d.", WEXITSTATUS (status));
-
-		g_strfreev (argv);
 	}
 
 	g_slist_free (list);

configure.ac
@@ -369,6 +369,19 @@ if test -n "${RESOLVCONF_PATH}"; then
 	AC_DEFINE_UNQUOTED(RESOLVCONF_PATH, "$RESOLVCONF_PATH", [Define if you have a resolvconf implementation])
 fi
 
+# iptables path
+AC_ARG_WITH(iptables, AC_HELP_STRING([--with-iptables=/path/to/iptables], [path to iptables])) 
+if test "x${with_iptables}" = x; then
+  AC_PATH_PROG(IPTABLES_PATH, iptables, [], $PATH:/sbin:/usr/sbin)
+  if ! test -x "$IPTABLES_PATH"; then
+        AC_MSG_ERROR(iptables was not installed.)
+  fi
+else
+  IPTABLES_PATH="$with_iptables"
+fi
+AC_DEFINE_UNQUOTED(IPTABLES_PATH, "$IPTABLES_PATH", [Define to path of iptables binary])
+AC_SUBST(IPTABLES_PATH)
+
 # system CA certificates path
 AC_ARG_WITH(system-ca-path, AS_HELP_STRING([--with-system-ca-path=/path/to/ssl/certs], [path to system CA certificates])) 
 if test "x${with_system_ca_path}" = x; then

commit 59d89050f255f5a15c1bdcbb33dffa638cfaf695

sharing: fix sharing after 715ddd2045cf786e6c80e6d3d8617fe70ad24552 (lp:444756)
Commit 715ddd2045cf786e6c80e6d3d8617fe70ad24552 broke sharing because
sometimes a multi-argument string gets passed in rule->rule.  Revert
most of that commit.  A bunch of cleanups by dcbw too.

src/nm-activation-request.c
@@ -575,35 +575,35 @@ nm_act_request_set_shared (NMActRequest *req, gboolean shared)
 	for (iter = list; iter; iter = g_slist_next (iter)) {
 		ShareRule *rule = (ShareRule *) iter->data;
 		char *envp[1] = { NULL };
-		char *argv[6];
+		char **argv;
 		char *cmd;
-		int status;
-		GError *error = NULL;
 
-		argv[0] = IPTABLES_PATH;
-		argv[1] = "--table";
-		argv[2] = rule->table;
+		cmd = g_strdup_printf ("%s --table %s %s %s",
+		                       IPTABLES_PATH,
+		                       rule->table,
+		                       shared ? "--insert" : "--delete",
+		                       rule->rule);
+		if (!cmd)
+			continue;
 
-		if (shared)
-			argv[3] = "--insert";
-		else
-			argv[3] = "--delete";
+		argv = g_strsplit (cmd, " ", 0);
+		if (argv && argv[0]) {
+			int status;
+			GError *error = NULL;
 
-		argv[4] = rule->rule;
-		argv[5] = NULL;
-
-		cmd = g_strjoinv (" ", argv);
-		nm_info ("Executing: %s", cmd);
+			nm_info ("Executing: %s", cmd);
+			if (!g_spawn_sync ("/", argv, envp, G_SPAWN_STDOUT_TO_DEV_NULL | G_SPAWN_STDERR_TO_DEV_NULL,
+			                   share_child_setup, NULL, NULL, NULL, &status, &error)) {
+				nm_info ("Error executing command: (%d) %s",
+				         error ? error->code : -1,
+				         (error && error->message) ? error->message : "(unknown)");
+				g_clear_error (&error);
+			} else if (WEXITSTATUS (status))
+				nm_info ("** Command returned exit status %d.", WEXITSTATUS (status));
+		}
 		g_free (cmd);
-
-		if (!g_spawn_sync ("/", argv, envp, G_SPAWN_STDOUT_TO_DEV_NULL | G_SPAWN_STDERR_TO_DEV_NULL,
-		                   share_child_setup, NULL, NULL, NULL, &status, &error)) {
-			nm_info ("Error executing command: (%d) %s",
-			         error ? error->code : 0, (error && error->message) ? error->message : "unknown");
-			if (error)
-				g_error_free (error);
-		} else if (WEXITSTATUS (status))
-			nm_info ("** Command returned exit status %d.", WEXITSTATUS (status));
+		if (argv)
+			g_strfreev (argv);
 	}
 
 	g_slist_free (list);