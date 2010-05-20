a729d2f649fefc77fcff7ad250c5c403a4dd59b7
Dan Williams <dcbw@redhat.com>
2010-05-20 10:24:13 -0700

    ip6: remove standalone DHCP method since DHCPv6 can't provide gateways
    All IPv6 enabled sites are expected to provide router advertisement
    support apparently.  If standalone DHCP is really used in the wild
    then we can clearly re-enable it later.

--- libnm-util/nm-setting-ip6-config.h
+++ libnm-util/nm-setting-ip6-config.h
@@ -66,7 +66,6 @@ GQuark nm_setting_ip6_config_error_quark (void);
 
 #define NM_SETTING_IP6_CONFIG_METHOD_IGNORE     "ignore"
 #define NM_SETTING_IP6_CONFIG_METHOD_AUTO       "auto"
-#define NM_SETTING_IP6_CONFIG_METHOD_DHCP       "dhcp"
 #define NM_SETTING_IP6_CONFIG_METHOD_LINK_LOCAL "link-local"
 #define NM_SETTING_IP6_CONFIG_METHOD_MANUAL     "manual"
 #define NM_SETTING_IP6_CONFIG_METHOD_SHARED     "shared"
