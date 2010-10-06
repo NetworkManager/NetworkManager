febb3d2cb460e28b42fc3abeeaaa5b9fc94fde81
Martin Pitt <martin.pitt@ubuntu.com>
2010-10-06 17:14:54 -0500

    system-settings: add timestamp to default auto wired connections (bgo #583756)

--- src/system-settings/nm-default-wired-connection.c
+++ src/system-settings/nm-default-wired-connection.c
@@ -163,6 +163,7 @@ constructor (GType type,
 		      NM_SETTING_CONNECTION_AUTOCONNECT, TRUE,
 		      NM_SETTING_CONNECTION_UUID, uuid,
 		      NM_SETTING_CONNECTION_READ_ONLY, priv->read_only,
+		      NM_SETTING_CONNECTION_TIMESTAMP, time (NULL),
 		      NULL);
 
 	g_free (id);
