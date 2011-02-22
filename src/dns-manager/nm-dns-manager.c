5444a3569352312118e3ad0b37f61afc5842b190
Jiří Klimeš <jklimes@redhat.com>
2011-02-22 18:28:12 +0100

    dns: fix up updating resolv.conf (rh #672282)

--- src/dns-manager/nm-dns-manager.c
+++ src/dns-manager/nm-dns-manager.c
@@ -16,7 +16,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  * Copyright (C) 2004 - 2005 Colin Walters <walters@redhat.com>
- * Copyright (C) 2004 - 2010 Red Hat, Inc.
+ * Copyright (C) 2004 - 2011 Red Hat, Inc.
  * Copyright (C) 2005 - 2008 Novell, Inc.
  *   and others
  */
@@ -861,7 +861,7 @@ nm_dns_manager_remove_ip4_config (NMDnsManager *mgr,
 
 	g_object_unref (config);
 
-	if (config_changed (mgr))
+	if (!config_changed (mgr))
 		return TRUE;
 
 	if (!update_dns (mgr, iface, FALSE, &error)) {
@@ -906,7 +906,7 @@ nm_dns_manager_add_ip6_config (NMDnsManager *mgr,
 	if (!g_slist_find (priv->configs, config))
 		priv->configs = g_slist_append (priv->configs, g_object_ref (config));
 
-	if (config_changed (mgr))
+	if (!config_changed (mgr))
 		return TRUE;
 
 	if (!update_dns (mgr, iface, FALSE, &error)) {
@@ -946,7 +946,7 @@ nm_dns_manager_remove_ip6_config (NMDnsManager *mgr,
 
 	g_object_unref (config);	
 
-	if (config_changed (mgr))
+	if (!config_changed (mgr))
 		return TRUE;
 
 	if (!update_dns (mgr, iface, FALSE, &error)) {
