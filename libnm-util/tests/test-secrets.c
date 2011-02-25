NetworkManager/NetworkManager

commit c3029b31af07e35338f4ad4ad32e4e5b9547a55b

libnm-util: fix updating secrets
Broken by 52f2295a4925acc892e2fd67540671d4a8047f55.

libnm-util/nm-connection.c
@@ -720,28 +720,39 @@ nm_connection_verify (NMConnection *connection, GError **error)
 gboolean
 nm_connection_update_secrets (NMConnection *connection,
                               const char *setting_name,
-                              GHashTable *secrets,
+                              GHashTable *all_secrets,
                               GError **error)
 {
 	NMSetting *setting;
 	gboolean success;
+	GHashTable *setting_secrets;
 
 	g_return_val_if_fail (connection != NULL, FALSE);
 	g_return_val_if_fail (NM_IS_CONNECTION (connection), FALSE);
 	g_return_val_if_fail (setting_name != NULL, FALSE);
-	g_return_val_if_fail (secrets != NULL, FALSE);
+	g_return_val_if_fail (all_secrets != NULL, FALSE);
 	if (error)
 		g_return_val_if_fail (*error == NULL, FALSE);
 
 	setting = nm_connection_get_setting (connection, nm_connection_lookup_setting_type (setting_name));
 	if (!setting) {
-		g_set_error (error, NM_CONNECTION_ERROR,
-		             NM_CONNECTION_ERROR_CONNECTION_SETTING_NOT_FOUND,
-		             "%s", setting_name);
+		g_set_error_literal (error,
+		                     NM_CONNECTION_ERROR,
+		                     NM_CONNECTION_ERROR_CONNECTION_SETTING_NOT_FOUND,
+		                     setting_name);
 		return FALSE;
 	}
 
-	success = nm_setting_update_secrets (setting, secrets, error);
+	setting_secrets = g_hash_table_lookup (all_secrets, setting_name);
+	if (!setting_secrets) {
+		g_set_error_literal (error,
+		                     NM_CONNECTION_ERROR,
+		                     NM_CONNECTION_ERROR_CONNECTION_SETTING_NOT_FOUND,
+		                     setting_name);
+		return FALSE;
+	}
+
+	success = nm_setting_update_secrets (setting, setting_secrets, error);
 	if (success)
 		g_signal_emit (connection, signals[SECRETS_UPDATED], 0, setting_name);
 	return success;

libnm-util/nm-setting.c
@@ -666,7 +666,6 @@ nm_setting_update_secrets (NMSetting *setting, GHashTable *secrets, GError **err
 	GHashTableIter iter;
 	gpointer key, data;
 	GError *tmp_error = NULL;
-	gboolean success = TRUE;
 
 	g_return_val_if_fail (setting != NULL, FALSE);
 	g_return_val_if_fail (NM_IS_SETTING (setting), FALSE);
@@ -675,18 +674,18 @@ nm_setting_update_secrets (NMSetting *setting, GHashTable *secrets, GError **err
 		g_return_val_if_fail (*error == NULL, FALSE);
 
 	g_hash_table_iter_init (&iter, secrets);
-	while (g_hash_table_iter_next (&iter, &key, &data) && !tmp_error) {
+	while (g_hash_table_iter_next (&iter, &key, &data)) {
 		const char *secret_key = (const char *) key;
 		GValue *secret_value = (GValue *) data;
 
 		NM_SETTING_GET_CLASS (setting)->update_one_secret (setting, secret_key, secret_value, &tmp_error);
-	}
-	if (tmp_error) {
-		success = FALSE;
-		g_propagate_error (error, tmp_error);
+		if (tmp_error) {
+			g_propagate_error (error, tmp_error);
+			return FALSE;
+		}
 	}
 
-	return success;
+	return TRUE;
 }
 
 /**

commit a2739c8e08676b56e3256feb5f4efa535aad7869

libnm-util: fix updating secrets (rh #680385)
Due to confusion over what nm_connection_update_secrets() actually
did, this got broken.  You'd think nm_connection_update_secrets()
would take a hash of hashes (ie, output of nm_connection_to_hash())
but it doesn't; it takes just the string:GValue hash of a single
setting's secrets.  So this got broken by
c3029b31af07e35338f4ad4ad32e4e5b9547a55b while fixing something else.
Add a testcase so this doesn't happen again.

libnm-util/nm-connection.c
@@ -707,7 +707,7 @@ nm_connection_verify (NMConnection *connection, GError **error)
  * nm_connection_update_secrets:
  * @connection: the #NMConnection
  * @setting_name: the setting object name to which the secrets apply
- * @secrets: a #GHashTable mapping string:#GValue of setting property names and
+ * @setting_secrets: a #GHashTable mapping string:#GValue of setting property names and
  * secrets
  * @error: location to store error, or %NULL
  *
@@ -720,31 +720,21 @@ nm_connection_verify (NMConnection *connection, GError **error)
 gboolean
 nm_connection_update_secrets (NMConnection *connection,
                               const char *setting_name,
-                              GHashTable *all_secrets,
+                              GHashTable *setting_secrets,
                               GError **error)
 {
 	NMSetting *setting;
 	gboolean success;
-	GHashTable *setting_secrets;
 
 	g_return_val_if_fail (connection != NULL, FALSE);
 	g_return_val_if_fail (NM_IS_CONNECTION (connection), FALSE);
 	g_return_val_if_fail (setting_name != NULL, FALSE);
-	g_return_val_if_fail (all_secrets != NULL, FALSE);
+	g_return_val_if_fail (setting_secrets != NULL, FALSE);
 	if (error)
 		g_return_val_if_fail (*error == NULL, FALSE);
 
 	setting = nm_connection_get_setting (connection, nm_connection_lookup_setting_type (setting_name));
 	if (!setting) {
-		g_set_error_literal (error,
-		                     NM_CONNECTION_ERROR,
-		                     NM_CONNECTION_ERROR_CONNECTION_SETTING_NOT_FOUND,
-		                     setting_name);
-		return FALSE;
-	}
-
-	setting_secrets = g_hash_table_lookup (all_secrets, setting_name);
-	if (!setting_secrets) {
 		g_set_error_literal (error,
 		                     NM_CONNECTION_ERROR,
 		                     NM_CONNECTION_ERROR_CONNECTION_SETTING_NOT_FOUND,

libnm-util/tests/Makefile.am
@@ -7,7 +7,7 @@ INCLUDES = \
 noinst_PROGRAMS = \
 	test-settings-defaults \
 	test-crypto \
-	test-need-secrets \
+	test-secrets \
 	test-general \
 	test-setting-8021x
 
@@ -34,15 +34,15 @@ test_crypto_LDADD = \
 	$(top_builddir)/libnm-util/libnm-util.la \
 	$(GLIB_LIBS)
 
-test_need_secrets_SOURCES = \
-	test-need-secrets.c
+test_secrets_SOURCES = \
+	test-secrets.c
 
-test_need_secrets_CPPFLAGS = \
+test_secrets_CPPFLAGS = \
 	-DTEST_CERT_DIR=\"$(top_srcdir)/libnm-util/tests/certs/\" \
 	$(GLIB_CFLAGS) \
 	$(DBUS_CFLAGS)
 
-test_need_secrets_LDADD = \
+test_secrets_LDADD = \
 	$(top_builddir)/libnm-util/libnm-util.la \
 	$(GLIB_LIBS) \
 	$(DBUS_LIBS)
@@ -73,9 +73,9 @@ test_setting_8021x_LDADD = \
 
 if WITH_TESTS
 
-check-local: test-settings-defaults test-crypto test-need-secrets
+check-local: test-settings-defaults test-crypto test-secrets
 	$(abs_builddir)/test-settings-defaults
-	$(abs_builddir)/test-need-secrets
+	$(abs_builddir)/test-secrets
 	$(abs_builddir)/test-general
 
 # Private key and CA certificate in the same file (PEM)

libnm-util/tests/test-secrets.c
@@ -15,7 +15,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Copyright (C) 2008 - 2009 Red Hat, Inc.
+ * Copyright (C) 2008 - 2011 Red Hat, Inc.
  *
  */
 
@@ -30,6 +30,7 @@
 #include "nm-setting-wired.h"
 #include "nm-setting-8021x.h"
 #include "nm-setting-ip4-config.h"
+#include "nm-setting-wireless.h"
 #include "nm-setting-wireless-security.h"
 #include "nm-setting-cdma.h"
 #include "nm-setting-gsm.h"
@@ -540,6 +541,109 @@ test_need_tls_phase2_secrets_blob (void)
 	g_object_unref (connection);
 }
 
+static void
+value_destroy (gpointer data)
+{
+	GValue *value = (GValue *) data;
+
+	g_value_unset (value);
+	g_slice_free (GValue, value);
+}
+
+static GValue *
+string_to_gvalue (const char *str)
+{
+	GValue *val = g_slice_new0 (GValue);
+
+	g_value_init (val, G_TYPE_STRING);
+	g_value_set_string (val, str);
+	return val;
+}
+
+static GValue *
+uint_to_gvalue (guint32 i)
+{
+	GValue *val;
+
+	val = g_slice_new0 (GValue);
+	g_value_init (val, G_TYPE_UINT);
+	g_value_set_uint (val, i);
+	return val;
+}
+
+static void
+test_update_secrets_wifi (void)
+{
+	NMConnection *connection;
+	NMSettingConnection *s_con;
+	NMSettingWireless *s_wifi;
+	NMSettingWirelessSecurity *s_wsec;
+	unsigned char tmpssid[] = { 0x31, 0x33, 0x33, 0x37 };
+	const char *wepkey = "11111111111111111111111111";
+	GHashTable *secrets;
+	GError *error = NULL;
+	char *uuid;
+	GByteArray *ssid;
+	gboolean success;
+
+	connection = nm_connection_new ();
+	g_assert (connection);
+
+	/* Connection setting */
+	s_con = (NMSettingConnection *) nm_setting_connection_new ();
+	g_assert (s_con);
+
+	uuid = nm_utils_uuid_generate ();
+	g_object_set (s_con,
+	              NM_SETTING_CONNECTION_ID, "Test Wireless",
+	              NM_SETTING_CONNECTION_UUID, uuid,
+	              NM_SETTING_CONNECTION_AUTOCONNECT, FALSE,
+	              NM_SETTING_CONNECTION_TYPE, NM_SETTING_WIRELESS_SETTING_NAME,
+	              NULL);
+	g_free (uuid);
+	nm_connection_add_setting (connection, NM_SETTING (s_con));
+
+	/* Wireless setting */
+	s_wifi = (NMSettingWireless *) nm_setting_wireless_new ();
+	g_assert (s_wifi);
+
+	ssid = g_byte_array_sized_new (sizeof (tmpssid));
+	g_byte_array_append (ssid, &tmpssid[0], sizeof (tmpssid));
+	g_object_set (s_wifi,
+	              NM_SETTING_WIRELESS_SSID, ssid,
+	              NM_SETTING_WIRELESS_SEC, NM_SETTING_WIRELESS_SECURITY_SETTING_NAME,
+	              NULL);
+	g_byte_array_free (ssid, TRUE);
+	nm_connection_add_setting (connection, NM_SETTING (s_wifi));
+
+	/* Wifi security */
+	s_wsec = (NMSettingWirelessSecurity *) nm_setting_wireless_security_new ();
+	g_assert (s_wsec);
+
+	g_object_set (G_OBJECT (s_wsec),
+	              NM_SETTING_WIRELESS_SECURITY_KEY_MGMT, "none",
+	              NULL);
+	nm_connection_add_setting (connection, NM_SETTING (s_wsec));
+
+	/* Build up the secrets hash */
+	secrets = g_hash_table_new_full (g_str_hash, g_str_equal, NULL, value_destroy);
+	g_hash_table_insert (secrets, NM_SETTING_WIRELESS_SECURITY_WEP_KEY0, string_to_gvalue (wepkey));
+	g_hash_table_insert (secrets, NM_SETTING_WIRELESS_SECURITY_WEP_KEY_TYPE, uint_to_gvalue (NM_WEP_KEY_TYPE_KEY));
+
+	success = nm_connection_update_secrets (connection,
+	                                        NM_SETTING_WIRELESS_SECURITY_SETTING_NAME,
+	                                        secrets,
+	                                        &error);
+	if (!success) {
+		/* Print the warning message before we assert success */
+		g_assert (error);
+		g_warning ("Error updating connection secrets: %s", error->message);
+		g_clear_error (&error);
+	}
+	g_assert (success);
+}
+
+
 int main (int argc, char **argv)
 {
 	GError *error = NULL;
@@ -557,6 +661,8 @@ int main (int argc, char **argv)
 	test_need_tls_secrets_blob ();
 	test_need_tls_phase2_secrets_path ();
 	test_need_tls_phase2_secrets_blob ();
+
+	test_update_secrets_wifi ();
 
 	base = g_path_get_basename (argv[0]);
 	fprintf (stdout, "%s: SUCCESS\n", base);