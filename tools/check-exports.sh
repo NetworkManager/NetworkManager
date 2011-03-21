34d153ec19b4e2b88fbfd9c43dff1fda1b562acb
Dan Williams <dcbw@redhat.com>
2011-03-21 12:23:51 -0500

    tools: add script to check library exports against .ver files

---
+++ tools/check-exports.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+LC_ALL=C
+export LC_ALL
+
+stat=0
+so=$1
+def=$2
+
+# Have to prefix with a tab and suffix with a ';' to match .ver file format
+get_syms='( objdump -t "$so" | grep "^[^ ]* [^l.*]*[.]"; objdump -t "$so" | grep "[.]hidden.*"; ) | sed "s/.* //" | sed "s/^/\t/" | sed "s/$/;/"'
+
+echo $so: checking exported symbols against $def
+
+{
+	echo "{"
+	echo "global:"
+	eval $get_syms | sort -u
+	echo "local:"
+	echo "	*;"
+	echo "};"
+} | diff -u "$def" - >&2 || stat=1
+
+exit $stat
+
