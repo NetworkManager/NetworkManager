78ce088843d59d4494965bfc40b30a2e63d065f6
Dan Williams <dcbw@redhat.com>
2011-05-26 22:19:35 -0500

    libnm-util: remove some left-over debugging code

--- libnm-util/nm-setting-vpn.c
+++ libnm-util/nm-setting-vpn.c
@@ -457,7 +457,6 @@ destroy_one_secret (gpointer data)
 	char *secret = (char *) data;
 
 	/* Don't leave the secret lying around in memory */
-g_message ("%s: destroying %s", __func__, secret);
 	memset (secret, 0, strlen (secret));
 	g_free (secret);
 }
