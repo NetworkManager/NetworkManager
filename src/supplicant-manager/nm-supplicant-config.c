NetworkManager/NetworkManager

commit b08e2b8932ed6aa735b4ef4ed04fc173f275b16e

supplicant: If password field is empty, check password-raw

src/supplicant-manager/nm-supplicant-config.c
@@ -728,8 +728,21 @@ nm_supplicant_config_add_setting_8021x (NMSupplicantConfig *self,
 	g_return_val_if_fail (connection_uid != NULL, FALSE);
 
 	value = nm_setting_802_1x_get_password (setting);
-	if (!add_string_val (self, value, "password", FALSE, TRUE))
-		return FALSE;
+	if (value) {
+		if (!add_string_val (self, value, "password", FALSE, TRUE))
+			return FALSE;
+	} else {
+		array = nm_setting_802_1x_get_password_raw (setting);
+		success = nm_supplicant_config_add_option (self,
+		                                           "password",
+		                                           (const char *)array->data,
+		                                           array->len,
+		                                           TRUE);
+		if (!success) {
+			nm_log_warn (LOGD_SUPPLICANT, "Error adding password-raw to supplicant config.");
+			return FALSE;
+		}
+	}
 	value = nm_setting_802_1x_get_pin (setting);
 	if (!add_string_val (self, value, "pin", FALSE, TRUE))
 		return FALSE;

commit b127fda19f9b81ec0bf3e8c6cd04bfcb8c7f30bf

supplicant: fix crash when connecting without both 'password' and 'password-raw'
It is the case e.g. for EAP-TLS. This regression was caused by commit
b08e2b8932ed6aa735b4ef4ed04fc173f275b16e.

src/supplicant-manager/nm-supplicant-config.c
@@ -734,14 +734,16 @@ nm_supplicant_config_add_setting_8021x (NMSupplicantConfig *self,
 			return FALSE;
 	} else {
 		array = nm_setting_802_1x_get_password_raw (setting);
-		success = nm_supplicant_config_add_option (self,
-		                                           "password",
-		                                           (const char *)array->data,
-		                                           array->len,
-		                                           TRUE);
-		if (!success) {
-			nm_log_warn (LOGD_SUPPLICANT, "Error adding password-raw to supplicant config.");
-			return FALSE;
+		if (array) {
+			success = nm_supplicant_config_add_option (self,
+			                                           "password",
+			                                           (const char *)array->data,
+			                                           array->len,
+			                                           TRUE);
+			if (!success) {
+				nm_log_warn (LOGD_SUPPLICANT, "Error adding password-raw to supplicant config.");
+				return FALSE;
+			}
 		}
 	}
 	value = nm_setting_802_1x_get_pin (setting);