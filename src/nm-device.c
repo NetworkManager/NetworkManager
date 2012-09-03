NetworkManager/NetworkManager

commit 6ba93343e6bf41b5182d5836ec5f3ee63e990c68

core: fix possible uninitialized memory usage

src/nm-device.c
@@ -387,6 +387,7 @@ device_get_driver_info (const char *iface, char **driver_version, char **firmwar
 	memset (&req, 0, sizeof (struct ifreq));
 	strncpy (req.ifr_name, iface, IFNAMSIZ);
 	drvinfo.cmd = ETHTOOL_GDRVINFO;
+	memset (&drvinfo, 0, sizeof (drvinfo));
 	req.ifr_data = &drvinfo;
 
 	errno = 0;

commit ddc3f727d039a39756b827e182645b51d26e4e52

core: fix a regression causing that driver and firmware versions are not set
Regression caused by 6ba93343e6bf41b5182d5836ec5f3ee63e990c68

src/nm-device.c
@@ -384,10 +384,10 @@ device_get_driver_info (const char *iface, char **driver_version, char **firmwar
 	}
 
 	/* Get driver and firmware version info */
+	memset (&drvinfo, 0, sizeof (drvinfo));
 	memset (&req, 0, sizeof (struct ifreq));
 	strncpy (req.ifr_name, iface, IFNAMSIZ);
 	drvinfo.cmd = ETHTOOL_GDRVINFO;
-	memset (&drvinfo, 0, sizeof (drvinfo));
 	req.ifr_data = &drvinfo;
 
 	errno = 0;