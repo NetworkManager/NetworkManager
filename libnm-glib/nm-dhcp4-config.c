632c52ceeaf4d240786b64874ddf1601f487d77f
Dan Williams <dcbw@redhat.com>
2013-02-27 18:01:45 +0100

    libnm-glib: fix type annotation of NMDHCP[46]Config:options
    The GOI syntax changed a while back and these didn't follow.

--- libnm-glib/nm-dhcp4-config.c
+++ libnm-glib/nm-dhcp4-config.c
@@ -163,8 +163,7 @@ nm_dhcp4_config_class_init (NMDHCP4ConfigClass *config_class)
 	 *
 	 * The #GHashTable containing options of the configuration.
 	 *
-	 * Type: GLib.HashTable
-	 * Element-Type: utf8,GObject.Value
+	 * Type: GLib.HashTable(utf8,GObject.Value)
 	 **/
 	g_object_class_install_property
 		(object_class, PROP_OPTIONS,
