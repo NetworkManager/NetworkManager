NetworkManager/NetworkManager

commit 53d8f49bcd2a2a49fd01e56321527cd2480014e7

wifi: AP mode connections are always available
Just like adhoc, they don't need to be found in the scan list.

src/nm-device-wifi.c
@@ -1136,7 +1136,7 @@ check_connection_available (NMDevice *device, NMConnection *connection)
 	 * at any time.
 	 */
 	mode = nm_setting_wireless_get_mode (s_wifi);
-	if (g_strcmp0 (mode, "adhoc") == 0)
+	if (g_str_equal (mode, "adhoc") || g_str_equal (mode, "ap"))
 		return TRUE;
 
 	/* Hidden SSIDs obviously don't always appear in the scan list either */

commit fa20c8081998822bd374f3d24e8102720ec351c3

wifi: fix NM crash on mode == NULL
caused by 53d8f49bcd2a2a49fd01e56321527cd2480014e7

src/nm-device-wifi.c
@@ -1132,11 +1132,11 @@ check_connection_available (NMDevice *device, NMConnection *connection)
 
 	s_wifi = nm_connection_get_setting_wireless (connection);
 
-	/* Ad-Hoc connections are always available because they may be started
-	 * at any time.
+	/* Ad-Hoc and AP connections are always available because they may be
+	 * started at any time.
 	 */
 	mode = nm_setting_wireless_get_mode (s_wifi);
-	if (g_str_equal (mode, "adhoc") || g_str_equal (mode, "ap"))
+	if (g_strcmp0 (mode, "adhoc") == 0 || g_strcmp0 (mode, "ap") == 0)
 		return TRUE;
 
 	/* Hidden SSIDs obviously don't always appear in the scan list either */