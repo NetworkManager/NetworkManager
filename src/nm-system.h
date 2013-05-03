393346b4fcd2dac559df54d87befbe7cd2cdc421
Pavel Šimerda <psimerda@redhat.com>
2013-05-04 00:35:30 +0200

    cleanup: remove unused netlink/system code

--- src/nm-system.h
+++ src/nm-system.h
@@ -22,58 +22,20 @@
 #ifndef NETWORK_MANAGER_SYSTEM_H
 #define NETWORK_MANAGER_SYSTEM_H
 
-#include <glib.h>
-#include <netlink/route/route.h>
-
 #include "nm-platform.h"
 #include "nm-device.h"
 #include "nm-ip4-config.h"
 #include "nm-setting-bond.h"
-
-gboolean        nm_system_iface_flush_routes         (int ifindex, int family);
-
-gboolean		nm_system_replace_default_ip4_route   (int ifindex,
-                                                       guint32 gw,
-                                                       guint32 mss);
-
-gboolean		nm_system_replace_default_ip6_route   (int ifindex,
-                                                       const struct in6_addr *gw);
-
-gboolean		nm_system_replace_default_ip4_route_vpn (int ifindex,
-                                                         guint32 ext_gw,
-                                                         guint32 int_gw,
-                                                         guint32 mss,
-                                                         int parent_ifindex,
-                                                         guint32 parent_mss);
-
-gboolean		nm_system_replace_default_ip6_route_vpn (int ifindex,
-                                                         const struct in6_addr *ext_gw,
-                                                         const struct in6_addr *int_gw,
-                                                         guint32 mss,
-                                                         int parent_ifindex,
-                                                         guint32 parent_mss);
 
 NMPlatformIP4Route *nm_system_add_ip4_vpn_gateway_route (NMDevice *parent_device,
                                                         guint32 vpn_gw);
 NMPlatformIP6Route *nm_system_add_ip6_vpn_gateway_route (NMDevice *parent_device,
                                                         const struct in6_addr *vpn_gw);
 
-gboolean        nm_system_iface_flush_addresses         (int ifindex, int family);
-
 gboolean		nm_system_apply_ip4_config              (int ifindex,
                                                          NMIP4Config *config,
                                                          int priority,
                                                          NMIP4ConfigCompareFlags flags);
-
-int             nm_system_set_ip6_route                 (int ifindex,
-                                                         const struct in6_addr *ip6_dest,
-                                                         guint32 ip6_prefix,
-                                                         const struct in6_addr *ip6_gateway,
-                                                         guint32 metric,
-                                                         int mss,
-                                                         int protocol,
-                                                         int table,
-                                                         struct rtnl_route **out_route);
 
 gboolean		nm_system_apply_ip6_config              (int ifindex,
                                                          NMIP6Config *config,
