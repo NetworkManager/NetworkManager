ebc11835429bf7bd949ed4488929dbbd0d75003b
Jiří Klimeš <jklimes@redhat.com>
2013-05-16 12:18:31 +0200

    contrib/nm-live-vm: documentation/man pages now depend on '--enable-gtk-doc'

--- contrib/fedora/nm-live-vm/nm-make-script.sh
+++ contrib/fedora/nm-live-vm/nm-make-script.sh
@@ -9,7 +9,7 @@ test -d NetworkManager || git clone git://anongit.freedesktop.org/NetworkManager
 cd NetworkManager/ || exit 1
 git fetch
 git checkout -f $COMMIT || exit 1
-./autogen.sh --prefix=/ --exec-prefix=/usr --libdir=/usr/lib --datadir=/usr/share --mandir=/usr/share/man --enable-doc || exit 1
+./autogen.sh --prefix=/ --exec-prefix=/usr --libdir=/usr/lib --datadir=/usr/share --mandir=/usr/share/man --enable-gtk-doc || exit 1
 make || exit 1
 #make check || exit 1
 make install || exit 1
