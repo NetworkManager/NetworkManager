8e9e3923b1795d8314b47aabdd99c934ba5bb3f3
Jiří Klimeš <jklimes@redhat.com>
2013-06-24 15:08:13 +0200

    libnm-util: remove 'downdelay' and 'updelay' when setting 'arp_interval'
    Both 'downdelay' and 'updelay' bonding options are only valid with 'miimon'
    that is being removed when setting 'arp_interval'.

--- libnm-util/nm-setting-bond.c
+++ libnm-util/nm-setting-bond.c
@@ -260,8 +260,11 @@ gboolean nm_setting_bond_add_option (NMSettingBond *setting,
 	if (!strcmp (name, NM_SETTING_BOND_OPTION_MIIMON)) {
 		g_hash_table_remove (priv->options, NM_SETTING_BOND_OPTION_ARP_INTERVAL);
 		g_hash_table_remove (priv->options, NM_SETTING_BOND_OPTION_ARP_IP_TARGET);
-	} else if (!strcmp (name, NM_SETTING_BOND_OPTION_ARP_INTERVAL))
+	} else if (!strcmp (name, NM_SETTING_BOND_OPTION_ARP_INTERVAL)) {
 		g_hash_table_remove (priv->options, NM_SETTING_BOND_OPTION_MIIMON);
+		g_hash_table_remove (priv->options, NM_SETTING_BOND_OPTION_DOWNDELAY);
+		g_hash_table_remove (priv->options, NM_SETTING_BOND_OPTION_UPDELAY);
+	}
 
 	return TRUE;
 }
