NetworkManager/NetworkManager

commit 0e361e894c4c3b699e89c68ccc7fee6b4adb09f9

core: turn link_changed() into a proper virtual function

src/devices/nm-device-gre.c
@@ -59,11 +59,13 @@ enum {
 /**************************************************************/
 
 static void
-link_changed (NMDevice *device)
+link_changed (NMDevice *device, NMPlatformLink *info)
 {
 	NMDeviceGrePrivate *priv = NM_DEVICE_GRE_GET_PRIVATE (device);
 	GObject *object = G_OBJECT (device);
 	NMPlatformGreProperties props;
+
+	NM_DEVICE_CLASS (nm_device_gre_parent_class)->link_changed (device, info);
 
 	if (!nm_platform_gre_get_properties (nm_device_get_ifindex (device), &props)) {
 		nm_log_warn (LOGD_HW, "(%s): could not read gre properties",
@@ -128,7 +130,7 @@ nm_device_gre_init (NMDeviceGre *self)
 static void
 constructed (GObject *object)
 {
-	link_changed (NM_DEVICE (object));
+	link_changed (NM_DEVICE (object), NULL);
 
 	G_OBJECT_CLASS (nm_device_gre_parent_class)->constructed (object);
 }

src/devices/nm-device-macvlan.c
@@ -54,11 +54,13 @@ enum {
 /**************************************************************/
 
 static void
-link_changed (NMDevice *device)
+link_changed (NMDevice *device, NMPlatformLink *info)
 {
 	NMDeviceMacvlanPrivate *priv = NM_DEVICE_MACVLAN_GET_PRIVATE (device);
 	GObject *object = G_OBJECT (device);
 	NMPlatformMacvlanProperties props;
+
+	NM_DEVICE_CLASS (nm_device_macvlan_parent_class)->link_changed (device, info);
 
 	if (!nm_platform_macvlan_get_properties (nm_device_get_ifindex (device), &props)) {
 		nm_log_warn (LOGD_HW, "(%s): could not read macvlan properties",
@@ -108,7 +110,7 @@ nm_device_macvlan_init (NMDeviceMacvlan *self)
 static void
 constructed (GObject *object)
 {
-	link_changed (NM_DEVICE (object));
+	link_changed (NM_DEVICE (object), NULL);
 
 	G_OBJECT_CLASS (nm_device_macvlan_parent_class)->constructed (object);
 }

src/devices/nm-device-tun.c
@@ -53,11 +53,13 @@ enum {
 };
 
 static void
-link_changed (NMDevice *device)
+link_changed (NMDevice *device, NMPlatformLink *info)
 {
 	NMDeviceTunPrivate *priv = NM_DEVICE_TUN_GET_PRIVATE (device);
 	GObject *object = G_OBJECT (device);
 	NMPlatformTunProperties props;
+
+	NM_DEVICE_CLASS (nm_device_tun_parent_class)->link_changed (device, info);
 
 	if (!nm_platform_tun_get_properties (nm_device_get_ifindex (device), &props)) {
 		nm_log_warn (LOGD_HW, "(%s): could not read tun properties",

src/devices/nm-device.c
@@ -1151,15 +1151,21 @@ nm_device_set_carrier (NMDevice *device, gboolean carrier)
 static void
 link_changed_cb (NMPlatform *platform, int ifindex, NMPlatformLink *info, NMDevice *device)
 {
+	NMDeviceClass *klass = NM_DEVICE_GET_CLASS (device);
+
 	if (ifindex != nm_device_get_ifindex (device))
 		return;
 
+	if (klass->link_changed)
+		klass->link_changed (device, info);
+}
+
+static void
+link_changed (NMDevice *device, NMPlatformLink *info)
+{
 	if (   device_has_capability (device, NM_DEVICE_CAP_CARRIER_DETECT)
 	    && !device_has_capability (device, NM_DEVICE_CAP_NONSTANDARD_CARRIER))
 		nm_device_set_carrier (device, info->connected);
-
-	if (NM_DEVICE_GET_CLASS (device)->link_changed)
-		NM_DEVICE_GET_CLASS (device)->link_changed (device);
 }
 
 static void
@@ -5123,6 +5129,8 @@ nm_device_class_init (NMDeviceClass *klass)
 	object_class->get_property = get_property;
 	object_class->constructor = constructor;
 	object_class->constructed = constructed;
+
+	klass->link_changed = link_changed;
 
 	klass->is_available = is_available;
 	klass->act_stage1_prepare = act_stage1_prepare;

src/devices/nm-device.h
@@ -101,6 +101,8 @@ typedef struct {
 	                       NMDeviceState old_state,
 	                       NMDeviceStateReason reason);
 
+	void            (* link_changed) (NMDevice *self, NMPlatformLink *info);
+
 	/* Hardware state (IFF_UP) */
 	gboolean        (*is_up)      (NMDevice *self);
 	gboolean        (*bring_up)   (NMDevice *self, gboolean *no_firmware);
@@ -186,8 +188,6 @@ typedef struct {
 
 	gboolean        (* have_any_ready_slaves) (NMDevice *self,
 	                                           const GSList *slaves);
-
-	void            (* link_changed) (NMDevice *self);
 } NMDeviceClass;
 
 

commit 34d28376ed0979f547249a59c2e9b1e47e0eaa97

core: fix segfault on initialization of macvlan and gre device
The link_changed method expects a valid info parameter.
NMDeviceMacvlan and NMDeviceGre calls link_changed
during construction for initialization.

As it was before, NMDeviceMacvlan and NMDeviceGre passed
NULL as NMPlatformLink, causing NM to segfault.

(Regression was introduced in 0e361e894c4c3b699e89c68ccc7fee6b4adb09f9)

https://bugzilla.redhat.com/show_bug.cgi?id=997396

Signed-off-by: Thomas Haller <thaller@redhat.com>

src/devices/nm-device-gre.c
@@ -59,13 +59,11 @@ enum {
 /**************************************************************/
 
 static void
-link_changed (NMDevice *device, NMPlatformLink *info)
+update_properties (NMDevice *device)
 {
 	NMDeviceGrePrivate *priv = NM_DEVICE_GRE_GET_PRIVATE (device);
 	GObject *object = G_OBJECT (device);
 	NMPlatformGreProperties props;
-
-	NM_DEVICE_CLASS (nm_device_gre_parent_class)->link_changed (device, info);
 
 	if (!nm_platform_gre_get_properties (nm_device_get_ifindex (device), &props)) {
 		nm_log_warn (LOGD_HW, "(%s): could not read gre properties",
@@ -108,6 +106,13 @@ link_changed (NMDevice *device, NMPlatformLink *info)
 	g_object_thaw_notify (object);
 }
 
+static void
+link_changed (NMDevice *device, NMPlatformLink *info)
+{
+	NM_DEVICE_CLASS (nm_device_gre_parent_class)->link_changed (device, info);
+	update_properties (device);
+}
+
 /**************************************************************/
 
 NMDevice *
@@ -130,7 +135,7 @@ nm_device_gre_init (NMDeviceGre *self)
 static void
 constructed (GObject *object)
 {
-	link_changed (NM_DEVICE (object), NULL);
+	update_properties (NM_DEVICE (object));
 
 	G_OBJECT_CLASS (nm_device_gre_parent_class)->constructed (object);
 }

src/devices/nm-device-macvlan.c
@@ -54,13 +54,11 @@ enum {
 /**************************************************************/
 
 static void
-link_changed (NMDevice *device, NMPlatformLink *info)
+update_properties (NMDevice *device)
 {
 	NMDeviceMacvlanPrivate *priv = NM_DEVICE_MACVLAN_GET_PRIVATE (device);
 	GObject *object = G_OBJECT (device);
 	NMPlatformMacvlanProperties props;
-
-	NM_DEVICE_CLASS (nm_device_macvlan_parent_class)->link_changed (device, info);
 
 	if (!nm_platform_macvlan_get_properties (nm_device_get_ifindex (device), &props)) {
 		nm_log_warn (LOGD_HW, "(%s): could not read macvlan properties",
@@ -88,6 +86,13 @@ link_changed (NMDevice *device, NMPlatformLink *info)
 	g_object_thaw_notify (object);
 }
 
+static void
+link_changed (NMDevice *device, NMPlatformLink *info)
+{
+	NM_DEVICE_CLASS (nm_device_macvlan_parent_class)->link_changed (device, info);
+	update_properties (device);
+}
+
 /**************************************************************/
 
 NMDevice *
@@ -110,7 +115,7 @@ nm_device_macvlan_init (NMDeviceMacvlan *self)
 static void
 constructed (GObject *object)
 {
-	link_changed (NM_DEVICE (object), NULL);
+	update_properties (NM_DEVICE (object));
 
 	G_OBJECT_CLASS (nm_device_macvlan_parent_class)->constructed (object);
 }