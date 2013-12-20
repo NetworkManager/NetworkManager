0fe46a5839e268a6a56eb39f538a1cb97d5b716e
Dan Winship <danw@gnome.org>
2013-12-20 12:47:55 -0500

    tui: reorganize "nmtui connect" code
    Make NmtConnectConnectionList just a dumb connection list, and have
    the actual connecting code be in nmtui-connect.c

--- tui/nmtui-connect.c
+++ tui/nmtui-connect.c
@@ -39,41 +39,138 @@
 #include "nmtui.h"
 #include "nmtui-connect.h"
 #include "nmt-connect-connection-list.h"
+#include "nmt-password-dialog.h"
+#include "nmt-secret-agent.h"
 #include "nmt-utils.h"
 
 static void
-connect_complete (GObject      *object,
-                  GAsyncResult *result,
-                  gpointer      user_data)
+secrets_requested (NmtSecretAgent *agent,
+                   const char     *request_id,
+                   const char     *title,
+                   const char     *msg,
+                   GPtrArray      *secrets,
+                   gpointer        user_data)
+{
+	NmtNewtForm *form;
+
+	form = nmt_password_dialog_new (request_id, title, msg, secrets);
+	nmt_newt_form_run_sync (form);
+
+	if (nmt_password_dialog_succeeded (NMT_PASSWORD_DIALOG (form)))
+		nmt_secret_agent_response (agent, request_id, secrets);
+	else
+		nmt_secret_agent_response (agent, request_id, NULL);
+
+	g_object_unref (form);
+}
+
+static void
+activate_ac_state_changed (GObject    *object,
+                           GParamSpec *pspec,
+                           gpointer    user_data)
 {
 	NmtSyncOp *op = user_data;
+	NMActiveConnectionState state;
 	GError *error = NULL;
 
-	if (nmt_connect_connection_list_activate_finish (NMT_CONNECT_CONNECTION_LIST (object),
-	                                                 result, &error))
-		nmt_sync_op_complete_boolean (op, TRUE, NULL);
-	else
-		nmt_sync_op_complete_boolean (op, FALSE, error);
+	state = nm_active_connection_get_state (NM_ACTIVE_CONNECTION (object));
+	if (state == NM_ACTIVE_CONNECTION_STATE_ACTIVATING)
+		return;
+
+	if (state != NM_ACTIVE_CONNECTION_STATE_ACTIVATED) {
+		error = g_error_new_literal (NM_CLIENT_ERROR, NM_CLIENT_ERROR_UNKNOWN,
+		                             _("Activation failed"));
+	}
+
+	g_signal_handlers_disconnect_by_func (object, G_CALLBACK (activate_ac_state_changed), op);
+	g_object_unref (object);
+
+	nmt_sync_op_complete_boolean (op, error == NULL, error);
 	g_clear_error (&error);
 }
 
 static void
-nmt_connect_connection (const char *identifier)
+activation_callback (NMClient           *client,
+                     NMActiveConnection *ac,
+                     GError             *error,
+                     gpointer            user_data)
 {
-	NmtNewtWidget *list;
+	NmtSyncOp *op = user_data;
+
+	if (error || nm_active_connection_get_state (ac) == NM_ACTIVE_CONNECTION_STATE_ACTIVATED) {
+		nmt_sync_op_complete_boolean (op, error == NULL, error);
+		return;
+	}
+
+	g_object_ref (ac);
+	g_signal_connect (ac, "notify::" NM_ACTIVE_CONNECTION_STATE,
+	                  G_CALLBACK (activate_ac_state_changed), op);
+}
+
+static void
+activate_connection (NMConnection *connection,
+                     NMDevice     *device,
+                     NMObject     *specific_object)
+{
+	NmtNewtForm *form;
+	NMSecretAgent *agent;
+	NmtNewtWidget *label;
 	NmtSyncOp op;
 	GError *error = NULL;
 
+	form = g_object_new (NMT_TYPE_NEWT_FORM, NULL);
+	label = nmt_newt_label_new (_("Connecting..."));
+	nmt_newt_form_set_content (form, label);
+
+	agent = nmt_secret_agent_new ();
+	nm_secret_agent_register (agent);
+	g_signal_connect (agent, "request-secrets", G_CALLBACK (secrets_requested), NULL);
+
+	/* FIXME: cancel button */
+
 	nmt_sync_op_init (&op);
-	list = nmt_connect_connection_list_new ();
-	nmt_connect_connection_list_activate_async (NMT_CONNECT_CONNECTION_LIST (list), identifier,
-	                                            connect_complete, &op);
+	nm_client_activate_connection (nm_client,
+	                               connection, device,
+	                               specific_object ? nm_object_get_path (specific_object) : NULL,
+	                               activation_callback, &op);
+	nmt_newt_form_show (form);
+
 	if (!nmt_sync_op_wait_boolean (&op, &error)) {
 		nmt_newt_message_dialog (_("Could not activate connection: %s"), error->message);
 		g_error_free (error);
-		nmtui_quit ();
 	}
-	g_object_unref (list);
+
+	nmt_newt_form_quit (form);
+	g_object_unref (form);
+
+	/* If the activation failed very quickly, then agent won't be registered yet,
+	 * and nm_secret_agent_unregister() would complain if we called it...
+	 */
+	if (nm_secret_agent_get_registered (agent))
+		nm_secret_agent_unregister (agent);
+	g_object_unref (agent);
+}
+
+static void
+listbox_activated (NmtNewtListbox *listbox,
+                   gpointer        user_data)
+{
+	NmtConnectConnectionList *list = NMT_CONNECT_CONNECTION_LIST (listbox);
+	NMConnection *connection;
+	NMDevice *device;
+	NMObject *specific_object;
+
+	if (!nmt_connect_connection_list_get_selection (list, &connection, &device, &specific_object))
+		return;
+
+	activate_connection (connection, device, specific_object);
+}
+
+static void
+activate_clicked (NmtNewtButton *button,
+                  gpointer       listbox)
+{
+	listbox_activated (listbox, NULL);
 }
 
 static void
@@ -105,13 +202,15 @@ nmt_connect_connection_list (void)
 	nmt_newt_grid_set_flags (NMT_NEWT_GRID (grid), list,
 	                         NMT_NEWT_GRID_FILL_X | NMT_NEWT_GRID_FILL_Y |
 	                         NMT_NEWT_GRID_EXPAND_X | NMT_NEWT_GRID_EXPAND_Y);
+	g_signal_connect (list, "activated", G_CALLBACK (listbox_activated), NULL);
 
 	bbox = nmt_newt_button_box_new (NMT_NEWT_BUTTON_BOX_VERTICAL);
 	nmt_newt_grid_add (NMT_NEWT_GRID (grid), bbox, 1, 0);
 	nmt_newt_widget_set_padding (bbox, 1, 1, 0, 1);
 
-	// FIXME: the activate button doesn't do anything
 	activate = nmt_newt_button_box_add_start (NMT_NEWT_BUTTON_BOX (bbox), _("Activate"));
+	g_signal_connect (activate, "clicked", G_CALLBACK (activate_clicked), list);
+
 	quit = nmt_newt_button_box_add_end (NMT_NEWT_BUTTON_BOX (bbox), _("Quit"));
 	nmt_newt_widget_set_exit_on_activate (quit, TRUE);
 	g_signal_connect (quit, "clicked", G_CALLBACK (quit_clicked), NULL);
@@ -119,6 +218,25 @@ nmt_connect_connection_list (void)
 	nmt_newt_form_set_content (form, grid);
 	nmt_newt_form_show (form);
 	g_object_unref (form);
+}
+
+static void
+nmt_connect_connection (const char *identifier)
+{
+	NmtNewtWidget *list;
+	NMConnection *connection;
+	NMDevice *device;
+	NMObject *specific_object;
+
+	list = nmt_connect_connection_list_new ();
+	if (nmt_connect_connection_list_get_connection (NMT_CONNECT_CONNECTION_LIST (list), identifier,
+	                                                &connection, &device, &specific_object))
+		activate_connection (connection, device, specific_object);
+	else
+		nmt_newt_message_dialog (_("No such connection '%s'"), identifier);
+	g_object_unref (list);
+
+	nmtui_quit ();
 }
 
 void
