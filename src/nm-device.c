NetworkManager/NetworkManager

commit 46be6b344deb428f8ec7968527b782b6c3feedcc

core: fix stale DHCPv4 information for dispatcher lease-change events (bgo #721002)
dhcp4_lease_change() needs to be called *after* updating the DHCP config
object to ensure that the dispatcher gets the new values.

src/nm-device.c
@@ -1952,21 +1952,23 @@ dhcp4_state_changed (NMDHCPClient *client,
 	case DHC_REBOOT:     /* have valid lease, but now obtained a different one */
 	case DHC_REBIND4:    /* new, different lease */
 		config = nm_dhcp_client_get_ip4_config (priv->dhcp4_client, FALSE);
+
+		/* Update the DHCP4 config object with new DHCP options */
+		nm_dhcp4_config_reset (priv->dhcp4_config);
+		if (config) {
+			nm_dhcp_client_foreach_option (priv->dhcp4_client,
+			                               dhcp4_add_option_cb,
+			                               config);
+		}
+		g_object_notify (G_OBJECT (device), NM_DEVICE_DHCP4_CONFIG);
+
 		if (priv->ip4_state == IP_CONF)
 			nm_device_activate_schedule_ip4_config_result (device, config);
 		else if (priv->ip4_state == IP_DONE)
 			dhcp4_lease_change (device, config);
 
-		if (config) {
-			/* Update the DHCP4 config object with new DHCP options */
-			nm_dhcp4_config_reset (priv->dhcp4_config);
-			nm_dhcp_client_foreach_option (priv->dhcp4_client,
-			                               dhcp4_add_option_cb,
-			                               priv->dhcp4_config);
-			g_object_notify (G_OBJECT (device), NM_DEVICE_DHCP4_CONFIG);
-
+		if (config)
 			g_object_unref (config);
-		}
 		break;
 	case DHC_TIMEOUT: /* timed out contacting DHCP server */
 		dhcp4_fail (device, TRUE);

commit 54b6501d020506497f33e204fbabd24052ea0899

core: bugfix potential crash in dhcp4_state_changed
Causes glib warning
  GLib-GObject-WARNING **: invalid cast from 'NMIP4Config' to 'NMDHCP4Config'

  #0  0x0000003370c504e9 in g_logv () from /lib64/libglib-2.0.so.0
  #1  0x0000003370c5063f in g_log () from /lib64/libglib-2.0.so.0
  #2  0x0000003371c32c43 in g_type_check_instance_cast () from /lib64/libgobject-2.0.so.0
  #3  0x000000000042aece in dhcp4_add_option_cb (key=0x1df8350, value=0x1dbca10, user_data=0x1e0d9f0) at nm-device.c:1884
  #4  0x00000000004953e5 in nm_dhcp_client_foreach_option (self=0x1dcb2a0, func=0x42aea3 <dhcp4_add_option_cb>, user_data=0x1e0d9f0) at nm-dhcp-client.c:807
  #5  0x000000000042b1dc in dhcp4_state_changed (client=0x1dcb2a0, state=DHC_BOUND4, user_data=0x1de4330) at nm-device.c:1959
  #6  0x0000003371c126ac in g_cclosure_marshal_VOID(unsigned int0_t, void) () from /lib64/libgobject-2.0.so.0
  #7  0x0000003371c104c7 in _g_closure_invoke_va () from /lib64/libgobject-2.0.so.0
  #8  0x0000003371c29749 in g_signal_emit_valist () from /lib64/libgobject-2.0.so.0
  #9  0x0000003371c2a3af in g_signal_emit () from /lib64/libgobject-2.0.so.0
  #10 0x0000000000493d01 in dhcp_client_set_state (self=0x1dcb2a0, state=DHC_BOUND4, emit_state=1, remove_now=0) at nm-dhcp-client.c:241
  ...

regression, introduced by 46be6b344deb428f8ec7968527b782b6c3feedcc.

Signed-off-by: Thomas Haller <thaller@redhat.com>

src/nm-device.c
@@ -1958,7 +1958,7 @@ dhcp4_state_changed (NMDHCPClient *client,
 		if (config) {
 			nm_dhcp_client_foreach_option (priv->dhcp4_client,
 			                               dhcp4_add_option_cb,
-			                               config);
+			                               priv->dhcp4_config);
 		}
 		g_object_notify (G_OBJECT (device), NM_DEVICE_DHCP4_CONFIG);
 