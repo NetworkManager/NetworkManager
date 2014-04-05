4618a0730418b99b5f81af2d1843649a7b8186ae
Dan Winship <danw@gnome.org>
2014-04-05 09:45:28 -0400

    libnm-util: use g_test_expect_message() in tests
    Use g_test_expect_message(), to avoid spewing tons of error messages
    when (successfully) running "make check".

--- libnm-util/tests/test-setting-dcb.c
+++ libnm-util/tests/test-setting-dcb.c
@@ -22,6 +22,7 @@
 #include <glib.h>
 #include <string.h>
 #include <nm-utils.h>
+#include <nm-glib-compat.h>
 #include "nm-setting-dcb.h"
 
 #define DCB_FLAGS_ALL (NM_SETTING_DCB_FLAG_ENABLE | \
@@ -91,12 +92,26 @@ test_dcb_flags_invalid (void)
 	s_dcb = (NMSettingDcb *) nm_setting_dcb_new ();
 	g_assert (s_dcb);
 
+	g_test_expect_message ("GLib-GObject", G_LOG_LEVEL_WARNING, "*invalid or out of range*");
 	TEST_FLAG (NM_SETTING_DCB_APP_FCOE_FLAGS, nm_setting_dcb_get_app_fcoe_flags, 0x332523);
+	g_test_assert_expected_messages ();
+
+	g_test_expect_message ("GLib-GObject", G_LOG_LEVEL_WARNING, "*invalid or out of range*");
 	TEST_FLAG (NM_SETTING_DCB_APP_ISCSI_FLAGS, nm_setting_dcb_get_app_iscsi_flags, 0xFF);
+	g_test_assert_expected_messages ();
+
+	g_test_expect_message ("GLib-GObject", G_LOG_LEVEL_WARNING, "*invalid or out of range*");
 	TEST_FLAG (NM_SETTING_DCB_APP_FIP_FLAGS, nm_setting_dcb_get_app_fip_flags, 0x1111);
+	g_test_assert_expected_messages ();
+
+	g_test_expect_message ("GLib-GObject", G_LOG_LEVEL_WARNING, "*invalid or out of range*");
 	TEST_FLAG (NM_SETTING_DCB_PRIORITY_FLOW_CONTROL_FLAGS, nm_setting_dcb_get_priority_flow_control_flags, G_MAXUINT32);
+	g_test_assert_expected_messages ();
+
+	g_test_expect_message ("GLib-GObject", G_LOG_LEVEL_WARNING, "*invalid or out of range*");
 	TEST_FLAG (NM_SETTING_DCB_PRIORITY_GROUP_FLAGS, nm_setting_dcb_get_priority_group_flags,
 	           (NM_SETTING_DCB_FLAG_ENABLE | NM_SETTING_DCB_FLAG_ADVERTISE | NM_SETTING_DCB_FLAG_WILLING) + 1);
+	g_test_assert_expected_messages ();
 }
 
 #define TEST_APP_PRIORITY(lcprop, ucprop, v) \
@@ -295,7 +310,9 @@ int main (int argc, char **argv)
 	g_assert_no_error (error);
 	g_assert (success);
 
+#if !GLIB_CHECK_VERSION(2,34,0)
 	g_log_set_always_fatal (G_LOG_LEVEL_CRITICAL);
+#endif
 
 	g_test_add_func (TPATH "flags-valid", test_dcb_flags_valid);
 	g_test_add_func (TPATH "flags-invalid", test_dcb_flags_invalid);
