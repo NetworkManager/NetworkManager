9c6ac81aa365025e6210741361ab79176ede206e
Thomas Haller <thaller@redhat.com>
2014-05-30 21:19:10 +0200

    bluez: fix crash when removing NMBluezDevice
    Fixes regression introduced by commit 4674f1039940fadc7bbc8573323e8dac0ad2f727.
    
    Signed-off-by: Thomas Haller <thaller@redhat.com>

--- src/devices/bluetooth/nm-device-bt.c
+++ src/devices/bluetooth/nm-device-bt.c
@@ -1202,7 +1202,6 @@ finalize (GObject *object)
 	NMDeviceBtPrivate *priv = NM_DEVICE_BT_GET_PRIVATE (object);
 
 	g_free (priv->rfcomm_iface);
-	g_free (priv->bdaddr);
 	g_free (priv->name);
 
 	G_OBJECT_CLASS (nm_device_bt_parent_class)->finalize (object);
