NetworkManager/NetworkManager

commit d00e2147de148bcec1d32023bda8a90dab552a18

core: implement PRE_DOWN dispatcher actions (bgo #387832)
https://bugzilla.gnome.org/show_bug.cgi?id=387832

src/devices/nm-device.c
@@ -216,6 +216,7 @@ typedef struct {
 	guint           act_source6_id;
 	gpointer        act_source6_func;
 	guint           recheck_assume_id;
+	guint           dispatcher_id;
 
 	/* Link stuff */
 	guint           link_connected_id;
@@ -6464,6 +6465,29 @@ nm_device_cleanup (NMDevice *self, NMDeviceStateReason reason)
 
 /***********************************************************/
 
+static void
+dispatcher_pre_down_done (guint call_id, gpointer user_data)
+{
+	NMDevice *self = NM_DEVICE (user_data);
+	NMDevicePrivate *priv = NM_DEVICE_GET_PRIVATE (self);
+
+	g_return_if_fail (call_id == priv->dispatcher_id);
+
+	priv->dispatcher_id = 0;
+	nm_device_queue_state (self, NM_DEVICE_STATE_DISCONNECTED, NM_DEVICE_STATE_REASON_NONE);
+}
+
+static void
+dispatcher_cleanup (NMDevice *self)
+{
+	NMDevicePrivate *priv = NM_DEVICE_GET_PRIVATE (self);
+
+	if (priv->dispatcher_id) {
+		nm_dispatcher_call_cancel (priv->dispatcher_id);
+		priv->dispatcher_id = 0;
+	}
+}
+
 static gboolean
 ip_config_valid (NMDeviceState state)
 {
@@ -6524,6 +6548,8 @@ nm_device_state_changed (NMDevice *device,
 
 	/* Clear any queued transitions */
 	nm_device_queued_state_clear (device);
+
+	dispatcher_cleanup (device);
 
 	/* Cache the activation request for the dispatcher */
 	req = priv->act_request ? g_object_ref (priv->act_request) : NULL;
@@ -6621,7 +6647,15 @@ nm_device_state_changed (NMDevice *device,
 		}
 		break;
 	case NM_DEVICE_STATE_DEACTIVATING:
-		nm_device_queue_state (device, NM_DEVICE_STATE_DISCONNECTED, reason);
+		if (!nm_dispatcher_call (DISPATCHER_ACTION_PRE_DOWN,
+		                         nm_act_request_get_connection (req),
+		                         device,
+		                         dispatcher_pre_down_done,
+		                         device,
+		                         &priv->dispatcher_id)) {
+			/* Just proceed on errors */
+			dispatcher_pre_down_done (0, device);
+		}
 		break;
 	case NM_DEVICE_STATE_DISCONNECTED:
 		if (priv->queued_act_request) {
@@ -6687,7 +6721,8 @@ nm_device_state_changed (NMDevice *device,
 	if (state > NM_DEVICE_STATE_DISCONNECTED)
 		delete_on_deactivate_unschedule (device);
 
-	if (old_state == NM_DEVICE_STATE_ACTIVATED)
+	if (   (old_state == NM_DEVICE_STATE_ACTIVATED || old_state == NM_DEVICE_STATE_DEACTIVATING)
+	    && (state != NM_DEVICE_STATE_DEACTIVATING))
 		nm_dispatcher_call (DISPATCHER_ACTION_DOWN, nm_act_request_get_connection (req), device, NULL, NULL, NULL);
 
 	/* IP-related properties are only valid when the device has IP configuration.
@@ -7185,6 +7220,8 @@ dispose (GObject *object)
 	NMDevice *self = NM_DEVICE (object);
 	NMDevicePrivate *priv = NM_DEVICE_GET_PRIVATE (self);
 	NMPlatform *platform;
+
+	dispatcher_cleanup (self);
 
 	_cleanup_generic_pre (self, FALSE);
 

commit 524fc6d454e063cd84b004d23e2a0ccc163b8588

core: preserve reason on device deactivation while pre-down (fix tearing down slave when deactivating master)
When delaying the deactivation of a device during dispatcher-pre-down,
we must preseve the reason to pass it on.

This is especially important, because nm_device_slave_notify_release()
checks for the reason, and does not deactivate the slave if no reason is
given. This error caused slaves the be left up when deactivating the master.

Also update the call to nm_device_slave_notify_release() to ensure we
have a valid state reason when configuring the slave. This would have
pointed out the issue and would even work around it.

Regression introduced by commit d00e2147de148bcec1d32023bda8a90dab552a18.

Signed-off-by: Thomas Haller <thaller@redhat.com>

src/devices/nm-device.c
@@ -217,6 +217,7 @@ typedef struct {
 	gpointer        act_source6_func;
 	guint           recheck_assume_id;
 	guint           dispatcher_id;
+	NMDeviceStateReason dispatcher_pre_down_reason;
 
 	/* Link stuff */
 	guint           link_connected_id;
@@ -831,8 +832,12 @@ nm_device_release_one_slave (NMDevice *dev, NMDevice *slave, gboolean configure)
 		reason = NM_DEVICE_STATE_REASON_NONE;
 	else if (priv->state == NM_DEVICE_STATE_FAILED)
 		reason = NM_DEVICE_STATE_REASON_DEPENDENCY_FAILED;
-	else
+	else if (priv->state_reason != NM_DEVICE_STATE_REASON_NONE)
 		reason = priv->state_reason;
+	else {
+		g_warn_if_reached ();
+		reason = NM_DEVICE_STATE_REASON_UNKNOWN;
+	}
 	nm_device_slave_notify_release (info->slave, reason);
 
 	free_slave_info (info);
@@ -6530,7 +6535,7 @@ dispatcher_pre_down_done (guint call_id, gpointer user_data)
 	g_return_if_fail (call_id == priv->dispatcher_id);
 
 	priv->dispatcher_id = 0;
-	nm_device_queue_state (self, NM_DEVICE_STATE_DISCONNECTED, NM_DEVICE_STATE_REASON_NONE);
+	nm_device_queue_state (self, NM_DEVICE_STATE_DISCONNECTED, priv->dispatcher_pre_down_reason);
 }
 
 static void
@@ -6709,6 +6714,7 @@ _set_state_full (NMDevice *device,
 			                         nm_act_request_get_connection (req),
 			                         device);
 		} else {
+			priv->dispatcher_pre_down_reason = reason;
 			if (!nm_dispatcher_call (DISPATCHER_ACTION_PRE_DOWN,
 			                         nm_act_request_get_connection (req),
 			                         device,