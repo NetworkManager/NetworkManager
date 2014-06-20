ebeaeaed4d3180c27cc059fe0ff199153fa9ec24
Dan Winship <danw@gnome.org>
2014-06-20 15:41:24 -0400

    core: make veth devices default-unmanaged for now
    We only want to treat a veth device as ethernet if (a) NM is running
    inside a container, and (b) the veth in question is the "inside" end
    of the veth, not the outside. Unfortunately, we don't have good
    heuristics for this at the moment, so just ignore veths for now.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=731014

--- src/devices/nm-device-veth.c
+++ src/devices/nm-device-veth.c
@@ -111,6 +111,7 @@ nm_device_veth_new (NMPlatformLink *platform_device)
 static void
 nm_device_veth_init (NMDeviceVeth *self)
 {
+	nm_device_set_initial_unmanaged_flag (NM_DEVICE (self), NM_UNMANAGED_DEFAULT, TRUE);
 }
 
 static void
