bef8723189523b550aad1adc6d7b1617a7a26c53
Thomas Haller <thaller@redhat.com>
2014-07-26 21:41:41 +0200

    core: fix warning about comparing unsigned enum values being positive
    clang warns:
    
        make[4]: Entering directory `./NetworkManager/src'
          CC       nm-device.lo
        devices/nm-device.c:367:12: error: comparison of unsigned enum expression >= 0 is always true [-Werror,-Wtautological-compare]
                if (state >= 0 && state < G_N_ELEMENTS (state_table))
                    ~~~~~ ^  ~
        devices/nm-device.c:443:13: error: comparison of unsigned enum expression >= 0 is always true [-Werror,-Wtautological-compare]
                if (reason >= 0 && reason < G_N_ELEMENTS (reason_table))
                    ~~~~~~ ^  ~
    
    Signed-off-by: Thomas Haller <thaller@redhat.com>
    (cherry picked from commit a2a36d845066a5e3ad1b6d2953993a1a433340ff)
    
    Conflicts:
    	src/dhcp-manager/nm-dhcp-client.c

--- src/dhcp-manager/nm-dhcp-client.c
+++ src/dhcp-manager/nm-dhcp-client.c
@@ -607,7 +607,7 @@ static const char *state_table[] = {
 static const char *
 state_to_string (NMDHCPState state)
 {
-	if (state >= 0 && state < G_N_ELEMENTS (state_table))
+	if ((gsize) state < G_N_ELEMENTS (state_table))
 		return state_table[state];
 	return NULL;
 }
