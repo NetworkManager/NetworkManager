NetworkManager/NetworkManager

commit 6a4127cfa063a77e3ee886a999441c71f6a0e11c

libnm-core: change list-of-string and array-of-string properties to G_TYPE_STRV
Change all DBUS_TYPE_G_LIST_OF_STRING and DBUS_TYPE_G_ARRAY_OF_STRING
properties to G_TYPE_STRV, and update everything accordingly.

(This doesn't actually require using
_nm_setting_class_transform_property(); dbus-glib is happy to transform
between 'as' and G_TYPE_STRV.)

src/settings/plugins/ifcfg-rh/reader.c
@@ -2915,8 +2915,7 @@ read_8021x_list_value (shvarFile *ifcfg,
                        const char *prop_name)
 {
 	char *value;
-	char **strv, **iter;
-	GSList *gslist = NULL;
+	char **strv;
 
 	g_return_if_fail (ifcfg != NULL);
 	g_return_if_fail (ifcfg_var_name != NULL);
@@ -2927,16 +2926,8 @@ read_8021x_list_value (shvarFile *ifcfg,
 		return;
 
 	strv = g_strsplit_set (value, " \t", 0);
-	for (iter = strv; iter && *iter; iter++) {
-		if (*iter[0] == '\0')
-			continue;
-		gslist = g_slist_prepend (gslist, *iter);
-	}
-	if (gslist) {
-		gslist = g_slist_reverse (gslist);
-		g_object_set (setting, prop_name, gslist, NULL);
-		g_slist_free (gslist);
-	}
+	if (strv && strv[0])
+		g_object_set (setting, prop_name, strv, NULL);
 	g_strfreev (strv);
 	g_free (value);
 }
@@ -3251,7 +3242,6 @@ make_wireless_setting (shvarFile *ifcfg,
 {
 	NMSettingWireless *s_wireless;
 	GByteArray *array = NULL;
-	GSList *macaddr_blacklist = NULL;
 	char *value = NULL;
 
 	s_wireless = NM_SETTING_WIRELESS (nm_setting_wireless_new ());
@@ -3272,25 +3262,12 @@ make_wireless_setting (shvarFile *ifcfg,
 
 	value = svGetValue (ifcfg, "HWADDR_BLACKLIST", FALSE);
 	if (value) {
-		char **list = NULL, **iter;
+		char **list;
 
 		list = g_strsplit_set (value, " \t", 0);
-		for (iter = list; iter && *iter; iter++) {
-			if (**iter == '\0')
-				continue;
-			if (!nm_utils_hwaddr_valid (*iter, ETH_ALEN)) {
-				PARSE_WARNING ("invalid MAC in HWADDR_BLACKLIST '%s'", *iter);
-				continue;
-			}
-			macaddr_blacklist = g_slist_prepend (macaddr_blacklist, *iter);
-		}
-		if (macaddr_blacklist) {
-			macaddr_blacklist = g_slist_reverse (macaddr_blacklist);
-			g_object_set (s_wireless, NM_SETTING_WIRELESS_MAC_ADDRESS_BLACKLIST, macaddr_blacklist, NULL);
-			g_slist_free (macaddr_blacklist);
-		}
-		g_free (value);
+		g_object_set (s_wireless, NM_SETTING_WIRELESS_MAC_ADDRESS_BLACKLIST, list, NULL);
 		g_strfreev (list);
+		g_free (value);
 	}
 
 	value = svGetValue (ifcfg, "ESSID", TRUE);
@@ -3558,17 +3535,8 @@ make_wired_setting (shvarFile *ifcfg,
 			if (num_chans < 2 || num_chans > 3) {
 				PARSE_WARNING ("invalid SUBCHANNELS '%s' (%d channels, 2 or 3 expected)",
 				               value, g_strv_length (chans));
-			} else {
-				GPtrArray *array = g_ptr_array_sized_new (num_chans);
-
-				g_ptr_array_add (array, chans[0]);
-				g_ptr_array_add (array, chans[1]);
-				if (num_chans == 3)
-					g_ptr_array_add (array, chans[2]);
-
-				g_object_set (s_wired, NM_SETTING_WIRED_S390_SUBCHANNELS, array, NULL);
-				g_ptr_array_free (array, TRUE);
-			}
+			} else
+				g_object_set (s_wired, NM_SETTING_WIRED_S390_SUBCHANNELS, chans, NULL);
 			g_strfreev (chans);
 		}
 		g_free (value);

src/settings/plugins/ifcfg-rh/tests/test-ifcfg-rh.c
@@ -2554,7 +2554,7 @@ test_read_wired_aliases_good (void)
 	const char *expected_id = "System aliasem0";
 	int expected_num_addresses = 4, expected_prefix = 24;
 	const char *expected_address[4] = { "192.168.1.5", "192.168.1.6", "192.168.1.9", "192.168.1.99" };
-	const char *expected_label[4] = { NULL, "aliasem0:1", "aliasem0:2", "aliasem0:99" };
+	const char *expected_label[4] = { "", "aliasem0:1", "aliasem0:2", "aliasem0:99" };
 	const char *expected_gateway[4] = { "192.168.1.1", "192.168.1.1", "192.168.1.1", "192.168.1.1" };
 	int i, j;
 
@@ -2699,7 +2699,7 @@ test_read_wired_aliases_bad (const char *base, const char *expected_id)
 	const char *tmp;
 	int expected_num_addresses = 1, expected_prefix = 24;
 	const char *expected_address = "192.168.1.5";
-	const char *expected_label = NULL;
+	const char *expected_label = "";
 	const char *expected_gateway = "192.168.1.1";
 	NMIP4Address *ip4_addr;
 	struct in_addr addr;
@@ -5850,7 +5850,7 @@ test_read_wired_qeth_static (void)
 	const char *expected_channel0 = "0.0.0600";
 	const char *expected_channel1 = "0.0.0601";
 	const char *expected_channel2 = "0.0.0602";
-	const GPtrArray *subchannels;
+	const char * const *subchannels;
 
 	connection = connection_from_file (TEST_IFCFG_WIRED_QETH_STATIC,
 	                                   NULL,
@@ -5914,24 +5914,19 @@ test_read_wired_qeth_static (void)
 	        TEST_IFCFG_WIRED_QETH_STATIC,
 	        NM_SETTING_WIRED_SETTING_NAME,
 	        NM_SETTING_WIRED_S390_SUBCHANNELS);
-	ASSERT (subchannels->len == 3,
+	ASSERT (subchannels[0] && subchannels[1] && subchannels[2] && !subchannels[3],
 	        "wired-qeth-static-verify-wired", "failed to verify %s: invalid %s / %s key (not 3 elements)",
 	        TEST_IFCFG_WIRED_QETH_STATIC,
 	        NM_SETTING_WIRED_SETTING_NAME,
 	        NM_SETTING_WIRED_S390_SUBCHANNELS);
 
-	tmp = (const char *) g_ptr_array_index (subchannels, 0);
-	ASSERT (strcmp (tmp, expected_channel0) == 0,
+	ASSERT (strcmp (subchannels[0], expected_channel0) == 0,
 	        "wired-qeth-static-verify-wired", "failed to verify %s: unexpected subchannel #0",
 	        TEST_IFCFG_WIRED_QETH_STATIC);
-
-	tmp = (const char *) g_ptr_array_index (subchannels, 1);
-	ASSERT (strcmp (tmp, expected_channel1) == 0,
+	ASSERT (strcmp (subchannels[1], expected_channel1) == 0,
 	        "wired-qeth-static-verify-wired", "failed to verify %s: unexpected subchannel #1",
 	        TEST_IFCFG_WIRED_QETH_STATIC);
-
-	tmp = (const char *) g_ptr_array_index (subchannels, 2);
-	ASSERT (strcmp (tmp, expected_channel2) == 0,
+	ASSERT (strcmp (subchannels[2], expected_channel2) == 0,
 	        "wired-qeth-static-verify-wired", "failed to verify %s: unexpected subchannel #2",
 	        TEST_IFCFG_WIRED_QETH_STATIC);
 
@@ -6022,7 +6017,7 @@ test_read_wired_ctc_static (void)
 	const char *expected_id = "System test-wired-ctc-static";
 	const char *expected_channel0 = "0.0.1b00";
 	const char *expected_channel1 = "0.0.1b01";
-	const GPtrArray *subchannels;
+	const char * const *subchannels;
 	gboolean success;
 
 	connection = connection_from_file (TEST_IFCFG_WIRED_CTC_STATIC,
@@ -6056,10 +6051,10 @@ test_read_wired_ctc_static (void)
 	/* Subchannels */
 	subchannels = nm_setting_wired_get_s390_subchannels (s_wired);
 	g_assert (subchannels != NULL);
-	g_assert_cmpint (subchannels->len, ==, 2);
+	g_assert (subchannels[0] && subchannels[1] && !subchannels[2]);
 
-	g_assert_cmpstr (g_ptr_array_index (subchannels, 0), ==, expected_channel0);
-	g_assert_cmpstr (g_ptr_array_index (subchannels, 1), ==, expected_channel1);
+	g_assert_cmpstr (subchannels[0], ==, expected_channel0);
+	g_assert_cmpstr (subchannels[1], ==, expected_channel1);
 
 	/* Nettype */
 	g_assert_cmpstr (nm_setting_wired_get_s390_nettype (s_wired), ==, "ctc");
@@ -7736,7 +7731,7 @@ test_write_wired_aliases (void)
 	char *uuid;
 	int num_addresses = 4;
 	guint32 ip[] = { 0x01010101, 0x01010102, 0x01010103, 0x01010104 };
-	const char *label[] = { NULL, "alias0:2", NULL, "alias0:3" };
+	const char *label[] = { "", "alias0:2", "", "alias0:3" };
 	const guint32 gw = htonl (0x01010101);
 	const guint32 prefix = 24;
 	NMIP4Address *addr;
@@ -10329,7 +10324,7 @@ test_write_wifi_wpa_then_wep_with_perms (void)
 	char *route6file = NULL;
 	gboolean ignore_error = FALSE;
 	GByteArray *ssid;
-	GSList *perm_list = NULL;
+	char **perms;
 	const unsigned char ssid_data[] = "SomeSSID";
 
 	/* Test that writing out a WPA config then changing that to a WEP
@@ -10345,16 +10340,16 @@ test_write_wifi_wpa_then_wep_with_perms (void)
 	nm_connection_add_setting (connection, NM_SETTING (s_con));
 
 	uuid = nm_utils_uuid_generate ();
-	perm_list = g_slist_append (perm_list, "user:superman:");
+	perms = g_strsplit ("user:superman:", ",", -1);
 	g_object_set (s_con,
 	              NM_SETTING_CONNECTION_ID, "random wifi connection 2",
 	              NM_SETTING_CONNECTION_UUID, uuid,
 	              NM_SETTING_CONNECTION_AUTOCONNECT, TRUE,
-	              NM_SETTING_CONNECTION_PERMISSIONS, perm_list,
+	              NM_SETTING_CONNECTION_PERMISSIONS, perms,
 	              NM_SETTING_CONNECTION_TYPE, NM_SETTING_WIRELESS_SETTING_NAME,
 	              NULL);
 	g_free (uuid);
-	g_slist_free (perm_list);
+	g_strfreev (perms);
 	ASSERT (nm_setting_connection_get_num_permissions (s_con) == 1,
                 "test_write_wifi_wpa_then_wep_with_perms", "unexpected failure adding valid user permisson");
 
@@ -10679,7 +10674,7 @@ test_write_wired_qeth_dhcp (void)
 	NMSettingIP4Config *s_ip4;
 	NMSettingIP6Config *s_ip6;
 	char *uuid;
-	GPtrArray *subchans;
+	char **subchans;
 	gboolean success;
 	GError *error = NULL;
 	char *testfile = NULL;
@@ -10708,15 +10703,12 @@ test_write_wired_qeth_dhcp (void)
 	s_wired = (NMSettingWired *) nm_setting_wired_new ();
 	nm_connection_add_setting (connection, NM_SETTING (s_wired));
 
-	subchans = g_ptr_array_sized_new (3);
-	g_ptr_array_add (subchans, "0.0.600");
-	g_ptr_array_add (subchans, "0.0.601");
-	g_ptr_array_add (subchans, "0.0.602");
+	subchans = g_strsplit ("0.0.600,0.0.601,0.0.602", ",", -1);
 	g_object_set (s_wired,
 	              NM_SETTING_WIRED_S390_SUBCHANNELS, subchans,
 	              NM_SETTING_WIRED_S390_NETTYPE, "qeth",
 	              NULL);
-	g_ptr_array_free (subchans, TRUE);
+	g_strfreev (subchans);
 
 	nm_setting_wired_add_s390_option (s_wired, "portname", "FOOBAR");
 	nm_setting_wired_add_s390_option (s_wired, "portno", "1");
@@ -10803,7 +10795,7 @@ test_write_wired_ctc_dhcp (void)
 	NMSettingIP4Config *s_ip4;
 	NMSettingIP6Config *s_ip6;
 	char *uuid;
-	GPtrArray *subchans;
+	char **subchans;
 	gboolean success;
 	GError *error = NULL;
 	char *testfile = NULL;
@@ -10836,14 +10828,12 @@ test_write_wired_ctc_dhcp (void)
 	g_assert (s_wired);
 	nm_connection_add_setting (connection, NM_SETTING (s_wired));
 
-	subchans = g_ptr_array_sized_new (2);
-	g_ptr_array_add (subchans, "0.0.600");
-	g_ptr_array_add (subchans, "0.0.601");
+	subchans = g_strsplit ("0.0.600,0.0.601", ",", -1);
 	g_object_set (s_wired,
 	              NM_SETTING_WIRED_S390_SUBCHANNELS, subchans,
 	              NM_SETTING_WIRED_S390_NETTYPE, "ctc",
 	              NULL);
-	g_ptr_array_free (subchans, TRUE);
+	g_strfreev (subchans);
 	nm_setting_wired_add_s390_option (s_wired, "ctcprot", "0");
 
 	/* IP4 setting */

src/settings/plugins/keyfile/reader.c
@@ -1134,20 +1134,12 @@ read_one_setting_value (NMSetting *setting,
 		g_object_set (setting, key, array, NULL);
 		g_byte_array_free (array, TRUE);
 		g_free (tmp);
- 	} else if (type == DBUS_TYPE_G_LIST_OF_STRING) {
+	} else if (type == G_TYPE_STRV) {
 		gchar **sa;
 		gsize length;
-		int i;
-		GSList *list = NULL;
 
 		sa = nm_keyfile_plugin_kf_get_string_list (info->keyfile, setting_name, key, &length, NULL);
-		for (i = 0; i < length; i++)
-			list = g_slist_prepend (list, sa[i]);
-
-		list = g_slist_reverse (list);
-		g_object_set (setting, key, list, NULL);
-
-		g_slist_free (list);
+		g_object_set (setting, key, sa, NULL);
 		g_strfreev (sa);
 	} else if (type == DBUS_TYPE_G_MAP_OF_STRING) {
 		read_hash_of_string (info->keyfile, setting, key);

clients/cli/nmcli.c
@@ -388,6 +388,26 @@ setup_signals (void)
 	return TRUE;
 }
 
+static void
+nmc_convert_strv_to_string (const GValue *src_value, GValue *dest_value)
+{
+	char **strings;
+
+	strings = g_value_get_boxed (src_value);
+	if (strings)
+		g_value_take_string (dest_value, g_strjoinv (",", strings));
+	else
+		g_value_set_string (dest_value, "");
+}
+
+static void
+nmc_value_transforms_register (void)
+{
+	g_value_register_transform_func (G_TYPE_STRV,
+	                                 G_TYPE_STRING,
+	                                 nmc_convert_strv_to_string);
+}
+
 static NMClient *
 nmc_get_client (NmCli *nmc)
 {
@@ -495,6 +515,8 @@ main (int argc, char *argv[])
 	 * See https://bugzilla.redhat.com/show_bug.cgi?id=1109946
 	 */
 	rl_set_keyboard_input_timeout (10000);
+
+	nmc_value_transforms_register ();
 
 	nmc_init (&nm_cli);
 	g_idle_add (start, &args_info);

clients/tui/nmt-page-main.c
@@ -96,9 +96,9 @@ permissions_transform_to_allusers (GBinding     *binding,
                                    GValue       *target_value,
                                    gpointer      user_data)
 {
-	GSList *perms = g_value_get_boxed (source_value);
+	char **perms = g_value_get_boxed (source_value);
 
-	g_value_set_boolean (target_value, perms == NULL);
+	g_value_set_boolean (target_value, g_strv_length (perms) == 0);
 	return TRUE;
 }
 
@@ -109,12 +109,13 @@ permissions_transform_from_allusers (GBinding     *binding,
                                      gpointer      user_data)
 {
 	gboolean allusers = g_value_get_boolean (source_value);
-	GSList *perms = NULL;
+	char **perms = NULL;
 
 	if (allusers) {
-		char *perm = g_strdup_printf ("user:%s:", g_get_user_name ());
+		perms = g_new (char *, 2);
 
-		perms = g_slist_prepend (perms, perm);
+		perms[0] = g_strdup_printf ("user:%s:", g_get_user_name ());
+		perms[1] = NULL;
 	}
 	g_value_take_boxed (target_value, perms);
 	return TRUE;

libnm-core/nm-connection.c
@@ -256,12 +256,11 @@ validate_permissions_type (GHashTable *hash, GError **error)
 	if (s_con) {
 		permissions = g_hash_table_lookup (s_con, NM_SETTING_CONNECTION_PERMISSIONS);
 		if (permissions) {
-			if (   !G_VALUE_HOLDS (permissions, G_TYPE_STRV)
-			    && !G_VALUE_HOLDS (permissions, DBUS_TYPE_G_LIST_OF_STRING)) {
+			if (!G_VALUE_HOLDS (permissions, G_TYPE_STRV)) {
 				g_set_error_literal (error,
 				                     NM_SETTING_ERROR,
 				                     NM_SETTING_ERROR_PROPERTY_TYPE_MISMATCH,
-				                     "Wrong permissions property type; should be a list of strings.");
+				                     "Wrong permissions property type; should be an array of strings.");
 				return FALSE;
 			}
 		}

libnm-core/nm-setting-8021x.c
@@ -2819,7 +2819,7 @@ set_property (GObject *object, guint prop_id,
 	switch (prop_id) {
 	case PROP_EAP:
 		g_slist_free_full (priv->eap, g_free);
-		priv->eap = g_value_dup_boxed (value);
+		priv->eap = _nm_utils_strv_to_slist (g_value_get_boxed (value));
 		break;
 	case PROP_IDENTITY:
 		g_free (priv->identity);
@@ -2855,7 +2855,7 @@ set_property (GObject *object, guint prop_id,
 		break;
 	case PROP_ALTSUBJECT_MATCHES:
 		g_slist_free_full (priv->altsubject_matches, g_free);
-		priv->altsubject_matches = g_value_dup_boxed (value);
+		priv->altsubject_matches = _nm_utils_strv_to_slist (g_value_get_boxed (value));
 		break;
 	case PROP_CLIENT_CERT:
 		if (priv->client_cert) {
@@ -2911,7 +2911,7 @@ set_property (GObject *object, guint prop_id,
 		break;
 	case PROP_PHASE2_ALTSUBJECT_MATCHES:
 		g_slist_free_full (priv->phase2_altsubject_matches, g_free);
-		priv->phase2_altsubject_matches = g_value_dup_boxed (value);
+		priv->phase2_altsubject_matches = _nm_utils_strv_to_slist (g_value_get_boxed (value));
 		break;
 	case PROP_PHASE2_CLIENT_CERT:
 		if (priv->phase2_client_cert) {
@@ -3003,7 +3003,7 @@ get_property (GObject *object, guint prop_id,
 
 	switch (prop_id) {
 	case PROP_EAP:
-		g_value_set_boxed (value, priv->eap);
+		g_value_take_boxed (value, _nm_utils_slist_to_strv (priv->eap));
 		break;
 	case PROP_IDENTITY:
 		g_value_set_string (value, priv->identity);
@@ -3024,7 +3024,7 @@ get_property (GObject *object, guint prop_id,
 		g_value_set_string (value, priv->subject_match);
 		break;
 	case PROP_ALTSUBJECT_MATCHES:
-		g_value_set_boxed (value, priv->altsubject_matches);
+		g_value_take_boxed (value, _nm_utils_slist_to_strv (priv->altsubject_matches));
 		break;
 	case PROP_CLIENT_CERT:
 		g_value_set_boxed (value, priv->client_cert);
@@ -3054,7 +3054,7 @@ get_property (GObject *object, guint prop_id,
 		g_value_set_string (value, priv->phase2_subject_match);
 		break;
 	case PROP_PHASE2_ALTSUBJECT_MATCHES:
-		g_value_set_boxed (value, priv->phase2_altsubject_matches);
+		g_value_take_boxed (value, _nm_utils_slist_to_strv (priv->phase2_altsubject_matches));
 		break;
 	case PROP_PHASE2_CLIENT_CERT:
 		g_value_set_boxed (value, priv->phase2_client_cert);
@@ -3135,7 +3135,7 @@ nm_setting_802_1x_class_init (NMSetting8021xClass *setting_class)
 	g_object_class_install_property
 		(object_class, PROP_EAP,
 		 g_param_spec_boxed (NM_SETTING_802_1X_EAP, "", "",
-		                     DBUS_TYPE_G_LIST_OF_STRING,
+		                     G_TYPE_STRV,
 		                     G_PARAM_READWRITE |
 		                     G_PARAM_STATIC_STRINGS));
 
@@ -3241,7 +3241,7 @@ nm_setting_802_1x_class_init (NMSetting8021xClass *setting_class)
 	g_object_class_install_property
 		(object_class, PROP_ALTSUBJECT_MATCHES,
 		 g_param_spec_boxed (NM_SETTING_802_1X_ALTSUBJECT_MATCHES, "", "",
-		                     DBUS_TYPE_G_LIST_OF_STRING,
+		                     G_TYPE_STRV,
 		                     G_PARAM_READWRITE |
 		                     G_PARAM_STATIC_STRINGS));
 
@@ -3417,7 +3417,7 @@ nm_setting_802_1x_class_init (NMSetting8021xClass *setting_class)
 	g_object_class_install_property
 		(object_class, PROP_PHASE2_ALTSUBJECT_MATCHES,
 		 g_param_spec_boxed (NM_SETTING_802_1X_PHASE2_ALTSUBJECT_MATCHES, "", "",
-		                     DBUS_TYPE_G_LIST_OF_STRING,
+		                     G_TYPE_STRV,
 		                     G_PARAM_READWRITE |
 		                     G_PARAM_STATIC_STRINGS));
 

libnm-core/nm-setting-connection.c
@@ -24,7 +24,7 @@
 #include <glib/gi18n.h>
 
 #include "nm-utils.h"
-#include "nm-dbus-glib-types.h"
+#include "nm-utils-private.h"
 #include "nm-setting-connection.h"
 #include "nm-setting-private.h"
 
@@ -1033,14 +1033,15 @@ finalize (GObject *object)
 }
 
 static GSList *
-perm_stringlist_to_permlist (GSList *strlist)
+perm_strv_to_permlist (char **strv)
 {
-	GSList *list = NULL, *iter;
+	GSList *list = NULL;
+	int i;
 
-	for (iter = strlist; iter; iter = g_slist_next (iter)) {
+	for (i = 0; strv[i]; i++) {
 		Permission *p;
 
-		p = permission_new_from_str ((const char *) iter->data);
+		p = permission_new_from_str (strv[i]);
 		if (p)
 			list = g_slist_append (list, p);
 	}
@@ -1073,7 +1074,7 @@ set_property (GObject *object, guint prop_id,
 		break;
 	case PROP_PERMISSIONS:
 		g_slist_free_full (priv->permissions, (GDestroyNotify) permission_free);
-		priv->permissions = perm_stringlist_to_permlist (g_value_get_boxed (value));
+		priv->permissions = perm_strv_to_permlist (g_value_get_boxed (value));
 		break;
 	case PROP_AUTOCONNECT:
 		priv->autoconnect = g_value_get_boolean (value);
@@ -1098,7 +1099,7 @@ set_property (GObject *object, guint prop_id,
 		break;
 	case PROP_SECONDARIES:
 		g_slist_free_full (priv->secondaries, g_free);
-		priv->secondaries = g_value_dup_boxed (value);
+		priv->secondaries = _nm_utils_strv_to_slist (g_value_get_boxed (value));
 		break;
 	case PROP_GATEWAY_PING_TIMEOUT:
 		priv->gateway_ping_timeout = g_value_get_uint (value);
@@ -1109,14 +1110,18 @@ set_property (GObject *object, guint prop_id,
 	}
 }
 
-static GSList *
-perm_permlist_to_stringlist (GSList *permlist)
+static char **
+perm_permlist_to_strv (GSList *permlist)
 {
-	GSList *list = NULL, *iter;
+	GPtrArray *strings;
+	GSList *iter;
 
+	strings = g_ptr_array_new ();
 	for (iter = permlist; iter; iter = g_slist_next (iter))
-		list = g_slist_append (list, permission_to_string ((Permission *) iter->data));
-	return list;
+		g_ptr_array_add (strings, permission_to_string ((Permission *) iter->data));
+	g_ptr_array_add (strings, NULL);
+
+	return (char **) g_ptr_array_free (strings, FALSE);
 }
 
 static void
@@ -1140,7 +1145,7 @@ get_property (GObject *object, guint prop_id,
 		g_value_set_string (value, nm_setting_connection_get_connection_type (setting));
 		break;
 	case PROP_PERMISSIONS:
-		g_value_take_boxed (value, perm_permlist_to_stringlist (priv->permissions));
+		g_value_take_boxed (value, perm_permlist_to_strv (priv->permissions));
 		break;
 	case PROP_AUTOCONNECT:
 		g_value_set_boolean (value, nm_setting_connection_get_autoconnect (setting));
@@ -1161,7 +1166,7 @@ get_property (GObject *object, guint prop_id,
 		g_value_set_string (value, nm_setting_connection_get_slave_type (setting));
 		break;
 	case PROP_SECONDARIES:
-		g_value_set_boxed (value, priv->secondaries);
+		g_value_take_boxed (value, _nm_utils_slist_to_strv (priv->secondaries));
 		break;
 	case PROP_GATEWAY_PING_TIMEOUT:
 		g_value_set_uint (value, priv->gateway_ping_timeout);
@@ -1289,7 +1294,7 @@ nm_setting_connection_class_init (NMSettingConnectionClass *setting_class)
 	g_object_class_install_property
 		(object_class, PROP_PERMISSIONS,
 		 g_param_spec_boxed (NM_SETTING_CONNECTION_PERMISSIONS, "", "",
-		                     DBUS_TYPE_G_LIST_OF_STRING,
+		                     G_TYPE_STRV,
 		                     G_PARAM_READWRITE |
 		                     G_PARAM_STATIC_STRINGS));
 
@@ -1403,7 +1408,7 @@ nm_setting_connection_class_init (NMSettingConnectionClass *setting_class)
 	g_object_class_install_property
 		(object_class, PROP_SECONDARIES,
 		 g_param_spec_boxed (NM_SETTING_CONNECTION_SECONDARIES, "", "",
-		                     DBUS_TYPE_G_LIST_OF_STRING,
+		                     G_TYPE_STRV,
 		                     G_PARAM_READWRITE |
 		                     NM_SETTING_PARAM_FUZZY_IGNORE |
 		                     G_PARAM_STATIC_STRINGS));

commit 8657b4229ce5c572139f3b14cfb790b42490c194

ifcfg-rh: fix reading HWADDR_BLACKLIST
Adding GSList to the property may crash NetworkManager. Also check MAC
addresses and filter out the invalid ones.

(broken by commit 6a4127cfa063a77e3ee886a999441c71f6a0e11c)

src/settings/plugins/ifcfg-rh/reader.c
@@ -15,7 +15,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Copyright (C) 2008 - 2013 Red Hat, Inc.
+ * Copyright 2008 - 2014 Red Hat, Inc.
  */
 
 #include <config.h>
@@ -49,6 +49,7 @@
 #include <nm-setting-dcb.h>
 #include <nm-setting-generic.h>
 #include "nm-core-internal.h"
+#include "nm-utils-private.h"
 #include <nm-utils.h>
 
 #include "nm-platform.h"
@@ -3238,6 +3239,28 @@ make_wireless_security_setting (shvarFile *ifcfg,
 	return NULL; /* unencrypted */
 }
 
+static char **
+transform_hwddr_blacklist (const char *blacklist)
+{
+	char **strv, **iter;
+	int shift = 0;
+
+	strv = _nm_utils_strsplit_set (blacklist, " \t", 0);
+	for (iter = strv; iter && *iter; iter++) {
+		if (shift) {
+			*(iter-shift) = *iter;
+			*iter = NULL;
+		}
+		if (!nm_utils_hwaddr_valid (*(iter-shift), ETH_ALEN)) {
+			PARSE_WARNING ("invalid MAC in HWADDR_BLACKLIST '%s'", *(iter-shift));
+			g_free (*(iter-shift));
+			*(iter-shift) = NULL;
+			shift++;
+		}
+	}
+	return strv;
+}
+
 static NMSetting *
 make_wireless_setting (shvarFile *ifcfg,
                        GError **error)
@@ -3264,11 +3287,11 @@ make_wireless_setting (shvarFile *ifcfg,
 
 	value = svGetValue (ifcfg, "HWADDR_BLACKLIST", FALSE);
 	if (value) {
-		char **list;
+		char **strv;
 
-		list = g_strsplit_set (value, " \t", 0);
-		g_object_set (s_wireless, NM_SETTING_WIRELESS_MAC_ADDRESS_BLACKLIST, list, NULL);
-		g_strfreev (list);
+		strv = transform_hwddr_blacklist (value);
+		g_object_set (s_wireless, NM_SETTING_WIRELESS_MAC_ADDRESS_BLACKLIST, strv, NULL);
+		g_strfreev (strv);
 		g_free (value);
 	}
 
@@ -3489,7 +3512,6 @@ make_wired_setting (shvarFile *ifcfg,
 	NMSettingWired *s_wired;
 	char *value = NULL;
 	int mtu;
-	GSList *macaddr_blacklist = NULL;
 	char *nettype;
 
 	s_wired = NM_SETTING_WIRED (nm_setting_wired_new ());
@@ -3594,25 +3616,12 @@ make_wired_setting (shvarFile *ifcfg,
 
 	value = svGetValue (ifcfg, "HWADDR_BLACKLIST", FALSE);
 	if (value) {
-		char **list = NULL, **iter;
+		char **strv;
 
-		list = g_strsplit_set (value, " \t", 0);
-		for (iter = list; iter && *iter; iter++) {
-			if (**iter == '\0')
-				continue;
-			if (!nm_utils_hwaddr_valid (*iter, ETH_ALEN)) {
-				PARSE_WARNING ("invalid MAC in HWADDR_BLACKLIST '%s'", *iter);
-				continue;
-			}
-			macaddr_blacklist = g_slist_prepend (macaddr_blacklist, *iter);
-		}
-		if (macaddr_blacklist) {
-			macaddr_blacklist = g_slist_reverse (macaddr_blacklist);
-			g_object_set (s_wired, NM_SETTING_WIRED_MAC_ADDRESS_BLACKLIST, macaddr_blacklist, NULL);
-			g_slist_free (macaddr_blacklist);
-		}
+		strv = transform_hwddr_blacklist (value);
+		g_object_set (s_wired, NM_SETTING_WIRED_MAC_ADDRESS_BLACKLIST, strv, NULL);
+		g_strfreev (strv);
 		g_free (value);
-		g_strfreev (list);
 	}
 
 	value = svGetValue (ifcfg, "KEY_MGMT", FALSE);

src/settings/plugins/ifcfg-rh/tests/network-scripts/Makefile.am
@@ -1,5 +1,6 @@
 EXTRA_DIST = \
 	ifcfg-test-minimal \
+	ifcfg-test-misc-variables \
 	ifcfg-test-variables-corner-cases-1 \
 	ifcfg-test-nm-controlled \
 	ifcfg-test-nm-controlled-unrecognized \

src/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-misc-variables
@@ -1,0 +1,3 @@
+DEVICE=eth0
+HWADDR_BLACKLIST="00:16:41:11:22:88 00:16:41:11:22:99"
+

src/settings/plugins/ifcfg-rh/tests/test-ifcfg-rh.c
@@ -238,6 +238,53 @@ test_read_basic (void)
 }
 
 static void
+test_read_miscellaneous_variables (void)
+{
+	NMConnection *connection;
+	NMSettingConnection *s_con;
+	NMSettingWired *s_wired;
+	NMSettingIP4Config *s_ip4;
+	GError *error = NULL;
+	char *expected_mac_blacklist[2] = { "00:16:41:11:22:88", "00:16:41:11:22:99" };
+	int mac_blacklist_num, i;
+	guint64 expected_timestamp = 0;
+	gboolean success;
+
+	connection = connection_from_file (TEST_IFCFG_DIR"/network-scripts/ifcfg-test-misc-variables",
+	                                   NULL, TYPE_ETHERNET, NULL, NULL, NULL, NULL, &error, NULL);
+	g_assert_no_error (error);
+	g_assert (connection);
+	success = nm_connection_verify (connection, &error);
+	g_assert_no_error (error);
+	g_assert (success);
+
+	/* ===== CONNECTION SETTING ===== */
+	s_con = nm_connection_get_setting_connection (connection);
+	g_assert (s_con);
+	g_assert_cmpint (nm_setting_connection_get_timestamp (s_con), ==, expected_timestamp);
+	g_assert (nm_setting_connection_get_autoconnect (s_con));
+
+	/* ===== WIRED SETTING ===== */
+	s_wired = nm_connection_get_setting_wired (connection);
+	g_assert (s_wired);
+	g_assert_cmpint (nm_setting_wired_get_mtu (s_wired), ==, 0);
+
+	/* MAC blacklist */
+	mac_blacklist_num = nm_setting_wired_get_num_mac_blacklist_items (s_wired);
+	g_assert_cmpint (mac_blacklist_num, ==, 2);
+	for (i = 0; i < mac_blacklist_num; i++)
+		g_assert (nm_utils_hwaddr_matches (nm_setting_wired_get_mac_blacklist_item (s_wired, i), -1, expected_mac_blacklist[i], -1));
+
+	/* ===== IPv4 SETTING ===== */
+	s_ip4 = nm_connection_get_setting_ip4_config (connection);
+	g_assert (s_ip4);
+	g_assert_cmpstr (nm_setting_ip4_config_get_method (s_ip4), ==, NM_SETTING_IP4_CONFIG_METHOD_DISABLED);
+	g_assert (nm_setting_ip4_config_get_never_default (s_ip4) == FALSE);
+
+	g_object_unref (connection);
+}
+
+static void
 test_read_variables_corner_cases (void)
 {
 	NMConnection *connection;
@@ -13631,6 +13678,7 @@ int main (int argc, char **argv)
 	g_test_add_func (TPATH "unmanaged-unrecognized", test_read_unmanaged_unrecognized);
 	g_test_add_func (TPATH "unrecognized", test_read_unrecognized);
 	g_test_add_func (TPATH "basic", test_read_basic);
+	g_test_add_func (TPATH "miscellaneous-variables", test_read_miscellaneous_variables);
 	g_test_add_func (TPATH "variables-corner-cases", test_read_variables_corner_cases);
 	g_test_add_data_func (TPATH "no-prefix/8", GUINT_TO_POINTER (8), test_read_wired_static_no_prefix);
 	g_test_add_data_func (TPATH "no-prefix/16", GUINT_TO_POINTER (16), test_read_wired_static_no_prefix);