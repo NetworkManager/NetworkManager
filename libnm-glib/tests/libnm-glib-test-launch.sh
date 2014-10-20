e9bfd0e29c164b79205c23617b199573b78abd5b
Lubomir Rintel <lkundrak@v3.sk>
2014-10-20 18:24:26 +0200

    tests: Don't run session-long dbus daemons for tests
    They require a tty or X11 displays, thus are not suitable for headless runs
    (such as in mock). Furthermore, they die with the tty or X11 session, which
    is somehow late -- a lot of them may accumulate. Let's kill them right away.

--- libnm-glib/tests/libnm-glib-test-launch.sh
+++ libnm-glib/tests/libnm-glib-test-launch.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# Spawn DBus if there's none
 if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-    exec dbus-launch --exit-with-session "$@"
-else
-    exec "$@"
+    eval `dbus-launch --sh-syntax`
+    trap "kill $DBUS_SESSION_BUS_PID" EXIT
 fi
+
+"$@"
