NetworkManager/NetworkManager

commit 7d04618645f797d84c3f8692af6dfac1d67cb376

settings: pass valid error domain to g_set_error() in load_plugins()
glib asserts that the error domain parameter is a non-zero quark.

Signed-off-by: Thomas Haller <thaller@redhat.com>

src/settings/nm-settings.c
@@ -651,7 +651,7 @@ load_plugins (NMSettings *self, const char **plugins, GError **error)
 
 		plugin = g_module_open (path, G_MODULE_BIND_LOCAL);
 		if (!plugin) {
-			g_set_error (error, 0, 0,
+			g_set_error (error, NM_SETTINGS_ERROR, 0,
 			             "Could not load plugin '%s': %s",
 			             pname, g_module_error ());
 			g_free (full_name);
@@ -664,7 +664,7 @@ load_plugins (NMSettings *self, const char **plugins, GError **error)
 		g_free (path);
 
 		if (!g_module_symbol (plugin, "nm_system_config_factory", (gpointer) (&factory_func))) {
-			g_set_error (error, 0, 0,
+			g_set_error (error, NM_SETTINGS_ERROR, 0,
 			             "Could not find plugin '%s' factory function.",
 			             pname);
 			success = FALSE;
@@ -673,7 +673,7 @@ load_plugins (NMSettings *self, const char **plugins, GError **error)
 
 		obj = (*factory_func) ();
 		if (!obj || !NM_IS_SYSTEM_CONFIG_INTERFACE (obj)) {
-			g_set_error (error, 0, 0,
+			g_set_error (error, NM_SETTINGS_ERROR, 0,
 			             "Plugin '%s' returned invalid system config object.",
 			             pname);
 			success = FALSE;

commit 3423e54c4e74b7a8c6b31450c5b5daadf6a2293f

settings: pass valid error code to g_set_error() in load_plugins()
Fixes: 7d04618645f797d84c3f8692af6dfac1d67cb376
Signed-off-by: Thomas Haller <thaller@redhat.com>

src/settings/nm-settings.c
@@ -650,7 +650,7 @@ load_plugins (NMSettings *self, const char **plugins, GError **error)
 
 		plugin = g_module_open (path, G_MODULE_BIND_LOCAL);
 		if (!plugin) {
-			g_set_error (error, NM_SETTINGS_ERROR, 0,
+			g_set_error (error, NM_SETTINGS_ERROR, NM_SETTINGS_ERROR_FAILED,
 			             "Could not load plugin '%s': %s",
 			             pname, g_module_error ());
 			g_free (full_name);
@@ -663,7 +663,7 @@ load_plugins (NMSettings *self, const char **plugins, GError **error)
 		g_free (path);
 
 		if (!g_module_symbol (plugin, "nm_system_config_factory", (gpointer) (&factory_func))) {
-			g_set_error (error, NM_SETTINGS_ERROR, 0,
+			g_set_error (error, NM_SETTINGS_ERROR, NM_SETTINGS_ERROR_FAILED,
 			             "Could not find plugin '%s' factory function.",
 			             pname);
 			success = FALSE;
@@ -672,7 +672,7 @@ load_plugins (NMSettings *self, const char **plugins, GError **error)
 
 		obj = (*factory_func) ();
 		if (!obj || !NM_IS_SYSTEM_CONFIG_INTERFACE (obj)) {
-			g_set_error (error, NM_SETTINGS_ERROR, 0,
+			g_set_error (error, NM_SETTINGS_ERROR, NM_SETTINGS_ERROR_FAILED,
 			             "Plugin '%s' returned invalid system config object.",
 			             pname);
 			success = FALSE;