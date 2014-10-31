84d875546bb5bb9331114559bfb9fc2df129d8ea
Dan Winship <danw@gnome.org>
2014-10-31 13:42:45 -0400

    tui: merge NmtPageMain into NmtEditor
    All page types except NmtPageMain are displayed as collapsible
    sections. NmtPageMain behaved quite differently from everything else.
    So merge it into its container, NmtEditor.

--- clients/tui/nmt-editor.c
+++ clients/tui/nmt-editor.c
@@ -34,8 +34,25 @@
 #include "nmtui.h"
 
 #include "nm-editor-utils.h"
-#include "nmt-page-main.h"
 #include "nmt-utils.h"
+
+#include "nmt-device-entry.h"
+#include "nmt-mac-entry.h"
+#include "nmt-mtu-entry.h"
+
+#include "nmt-page-bond.h"
+#include "nmt-page-bridge.h"
+#include "nmt-page-bridge-port.h"
+#include "nmt-page-dsl.h"
+#include "nmt-page-ethernet.h"
+#include "nmt-page-infiniband.h"
+#include "nmt-page-ip4.h"
+#include "nmt-page-ip6.h"
+#include "nmt-page-ppp.h"
+#include "nmt-page-team.h"
+#include "nmt-page-team-port.h"
+#include "nmt-page-vlan.h"
+#include "nmt-page-wifi.h"
 
 G_DEFINE_TYPE (NmtEditor, nmt_editor, NMT_TYPE_NEWT_FORM)
 
@@ -217,12 +234,88 @@ build_edit_connection (NMConnection *orig_connection)
 	return edit_connection;
 }
 
+static gboolean
+permissions_transform_to_allusers (GBinding     *binding,
+                                   const GValue *source_value,
+                                   GValue       *target_value,
+                                   gpointer      user_data)
+{
+	char **perms = g_value_get_boxed (source_value);
+
+	g_value_set_boolean (target_value, g_strv_length (perms) == 0);
+	return TRUE;
+}
+
+static gboolean
+permissions_transform_from_allusers (GBinding     *binding,
+                                     const GValue *source_value,
+                                     GValue       *target_value,
+                                     gpointer      user_data)
+{
+	gboolean allusers = g_value_get_boolean (source_value);
+	char **perms = NULL;
+
+	if (allusers) {
+		perms = g_new (char *, 2);
+
+		perms[0] = g_strdup_printf ("user:%s:", g_get_user_name ());
+		perms[1] = NULL;
+	}
+	g_value_take_boxed (target_value, perms);
+	return TRUE;
+}
+
+static NmtNewtWidget *
+add_section_for_page (NmtEditorGrid *grid, NmtNewtWidget *widget)
+{
+	NmtEditorPage *page;
+	NmtNewtWidget *section, *header, *toggle;
+
+	g_return_val_if_fail (NMT_IS_EDITOR_PAGE (widget), NULL);
+	g_return_val_if_fail (nmt_newt_widget_get_parent (widget) == NULL, NULL);
+
+	page = NMT_EDITOR_PAGE (widget);
+
+	section = nmt_newt_section_new (TRUE);
+
+	toggle = nmt_newt_toggle_button_new (_("Hide"), _("Show"));
+
+	header = nmt_editor_grid_new ();
+	nmt_editor_grid_append (NMT_EDITOR_GRID (header),
+	                        nmt_editor_page_get_title (page),
+	                        nmt_editor_page_get_header_widget (page),
+	                        toggle);
+	nmt_editor_grid_set_row_flags (NMT_EDITOR_GRID (header),
+	                               nmt_editor_page_get_header_widget (page),
+	                               NMT_EDITOR_GRID_ROW_LABEL_ALIGN_LEFT |
+	                               NMT_EDITOR_GRID_ROW_EXTRA_ALIGN_RIGHT);
+	nmt_newt_section_set_header (NMT_NEWT_SECTION (section), header);
+
+	nmt_newt_section_set_body (NMT_NEWT_SECTION (section), widget);
+
+	g_object_bind_property (toggle, "active",
+	                        section, "open",
+	                        G_BINDING_SYNC_CREATE);
+
+	if (nmt_editor_page_show_by_default (page) || !nmt_newt_widget_get_valid (section))
+		nmt_newt_toggle_button_set_active (NMT_NEWT_TOGGLE_BUTTON (toggle), TRUE);
+
+	nmt_editor_grid_append (grid, NULL, section, NULL);
+	return section;
+}
+
 static void
 nmt_editor_constructed (GObject *object)
 {
 	NmtEditor *editor = NMT_EDITOR (object);
 	NmtEditorPrivate *priv = NMT_EDITOR_GET_PRIVATE (editor);
-	NmtNewtWidget *vbox, *buttons, *page;
+	NMSettingConnection *s_con;
+	NmtNewtWidget *vbox, *widget, *buttons;
+	NmtEditorGrid *grid;
+	const char *deventry_label;
+	NmtDeviceEntry *deventry;
+	GType hardware_type;
+	const char *slave_type;
 
 	if (G_OBJECT_CLASS (nmt_editor_parent_class)->constructed)
 		G_OBJECT_CLASS (nmt_editor_parent_class)->constructed (object);
@@ -231,8 +324,103 @@ nmt_editor_constructed (GObject *object)
 
 	vbox = nmt_newt_grid_new ();
 
-	page = nmt_page_main_new (priv->edit_connection, priv->type_data);
-	nmt_newt_grid_add (NMT_NEWT_GRID (vbox), page, 0, 0);
+	s_con = nm_connection_get_setting_connection (priv->edit_connection);
+
+	grid = NMT_EDITOR_GRID (nmt_editor_grid_new ());
+	nmt_newt_grid_add (NMT_NEWT_GRID (vbox), NMT_NEWT_WIDGET (grid), 0, 0);
+
+	/* Add the top widgets */
+
+	widget = nmt_newt_entry_new (40, NMT_NEWT_ENTRY_NONEMPTY);
+	g_object_bind_property (s_con, NM_SETTING_CONNECTION_ID,
+	                        widget, "text",
+	                        G_BINDING_BIDIRECTIONAL | G_BINDING_SYNC_CREATE);
+	nmt_editor_grid_append (grid, _("Profile name"), widget, NULL);
+
+	if (priv->type_data->virtual)
+		hardware_type = G_TYPE_NONE;
+	else
+		hardware_type = priv->type_data->device_type;
+
+	/* For connections involving multiple network devices, clarify which one
+	 * NMSettingConnection:interface-name refers to.
+	 */
+	if (nm_connection_is_type (priv->edit_connection, NM_SETTING_PPPOE_SETTING_NAME))
+		deventry_label = _("Ethernet device");
+	else
+		deventry_label = _("Device");
+
+	widget = nmt_device_entry_new (deventry_label, 40, hardware_type);
+	nmt_editor_grid_append (grid, NULL, widget, NULL);
+	deventry = NMT_DEVICE_ENTRY (widget);
+	g_object_bind_property (s_con, NM_SETTING_CONNECTION_INTERFACE_NAME,
+	                        deventry, "interface-name",
+	                        G_BINDING_BIDIRECTIONAL | G_BINDING_SYNC_CREATE);
+
+	nmt_editor_grid_append (grid, NULL, nmt_newt_separator_new (), NULL);
+
+	/* Now add the various pages... */
+
+	if (nm_connection_is_type (priv->edit_connection, NM_SETTING_BOND_SETTING_NAME))
+		add_section_for_page (grid, nmt_page_bond_new (priv->edit_connection, deventry));
+	else if (nm_connection_is_type (priv->edit_connection, NM_SETTING_BRIDGE_SETTING_NAME))
+		add_section_for_page (grid, nmt_page_bridge_new (priv->edit_connection, deventry));
+	else if (nm_connection_is_type (priv->edit_connection, NM_SETTING_INFINIBAND_SETTING_NAME))
+		add_section_for_page (grid, nmt_page_infiniband_new (priv->edit_connection, deventry));
+	else if (nm_connection_is_type (priv->edit_connection, NM_SETTING_PPPOE_SETTING_NAME)) {
+		add_section_for_page (grid, nmt_page_dsl_new (priv->edit_connection));
+		add_section_for_page (grid, nmt_page_ethernet_new (priv->edit_connection, deventry));
+		add_section_for_page (grid, nmt_page_ppp_new (priv->edit_connection));
+	} else if (nm_connection_is_type (priv->edit_connection, NM_SETTING_TEAM_SETTING_NAME))
+		add_section_for_page (grid, nmt_page_team_new (priv->edit_connection, deventry));
+	else if (nm_connection_is_type (priv->edit_connection, NM_SETTING_VLAN_SETTING_NAME))
+		add_section_for_page (grid, nmt_page_vlan_new (priv->edit_connection, deventry));
+	else if (nm_connection_is_type (priv->edit_connection, NM_SETTING_WIRED_SETTING_NAME))
+		add_section_for_page (grid, nmt_page_ethernet_new (priv->edit_connection, deventry));
+	else if (nm_connection_is_type (priv->edit_connection, NM_SETTING_WIRELESS_SETTING_NAME))
+		add_section_for_page (grid, nmt_page_wifi_new (priv->edit_connection, deventry));
+
+	nmt_editor_grid_append (grid, NULL, nmt_newt_separator_new (), NULL);
+
+	slave_type = nm_setting_connection_get_slave_type (s_con);
+	if (slave_type) {
+		if (!strcmp (slave_type, NM_SETTING_BRIDGE_SETTING_NAME))
+			add_section_for_page (grid, nmt_page_bridge_port_new (priv->edit_connection));
+		else if (!strcmp (slave_type, NM_SETTING_TEAM_SETTING_NAME))
+			add_section_for_page (grid, nmt_page_team_port_new (priv->edit_connection));
+	} else {
+		NmtNewtWidget *section;
+
+		section = add_section_for_page (grid, nmt_page_ip4_new (priv->edit_connection));
+
+		/* Add a separator between ip4 and ip6 that's only visible if ip4 is open */
+		widget = nmt_newt_separator_new ();
+		g_object_bind_property (section, "open", widget, "visible", G_BINDING_SYNC_CREATE);
+		nmt_editor_grid_append (grid, NULL, widget, NULL);
+
+		add_section_for_page (grid, nmt_page_ip6_new (priv->edit_connection));
+
+		nmt_editor_grid_append (grid, NULL, nmt_newt_separator_new (), NULL);
+	}
+
+	/* And finally the bottom widgets */
+
+	widget = nmt_newt_checkbox_new (_("Automatically connect"));
+	g_object_bind_property (s_con, NM_SETTING_CONNECTION_AUTOCONNECT,
+	                        widget, "active",
+	                        G_BINDING_BIDIRECTIONAL | G_BINDING_SYNC_CREATE);
+	nmt_editor_grid_append (grid, NULL, widget, NULL);
+
+	widget = nmt_newt_checkbox_new (_("Available to all users"));
+	g_object_bind_property_full (s_con, NM_SETTING_CONNECTION_PERMISSIONS,
+	                             widget, "active",
+	                             G_BINDING_BIDIRECTIONAL | G_BINDING_SYNC_CREATE,
+	                             permissions_transform_to_allusers,
+	                             permissions_transform_from_allusers,
+	                             NULL, NULL);
+	nmt_editor_grid_append (grid, NULL, widget, NULL);
+
+	/* And the button box */
 
 	buttons = nmt_newt_button_box_new (NMT_NEWT_BUTTON_BOX_HORIZONTAL);
 	nmt_newt_grid_add (NMT_NEWT_GRID (vbox), buttons, 0, 1);
@@ -243,7 +431,7 @@ nmt_editor_constructed (GObject *object)
 
 	priv->ok = nmt_newt_button_box_add_end (NMT_NEWT_BUTTON_BOX (buttons), _("OK"));
 	g_signal_connect (priv->ok, "clicked", G_CALLBACK (save_connection_and_exit), editor);
-	g_object_bind_property (page, "valid",
+	g_object_bind_property (NMT_NEWT_WIDGET (grid), "valid",
 	                        priv->ok, "sensitive",
 	                        G_BINDING_SYNC_CREATE);
 
