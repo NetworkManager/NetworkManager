NetworkManager/NetworkManager

commit f17699f4e3dacb9358a8503c8b15efe3cb852b48

libnm-core: add NMSettingIPConfig:gateway, drop NMIPAddress:gateway
The gateway is a global property of the IPv4/IPv6 configuration, not
an attribute of any particular address. So represent it as such in the
API; remove the gateway from NMIPAddress, and add it to
NMSettingIPConfig.

Behind the scenes, the gateway is still serialized along with the
first address in NMSettingIPConfig:addresses, and is deserialized from
that if the settings dictionary doesn't contain a 'gateway' key.

Adjust nmcli's interactive mode to prompt for IP addresses and gateway
separately. (Patch partly from Jirka Klimeš.)

src/nm-ip4-config.c
@@ -319,14 +319,11 @@ nm_ip4_config_merge_setting (NMIP4Config *config, NMSettingIPConfig *setting, in
 		nm_ip4_config_set_never_default (config, TRUE);
 	else if (nm_setting_ip_config_get_ignore_auto_routes (setting))
 		nm_ip4_config_set_never_default (config, FALSE);
-	if (naddresses) {
-		const char *gateway_str = nm_ip_address_get_gateway (nm_setting_ip_config_get_address (setting, 0));
+	if (nm_setting_ip_config_get_gateway (setting)) {
 		guint32 gateway;
 
-		if (gateway_str) {
-			inet_pton (AF_INET, gateway_str, &gateway);
-			nm_ip4_config_set_gateway (config, gateway);
-		}
+		inet_pton (AF_INET, nm_setting_ip_config_get_gateway (setting), &gateway);
+		nm_ip4_config_set_gateway (config, gateway);
 	}
 
 	/* Addresses */
@@ -425,14 +422,19 @@ nm_ip4_config_create_setting (const NMIP4Config *config)
 		if (!method)
 			method = NM_SETTING_IP4_CONFIG_METHOD_MANUAL;
 
-		s_addr = nm_ip_address_new_binary (AF_INET, &address->address, address->plen,
-		                                   i == 0 ? &gateway : NULL,
-		                                   NULL);
+		s_addr = nm_ip_address_new_binary (AF_INET, &address->address, address->plen, NULL);
 		if (*address->label)
 			nm_ip_address_set_attribute (s_addr, "label", g_variant_new_string (address->label));
 
 		nm_setting_ip_config_add_address (s_ip4, s_addr);
 		nm_ip_address_unref (s_addr);
+	}
+
+	/* Gateway */
+	if (gateway) {
+		g_object_set (s_ip4,
+		              NM_SETTING_IP_CONFIG_GATEWAY, nm_utils_inet4_ntop (gateway, NULL),
+		              NULL);
 	}
 
 	/* Use 'disabled' if the method wasn't previously set */

src/nm-ip6-config.c
@@ -404,6 +404,7 @@ void
 nm_ip6_config_merge_setting (NMIP6Config *config, NMSettingIPConfig *setting, int default_route_metric)
 {
 	guint naddresses, nroutes, nnameservers, nsearches;
+	const char *gateway_str;
 	int i;
 
 	if (!setting)
@@ -423,14 +424,12 @@ nm_ip6_config_merge_setting (NMIP6Config *config, NMSettingIPConfig *setting, in
 		nm_ip6_config_set_never_default (config, TRUE);
 	else if (nm_setting_ip_config_get_ignore_auto_routes (setting))
 		nm_ip6_config_set_never_default (config, FALSE);
-	if (naddresses) {
-		const char *gateway_str = nm_ip_address_get_gateway (nm_setting_ip_config_get_address (setting, 0));
+	gateway_str = nm_setting_ip_config_get_gateway (setting);
+	if (gateway_str) {
 		struct in6_addr gateway;
 
-		if (gateway_str) {
-			inet_pton (AF_INET6, gateway_str, &gateway);
-			nm_ip6_config_set_gateway (config, &gateway);
-		}
+		inet_pton (AF_INET6, gateway_str, &gateway);
+		nm_ip6_config_set_gateway (config, &gateway);
 	}
 
 	/* Addresses */
@@ -531,11 +530,16 @@ nm_ip6_config_create_setting (const NMIP6Config *config)
 		if (!method || strcmp (method, NM_SETTING_IP6_CONFIG_METHOD_LINK_LOCAL) == 0)
 			method = NM_SETTING_IP6_CONFIG_METHOD_MANUAL;
 
-		s_addr = nm_ip_address_new_binary (AF_INET6, &address->address, address->plen,
-		                                   i == 0 ? gateway : NULL,
-		                                   NULL);
+		s_addr = nm_ip_address_new_binary (AF_INET6, &address->address, address->plen, NULL);
 		nm_setting_ip_config_add_address (s_ip6, s_addr);
 		nm_ip_address_unref (s_addr);
+	}
+
+	/* Gateway */
+	if (gateway) {
+		g_object_set (s_ip6,
+		              NM_SETTING_IP_CONFIG_GATEWAY, nm_utils_inet6_ntop (gateway, NULL),
+		              NULL);
 	}
 
 	/* Use 'ignore' if the method wasn't previously set */

libnm/nm-ip4-config.c
@@ -74,7 +74,7 @@ demarshal_ip4_address_array (NMObject *object, GParamSpec *pspec, GVariant *valu
 	NMIP4ConfigPrivate *priv = NM_IP4_CONFIG_GET_PRIVATE (object);
 
 	g_ptr_array_unref (priv->addresses);
-	priv->addresses = nm_utils_ip4_addresses_from_variant (value);
+	priv->addresses = nm_utils_ip4_addresses_from_variant (value, NULL);
 	_nm_object_queue_notify (object, NM_IP4_CONFIG_ADDRESSES);
 
 	return TRUE;

libnm/nm-ip6-config.c
@@ -59,7 +59,7 @@ demarshal_ip6_address_array (NMObject *object, GParamSpec *pspec, GVariant *valu
 	NMIP6ConfigPrivate *priv = NM_IP6_CONFIG_GET_PRIVATE (object);
 
 	g_ptr_array_unref (priv->addresses);
-	priv->addresses = nm_utils_ip6_addresses_from_variant (value);
+	priv->addresses = nm_utils_ip6_addresses_from_variant (value, NULL);
 	_nm_object_queue_notify (object, NM_IP6_CONFIG_ADDRESSES);
 
 	return TRUE;

callouts/nm-dispatcher-utils.c
@@ -92,7 +92,7 @@ static GSList *
 construct_ip4_items (GSList *items, GVariant *ip4_config, const char *prefix)
 {
 	GPtrArray *addresses, *routes;
-	char **dns, **wins;
+	char **dns, **wins, *gateway;
 	GString *tmp;
 	GVariant *val;
 	int i;
@@ -106,26 +106,24 @@ construct_ip4_items (GSList *items, GVariant *ip4_config, const char *prefix)
 	/* IP addresses */
 	val = g_variant_lookup_value (ip4_config, "addresses", G_VARIANT_TYPE ("aau"));
 	if (val) {
-		addresses = nm_utils_ip4_addresses_from_variant (val);
+		addresses = nm_utils_ip4_addresses_from_variant (val, &gateway);
+		if (!gateway)
+			gateway = g_strdup ("0.0.0.0");
 
 		for (i = 0; i < addresses->len; i++) {
 			NMIPAddress *addr = addresses->pdata[i];
-			const char *gw;
 			char *addrtmp;
-
-			gw = nm_ip_address_get_gateway (addr);
-			if (!gw)
-				gw = "0.0.0.0";
 
 			addrtmp = g_strdup_printf ("%sIP4_ADDRESS_%d=%s/%d %s", prefix, i,
 			                           nm_ip_address_get_address (addr),
 			                           nm_ip_address_get_prefix (addr),
-			                           gw);
+			                           gateway);
 			items = g_slist_prepend (items, addrtmp);
 		}
 		if (addresses->len)
 			items = g_slist_prepend (items, g_strdup_printf ("%sIP4_NUM_ADDRESSES=%d", prefix, addresses->len));
 		g_ptr_array_unref (addresses);
+		g_free (gateway);
 		g_variant_unref (val);
 	}
 
@@ -228,7 +226,7 @@ static GSList *
 construct_ip6_items (GSList *items, GVariant *ip6_config, const char *prefix)
 {
 	GPtrArray *addresses, *routes;
-	char **dns;
+	char **dns, *gateway = NULL;
 	GString *tmp;
 	GVariant *val;
 	int i;
@@ -242,26 +240,24 @@ construct_ip6_items (GSList *items, GVariant *ip6_config, const char *prefix)
 	/* IP addresses */
 	val = g_variant_lookup_value (ip6_config, "addresses", G_VARIANT_TYPE ("a(ayuay)"));
 	if (val) {
-		addresses = nm_utils_ip6_addresses_from_variant (val);
+		addresses = nm_utils_ip6_addresses_from_variant (val, &gateway);
+		if (!gateway)
+			gateway = g_strdup ("::");
 
 		for (i = 0; i < addresses->len; i++) {
 			NMIPAddress *addr = addresses->pdata[i];
-			const char *gw;
 			char *addrtmp;
-
-			gw = nm_ip_address_get_gateway (addr);
-			if (!gw)
-				gw = "::";
 
 			addrtmp = g_strdup_printf ("%sIP6_ADDRESS_%d=%s/%d %s", prefix, i,
 			                           nm_ip_address_get_address (addr),
 			                           nm_ip_address_get_prefix (addr),
-			                           gw);
+			                           gateway);
 			items = g_slist_prepend (items, addrtmp);
 		}
 		if (addresses->len)
 			items = g_slist_prepend (items, g_strdup_printf ("%sIP6_NUM_ADDRESSES=%d", prefix, addresses->len));
 		g_ptr_array_unref (addresses);
+		g_free (gateway);
 		g_variant_unref (val);
 	}
 

callouts/tests/test-dispatcher-envp.c
@@ -183,6 +183,7 @@ parse_ip4 (GKeyFile *kf, GVariant **out_props, const char *section, GError **err
 	char *tmp;
 	char **split, **iter;
 	GPtrArray *addresses, *routes;
+	const char *gateway = NULL;
 
 	g_variant_builder_init (&props, G_VARIANT_TYPE ("a{sv}"));
 
@@ -221,7 +222,7 @@ parse_ip4 (GKeyFile *kf, GVariant **out_props, const char *section, GError **err
 		addresses = g_ptr_array_new_with_free_func ((GDestroyNotify) nm_ip_address_unref);
 		for (iter = split; iter && *iter; iter++) {
 			NMIPAddress *addr;
-			char *ip, *prefix, *gw;
+			char *ip, *prefix;
 
 			if (strlen (g_strstrip (*iter)) == 0)
 				continue;
@@ -232,11 +233,13 @@ parse_ip4 (GKeyFile *kf, GVariant **out_props, const char *section, GError **err
 			g_assert (prefix);
 			*prefix++ = '\0';
 
-			gw = strchr (prefix, ' ');
-			g_assert (gw);
-			gw++;
+			if (addresses->len == 0) {
+				gateway = strchr (prefix, ' ');
+				g_assert (gateway);
+				gateway++;
+			}
 
-			addr = nm_ip_address_new (AF_INET, ip, (guint) atoi (prefix), gw, error);
+			addr = nm_ip_address_new (AF_INET, ip, (guint) atoi (prefix), error);
 			if (!addr) {
 				g_ptr_array_unref (addresses);
 				return FALSE;
@@ -245,7 +248,7 @@ parse_ip4 (GKeyFile *kf, GVariant **out_props, const char *section, GError **err
 		}
 
 		g_variant_builder_add (&props, "{sv}", "addresses",
-		                       nm_utils_ip4_addresses_to_variant (addresses));
+		                       nm_utils_ip4_addresses_to_variant (addresses, gateway));
 		g_ptr_array_unref (addresses);
 	}
 	g_strfreev (split);

clients/cli/common.c
@@ -82,6 +82,7 @@ print_ip4_config (NMIP4Config *cfg4,
                   const char *one_field)
 {
 	GPtrArray *ptr_array;
+	const char *gw;
 	char **addr_arr = NULL;
 	char **route_arr = NULL;
 	char **dns_arr = NULL;
@@ -103,20 +104,16 @@ print_ip4_config (NMIP4Config *cfg4,
 
 	/* addresses */
 	ptr_array = nm_ip4_config_get_addresses (cfg4);
+	gw = nm_ip4_config_get_gateway (cfg4);
 	if (ptr_array) {
 		addr_arr = g_new (char *, ptr_array->len + 1);
 		for (i = 0; i < ptr_array->len; i++) {
 			NMIPAddress *addr = (NMIPAddress *) g_ptr_array_index (ptr_array, i);
-			const char *gw;
-
-			gw = nm_ip_address_get_gateway (addr);
-			if (!gw)
-				gw = "0.0.0.0";
 
 			addr_arr[i] = g_strdup_printf ("ip = %s/%u, gw = %s",
 			                               nm_ip_address_get_address (addr),
 			                               nm_ip_address_get_prefix (addr),
-			                               gw);
+			                               (i == 0) && gw ? gw : "0.0.0.0");
 		}
 		addr_arr[i] = NULL;
 	}
@@ -175,6 +172,7 @@ print_ip6_config (NMIP6Config *cfg6,
                   const char *one_field)
 {
 	GPtrArray *ptr_array;
+	const char *gw;
 	char **addr_arr = NULL;
 	char **route_arr = NULL;
 	char **dns_arr = NULL;
@@ -195,20 +193,16 @@ print_ip6_config (NMIP6Config *cfg6,
 
 	/* addresses */
 	ptr_array = nm_ip6_config_get_addresses (cfg6);
+	gw = nm_ip6_config_get_gateway (cfg6);
 	if (ptr_array) {
 		addr_arr = g_new (char *, ptr_array->len + 1);
 		for (i = 0; i < ptr_array->len; i++) {
 			NMIPAddress *addr = (NMIPAddress *) g_ptr_array_index (ptr_array, i);
-			const char *gw;
-
-			gw = nm_ip_address_get_gateway (addr);
-			if (!gw)
-				gw = "::";
 
 			addr_arr[i] = g_strdup_printf ("ip = %s/%u, gw = %s",
 			                               nm_ip_address_get_address (addr),
 			                               nm_ip_address_get_prefix (addr),
-			                               gw);
+			                               (i == 0) && gw ? gw : "::");
 		}
 		addr_arr[i] = NULL;
 	}
@@ -355,10 +349,9 @@ print_dhcp6_config (NMDhcp6Config *dhcp6,
 /*
  * Parse IP address from string to NMIPAddress stucture.
  * ip_str is the IP address in the form address/prefix
- * gw_str is the gateway address (it is optional)
  */
 NMIPAddress *
-nmc_parse_and_build_address (int family, const char *ip_str, const char *gw_str, GError **error)
+nmc_parse_and_build_address (int family, const char *ip_str, GError **error)
 {
 	int max_prefix = (family == AF_INET) ? 32 : 128;
 	NMIPAddress *addr = NULL;
@@ -387,7 +380,7 @@ nmc_parse_and_build_address (int family, const char *ip_str, const char *gw_str,
 		}
 	}
 
-	addr = nm_ip_address_new (family, ip, (guint32) prefix, gw_str, &local);
+	addr = nm_ip_address_new (family, ip, (guint32) prefix, &local);
 	if (!addr) {
 		g_set_error (error, NMCLI_ERROR, NMC_RESULT_ERROR_USER_INPUT,
 		             _("invalid IP address: %s"), local->message);

clients/cli/common.h
@@ -29,7 +29,7 @@ gboolean print_ip6_config (NMIP6Config *cfg6, NmCli *nmc, const char *group_pref
 gboolean print_dhcp4_config (NMDhcp4Config *dhcp4, NmCli *nmc, const char *group_prefix, const char *one_field);
 gboolean print_dhcp6_config (NMDhcp6Config *dhcp6, NmCli *nmc, const char *group_prefix, const char *one_field);
 
-NMIPAddress *nmc_parse_and_build_address (int family, const char *ip_str, const char *gw_str, GError **error);
+NMIPAddress *nmc_parse_and_build_address (int family, const char *ip_str, GError **error);
 NMIPRoute *nmc_parse_and_build_route (int family, const char *first, const char *second, const char *third, GError **error);
 
 const char * nmc_device_state_to_string (NMDeviceState state);

commit 2cfd1647d3723baa42ea48f50f0c8a97c3d96d2f

core: ensure generated connections has no gateway if it has no addresses
Fixes: f17699f4e3dacb9358a8503c8b15efe3cb852b48
Signed-off-by: Thomas Haller <thaller@redhat.com>

src/nm-ip4-config.c
@@ -430,7 +430,8 @@ nm_ip4_config_create_setting (const NMIP4Config *config)
 	}
 
 	/* Gateway */
-	if (gateway) {
+	if (   gateway
+	    && nm_setting_ip_config_get_num_addresses (s_ip4) > 0) {
 		g_object_set (s_ip4,
 		              NM_SETTING_IP_CONFIG_GATEWAY, nm_utils_inet4_ntop (gateway, NULL),
 		              NULL);
@@ -584,6 +585,9 @@ nm_ip4_config_subtract (NMIP4Config *dst, const NMIP4Config *src)
 
 	/* default gateway */
 	if (nm_ip4_config_get_gateway (src) == nm_ip4_config_get_gateway (dst))
+		nm_ip4_config_set_gateway (dst, 0);
+
+	if (!nm_ip4_config_get_num_addresses (dst))
 		nm_ip4_config_set_gateway (dst, 0);
 
 	/* routes */

src/nm-ip6-config.c
@@ -538,7 +538,8 @@ nm_ip6_config_create_setting (const NMIP6Config *config)
 	}
 
 	/* Gateway */
-	if (gateway) {
+	if (   gateway
+	    && nm_setting_ip_config_get_num_addresses (s_ip6) > 0) {
 		g_object_set (s_ip6,
 		              NM_SETTING_IP_CONFIG_GATEWAY, nm_utils_inet6_ntop (gateway, NULL),
 		              NULL);
@@ -698,6 +699,9 @@ nm_ip6_config_subtract (NMIP6Config *dst, const NMIP6Config *src)
 	src_tmp = nm_ip6_config_get_gateway (src);
 	dst_tmp = nm_ip6_config_get_gateway (dst);
 	if (src_tmp && dst_tmp && IN6_ARE_ADDR_EQUAL (src_tmp, dst_tmp))
+		nm_ip6_config_set_gateway (dst, NULL);
+
+	if (!nm_ip6_config_get_num_addresses (dst))
 		nm_ip6_config_set_gateway (dst, NULL);
 
 	/* routes */