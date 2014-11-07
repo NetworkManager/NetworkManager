dde8e703f86d2e0aebd0840135ca43f0be822007
Thomas Haller <thaller@redhat.com>
2014-11-07 23:08:13 +0100

    libnm: make dispose() of NMClient reentrant
    Signed-off-by: Thomas Haller <thaller@redhat.com>

--- libnm/nm-client.c
+++ libnm/nm-client.c
@@ -1825,10 +1825,14 @@ dispose (GObject *object)
 {
 	NMClientPrivate *priv = NM_CLIENT_GET_PRIVATE (object);
 
-	g_signal_handlers_disconnect_by_data (priv->manager, object);
-	g_clear_object (&priv->manager);
-	g_signal_handlers_disconnect_by_data (priv->settings, object);
-	g_clear_object (&priv->settings);
+	if (priv->manager) {
+		g_signal_handlers_disconnect_by_data (priv->manager, object);
+		g_clear_object (&priv->manager);
+	}
+	if (priv->settings) {
+		g_signal_handlers_disconnect_by_data (priv->settings, object);
+		g_clear_object (&priv->settings);
+	}
 
 	G_OBJECT_CLASS (nm_client_parent_class)->dispose (object);
 }
