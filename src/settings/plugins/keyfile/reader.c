b15994679882805f29403d9635bc0913f37e683d
Thomas Haller <thaller@redhat.com>
2014-11-28 12:53:04 +0100

    settings: change algorithm for UUID generation based on strings
    In several cases, connection uuids are generated based on
    some strings. Change the algorithm, to prefix the hashed
    identifier differently for each setting type. This makes
    collisions very unlikely.
    
    Also, change the algorithm, to create proper Variant3 UUIDs.
    
    This is a behavioral change, but it only affects code places
    that were added since nm-0-9-10 and were not yet part of
    a stable release.

--- src/settings/plugins/keyfile/reader.c
+++ src/settings/plugins/keyfile/reader.c
@@ -1316,7 +1316,7 @@ nm_keyfile_plugin_connection_from_file (const char *filename, GError **error)
 	if (!nm_setting_connection_get_uuid (s_con)) {
 		char *hashed_uuid;
 
-		hashed_uuid = nm_utils_uuid_generate_from_string (filename, -1, NM_UTILS_UUID_TYPE_LEGACY, NULL);
+		hashed_uuid = nm_utils_uuid_generate_from_strings ("keyfile", filename, NULL);
 		g_object_set (s_con, NM_SETTING_CONNECTION_UUID, hashed_uuid, NULL);
 		g_free (hashed_uuid);
 	}
