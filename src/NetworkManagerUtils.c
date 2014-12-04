e52f352339f272987c415ba1512f3b9fbcb92509
Jiří Klimeš <jklimes@redhat.com>
2014-12-04 18:11:39 +0100

    util: fix _log_connection_sort_names_fcn()
    Coverity:
    Defect type: CONSTANT_EXPRESSION_RESULT
    src/NetworkManagerUtils.c:1978: same_on_both_sides: "(v1->diff_result & NM_SETTING_DIFF_RESULT_IN_B) != (v1->diff_result & NM_SETTING_DIFF_RESULT_IN_B)" is always false regardless of the values of its operands because those operands are identical. This occurs as the logical operand of if.

--- src/NetworkManagerUtils.c
+++ src/NetworkManagerUtils.c
@@ -2015,9 +2015,9 @@ _log_connection_sort_names_fcn (gconstpointer a, gconstpointer b)
 	/* we want to first show the items, that disappeared, then the one that changed and
 	 * then the ones that were added. */
 
-	if ((v1->diff_result & NM_SETTING_DIFF_RESULT_IN_A) != (v1->diff_result & NM_SETTING_DIFF_RESULT_IN_A))
+	if ((v1->diff_result & NM_SETTING_DIFF_RESULT_IN_A) != (v2->diff_result & NM_SETTING_DIFF_RESULT_IN_A))
 		return (v1->diff_result & NM_SETTING_DIFF_RESULT_IN_A) ? -1 : 1;
-	if ((v1->diff_result & NM_SETTING_DIFF_RESULT_IN_B) != (v1->diff_result & NM_SETTING_DIFF_RESULT_IN_B))
+	if ((v1->diff_result & NM_SETTING_DIFF_RESULT_IN_B) != (v2->diff_result & NM_SETTING_DIFF_RESULT_IN_B))
 		return (v1->diff_result & NM_SETTING_DIFF_RESULT_IN_B) ? 1 : -1;
 	return strcmp (v1->item_name, v2->item_name);
 }
