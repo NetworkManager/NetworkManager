7972e184e9fcebc6dbd1e0635d63ba2fdd679140
Thomas Haller <thaller@redhat.com>
2014-12-16 23:18:30 +0100

    contrib/bkr: detect GIT_TARGETBRANCH for new 1.0-beta builds

--- contrib/rh-bkr/bkr.py
+++ contrib/rh-bkr/bkr.py
@@ -570,7 +570,9 @@ class CmdSubmit(CmdBase):
                     if re.match(r'^.*/NetworkManager-0.9.9.1-[1-9][0-9]*\.git20140326\.4dba720\.el7\.x86_64\.rpm$', u):
                         return 'rhel-7.0' # stable rhel-7.0 release
                     if re.match(r'^.*/NetworkManager-0.9.11.0-[0-9]+\.[a-f0-9]+\.el7.x86_64.rpm$', u):
-                        return 'master' # current development, pre 1.0
+                        return 'master' # upstream pre 1.0
+                    if re.match(r'^.*/NetworkManager-0.9[0-9][0-9]+.0.0-[0-9]+\.[a-f0-9]+\.el7.x86_64.rpm$', u):
+                        return 'master' # upstream 1.0-beta
                     if re.match(r'^.*/NetworkManager-0.9.10.[0-9]+-[0-9]+\.[a-f0-9]+\.el7.x86_64.rpm$', u):
                         return 'master' # 0.9.10
                     if re.match(r'^.*/NetworkManager-0.9.9.9[0-9]+-[0-9]+\.[a-f0-9]+\.el7.x86_64.rpm$', u):
