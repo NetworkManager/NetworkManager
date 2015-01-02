874e4a7595ac11e53e333dcdd0ad988a47391cc9
Lubomir Rintel <lkundrak@v3.sk>
2015-01-02 15:42:11 +0100

    core: split route management code out from platform
    Create a NMRouteManager singleton.
    
    Refactor, no functional changes apart from change of log domain from
    LOGD_PLATFORM to LOGD_CORE.
    
    Subsequent commit will keep track of the conflicting routes, avoid overwriting
    older ones with newer ones and apply the new ones when the old ones go away.

--- src/platform/nm-platform.h
+++ src/platform/nm-platform.h
@@ -595,9 +595,6 @@ gboolean nm_platform_ip4_route_delete (int ifindex, in_addr_t network, int plen,
 gboolean nm_platform_ip6_route_delete (int ifindex, struct in6_addr network, int plen, guint32 metric);
 gboolean nm_platform_ip4_route_exists (int ifindex, in_addr_t network, int plen, guint32 metric);
 gboolean nm_platform_ip6_route_exists (int ifindex, struct in6_addr network, int plen, guint32 metric);
-gboolean nm_platform_ip4_route_sync (int ifindex, const GArray *known_routes);
-gboolean nm_platform_ip6_route_sync (int ifindex, const GArray *known_routes);
-gboolean nm_platform_route_flush (int ifindex);
 
 const char *nm_platform_link_to_string (const NMPlatformLink *link);
 const char *nm_platform_ip4_address_to_string (const NMPlatformIP4Address *address);
