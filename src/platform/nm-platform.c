435632a757df1886e7427715bede8e53b44e1dde
Lubomir Rintel <lkundrak@v3.sk>
2015-01-25 18:35:42 +0100

    platform: fix route addition ordering
    (cherry picked from commit 47167cab4f4c09861874fcb894afc8770437a292)

--- src/platform/nm-platform.c
+++ src/platform/nm-platform.c
@@ -2101,7 +2101,7 @@ nm_platform_ip4_route_sync (int ifindex, const GArray *known_routes)
 			if (NM_PLATFORM_IP_ROUTE_IS_DEFAULT (known_route))
 				continue;
 
-			if ((known_route->gateway == 0) ^ (i_type != 0)) {
+			if ((known_route->gateway == 0) ^ (i_type == 0)) {
 				/* Make two runs over the list of routes. On the first, only add
 				 * device routes, on the second the others (gateway routes). */
 				continue;
@@ -2175,7 +2175,7 @@ nm_platform_ip6_route_sync (int ifindex, const GArray *known_routes)
 			if (NM_PLATFORM_IP_ROUTE_IS_DEFAULT (known_route))
 				continue;
 
-			if (IN6_IS_ADDR_UNSPECIFIED (&known_route->gateway) ^ (i_type != 0)) {
+			if (IN6_IS_ADDR_UNSPECIFIED (&known_route->gateway) ^ (i_type == 0)) {
 				/* Make two runs over the list of routes. On the first, only add
 				 * device routes, on the second the others (gateway routes). */
 				continue;
