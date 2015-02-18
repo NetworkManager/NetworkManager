1fc9bc401e1de676bac3a3220af1bc094bed6e22
Thomas Haller <thaller@redhat.com>
2015-02-18 19:59:28 +0100

    keyfile: copy read/writer files to libnm-core
    This is the first step to move keyfile to libnm. For now, only
    copy the files to make later changes nicer in git-history.
    
      /bin/cp src/settings/plugins/keyfile/reader.c libnm-core/nm-keyfile-reader.c
      /bin/cp src/settings/plugins/keyfile/reader.h libnm-core/nm-keyfile-reader.h
      /bin/cp src/settings/plugins/keyfile/utils.c  libnm-core/nm-keyfile-utils.c
      /bin/cp src/settings/plugins/keyfile/utils.h  libnm-core/nm-keyfile-utils.h
      /bin/cp src/settings/plugins/keyfile/writer.c libnm-core/nm-keyfile-writer.c
      /bin/cp src/settings/plugins/keyfile/writer.h libnm-core/nm-keyfile-writer.h

---
+++ libnm-core/nm-keyfile-reader.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* NetworkManager system settings service - keyfile plugin
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Copyright (C) 2008 Novell, Inc.
+ * Copyright (C) 2008 Red Hat, Inc.
+ */
+
+#ifndef _KEYFILE_PLUGIN_READER_H
+#define _KEYFILE_PLUGIN_READER_H
+
+#include <glib.h>
+#include <nm-connection.h>
+
+NMConnection *nm_keyfile_plugin_connection_from_file (const char *filename, GError **error);
+
+#endif /* _KEYFILE_PLUGIN_READER_H */
