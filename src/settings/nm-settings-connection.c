9b7c8db16a0f8e4f87c793412fd519d944a7ee7e
Lubomir Rintel <lkundrak@v3.sk>
2015-02-18 12:41:49 +0100

    settings-connection: memleak: free filename on dispose
    ==4203== 97 (+97) bytes in 2 (+2) blocks are definitely lost in loss record 4,586 of 5,632
    ==4203==    at 0x4C29BCF: malloc (in /usr/lib64/valgrind/vgpreload_memcheck-amd64-linux.so)
    ==4203==    by 0x7F4A6F5: g_malloc (gmem.c:97)
    ==4203==    by 0x7F6301E: g_strdup (gstrfuncs.c:356)
    ==4203==    by 0x47E4C8: nm_settings_connection_set_filename (nm-settings-connection.c:2228)
    ==4203==    by 0x7CBF6EC: object_set_property (gobject.c:1415)
    ==4203==    by 0x7CBF6EC: g_object_new_internal (gobject.c:1828)
    ==4203==    by 0x7CC1194: g_object_new_valist (gobject.c:2034)
    ==4203==    by 0x7CC14D0: g_object_new (gobject.c:1617)
    ==4203==    by 0x12A08193: nm_ifcfg_connection_new (nm-ifcfg-connection.c:229)
    ==4203==    by 0x12A0542B: update_connection (plugin.c:225)
    ==4203==    by 0x12A0696A: add_connection (plugin.c:715)
    ==4203==    by 0x4814BB: nm_settings_add_connection (nm-settings.c:1030)
    ==4203==    by 0x4817DE: pk_add_cb (nm-settings.c:1136)

--- src/settings/nm-settings-connection.c
+++ src/settings/nm-settings-connection.c
@@ -2314,6 +2314,8 @@ dispose (GObject *object)
 	}
 	g_clear_object (&priv->agent_mgr);
 
+	g_clear_pointer (&priv->filename, g_free);
+
 	G_OBJECT_CLASS (nm_settings_connection_parent_class)->dispose (object);
 }
 
