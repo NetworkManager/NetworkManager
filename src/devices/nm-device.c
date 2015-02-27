NetworkManager/NetworkManager

commit 94c1e7827a84bb165787d4ff65cc1e89a61f22e7

device: fix leaking queued NMActiveConnection
The queued activation request must transition state to
DEACTIVATED, otherwise it is not removed from the list
of active_connections in NMManager.

(cherry picked from commit b9da094da90a54a591f25245a2d703a2bac3aab9)

Conflicts:
	src/devices/nm-device.c

src/devices/nm-device.c
@@ -5046,6 +5046,14 @@ disconnect_cb (NMDevice *device,
 	}
 }
 
+_clear_queued_act_request (NMDevicePrivate *priv)
+{
+	if (priv->queued_act_request) {
+		nm_active_connection_set_state ((NMActiveConnection *) priv->queued_act_request, NM_ACTIVE_CONNECTION_STATE_DEACTIVATED);
+		g_clear_object (&priv->queued_act_request);
+	}
+}
+
 static void
 impl_device_disconnect (NMDevice *device, DBusGMethodInvocation *context)
 {
@@ -5121,7 +5129,7 @@ nm_device_queue_activation (NMDevice *self, NMActRequest *req)
 	}
 
 	/* supercede any already-queued request */
-	g_clear_object (&priv->queued_act_request);
+	_clear_queued_act_request (priv);
 	priv->queued_act_request = g_object_ref (req);
 
 	/* Deactivate existing activation request first */
@@ -6766,7 +6774,7 @@ _set_state_full (NMDevice *device,
 
 	if (state <= NM_DEVICE_STATE_UNAVAILABLE) {
 		_clear_available_connections (device, TRUE);
-		g_clear_object (&priv->queued_act_request);
+		_clear_queued_act_request (priv);
 	}
 
 	/* Update the available connections list when a device first becomes available */
@@ -7491,7 +7499,7 @@ dispose (GObject *object)
 		priv->carrier_wait_id = 0;
 	}
 
-	g_clear_object (&priv->queued_act_request);
+	_clear_queued_act_request (priv);
 
 	platform = nm_platform_get ();
 	g_signal_handlers_disconnect_by_func (platform, G_CALLBACK (device_ip_changed), self);

commit f8c69727a60c6921fc6c4f95398030911c691dc3

device: fix build after 94c1e7827a84bb165787d4ff65cc1e89a61f22e7
Fixes: 94c1e7827a84bb165787d4ff65cc1e89a61f22e7

src/devices/nm-device.c
@@ -5046,6 +5046,7 @@ disconnect_cb (NMDevice *device,
 	}
 }
 
+static void
 _clear_queued_act_request (NMDevicePrivate *priv)
 {
 	if (priv->queued_act_request) {