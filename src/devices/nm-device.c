NetworkManager/NetworkManager

commit 5149fd120d24e7622fb264fffaa4bed04eb579d6

iface-helper: add nm-iface-helper for dynamic configure-then-quit support
When quitting, the Manager asks each device to spawn the interface helper,
which persists and manages dynamic address on the interface after NetworkManager
is gone.  If the dynamic address cannot be maintaned, the helper quits and
the interface's address may be removed when their lifetime runs out.

To keep the helper as simple as possible, NetworkManager passes most of the
configuration on the command-line, including some properties of the device's
current state, which are necessary for the helper to maintain DHCP leases
or IPv6 SLAAC addresses.

src/devices/nm-device.c
@@ -6990,6 +6990,173 @@ nm_device_cleanup (NMDevice *self, NMDeviceStateReason reason)
 	_cleanup_generic_post (self, TRUE);
 }
 
+static char *
+bin2hexstr (const char *bytes, gsize len)
+{
+	GString *str;
+	int i;
+
+	g_return_val_if_fail (bytes != NULL, NULL);
+	g_return_val_if_fail (len > 0, NULL);
+
+	str = g_string_sized_new (len * 2 + 1);
+	for (i = 0; i < len; i++) {
+		if (str->len)
+			g_string_append_c (str, ':');
+		g_string_append_printf (str, "%02x", (guint8) bytes[i]);
+	}
+	return g_string_free (str, FALSE);
+}
+
+static char *
+find_dhcp4_address (NMDevice *self)
+{
+	NMDevicePrivate *priv = NM_DEVICE_GET_PRIVATE (self);
+	guint i, n;
+
+	if (!priv->ip4_config)
+		return NULL;
+
+	n = nm_ip4_config_get_num_addresses (priv->ip4_config);
+	for (i = 0; i < n; i++) {
+		const NMPlatformIP4Address *a = nm_ip4_config_get_address (priv->ip4_config, i);
+
+		if (a->source == NM_IP_CONFIG_SOURCE_DHCP)
+			return g_strdup (nm_utils_inet4_ntop (a->address, NULL));
+	}
+	return NULL;
+}
+
+void
+nm_device_spawn_iface_helper (NMDevice *self)
+{
+	NMDevicePrivate *priv = NM_DEVICE_GET_PRIVATE (self);
+	gboolean priority_set = FALSE, configured = FALSE;
+	NMConnection *connection;
+	GError *error = NULL;
+	const char *method;
+	GPtrArray *argv;
+	gs_free char *dhcp4_address = NULL;
+
+	if (priv->state != NM_DEVICE_STATE_ACTIVATED)
+		return;
+	if (!nm_device_can_assume_connections (self))
+		return;
+
+	connection = nm_device_get_connection (self);
+	g_assert (connection);
+
+	argv = g_ptr_array_sized_new (10);
+	g_ptr_array_set_free_func (argv, g_free);
+
+	g_ptr_array_add (argv, g_strdup (LIBEXECDIR "/nm-iface-helper"));
+	g_ptr_array_add (argv, g_strdup ("--ifname"));
+	g_ptr_array_add (argv, g_strdup (nm_device_get_ip_iface (self)));
+	g_ptr_array_add (argv, g_strdup ("--uuid"));
+	g_ptr_array_add (argv, g_strdup (nm_connection_get_uuid (connection)));
+
+	dhcp4_address = find_dhcp4_address (self);
+
+	method = nm_utils_get_ip_config_method (connection, NM_TYPE_SETTING_IP4_CONFIG);
+	if (   priv->ip4_config
+	    && priv->ip4_state == IP_DONE
+	    && g_strcmp0 (method, NM_SETTING_IP4_CONFIG_METHOD_AUTO) == 0
+	    && priv->dhcp4_client
+	    && dhcp4_address) {
+		NMSettingIPConfig *s_ip4;
+		GBytes *client_id;
+		char *hex_client_id;
+		const char *hostname;
+
+		s_ip4 = nm_connection_get_setting_ip4_config (connection);
+		g_assert (s_ip4);
+
+		g_ptr_array_add (argv, g_strdup ("--priority"));
+		g_ptr_array_add (argv, g_strdup_printf ("%u", nm_dhcp_client_get_priority (priv->dhcp4_client)));
+		priority_set = TRUE;
+
+		g_ptr_array_add (argv, g_strdup ("--dhcp4"));
+		g_ptr_array_add (argv, g_strdup (dhcp4_address));
+		if (nm_setting_ip_config_get_may_fail (s_ip4) == FALSE)
+			g_ptr_array_add (argv, g_strdup ("--dhcp4-required"));
+
+		client_id = nm_dhcp_client_get_client_id (priv->dhcp4_client);
+		if (client_id) {
+			g_ptr_array_add (argv, g_strdup ("--dhcp4-clientid"));
+			hex_client_id = bin2hexstr (g_bytes_get_data (client_id, NULL),
+			                            g_bytes_get_size (client_id));
+			g_ptr_array_add (argv, hex_client_id);
+		}
+
+		hostname = nm_dhcp_client_get_hostname (priv->dhcp4_client);
+		if (client_id) {
+			g_ptr_array_add (argv, g_strdup ("--dhcp4-hostname"));
+			g_ptr_array_add (argv, g_strdup (hostname));
+		}
+
+		configured = TRUE;
+	}
+
+	method = nm_utils_get_ip_config_method (connection, NM_TYPE_SETTING_IP6_CONFIG);
+	if (   priv->ip6_config
+	    && priv->ip6_state == IP_DONE
+	    && g_strcmp0 (method, NM_SETTING_IP6_CONFIG_METHOD_AUTO) == 0
+	    && priv->rdisc
+	    && priv->ac_ip6_config) {
+		NMSettingIPConfig *s_ip6;
+		char *hex_iid;
+		NMUtilsIPv6IfaceId iid = NM_UTILS_IPV6_IFACE_ID_INIT;
+
+		s_ip6 = nm_connection_get_setting_ip6_config (connection);
+		g_assert (s_ip6);
+
+		g_ptr_array_add (argv, g_strdup ("--slaac"));
+
+		if (nm_setting_ip_config_get_may_fail (s_ip6) == FALSE)
+			g_ptr_array_add (argv, g_strdup ("--slaac-required"));
+
+		g_ptr_array_add (argv, g_strdup ("--slaac-tempaddr"));
+		g_ptr_array_add (argv, g_strdup_printf ("%d", priv->rdisc_use_tempaddr));
+
+		if (nm_device_get_ip_iface_identifier (self, &iid)) {
+			g_ptr_array_add (argv, g_strdup ("--iid"));
+			hex_iid = bin2hexstr ((const char *) iid.id_u8, sizeof (NMUtilsIPv6IfaceId));
+			g_ptr_array_add (argv, hex_iid);
+		}
+
+		configured = TRUE;
+	}
+
+	if (configured) {
+		GPid pid;
+
+		if (!priority_set) {
+			g_ptr_array_add (argv, g_strdup ("--priority"));
+			g_ptr_array_add (argv, g_strdup_printf ("%u", nm_device_get_priority (self)));
+		}
+
+		g_ptr_array_add (argv, NULL);
+
+		if (nm_logging_enabled (LOGL_DEBUG, LOGD_DEVICE)) {
+			char *tmp;
+
+			tmp = g_strjoinv (" ", (char **) argv->pdata);
+			_LOGD (LOGD_DEVICE, "running '%s'", tmp);
+			g_free (tmp);
+		}
+
+		if (g_spawn_async (NULL, (char **) argv->pdata, NULL,
+		                   G_SPAWN_DO_NOT_REAP_CHILD, NULL, NULL, &pid, &error)) {
+			_LOGI (LOGD_DEVICE, "spawned helper PID %u", (guint) pid);
+		} else {
+			_LOGW (LOGD_DEVICE, "failed to spawn helper: %s", error->message);
+			g_error_free (error);
+		}
+	}
+
+	g_ptr_array_unref (argv);
+}
+
 /***********************************************************/
 
 static gboolean

.gitignore
@@ -232,6 +232,7 @@ valgrind-*.log
 /src/dhcp-manager/tests/test-dhcp-options
 /src/dhcp-manager/tests/test-dhcp-utils
 /src/dnsmasq-manager/tests/test-dnsmasq-utils
+/src/nm-iface-helper
 /src/settings/plugins/ibft/tests/test-ibft
 /src/settings/plugins/ifcfg-rh/tests/network-scripts/*-Test_Write_*
 /src/settings/plugins/ifcfg-rh/tests/network-scripts/Test_Write_*

contrib/fedora/rpm/NetworkManager.spec
@@ -518,6 +518,7 @@ fi
 %{_libexecdir}/nm-dhcp-helper
 %{_libexecdir}/nm-avahi-autoipd.action
 %{_libexecdir}/nm-dispatcher
+%{_libexecdir}/nm-iface-helper
 %dir %{_libdir}/NetworkManager
 %{_libdir}/NetworkManager/libnm-settings-plugin*.so
 %{_mandir}/man1/*

po/POTFILES.in
@@ -147,6 +147,7 @@ src/devices/wifi/nm-wifi-ap-utils.c
 src/devices/wimax/nm-device-wimax.c
 src/devices/wwan/nm-modem-broadband.c
 src/nm-config.c
+src/nm-iface-helper.c
 src/nm-logging.c
 src/nm-manager.c
 src/nm-sleep-monitor-systemd.c

src/Makefile.am
@@ -48,7 +48,10 @@ AM_CPPFLAGS =				\
 # primarily for its side effect of removing duplicates.
 AM_CPPFLAGS += $(foreach d,$(sort $(dir $(libNetworkManager_la_SOURCES))),-I$(top_srcdir)/src/$d)
 
-noinst_LTLIBRARIES = libNetworkManager.la libsystemd-dhcp.la
+noinst_LTLIBRARIES = \
+	libNetworkManager.la \
+	libnm-iface-helper.la \
+	libsystemd-dhcp.la
 
 ######################
 # libsystemd-dhcp
@@ -455,6 +458,86 @@ libNetworkManager_la_LIBADD += $(LIBSOUP_LIBS)
 endif
 
 NetworkManager_LDFLAGS = -rdynamic
+
+######################
+
+libnm_iface_helper_la_SOURCES = \
+	dhcp-manager/nm-dhcp-client.c \
+	dhcp-manager/nm-dhcp-client.h \
+	dhcp-manager/nm-dhcp-utils.c \
+	dhcp-manager/nm-dhcp-utils.h \
+	dhcp-manager/nm-dhcp-manager.c \
+	dhcp-manager/nm-dhcp-manager.h \
+	\
+	platform/nm-linux-platform.c \
+	platform/nm-linux-platform.h \
+	platform/nm-platform.c \
+	platform/nm-platform.h \
+	platform/wifi/wifi-utils-nl80211.c \
+	platform/wifi/wifi-utils-nl80211.h \
+	platform/wifi/wifi-utils-private.h \
+	platform/wifi/wifi-utils.c \
+	platform/wifi/wifi-utils.h \
+	\
+	rdisc/nm-fake-rdisc.c \
+	rdisc/nm-fake-rdisc.h \
+	rdisc/nm-lndp-rdisc.c \
+	rdisc/nm-lndp-rdisc.h \
+	rdisc/nm-rdisc.c \
+	rdisc/nm-rdisc.h \
+	\
+	nm-ip4-config.c \
+	nm-ip4-config.h \
+	nm-ip6-config.c \
+	nm-ip6-config.h \
+	\
+	nm-enum-types.c \
+	nm-enum-types.h \
+	nm-logging.c \
+	nm-logging.h \
+	nm-posix-signals.c \
+	nm-posix-signals.h \
+	NetworkManagerUtils.c \
+	NetworkManagerUtils.h
+
+if WITH_WEXT
+libnm_iface_helper_la_SOURCES += \
+	platform/wifi/wifi-utils-wext.c \
+	platform/wifi/wifi-utils-wext.h
+endif
+
+libnm_iface_helper_la_LIBADD = \
+	$(top_builddir)/libnm-core/libnm-core.la \
+	libsystemd-dhcp.la \
+	$(DBUS_LIBS) \
+	$(GLIB_LIBS) \
+	$(GUDEV_LIBS) \
+	$(LIBNL_LIBS) \
+	$(LIBNDP_LIBS) \
+	$(LIBDL) \
+	$(LIBM)
+
+libexec_PROGRAMS = nm-iface-helper
+
+nm_iface_helper_SOURCES = \
+	dhcp-manager/nm-dhcp-systemd.h \
+	dhcp-manager/nm-dhcp-systemd.c \
+	nm-iface-helper.c \
+	main-utils.c \
+	main-utils.h
+
+nm_iface_helper_LDADD = \
+	$(top_builddir)/libnm-core/libnm-core.la \
+	libsystemd-dhcp.la \
+	libnm-iface-helper.la \
+	$(DBUS_LIBS) \
+	$(GLIB_LIBS) \
+	$(GUDEV_LIBS) \
+	$(LIBNL_LIBS) \
+	$(LIBNDP_LIBS) \
+	$(LIBM)
+
+nm_iface_helper_LDFLAGS = -rdynamic
 
 ######################
 

src/devices/nm-device.h
@@ -365,6 +365,8 @@ NMConnection *nm_device_new_default_connection (NMDevice *self);
 const NMPlatformIP4Route *nm_device_get_ip4_default_route (NMDevice *self);
 const NMPlatformIP6Route *nm_device_get_ip6_default_route (NMDevice *self);
 
+void nm_device_spawn_iface_helper (NMDevice *self);
+
 G_END_DECLS
 
 /* For testing only */

src/dhcp-manager/nm-dhcp-manager.c
@@ -386,11 +386,18 @@ static void
 nm_dhcp_manager_init (NMDhcpManager *self)
 {
 	NMDhcpManagerPrivate *priv = NM_DHCP_MANAGER_GET_PRIVATE (self);
+	NMConfig *config = nm_config_get ();
 	const char *client;
 	GError *error = NULL;
 
 	/* Client-specific setup */
-	client = nm_config_get_dhcp_client (nm_config_get ());
+	client = nm_config_get_dhcp_client (config);
+	if (nm_config_get_configure_and_quit (config)) {
+		if (g_strcmp0 (client, "internal") != 0)
+			nm_log_warn (LOGD_DHCP, "Using internal DHCP client since configure-and-quit is set.");
+		client = "internal";
+	}
+
 	priv->client_type = get_client_type (client, &error);
 	if (priv->client_type == G_TYPE_INVALID) {
 		nm_log_warn (LOGD_DHCP, "No usable DHCP client found (%s)! DHCP configurations will fail.",

src/main.c
@@ -178,17 +178,10 @@ _init_nm_debug (const char *debug)
 }
 
 static void
-manager_startup_complete (NMManager *manager, GParamSpec *pspec, gpointer user_data)
+manager_configure_quit (NMManager *manager, gpointer user_data)
 {
-	NMConfig *config = NM_CONFIG (user_data);
-	gboolean startup = FALSE;
-
-	g_object_get (G_OBJECT (manager), NM_MANAGER_STARTUP, &startup, NULL);
-
-	if (!startup && nm_config_get_configure_and_quit (config)) {
-		nm_log_info (LOGD_CORE, "quitting now that startup is complete");
-		g_main_loop_quit (main_loop);
-	}
+	nm_log_info (LOGD_CORE, "quitting now that startup is complete");
+	g_main_loop_quit (main_loop);
 }
 
 /*
@@ -462,10 +455,7 @@ main (int argc, char *argv[])
 		}
 	}
 
-	g_signal_connect (manager,
-	                  "notify::" NM_MANAGER_STARTUP,
-	                  G_CALLBACK (manager_startup_complete),
-	                  config);
+	g_signal_connect (manager, NM_MANAGER_CONFIGURE_QUIT, G_CALLBACK (manager_configure_quit), config);
 
 	nm_manager_start (manager);
 
@@ -485,10 +475,10 @@ main (int argc, char *argv[])
 	success = TRUE;
 
 	/* Told to quit before getting to the mainloop by the signal handler */
-	if (quit_early == TRUE)
-		goto done;
+	if (!quit_early)
+		g_main_loop_run (main_loop);
 
-	g_main_loop_run (main_loop);
+	nm_manager_stop (manager);
 
 done:
 	g_clear_object (&manager);

commit 1dae47e9cc4c2ec43bcabf9fa8610acc0b6e257b

device: fix handling if DHCP hostname for configure-and-quit
Fixes: 5149fd120d24e7622fb264fffaa4bed04eb579d6

src/devices/nm-device.c
@@ -7602,7 +7602,7 @@ nm_device_spawn_iface_helper (NMDevice *self)
 		}
 
 		hostname = nm_dhcp_client_get_hostname (priv->dhcp4_client);
-		if (client_id) {
+		if (hostname) {
 			g_ptr_array_add (argv, g_strdup ("--dhcp4-hostname"));
 			g_ptr_array_add (argv, g_strdup (hostname));
 		}