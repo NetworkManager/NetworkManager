256f11e6fc7fc2f51da9ea0c40e0aad52089de7a
Dan Williams <dcbw@redhat.com>
2015-03-12 13:24:32 -0500

    dispatcher: fix memleak in construct_device_dhcp4_items()
    When iterating over a container variant, the children values
    must be unrefed when no longer used.
    
    (cherry picked from commit acd6226e8ac2d511026052011627bd025362956d)

--- callouts/nm-dispatcher-utils.c
+++ callouts/nm-dispatcher-utils.c
@@ -210,6 +210,7 @@ construct_device_dhcp4_items (GSList *items, GVariant *dhcp4_config)
 		tmp = g_variant_get_string (val, NULL);
 		items = g_slist_prepend (items, g_strdup_printf ("DHCP4_%s=%s", ucased, tmp));
 		g_free (ucased);
+		g_variant_unref (val);
 	}
 	return items;
 }
