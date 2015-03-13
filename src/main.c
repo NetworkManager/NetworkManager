7be9024c826f524a1383bb6fd1264a1f9247b1d1
Thomas Haller <thaller@redhat.com>
2015-03-13 23:28:13 +0100

    main: (order) parse state file later and use nm-logging
    Parse the state file a bit later after daemonizing and setting up
    logging. That way, we can use nm-logging.

--- src/main.c
+++ src/main.c
@@ -366,16 +366,6 @@ main (int argc, char *argv[])
 		}
 	}
 
-	/* Parse the state file */
-	if (!parse_state_file (global_opt.state_file, &net_enabled, &wifi_enabled, &wwan_enabled, &wimax_enabled, &error)) {
-		fprintf (stderr, _("State file %s parsing failed: (%d) %s\n"),
-		         global_opt.state_file,
-		         error ? error->code : -1,
-		         (error && error->message) ? error->message : _("unknown"));
-		/* Not a hard failure */
-	}
-	g_clear_error (&error);
-
 	if (global_opt.become_daemon && !global_opt.debug) {
 		if (daemon (0, 0) < 0) {
 			int saved_errno;
@@ -393,6 +383,16 @@ main (int argc, char *argv[])
 	nm_main_utils_setup_signals (main_loop);
 
 	nm_logging_syslog_openlog (global_opt.debug);
+
+	/* Parse the state file */
+	if (!parse_state_file (global_opt.state_file, &net_enabled, &wifi_enabled, &wwan_enabled, &wimax_enabled, &error)) {
+		nm_log_err (LOGD_CORE, "State file %s parsing failed: (%d) %s",
+		            global_opt.state_file,
+		            error ? error->code : -1,
+		            (error && error->message) ? error->message : _("unknown"));
+		/* Not a hard failure */
+	}
+	g_clear_error (&error);
 
 	dbus_threads_init_default ();
 
