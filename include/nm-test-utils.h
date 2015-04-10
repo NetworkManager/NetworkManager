NetworkManager/NetworkManager

commit 373d09b0425c6dfb699ee4b6615ded0177d0e344

test: log message at g_test_assert_expected_messages()
When disabling assert-logging with no-expect-message,
print a line at every g_test_assert_expected_messages()
invocation.

include/nm-test-utils.h
@@ -135,8 +135,9 @@ struct __nmtst_internal __nmtst_internal = { 0 }; \
 __attribute__ ((destructor)) static void \
 _nmtst_exit (void) \
 { \
-	nmtst_free (); \
+	__nmtst_internal.assert_logging = FALSE; \
 	g_test_assert_expected_messages (); \
+	nmtst_free (); \
 }
 
 
@@ -434,12 +435,23 @@ nmtst_is_debug (void)
 	G_STMT_START { \
 		g_assert (nmtst_initialized ()); \
 		if (__nmtst_internal.assert_logging && __nmtst_internal.no_expect_message) { \
-			g_debug ("nmtst: swallow g_test_expect_message %s", G_STRINGIFY ((__VA_ARGS__))); \
+			g_debug ("nmtst: assert-logging: g_test_expect_message %s", G_STRINGIFY ((__VA_ARGS__))); \
 		} else { \
 			G_GNUC_BEGIN_IGNORE_DEPRECATIONS \
 			g_test_expect_message (__VA_ARGS__); \
 			G_GNUC_END_IGNORE_DEPRECATIONS \
 		} \
+	} G_STMT_END
+#undef g_test_assert_expected_messages_internal
+#define g_test_assert_expected_messages_internal(domain, file, line, func) \
+	G_STMT_START { \
+		g_assert (nmtst_initialized ()); \
+		if (__nmtst_internal.assert_logging && __nmtst_internal.no_expect_message) \
+			g_debug ("nmtst: assert-logging: g_test_assert_expected_messages(%s, %s:%d, %s)", domain, file, line, func); \
+		\
+		G_GNUC_BEGIN_IGNORE_DEPRECATIONS \
+		g_test_assert_expected_messages_internal (domain, file, line, func); \
+		G_GNUC_END_IGNORE_DEPRECATIONS \
 	} G_STMT_END
 #endif
 

commit 0aca44c857d8d949b272baacbfbaef08d16618d4

test: fix compiler warning in g_test_assert_expected_messages_internal()
-Wformat of gcc determines that the string arguments are NULL.

Fixes: 373d09b0425c6dfb699ee4b6615ded0177d0e344

include/nm-test-utils.h
@@ -445,12 +445,17 @@ nmtst_is_debug (void)
 #undef g_test_assert_expected_messages_internal
 #define g_test_assert_expected_messages_internal(domain, file, line, func) \
 	G_STMT_START { \
+		const char *_domain = (domain); \
+		const char *_file = (file); \
+		const char *_func = (func); \
+		int _line = (line); \
+		\
 		g_assert (nmtst_initialized ()); \
 		if (__nmtst_internal.assert_logging && __nmtst_internal.no_expect_message) \
-			g_debug ("nmtst: assert-logging: g_test_assert_expected_messages(%s, %s:%d, %s)", domain, file, line, func); \
+			g_debug ("nmtst: assert-logging: g_test_assert_expected_messages(%s, %s:%d, %s)", _domain?:"", _file?:"", _line, _func?:""); \
 		\
 		G_GNUC_BEGIN_IGNORE_DEPRECATIONS \
-		g_test_assert_expected_messages_internal (domain, file, line, func); \
+		g_test_assert_expected_messages_internal (_domain, _file, _line, _func); \
 		G_GNUC_END_IGNORE_DEPRECATIONS \
 	} G_STMT_END
 #endif