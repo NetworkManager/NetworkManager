967376b84036686a9694b46decc301a098ab74cc
Thomas Haller <thaller@redhat.com>
2015-04-21 11:48:25 -0400

    core: add nm_utils_monotonic_timestamp_as_boottime() function

--- src/NetworkManagerUtils.h
+++ src/NetworkManagerUtils.h
@@ -141,6 +141,7 @@ gint64 nm_utils_get_monotonic_timestamp_ns (void);
 gint64 nm_utils_get_monotonic_timestamp_us (void);
 gint64 nm_utils_get_monotonic_timestamp_ms (void);
 gint32 nm_utils_get_monotonic_timestamp_s (void);
+gint64 nm_utils_monotonic_timestamp_as_boottime (gint64 timestamp, gint64 timestamp_ticks_per_ns);
 
 const char *ASSERT_VALID_PATH_COMPONENT (const char *name) G_GNUC_WARN_UNUSED_RESULT;
 const char *nm_utils_ip6_property_path (const char *ifname, const char *property);
