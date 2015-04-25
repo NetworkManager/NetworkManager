3cdde18f52235e5bc9892a871a88ec84df63bf5d
Thomas Haller <thaller@redhat.com>
2015-04-25 16:42:26 +0200

    platform: add vlan_id parameter to NMPlatformLink

--- src/platform/nm-platform.c
+++ src/platform/nm-platform.c
@@ -2442,6 +2442,7 @@ nm_platform_link_to_string (const NMPlatformLink *link)
 	char parent[20];
 	char str_flags[64];
 	char *driver, *udi;
+	char str_vlan[16];
 	GString *str;
 
 	if (!link)
@@ -2472,18 +2473,25 @@ nm_platform_link_to_string (const NMPlatformLink *link)
 	else
 		parent[0] = 0;
 
+	if (link->vlan_id)
+		g_snprintf (str_vlan, sizeof (str_vlan), " vlan %u", (guint) link->vlan_id);
+	else
+		str_vlan[0] = '\0';
+
 	driver = link->driver ? g_strdup_printf (" driver '%s'", link->driver) : NULL;
 	udi = link->udi ? g_strdup_printf (" udi '%s'", link->udi) : NULL;
 
 	g_snprintf (to_string_buffer, sizeof (to_string_buffer), "%d: %s%s <%s> mtu %d%s "
 	            "%s" /* link->type */
 	            "%s%s" /* kind */
+	            "%s" /* vlan */
 	            "%s%s",
 	            link->ifindex, link->name, parent, str->str,
 	            link->mtu, master,
 	            nm_link_type_to_string (link->type),
 	            link->type != NM_LINK_TYPE_UNKNOWN && link->kind ? " kind " : "",
 	            link->type != NM_LINK_TYPE_UNKNOWN && link->kind ? link->kind : "",
+	            str_vlan,
 	            driver ? driver : "", udi ? udi : "");
 	g_string_free (str, TRUE);
 	g_free (driver);
@@ -2769,6 +2777,7 @@ nm_platform_link_cmp (const NMPlatformLink *a, const NMPlatformLink *b)
 	_CMP_FIELD (a, b, master);
 	_CMP_FIELD (a, b, parent);
 	_CMP_FIELD (a, b, up);
+	_CMP_FIELD (a, b, vlan_id);
 	_CMP_FIELD (a, b, flags);
 	_CMP_FIELD (a, b, connected);
 	_CMP_FIELD (a, b, arp);
