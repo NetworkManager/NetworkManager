acb39d4b3190968d6f84bd229ac67325be479eff
Thomas Haller <thaller@redhat.com>
2015-05-04 15:55:12 +0200

    platform: merge setup_devices() function into constructed()
    We already populate the netlink cache in constructed(). No need
    to wait with udev devices until nm_platform_query_devices(). Just
    do it right away.
    
    Add a hack to keep 'lo' default-unmanaged. Now that we load
    udev devices earlier, we end up clearing the default-unmanged
    flag on 'lo', which has bad consequences.
    
    (cherry picked from commit d6ce01f115e0195da08e0b1c0cde1c25ea747f77)

--- src/platform/nm-platform.h
+++ src/platform/nm-platform.h
@@ -368,8 +368,6 @@ struct _NMPlatform {
 typedef struct {
 	GObjectClass parent;
 
-	void (*setup_devices) (NMPlatform *);
-
 	gboolean (*sysctl_set) (NMPlatform *, const char *path, const char *value);
 	char * (*sysctl_get) (NMPlatform *, const char *path);
 
