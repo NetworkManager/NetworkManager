f575c6a5cbf9266d6bfa21d068889ed449f0e476
Dan Williams <dcbw@redhat.com>
2015-05-06 14:55:46 -0500

    dns: clear error before updating resolv.conf
    If the resolv.conf method failed, don't trigger a warning by overwriting
    an already-set error.

--- src/dns-manager/nm-dns-manager.c
+++ src/dns-manager/nm-dns-manager.c
@@ -848,8 +848,10 @@ update_dns (NMDnsManager *self,
 
 	/* Unless we've already done it, update private resolv.conf in NMRUNDIR
 	   ignoring any errors */
-	if (!(update && priv->rc_manager == NM_DNS_MANAGER_RESOLV_CONF_MAN_NONE))
+	if (!(update && priv->rc_manager == NM_DNS_MANAGER_RESOLV_CONF_MAN_NONE)) {
+		g_clear_error (error);
 		update_resolv_conf (searches, nameservers, error, FALSE);
+	}
 
 	/* signal that resolv.conf was changed */
 	if (update && success)
