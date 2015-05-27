dce050a45d66948a6243ee08ebaf6dee4494f1c6
Jiří Klimeš <jklimes@redhat.com>
2015-05-27 11:20:31 +0200

    device: trivial: fix a copy/paste error in comment

--- src/devices/nm-device.c
+++ src/devices/nm-device.c
@@ -585,7 +585,7 @@ nm_device_get_ip_ifindex (NMDevice *self)
 	g_return_val_if_fail (self != NULL, 0);
 
 	priv = NM_DEVICE_GET_PRIVATE (self);
-	/* If it's not set, default to iface */
+	/* If it's not set, default to ifindex */
 	return priv->ip_iface ? priv->ip_ifindex : priv->ifindex;
 }
 
