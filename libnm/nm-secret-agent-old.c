NetworkManager/NetworkManager

commit 9ffcecf86ad2230860cf8fdf5667884782ee64dd

libnm: don't take ownership of input argument in NMSecretAgentOld:get_secrets_cb()
Fixes: 6793a32a8c5445103ba3680bb5e4c31727096099

libnm/nm-secret-agent-old.c
@@ -302,7 +302,7 @@ get_secrets_cb (NMSecretAgentOld *self,
 		g_dbus_method_invocation_return_gerror (info->context, error);
 	else {
 		g_dbus_method_invocation_return_value (info->context,
-		                                       g_variant_new ("(@a{sa{sv}})", secrets));
+		                                       g_variant_new ("(a{sa{sv}})", secrets));
 	}
 
 	/* Remove the request from internal tracking */

commit 2071e4794f6cdf551d31584c17c8896d7029e3ac

libnm: fix take ownership of floating argument in NMSecretAgentOld:get_secrets_cb()
The previous patch 9ffcecf86ad2230860cf8fdf5667884782ee64dd was
completely wrong.

It tried to fix callers that provided a floating GVariant reference.
We require the caller to unref @secrets, so the correct fix it to
ensure that the reference is not floating.

Fixes: 9ffcecf86ad2230860cf8fdf5667884782ee64dd
Fixes: 6793a32a8c5445103ba3680bb5e4c31727096099

libnm/nm-secret-agent-old.c
@@ -301,8 +301,9 @@ get_secrets_cb (NMSecretAgentOld *self,
 	if (error)
 		g_dbus_method_invocation_return_gerror (info->context, error);
 	else {
+		g_variant_take_ref (secrets);
 		g_dbus_method_invocation_return_value (info->context,
-		                                       g_variant_new ("(a{sa{sv}})", secrets));
+		                                       g_variant_new ("(@a{sa{sv}})", secrets));
 	}
 
 	/* Remove the request from internal tracking */