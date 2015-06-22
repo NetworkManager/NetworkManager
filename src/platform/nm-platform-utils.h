a3c4bf81bba6aeb920806a1c2ebf7b2d2a11cfe8
Thomas Haller <thaller@redhat.com>
2015-06-22 15:45:10 +0200

    platform: move address lifetime utils function to nm-platform-utils.c
    (cherry picked from commit 626a85530ee646a2333c89818b273f0598f9bc6d)

--- src/platform/nm-platform-utils.h
+++ src/platform/nm-platform-utils.h
@@ -50,5 +50,17 @@ gboolean nmp_utils_mii_supports_carrier_detect (const char *ifname);
 
 const char *nmp_utils_udev_get_driver (GUdevDevice *device);
 
+guint32 nmp_utils_lifetime_rebase_relative_time_on_now (guint32 timestamp,
+                                                        guint32 duration,
+                                                        guint32 now,
+                                                        guint32 padding);
+
+gboolean nmp_utils_lifetime_get (guint32 timestamp,
+                                 guint32 lifetime,
+                                 guint32 preferred,
+                                 guint32 now,
+                                 guint32 padding,
+                                 guint32 *out_lifetime,
+                                 guint32 *out_preferred);
 
 #endif /* __NM_PLATFORM_UTILS_H__ */
