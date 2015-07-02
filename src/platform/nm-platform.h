NetworkManager/NetworkManager

commit a130f72d745bcfef8aca228f40e8d94ce18bbaef

platform: mark internal enums to be skipped by glib-mkenums
No need to generate glib type information for these internal
enums.

src/platform/nm-platform.h
@@ -54,7 +54,7 @@ typedef struct _NMPlatform NMPlatform;
 #define IFA_F_NOPREFIXROUTE 0x200
 #endif
 
-typedef enum {
+typedef enum { /*< skip >*/
 
 	/* dummy value, to enforce that the enum type is signed and has a size
 	 * to hold an integer. We want to encode errno from <errno.h> as negative
@@ -74,7 +74,7 @@ typedef enum {
 	NM_PLATFORM_ERROR_NO_FIRMWARE,
 } NMPlatformError;
 
-typedef enum {
+typedef enum { /*< skip >*/
 	NM_PLATFORM_REASON_NONE,
 	/* Event was requested by NetworkManager. */
 	NM_PLATFORM_REASON_INTERNAL,
@@ -141,7 +141,7 @@ struct _NMPlatformLink {
 	guint mtu;
 };
 
-typedef enum {
+typedef enum { /*< skip >*/
 	NM_PLATFORM_SIGNAL_NONE,
 	NM_PLATFORM_SIGNAL_ADDED,
 	NM_PLATFORM_SIGNAL_CHANGED,
@@ -150,7 +150,7 @@ typedef enum {
 
 #define NM_PLATFORM_LIFETIME_PERMANENT G_MAXUINT32
 
-typedef enum {
+typedef enum { /*< skip >*/
 	NM_PLATFORM_GET_ROUTE_FLAGS_NONE                            = 0,
 
 	/* Whether to include default-routes/non-default-routes. Omitting

commit 17216ed453c366e961b52dff94e413ba3080c1e3

platform: fix wrong removal of generated enums
Fixes: a130f72d745bcfef8aca228f40e8d94ce18bbaef

src/platform/nm-platform.h
@@ -74,7 +74,7 @@ typedef enum { /*< skip >*/
 	NM_PLATFORM_ERROR_NO_FIRMWARE,
 } NMPlatformError;
 
-typedef enum { /*< skip >*/
+typedef enum {
 	NM_PLATFORM_REASON_NONE,
 	/* Event was requested by NetworkManager. */
 	NM_PLATFORM_REASON_INTERNAL,
@@ -141,7 +141,7 @@ struct _NMPlatformLink {
 	guint mtu;
 };
 
-typedef enum { /*< skip >*/
+typedef enum {
 	NM_PLATFORM_SIGNAL_NONE,
 	NM_PLATFORM_SIGNAL_ADDED,
 	NM_PLATFORM_SIGNAL_CHANGED,