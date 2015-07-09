NetworkManager/NetworkManager

commit 2a5eea7e7696c00befe81b934734f34ee150768f

device: fix handling of ignore-auto-dns and ignore-auto-routes
Since commit ab6548c62134 ("device: better accept external IP
changes"), ipX_config_merge_and_apply() applies the configuration
static settings to an intermediate, initially empty configuration
(priv->con_ipX_config) instead of the composite configuration, and
thus the handling of ignore-auto-routes and ignore-auto-dns options
done in nm_ipX_config_merge_setting() has no effect.

Fix this by clearing the routes and the DNS information when needed
before merging static settings to the composite configuration.

Fixes: ab6548c62134518ba2871306397e7fb9c84260ca
(cherry picked from commit 79630c11e502c3b0b958abc0b1b5d777a3db2a98)

src/devices/nm-device.c
@@ -3216,12 +3216,24 @@ ip4_config_merge_and_apply (NMDevice *self,
 	if (priv->wwan_ip4_config)
 		nm_ip4_config_merge (composite, priv->wwan_ip4_config);
 
+	/* Apply ignore-auto-routes and ignore-auto-dns settings */
+	connection = nm_device_get_connection (self);
+	if (connection) {
+		NMSettingIPConfig *s_ip4 = nm_connection_get_setting_ip4_config (connection);
+
+		if (nm_setting_ip_config_get_ignore_auto_routes (s_ip4))
+			nm_ip4_config_reset_routes (composite);
+		if (nm_setting_ip_config_get_ignore_auto_dns (s_ip4))
+			nm_ip4_config_reset_nameservers (composite);
+			nm_ip4_config_reset_domains (composite);
+			nm_ip4_config_reset_searches (composite);
+	}
+
 	/* Merge user overrides into the composite config. For assumed connections,
 	 * con_ip4_config is empty. */
 	if (priv->con_ip4_config)
 		nm_ip4_config_merge (composite, priv->con_ip4_config);
 
-	connection = nm_device_get_connection (self);
 
 	/* Add the default route.
 	 *
@@ -3812,12 +3824,23 @@ ip6_config_merge_and_apply (NMDevice *self,
 	if (priv->wwan_ip6_config)
 		nm_ip6_config_merge (composite, priv->wwan_ip6_config);
 
+	/* Apply ignore-auto-routes and ignore-auto-dns settings */
+	connection = nm_device_get_connection (self);
+	if (connection) {
+		NMSettingIPConfig *s_ip6 = nm_connection_get_setting_ip6_config (connection);
+
+		if (nm_setting_ip_config_get_ignore_auto_routes (s_ip6))
+			nm_ip6_config_reset_routes (composite);
+		if (nm_setting_ip_config_get_ignore_auto_dns (s_ip6))
+			nm_ip6_config_reset_nameservers (composite);
+			nm_ip6_config_reset_domains (composite);
+			nm_ip6_config_reset_searches (composite);
+	}
+
 	/* Merge user overrides into the composite config. For assumed connections,
 	 * con_ip6_config is empty. */
 	if (priv->con_ip6_config)
 		nm_ip6_config_merge (composite, priv->con_ip6_config);
-
-	connection = nm_device_get_connection (self);
 
 	/* Add the default route.
 	 *

commit 0eef608cd1ab7c20b4f1e40e02da7c8aaef2cf29

device: fix regression for ignored search domains in ip._config_merge_and_apply()
Reported-by: Fabrice Bellet <fabrice@bellet.info>
Fixes: 2a5eea7e7696c00befe81b934734f34ee150768f

https://bugzilla.gnome.org/show_bug.cgi?id=752193
(cherry picked from commit d6831efc6bf37594b1674e442ca75e0e957bfecf)

src/devices/nm-device.c
@@ -3241,10 +3241,11 @@ ip4_config_merge_and_apply (NMDevice *self,
 		if (s_ip4) {
 			if (nm_setting_ip_config_get_ignore_auto_routes (s_ip4))
 				nm_ip4_config_reset_routes (composite);
-			if (nm_setting_ip_config_get_ignore_auto_dns (s_ip4))
+			if (nm_setting_ip_config_get_ignore_auto_dns (s_ip4)) {
 				nm_ip4_config_reset_nameservers (composite);
 				nm_ip4_config_reset_domains (composite);
 				nm_ip4_config_reset_searches (composite);
+			}
 		}
 	}
 
@@ -3852,10 +3853,11 @@ ip6_config_merge_and_apply (NMDevice *self,
 		if (s_ip6) {
 			if (nm_setting_ip_config_get_ignore_auto_routes (s_ip6))
 				nm_ip6_config_reset_routes (composite);
-			if (nm_setting_ip_config_get_ignore_auto_dns (s_ip6))
+			if (nm_setting_ip_config_get_ignore_auto_dns (s_ip6)) {
 				nm_ip6_config_reset_nameservers (composite);
 				nm_ip6_config_reset_domains (composite);
 				nm_ip6_config_reset_searches (composite);
+			}
 		}
 	}
 