NetworkManager/NetworkManager

commit aa12bb353bca34be1bea0625c8e6e7715f24deb3

cli: discover slave type for a connection with a master
Rename verify_master_for_slave(), since it does a lot more than just verifying
the master setting.

Make the type check optional and return the type of the connection that
matched. This makes it possible to omit setting the slave type on a command
line and still get the slave type right.

clients/cli/connections.c
@@ -3152,27 +3152,31 @@ _strip_master_prefix (const char *master, const char *(**func)(NMConnection *))
 	return master;
 }
 
-/* verify_master_for_slave:
+/* normalized_master_for_slave:
  * @connections: list af all connections
  * @master: UUID, ifname or ID of the master connection
- * @type: virtual connection type (bond, team, bridge, ...)
+ * @type: virtual connection type (bond, team, bridge, ...) or %NULL
+ * @out_type: type of the connection that matched
  *
- * Check whether master is a valid interface name, UUID or ID of some @type connection.
+ * Check whether master is a valid interface name, UUID or ID of some connection,
+ * possibly of a specified @type.
  * First UUID and ifname are checked. If they don't match, ID is checked
  * and replaced by UUID on a match.
  *
  * Returns: identifier of master connection if found, %NULL otherwise
  */
 static const char *
-verify_master_for_slave (const GPtrArray *connections,
-                         const char *master,
-                         const char *type)
+normalized_master_for_slave (const GPtrArray *connections,
+                             const char *master,
+                             const char *type,
+                             const char **out_type)
 {
 	NMConnection *connection;
 	NMSettingConnection *s_con;
-	const char *con_type, *id, *uuid, *ifname;
+	const char *con_type = NULL, *id, *uuid, *ifname;
 	int i;
 	const char *found_by_id = NULL;
+	const char *out_type_by_id = NULL;
 	const char *out_master = NULL;
 	const char *(*func) (NMConnection *) = NULL;
 
@@ -3185,11 +3189,12 @@ verify_master_for_slave (const GPtrArray *connections,
 		s_con = nm_connection_get_setting_connection (connection);
 		g_assert (s_con);
 		con_type = nm_setting_connection_get_connection_type (s_con);
-		if (g_strcmp0 (con_type, type) != 0)
+		if (type && g_strcmp0 (con_type, type) != 0)
 			continue;
 		if (func) {
 			/* There was a prefix; only compare to that type. */
 			if (g_strcmp0 (master, func (connection)) == 0) {
+				*out_type = con_type;
 				if (func == nm_connection_get_id)
 					out_master = nm_connection_get_uuid (connection);
 				else
@@ -3203,13 +3208,30 @@ verify_master_for_slave (const GPtrArray *connections,
 			if (   g_strcmp0 (master, uuid) == 0
 			    || g_strcmp0 (master, ifname) == 0) {
 				out_master = master;
+				*out_type = con_type;
 				break;
 			}
-			if (!found_by_id && g_strcmp0 (master, id) == 0)
+			if (!found_by_id && g_strcmp0 (master, id) == 0) {
+				out_type_by_id = con_type;
 				found_by_id = uuid;
+			}
 		}
 	}
-	return out_master ? out_master : found_by_id;
+
+	if (!out_master) {
+		out_master = found_by_id;
+		if (out_type)
+			*out_type = out_type_by_id;
+	}
+
+	if (!out_master) {
+		g_print (_("Warning: master='%s' doesn't refer to any existing profile.\n"), master);
+		out_master = master;
+		if (out_type)
+			*out_type = type;
+	}
+
+	return out_master;
 }
 
 static gboolean
@@ -4316,7 +4338,7 @@ complete_slave (NMSettingConnection *s_con,
 			return FALSE;
 		}
 		/* Verify master argument */
-		checked_master = verify_master_for_slave (all_connections, master, slave_type);
+		checked_master = normalized_master_for_slave (all_connections, master, slave_type, NULL);
 		if (!checked_master)
 			g_print (_("Warning: master='%s' doesn't refer to any existing profile.\n"), master);
 

commit 7b6299d8dd969c10ee0b5ec203d812cf9a6c7a56

cli: fix a crash adding a slave connection with 'nmcli -a con add'
Valid types: [generic, 802-3-ethernet (ethernet), pppoe, 802-11-wireless (wifi), wimax, gsm, cdma, infiniband, adsl, bluetooth, vpn, 802-11-olpc-mesh (olpc-mesh), vlan, bond, team, bridge, bond-slave, team-slave, bridge-slave]
Connection type: team-slave
Interface name [*]: eth5
Master: nm-team

Program received signal SIGSEGV, Segmentation fault.
0x000000000041ae6d in normalized_master_for_slave (connections=connections@entry=0x7fffec0019a0, master=<optimized out>, master@entry=0x73b470 "nm-team",
    type=type@entry=0x72f7c0 "team", out_type=out_type@entry=0x0) at connections.c:3218
3218					*out_type = con_type;

Fixes: aa12bb353bca34be1bea0625c8e6e7715f24deb3

clients/cli/connections.c
@@ -3201,7 +3201,8 @@ normalized_master_for_slave (const GPtrArray *connections,
 		if (func) {
 			/* There was a prefix; only compare to that type. */
 			if (g_strcmp0 (master, func (connection)) == 0) {
-				*out_type = con_type;
+				if (out_type)
+					*out_type = con_type;
 				if (func == nm_connection_get_id)
 					out_master = nm_connection_get_uuid (connection);
 				else
@@ -3215,7 +3216,8 @@ normalized_master_for_slave (const GPtrArray *connections,
 			if (   g_strcmp0 (master, uuid) == 0
 			    || g_strcmp0 (master, ifname) == 0) {
 				out_master = master;
-				*out_type = con_type;
+				if (out_type)
+					*out_type = con_type;
 				break;
 			}
 			if (!found_by_id && g_strcmp0 (master, id) == 0) {