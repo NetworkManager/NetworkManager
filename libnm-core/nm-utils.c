NetworkManager/NetworkManager

commit eafa6c3584421d14e09a18f76bad7b1b5ebc2288

libnm: add load method to NMVpnPluginInfo
https://bugzilla.gnome.org/show_bug.cgi?id=749877

libnm-core/nm-vpn-plugin-info.c
@@ -56,6 +56,9 @@ typedef struct {
 	 * value somewhere... let's put it in an internal hash table.
 	 * This contains a clone of all the strings in keyfile. */
 	GHashTable *keys;
+
+	gboolean editor_plugin_loaded;
+	NMVpnEditorPlugin *editor_plugin;
 } NMVpnPluginInfoPrivate;
 
 static void nm_vpn_plugin_info_initable_iface_init (GInitableIface *iface);
@@ -647,6 +650,112 @@ nm_vpn_plugin_info_lookup_property (NMVpnPluginInfo *self, const char *group, co
 /*********************************************************************/
 
 /**
+ * nm_vpn_plugin_info_get_editor_plugin:
+ * @self: plugin info instance
+ *
+ * Returns: the cached #NMVpnEditorPlugin instance.
+ *
+ * Since: 1.2
+ */
+NMVpnEditorPlugin *
+nm_vpn_plugin_info_get_editor_plugin (NMVpnPluginInfo *self)
+{
+	g_return_val_if_fail (NM_IS_VPN_PLUGIN_INFO (self), NULL);
+
+	return NM_VPN_PLUGIN_INFO_GET_PRIVATE (self)->editor_plugin;
+}
+
+/**
+ * nm_vpn_plugin_info_set_editor_plugin:
+ * @self: plugin info instance
+ * @plugin: (allow-none): plugin instance
+ *
+ * Set the internal plugin instance. If %NULL, only clear the previous instance.
+ *
+ * Since: 1.2
+ */
+void
+nm_vpn_plugin_info_set_editor_plugin (NMVpnPluginInfo *self, NMVpnEditorPlugin *plugin)
+{
+	NMVpnPluginInfoPrivate *priv;
+	NMVpnEditorPlugin *old;
+
+	g_return_if_fail (NM_IS_VPN_PLUGIN_INFO (self));
+	g_return_if_fail (!plugin || G_IS_OBJECT (plugin));
+
+	priv = NM_VPN_PLUGIN_INFO_GET_PRIVATE (self);
+
+	if (!plugin) {
+		priv->editor_plugin_loaded = FALSE;
+		g_clear_object (&priv->editor_plugin);
+	} else {
+		old = priv->editor_plugin;
+		priv->editor_plugin = g_object_ref (plugin);
+		priv->editor_plugin_loaded = TRUE;
+		if (old)
+			g_object_unref (old);
+	}
+}
+
+/**
+ * nm_vpn_plugin_info_load_editor_plugin:
+ * @self: plugin info instance
+ * @error: error reason on failure
+ *
+ * Returns: loads the plugin and returns the newly created instance.
+ *   The plugin is owned by @self and can be later retrieved again
+ *   via nm_vpn_plugin_info_get_editor_plugin(). You can load the
+ *   plugin only once, unless you reset the state via
+ *   nm_vpn_plugin_info_set_editor_plugin().
+ *
+ * Since: 1.2
+ */
+NMVpnEditorPlugin *
+nm_vpn_plugin_info_load_editor_plugin (NMVpnPluginInfo *self, GError **error)
+{
+	NMVpnPluginInfoPrivate *priv;
+	const char *plugin_filename;
+
+	g_return_val_if_fail (NM_IS_VPN_PLUGIN_INFO (self), NULL);
+
+	priv = NM_VPN_PLUGIN_INFO_GET_PRIVATE (self);
+
+	if (priv->editor_plugin)
+		return priv->editor_plugin;
+
+	plugin_filename = nm_vpn_plugin_info_get_plugin (self);
+	if (!plugin_filename || !*plugin_filename) {
+		g_set_error (error,
+		             NM_VPN_PLUGIN_ERROR,
+		             NM_VPN_PLUGIN_ERROR_FAILED,
+		             _("missing \"plugin\" setting"));
+		return NULL;
+	}
+
+	/* We only try once to load the plugin. If we previously tried and it was
+	 * unsuccessful, error out immediately. */
+	if (priv->editor_plugin_loaded) {
+		g_set_error (error,
+		             NM_VPN_PLUGIN_ERROR,
+		             NM_VPN_PLUGIN_ERROR_FAILED,
+		             _("%s: don't retry loading plugin which already failed previously"), priv->name);
+		return NULL;
+	}
+
+	priv->editor_plugin_loaded = TRUE;
+	priv->editor_plugin = nm_vpn_editor_plugin_load_from_file (plugin_filename,
+	                                                           priv->name,
+	                                                           nm_vpn_plugin_info_get_service (self),
+	                                                           getuid (),
+	                                                           NULL,
+	                                                           NULL,
+	                                                           error);
+	return priv->editor_plugin;
+}
+
+/*********************************************************************/
+
+/**
  * nm_vpn_plugin_info_new_from_file:
  * @filename: filename to read.
  * @error: on failure, the error reason.
@@ -809,6 +918,17 @@ get_property (GObject *object, guint prop_id,
 }
 
 static void
+dispose (GObject *object)
+{
+	NMVpnPluginInfo *self = NM_VPN_PLUGIN_INFO (object);
+	NMVpnPluginInfoPrivate *priv = NM_VPN_PLUGIN_INFO_GET_PRIVATE (self);
+
+	g_clear_object (&priv->editor_plugin);
+
+	G_OBJECT_CLASS (nm_vpn_plugin_info_parent_class)->dispose (object);
+}
+
+static void
 finalize (GObject *object)
 {
 	NMVpnPluginInfo *self = NM_VPN_PLUGIN_INFO (object);
@@ -833,6 +953,7 @@ nm_vpn_plugin_info_class_init (NMVpnPluginInfoClass *plugin_class)
 	/* virtual methods */
 	object_class->set_property = set_property;
 	object_class->get_property = get_property;
+	object_class->dispose      = dispose;
 	object_class->finalize     = finalize;
 
 	/* properties */

libnm-core/nm-vpn-plugin-info.h
@@ -25,6 +25,7 @@
 #include <glib-object.h>
 
 #include "nm-utils.h"
+#include "nm-vpn-editor-plugin.h"
 
 G_BEGIN_DECLS
 
@@ -97,6 +98,16 @@ NM_AVAILABLE_IN_1_2
 NMVpnPluginInfo *nm_vpn_plugin_info_list_find_by_filename (GSList *list, const char *filename);
 NM_AVAILABLE_IN_1_2
 NMVpnPluginInfo *nm_vpn_plugin_info_list_find_by_service  (GSList *list, const char *service);
+
+
+NM_AVAILABLE_IN_1_2
+NMVpnEditorPlugin *nm_vpn_plugin_info_get_editor_plugin  (NMVpnPluginInfo *plugin_info);
+NM_AVAILABLE_IN_1_2
+void               nm_vpn_plugin_info_set_editor_plugin  (NMVpnPluginInfo *self,
+                                                          NMVpnEditorPlugin *plugin);
+NM_AVAILABLE_IN_1_2
+NMVpnEditorPlugin *nm_vpn_plugin_info_load_editor_plugin (NMVpnPluginInfo *plugin_info,
+                                                          GError **error);
 
 G_END_DECLS
 

libnm/libnm.ver
@@ -884,14 +884,17 @@ global:
 	nm_utils_enum_to_str;
 	nm_vpn_editor_plugin_load_from_file;
 	nm_vpn_plugin_info_get_filename;
+	nm_vpn_plugin_info_get_editor_plugin;
 	nm_vpn_plugin_info_get_name;
 	nm_vpn_plugin_info_get_plugin;
 	nm_vpn_plugin_info_get_program;
 	nm_vpn_plugin_info_get_service;
 	nm_vpn_plugin_info_get_type;
+	nm_vpn_plugin_info_load_editor_plugin;
 	nm_vpn_plugin_info_lookup_property;
 	nm_vpn_plugin_info_new_from_file;
 	nm_vpn_plugin_info_new_with_data;
+	nm_vpn_plugin_info_set_editor_plugin;
 	nm_vpn_plugin_info_validate_filename;
 	nm_vpn_plugin_info_list_add;
 	nm_vpn_plugin_info_list_find_by_filename;

commit 3a7db0dd9e59d948beda82a395d9756a242c5143

introspection: fix some annotations
Fixes: eafa6c3584421d14e09a18f76bad7b1b5ebc2288
Fixes: eed0d0c58f7f13638eb587e240737048d729cb68
Fixes: b5cc017ba473de011881e40b26b3ee3433f1567c
Fixes: bce040daa2399688b437ac5609339f3d3e1b17b8

libnm-core/nm-utils.c
@@ -3997,8 +3997,8 @@ char *nm_utils_enum_to_str (GType type, int value)
  * nm_utils_enum_from_str
  * @type: the %GType of the enum
  * @str: the input string
- * @out_value: (out) (allow-none) the output value
- * @err_token: (out) (allow-none) location to store the first unrecognized token
+ * @out_value: (out) (allow-none): the output value
+ * @err_token: (out) (allow-none): location to store the first unrecognized token
  *
  * Converts a string to the matching enum value.
  *

libnm-core/nm-vpn-editor-plugin.c
@@ -91,7 +91,7 @@ nm_vpn_editor_plugin_default_init (NMVpnEditorPluginInterface *iface)
  * @check_owner: if non-negative, check whether the file is owned
  *  by UID @check_owner or by root. In this case also check that
  *  the file is not writable by anybody else.
- * @check_file: optional callback to validate the file prior to
+ * @check_file: (scope call): optional callback to validate the file prior to
  *   loading the shared library.
  * @user_data: user data for @check_file
  * @error: on failure the error reason.
@@ -100,7 +100,7 @@ nm_vpn_editor_plugin_default_init (NMVpnEditorPluginInterface *iface)
  * #NMVpnEditorPlugin instace via the #NMVpnEditorPluginFactory
  * function.
  *
- * Returns: (transfer-full): a new plugin instance or %NULL on error.
+ * Returns: (transfer full): a new plugin instance or %NULL on error.
  *
  * Since: 1.2
  */

libnm-core/nm-vpn-plugin-info.c
@@ -194,7 +194,7 @@ _sort_files (LoadDirInfo *a, LoadDirInfo *b)
 /**
  * _nm_vpn_plugin_info_get_default_dir_etc:
  *
- * Returns: (transfer-none): compile time constant of the default
+ * Returns: (transfer none): compile time constant of the default
  *   VPN plugin directory.
  */
 const char *
@@ -206,7 +206,7 @@ _nm_vpn_plugin_info_get_default_dir_etc ()
 /**
  * _nm_vpn_plugin_info_get_default_dir_lib:
  *
- * Returns: (transfer-none): compile time constant of the default
+ * Returns: (transfer none): compile time constant of the default
  *   VPN plugin directory.
  */
 const char *
@@ -241,7 +241,7 @@ _nm_vpn_plugin_info_get_default_dir_user ()
  *
  * Iterate over the content of @dirname and load name files.
  *
- * Returns: (transfer-full): list of loaded plugin infos.
+ * Returns: (transfer full) (element-type NMVpnPluginInfo): list of loaded plugin infos.
  */
 GSList *
 _nm_vpn_plugin_info_list_load_dir (const char *dirname,
@@ -303,7 +303,7 @@ _nm_vpn_plugin_info_list_load_dir (const char *dirname,
 /**
  * nm_vpn_plugin_info_list_load:
  *
- * Returns: (tranfer-full): list of plugins loaded from the default
+ * Returns: (tranfer full): list of plugins loaded from the default
  * directories rejecting duplicates.
  *
  * Since: 1.2
@@ -469,7 +469,7 @@ nm_vpn_plugin_info_list_remove (GSList **list, NMVpnPluginInfo *plugin_info)
  * @list: list of plugins
  * @name: name to search
  *
- * Returns: the first plugin with a matching @name (or %NULL).
+ * Returns: (transfer none): the first plugin with a matching @name (or %NULL).
  *
  * Since: 1.2
  */
@@ -493,7 +493,7 @@ nm_vpn_plugin_info_list_find_by_name (GSList *list, const char *name)
  * @list: list of plugins
  * @filename: filename to search
  *
- * Returns: the first plugin with a matching @filename (or %NULL).
+ * Returns: (transfer none): the first plugin with a matching @filename (or %NULL).
  *
  * Since: 1.2
  */
@@ -517,7 +517,7 @@ nm_vpn_plugin_info_list_find_by_filename (GSList *list, const char *filename)
  * @list: list of plugins
  * @service: service to search
  *
- * Returns: the first plugin with a matching @service (or %NULL).
+ * Returns: (transfer none): the first plugin with a matching @service (or %NULL).
  *
  * Since: 1.2
  */
@@ -542,7 +542,7 @@ nm_vpn_plugin_info_list_find_by_service (GSList *list, const char *service)
  * nm_vpn_plugin_info_get_filename:
  * @self: plugin info instance
  *
- * Returns: (transfer-none): the filename. Can be %NULL.
+ * Returns: (transfer none): the filename. Can be %NULL.
  *
  * Since: 1.2
  */
@@ -558,7 +558,7 @@ nm_vpn_plugin_info_get_filename (NMVpnPluginInfo *self)
  * nm_vpn_plugin_info_get_name:
  * @self: plugin info instance
  *
- * Returns: (transfer-none): the name. Cannot be %NULL.
+ * Returns: (transfer none): the name. Cannot be %NULL.
  *
  * Since: 1.2
  */
@@ -574,7 +574,7 @@ nm_vpn_plugin_info_get_name (NMVpnPluginInfo *self)
  * nm_vpn_plugin_info_get_service:
  * @self: plugin info instance
  *
- * Returns: (transfer-none): the service. Cannot be %NULL.
+ * Returns: (transfer none): the service. Cannot be %NULL.
  *
  * Since: 1.2
  */
@@ -590,7 +590,7 @@ nm_vpn_plugin_info_get_service (NMVpnPluginInfo *self)
  * nm_vpn_plugin_info_get_plugin:
  * @self: plugin info instance
  *
- * Returns: (transfer-none): the plugin. Can be %NULL.
+ * Returns: (transfer none): the plugin. Can be %NULL.
  *
  * Since: 1.2
  */
@@ -607,7 +607,7 @@ nm_vpn_plugin_info_get_plugin (NMVpnPluginInfo *self)
  * nm_vpn_plugin_info_get_program:
  * @self: plugin info instance
  *
- * Returns: (transfer-none): the program. Can be %NULL.
+ * Returns: (transfer none): the program. Can be %NULL.
  *
  * Since: 1.2
  */
@@ -626,7 +626,7 @@ nm_vpn_plugin_info_get_program (NMVpnPluginInfo *self)
  * @group: group name
  * @key: name of the property
  *
- * Returns: (transfer-none): #NMVpnPluginInfo is internally a #GKeyFile. Returns the matching
+ * Returns: (transfer none): #NMVpnPluginInfo is internally a #GKeyFile. Returns the matching
  * property.
  *
  * Since: 1.2
@@ -653,7 +653,7 @@ nm_vpn_plugin_info_lookup_property (NMVpnPluginInfo *self, const char *group, co
  * nm_vpn_plugin_info_get_editor_plugin:
  * @self: plugin info instance
  *
- * Returns: the cached #NMVpnEditorPlugin instance.
+ * Returns: (transfer none): the cached #NMVpnEditorPlugin instance.
  *
  * Since: 1.2
  */
@@ -702,8 +702,8 @@ nm_vpn_plugin_info_set_editor_plugin (NMVpnPluginInfo *self, NMVpnEditorPlugin *
  * @self: plugin info instance
  * @error: error reason on failure
  *
- * Returns: loads the plugin and returns the newly created instance.
- *   The plugin is owned by @self and can be later retrieved again
+ * Returns: (transfer none): loads the plugin and returns the newly created
+ *   instance. The plugin is owned by @self and can be later retrieved again
  *   via nm_vpn_plugin_info_get_editor_plugin(). You can load the
  *   plugin only once, unless you reset the state via
  *   nm_vpn_plugin_info_set_editor_plugin().

libnm-core/nm-vpn-plugin-info.h
@@ -101,12 +101,12 @@ NMVpnPluginInfo *nm_vpn_plugin_info_list_find_by_service  (GSList *list, const c
 
 
 NM_AVAILABLE_IN_1_2
-NMVpnEditorPlugin *nm_vpn_plugin_info_get_editor_plugin  (NMVpnPluginInfo *plugin_info);
+NMVpnEditorPlugin *nm_vpn_plugin_info_get_editor_plugin  (NMVpnPluginInfo *self);
 NM_AVAILABLE_IN_1_2
 void               nm_vpn_plugin_info_set_editor_plugin  (NMVpnPluginInfo *self,
                                                           NMVpnEditorPlugin *plugin);
 NM_AVAILABLE_IN_1_2
-NMVpnEditorPlugin *nm_vpn_plugin_info_load_editor_plugin (NMVpnPluginInfo *plugin_info,
+NMVpnEditorPlugin *nm_vpn_plugin_info_load_editor_plugin (NMVpnPluginInfo *self,
                                                           GError **error);
 
 G_END_DECLS