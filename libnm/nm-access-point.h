b503b02caccb6f3252d014a475a5c98bb9a9f9da
Thomas Haller <thaller@redhat.com>
2015-08-09 12:24:22 +0200

    libnm: add NM_AVAILABLE_IN_1_2 for API nm_access_point_get_last_seen()
    Fixes: 1e85c3584670c167b36b784f3504f190047b84f8

--- libnm/nm-access-point.h
+++ libnm/nm-access-point.h
@@ -74,6 +74,7 @@ guint32                nm_access_point_get_frequency    (NMAccessPoint *ap);
 NM80211Mode            nm_access_point_get_mode         (NMAccessPoint *ap);
 guint32                nm_access_point_get_max_bitrate  (NMAccessPoint *ap);
 guint8                 nm_access_point_get_strength     (NMAccessPoint *ap);
+NM_AVAILABLE_IN_1_2
 gint                   nm_access_point_get_last_seen    (NMAccessPoint *ap);
 
 GPtrArray *            nm_access_point_filter_connections (NMAccessPoint *ap,
