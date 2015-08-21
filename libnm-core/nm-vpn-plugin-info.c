6c213e3cb4ed080fa94929e599e79e109625be27
Lubomir Rintel <lkundrak@v3.sk>
2015-08-21 16:14:27 +0200

    libnm-core/vpn-plugin-info: add nm_vpn_plugin_info_supports_multiple()

--- libnm-core/nm-vpn-plugin-info.c
+++ libnm-core/nm-vpn-plugin-info.c
@@ -620,6 +620,26 @@ nm_vpn_plugin_info_get_program (NMVpnPluginInfo *self)
 }
 
 /**
+ * nm_vpn_plugin_info_supports_multiple:
+ * @self: plugin info instance
+ *
+ * Returns: %TRUE if the service supports multiple instances with different bus names, otherwise %FALSE
+ *
+ * Since: 1.2
+ */
+gboolean
+nm_vpn_plugin_info_supports_multiple (NMVpnPluginInfo *self)
+{
+	g_return_val_if_fail (NM_IS_VPN_PLUGIN_INFO (self), FALSE);
+
+	return g_key_file_get_boolean (NM_VPN_PLUGIN_INFO_GET_PRIVATE (self)->keyfile,
+	                               NM_VPN_PLUGIN_INFO_KF_GROUP_CONNECTION,
+	                               "supports-multiple-connections",
+	                               NULL);
+}
+
+
+/**
  * nm_vpn_plugin_info_lookup_property:
  * @self: plugin info instance
  * @group: group name
--- libnm-core/nm-vpn-plugin-info.h
+++ libnm-core/nm-vpn-plugin-info.h
@@ -81,6 +81,8 @@ const char *nm_vpn_plugin_info_get_plugin      (NMVpnPluginInfo *self);
 NM_AVAILABLE_IN_1_2
 const char *nm_vpn_plugin_info_get_program     (NMVpnPluginInfo *self);
 NM_AVAILABLE_IN_1_2
+gboolean nm_vpn_plugin_info_supports_multiple  (NMVpnPluginInfo *self);
+NM_AVAILABLE_IN_1_2
 const char *nm_vpn_plugin_info_lookup_property (NMVpnPluginInfo *self, const char *group, const char *key);
 
 NM_AVAILABLE_IN_1_2
