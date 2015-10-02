26a2b38938bb01b4df90992f30faa3fed398c1f1
Thomas Haller <thaller@redhat.com>
2015-10-02 20:24:30 +0200

    device: reorder unmanaged-flags
    A later commit will rename several flags. So reorder them now to
    make the following diff smaller (and easier to understand).

--- src/devices/nm-device.h
+++ src/devices/nm-device.h
@@ -444,25 +444,27 @@ RfKillType nm_device_get_rfkill_type (NMDevice *device);
 /**
  * NMUnmanagedFlags:
  * @NM_UNMANAGED_NONE: placeholder value
- * @NM_UNMANAGED_DEFAULT: %TRUE when unmanaged by default (ie, Generic devices)
  * @NM_UNMANAGED_INTERNAL: %TRUE when unmanaged by internal decision (ie,
  *   because NM is sleeping or not managed for some other reason)
- * @NM_UNMANAGED_USER: %TRUE when unmanaged by user decision (via unmanaged-specs)
  * @NM_UNMANAGED_PARENT: %TRUE when unmanaged due to parent device being unmanaged
- * @NM_UNMANAGED_EXTERNAL_DOWN: %TRUE when unmanaged because !IFF_UP and not created by NM
+ * @NM_UNMANAGED_LOOPBACK: %TRUE for unmanaging loopback device
  * @NM_UNMANAGED_PLATFORM_INIT: %TRUE when unmanaged because platform link not
  *   yet initialized
- * @NM_UNMANAGED_LOOPBACK: %TRUE for unmanaging loopback device
+ * @NM_UNMANAGED_USER: %TRUE when unmanaged by user decision (via unmanaged-specs)
+ * @NM_UNMANAGED_DEFAULT: %TRUE when unmanaged by default (ie, Generic devices)
+ * @NM_UNMANAGED_EXTERNAL_DOWN: %TRUE when unmanaged because !IFF_UP and not created by NM
  */
-typedef enum {
+typedef enum { /*< skip >*/
 	NM_UNMANAGED_NONE          = 0,
-	NM_UNMANAGED_DEFAULT       = (1LL <<  0),
-	NM_UNMANAGED_INTERNAL      = (1LL <<  1),
-	NM_UNMANAGED_USER          = (1LL <<  2),
-	NM_UNMANAGED_PARENT        = (1LL <<  3),
-	NM_UNMANAGED_EXTERNAL_DOWN = (1LL <<  4),
-	NM_UNMANAGED_PLATFORM_INIT = (1LL <<  5),
-	NM_UNMANAGED_LOOPBACK      = (1LL <<  6),
+
+	NM_UNMANAGED_INTERNAL      = (1LL <<  0),
+	NM_UNMANAGED_PARENT        = (1LL <<  1),
+	NM_UNMANAGED_LOOPBACK      = (1LL <<  2),
+	NM_UNMANAGED_PLATFORM_INIT = (1LL <<  3),
+	NM_UNMANAGED_USER          = (1LL <<  4),
+
+	NM_UNMANAGED_DEFAULT       = (1LL <<  8),
+	NM_UNMANAGED_EXTERNAL_DOWN = (1LL << 11),
 
 	/* Boundary value */
 	__NM_UNMANAGED_LAST,
