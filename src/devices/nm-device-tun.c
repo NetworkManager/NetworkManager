NetworkManager/NetworkManager

commit 4dbaac4ba24ebc8b257fffe5197cc8e362804a58

core: create devices first and realize them later
Unrealized devices aren't backed by kernel resources and so won't know
all of their attributes.  That means three things:

1) they must update their attributes when they become realized
2) they must clear those attributes when unrealized
3) they must be looser in checking compatible connections until
they are realized

This requires that the setup() function be split into two parts, start & finish,
because finish must be run after add_device()

Also, we can simplify whether to pay attention to 'recheck-assume', which
is now dependent on priv->is_nm_owned, because the only case where NM should
*not* listen for the 'recheck-assume' signal is when the device is a
software device created by NM itself.  That logic was previously spread
across the callers of add_device() but is now consolidated into
nm-manager.c::device_realized() and nm-device.c::nm_device_create_and_realize().

src/devices/nm-device-tun.c
@@ -226,6 +226,13 @@ realize (NMDevice *device, NMPlatformLink *plink, GError **error)
 	return TRUE;
 }
 
+static void
+setup_start (NMDevice *device, NMPlatformLink *plink)
+{
+	NM_DEVICE_CLASS (nm_device_tun_parent_class)->setup_start (device, plink);
+	reload_tun_properties (device);
+}
+
 static gboolean
 check_connection_compatible (NMDevice *device, NMConnection *connection)
 {
@@ -235,8 +242,6 @@ check_connection_compatible (NMDevice *device, NMConnection *connection)
 	NMSettingTun *s_tun;
 	gint64 user, group;
 
-	reload_tun_properties (self);
-
 	if (!NM_DEVICE_CLASS (nm_device_tun_parent_class)->check_connection_compatible (device, connection))
 		return FALSE;
 
@@ -244,23 +249,25 @@ check_connection_compatible (NMDevice *device, NMConnection *connection)
 	if (!s_tun)
 		return FALSE;
 
-	mode = tun_mode_from_string (priv->mode);
-	if (mode != nm_setting_tun_get_mode (s_tun))
-		return FALSE;
+	if (nm_device_is_real (device)) {
+		mode = tun_mode_from_string (priv->mode);
+		if (mode != nm_setting_tun_get_mode (s_tun))
+			return FALSE;
 
-	user = _nm_utils_ascii_str_to_int64 (nm_setting_tun_get_owner (s_tun), 10, 0, G_MAXINT32, -1);
-	group = _nm_utils_ascii_str_to_int64 (nm_setting_tun_get_group (s_tun), 10, 0, G_MAXINT32, -1);
+		user = _nm_utils_ascii_str_to_int64 (nm_setting_tun_get_owner (s_tun), 10, 0, G_MAXINT32, -1);
+		group = _nm_utils_ascii_str_to_int64 (nm_setting_tun_get_group (s_tun), 10, 0, G_MAXINT32, -1);
 
-	if (user != priv->props.owner)
-		return FALSE;
-	if (group != priv->props.group)
-		return FALSE;
-	if (nm_setting_tun_get_pi (s_tun) == priv->props.no_pi)
-		return FALSE;
-	if (nm_setting_tun_get_vnet_hdr (s_tun) != priv->props.vnet_hdr)
-		return FALSE;
-	if (nm_setting_tun_get_multi_queue (s_tun) != priv->props.multi_queue)
-		return FALSE;
+		if (user != priv->props.owner)
+			return FALSE;
+		if (group != priv->props.group)
+			return FALSE;
+		if (nm_setting_tun_get_pi (s_tun) == priv->props.no_pi)
+			return FALSE;
+		if (nm_setting_tun_get_vnet_hdr (s_tun) != priv->props.vnet_hdr)
+			return FALSE;
+		if (nm_setting_tun_get_multi_queue (s_tun) != priv->props.multi_queue)
+			return FALSE;
+	}
 
 	return TRUE;
 }
@@ -367,6 +374,7 @@ nm_device_tun_class_init (NMDeviceTunClass *klass)
 	device_class->check_connection_compatible = check_connection_compatible;
 	device_class->create_and_realize = create_and_realize;
 	device_class->realize = realize;
+	device_class->setup_start = setup_start;
 	device_class->unrealize = unrealize;
 	device_class->update_connection = update_connection;
 

src/devices/nm-device-bridge.c
@@ -97,7 +97,7 @@ check_connection_compatible (NMDevice *device, NMConnection *connection)
 		return FALSE;
 
 	mac_address = nm_setting_bridge_get_mac_address (s_bridge);
-	if (mac_address) {
+	if (mac_address && nm_device_is_real (device)) {
 		const char *hw_addr;
 
 		hw_addr = nm_device_get_hw_address (device);

src/devices/nm-device-ethernet.c
@@ -305,9 +305,9 @@ nm_device_ethernet_init (NMDeviceEthernet *self)
 }
 
 static void
-setup (NMDevice *device, NMPlatformLink *plink)
+setup_start (NMDevice *device, NMPlatformLink *plink)
 {
-	NM_DEVICE_CLASS (nm_device_ethernet_parent_class)->setup (device, plink);
+	NM_DEVICE_CLASS (nm_device_ethernet_parent_class)->setup_start (device, plink);
 
 	g_object_notify (G_OBJECT (device), NM_DEVICE_ETHERNET_PERMANENT_HW_ADDRESS);
 }
@@ -1716,7 +1716,7 @@ nm_device_ethernet_class_init (NMDeviceEthernetClass *klass)
 	object_class->set_property = set_property;
 
 	parent_class->get_generic_capabilities = get_generic_capabilities;
-	parent_class->setup = setup;
+	parent_class->setup_start = setup_start;
 	parent_class->check_connection_compatible = check_connection_compatible;
 	parent_class->complete_connection = complete_connection;
 	parent_class->new_default_connection = new_default_connection;

src/devices/nm-device-generic.c
@@ -61,13 +61,13 @@ get_type_description (NMDevice *device)
 }
 
 static void
-setup (NMDevice *device, NMPlatformLink *plink)
+setup_start (NMDevice *device, NMPlatformLink *plink)
 {
 	NMDeviceGeneric *self = NM_DEVICE_GENERIC (device);
 	NMDeviceGenericPrivate *priv = NM_DEVICE_GENERIC_GET_PRIVATE (self);
 	int ifindex;
 
-	NM_DEVICE_CLASS (nm_device_generic_parent_class)->setup (device, plink);
+	NM_DEVICE_CLASS (nm_device_generic_parent_class)->setup_start (device, plink);
 
 	g_clear_pointer (&priv->type_description, g_free);
 	ifindex = nm_device_get_ip_ifindex (NM_DEVICE (self));
@@ -203,7 +203,7 @@ nm_device_generic_class_init (NMDeviceGenericClass *klass)
 	object_class->get_property = get_property;
 	object_class->set_property = set_property;
 
-	parent_class->setup = setup;
+	parent_class->setup_start = setup_start;
 	parent_class->get_generic_capabilities = get_generic_capabilities;
 	parent_class->get_type_description = get_type_description;
 	parent_class->check_connection_compatible = check_connection_compatible;

src/devices/nm-device-infiniband.c
@@ -149,7 +149,7 @@ check_connection_compatible (NMDevice *device, NMConnection *connection)
 	if (!s_infiniband)
 		return FALSE;
 
-	if (s_infiniband) {
+	if (nm_device_is_real (device)) {
 		const char *mac;
 
 		mac = nm_setting_infiniband_get_mac_address (s_infiniband);

src/devices/nm-device-ip-tunnel.c
@@ -503,41 +503,43 @@ check_connection_compatible (NMDevice *device, NMConnection *connection)
 	if (!s_ip_tunnel)
 		return FALSE;
 
-	/* Check parent interface; could be an interface name or a UUID */
-	parent = nm_setting_ip_tunnel_get_parent (s_ip_tunnel);
-	if (parent) {
-		if (!match_parent (priv->parent, parent))
-			return FALSE;
-	}
-
 	if (nm_setting_ip_tunnel_get_mode (s_ip_tunnel) != priv->mode)
 		return FALSE;
 
-	if (!address_equal_pp (priv->addr_family,
-	                       nm_setting_ip_tunnel_get_local (s_ip_tunnel),
-	                       priv->local))
-		return FALSE;
+	if (nm_device_is_real (device)) {
+		/* Check parent interface; could be an interface name or a UUID */
+		parent = nm_setting_ip_tunnel_get_parent (s_ip_tunnel);
+		if (parent) {
+			if (!match_parent (priv->parent, parent))
+				return FALSE;
+		}
 
-	if (!address_equal_pp (priv->addr_family,
-	                       nm_setting_ip_tunnel_get_remote (s_ip_tunnel),
-	                       priv->remote))
-		return FALSE;
-
-	if (nm_setting_ip_tunnel_get_ttl (s_ip_tunnel) != priv->ttl)
-		return FALSE;
-
-	if (nm_setting_ip_tunnel_get_tos (s_ip_tunnel) != priv->tos)
-		return FALSE;
-
-	if (priv->addr_family == AF_INET) {
-		if (nm_setting_ip_tunnel_get_path_mtu_discovery (s_ip_tunnel) != priv->path_mtu_discovery)
-			return FALSE;
-	} else {
-		if (nm_setting_ip_tunnel_get_encapsulation_limit (s_ip_tunnel) != priv->encap_limit)
+		if (!address_equal_pp (priv->addr_family,
+		                       nm_setting_ip_tunnel_get_local (s_ip_tunnel),
+		                       priv->local))
 			return FALSE;
 
-		if (nm_setting_ip_tunnel_get_flow_label (s_ip_tunnel) != priv->flow_label)
+		if (!address_equal_pp (priv->addr_family,
+		                       nm_setting_ip_tunnel_get_remote (s_ip_tunnel),
+		                       priv->remote))
 			return FALSE;
+
+		if (nm_setting_ip_tunnel_get_ttl (s_ip_tunnel) != priv->ttl)
+			return FALSE;
+
+		if (nm_setting_ip_tunnel_get_tos (s_ip_tunnel) != priv->tos)
+			return FALSE;
+
+		if (priv->addr_family == AF_INET) {
+			if (nm_setting_ip_tunnel_get_path_mtu_discovery (s_ip_tunnel) != priv->path_mtu_discovery)
+				return FALSE;
+		} else {
+			if (nm_setting_ip_tunnel_get_encapsulation_limit (s_ip_tunnel) != priv->encap_limit)
+				return FALSE;
+
+			if (nm_setting_ip_tunnel_get_flow_label (s_ip_tunnel) != priv->flow_label)
+				return FALSE;
+		}
 	}
 
 	return TRUE;
@@ -751,9 +753,9 @@ create_and_realize (NMDevice *device,
 }
 
 static void
-setup (NMDevice *device, NMPlatformLink *plink)
+setup_start (NMDevice *device, NMPlatformLink *plink)
 {
-	NM_DEVICE_CLASS (nm_device_ip_tunnel_parent_class)->setup (device, plink);
+	NM_DEVICE_CLASS (nm_device_ip_tunnel_parent_class)->setup_start (device, plink);
 
 	update_properties (device);
 }
@@ -847,7 +849,7 @@ nm_device_ip_tunnel_class_init (NMDeviceIPTunnelClass *klass)
 	device_class->check_connection_compatible = check_connection_compatible;
 	device_class->create_and_realize = create_and_realize;
 	device_class->realize = realize;
-	device_class->setup = setup;
+	device_class->setup_start = setup_start;
 	device_class->unrealize = unrealize;
 
 	device_class->connection_type = NM_SETTING_IP_TUNNEL_SETTING_NAME;

src/devices/nm-device-vlan.c
@@ -148,12 +148,12 @@ nm_device_vlan_set_parent (NMDeviceVlan *self, NMDevice *parent)
 }
 
 static void
-setup (NMDevice *device, NMPlatformLink *plink)
+setup_start (NMDevice *device, NMPlatformLink *plink)
 {
 	NMDeviceVlan *self = NM_DEVICE_VLAN (device);
 	NMDeviceVlanPrivate *priv = NM_DEVICE_VLAN_GET_PRIVATE (self);
 
-	NM_DEVICE_CLASS (nm_device_vlan_parent_class)->setup (device, plink);
+	NM_DEVICE_CLASS (nm_device_vlan_parent_class)->setup_start (device, plink);
 
 	_LOGI (LOGD_HW | LOGD_VLAN, "VLAN ID %d with parent %s",
 	       priv->vlan_id, nm_device_get_iface (priv->parent));
@@ -311,6 +311,9 @@ notify_new_device_added (NMDevice *device, NMDevice *new_device)
 	if (priv->parent)
 		return;
 
+	if (!nm_device_is_real (device))
+		return;
+
 	plnk = nm_platform_link_get_lnk_vlan (NM_PLATFORM_GET, nm_device_get_ifindex (device), &plink);
 	if (!plnk) {
 		_LOGW (LOGD_VLAN, "failed to get VLAN interface info while checking added component.");
@@ -397,18 +400,21 @@ check_connection_compatible (NMDevice *device, NMConnection *connection)
 	if (!s_vlan)
 		return FALSE;
 
-	if (nm_setting_vlan_get_id (s_vlan) != priv->vlan_id)
-		return FALSE;
+	/* Before the device is realized some properties will not be set */
+	if (nm_device_is_real (device)) {
+		if (nm_setting_vlan_get_id (s_vlan) != priv->vlan_id)
+			return FALSE;
 
-	/* Check parent interface; could be an interface name or a UUID */
-	parent = nm_setting_vlan_get_parent (s_vlan);
-	if (parent) {
-		if (!match_parent (NM_DEVICE_VLAN (device), parent))
-			return FALSE;
-	} else {
-		/* Parent could be a MAC address in an NMSettingWired */
-		if (!match_hwaddr (device, connection, TRUE))
-			return FALSE;
+		/* Check parent interface; could be an interface name or a UUID */
+		parent = nm_setting_vlan_get_parent (s_vlan);
+		if (parent) {
+			if (!match_parent (NM_DEVICE_VLAN (device), parent))
+				return FALSE;
+		} else {
+			/* Parent could be a MAC address in an NMSettingWired */
+			if (!match_hwaddr (device, connection, TRUE))
+				return FALSE;
+		}
 	}
 
 	/* Ensure the interface name matches.  If not specified we assume a match
@@ -672,7 +678,7 @@ nm_device_vlan_class_init (NMDeviceVlanClass *klass)
 
 	parent_class->create_and_realize = create_and_realize;
 	parent_class->realize = realize;
-	parent_class->setup = setup;
+	parent_class->setup_start = setup_start;
 	parent_class->unrealize = unrealize;
 	parent_class->get_generic_capabilities = get_generic_capabilities;
 	parent_class->bring_up = bring_up;

src/devices/nm-device-vxlan.c
@@ -133,11 +133,11 @@ link_changed (NMDevice *device, NMPlatformLink *info)
 }
 
 static void
-setup (NMDevice *device, NMPlatformLink *plink)
+setup_start (NMDevice *device, NMPlatformLink *plink)
 {
 	g_assert (plink->type == NM_LINK_TYPE_VXLAN);
 
-	NM_DEVICE_CLASS (nm_device_vxlan_parent_class)->setup (device, plink);
+	NM_DEVICE_CLASS (nm_device_vxlan_parent_class)->setup_start (device, plink);
 
 	update_properties (device);
 }
@@ -247,7 +247,7 @@ nm_device_vxlan_class_init (NMDeviceVxlanClass *klass)
 	object_class->get_property = get_property;
 
 	device_class->link_changed = link_changed;
-	device_class->setup = setup;
+	device_class->setup_start = setup_start;
 	device_class->unrealize = unrealize;
 
 	/* properties */

commit 72c36bd6db9652fce8d037d671cb5c62afad2ac2

device/tun: fix reloading tun properties
Before the device is setup, we call nm_platform_tun_get_properties() without
a valid ifindex. That triggered an assertion [1].

Thereby, change nm_platform_tun_get_properties() to effectively clear
the tun properties when we are unable to fetch them. Also, never modify
the tun-mode of NMDeviceTun.

[1]
    #0  0x00007f0a4173e81b in g_logv (breakpoint=1) at gmessages.c:324
    #1  0x00007f0a4173e81b in g_logv (log_domain=0x561e9264ccf6 "NetworkManager", log_level=G_LOG_LEVEL_CRITICAL, format=<optimized out>, args=args@entry=0x7ffd71a0d4d0) at gmessages.c:1081
    #2  0x00007f0a4173e98f in g_log (log_domain=<optimized out>, log_level=<optimized out>, format=<optimized out>) at gmessages.c:1119
    #3  0x0000561e9241ecec in nm_platform_tun_get_properties (self=0x561e9354ba70 [NMLinuxPlatform], ifindex=0, props=0x7ffd71a0d650) at platform/nm-platform.c:2081
    #4  0x0000561e923aad9c in reload_tun_properties (self=0x561e937fb080 [NMDeviceTun]) at devices/nm-device-tun.c:68
    #5  0x0000561e923aa795 in realize (device=0x561e937fb080 [NMDeviceTun], plink=0x7ffd71a0d818, error=0x7ffd71a0d798) at devices/nm-device-tun.c:225
    #6  0x0000561e923bdc06 in nm_device_realize (self=0x561e937fb080 [NMDeviceTun], plink=0x7ffd71a0d818, out_compatible=0x7ffd71a0d77c, error=0x7ffd71a0d798) at devices/nm-device.c:1713
    #7  0x0000561e924ad995 in platform_link_added (self=0x561e9356e230 [NMManager], ifindex=33, plink=0x7ffd71a0d818) at nm-manager.c:1947
    #8  0x0000561e924ad717 in _platform_link_cb_idle (data=0x561e937eb940) at nm-manager.c:2029
    #9  0x00007f0a41737e3a in g_main_context_dispatch (context=0x561e93547530) at gmain.c:3154
    #10 0x00007f0a41737e3a in g_main_context_dispatch (context=context@entry=0x561e93547530) at gmain.c:3769
    #11 0x00007f0a417381d0 in g_main_context_iterate (context=0x561e93547530, block=block@entry=1, dispatch=dispatch@entry=1, self=<optimized out>) at gmain.c:3840
    #12 0x00007f0a417384f2 in g_main_loop_run (loop=0x561e935475f0) at gmain.c:4034
    #13 0x0000561e923ba3f3 in main (argc=1, argv=0x7ffd71a0dc68) at main.c:488

Fixes: 4dbaac4ba24ebc8b257fffe5197cc8e362804a58

src/devices/nm-device-tun.c
@@ -64,11 +64,26 @@ reload_tun_properties (NMDeviceTun *self)
 	NMDeviceTunPrivate *priv = NM_DEVICE_TUN_GET_PRIVATE (self);
 	GObject *object = G_OBJECT (self);
 	NMPlatformTunProperties props;
+	int ifindex;
 
-	if (!nm_platform_tun_get_properties (NM_PLATFORM_GET, nm_device_get_ifindex (NM_DEVICE (self)), &props)) {
-		_LOGD (LOGD_HW, "could not read tun properties");
-		return;
-	}
+	ifindex = nm_device_get_ifindex (NM_DEVICE (self));
+	if (ifindex > 0) {
+		if (!nm_platform_tun_get_properties (NM_PLATFORM_GET, ifindex, &props)) {
+			_LOGD (LOGD_DEVICE, "tun-properties: cannot loading tun properties from platform for ifindex %d", ifindex);
+			ifindex = 0;
+		} else if (g_strcmp0 (priv->mode, props.mode) != 0) {
+			/* if the mode differs, we ignore what we loaded. A NMDeviceTun cannot
+			 * change the mode after construction. */
+			_LOGD (LOGD_DEVICE, "tun-properties: loading tun properties yielded tun-mode %s%s%s, but %s%s%s expected (ifindex %d)",
+			       NM_PRINT_FMT_QUOTE_STRING (props.mode),
+			       NM_PRINT_FMT_QUOTE_STRING (priv->mode),
+			       ifindex);
+			ifindex = 0;
+		}
+	} else
+		_LOGD (LOGD_DEVICE, "tun-properties: ignore loading properties due to missing ifindex");
+	if (ifindex <= 0)
+		memset (&props, 0, sizeof (props));
 
 	g_object_freeze_notify (object);
 
@@ -83,7 +98,6 @@ reload_tun_properties (NMDeviceTun *self)
 	if (priv->props.multi_queue != props.multi_queue)
 		g_object_notify (object, NM_DEVICE_TUN_MULTI_QUEUE);
 
-	priv->mode = props.mode;
 	memcpy (&priv->props, &props, sizeof (NMPlatformTunProperties));
 
 	g_object_thaw_notify (object);