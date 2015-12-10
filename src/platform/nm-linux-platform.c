NetworkManager/NetworkManager

commit e9f364548a65fd4e26bf22367fe7c28fe127ab41

platform, devices: add support for macvlan and macvtap devices

src/platform/nm-linux-platform.c
@@ -26,6 +26,7 @@
 #include <netinet/icmp6.h>
 #include <netinet/in.h>
 #include <linux/if_arp.h>
+#include <linux/if_link.h>
 #include <linux/if_tun.h>
 #include <sys/ioctl.h>
 #include <linux/sockios.h>
@@ -259,6 +260,88 @@ delete_kernel_object (struct nl_sock *sock, struct nl_object *object)
 	}
 }
 
+/* nm_rtnl_link_parse_info_data(): Re-fetches a link from the kernel
+ * and parses its IFLA_INFO_DATA using a caller-provided parser.
+ *
+ * Code is stolen from rtnl_link_get_kernel(), nl_pickup(), and link_msg_parser().
+ */
+
+typedef int (*NMNLInfoDataParser) (struct nlattr *info_data, gpointer parser_data);
+
+typedef struct {
+	NMNLInfoDataParser parser;
+	gpointer parser_data;
+} NMNLInfoDataClosure;
+
+static struct nla_policy info_data_link_policy[IFLA_MAX + 1] = {
+	[IFLA_LINKINFO]	= { .type = NLA_NESTED },
+};
+
+static struct nla_policy info_data_link_info_policy[IFLA_INFO_MAX + 1] = {
+	[IFLA_INFO_DATA]	= { .type = NLA_NESTED },
+};
+
+static int
+info_data_parser (struct nl_msg *msg, void *arg)
+{
+	NMNLInfoDataClosure *closure = arg;
+	struct nlmsghdr *n = nlmsg_hdr (msg);
+	struct nlattr *tb[IFLA_MAX + 1];
+	struct nlattr *li[IFLA_INFO_MAX + 1];
+	int err;
+
+	if (!nlmsg_valid_hdr (n, sizeof (struct ifinfomsg)))
+		return -NLE_MSG_TOOSHORT;
+
+	err = nlmsg_parse (n, sizeof (struct ifinfomsg), tb, IFLA_MAX, info_data_link_policy);
+	if (err < 0)
+		return err;
+
+	if (!tb[IFLA_LINKINFO])
+		return -NLE_MISSING_ATTR;
+
+	err = nla_parse_nested (li, IFLA_INFO_MAX, tb[IFLA_LINKINFO], info_data_link_info_policy);
+	if (err < 0)
+		return err;
+
+	if (!li[IFLA_INFO_DATA])
+		return -NLE_MISSING_ATTR;
+
+	return closure->parser (li[IFLA_INFO_DATA], closure->parser_data);
+}
+
+static int
+nm_rtnl_link_parse_info_data (struct nl_sock *sk, int ifindex,
+                              NMNLInfoDataParser parser, gpointer parser_data)
+{
+	NMNLInfoDataClosure data = { .parser = parser, .parser_data = parser_data };
+	struct nl_msg *msg = NULL;
+	struct nl_cb *cb;
+	int err;
+
+	err = rtnl_link_build_get_request (ifindex, NULL, &msg);
+	if (err < 0)
+		return err;
+
+	err = nl_send_auto (sk, msg);
+	nlmsg_free (msg);
+	if (err < 0)
+		return err;
+
+	cb = nl_cb_clone (nl_socket_get_cb (sk));
+	if (cb == NULL)
+		return -NLE_NOMEM;
+	nl_cb_set (cb, NL_CB_VALID, NL_CB_CUSTOM, info_data_parser, &data);
+
+	err = nl_recvmsgs (sk, cb);
+	nl_cb_put (cb);
+	if (err < 0)
+		return err;
+
+	nl_wait_for_ack (sk);
+	return 0;
+}
+
 /******************************************************************/
 
 /* Object type specific utilities */
@@ -272,6 +355,10 @@ type_to_string (NMLinkType type)
 		return "dummy";
 	case NM_LINK_TYPE_IFB:
 		return "ifb";
+	case NM_LINK_TYPE_MACVLAN:
+		return "macvlan";
+	case NM_LINK_TYPE_MACVTAP:
+		return "macvtap";
 	case NM_LINK_TYPE_TAP:
 		return "tap";
 	case NM_LINK_TYPE_TUN:
@@ -333,6 +420,10 @@ link_extract_type (struct rtnl_link *rtnllink, const char **out_name)
 		return_type (NM_LINK_TYPE_DUMMY, "dummy");
 	else if (!strcmp (type, "ifb"))
 		return_type (NM_LINK_TYPE_IFB, "ifb");
+	else if (!strcmp (type, "macvlan"))
+		return_type (NM_LINK_TYPE_MACVLAN, "macvlan");
+	else if (!strcmp (type, "macvtap"))
+		return_type (NM_LINK_TYPE_MACVTAP, "macvtap");
 	else if (!strcmp (type, "tun")) {
 		NMPlatformTunProperties props;
 
@@ -1488,6 +1579,63 @@ tun_get_properties (NMPlatform *platform, int ifindex, NMPlatformTunProperties *
 	return TRUE;
 }
 
+static const struct nla_policy macvlan_info_policy[IFLA_MACVLAN_MAX + 1] = {
+	[IFLA_MACVLAN_MODE]  = { .type = NLA_U32 },
+	[IFLA_MACVLAN_FLAGS] = { .type = NLA_U16 },
+};
+
+static int
+macvlan_info_data_parser (struct nlattr *info_data, gpointer parser_data)
+{
+	NMPlatformMacvlanProperties *props = parser_data;
+	struct nlattr *tb[IFLA_MACVLAN_MAX + 1];
+	int err;
+
+	err = nla_parse_nested (tb, IFLA_MACVLAN_MAX, info_data,
+	                        (struct nla_policy *) macvlan_info_policy);
+	if (err < 0)
+		return err;
+
+	switch (nla_get_u32 (tb[IFLA_MACVLAN_MODE])) {
+	case MACVLAN_MODE_PRIVATE:
+		props->mode = "private";
+		break;
+	case MACVLAN_MODE_VEPA:
+		props->mode = "vepa";
+		break;
+	case MACVLAN_MODE_BRIDGE:
+		props->mode = "bridge";
+		break;
+	case MACVLAN_MODE_PASSTHRU:
+		props->mode = "passthru";
+		break;
+	default:
+		return -NLE_PARSE_ERR;
+	}
+
+	props->no_promisc = !!(nla_get_u16 (tb[IFLA_MACVLAN_FLAGS]) & MACVLAN_FLAG_NOPROMISC);
+
+	return 0;
+}
+
+static gboolean
+macvlan_get_properties (NMPlatform *platform, int ifindex, NMPlatformMacvlanProperties *props)
+{
+	NMLinuxPlatformPrivate *priv = NM_LINUX_PLATFORM_GET_PRIVATE (platform);
+	auto_nl_object struct rtnl_link *rtnllink;
+	int err;
+
+	rtnllink = link_get (platform, ifindex);
+	if (!rtnllink)
+		return FALSE;
+
+	props->parent_ifindex = rtnl_link_get_link (rtnllink);
+
+	err = nm_rtnl_link_parse_info_data (priv->nlh, ifindex,
+	                                    macvlan_info_data_parser, props);
+	return (err == 0);
+}
+
 /******************************************************************/
 
 static int
@@ -1972,6 +2120,7 @@ nm_linux_platform_class_init (NMLinuxPlatformClass *klass)
 
 	platform_class->veth_get_properties = veth_get_properties;
 	platform_class->tun_get_properties = tun_get_properties;
+	platform_class->macvlan_get_properties = macvlan_get_properties;
 
 	platform_class->ip4_address_get_all = ip4_address_get_all;
 	platform_class->ip6_address_get_all = ip6_address_get_all;

include/NetworkManager.h
@@ -56,6 +56,7 @@
 #define NM_DBUS_INTERFACE_DEVICE_GENERIC    NM_DBUS_INTERFACE_DEVICE ".Generic"
 #define NM_DBUS_INTERFACE_DEVICE_VETH       NM_DBUS_INTERFACE_DEVICE ".Veth"
 #define NM_DBUS_INTERFACE_DEVICE_TUN        NM_DBUS_INTERFACE_DEVICE ".Tun"
+#define NM_DBUS_INTERFACE_DEVICE_MACVLAN    NM_DBUS_INTERFACE_DEVICE ".Macvlan"
 
 
 #define NM_DBUS_IFACE_SETTINGS            "org.freedesktop.NetworkManager.Settings"

introspection/Makefile.am
@@ -18,6 +18,7 @@ EXTRA_DIST = \
 	nm-device-generic.xml \
 	nm-device-veth.xml \
 	nm-device-tun.xml \
+	nm-device-macvlan.xml \
 	nm-device.xml \
 	nm-ip4-config.xml \
 	nm-ip6-config.xml \

introspection/nm-device-macvlan.xml
@@ -1,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+
+<node name="/" xmlns:tp="http://telepathy.freedesktop.org/wiki/DbusSpec#extensions-v0">
+  <interface name="org.freedesktop.NetworkManager.Device.Macvlan">
+
+    <property name="Parent" type="o" access="read">
+      <tp:docstring>
+        The object path of the parent device.
+      </tp:docstring>
+    </property>
+
+    <property name="Mode" type="s" access="read">
+      <tp:docstring>
+        The macvlan mode, one of "private", "vepa", "bridge", or "passthru".
+      </tp:docstring>
+    </property>
+
+    <property name="NoPromisc" type="b" access="read">
+      <tp:docstring>
+        Whether the device is blocked from going into promiscuous mode.
+      </tp:docstring>
+    </property>
+
+    <signal name="PropertiesChanged">
+        <arg name="properties" type="a{sv}" tp:type="String_Variant_Map">
+            <tp:docstring>
+                A dictionary mapping property names to variant boxed values
+            </tp:docstring>
+        </arg>
+    </signal>
+
+  </interface>
+</node>

src/Makefile.am
@@ -78,6 +78,8 @@ nm_sources = \
 	devices/nm-device-generic.h \
 	devices/nm-device-infiniband.c \
 	devices/nm-device-infiniband.h \
+	devices/nm-device-macvlan.c \
+	devices/nm-device-macvlan.h \
 	devices/nm-device-modem.c \
 	devices/nm-device-modem.h \
 	devices/nm-device-olpc-mesh.c \
@@ -313,6 +315,7 @@ glue_sources = \
 	nm-device-generic-glue.h \
 	nm-device-glue.h \
 	nm-device-infiniband-glue.h \
+	nm-device-macvlan-glue.h \
 	nm-device-modem-glue.h \
 	nm-device-olpc-mesh-glue.h \
 	nm-device-tun-glue.h \

src/devices/nm-device-macvlan.c
@@ -1,0 +1,182 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* NetworkManager -- Network link manager
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Copyright 2013 Red Hat, Inc.
+ */
+
+#include "config.h"
+
+#include <string.h>
+
+#include "nm-device-macvlan.h"
+#include "nm-dbus-manager.h"
+#include "nm-logging.h"
+#include "nm-manager.h"
+#include "nm-platform.h"
+
+#include "nm-device-macvlan-glue.h"
+
+G_DEFINE_TYPE (NMDeviceMacvlan, nm_device_macvlan, NM_TYPE_DEVICE_GENERIC)
+
+#define NM_DEVICE_MACVLAN_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), NM_TYPE_DEVICE_MACVLAN, NMDeviceMacvlanPrivate))
+
+typedef struct {
+	NMDevice *parent;
+	NMPlatformMacvlanProperties props;
+} NMDeviceMacvlanPrivate;
+
+enum {
+	PROP_0,
+	PROP_PARENT,
+	PROP_MODE,
+	PROP_NO_PROMISC,
+
+	LAST_PROP
+};
+
+/**************************************************************/
+
+/**************************************************************/
+
+static void
+link_changed (NMDevice *device)
+{
+	NMDeviceMacvlanPrivate *priv = NM_DEVICE_MACVLAN_GET_PRIVATE (device);
+	GObject *object = G_OBJECT (device);
+	NMPlatformMacvlanProperties props;
+
+	if (!nm_platform_macvlan_get_properties (nm_device_get_ifindex (device), &props)) {
+		nm_log_warn (LOGD_HW, "(%s): could not read macvlan properties",
+		             nm_device_get_iface (device));
+		return;
+	}
+
+	g_object_freeze_notify (object);
+
+	if (priv->props.parent_ifindex != props.parent_ifindex) {
+		g_object_notify (object, NM_DEVICE_MACVLAN_PARENT);
+		if (priv->parent)
+			g_object_remove_weak_pointer (G_OBJECT (priv->parent), (gpointer *) &priv->parent);
+		priv->parent = nm_manager_get_device_by_ifindex (nm_manager_get (), props.parent_ifindex);
+		if (priv->parent)
+			g_object_add_weak_pointer (G_OBJECT (priv->parent), (gpointer *) &priv->parent);
+	}
+	if (g_strcmp0 (priv->props.mode, props.mode) != 0)
+		g_object_notify (object, NM_DEVICE_MACVLAN_MODE);
+	if (priv->props.no_promisc != props.no_promisc)
+		g_object_notify (object, NM_DEVICE_MACVLAN_NO_PROMISC);
+
+	memcpy (&priv->props, &props, sizeof (NMPlatformMacvlanProperties));
+
+	g_object_thaw_notify (object);
+}
+
+/**************************************************************/
+
+NMDevice *
+nm_device_macvlan_new (const char *udi,
+                       const char *iface,
+                       const char *driver)
+{
+	g_return_val_if_fail (udi != NULL, NULL);
+
+	return (NMDevice *) g_object_new (NM_TYPE_DEVICE_MACVLAN,
+	                                  NM_DEVICE_UDI, udi,
+	                                  NM_DEVICE_IFACE, iface,
+	                                  NM_DEVICE_DRIVER, driver,
+	                                  NM_DEVICE_TYPE_DESC, "Macvlan",
+	                                  NM_DEVICE_DEVICE_TYPE, NM_DEVICE_TYPE_GENERIC,
+	                                  NULL);
+}
+
+static void
+nm_device_macvlan_init (NMDeviceMacvlan *self)
+{
+}
+
+static void
+constructed (GObject *object)
+{
+	link_changed (NM_DEVICE (object));
+
+	G_OBJECT_CLASS (nm_device_macvlan_parent_class)->constructed (object);
+}
+
+static void
+get_property (GObject *object, guint prop_id,
+              GValue *value, GParamSpec *pspec)
+{
+	NMDeviceMacvlanPrivate *priv = NM_DEVICE_MACVLAN_GET_PRIVATE (object);
+
+	switch (prop_id) {
+	case PROP_PARENT:
+		g_value_set_boxed (value, priv->parent ? nm_device_get_path (priv->parent) : "/");
+		break;
+	case PROP_MODE:
+		g_value_set_string (value, priv->props.mode);
+		break;
+	case PROP_NO_PROMISC:
+		g_value_set_boolean (value, priv->props.no_promisc);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static void
+nm_device_macvlan_class_init (NMDeviceMacvlanClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	NMDeviceClass *device_class = NM_DEVICE_CLASS (klass);
+
+	g_type_class_add_private (klass, sizeof (NMDeviceMacvlanPrivate));
+
+	object_class->constructed = constructed;
+	object_class->get_property = get_property;
+
+	device_class->link_changed = link_changed;
+
+	/* properties */
+	g_object_class_install_property
+		(object_class, PROP_PARENT,
+		 g_param_spec_boxed (NM_DEVICE_MACVLAN_PARENT,
+		                     "Parent",
+		                     "Parent device",
+		                     DBUS_TYPE_G_OBJECT_PATH,
+		                     G_PARAM_READABLE));
+
+	g_object_class_install_property
+		(object_class, PROP_MODE,
+		 g_param_spec_string (NM_DEVICE_MACVLAN_MODE,
+		                      "Mode",
+		                      "Mode: 'private', 'vepa', 'bridge', or 'passthru'",
+		                      NULL,
+		                      G_PARAM_READABLE));
+
+	g_object_class_install_property
+		(object_class, PROP_NO_PROMISC,
+		 g_param_spec_boolean (NM_DEVICE_MACVLAN_NO_PROMISC,
+		                       "No-promisc",
+		                       "No promiscuous mode",
+		                       FALSE,
+		                       G_PARAM_READABLE));
+
+	nm_dbus_manager_register_exported_type (nm_dbus_manager_get (),
+	                                        G_TYPE_FROM_CLASS (klass),
+	                                        &dbus_glib_nm_device_macvlan_object_info);
+}

src/devices/nm-device-macvlan.h
@@ -1,0 +1,58 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* NetworkManager -- Network link manager
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Copyright 2013 Red Hat, Inc.
+ */
+
+#ifndef NM_DEVICE_MACVLAN_H
+#define NM_DEVICE_MACVLAN_H
+
+#include <glib-object.h>
+
+#include "nm-device-generic.h"
+
+G_BEGIN_DECLS
+
+#define NM_TYPE_DEVICE_MACVLAN            (nm_device_macvlan_get_type ())
+#define NM_DEVICE_MACVLAN(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), NM_TYPE_DEVICE_MACVLAN, NMDeviceMacvlan))
+#define NM_DEVICE_MACVLAN_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  NM_TYPE_DEVICE_MACVLAN, NMDeviceMacvlanClass))
+#define NM_IS_DEVICE_MACVLAN(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NM_TYPE_DEVICE_MACVLAN))
+#define NM_IS_DEVICE_MACVLAN_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  NM_TYPE_DEVICE_MACVLAN))
+#define NM_DEVICE_MACVLAN_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  NM_TYPE_DEVICE_MACVLAN, NMDeviceMacvlanClass))
+
+#define NM_DEVICE_MACVLAN_PARENT     "parent"
+#define NM_DEVICE_MACVLAN_MODE       "mode"
+#define NM_DEVICE_MACVLAN_NO_PROMISC "no-promisc"
+
+typedef struct {
+	NMDeviceGeneric parent;
+} NMDeviceMacvlan;
+
+typedef struct {
+	NMDeviceGenericClass parent;
+
+} NMDeviceMacvlanClass;
+
+GType nm_device_macvlan_get_type (void);
+
+NMDevice *nm_device_macvlan_new (const char *udi,
+                                 const char *iface,
+                                 const char *driver);
+
+G_END_DECLS
+
+#endif	/* NM_DEVICE_MACVLAN_H */

src/nm-manager.c
@@ -54,6 +54,7 @@
 #include "nm-device-generic.h"
 #include "nm-device-veth.h"
 #include "nm-device-tun.h"
+#include "nm-device-macvlan.h"
 #include "nm-system.h"
 #include "nm-setting-bluetooth.h"
 #include "nm-setting-connection.h"
@@ -2296,6 +2297,10 @@ udev_device_added_cb (NMUdevManager *udev_mgr,
 		case NM_LINK_TYPE_TUN:
 		case NM_LINK_TYPE_TAP:
 			device = nm_device_tun_new (sysfs_path, iface, driver);
+			break;
+		case NM_LINK_TYPE_MACVLAN:
+		case NM_LINK_TYPE_MACVTAP:
+			device = nm_device_macvlan_new (sysfs_path, iface, driver);
 			break;
 
 		default:

commit 088604f62ea77fcc492eb6cb4df70dcdbc318751

platform: fix memleak in _nl_link_parse_info_data()
Fixes: e9f364548a65fd4e26bf22367fe7c28fe127ab41

src/platform/nm-linux-platform.c
@@ -384,6 +384,7 @@ _nl_link_parse_info_data (struct nl_sock *sk, int ifindex,
 	NMNLInfoDataClosure data = { .parser = parser, .parser_data = parser_data };
 	struct nl_msg *msg = NULL;
 	struct nl_cb *cb;
+	struct nl_cb *cb0;
 	int err;
 
 	err = rtnl_link_build_get_request (ifindex, NULL, &msg);
@@ -395,7 +396,9 @@ _nl_link_parse_info_data (struct nl_sock *sk, int ifindex,
 	if (err < 0)
 		return err;
 
-	cb = nl_cb_clone (nl_socket_get_cb (sk));
+	cb0 = nl_socket_get_cb (sk);
+	cb = nl_cb_clone (cb0);
+	nl_cb_put (cb0);
 	if (cb == NULL)
 		return -NLE_NOMEM;
 	nl_cb_set (cb, NL_CB_VALID, NL_CB_CUSTOM, _nl_link_parse_info_data_cb, &data);