NetworkManager/NetworkManager

commit 3a6cd8204f80f607568657bbb05224331cb92a1f

cli: remove stray space from help description

clients/cli/connections.c
@@ -383,7 +383,7 @@ usage_connection_add (void)
 	              "                  [p-key <IPoIB P_Key>]\n\n"
 	              "    bluetooth:    [addr <bluetooth address>]\n"
 	              "                  [bt-type panu|dun-gsm|dun-cdma]\n\n"
-	              "    vlan:         dev <parent device (connection  UUID, ifname, or MAC)>\n"
+	              "    vlan:         dev <parent device (connection UUID, ifname, or MAC)>\n"
 	              "                  id <VLAN ID>\n"
 	              "                  [flags <VLAN flags>]\n"
 	              "                  [ingress <ingress priority mapping>]\n"
@@ -434,6 +434,7 @@ usage_connection_add (void)
 	              "                  [local <local endpoint IP>]\n"
 	              "                  [dev <parent device (ifname or connection UUID)>]\n\n"
 	              "    macvlan:      dev <parent device (connection  UUID, ifname, or MAC)>\n"
+	              "    macvlan:      dev <parent device (connection UUID, ifname, or MAC)>\n"
 	              "                  mode vepa|bridge|private|passthru|source\n"
 	              "                  [tap yes|no]\n\n"
 	              "    vxlan:        id <VXLAN ID>\n"

commit 81dcf9b218b9568169e0b17d7f698e7ec074ce6c

cli: really fix the description this time
Fixes: 3a6cd8204f80f607568657bbb05224331cb92a1f

clients/cli/connections.c
@@ -435,7 +435,6 @@ usage_connection_add (void)
 	              "                  remote <remote endpoint IP>\n"
 	              "                  [local <local endpoint IP>]\n"
 	              "                  [dev <parent device (ifname or connection UUID)>]\n\n"
-	              "    macvlan:      dev <parent device (connection  UUID, ifname, or MAC)>\n"
 	              "    macvlan:      dev <parent device (connection UUID, ifname, or MAC)>\n"
 	              "                  mode vepa|bridge|private|passthru|source\n"
 	              "                  [tap yes|no]\n\n"