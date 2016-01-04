4b5352057aebd9f1dc4e3a44a3258f6ce55f0266
Thomas Haller <thaller@redhat.com>
2016-01-04 16:41:48 +0100

    platform/tests: avoid test failure in test_ip6_address_general()
      # Start of ipv6 tests
      # NetworkManager-FATAL-ERROR: NMPlatformSignalAssert: ../../../../../src/platform/tests/test-address.c:154, test_ip6_address_general(): failure to free non-accepted signal: ip6-address-changed-changed ifindex 11 (1 times received)

--- src/platform/tests/test-address.c
+++ src/platform/tests/test-address.c
@@ -150,6 +150,9 @@ test_ip6_address_general (void)
 	/* Remove address again */
 	nmtstp_ip6_address_del (EX, ifindex, addr, IP6_PLEN);
 
+	/* ensure no pending signal. */
+	accept_signals (address_changed, 0, 1);
+
 	free_signal (address_added);
 	free_signal (address_changed);
 	free_signal (address_removed);
