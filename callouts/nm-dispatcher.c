NetworkManager/NetworkManager

commit e97a334e375b936ac66864e6d69ffe75ffa22aa9

dispatcher: run "wait" scripts only after "no-wait" scripts complete
Previously, dispatcher would start all "no-wait" script right away,
but it would also start the first "wait" script while the "no-wait"
script are still running (unless, there are other "wait" scripts that
delay the execution).

Now, do not start processing the "wait" scripts, as long as there are
any "no-wait" scripts from the same request running.

callouts/nm-dispatcher.c
@@ -130,6 +130,7 @@ struct Request {
 	GPtrArray *scripts;  /* list of ScriptInfo */
 	guint idx;
 	gint num_scripts_done;
+	gint num_scripts_nowait;
 };
 
 static void
@@ -146,6 +147,7 @@ static void
 request_free (Request *request)
 {
 	g_assert_cmpuint (request->num_scripts_done, ==, request->scripts->len);
+	g_assert_cmpuint (request->num_scripts_nowait, ==, 0);
 
 	g_free (request->action);
 	g_free (request->iface);
@@ -295,8 +297,18 @@ complete_script (ScriptInfo *script)
 		/* this was a "no-wait" script. We either completed the request,
 		 * or there is nothing to do. Especially, there is no need to
 		 * queue the next_request() -- because no-wait scripts don't block
-		 * requests. */
-		return;
+		 * requests. However, if this was the last "no-wait" script and
+		 * there are "wait" scripts ready to run, launch them.
+		 */
+		if (   script->request->num_scripts_nowait == 0
+		    && handler->current_request == script->request) {
+
+			if (dispatch_one_script (script->request))
+				return;
+
+			complete_request (script->request);
+		} else
+			return;
 	}
 
 	while (next_request (handler, NULL)) {
@@ -328,6 +340,8 @@ script_watch_cb (GPid pid, gint status, gpointer user_data)
 	script->watch_id = 0;
 	nm_clear_g_source (&script->timeout_id);
 	script->request->num_scripts_done++;
+	if (!script->wait)
+		script->request->num_scripts_nowait--;
 
 	if (WIFEXITED (status)) {
 		err = WEXITSTATUS (status);
@@ -369,6 +383,8 @@ script_timeout_cb (gpointer user_data)
 	script->timeout_id = 0;
 	nm_clear_g_source (&script->watch_id);
 	script->request->num_scripts_done++;
+	if (!script->wait)
+		script->request->num_scripts_nowait--;
 
 	g_warning ("Script '%s' took too long; killing it.", script->script);
 
@@ -471,6 +487,8 @@ script_dispatch (ScriptInfo *script)
 	if (g_spawn_async ("/", argv, request->envp, G_SPAWN_DO_NOT_REAP_CHILD, NULL, NULL, &script->pid, &error)) {
 		script->watch_id = g_child_watch_add (script->pid, (GChildWatchFunc) script_watch_cb, script);
 		script->timeout_id = g_timeout_add_seconds (SCRIPT_TIMEOUT, script_timeout_cb, script);
+		if (!script->wait)
+			request->num_scripts_nowait++;
 		return TRUE;
 	} else {
 		g_warning ("Failed to execute script '%s': (%d) %s",
@@ -486,6 +504,9 @@ script_dispatch (ScriptInfo *script)
 static gboolean
 dispatch_one_script (Request *request)
 {
+	if (request->num_scripts_nowait > 0)
+		return TRUE;
+
 	while (request->idx < request->scripts->len) {
 		ScriptInfo *script;
 

commit ddc6d97f00e81cd422bb4d48adcadff70018de0f

dispatcher: only evaluate the number of the scripts if the request is still there
The request could have been completed by the call to complete_request() just above.
In that case we should just return instead of looking for nowait scripts to run.

  Core was generated by `/usr/libexec/nm-dispatcher'.
  Program terminated with signal SIGSEGV, Segmentation fault.
  #0  0x00007f6c916ad0fb in complete_script (script=<error reading variable: value has been optimized out>, script=<error reading variable: value has been optimized out>) at nm-dispatcher.c:349
  349                     if (   script->request->num_scripts_nowait == 0
  (gdb) bt
  #0  0x00007f6c916ad0fb in complete_script (script=<error reading variable: value has been optimized out>, script=<error reading variable: value has been optimized out>) at nm-dispatcher.c:349
  #1  0x00007f6c8f02d484 in g_child_watch_dispatch (source=<optimized out>, callback=<optimized out>, user_data=<optimized out>) at gmain.c:5148
  #2  0x00007f6c8f03079a in g_main_context_dispatch (context=0x7f6c9358d9a0) at gmain.c:3109
  #3  0x00007f6c8f03079a in g_main_context_dispatch (context=context@entry=0x7f6c9358d9a0) at gmain.c:3708
  #4  0x00007f6c8f030ae8 in g_main_context_iterate (context=0x7f6c9358d9a0, block=block@entry=1, dispatch=dispatch@entry=1, self=<optimized out>) at gmain.c:3779
  #5  0x00007f6c8f030dba in g_main_loop_run (loop=0x7f6c9358da80) at gmain.c:3973
  #6  0x00007f6c916abcd4 in main (argc=1, argv=0x7ffee4326768) at nm-dispatcher.c:935
  (gdb)

Fixes: e97a334e375b936ac66864e6d69ffe75ffa22aa9

https://bugzilla.redhat.com/show_bug.cgi?id=1297826

callouts/nm-dispatcher.c
@@ -346,8 +346,8 @@ complete_script (ScriptInfo *script)
 		 * requests. However, if this was the last "no-wait" script and
 		 * there are "wait" scripts ready to run, launch them.
 		 */
-		if (   script->request->num_scripts_nowait == 0
-		    && handler->current_request == script->request) {
+		if (   handler->current_request == script->request
+		    && script->request->num_scripts_nowait == 0) {
 
 			if (dispatch_one_script (script->request))
 				return;