NetworkManager/NetworkManager

commit 7e5f27a21ca22d986755e4cc480e85d8a51ff17d

manager: let external devices go on removal
If we didn't create them, we shouldn't keep them around unrealized.

src/nm-manager.c
@@ -2042,8 +2042,8 @@ _platform_link_cb_idle (PlatformLinkCbData *data)
 
 		device = nm_manager_get_device_by_ifindex (self, data->ifindex);
 		if (device) {
-			if (nm_device_is_software (device)) {
-				/* Software devices stick around until their connection is removed */
+			if (nm_device_is_software (device) && nm_device_get_is_nm_owned (device)) {
+				/* Our software devices stick around until their connection is removed */
 				if (!nm_device_unrealize (device, FALSE, &error)) {
 					nm_log_warn (LOGD_DEVICE, "(%s): failed to unrealize: %s",
 					             nm_device_get_iface (device),
@@ -2052,7 +2052,7 @@ _platform_link_cb_idle (PlatformLinkCbData *data)
 					remove_device (self, device, FALSE, TRUE);
 				}
 			} else {
-				/* Hardware devices always get removed when their kernel link is gone */
+				/* Hardware and external devices always get removed when their kernel link is gone */
 				remove_device (self, device, FALSE, TRUE);
 			}
 		}

commit 2292cb63bf6213f85b30071a2929e853e6640da6

core: always unrealize software devices when platform link disappears
We possibly need the unrealized device for connections that need
this. nm_device_unrealize() will check if there are still any
available-connections and possibly emit DEVICE_REMOVED signal.

Fixes: 7e5f27a21ca22d986755e4cc480e85d8a51ff17d

src/nm-manager.c
@@ -2088,7 +2088,7 @@ _platform_link_cb_idle (PlatformLinkCbData *data)
 
 		device = nm_manager_get_device_by_ifindex (self, data->ifindex);
 		if (device) {
-			if (nm_device_is_software (device) && nm_device_get_is_nm_owned (device)) {
+			if (nm_device_is_software (device)) {
 				/* Our software devices stick around until their connection is removed */
 				if (!nm_device_unrealize (device, FALSE, &error)) {
 					nm_log_warn (LOGD_DEVICE, "(%s): failed to unrealize: %s",