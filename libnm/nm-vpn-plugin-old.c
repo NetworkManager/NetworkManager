NetworkManager/NetworkManager

commit 867227dd4aac8371b3cacc366b32a3fa96eba42a

libnm/vpn: add new NMVpnServicePlugin class
After copying "nm-vpn-plugin-old.*" to "nm-vpn-service-plugin.*",
rename the class and add it to the Makefile.

This will become the new VPN Service API for libnm 1.2. No changes
done yet except renaming of the classes and functions.

Rename the previous classes NMVpnPlugin(Old) to NMVpnServicePlugin
to have a distinct name from NMVpnEditorPlugin. Buth are plugins, but
with a different use.

https://bugzilla.gnome.org/show_bug.cgi?id=749951

docs/libnm/Makefile.am
@@ -54,6 +54,7 @@ IGNORE_HFILES= \
 	nm-types.h \
 	nm-utils-private.h \
 	nm-vpn-plugin-old.h \
+	nm-vpn-service-plugin.h \
 	nm-core-tests-enum-types.h
 
 # Images to copy into HTML directory.

libnm/libnm.ver
@@ -821,17 +821,6 @@ global:
 	nm_vpn_plugin_error_get_type;
 	nm_vpn_plugin_error_quark;
 	nm_vpn_plugin_failure_get_type;
-	nm_vpn_plugin_old_disconnect;
-	nm_vpn_plugin_old_failure;
-	nm_vpn_plugin_old_get_connection;
-	nm_vpn_plugin_old_get_secret_flags;
-	nm_vpn_plugin_old_get_state;
-	nm_vpn_plugin_old_get_type;
-	nm_vpn_plugin_old_read_vpn_details;
-	nm_vpn_plugin_old_secrets_required;
-	nm_vpn_plugin_old_set_ip4_config;
-	nm_vpn_plugin_old_set_login_banner;
-	nm_vpn_plugin_old_set_state;
 	nm_vpn_service_state_get_type;
 	nm_wep_key_type_get_type;
 	nm_wimax_nsp_connection_valid;
@@ -902,4 +891,15 @@ global:
 	nm_vpn_plugin_info_list_find_by_service;
 	nm_vpn_plugin_info_list_load;
 	nm_vpn_plugin_info_list_remove;
+	nm_vpn_service_plugin_disconnect;
+	nm_vpn_service_plugin_failure;
+	nm_vpn_service_plugin_get_connection;
+	nm_vpn_service_plugin_get_secret_flags;
+	nm_vpn_service_plugin_get_state;
+	nm_vpn_service_plugin_get_type;
+	nm_vpn_service_plugin_read_vpn_details;
+	nm_vpn_service_plugin_secrets_required;
+	nm_vpn_service_plugin_set_ip4_config;
+	nm_vpn_service_plugin_set_login_banner;
+	nm_vpn_service_plugin_set_state;
 } libnm_1_0_0;

libnm/Makefile.am
@@ -55,7 +55,8 @@ libnminclude_hfiles =			\
 
 libnminclude_nointrospect_hfiles =	\
 	nm-secret-agent-old.h		\
-	nm-vpn-plugin-old.h
+	nm-vpn-plugin-old.h         \
+	nm-vpn-service-plugin.h
 
 libnminclude_HEADERS =			\
 	$(libnminclude_hfiles)		\
@@ -109,6 +110,7 @@ libnm_la_csources =			\
 	nm-vpn-connection.c		\
 	nm-vpn-plugin-old.c		\
 	nm-vpn-editor.c		\
+	nm-vpn-service-plugin.c     \
 	nm-wimax-nsp.c
 
 libnm_la_SOURCES =			\

libnm/nm-vpn-service-plugin.h
@@ -16,11 +16,11 @@
  * Boston, MA 02110-1301 USA.
  *
  * Copyright 2007 - 2008 Novell, Inc.
- * Copyright 2007 - 2013 Red Hat, Inc.
+ * Copyright 2007 - 2015 Red Hat, Inc.
  */
 
-#ifndef __NM_VPN_PLUGIN_OLD_H__
-#define __NM_VPN_PLUGIN_OLD_H__
+#ifndef __NM_VPN_SERVICE_PLUGIN_H__
+#define __NM_VPN_SERVICE_PLUGIN_H__
 
 #include <gio/gio.h>
 #include <nm-vpn-dbus-interface.h>
@@ -28,109 +28,109 @@
 
 G_BEGIN_DECLS
 
-#define NM_TYPE_VPN_PLUGIN_OLD            (nm_vpn_plugin_old_get_type ())
-#define NM_VPN_PLUGIN_OLD(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), NM_TYPE_VPN_PLUGIN_OLD, NMVpnPluginOld))
-#define NM_VPN_PLUGIN_OLD_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), NM_TYPE_VPN_PLUGIN_OLD, NMVpnPluginOldClass))
-#define NM_IS_VPN_PLUGIN_OLD(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NM_TYPE_VPN_PLUGIN_OLD))
-#define NM_IS_VPN_PLUGIN_OLD_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), NM_TYPE_VPN_PLUGIN_OLD))
-#define NM_VPN_PLUGIN_OLD_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), NM_TYPE_VPN_PLUGIN_OLD, NMVpnPluginOldClass))
+#define NM_TYPE_VPN_SERVICE_PLUGIN            (nm_vpn_service_plugin_get_type ())
+#define NM_VPN_SERVICE_PLUGIN(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), NM_TYPE_VPN_SERVICE_PLUGIN, NMVpnServicePlugin))
+#define NM_VPN_SERVICE_PLUGIN_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), NM_TYPE_VPN_SERVICE_PLUGIN, NMVpnServicePluginClass))
+#define NM_IS_VPN_SERVICE_PLUGIN(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NM_TYPE_VPN_SERVICE_PLUGIN))
+#define NM_IS_VPN_SERVICE_PLUGIN_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), NM_TYPE_VPN_SERVICE_PLUGIN))
+#define NM_VPN_SERVICE_PLUGIN_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), NM_TYPE_VPN_SERVICE_PLUGIN, NMVpnServicePluginClass))
 
-#define NM_VPN_PLUGIN_OLD_DBUS_SERVICE_NAME "service-name"
-#define NM_VPN_PLUGIN_OLD_STATE             "state"
+#define NM_VPN_SERVICE_PLUGIN_DBUS_SERVICE_NAME "service-name"
+#define NM_VPN_SERVICE_PLUGIN_STATE             "state"
 
 typedef struct {
 	GObject parent;
-} NMVpnPluginOld;
+} NMVpnServicePlugin;
 
 typedef struct {
 	GObjectClass parent;
 
 	/* Signals */
-	void (*state_changed)  (NMVpnPluginOld *plugin,
+	void (*state_changed)  (NMVpnServicePlugin *plugin,
 	                        NMVpnServiceState state);
 
-	void (*ip4_config)     (NMVpnPluginOld *plugin,
+	void (*ip4_config)     (NMVpnServicePlugin *plugin,
 	                        GVariant  *ip4_config);
 
-	void (*login_banner)   (NMVpnPluginOld *plugin,
+	void (*login_banner)   (NMVpnServicePlugin *plugin,
 	                        const char *banner);
 
-	void (*failure)        (NMVpnPluginOld *plugin,
+	void (*failure)        (NMVpnServicePlugin *plugin,
 	                        NMVpnPluginFailure reason);
 
-	void (*quit)           (NMVpnPluginOld *plugin);
+	void (*quit)           (NMVpnServicePlugin *plugin);
 
-	void (*config)         (NMVpnPluginOld *plugin,
+	void (*config)         (NMVpnServicePlugin *plugin,
 	                        GVariant  *config);
 
-	void (*ip6_config)     (NMVpnPluginOld *plugin,
+	void (*ip6_config)     (NMVpnServicePlugin *plugin,
 	                        GVariant  *config);
 
 	/* virtual methods */
-	gboolean (*connect)      (NMVpnPluginOld   *plugin,
+	gboolean (*connect)      (NMVpnServicePlugin   *plugin,
 	                          NMConnection  *connection,
 	                          GError       **err);
 
-	gboolean (*need_secrets) (NMVpnPluginOld *plugin,
+	gboolean (*need_secrets) (NMVpnServicePlugin *plugin,
 	                          NMConnection *connection,
 	                          const char **setting_name,
 	                          GError **error);
 
-	gboolean (*disconnect)   (NMVpnPluginOld   *plugin,
+	gboolean (*disconnect)   (NMVpnServicePlugin   *plugin,
 	                          GError       **err);
 
-	gboolean (*new_secrets)  (NMVpnPluginOld *plugin,
+	gboolean (*new_secrets)  (NMVpnServicePlugin *plugin,
 	                          NMConnection *connection,
 	                          GError **error);
 
-	gboolean (*connect_interactive) (NMVpnPluginOld *plugin,
+	gboolean (*connect_interactive) (NMVpnServicePlugin *plugin,
 	                                 NMConnection *connection,
 	                                 GVariant *details,
 	                                 GError **error);
 
 	/*< private >*/
 	gpointer padding[8];
-} NMVpnPluginOldClass;
+} NMVpnServicePluginClass;
 
-GType  nm_vpn_plugin_old_get_type       (void);
+GType  nm_vpn_service_plugin_get_type       (void);
 
-GDBusConnection   *nm_vpn_plugin_old_get_connection (NMVpnPluginOld *plugin);
-NMVpnServiceState  nm_vpn_plugin_old_get_state      (NMVpnPluginOld *plugin);
-void               nm_vpn_plugin_old_set_state      (NMVpnPluginOld *plugin,
-                                                     NMVpnServiceState state);
+GDBusConnection   *nm_vpn_service_plugin_get_connection (NMVpnServicePlugin *plugin);
+NMVpnServiceState  nm_vpn_service_plugin_get_state      (NMVpnServicePlugin *plugin);
+void               nm_vpn_service_plugin_set_state      (NMVpnServicePlugin *plugin,
+                                                         NMVpnServiceState state);
 
-void               nm_vpn_plugin_old_secrets_required (NMVpnPluginOld *plugin,
-                                                       const char *message,
-                                                       const char **hints);
+void               nm_vpn_service_plugin_secrets_required (NMVpnServicePlugin *plugin,
+                                                           const char *message,
+                                                           const char **hints);
 
-void               nm_vpn_plugin_old_set_login_banner (NMVpnPluginOld *plugin,
-                                                       const char *banner);
+void               nm_vpn_service_plugin_set_login_banner (NMVpnServicePlugin *plugin,
+                                                           const char *banner);
 
-void               nm_vpn_plugin_old_failure        (NMVpnPluginOld *plugin,
-                                                     NMVpnPluginFailure reason);
+void               nm_vpn_service_plugin_failure        (NMVpnServicePlugin *plugin,
+                                                         NMVpnPluginFailure reason);
 
-void               nm_vpn_plugin_old_set_config     (NMVpnPluginOld *plugin,
-                                                     GVariant *config);
+void               nm_vpn_service_plugin_set_config     (NMVpnServicePlugin *plugin,
+                                                         GVariant *config);
 
-void               nm_vpn_plugin_old_set_ip4_config (NMVpnPluginOld *plugin,
-                                                     GVariant *ip4_config);
+void               nm_vpn_service_plugin_set_ip4_config (NMVpnServicePlugin *plugin,
+                                                         GVariant *ip4_config);
 
-void               nm_vpn_plugin_old_set_ip6_config (NMVpnPluginOld *plugin,
-                                                     GVariant *ip6_config);
+void               nm_vpn_service_plugin_set_ip6_config (NMVpnServicePlugin *plugin,
+                                                         GVariant *ip6_config);
 
-gboolean           nm_vpn_plugin_old_disconnect     (NMVpnPluginOld *plugin,
-                                                     GError **err);
+gboolean           nm_vpn_service_plugin_disconnect     (NMVpnServicePlugin *plugin,
+                                                         GError **err);
 
 /* Utility functions */
 
-gboolean nm_vpn_plugin_old_read_vpn_details (int fd,
-                                             GHashTable **out_data,
-                                             GHashTable **out_secrets);
+gboolean nm_vpn_service_plugin_read_vpn_details (int fd,
+                                                 GHashTable **out_data,
+                                                 GHashTable **out_secrets);
 
-gboolean nm_vpn_plugin_old_get_secret_flags (GHashTable *data,
-                                             const char *secret_name,
-                                             NMSettingSecretFlags *out_flags);
+gboolean nm_vpn_service_plugin_get_secret_flags (GHashTable *data,
+                                                 const char *secret_name,
+                                                 NMSettingSecretFlags *out_flags);
 
 G_END_DECLS
 
-#endif /* __NM_VPN_PLUGIN_OLD_H__ */
+#endif /* __NM_VPN_SERVICE_PLUGIN_H__ */

po/POTFILES.in
@@ -129,6 +129,7 @@ libnm/nm-object.c
 libnm/nm-remote-connection.c
 libnm/nm-remote-settings.c
 libnm/nm-vpn-plugin-old.c
+libnm/nm-vpn-service-plugin.c
 policy/org.freedesktop.NetworkManager.policy.in.in
 src/NetworkManagerUtils.c
 src/main.c

commit d59c1d4c8aee9f3bee7e30eb7505aace62572fff

libnm,vpn: restore export of deprecated NMVpnPluginOld symbols
Deprecated of course, but shouldn't have been removed from the ABI.

Fixes: 867227dd4aac8371b3cacc366b32a3fa96eba42a

docs/libnm/Makefile.am
@@ -54,7 +54,6 @@ IGNORE_HFILES= \
 	nm-setting-private.h \
 	nm-types.h \
 	nm-utils-private.h \
-	nm-vpn-plugin-old.h \
 	nm-vpn-service-plugin.h \
 	nm-core-tests-enum-types.h
 

libnm/libnm.ver
@@ -821,6 +821,17 @@ global:
 	nm_vpn_plugin_error_get_type;
 	nm_vpn_plugin_error_quark;
 	nm_vpn_plugin_failure_get_type;
+	nm_vpn_plugin_old_disconnect;
+	nm_vpn_plugin_old_failure;
+	nm_vpn_plugin_old_get_connection;
+	nm_vpn_plugin_old_get_secret_flags;
+	nm_vpn_plugin_old_get_state;
+	nm_vpn_plugin_old_get_type;
+	nm_vpn_plugin_old_read_vpn_details;
+	nm_vpn_plugin_old_secrets_required;
+	nm_vpn_plugin_old_set_ip4_config;
+	nm_vpn_plugin_old_set_login_banner;
+	nm_vpn_plugin_old_set_state;
 	nm_vpn_service_state_get_type;
 	nm_wep_key_type_get_type;
 	nm_wimax_nsp_connection_valid;

libnm/nm-vpn-plugin-old.c
@@ -129,6 +129,11 @@ nm_vpn_plugin_old_get_connection (NMVpnPluginOld *plugin)
 	return connection;
 }
 
+/**
+ * nm_vpn_plugin_old_get_state:
+ *
+ * Deprecated: 1.2: replaced by NMVpnServicePlugin
+ */
 NMVpnServiceState
 nm_vpn_plugin_old_get_state (NMVpnPluginOld *plugin)
 {
@@ -137,6 +142,11 @@ nm_vpn_plugin_old_get_state (NMVpnPluginOld *plugin)
 	return NM_VPN_PLUGIN_OLD_GET_PRIVATE (plugin)->state;
 }
 
+/**
+ * nm_vpn_plugin_old_set_state:
+ *
+ * Deprecated: 1.2: replaced by NMVpnServicePlugin
+ */
 void
 nm_vpn_plugin_old_set_state (NMVpnPluginOld *plugin,
                              NMVpnServiceState state)
@@ -152,6 +162,11 @@ nm_vpn_plugin_old_set_state (NMVpnPluginOld *plugin,
 	}
 }
 
+/**
+ * nm_vpn_plugin_old_set_login_banner:
+ *
+ * Deprecated: 1.2: replaced by NMVpnServicePlugin
+ */
 void
 nm_vpn_plugin_old_set_login_banner (NMVpnPluginOld *plugin,
                                     const char *banner)
@@ -162,6 +177,11 @@ nm_vpn_plugin_old_set_login_banner (NMVpnPluginOld *plugin,
 	g_signal_emit (plugin, signals[LOGIN_BANNER], 0, banner);
 }
 
+/**
+ * nm_vpn_plugin_old_failure:
+ *
+ * Deprecated: 1.2: replaced by NMVpnServicePlugin
+ */
 void
 nm_vpn_plugin_old_failure (NMVpnPluginOld *plugin,
                            NMVpnPluginFailure reason)
@@ -171,6 +191,11 @@ nm_vpn_plugin_old_failure (NMVpnPluginOld *plugin,
 	g_signal_emit (plugin, signals[FAILURE], 0, reason);
 }
 
+/**
+ * nm_vpn_plugin_old_disconnect:
+ *
+ * Deprecated: 1.2: replaced by NMVpnServicePlugin
+ */
 gboolean
 nm_vpn_plugin_old_disconnect (NMVpnPluginOld *plugin, GError **err)
 {
@@ -277,6 +302,11 @@ schedule_fail_stop (NMVpnPluginOld *plugin)
 	priv->fail_stop_id = g_idle_add (fail_stop, plugin);
 }
 
+/**
+ * nm_vpn_plugin_old_set_config:
+ *
+ * Deprecated: 1.2: replaced by NMVpnServicePlugin
+ */
 void
 nm_vpn_plugin_old_set_config (NMVpnPluginOld *plugin,
                               GVariant *config)
@@ -316,6 +346,11 @@ nm_vpn_plugin_old_set_config (NMVpnPluginOld *plugin,
 	g_signal_emit (plugin, signals[CONFIG], 0, config);
 }
 
+/**
+ * nm_vpn_plugin_old_set_ip4_config:
+ *
+ * Deprecated: 1.2: replaced by NMVpnServicePlugin
+ */
 void
 nm_vpn_plugin_old_set_ip4_config (NMVpnPluginOld *plugin,
                                   GVariant *ip4_config)
@@ -371,6 +406,11 @@ nm_vpn_plugin_old_set_ip4_config (NMVpnPluginOld *plugin,
 		nm_vpn_plugin_old_set_state (plugin, NM_VPN_SERVICE_STATE_STARTED);
 }
 
+/**
+ * nm_vpn_plugin_old_set_ip6_config:
+ *
+ * Deprecated: 1.2: replaced by NMVpnServicePlugin
+ */
 void
 nm_vpn_plugin_old_set_ip6_config (NMVpnPluginOld *plugin,
                                   GVariant *ip6_config)