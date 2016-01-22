fb1da0eab211fc50eb9a39fa24c63f7fdd2d7fc4
Thomas Haller <thaller@redhat.com>
2016-01-22 16:29:36 +0100

    tests: allow fallback to dbus-launch in case dbus-run-session is not available
    Like on Ubuntu 12.04 which is currently used by our Travic CI setup.

--- tools/run-test-dbus-session.sh
+++ tools/run-test-dbus-session.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+if ! which dbus-run-session &>/dev/null; then
+    eval `dbus-launch --sh-syntax`
+    trap "kill $DBUS_SESSION_BUS_PID" EXIT
+    "$@"
+    exit $?
+fi
+
 dbus-run-session -- "$@"
