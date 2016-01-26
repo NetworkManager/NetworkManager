NetworkManager/NetworkManager

commit f2399a69760e8d14b91e523127eb187d6337530f

libnm-glib: fix leak in get_permissions_reply()

libnm-glib/nm-client.c
@@ -344,6 +344,8 @@ get_permissions_reply (DBusGProxy *proxy,
 	                       G_TYPE_INVALID);
 	NM_CLIENT_GET_PRIVATE (self)->perm_call = NULL;
 	update_permissions (NM_CLIENT (user_data), error ? NULL : permissions);
+	if (permissions)
+		g_hash_table_destroy (permissions);
 	g_clear_error (&error);
 }
 

commit 1deae2a284fa42a14443b59a2d8915c5b8b21425

libnm-glib: fix crash due to uninitalized pointer in get_permissions_reply()
If the D-Bus call failed with error, @permissions would stay uninitialized.

Fixes: f2399a69760e8d14b91e523127eb187d6337530f
Fixes: 808f0126035a09c946c559c9b1c464c4523a8a0e

(cherry picked from commit e0601d501a09d2ea24c7d9d966056cb8042a49b3)

libnm-glib/nm-client.c
@@ -331,7 +331,7 @@ get_permissions_reply (DBusGProxy *proxy,
                        gpointer user_data)
 {
 	NMClient *self = NM_CLIENT (user_data);
-	GHashTable *permissions;
+	GHashTable *permissions = NULL;
 	GError *error = NULL;
 
 	dbus_g_proxy_end_call (proxy, call, &error,