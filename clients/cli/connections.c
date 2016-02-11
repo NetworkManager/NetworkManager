NetworkManager/NetworkManager

commit e8927c14ec3e830099e1525fa8fee4c9a7fc5032

client: fix NULL dereference with no connections
Coverity complains:

CID 59386 (#1 of 1): Dereference before null check (REVERSE_INULL)
check_after_deref: Null-checking cons suggests that it may be null, but it has
already been dereferenced on all paths leading to the check.

clients/cli/connections.c
@@ -1532,11 +1532,14 @@ sort_connections (const GPtrArray *cons, NmCli *nmc, const GArray *order)
 	int i;
 	NmcSortInfo compare_info;
 
+	if (!cons)
+		return;
+
 	compare_info.nmc = nmc;
 	compare_info.order = order;
 
 	sorted = g_ptr_array_sized_new (cons->len);
-	for (i = 0; cons && i < cons->len; i++)
+	for (i = 0; i < cons->len; i++)
 		g_ptr_array_add (sorted, cons->pdata[i]);
 	g_ptr_array_sort_with_data (sorted, compare_connections, &compare_info);
 	return sorted;

commit 9e5c99465dec212ad108879572e6509df11ac08e

cli: fix compile error
 connections.c: In function ‘sort_connections’:
 connections.c:1536:3: error: ‘return’ with no value, in function returning non-void [-Werror=return-type]
    return;
    ^

Fixes: e8927c14ec3e830099e1525fa8fee4c9a7fc5032

clients/cli/connections.c
@@ -1533,7 +1533,7 @@ sort_connections (const GPtrArray *cons, NmCli *nmc, const GArray *order)
 	NmcSortInfo compare_info;
 
 	if (!cons)
-		return;
+		return NULL;
 
 	compare_info.nmc = nmc;
 	compare_info.order = order;