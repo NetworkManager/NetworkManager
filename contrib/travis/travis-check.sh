7ec5acdc66b396cd2623a4a99be08e0267d93489
Thomas Haller <thaller@redhat.com>
2016-02-12 16:54:30 +0100

    travis: fix setting NMTST_DEBUG for travis-check.sh script

--- contrib/travis/travis-check.sh
+++ contrib/travis/travis-check.sh
@@ -13,7 +13,7 @@ if ! make check -k ; then
     print_test_logs "first-test"
 
     echo ">>>> RUN SECOND TEST (start)"
-    NMTST_DEBUG=TRACE,no-expect-messages make check -k || :
+    NMTST_DEBUG=TRACE,no-expect-message make check -k || :
     echo ">>>> RUN SECOND TEST (done)"
 
     print_test_logs "second-test"
