7812c8d2dcf32039c413abc66b16b8fa19b637b6
Adrian Likins <alikins@redhat.com>
2016-03-02 11:25:43 -0500

    Fix missing commas in list-devices devtypes dict.

--- examples/python/dbus/list-devices.py
+++ examples/python/dbus/list-devices.py
@@ -35,11 +35,11 @@ devtypes = { 1: "Ethernet",
              12: "ADSL",
              13: "Bridge",
              14: "Generic",
-             15: "Team"
-             16: "TUN"
-             17: "IPTunnel"
-             18: "MACVLAN"
-             19: "VXLAN"
+             15: "Team",
+             16: "TUN",
+             17: "IPTunnel",
+             18: "MACVLAN",
+             19: "VXLAN",
              20: "Veth"
            }
 
