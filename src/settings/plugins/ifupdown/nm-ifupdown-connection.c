NetworkManager/NetworkManager

commit 0cb45af97761e47d97c8f76346f7f1bef46ce2cb

ifupdown: don't remove the object in its constructor
This is wrong an all the complexity of a property and associated bookkeeping is
completely unnecessary.

src/settings/plugins/ifupdown/nm-ifupdown-connection.c
@@ -36,27 +36,26 @@
 
 G_DEFINE_TYPE (NMIfupdownConnection, nm_ifupdown_connection, NM_TYPE_SETTINGS_CONNECTION)
 
-#define NM_IFUPDOWN_CONNECTION_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), NM_TYPE_IFUPDOWN_CONNECTION, NMIfupdownConnectionPrivate))
-
-typedef struct {
-	if_block *ifblock;
-} NMIfupdownConnectionPrivate;
-
-enum {
-	PROP_ZERO,
-	PROP_IFBLOCK,
-	_PROP_END,
-};
-
-
 NMIfupdownConnection*
 nm_ifupdown_connection_new (if_block *block)
 {
+	GObject *object;
+	GError *error = NULL;
+
 	g_return_val_if_fail (block != NULL, NULL);
 
-	return (NMIfupdownConnection *) g_object_new (NM_TYPE_IFUPDOWN_CONNECTION,
-										 NM_IFUPDOWN_CONNECTION_IFBLOCK, block,
-										 NULL);
+	object = g_object_new (NM_TYPE_IFUPDOWN_CONNECTION, NULL);
+
+	if (!ifupdown_update_connection_from_if_block (NM_CONNECTION (object), priv->ifblock, &error)) {
+		nm_log_warn (LOGD_SETTINGS, "%s.%d - invalid connection read from /etc/network/interfaces: %s",
+		             __FILE__,
+		             __LINE__,
+		             error->message);
+		g_object_unref (object);
+		return NULL;
+	}
+
+	return (NMIfupdownConnection *) object;
 }
 
 static gboolean
@@ -72,97 +71,11 @@ nm_ifupdown_connection_init (NMIfupdownConnection *connection)
 {
 }
 
-static GObject *
-constructor (GType type,
-		   guint n_construct_params,
-		   GObjectConstructParam *construct_params)
-{
-	GObject *object;
-	NMIfupdownConnectionPrivate *priv;
-	GError *error = NULL;
-
-	object = G_OBJECT_CLASS (nm_ifupdown_connection_parent_class)->constructor (type, n_construct_params, construct_params);
-	g_return_val_if_fail (object, NULL);
-
-	priv = NM_IFUPDOWN_CONNECTION_GET_PRIVATE (object);
-	if (!priv) {
-		nm_log_warn (LOGD_SETTINGS, "%s.%d - no private instance.", __FILE__, __LINE__);
-		goto err;
-	}
-	if (!priv->ifblock) {
-		nm_log_warn (LOGD_SETTINGS, "(ifupdown) ifblock not provided to constructor.");
-		goto err;
-	}
-
-	if (!ifupdown_update_connection_from_if_block (NM_CONNECTION (object), priv->ifblock, &error)) {
-		nm_log_warn (LOGD_SETTINGS, "%s.%d - invalid connection read from /etc/network/interfaces: %s",
-		             __FILE__,
-		             __LINE__,
-		             error->message);
-		goto err;
-	}
-
-	return object;
-
- err:
-	g_object_unref (object);
-	return NULL;
-}
-
-static void
-set_property (GObject *object, guint prop_id,
-		    const GValue *value, GParamSpec *pspec)
-{
-	NMIfupdownConnectionPrivate *priv = NM_IFUPDOWN_CONNECTION_GET_PRIVATE (object);
-	g_return_if_fail (priv);
-
-	switch (prop_id) {
-	case PROP_IFBLOCK:
-		priv->ifblock = g_value_get_pointer (value);
-		break;
-	default:
-		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-		break;
-	}
-}
-
-static void
-get_property (GObject *object, guint prop_id,
-		    GValue *value, GParamSpec *pspec)
-{
-	NMIfupdownConnectionPrivate *priv = NM_IFUPDOWN_CONNECTION_GET_PRIVATE (object);
-	g_return_if_fail (priv);
-
-	switch (prop_id) {
-	case PROP_IFBLOCK:
-		g_value_set_pointer (value, priv->ifblock);
-		break;
-	default:
-		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-		break;
-	}
-}
-
 static void
 nm_ifupdown_connection_class_init (NMIfupdownConnectionClass *ifupdown_connection_class)
 {
-	GObjectClass *object_class = G_OBJECT_CLASS (ifupdown_connection_class);
 	NMSettingsConnectionClass *connection_class = NM_SETTINGS_CONNECTION_CLASS (ifupdown_connection_class);
 
-	g_type_class_add_private (ifupdown_connection_class, sizeof (NMIfupdownConnectionPrivate));
-
-	/* Virtual methods */
-	object_class->constructor  = constructor;
-	object_class->set_property = set_property;
-	object_class->get_property = get_property;
-
 	connection_class->supports_secrets = supports_secrets;
-
-	/* Properties */
-	g_object_class_install_property
-		(object_class, PROP_IFBLOCK,
-		 g_param_spec_pointer (NM_IFUPDOWN_CONNECTION_IFBLOCK, "", "",
-		                       G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY |
-		                       G_PARAM_STATIC_STRINGS));
 }
 

src/settings/plugins/ifupdown/nm-ifupdown-connection.h
@@ -37,8 +37,6 @@ G_BEGIN_DECLS
 #define NM_IS_IFUPDOWN_CONNECTION_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), NM_TYPE_IFUPDOWN_CONNECTION))
 #define NM_IFUPDOWN_CONNECTION_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), NM_TYPE_IFUPDOWN_CONNECTION, NMIfupdownConnectionClass))
 
-#define NM_IFUPDOWN_CONNECTION_IFBLOCK "ifblock"
-
 typedef struct {
 	NMSettingsConnection parent;
 } NMIfupdownConnection;

commit e4aaa48b8297359987a02e5bab9e119939237638

ifupdown: fix up a bad rebase
Fixes: 0cb45af97761e47d97c8f76346f7f1bef46ce2cb

src/settings/plugins/ifupdown/nm-ifupdown-connection.c
@@ -46,7 +46,7 @@ nm_ifupdown_connection_new (if_block *block)
 
 	object = g_object_new (NM_TYPE_IFUPDOWN_CONNECTION, NULL);
 
-	if (!ifupdown_update_connection_from_if_block (NM_CONNECTION (object), priv->ifblock, &error)) {
+	if (!ifupdown_update_connection_from_if_block (NM_CONNECTION (object), block, &error)) {
 		nm_log_warn (LOGD_SETTINGS, "%s.%d - invalid connection read from /etc/network/interfaces: %s",
 		             __FILE__,
 		             __LINE__,