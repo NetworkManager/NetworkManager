NetworkManager/NetworkManager

commit 8b2abe0e2c8f162544a2562fc18a7becbc55d233

device: check avaliable connections are really available
The nm_device_check_connection_available() call seem to have been accidentally
removed from nm_device_recheck_available_connections() resulting in all
connections always being added.

Fixes 02ec76df5aaa3a3ad197cb1d53c0388029775b07

src/devices/nm-device.c
@@ -9354,10 +9354,15 @@ nm_device_recheck_available_connections (NMDevice *self)
 		for (iter = connections; iter; iter = g_slist_next (iter)) {
 			connection = NM_CONNECTION (iter->data);
 
-			if (available_connections_add (self, connection)) {
-				if (prune_list)
-					g_hash_table_remove (prune_list, connection);
-				changed = TRUE;
+			if (nm_device_check_connection_available (self,
+				                                  connection,
+				                                  NM_DEVICE_CHECK_CON_AVAILABLE_NONE,
+				                                  NULL)) {
+				if (available_connections_add (self, connection))
+					changed = TRUE;
+			} else {
+				if (prune_list && g_hash_table_remove (prune_list, connection))
+					changed = TRUE;
 			}
 		}
 

commit ca5da5d80f3a21847bb807021e2e40d25bca50d0

device: fix handling of available connections
The prune list is for elements that must be deleted from the list of
available connections. So, when processing all the existing
connections an element must be deleted from the prune list iff it's
available.

Fixes: 8b2abe0e2c8f162544a2562fc18a7becbc55d233

https://bugzilla.redhat.com/show_bug.cgi?id=1316488

src/devices/nm-device.c
@@ -9664,14 +9664,13 @@ nm_device_recheck_available_connections (NMDevice *self)
 			connection = NM_CONNECTION (iter->data);
 
 			if (nm_device_check_connection_available (self,
-				                                  connection,
-				                                  NM_DEVICE_CHECK_CON_AVAILABLE_NONE,
-				                                  NULL)) {
+			                                          connection,
+			                                          NM_DEVICE_CHECK_CON_AVAILABLE_NONE,
+			                                          NULL)) {
 				if (available_connections_add (self, connection))
 					changed = TRUE;
-			} else {
-				if (prune_list && g_hash_table_remove (prune_list, connection))
-					changed = TRUE;
+				if (prune_list)
+					g_hash_table_remove (prune_list, connection);
 			}
 		}
 