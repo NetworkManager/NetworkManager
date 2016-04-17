NetworkManager/NetworkManager

commit 10c3e1180485679895da6c7cd8ba14f2bd934c3c

rdisc: downgrade logging severity and rate-limit logging for sending router solicitation
Downgrade the severity for failure to send router solicitations
to WARN level. Also, repeated (identical) lines will be further
downgraded to DEBUG level.

Also pass the error reason to the caller and combine the message with
the "sending router solicitation" message.

https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=775736
https://bugzilla.redhat.com/show_bug.cgi?id=1034443
https://bugzilla.redhat.com/show_bug.cgi?id=1010540
https://lists.launchpad.net/touch-packages/msg75697.html

https://bugzilla.gnome.org/show_bug.cgi?id=759596
(cherry picked from commit 21efcfe6b57ffc36e0e7a89ee4dcb10b35ee6827)

src/rdisc/nm-rdisc.c
@@ -38,6 +38,7 @@ typedef struct {
 	gint64 last_rs;
 	guint ra_timeout_id;  /* first RA timeout */
 	guint timeout_id;   /* prefix/dns/etc lifetime timeout */
+	char *last_send_rs_error;
 } NMRDiscPrivate;
 
 #define NM_RDISC_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), NM_TYPE_RDISC, NMRDiscPrivate))
@@ -280,11 +281,25 @@ send_rs (NMRDisc *rdisc)
 {
 	NMRDiscClass *klass = NM_RDISC_GET_CLASS (rdisc);
 	NMRDiscPrivate *priv = NM_RDISC_GET_PRIVATE (rdisc);
+	GError *error = NULL;
 
-	debug ("(%s): sending router solicitation", rdisc->ifname);
-
-	if (klass->send_rs (rdisc))
+	if (klass->send_rs (rdisc, &error)) {
+		debug ("(%s): router solicitation sent", rdisc->ifname);
 		priv->solicitations_left--;
+		g_clear_pointer (&priv->last_send_rs_error, g_free);
+	} else {
+		gboolean different_message;
+
+		different_message = g_strcmp0 (priv->last_send_rs_error, error->message) != 0;
+		nm_log (different_message ? LOGL_WARN : LOGL_DEBUG, LOGD_IP6,
+		        "(%s): failure sending router solicitation: %s",
+		        rdisc->ifname, error->message);
+		if (different_message) {
+			g_clear_pointer (&priv->last_send_rs_error, g_free);
+			priv->last_send_rs_error = g_strdup (error->message);
+		}
+		g_clear_error (&error);
+	}
 
 	priv->last_rs = nm_utils_get_monotonic_timestamp_s ();
 	if (priv->solicitations_left > 0) {
@@ -581,8 +596,11 @@ timeout_cb (gpointer user_data)
 void
 nm_rdisc_ra_received (NMRDisc *rdisc, guint32 now, NMRDiscConfigMap changed)
 {
+	NMRDiscPrivate *priv = NM_RDISC_GET_PRIVATE (rdisc);
+
 	clear_ra_timeout (rdisc);
 	clear_rs_timeout (rdisc);
+	g_clear_pointer (&priv->last_send_rs_error, g_free);
 	check_timestamps (rdisc, now, changed);
 }
 
@@ -621,6 +639,7 @@ dispose (GObject *object)
 
 	clear_ra_timeout (rdisc);
 	clear_rs_timeout (rdisc);
+	g_clear_pointer (&priv->last_send_rs_error, g_free);
 
 	if (priv->timeout_id) {
 		g_source_remove (priv->timeout_id);

src/rdisc/nm-fake-rdisc.c
@@ -236,7 +236,7 @@ nm_fake_rdisc_done (NMFakeRDisc *self)
 /******************************************************************/
 
 static gboolean
-send_rs (NMRDisc *rdisc)
+send_rs (NMRDisc *rdisc, GError **error)
 {
 	g_signal_emit (rdisc, signals[RS_SENT], 0);
 	return TRUE;

src/rdisc/nm-lndp-rdisc.c
@@ -52,20 +52,28 @@ G_DEFINE_TYPE (NMLNDPRDisc, nm_lndp_rdisc, NM_TYPE_RDISC)
 /******************************************************************/
 
 static gboolean
-send_rs (NMRDisc *rdisc)
+send_rs (NMRDisc *rdisc, GError **error)
 {
 	NMLNDPRDiscPrivate *priv = NM_LNDP_RDISC_GET_PRIVATE (rdisc);
 	struct ndp_msg *msg;
-	int error;
+	int errsv;
 
-	error = ndp_msg_new (&msg, NDP_MSG_RS);
-	g_assert (!error);
+	errsv = ndp_msg_new (&msg, NDP_MSG_RS);
+	if (!errsv) {
+		errsv = errsv > 0 ? errsv : -errsv;
+		g_set_error_literal (error, NM_UTILS_ERROR, NM_UTILS_ERROR_UNKNOWN,
+		                     "cannot create router solicitation");
+		return FALSE;
+	}
 	ndp_msg_ifindex_set (msg, rdisc->ifindex);
 
-	error = ndp_msg_send (priv->ndp, msg);
+	errsv = ndp_msg_send (priv->ndp, msg);
 	ndp_msg_destroy (msg);
-	if (error) {
-		error ("(%s): cannot send router solicitation: %d.", rdisc->ifname, error);
+	if (errsv) {
+		errsv = errsv > 0 ? errsv : -errsv;
+		g_set_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_UNKNOWN,
+		             "%s (%d)",
+		             g_strerror (errsv), errsv);
 		return FALSE;
 	}
 

src/rdisc/nm-rdisc.h
@@ -133,7 +133,7 @@ typedef struct {
 	GObjectClass parent;
 
 	void (*start) (NMRDisc *rdisc);
-	gboolean (*send_rs) (NMRDisc *rdisc);
+	gboolean (*send_rs) (NMRDisc *rdisc, GError **error);
 	void (*config_changed) (NMRDisc *rdisc, NMRDiscConfigMap changed);
 	void (*ra_process) (NMRDisc *rdisc);
 	void (*ra_timeout) (NMRDisc *rdisc);

commit 868365180c8574dcbd14302fb0919848cae4dcc4

rdisc: fix build failure due to lack of g_clear_pointer()
Fixes: 10c3e1180485679895da6c7cd8ba14f2bd934c3c

src/rdisc/nm-rdisc.c
@@ -24,6 +24,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+#include "nm-glib-compat.h"
 #include "nm-rdisc.h"
 #include "nm-rdisc-private.h"
 