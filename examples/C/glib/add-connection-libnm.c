8fa4375e9ed45883d299ddaae4ed13f5333cde84
Beniamino Galvani <bgalvani@redhat.com>
2016-04-20 07:46:27 +0200

    examples: fix crash in add-connection-libnm
    Reported-by: Ali Nematollahi <alirezan1@gmail.com>
    (cherry picked from commit 76348adb8ea4f97497442d13c86f838332f035f0)

--- examples/C/glib/add-connection-libnm.c
+++ examples/C/glib/add-connection-libnm.c
@@ -37,7 +37,7 @@ added_cb (GObject *client,
 {
 	GMainLoop *loop = user_data;
 	NMRemoteConnection *remote;
-	GError *error;
+	GError *error = NULL;
 
 	/* NM responded to our request; either handle the resulting error or
 	 * print out the object path of the connection we just added.
