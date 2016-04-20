NetworkManager/NetworkManager

commit 9498ea507eb1d5042736c4351337e91b9c13bdf6

manager: ensure IP config of device is cleared before removal
If the manager removes the device, the IP config objects must
be cleared. The reason is that NMPolicy registers to the IP config
changed signal and passes these object on to NMDnsManager.

If the INTERNAL_DEVICE_REMOVED signal is emited with IP configuration
object pending, those objects will be leaked.

This partly redoes commit f72816bf108cf3fa52ae267e3622a1f19db28b21,
which was reverted.

Co-Authored-By: Thomas Haller <thaller@redhat.com>

https://bugzilla.gnome.org/show_bug.cgi?id=764483

src/devices/nm-device.c
@@ -2585,6 +2585,18 @@ nm_device_removed (NMDevice *self)
 		 * Release the slave from master, but don't touch the device. */
 		nm_device_master_release_one_slave (priv->master, self, FALSE, NM_DEVICE_STATE_REASON_CONNECTION_ASSUMED);
 	}
+
+	/* Clean up IP configs; this does not actually deconfigure the
+	 * interface, it just clears the configuration to which policy
+	 * is reacting via NM_DEVICE_IP4_CONFIG_CHANGED/NM_DEVICE_IP6_CONFIG_CHANGED
+	 * signal. As NMPolicy registered the NMIPxConfig instances in NMDnsManager,
+	 * these would be leaked otherwise. */
+	priv->default_route.v4_has = FALSE;
+	priv->default_route.v4_is_assumed = TRUE;
+	nm_device_set_ip4_config (self, NULL, 0, FALSE, FALSE, NULL);
+	priv->default_route.v6_has = FALSE;
+	priv->default_route.v6_is_assumed = TRUE;
+	nm_device_set_ip6_config (self, NULL, FALSE, FALSE, NULL);
 }
 
 static gboolean

src/nm-manager.c
@@ -869,9 +869,10 @@ remove_device (NMManager *self,
 	priv->devices = g_slist_remove (priv->devices, device);
 
 	if (nm_device_is_real (device)) {
+		nm_device_removed (device);
+
 		g_signal_emit (self, signals[DEVICE_REMOVED], 0, device);
 		_notify (self, PROP_DEVICES);
-		nm_device_removed (device);
 	}
 	g_signal_emit (self, signals[INTERNAL_DEVICE_REMOVED], 0, device);
 	_notify (self, PROP_ALL_DEVICES);

commit 2158d6a5a868ae0d274c45b05425f76ba3e677d7

manager: fix wrongly removing DNS configuration on shutdown
When NetworkManager exits, it must preserve the DNS configuration of
devices that are left up.

Fixes: 9498ea507eb1d5042736c4351337e91b9c13bdf6

src/devices/nm-device.c
@@ -2643,12 +2643,14 @@ nm_device_get_enslaved (NMDevice *self)
 /**
  * nm_device_removed:
  * @self: the #NMDevice
+ * @unconfigure_ip_config: whether to clear the IP config objects
+ *   of the device (provided, it is still not cleared at this point).
  *
  * Called by the manager when the device was removed. Releases the device from
  * the master in case it's enslaved.
  */
 void
-nm_device_removed (NMDevice *self)
+nm_device_removed (NMDevice *self, gboolean unconfigure_ip_config)
 {
 	NMDevicePrivate *priv;
 
@@ -2660,6 +2662,9 @@ nm_device_removed (NMDevice *self)
 		 * Release the slave from master, but don't touch the device. */
 		nm_device_master_release_one_slave (priv->master, self, FALSE, NM_DEVICE_STATE_REASON_CONNECTION_ASSUMED);
 	}
+
+	if (!unconfigure_ip_config)
+		return;
 
 	/* Clean up IP configs; this does not actually deconfigure the
 	 * interface, it just clears the configuration to which policy

src/devices/nm-device.h
@@ -385,7 +385,7 @@ gboolean        nm_device_has_unmodified_applied_connection (NMDevice *self,
                                                              NMSettingCompareFlags compare_flags);
 NMSetting *     nm_device_get_applied_setting   (NMDevice *dev, GType setting_type);
 
-void            nm_device_removed               (NMDevice *dev);
+void            nm_device_removed               (NMDevice *self, gboolean unconfigure_ip_config);
 
 gboolean        nm_device_is_available          (NMDevice *dev, NMDeviceCheckDevAvailableFlags flags);
 gboolean        nm_device_has_carrier           (NMDevice *dev);

src/nm-manager.c
@@ -829,13 +829,13 @@ remove_device (NMManager *self,
                gboolean allow_unmanage)
 {
 	NMManagerPrivate *priv = NM_MANAGER_GET_PRIVATE (self);
+	gboolean unmanage = FALSE;
 
 	_LOGD (LOGD_DEVICE, "(%s): removing device (allow_unmanage %d, managed %d)",
 	       nm_device_get_iface (device), allow_unmanage, nm_device_get_managed (device, FALSE));
 
 	if (allow_unmanage && nm_device_get_managed (device, FALSE)) {
 		NMActRequest *req = nm_device_get_act_request (device);
-		gboolean unmanage = FALSE;
 
 		/* Leave activated interfaces up when quitting so their configuration
 		 * can be taken over when NM restarts.  This ensures connectivity while
@@ -865,7 +865,18 @@ remove_device (NMManager *self,
 	priv->devices = g_slist_remove (priv->devices, device);
 
 	if (nm_device_is_real (device)) {
-		nm_device_removed (device);
+		gboolean unconfigure_ip_config = !quitting || unmanage;
+
+		/* When we don't unmanage the device on shutdown, we want to preserve the DNS
+		 * configuration in resolv.conf. For that, we must leak the configuration
+		 * in NMPolicy/NMDnsManager. We do that, by emitting the device-removed signal
+		 * with device's ip-config object still uncleared. In that case, NMPolicy
+		 * never learns to unconfigure the ip-config objects and does not remove them
+		 * from DNS on shutdown (which is ugly, because we don't cleanup the memory
+		 * properly).
+		 *
+		 * Control that by passing @unconfigure_ip_config.  */
+		nm_device_removed (device, unconfigure_ip_config);
 
 		g_signal_emit (self, signals[DEVICE_REMOVED], 0, device);
 		_notify (self, PROP_DEVICES);