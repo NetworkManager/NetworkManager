NetworkManager/NetworkManager

commit 3adf782ab556e343a34f0153f50f72a2fd0ee791

libnm/vpn: consider VPN service aliases in nm_vpn_plugin_info_new_search_file()
Fixes: 4271c9650c1cfcbd487cc471099b1c0c9bbfa290

libnm-core/nm-vpn-plugin-info.c
@@ -394,11 +394,21 @@ nm_vpn_plugin_info_new_search_file (const char *name, const char *service)
 		infos = _nm_vpn_plugin_info_list_load_dir (dir[i], TRUE, uid, NULL, NULL);
 
 		for (info = infos; info; info = info->next) {
-			if (   (!name || nm_streq (nm_vpn_plugin_info_get_name (info->data), name))
-			    && (!service || nm_streq (nm_vpn_plugin_info_get_service (info->data), service))) {
-				plugin_info = g_object_ref (info->data);
-				break;
+			NMVpnPluginInfo *p = info->data;
+			char **aliases;
+
+			if (name && !nm_streq (nm_vpn_plugin_info_get_name (p), name))
+				continue;
+			if (service) {
+				if (!nm_streq (nm_vpn_plugin_info_get_service (p), service))
+					continue;
+
+				aliases = NM_VPN_PLUGIN_INFO_GET_PRIVATE (p)->aliases;
+				if (_nm_utils_strv_find_first (aliases, -1, service) < 0)
+					continue;
 			}
+			plugin_info = g_object_ref (p);
+			break;
 		}
 
 		g_slist_free_full (infos, g_object_unref);

commit b52485b34b0d94e97a4d64a71a4448378eb43053

libnm/vpn: fix checking service name in nm_vpn_plugin_info_new_search_file()
Fixes: 3adf782ab556e343a34f0153f50f72a2fd0ee791

libnm-core/nm-vpn-plugin-info.c
@@ -395,18 +395,14 @@ nm_vpn_plugin_info_new_search_file (const char *name, const char *service)
 
 		for (info = infos; info; info = info->next) {
 			NMVpnPluginInfo *p = info->data;
-			char **aliases;
 
 			if (name && !nm_streq (nm_vpn_plugin_info_get_name (p), name))
 				continue;
-			if (service) {
-				if (!nm_streq (nm_vpn_plugin_info_get_service (p), service))
-					continue;
-
-				aliases = NM_VPN_PLUGIN_INFO_GET_PRIVATE (p)->aliases;
-				if (_nm_utils_strv_find_first (aliases, -1, service) < 0)
-					continue;
-			}
+			if (   service
+			    && !nm_streq (nm_vpn_plugin_info_get_service (p), service)
+			    && (_nm_utils_strv_find_first (NM_VPN_PLUGIN_INFO_GET_PRIVATE (p)->aliases,
+			                                   -1, service) < 0))
+				continue;
 			plugin_info = g_object_ref (p);
 			break;
 		}