69ab984fed80c70d1937684ee1884bd53ca6551f
Beniamino Galvani <bgalvani@redhat.com>
2016-05-05 11:22:14 +0200

    sleep-monitor: add functions for delaying the suspension
    To allow the execution of asynchronous actions before the system is
    suspended, add a mechanism for delaying the drop of inhibitor lock.
    Clients can supend the suspension by calling inhibit_take() in their
    handler for SLEEPING signal and use inhibit_release() later when they
    are done.
    
    We take a "delay" type inhibitor lock, which means that the system
    will proceed anyway after a certain amount of time.
    
    Co-Authored-By: Thomas Haller <thaller@redhat.com>

--- src/nm-sleep-monitor.h
+++ src/nm-sleep-monitor.h
@@ -36,6 +36,12 @@ typedef struct _NMSleepMonitorClass    NMSleepMonitorClass;
 GType           nm_sleep_monitor_get_type     (void) G_GNUC_CONST;
 NMSleepMonitor *nm_sleep_monitor_new          (void);
 
+typedef struct _NMSleepMonitorInhibitorHandle NMSleepMonitorInhibitorHandle;
+
+NMSleepMonitorInhibitorHandle *nm_sleep_monitor_inhibit_take    (NMSleepMonitor *self);
+void                           nm_sleep_monitor_inhibit_release (NMSleepMonitor *self,
+                                                                 NMSleepMonitorInhibitorHandle *handle);
+
 G_END_DECLS
 
 #endif /* __NETWORKMANAGER_SLEEP_MONITOR_H__ */
