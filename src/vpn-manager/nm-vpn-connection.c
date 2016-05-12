NetworkManager/NetworkManager

commit 6c12f04e87b439832bdbac57bc8bd85e369a0bc9

vpn-connection: do not leak the connection if there's no timeout

src/vpn-manager/nm-vpn-connection.c
@@ -1945,7 +1945,7 @@ nm_vpn_service_daemon_exec (NMVpnConnection *self, GError **error)
 
 	if (success) {
 		_LOGI ("Started the VPN service, PID %ld", (long int) pid);
-		priv->start_timeout = g_timeout_add_seconds (5, _daemon_exec_timeout, g_object_ref (self));
+		priv->start_timeout = g_timeout_add_seconds (5, _daemon_exec_timeout, self);
 	} else {
 		g_set_error (error,
 		             NM_MANAGER_ERROR, NM_MANAGER_ERROR_FAILED,
@@ -2400,6 +2400,8 @@ dispose (GObject *object)
 {
 	NMVpnConnection *self = NM_VPN_CONNECTION (object);
 	NMVpnConnectionPrivate *priv = NM_VPN_CONNECTION_GET_PRIVATE (self);
+
+	nm_clear_g_source (&priv->start_timeout);
 
 	g_clear_pointer (&priv->connect_hash, g_variant_unref);
 

commit 1ec5e0b9584a398333e5522e053f77d941fd5c4f

vpn: fix crash when VPN service times-out to start
Previously, we took a reference when scheduling _daemon_exec_timeout().
That was changed, but we still wrongly unref'ed the VPN connection
on timeout.

Related: https://bugzilla.gnome.org/show_bug.cgi?id=766307

Fixes: 6c12f04e87b439832bdbac57bc8bd85e369a0bc9

src/vpn-manager/nm-vpn-connection.c
@@ -1916,9 +1916,6 @@ _daemon_exec_timeout (gpointer data)
 	_LOGW ("Timed out waiting for the service to start");
 	priv->start_timeout = 0;
 	nm_vpn_connection_disconnect (self, NM_VPN_CONNECTION_STATE_REASON_SERVICE_START_TIMEOUT, FALSE);
-
-	g_object_unref (self);
-
 	return G_SOURCE_REMOVE;
 }
 