5ca97439b9d03b8dff691bfa7242ec133d9fea15
Atul Anand <atulhjp@gmail.com>
2016-05-12 19:51:07 +0530

    ProxyConfig created in nm_dhcp_client_handle_event ()

--- src/dhcp-manager/nm-dhcp-client.c
+++ src/dhcp-manager/nm-dhcp-client.c
@@ -716,6 +716,7 @@ nm_dhcp_client_handle_event (gpointer unused,
 	guint32 old_state;
 	guint32 new_state;
 	GHashTable *str_options = NULL;
+	GObject *proxy_config = NULL;
 	GObject *ip_config = NULL;
 
 	g_return_val_if_fail (NM_IS_DHCP_CLIENT (self), FALSE);
@@ -749,6 +750,13 @@ nm_dhcp_client_handle_event (gpointer unused,
 			g_variant_unref (value);
 		}
 
+		/* Create the Proxy Config */
+		g_warn_if_fail (g_hash_table_size (str_options));
+		if (g_hash_table_size (str_options))
+				proxy_config = (GObject *) nm_dhcp_utils_proxy_config_from_options (str_options);
+		if (proxy_config == NULL) 
+			_LOGW ("PAC url not received");
+
 		/* Create the IP config */
 		g_warn_if_fail (g_hash_table_size (str_options));
 		if (g_hash_table_size (str_options)) {
@@ -778,6 +786,7 @@ nm_dhcp_client_handle_event (gpointer unused,
 
 	if (str_options)
 		g_hash_table_destroy (str_options);
+	g_clear_object (&proxy_config);
 	g_clear_object (&ip_config);
 
 	return TRUE;
