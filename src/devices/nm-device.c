NetworkManager/NetworkManager

commit adbba0fb395b9910c4615e7be91ffe5b3ef70d6a

device: assertion object in NM_DEVICE_GET_PRIVATE()

src/devices/nm-device.c
@@ -72,7 +72,18 @@ _LOG_DECLARE_SELF (NMDevice);
 
 G_DEFINE_ABSTRACT_TYPE (NMDevice, nm_device, NM_TYPE_EXPORTED_OBJECT)
 
-#define NM_DEVICE_GET_PRIVATE(o) ((o)->priv)
+#define NM_DEVICE_GET_PRIVATE(o) \
+	({ \
+		/* preserve the const-ness of self. Unfortunately, that
+		 * way, @self cannot be a void pointer */ \
+		typeof (self) _self = (self); \
+		\
+		/* Get compiler error if variable is of wrong type */ \
+		_nm_unused const NMDevice *_self2 = (_self); \
+		\
+		nm_assert (NM_IS_DEVICE (_self)); \
+		_self->priv; \
+	})
 
 enum {
 	STATE_CHANGED,

commit 52819b7c94fa96ed184130e4e9a83f34ed91f607

device: fix NM_DEVICE_GET_PRIVATE() macro
Fixes: adbba0fb395b9910c4615e7be91ffe5b3ef70d6a

src/devices/nm-device.c
@@ -73,7 +73,7 @@ _LOG_DECLARE_SELF (NMDevice);
 
 G_DEFINE_ABSTRACT_TYPE (NMDevice, nm_device, NM_TYPE_EXPORTED_OBJECT)
 
-#define NM_DEVICE_GET_PRIVATE(o) \
+#define NM_DEVICE_GET_PRIVATE(self) \
 	({ \
 		/* preserve the const-ness of self. Unfortunately, that
 		 * way, @self cannot be a void pointer */ \