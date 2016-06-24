df48628a483f286f6e52e2d7d3cffe4fa31c4299
Beniamino Galvani <bgalvani@redhat.com>
2016-06-24 09:25:51 +0200

    vpn: don't merge DNS properties into parent device's configuration
    DNS properties should not be copied to parent device's configuration
    otherwise they will be applied twice, possibly with two different DNS
    priorities.

--- src/vpn-manager/nm-vpn-connection.c
+++ src/vpn-manager/nm-vpn-connection.c
@@ -1025,12 +1025,11 @@ apply_parent_device_config (NMVpnConnection *self)
 		ifindex = nm_device_get_ip_ifindex (parent_dev);
 		if (priv->ip4_config) {
 			vpn4_parent_config = nm_ip4_config_new (ifindex);
-			nm_ip4_config_merge (vpn4_parent_config, priv->ip4_config, NM_IP_CONFIG_MERGE_DEFAULT);
-			nm_ip4_config_unset_gateway (vpn4_parent_config);
+			nm_ip4_config_merge (vpn4_parent_config, priv->ip4_config, NM_IP_CONFIG_MERGE_NO_DNS);
 		}
 		if (priv->ip6_config) {
 			vpn6_parent_config = nm_ip6_config_new (ifindex);
-			nm_ip6_config_merge (vpn6_parent_config, priv->ip6_config, NM_IP_CONFIG_MERGE_DEFAULT);
+			nm_ip6_config_merge (vpn6_parent_config, priv->ip6_config, NM_IP_CONFIG_MERGE_NO_DNS);
 			nm_ip6_config_set_gateway (vpn6_parent_config, NULL);
 		}
 	}
