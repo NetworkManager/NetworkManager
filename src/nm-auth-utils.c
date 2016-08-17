NetworkManager/NetworkManager

commit 1cf35cb26b6cc04f8b2c51c3cde4bc08ef311062

core: final gdbus porting
Port remaining bits to gdbus and remove stray dbus-glib references

Drop the dbus-glib version check from configure, since nothing depends
on new dbus-glib any more.

Move nm-dbus-glib-types.h and nm-gvaluearray-compat.h from include/ to
libnm-util/ since they are now only used by libnm-util and libnm-glib.

src/nm-auth-utils.c
@@ -34,7 +34,7 @@ struct NMAuthChain {
 	GSList *calls;
 	GHashTable *data;
 
-	DBusGMethodInvocation *context;
+	GDBusMethodInvocation *context;
 	NMAuthSubject *subject;
 	GError *error;
 
@@ -83,7 +83,7 @@ auth_chain_finish (gpointer user_data)
 
 /* Creates the NMAuthSubject automatically */
 NMAuthChain *
-nm_auth_chain_new_context (DBusGMethodInvocation *context,
+nm_auth_chain_new_context (GDBusMethodInvocation *context,
                            NMAuthChainResultFunc done_func,
                            gpointer user_data)
 {
@@ -107,7 +107,7 @@ nm_auth_chain_new_context (DBusGMethodInvocation *context,
 /* Requires an NMAuthSubject */
 NMAuthChain *
 nm_auth_chain_new_subject (NMAuthSubject *subject,
-                           DBusGMethodInvocation *context,
+                           GDBusMethodInvocation *context,
                            NMAuthChainResultFunc done_func,
                            gpointer user_data)
 {

.gitignore
@@ -31,7 +31,6 @@ cscope.*out
 valgrind-*.log
 test-*.log
 test-*.trs
-*-glue.h
 
 /ABOUT-NLS
 /COPYING
@@ -156,6 +155,8 @@ test-*.trs
 /libnm-core/tests/test-setting-8021x
 /libnm-core/tests/test-setting-dcb
 
+/libnm-glib/nm-secret-agent-glue.h
+/libnm-glib/nm-vpn-plugin-glue.h
 /libnm-glib/libnm-glib-test
 /libnm-glib/tests/test-nm-client
 /libnm-glib/tests/test-remote-settings-client

callouts/Makefile.am
@@ -5,7 +5,6 @@ AM_CPPFLAGS = \
 	-I${top_srcdir}/libnm-core \
 	-I${top_builddir}/libnm-core \
 	$(GLIB_CFLAGS) \
-	$(DBUS_CFLAGS) \
 	-DNETWORKMANAGER_COMPILATION \
 	-DNM_VERSION_MAX_ALLOWED=NM_VERSION_NEXT_STABLE \
 	-DNMCONFDIR=\"$(nmconfdir)\" \

callouts/tests/Makefile.am
@@ -9,8 +9,7 @@ AM_CPPFLAGS = \
 	-DNETWORKMANAGER_COMPILATION \
 	-DNM_VERSION_MAX_ALLOWED=NM_VERSION_NEXT_STABLE \
 	-DSRCDIR=\"$(abs_srcdir)\" \
-	$(GLIB_CFLAGS) \
-	$(DBUS_CFLAGS)
+	$(GLIB_CFLAGS)
 
 noinst_PROGRAMS = \
 	test-dispatcher-envp
@@ -23,8 +22,7 @@ test_dispatcher_envp_SOURCES = \
 test_dispatcher_envp_LDADD = \
 	$(top_builddir)/libnm/libnm.la \
 	$(top_builddir)/callouts/libtest-dispatcher-envp.la \
-	$(GLIB_LIBS) \
-	$(DBUS_LIBS)
+	$(GLIB_LIBS)
 
 ###########################################
 

configure.ac
@@ -273,20 +273,6 @@ dnl Checks for dbus-glib
 dnl
 PKG_CHECK_MODULES(DBUS, dbus-1 >= 1.1 dbus-glib-1 >= 0.94)
 
-AC_CHECK_LIB([dbus-glib-1], [dbus_g_method_invocation_get_g_connection], ac_have_gmi_get_con="1", ac_have_gmi_get_con="0")
-AC_DEFINE_UNQUOTED(HAVE_DBUS_GLIB_GMI_GET_CONNECTION, $ac_have_gmi_get_con, [Define if you have a dbus-glib with dbus_g_method_invocation_get_g_connection()])
-
-dnl
-dnl Only dbus-glib >= 0.100 can use private dbus connections
-dnl
-PKG_CHECK_MODULES(DBUS_GLIB_100, [dbus-glib-1 >= 0.100], [have_dbus_glib_100=yes],[have_dbus_glib_100=no])
-if (test "${have_dbus_glib_100}" = "yes"); then
-	AC_DEFINE(HAVE_DBUS_GLIB_100, 1, [Define if you have dbus-glib >= 0.100])
-else
-	AC_DEFINE(HAVE_DBUS_GLIB_100, 0, [Define if you have dbus-glib >= 0.100])
-fi
-AM_CONDITIONAL(HAVE_DBUS_GLIB_100, test "${have_dbus_glib_100}" = "yes")
-
 PKG_CHECK_MODULES(GLIB, [gio-unix-2.0 >= 2.37.6 gmodule-2.0],
 	[AC_SUBST(LOG_DRIVER, '$(top_srcdir)/build-aux/tap-driver.sh'),
 	 AC_SUBST(AM_TESTS_FD_REDIRECT, '--tap')],

include/Makefile.am
@@ -1,10 +1,8 @@
 EXTRA_DIST = \
      gsystem-local-alloc.h \
      nm-dbus-compat.h \
-     nm-dbus-glib-types.h \
      nm-default.h \
      nm-glib.h \
-     nm-gvaluearray-compat.h \
      nm-test-utils.h \
      nm-macros-internal.h
 

include/nm-glib.h
@@ -38,9 +38,6 @@
 
 #endif
 
-
-#include "nm-gvaluearray-compat.h"
-
 static inline void
 __g_type_ensure (GType type)
 {

libnm-util/Makefile.am
@@ -62,9 +62,11 @@ nodist_libnm_util_include_HEADERS = \
 
 libnm_util_la_private_headers = \
 	crypto.h			\
+	nm-dbus-glib-types.h		\
+	nm-gvaluearray-compat.h		\
 	nm-param-spec-specialized.h	\
-	nm-utils-private.h		\
-	nm-setting-private.h
+	nm-setting-private.h		\
+	nm-utils-private.h
 
 libnm_util_la_csources = \
 	crypto.c			\

commit b9e89c918f13374772a72cefbe0cda6bb6bc88e4

core: fix builds without polkit support
Fix the following build error:

 nm-auth-utils.c: In function ‘nm_auth_chain_add_call’:
 nm-auth-utils.c:402:46: error: ‘DBUS_GERROR’ undeclared (first use in this function)
     call->chain->error = g_error_new_literal (DBUS_GERROR,

Fixes: 1cf35cb26b6cc04f8b2c51c3cde4bc08ef311062

src/nm-auth-utils.c
@@ -399,8 +399,8 @@ nm_auth_chain_add_call (NMAuthChain *self,
 		                                                      call);
 #else
 		if (!call->chain->error) {
-			call->chain->error = g_error_new_literal (DBUS_GERROR,
-			                                          DBUS_GERROR_FAILED,
+			call->chain->error = g_error_new_literal (NM_MANAGER_ERROR,
+			                                          NM_MANAGER_ERROR_FAILED,
 			                                          "Polkit support is disabled at compile time");
 		}
 		call->call_idle_id = g_idle_add ((GSourceFunc) auth_call_complete, call);