NetworkManager/NetworkManager

commit aff491b6f319f41d9c812400f8f561a138580bb7

contrib/rpm: update spec file to make it more similar to RHEL's version

contrib/fedora/rpm/NetworkManager.spec
@@ -18,6 +18,7 @@
 
 %global snapshot %{nil}
 %global git_sha __COMMIT__
+
 %global rpm_version __VERSION__
 %global real_version __VERSION__
 %global release_version __RELEASE_VERSION__
@@ -363,7 +364,9 @@ by nm-connection-editor and nm-applet in a non-graphical environment.
 #%patch1 -p1
 
 %build
+%if %{with regen_docs}
 gtkdocize
+%endif
 autoreconf --install --force
 intltoolize --automake --copy --force
 %configure \
@@ -419,7 +422,10 @@ intltoolize --automake --copy --force
 	--with-system-libndp=yes \
 	--with-pppd-plugin-dir=%{_libdir}/pppd/%{ppp_version} \
 	--with-dist-version=%{version}-%{release} \
-	--with-setting-plugins-default='ifcfg-rh,ibft'
+	--with-setting-plugins-default='ifcfg-rh,ibft' \
+	--with-config-dns-rc-manager-default=symlink \
+	--with-logging-backend-default=journal \
+	--enable-json-validation
 
 make %{?_smp_mflags}
 

commit 5d09d45b422415b58f3f9f280c87b53a3d199454

contrib/rpm: fix wrong argument to configure script
Fixes: aff491b6f319f41d9c812400f8f561a138580bb7

contrib/fedora/rpm/NetworkManager.spec
@@ -424,7 +424,7 @@ intltoolize --automake --copy --force
 	--with-dist-version=%{version}-%{release} \
 	--with-setting-plugins-default='ifcfg-rh,ibft' \
 	--with-config-dns-rc-manager-default=symlink \
-	--with-logging-backend-default=journal \
+	--with-config-logging-backend-default=journal \
 	--enable-json-validation
 
 make %{?_smp_mflags}