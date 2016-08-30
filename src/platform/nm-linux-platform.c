NetworkManager/NetworkManager

commit 02fb3eff481f79d3caa67fdde20b2ae7aa5e640b

platform: fix build
Fixes: 471521ca84187cd32afcd20aebe5a369fe7368dc

src/platform/nm-linux-platform.c
@@ -4071,7 +4071,7 @@ do_change_link_request (NMPlatform *platform,
 	int nle;
 
 	if (!nm_platform_netns_push (platform, &netns))
-		return NM_PLATFORM_ERROR_UNSPECIFIED;
+		return seq_result;
 
 retry:
 	nle = _nl_send_auto_with_seq (platform, nlmsg, &seq_result, NULL);
@@ -4079,7 +4079,7 @@ retry:
 		_LOGE ("do-change-link[%d]: failure sending netlink request \"%s\" (%d)",
 		       ifindex,
 		       nl_geterror (nle), -nle);
-		return NM_PLATFORM_ERROR_UNSPECIFIED;
+		return seq_result;
 	}
 
 	/* always refetch the link after changing it. There seems to be issues

commit 145d199589d34f8844d8e9cafc3c2c2ea8ec4d53

platform: fix return value for error case in do_change_link_request()
There is a "goto retry" in do_change_link_request(), at that point,
seq_result has the value -EOPNOTSUPP, instead of
WAIT_FOR_NL_RESPONSE_RESULT_UNKNOWN.

Fixes: 02fb3eff481f79d3caa67fdde20b2ae7aa5e640b

src/platform/nm-linux-platform.c
@@ -4071,7 +4071,7 @@ do_change_link_request (NMPlatform *platform,
 	int nle;
 
 	if (!nm_platform_netns_push (platform, &netns))
-		return seq_result;
+		return WAIT_FOR_NL_RESPONSE_RESULT_UNKNOWN;
 
 retry:
 	nle = _nl_send_auto_with_seq (platform, nlmsg, &seq_result, NULL);
@@ -4079,7 +4079,7 @@ retry:
 		_LOGE ("do-change-link[%d]: failure sending netlink request \"%s\" (%d)",
 		       ifindex,
 		       nl_geterror (nle), -nle);
-		return seq_result;
+		return WAIT_FOR_NL_RESPONSE_RESULT_UNKNOWN;
 	}
 
 	/* always refetch the link after changing it. There seems to be issues