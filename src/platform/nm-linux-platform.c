NetworkManager/NetworkManager

commit 471521ca84187cd32afcd20aebe5a369fe7368dc

platform: split processing result from do_change_link()
(cherry picked from commit 3dc09446771a3434ed948bdd5e6ca9f6ef9a9e76)

src/platform/nm-linux-platform.c
@@ -4060,18 +4060,14 @@ out:
 	return !!nmp_cache_lookup_obj (priv->cache, obj_id);
 }
 
-static NMPlatformError
-do_change_link (NMPlatform *platform,
-                int ifindex,
-                struct nl_msg *nlmsg)
+static WaitForNlResponseResult
+do_change_link_request (NMPlatform *platform,
+                        int ifindex,
+                        struct nl_msg *nlmsg)
 {
 	nm_auto_pop_netns NMPNetns *netns = NULL;
 	WaitForNlResponseResult seq_result = WAIT_FOR_NL_RESPONSE_RESULT_UNKNOWN;
 	int nle;
-	char s_buf[256];
-	NMPlatformError result = NM_PLATFORM_ERROR_SUCCESS;
-	NMLogLevel log_level = LOGL_DEBUG;
-	const char *log_result = "failure", *log_detail = "";
 
 	if (!nm_platform_netns_push (platform, &netns))
 		return NM_PLATFORM_ERROR_UNSPECIFIED;
@@ -4098,6 +4094,18 @@ retry:
 		nlmsg_hdr (nlmsg)->nlmsg_type = RTM_SETLINK;
 		goto retry;
 	}
+	return seq_result;
+}
+
+static NMPlatformError
+do_change_link_result (NMPlatform *platform,
+                       int ifindex,
+                       WaitForNlResponseResult seq_result)
+{
+	char s_buf[256];
+	NMPlatformError result = NM_PLATFORM_ERROR_SUCCESS;
+	NMLogLevel log_level = LOGL_DEBUG;
+	const char *log_result = "failure", *log_detail = "";
 
 	if (seq_result == WAIT_FOR_NL_RESPONSE_RESULT_RESPONSE_OK) {
 		log_result = "success";
@@ -4121,6 +4129,17 @@ retry:
 	        log_detail);
 
 	return result;
+}
+
+static NMPlatformError
+do_change_link (NMPlatform *platform,
+                int ifindex,
+                struct nl_msg *nlmsg)
+{
+	WaitForNlResponseResult seq_result;
+
+	seq_result = do_change_link_request (platform, ifindex, nlmsg);
+	return do_change_link_result (platform, ifindex, seq_result);
 }
 
 static gboolean

commit 02fb3eff481f79d3caa67fdde20b2ae7aa5e640b

platform: fix build
Fixes: 471521ca84187cd32afcd20aebe5a369fe7368dc

src/platform/nm-linux-platform.c
@@ -4071,7 +4071,7 @@ do_change_link_request (NMPlatform *platform,
 	int nle;
 
 	if (!nm_platform_netns_push (platform, &netns))
-		return NM_PLATFORM_ERROR_UNSPECIFIED;
+		return seq_result;
 
 retry:
 	nle = _nl_send_auto_with_seq (platform, nlmsg, &seq_result, NULL);
@@ -4079,7 +4079,7 @@ retry:
 		_LOGE ("do-change-link[%d]: failure sending netlink request \"%s\" (%d)",
 		       ifindex,
 		       nl_geterror (nle), -nle);
-		return NM_PLATFORM_ERROR_UNSPECIFIED;
+		return seq_result;
 	}
 
 	/* always refetch the link after changing it. There seems to be issues