NetworkManager/NetworkManager

commit 32f7c1d4b9aba597a99128631f07c2985149f303

device: wait for MAC address change to complete before setting interface up
Some drivers (brcmfmac) don't change the MAC address right away.
NetworkManager works around that by waiting synchronously until
the address changes (commit 1a85103765d4eaa0acab6b03658a4f9cfe684a64).

wpa_supplicant on the other hand, only re-reads the MAC address
when changing state from DISABLED to ENABLED, which happens when
the interface comes up.

That is a bug in wpa_supplicant and the driver, but we can work-around by
waiting until the MAC address actually changed before setting the interface
IFF_UP. Also note, that there is still a race in wpa_supplicant which might
miss a change to DISABLED state altogether.

https://bugzilla.gnome.org/show_bug.cgi?id=770504
https://bugzilla.redhat.com/show_bug.cgi?id=1374023

src/devices/nm-device.c
@@ -11847,12 +11847,8 @@ _hw_addr_set (NMDevice *self,
 		        nm_platform_error_to_string (plerr));
 	}
 
-	if (was_up) {
-		if (!nm_device_bring_up (self, TRUE, NULL))
-			return FALSE;
-	}
-
 	if (needs_refresh) {
+		success = TRUE;
 		if (_hw_addr_matches (self, addr)) {
 			/* the MAC address already changed during nm_device_bring_up() above. */
 		} else {
@@ -11889,15 +11885,24 @@ handle_wait:
 					continue;
 				}
 handle_fail:
-				_LOGW (LOGD_DEVICE,
-				       "set-hw-addr: new MAC address %s not successfully %s (%s)",
-				       addr, operation, detail);
-				return FALSE;
+				success = FALSE;
+				break;
 			}
 		}
 
-		_LOGI (LOGD_DEVICE, "set-hw-addr: %s MAC address to %s (%s)",
-		       operation, addr, detail);
+		if (success) {
+			_LOGI (LOGD_DEVICE, "set-hw-addr: %s MAC address to %s (%s)",
+			       operation, addr, detail);
+		} else {
+			_LOGW (LOGD_DEVICE,
+			       "set-hw-addr: new MAC address %s not successfully %s (%s)",
+			       addr, operation, detail);
+		}
+	}
+
+	if (was_up) {
+		if (!nm_device_bring_up (self, TRUE, NULL))
+			return FALSE;
 	}
 
 	return success;

commit e7a1008b4b405a83c78f7e44fe86cc3db8f2015e

device: cleanup _hw_addr_set()
No change in behavior, just reorganize.

Fixes: 32f7c1d4b9aba597a99128631f07c2985149f303

src/devices/nm-device.c
@@ -11793,7 +11793,6 @@ _hw_addr_set (NMDevice *self,
 {
 	NMDevicePrivate *priv;
 	gboolean success = FALSE;
-	gboolean needs_refresh = FALSE;
 	NMPlatformError plerr;
 	guint8 addr_bytes[NM_UTILS_HWADDR_LEN_MAX];
 	guint hw_addr_len;
@@ -11835,24 +11834,11 @@ _hw_addr_set (NMDevice *self,
 			_LOGI (LOGD_DEVICE, "set-hw-addr: %s MAC address to %s (%s)",
 			       operation, addr, detail);
 		} else {
+			gint64 poll_end, now;
+
 			_LOGD (LOGD_DEVICE,
 			       "set-hw-addr: new MAC address %s not successfully %s (%s) (refresh link)",
 			       addr, operation, detail);
-			needs_refresh = TRUE;
-		}
-	} else {
-		_NMLOG (plerr == NM_PLATFORM_ERROR_NOT_FOUND ? LOGL_DEBUG : LOGL_WARN,
-		        LOGD_DEVICE, "set-hw-addr: failed to %s MAC address to %s (%s) (%s)",
-		        operation, addr, detail,
-		        nm_platform_error_to_string (plerr));
-	}
-
-	if (needs_refresh) {
-		success = TRUE;
-		if (_hw_addr_matches (self, addr)) {
-			/* the MAC address already changed during nm_device_bring_up() above. */
-		} else {
-			gint64 poll_end, now;
 
 			/* The platform call indicated success, however the address is not
 			 * as expected. That is either due to a driver issue (brcmfmac, bgo#770456,
@@ -11888,16 +11874,21 @@ handle_fail:
 				success = FALSE;
 				break;
 			}
-		}
 
-		if (success) {
-			_LOGI (LOGD_DEVICE, "set-hw-addr: %s MAC address to %s (%s)",
-			       operation, addr, detail);
-		} else {
-			_LOGW (LOGD_DEVICE,
-			       "set-hw-addr: new MAC address %s not successfully %s (%s)",
-			       addr, operation, detail);
+			if (success) {
+				_LOGI (LOGD_DEVICE, "set-hw-addr: %s MAC address to %s (%s)",
+				       operation, addr, detail);
+			} else {
+				_LOGW (LOGD_DEVICE,
+				       "set-hw-addr: new MAC address %s not successfully %s (%s)",
+				       addr, operation, detail);
+			}
 		}
+	} else {
+		_NMLOG (plerr == NM_PLATFORM_ERROR_NOT_FOUND ? LOGL_DEBUG : LOGL_WARN,
+		        LOGD_DEVICE, "set-hw-addr: failed to %s MAC address to %s (%s) (%s)",
+		        operation, addr, detail,
+		        nm_platform_error_to_string (plerr));
 	}
 
 	if (was_up) {