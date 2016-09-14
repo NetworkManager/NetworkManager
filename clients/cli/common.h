1f0ba2e48738cbacb0dc3610bae776bf8f10d7e4
Jiří Klimeš <blueowl@centrum.cz>
2016-09-15 01:20:53 +0200

    cli: (trivial) move gen_func_ifnames() from devices.c to common.c
    and rename it to nmc_rl_gen_func()

--- clients/cli/common.h
+++ clients/cli/common.h
@@ -65,6 +65,7 @@ void nmc_cleanup_readline (void);
 char *nmc_readline (const char *prompt_fmt, ...) G_GNUC_PRINTF (1, 2);
 char *nmc_readline_echo (gboolean echo_on, const char *prompt_fmt, ...) G_GNUC_PRINTF (2, 3);
 char *nmc_rl_gen_func_basic (const char *text, int state, const char **words);
+char *nmc_rl_gen_func_ifnames (const char *text, int state);
 gboolean nmc_get_in_readline (void);
 void nmc_set_in_readline (gboolean in_readline);
 
