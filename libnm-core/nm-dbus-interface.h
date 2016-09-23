NetworkManager/NetworkManager

commit 93a753e31109997e4f00427db4f8a99a79a8f354

capabilities: add comment about capability range

libnm-core/nm-dbus-interface.h
@@ -90,6 +90,12 @@
  * #NMCapability names the numbers in the Capabilities property.
  **/
 typedef enum {
+	/* Capabilities are positive numbers. They are part of stable API
+	 * and a certain capability number is guaranteed not to change.
+	 *
+	 * The range 0x7000 - 0x7FFF of capabilities is guaranteed not to be
+	 * used by upstream NetworkManager. It could thus be used for downstream
+	 * extensions. */
 	NM_CAPABILITY_TEAM = 1,
 } NMCapability;
 

introspection/nm-manager.xml
@@ -385,7 +385,8 @@
 
         The current set of capabilities. See <link
         linkend="NMCapability">NMCapability</link> for currently
-        defined capability numbers.
+        defined capability numbers. The array is guaranteed to
+        be sorted in ascending order without duplicates.
     -->
     <property name="Capabilities" type="au" access="read"/>
 

commit e152eea21819cb31e6da779b06ae1b6df7270bb1

build: fix breakage of enums-to-docbook.pl script for NMCapability enum
      GEN      nm-dbus-types.xml
    Documentation for value '*' missing at ../tools/enums-to-docbook.pl line 134, <> line 95.
    Makefile:1579: recipe for target 'nm-dbus-types.xml' failed

Fixes: 93a753e31109997e4f00427db4f8a99a79a8f354

libnm-core/nm-dbus-interface.h
@@ -88,14 +88,14 @@
  * @NM_CAPABILITY_TEAM: Teams can be managed
  *
  * #NMCapability names the numbers in the Capabilities property.
- **/
+ * Capabilities are positive numbers. They are part of stable API
+ * and a certain capability number is guaranteed not to change.
+ *
+ * The range 0x7000 - 0x7FFF of capabilities is guaranteed not to be
+ * used by upstream NetworkManager. It could thus be used for downstream
+ * extensions.
+ */
 typedef enum {
-	/* Capabilities are positive numbers. They are part of stable API
-	 * and a certain capability number is guaranteed not to change.
-	 *
-	 * The range 0x7000 - 0x7FFF of capabilities is guaranteed not to be
-	 * used by upstream NetworkManager. It could thus be used for downstream
-	 * extensions. */
 	NM_CAPABILITY_TEAM = 1,
 } NMCapability;
 