7034ea7aa33328a2ab937d3b1559dfcb59bc8130
Beniamino Galvani <bgalvani@redhat.com>
2016-10-26 12:06:39 +0200

    wwan: fix wrong connection cast on device state change
    nm_settings_connection_set_autoconnect_blocked_reason() must be called
    on the settings-connection. Fixes the following:
    
    GLib-GObject-WARNING **: invalid cast from 'NMSimpleConnection' to 'NMSettingsConnection'
    
    Fixes: 06da3532428e3498c1e808ff8be1af48b540a6ff

--- src/devices/wwan/nm-device-modem.c
+++ src/devices/wwan/nm-device-modem.c
@@ -363,7 +363,7 @@ device_state_changed (NMDevice *device,
 {
 	NMDeviceModem *self = NM_DEVICE_MODEM (device);
 	NMDeviceModemPrivate *priv = NM_DEVICE_MODEM_GET_PRIVATE (self);
-	NMConnection *connection = nm_device_get_applied_connection (device);
+	NMSettingsConnection *connection = nm_device_get_settings_connection (device);
 
 	g_assert (priv->modem);
 
@@ -390,7 +390,7 @@ device_state_changed (NMDevice *device,
 		 * where a retry attempt would just fail again.
 		 */
 		if (connection)
-			nm_settings_connection_set_autoconnect_blocked_reason (NM_SETTINGS_CONNECTION (connection), reason);
+			nm_settings_connection_set_autoconnect_blocked_reason (connection, reason);
 		break;
 	default:
 		break;
