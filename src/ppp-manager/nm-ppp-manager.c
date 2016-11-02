NetworkManager/NetworkManager

commit c1dd3b6eed11eaa30d4dd80d22845676e3ff8fec

core: move D-Bus export/unexport into NMExportedObject
Move D-Bus export/unexport handling into NMExportedObject and remove
type-specific export/get_path methods (export paths are now specified
at the class level, and NMExportedObject handles the counters for all
exported types automatically).

Since all exportable objects now use the same get_path() method, we
can also add some helper methods to simplify get_property()
implementations for object-path and object-path-array properties.

src/ppp-manager/nm-ppp-manager.c
@@ -75,7 +75,6 @@ static void _ppp_kill (NMPPPManager *manager);
 
 typedef struct {
 	GPid pid;
-	char *dbus_path;
 
 	char *parent_iface;
 
@@ -123,13 +122,7 @@ nm_ppp_manager_init (NMPPPManager *manager)
 static void
 constructed (GObject *object)
 {
-	NMPPPManagerPrivate *priv = NM_PPP_MANAGER_GET_PRIVATE (object);
-	DBusGConnection *connection;
-	static guint32 counter = 0;
-
-	priv->dbus_path = g_strdup_printf (NM_DBUS_PATH "/PPP/%d", counter++);
-	connection = nm_dbus_manager_get_connection (nm_dbus_manager_get ());
-	dbus_g_connection_register_g_object (connection, priv->dbus_path, object);
+	nm_exported_object_export (NM_EXPORTED_OBJECT (object));
 
 	G_OBJECT_CLASS (nm_ppp_manager_parent_class)->constructed (object);
 }
@@ -618,8 +611,11 @@ static void
 nm_ppp_manager_class_init (NMPPPManagerClass *manager_class)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS (manager_class);
+	NMExportedObjectClass *exported_object_class = NM_EXPORTED_OBJECT_CLASS (manager_class);
 
 	g_type_class_add_private (manager_class, sizeof (NMPPPManagerPrivate));
+
+	exported_object_class->export_path = NM_DBUS_PATH "/PPP";
 
 	object_class->constructed = constructed;
 	object_class->dispose = dispose;
@@ -1001,7 +997,7 @@ create_pppd_cmd_line (NMPPPManager *self,
 	nm_cmd_line_add_int (cmd, 0);
 
 	nm_cmd_line_add_string (cmd, "ipparam");
-	nm_cmd_line_add_string (cmd, priv->dbus_path);
+	nm_cmd_line_add_string (cmd, nm_exported_object_get_path (NM_EXPORTED_OBJECT (self)));
 
 	nm_cmd_line_add_string (cmd, "plugin");
 	nm_cmd_line_add_string (cmd, NM_PPPD_PLUGIN);

src/NetworkManagerUtils.c
@@ -2904,3 +2904,46 @@ nm_utils_setpgid (gpointer unused G_GNUC_UNUSED)
 	pid = getpid ();
 	setpgid (pid, pid);
 }
+
+/**
+ * nm_utils_g_value_set_object_path:
+ * @value: a #GValue, initialized to store an object path
+ * @object: (allow-none): an #NMExportedObject
+ *
+ * Sets @value to @object's object path. If @object is %NULL, or not
+ * exported, @value is set to "/".
+ */
+void
+nm_utils_g_value_set_object_path (GValue *value, gpointer object)
+{
+	g_return_if_fail (!object || NM_IS_EXPORTED_OBJECT (object));
+
+	if (object && nm_exported_object_is_exported (object))
+		g_value_set_boxed (value, nm_exported_object_get_path (object));
+	else
+		g_value_set_boxed (value, "/");
+}
+
+/**
+ * nm_utils_g_value_set_object_path_array:
+ * @value: a #GValue, initialized to store an object path
+ * @objects: a #GSList of #NMExportedObjects
+ *
+ * Sets @value to an array of object paths of the objects in @objects.
+ */
+void
+nm_utils_g_value_set_object_path_array (GValue *value, GSList *objects)
+{
+	GPtrArray *paths;
+	GSList *iter;
+
+	paths = g_ptr_array_new ();
+	for (iter = objects; iter; iter = iter->next) {
+		NMExportedObject *object = iter->data;
+
+		if (!nm_exported_object_is_exported (object))
+			continue;
+		g_ptr_array_add (paths, g_strdup (nm_exported_object_get_path (object)));
+	}
+	g_value_take_boxed (value, paths);
+}

src/NetworkManagerUtils.h
@@ -262,4 +262,7 @@ gboolean nm_utils_get_testing_initialized (void);
 NMUtilsTestFlags nm_utils_get_testing (void);
 void _nm_utils_set_testing (NMUtilsTestFlags flags);
 
+void nm_utils_g_value_set_object_path (GValue *value, gpointer object);
+void nm_utils_g_value_set_object_path_array (GValue *value, GSList *objects);
+
 #endif /* __NETWORKMANAGER_UTILS_H__ */

src/devices/nm-device-bond.c
@@ -475,18 +475,14 @@ static void
 get_property (GObject *object, guint prop_id,
               GValue *value, GParamSpec *pspec)
 {
-	GPtrArray *slaves;
-	GSList *list, *iter;
+	GSList *list;
 
 	switch (prop_id) {
 		break;
 	case PROP_SLAVES:
-		slaves = g_ptr_array_new ();
 		list = nm_device_master_get_slaves (NM_DEVICE (object));
-		for (iter = list; iter; iter = iter->next)
-			g_ptr_array_add (slaves, g_strdup (nm_device_get_path (NM_DEVICE (iter->data))));
+		nm_utils_g_value_set_object_path_array (value, list);
 		g_slist_free (list);
-		g_value_take_boxed (value, slaves);
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);

src/devices/nm-device-bridge.c
@@ -401,17 +401,14 @@ static void
 get_property (GObject *object, guint prop_id,
               GValue *value, GParamSpec *pspec)
 {
-	GPtrArray *slaves;
-	GSList *list, *iter;
+	GSList *list;
 
 	switch (prop_id) {
+		break;
 	case PROP_SLAVES:
-		slaves = g_ptr_array_new ();
 		list = nm_device_master_get_slaves (NM_DEVICE (object));
-		for (iter = list; iter; iter = iter->next)
-			g_ptr_array_add (slaves, g_strdup (nm_device_get_path (NM_DEVICE (iter->data))));
+		nm_utils_g_value_set_object_path_array (value, list);
 		g_slist_free (list);
-		g_value_take_boxed (value, slaves);
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);

src/devices/nm-device-gre.c
@@ -136,7 +136,7 @@ get_property (GObject *object, guint prop_id,
 	switch (prop_id) {
 	case PROP_PARENT:
 		parent = nm_manager_get_device_by_ifindex (nm_manager_get (), priv->props.parent_ifindex);
-		g_value_set_boxed (value, parent ? nm_device_get_path (parent) : "/");
+		nm_utils_g_value_set_object_path (value, parent);
 		break;
 	case PROP_INPUT_FLAGS:
 		g_value_set_uint (value, priv->props.input_flags);

src/devices/nm-device-macvlan.c
@@ -114,7 +114,7 @@ get_property (GObject *object, guint prop_id,
 	switch (prop_id) {
 	case PROP_PARENT:
 		parent = nm_manager_get_device_by_ifindex (nm_manager_get (), priv->props.parent_ifindex);
-		g_value_set_boxed (value, parent ? nm_device_get_path (parent) : "/");
+		nm_utils_g_value_set_object_path (value, parent);
 		break;
 	case PROP_MODE:
 		g_value_set_string (value, priv->props.mode);

src/devices/nm-device-veth.c
@@ -136,7 +136,7 @@ get_property (GObject *object, guint prop_id,
 	switch (prop_id) {
 	case PROP_PEER:
 		peer = get_peer (self);
-		g_value_set_boxed (value, peer ? nm_device_get_path (peer) : "/");
+		nm_utils_g_value_set_object_path (value, peer);
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);

commit bc26f94d1ed1154459a6d76b60ac3b5f8155af8f

ppp: add counter to D-Bus object path for PPP manager instances
There can be multiple PPP connections active, each with its own PPP
manager.

Fixes: c1dd3b6eed11eaa30d4dd80d22845676e3ff8fec

src/ppp-manager/nm-ppp-manager.c
@@ -1199,7 +1199,7 @@ nm_ppp_manager_class_init (NMPPPManagerClass *manager_class)
 	object_class->get_property = get_property;
 	object_class->set_property = set_property;
 
-	exported_object_class->export_path = NM_DBUS_PATH "/PPP";
+	exported_object_class->export_path = NM_DBUS_PATH "/PPP/%u";
 	exported_object_class->export_on_construction = TRUE;
 
 	obj_properties[PROP_PARENT_IFACE] =