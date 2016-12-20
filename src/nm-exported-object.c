NetworkManager/NetworkManager

commit 8c4e824997dec18c5dc7a7dedd4053f51fece8f7

exported-object: don't assert the presence of PropertiesChanged signal
For new interfaces we don't define a PropertiesChanged signal as we
rely on GDBus to generate one on the standard
org.freedesktop.DBus.Properties interface.

src/nm-exported-object.c
@@ -947,8 +947,7 @@ vtype_found:
 		g_hash_table_insert (ifdata->pending_notifies,
 		                     (gpointer) dbus_property_name,
 		                     value_variant);
-	} else
-		nm_assert_not_reached ();
+	}
 
 	if (!priv->notify_idle_id)
 		priv->notify_idle_id = g_idle_add (idle_emit_properties_changed, self);

commit 9d589046f96b57865c3ee81778a5a641bdad074b

exported-object: fix memory leak
Fixes: 8c4e824997dec18c5dc7a7dedd4053f51fece8f7

src/nm-exported-object.c
@@ -947,7 +947,8 @@ vtype_found:
 		g_hash_table_insert (ifdata->pending_notifies,
 		                     (gpointer) dbus_property_name,
 		                     value_variant);
-	}
+	} else
+		g_variant_unref (value_variant);
 
 	if (!priv->notify_idle_id)
 		priv->notify_idle_id = g_idle_add (idle_emit_properties_changed, self);