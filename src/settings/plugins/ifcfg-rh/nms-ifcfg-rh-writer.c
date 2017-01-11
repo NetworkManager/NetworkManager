NetworkManager/NetworkManager

commit 30db08d38ed745bd5754e9676530eeee7efdaf3a

ifcfg-rh: don't ignore certificates with unknown scheme

src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-writer.c
@@ -249,7 +249,9 @@ write_object (NMSetting8021x *s_8021x,
 		path = (*(objtype->path_func))(s_8021x);
 		break;
 	default:
-		break;
+		g_set_error (error, NM_SETTINGS_ERROR, NM_SETTINGS_ERROR_FAILED,
+		             "Unhandled certificate object scheme");
+		return FALSE;
 	}
 
 	/* If certificate/private key wasn't sent, the connection may no longer be

commit 9b9a18059787ad13cc10058cfa15b153a695a007

ifcfg-rh: unknown scheme means no certificate at all
Fixes: 30db08d38ed745bd5754e9676530eeee7efdaf3a

src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-writer.c
@@ -242,6 +242,8 @@ write_object (NMSetting8021x *s_8021x,
 
 	scheme = (*(objtype->scheme_func))(s_8021x);
 	switch (scheme) {
+	case NM_SETTING_802_1X_CK_SCHEME_UNKNOWN:
+		break;
 	case NM_SETTING_802_1X_CK_SCHEME_BLOB:
 		blob = (*(objtype->blob_func))(s_8021x);
 		break;