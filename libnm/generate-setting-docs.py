NetworkManager/NetworkManager

commit 6c96aafaa9a61ca4fe713551a4cd4b15fb7e8a63

build: make the doc generator find the right libnm.so
At some point gobject-introspection added an API to add a library path
and stopped honoring the LD_LIBRARY_PATH (a bug, according to GI
documentation?).

libnm/generate-setting-docs.py
@@ -18,11 +18,23 @@
 
 from __future__ import print_function
 
+import os
 import gi
-gi.require_version('NM', '1.0')
-from gi.repository import NM, GObject
+gi.require_version('GIRepository', '2.0')
+from gi.repository import GIRepository
 import argparse, datetime, re, sys
 import xml.etree.ElementTree as ET
+
+try:
+    libs = os.environ['LD_LIBRARY_PATH'].split(':')
+    libs.reverse()
+    for lib in libs:
+        GIRepository.Repository.prepend_library_path(lib)
+except KeyError:
+        pass
+
+gi.require_version('NM', '1.0')
+from gi.repository import NM, GObject
 
 dbus_type_name_map = {
     'b': 'boolean',

commit 22722b77322fb3ddc1a99509340e8175acddb154

build: fix build with older gi
Fixes: 6c96aafaa9a61ca4fe713551a4cd4b15fb7e8a63

libnm/generate-setting-docs.py
@@ -30,6 +30,11 @@ try:
     libs.reverse()
     for lib in libs:
         GIRepository.Repository.prepend_library_path(lib)
+except AttributeError:
+        # An old GI version, that has no prepend_library_path
+        # It's alright, it probably interprets LD_LIBRARY_PATH
+        # correctly.
+        pass
 except KeyError:
         pass
 