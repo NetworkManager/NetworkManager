491ccd9d64b2ea16f2d399b42454bee9c1c7bac1
Thomas Haller <thaller@redhat.com>
2017-01-28 15:53:47 +0100

    ifcfg-rh: fix interpreting missing MAC_ADDRESS_RANDOMIZATION as permanent address
    With commit 4f6c91d6962cc031f07e52bb31adde560ad70fac, we aimed to
    enable mac-address-randomization by default for Wi-Fi. That however
    is not possible by default because it breaks various scenarios.
    Also, later wifi.mac-address-randomization was deprecated in favor
    of wifi.cloned-mac-address setting.
    
    Both wifi.mac-address-randomization and wifi.cloned-mac-address support
    global default values, so it is wrong to read a missing
    MAC_ADDRESS_RANDOMIZATION setting as "NEVER" -- which due to
    normalization also results in cloned-mac-address=permanent.
    
    See also commit 46d53e11012c047e09d04f663c1c36e6c47dc298 which does
    something similar for keyfile.
    
    This bug also prevents a user from clearing the cloned-mac-address:
    
      $ nmcli connection show "$CONN"
      ...
      802-11-wireless.cloned-mac-address:        permanent
      802-11-wireless.mac-address-randomization: never
      ...
      $ nmcli connection modify "$CONN: wifi.cloned-mac-address ''
      # ^ takes no effect
    
    As workaround, you also need to clear mac-address-randomization:
    
      $ nmcli connection modify "$CONN: wifi.cloned-mac-address '' \
              wifi.mac-address-randomization default
    
    https://mail.gnome.org/archives/networkmanager-list/2017-January/msg00060.html
    (cherry picked from commit 27cba47957630a89e3b1e3a06aa44a525270cec1)

--- src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-reader.c
+++ src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-reader.c
@@ -3387,7 +3387,7 @@ make_wireless_setting (shvarFile *ifcfg,
 	const char *cvalue;
 	char *value = NULL;
 	gint64 chan = 0;
-	NMSettingMacRandomization mac_randomization = NM_SETTING_MAC_RANDOMIZATION_NEVER;
+	NMSettingMacRandomization mac_randomization;
 	NMSettingWirelessPowersave powersave = NM_SETTING_WIRELESS_POWERSAVE_DEFAULT;
 
 	s_wireless = NM_SETTING_WIRELESS (nm_setting_wireless_new ());
@@ -3587,7 +3587,7 @@ make_wireless_setting (shvarFile *ifcfg,
 		}
 		g_free (value);
 	} else
-		mac_randomization = NM_SETTING_MAC_RANDOMIZATION_NEVER;
+		mac_randomization = NM_SETTING_MAC_RANDOMIZATION_DEFAULT;
 
 	g_object_set (s_wireless,
 	              NM_SETTING_WIRELESS_MAC_ADDRESS_RANDOMIZATION,
