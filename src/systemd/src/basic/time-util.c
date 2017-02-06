23d9fbb8589a3395b88e4b2f2551406878ad9aa4
Thomas Haller <thaller@redhat.com>
2017-02-06 17:54:28 +0100

    systemd: backport fall-through comments for -Wimplicit-fallthrough warning

--- src/systemd/src/basic/time-util.c
+++ src/systemd/src/basic/time-util.c
@@ -1277,7 +1277,7 @@ bool clock_supported(clockid_t clock) {
                 if (!clock_boottime_supported())
                         return false;
 
-                /* fall through, after checking the cached value for CLOCK_BOOTTIME. */
+                /* fall through */
 
         default:
                 /* For everything else, check properly */
