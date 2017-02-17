a72ded79026a2dcb28dbebf9f9f2077d9576569d
Thomas Haller <thaller@redhat.com>
2017-02-17 20:32:42 +0100

    device/wifi: fix #if check for more-logging
    Fixes: ae158bf178d9a7480a5e5378b2a8a511bdb5db9e
    (cherry picked from commit 35d4e8f7a04c80d2c67119cdbc6c4afacd23e9ec)

--- src/devices/wifi/nm-device-wifi.c
+++ src/devices/wifi/nm-device-wifi.c
@@ -448,7 +448,7 @@ periodic_update (NMDeviceWifi *self)
 		percent = nm_platform_wifi_get_quality (NM_PLATFORM_GET, ifindex);
 		if (percent >= 0 || ++priv->invalid_strength_counter > 3) {
 			if (nm_wifi_ap_set_strength (priv->current_ap, (gint8) percent)) {
-#if NM_MORE_LOGGING
+#ifdef NM_MORE_LOGGING
 				_ap_dump (self, LOGL_TRACE, priv->current_ap, "updated", 0);
 #endif
 			}
