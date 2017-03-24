29a47f40f8d438595c14b15e5456bf42f7e0804c
Thomas Haller <thaller@redhat.com>
2017-03-24 11:15:49 +0100

    build: configure explicit paths for dist tarball during "build_clean.sh"
    "build_clean.sh" is used to generate a distribution tarball. The tarball
    contains pregenerated man pages with default values for paths, which in
    turn depend on the configure options when creating the tarball.
    
    Previously, the man page would have paths like "usr/local/etc/NetworkManager/...",
    which doesn't seem the best choice for a default man page.
    
    Explicitly set the installation paths.
    
    Also, --disable-dependency-tracking in this mode. It may speed up the
    build.

--- contrib/fedora/rpm/build_clean.sh
+++ contrib/fedora/rpm/build_clean.sh
@@ -111,6 +111,22 @@ fi
 
 if [[ $NO_DIST != 1 ]]; then
     ./autogen.sh \
+        --program-prefix= \
+        --prefix=/usr \
+        --exec-prefix=/usr \
+        --bindir=/usr/bin \
+        --sbindir=/usr/sbin \
+        --sysconfdir=/etc \
+        --datadir=/usr/share \
+        --includedir=/usr/include \
+        --libdir=/usr/lib \
+        --libexecdir=/usr/libexec \
+        --localstatedir=/var \
+        --sharedstatedir=/var/lib \
+        --mandir=/usr/share/man \
+        --infodir=/usr/share/info \
+        \
+        --disable-dependency-tracking \
         --enable-gtk-doc \
         --enable-introspection \
         --with-libnm-glib \
