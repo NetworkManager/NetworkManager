NetworkManager/NetworkManager

commit acf1067a455b02b60985e209df568d3fcc3fac9e

nm-manager: try assuming connections on managed devices
Commit 850c97795 ("device: track system interface state in NMDevice")
introduced interface states for devices and prevented checking if a
connection should be assumed on already managed devices.
This prevented to properly manage the event of an ip configuration added
externally to NM to a managed but not (yet) activated device.

Fixes: 850c977953e4de3c8bbee64a3d2e8726c971761c

src/nm-manager.c
@@ -1852,6 +1852,7 @@ recheck_assume_connection (NMManager *self,
 	gboolean was_unmanaged = FALSE;
 	gboolean generated = FALSE;
 	NMDeviceState state;
+	NMDeviceSysIfaceState if_state;
 
 	g_return_val_if_fail (NM_IS_MANAGER (self), FALSE);
 	g_return_val_if_fail (NM_IS_DEVICE (device), FALSE);
@@ -1866,7 +1867,10 @@ recheck_assume_connection (NMManager *self,
 	if (state > NM_DEVICE_STATE_DISCONNECTED)
 		return FALSE;
 
-	if (nm_device_sys_iface_state_get (device) != NM_DEVICE_SYS_IFACE_STATE_EXTERNAL)
+	if_state = nm_device_sys_iface_state_get (device);
+	if (if_state == NM_DEVICE_SYS_IFACE_STATE_MANAGED)
+		nm_assert (!guess_assume && (assume_connection_uuid == NULL));
+	else if (if_state != NM_DEVICE_SYS_IFACE_STATE_EXTERNAL)
 		return FALSE;
 
 	connection = get_existing_connection (self, device, guess_assume, assume_connection_uuid, &generated);

commit b6b7d909f7023ef1830fe5a072b64e109397f609

manager: avoid generating in memory connections during startup for managed devices
Commit #acf1067a allowed to assume connections on already managed
devices. Anyway, in complex scenario with layered connections, during
the startup of NetworkManager, this could interfere with the connection
assumption based on saved state.
So, avoid to re-assume connections on already managed devices during
startup.

Fixes: acf1067a455b02b60985e209df568d3fcc3fac9e

src/nm-manager.c
@@ -1853,6 +1853,7 @@ recheck_assume_connection (NMManager *self,
 	gboolean generated = FALSE;
 	NMDeviceState state;
 	NMDeviceSysIfaceState if_state;
+	NMManagerPrivate *priv = NM_MANAGER_GET_PRIVATE (self);
 
 	g_return_val_if_fail (NM_IS_MANAGER (self), FALSE);
 	g_return_val_if_fail (NM_IS_DEVICE (device), FALSE);
@@ -1868,7 +1869,7 @@ recheck_assume_connection (NMManager *self,
 		return FALSE;
 
 	if_state = nm_device_sys_iface_state_get (device);
-	if (if_state == NM_DEVICE_SYS_IFACE_STATE_MANAGED)
+	if (!priv->startup  && (if_state == NM_DEVICE_SYS_IFACE_STATE_MANAGED))
 		nm_assert (!guess_assume && (assume_connection_uuid == NULL));
 	else if (if_state != NM_DEVICE_SYS_IFACE_STATE_EXTERNAL)
 		return FALSE;