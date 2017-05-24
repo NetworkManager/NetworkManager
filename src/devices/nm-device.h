805d3240f9363f5ea5d57000f2920234bf6cd152
Lubomir Rintel <lkundrak@v3.sk>
2017-05-24 16:35:03 +0200

    devices/factory: allow announcing a NULL component
    We'll use this to let the devices know they can retry autoactivation
    because some component became available without actually having any
    data that would be useful for that device.
    
    Adjust the comment.

--- src/devices/nm-device.h
+++ src/devices/nm-device.h
@@ -375,10 +375,10 @@ typedef struct {
 	 * @self: the #NMDevice
 	 * @component: the component (device, modem, etc) which was added
 	 *
-	 * Notifies @self that a new component was added to the Manager.  This
-	 * may include any kind of %GObject subclass, and the device is expected
-	 * to match only specific components they care about, like %NMModem objects
-	 * or %NMDevice objects.
+	 * Notifies @self that a new component that a device might be interested
+	 * in was detected by some device factory. It may include an object of
+	 * %GObject subclass to help the devices decide whether it claims that
+	 * particular object itself and the emitting factory should not.
 	 *
 	 * Returns: %TRUE if the component was claimed exclusively and no further
 	 * devices should be notified of the new component.  %FALSE to indicate
