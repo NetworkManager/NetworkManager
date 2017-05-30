NetworkManager/NetworkManager

commit f80d0eb29e5fed5798a3d08b37a235d0ceede451

ifcfg-rh: use svSetValueInt64_cond() to write MTU value

src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-writer.c
@@ -768,10 +768,8 @@ write_wireless_setting (NMConnection *connection,
 		g_free (blacklist_str);
 	}
 
-	svUnsetValue (ifcfg, "MTU");
 	mtu = nm_setting_wireless_get_mtu (s_wireless);
-	if (mtu)
-		svSetValueInt64 (ifcfg, "MTU", mtu);
+	svSetValueInt64_cond (ifcfg, "MTU", mtu != 0, mtu);
 
 	ssid = nm_setting_wireless_get_ssid (s_wireless);
 	if (!ssid) {
@@ -954,10 +952,8 @@ write_infiniband_setting (NMConnection *connection, shvarFile *ifcfg, GError **e
 	mac = nm_setting_infiniband_get_mac_address (s_infiniband);
 	svSetValueStr (ifcfg, "HWADDR", mac);
 
-	svUnsetValue (ifcfg, "MTU");
 	mtu = nm_setting_infiniband_get_mtu (s_infiniband);
-	if (mtu)
-		svSetValueInt64 (ifcfg, "MTU", mtu);
+	svSetValueInt64_cond (ifcfg, "MTU", mtu != 0, mtu);
 
 	transport_mode = nm_setting_infiniband_get_transport_mode (s_infiniband);
 	svSetValueBoolean (ifcfg, "CONNECTED_MODE", nm_streq (transport_mode, "connected"));
@@ -1018,10 +1014,8 @@ write_wired_setting (NMConnection *connection, shvarFile *ifcfg, GError **error)
 		g_free (blacklist_str);
 	}
 
-	svUnsetValue (ifcfg, "MTU");
 	mtu = nm_setting_wired_get_mtu (s_wired);
-	if (mtu)
-		svSetValueInt64 (ifcfg, "MTU", mtu);
+	svSetValueInt64 (ifcfg, "MTU", mtu != 0, mtu);
 
 	svUnsetValue (ifcfg, "SUBCHANNELS");
 	s390_subchannels = nm_setting_wired_get_s390_subchannels (s_wired);
@@ -1189,10 +1183,7 @@ write_wired_for_virtual (NMConnection *connection, shvarFile *ifcfg)
 		               nm_setting_wired_get_generate_mac_address_mask (s_wired));
 
 		mtu = nm_setting_wired_get_mtu (s_wired);
-		if (mtu)
-			svSetValueInt64 (ifcfg, "MTU", mtu);
-		else
-			svUnsetValue (ifcfg, "MTU");
+		svSetValueInt64_cond (ifcfg, "MTU", mtu != 0, mtu);
 	}
 	return has_wired;
 }

commit 84f2d226b54a7945a493a14d8ba0b15c75813892

ifcfg-rh: fix build failure in write_wired_setting()
Fixes: f80d0eb29e5fed5798a3d08b37a235d0ceede451

src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-writer.c
@@ -1015,7 +1015,7 @@ write_wired_setting (NMConnection *connection, shvarFile *ifcfg, GError **error)
 	}
 
 	mtu = nm_setting_wired_get_mtu (s_wired);
-	svSetValueInt64 (ifcfg, "MTU", mtu != 0, mtu);
+	svSetValueInt64_cond (ifcfg, "MTU", mtu != 0, mtu);
 
 	svUnsetValue (ifcfg, "SUBCHANNELS");
 	s390_subchannels = nm_setting_wired_get_s390_subchannels (s_wired);