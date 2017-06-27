NetworkManager/NetworkManager

commit df0dc912cc6d9252759fb6de22f7607324c7ae0e

8021x: don't request secrets if they are empty and system owned
Empty secrets are fine. In particular, for PKCS#11 it means that protected
authentication path is used (the secrets are obtained on-demand from the
pinpad).

libnm-core/nm-setting-8021x.c
@@ -2778,7 +2778,7 @@ need_private_key_password (GBytes *blob,
 {
 	NMCryptoFileFormat format = NM_CRYPTO_FILE_FORMAT_UNKNOWN;
 
-	if (flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED)
+	if (flags == NM_SETTING_SECRET_FLAG_NONE || flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED)
 		return FALSE;
 
 	/* Private key password is required */
@@ -2822,13 +2822,15 @@ need_secrets_tls (NMSetting8021x *self,
 
 		scheme = nm_setting_802_1x_get_phase2_ca_cert_scheme (self);
 		if (    scheme == NM_SETTING_802_1X_CK_SCHEME_PKCS11
-		    && !(priv->phase2_ca_cert_password_flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED)
+		    && !(   priv->phase2_ca_cert_password_flags == NM_SETTING_SECRET_FLAG_NONE
+		         || priv->phase2_ca_cert_password_flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED)
 		    && !priv->phase2_ca_cert_password)
 			g_ptr_array_add (secrets, NM_SETTING_802_1X_PHASE2_CA_CERT_PASSWORD);
 
 		scheme = nm_setting_802_1x_get_phase2_client_cert_scheme (self);
 		if (    scheme == NM_SETTING_802_1X_CK_SCHEME_PKCS11
-		    && !(priv->phase2_client_cert_password_flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED)
+		    && !(   priv->phase2_client_cert_password_flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED
+		         || priv->phase2_client_cert_password_flags == NM_SETTING_SECRET_FLAG_NONE)
 		    && !priv->phase2_client_cert_password)
 			g_ptr_array_add (secrets, NM_SETTING_802_1X_PHASE2_CLIENT_CERT_PASSWORD);
 	} else {
@@ -2847,13 +2849,15 @@ need_secrets_tls (NMSetting8021x *self,
 
 		scheme = nm_setting_802_1x_get_ca_cert_scheme (self);
 		if (    scheme == NM_SETTING_802_1X_CK_SCHEME_PKCS11
-		    && !(priv->ca_cert_password_flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED)
+		    && !(   priv->ca_cert_password_flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED
+		         || priv->ca_cert_password_flags == NM_SETTING_SECRET_FLAG_NONE)
 		    && !priv->ca_cert_password)
 			g_ptr_array_add (secrets, NM_SETTING_802_1X_CA_CERT_PASSWORD);
 
 		scheme = nm_setting_802_1x_get_client_cert_scheme (self);
 		if (    scheme == NM_SETTING_802_1X_CK_SCHEME_PKCS11
-		    && !(priv->client_cert_password_flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED)
+		    && !(   priv->client_cert_password_flags == NM_SETTING_SECRET_FLAG_NONE
+		         || priv->client_cert_password_flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED)
 		    && !priv->client_cert_password)
 			g_ptr_array_add (secrets, NM_SETTING_802_1X_CLIENT_CERT_PASSWORD);
 	}

commit 699492c1a5509083aa87e770cc1df7de1a52f1ed

libnm-core: 8021x: fix check on private key password
Commit df0dc912cc6d ("8021x: don't request secrets if they are empty
and system owned") changed need_private_key_password() to return FALSE
when flags are NONE. This broke authentication using an encrypted
private key because after this the key password is never added to the
applied connection.

Don't require a password with NONE flags only for the PKCS11 scheme.

Fixes: df0dc912cc6d9252759fb6de22f7607324c7ae0e

libnm-core/nm-setting-8021x.c
@@ -2772,13 +2772,18 @@ need_secrets_sim (NMSetting8021x *self,
 
 static gboolean
 need_private_key_password (GBytes *blob,
+                           NMSetting8021xCKScheme scheme,
                            const char *path,
                            const char *password,
                            NMSettingSecretFlags flags)
 {
 	NMCryptoFileFormat format = NM_CRYPTO_FILE_FORMAT_UNKNOWN;
 
-	if (flags == NM_SETTING_SECRET_FLAG_NONE || flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED)
+	if (flags & NM_SETTING_SECRET_FLAG_NOT_REQUIRED)
+		return FALSE;
+
+	if (   scheme == NM_SETTING_802_1X_CK_SCHEME_PKCS11
+	    && flags == NM_SETTING_SECRET_FLAG_NONE)
 		return FALSE;
 
 	/* Private key password is required */
@@ -2815,7 +2820,7 @@ need_secrets_tls (NMSetting8021x *self,
 		else if (scheme != NM_SETTING_802_1X_CK_SCHEME_PKCS11)
 			g_warning ("%s: unknown phase2 private key scheme %d", __func__, scheme);
 
-		if (need_private_key_password (blob, path,
+		if (need_private_key_password (blob, scheme, path,
 		                               priv->phase2_private_key_password,
 		                               priv->phase2_private_key_password_flags))
 			g_ptr_array_add (secrets, NM_SETTING_802_1X_PHASE2_PRIVATE_KEY_PASSWORD);
@@ -2842,7 +2847,7 @@ need_secrets_tls (NMSetting8021x *self,
 		else if (scheme != NM_SETTING_802_1X_CK_SCHEME_PKCS11)
 			g_warning ("%s: unknown private key scheme %d", __func__, scheme);
 
-		if (need_private_key_password (blob, path,
+		if (need_private_key_password (blob, scheme, path,
 		                               priv->private_key_password,
 		                               priv->private_key_password_flags))
 			g_ptr_array_add (secrets, NM_SETTING_802_1X_PRIVATE_KEY_PASSWORD);