NetworkManager/NetworkManager

commit afac7621ae3b306701fa388e31f57b082a5b1510

clients: return NULL array on auto-completion failure
readline crashes if we return an empty completion list; return NULL
instead.

This is reproducible, for example, with:

 $ nmcli --ask connection add
 Interface name [*]: doesnotexist<TAB>
 Segmentation fault (core dumped)

 $ nmcli --ask connection add
 Interface name [*]:
 Connection type: avian-carr<TAB>
 Segmentation fault (core dumped)

clients/common/nm-meta-setting-access.c
@@ -294,13 +294,18 @@ nm_meta_abstract_info_complete (const NMMetaAbstractInfo *abstract_info,
 	if (*out_to_free) {
 		char **v = *out_to_free;
 
-		for (i =0, j = 0; v[i]; i++) {
+		for (i = 0, j = 0; v[i]; i++) {
 			if (strncmp (v[i], text, text_len) != 0)
 				continue;
 			v[j++] = v[i];
 		}
-		v[j++] = NULL;
-		return (const char *const*) *out_to_free;
+		if (j)
+			v[j++] = NULL;
+		else {
+			g_free (v);
+			v = NULL;
+		}
+		return (const char *const*) v;
 	} else {
 		const char *const*v = values;
 		char **r;
@@ -312,6 +317,8 @@ nm_meta_abstract_info_complete (const NMMetaAbstractInfo *abstract_info,
 		}
 		if (j == i)
 			return values;
+		else if (!j)
+			return NULL;
 
 		r = g_new (char *, j + 1);
 		v = values;

clients/common/nm-meta-setting-desc.c
@@ -1759,7 +1759,12 @@ _complete_fcn_vpn_service_type (ARGS_COMPLETE_FCN)
 				values[j] = values[i];
 			j++;
 		}
-		values[j++] = NULL;
+		if (j)
+			values[j++] = NULL;
+		else {
+			g_free (values);
+			values = NULL;
+		}
 	}
 	return (const char *const*) (*out_to_free = values);
 }
@@ -2378,7 +2383,12 @@ _complete_fcn_connection_type (ARGS_COMPLETE_FCN)
 				result[j++] = g_strdup (v);
 		}
 	}
-	result[j++] = NULL;
+	if (j)
+		result[j++] = NULL;
+	else {
+		g_free (result);
+		result = NULL;
+	}
 
 	return (const char *const*) (*out_to_free = result);
 }
@@ -2526,10 +2536,14 @@ _complete_fcn_connection_master (ARGS_COMPLETE_FCN)
 		if (v && (!text || strncmp (text, v, text_len) == 0))
 			result[j++] = g_strdup (v);
 	}
-	result[j++] = NULL;
+	if (j)
+		result[j++] = NULL;
+	else {
+		g_free (result);
+		result = NULL;
+	}
 
-	*out_to_free = NULL;
-	return (const char *const*) result;
+	return (const char *const*) (*out_to_free = result);
 }
 
 static gboolean

commit 8efeb3688cd25a53c324f595f774f80276f90d65

cli: fix returning no results in complete function
For consistency, never return an empty array @values.
If we have an empty array, instead return NULL.

Also fixes commit afac7621a "clients: return NULL array on
auto-completion failure", which claims that readline crashes
with empty strv arrays.

Fixes: afac7621ae3b306701fa388e31f57b082a5b1510

clients/common/nm-meta-setting-access.c
@@ -283,10 +283,18 @@ nm_meta_abstract_info_complete (const NMMetaAbstractInfo *abstract_info,
 
 	nm_assert (!*out_to_free || values == (const char *const*) *out_to_free);
 
-	if (!text || !text[0] || !values || !values[0])
+	if (!values)
+		return NULL;
+
+	if (!values[0]) {
+		nm_clear_g_free (out_to_free);
+		return NULL;
+	}
+
+	if (!text || !text[0])
 		return values;
 
-	/* for convenience, we all the complete_fcn() implementations to
+	/* for convenience, we allow the complete_fcn() implementations to
 	 * ignore "text". We filter out invalid matches here. */
 
 	text_len = strlen (text);