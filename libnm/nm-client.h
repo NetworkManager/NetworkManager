9f510fd662ef1247840e58760ae505f8dd2bf2c9
James Henstridge <james@jamesh.id.au>
2017-07-19 15:24:12 +0800

    client: expose connectivity-check-{available,enabled} properties

--- libnm/nm-client.h
+++ libnm/nm-client.h
@@ -50,6 +50,8 @@ G_BEGIN_DECLS
 #define NM_CLIENT_WIMAX_HARDWARE_ENABLED "wimax-hardware-enabled"
 #define NM_CLIENT_ACTIVE_CONNECTIONS "active-connections"
 #define NM_CLIENT_CONNECTIVITY "connectivity"
+#define NM_CLIENT_CONNECTIVITY_CHECK_AVAILABLE "connectivity-check-available"
+#define NM_CLIENT_CONNECTIVITY_CHECK_ENABLED "connectivity-check-enabled"
 #define NM_CLIENT_PRIMARY_CONNECTION "primary-connection"
 #define NM_CLIENT_ACTIVATING_CONNECTION "activating-connection"
 #define NM_CLIENT_DEVICES "devices"
@@ -248,6 +250,11 @@ gboolean nm_client_wwan_hardware_get_enabled (NMClient *client);
 gboolean nm_client_wimax_get_enabled (NMClient *client);
 void     nm_client_wimax_set_enabled (NMClient *client, gboolean enabled);
 gboolean nm_client_wimax_hardware_get_enabled (NMClient *client);
+
+gboolean nm_client_connectivity_check_get_available (NMClient *client);
+gboolean nm_client_connectivity_check_get_enabled (NMClient *client);
+void     nm_client_connectivity_check_set_enabled (NMClient *client,
+												   gboolean enabled);
 
 gboolean nm_client_get_logging (NMClient *client,
                                 char **level,
