NetworkManager/NetworkManager

commit 6e9aa9402a67bab563320fb0b37df242df15214c

core: allow NULL argument to nm-ip-config's for-each macros
Allow omitting the platform output argument. One can use the iterator to
access the current entry.

src/nm-ip4-config.h
@@ -46,10 +46,8 @@ nm_ip_config_iter_ip4_address_next (NMDedupMultiIter *ipconf_iter, const NMPlatf
 {
 	gboolean has_next;
 
-	g_return_val_if_fail (out_address, FALSE);
-
 	has_next = nm_dedup_multi_iter_next (ipconf_iter);
-	if (has_next)
+	if (has_next && out_address)
 		*out_address = NMP_OBJECT_CAST_IP4_ADDRESS (ipconf_iter->current->obj);
 	return has_next;
 }
@@ -59,21 +57,19 @@ nm_ip_config_iter_ip4_route_next (NMDedupMultiIter *ipconf_iter, const NMPlatfor
 {
 	gboolean has_next;
 
-	g_return_val_if_fail (out_route, FALSE);
-
 	has_next = nm_dedup_multi_iter_next (ipconf_iter);
-	if (has_next)
+	if (has_next && out_route)
 		*out_route = NMP_OBJECT_CAST_IP4_ROUTE (ipconf_iter->current->obj);
 	return has_next;
 }
 
 #define nm_ip_config_iter_ip4_address_for_each(iter, self, address) \
-    for (*(address) = NULL, nm_ip_config_iter_ip4_address_init ((iter), (self)); \
+    for (({ if (address) { *((const NMPlatformIP4Address **) (address)) = NULL; } }), nm_ip_config_iter_ip4_address_init ((iter), (self)); \
          nm_ip_config_iter_ip4_address_next ((iter), (address)); \
          )
 
 #define nm_ip_config_iter_ip4_route_for_each(iter, self, route) \
-    for (*(route) = NULL, nm_ip_config_iter_ip4_route_init ((iter), (self)); \
+    for (({ if (route) { *((const NMPlatformIP4Route **) (route)) = NULL; } }), nm_ip_config_iter_ip4_route_init ((iter), (self)); \
          nm_ip_config_iter_ip4_route_next ((iter), (route)); \
          )
 

src/nm-ip6-config.h
@@ -39,10 +39,8 @@ nm_ip_config_iter_ip6_address_next (NMDedupMultiIter *ipconf_iter, const NMPlatf
 {
 	gboolean has_next;
 
-	g_return_val_if_fail (out_address, FALSE);
-
 	has_next = nm_dedup_multi_iter_next (ipconf_iter);
-	if (has_next)
+	if (has_next && out_address)
 		*out_address = NMP_OBJECT_CAST_IP6_ADDRESS (ipconf_iter->current->obj);
 	return has_next;
 }
@@ -52,21 +50,19 @@ nm_ip_config_iter_ip6_route_next (NMDedupMultiIter *ipconf_iter, const NMPlatfor
 {
 	gboolean has_next;
 
-	g_return_val_if_fail (out_route, FALSE);
-
 	has_next = nm_dedup_multi_iter_next (ipconf_iter);
-	if (has_next)
+	if (has_next && out_route)
 		*out_route = NMP_OBJECT_CAST_IP6_ROUTE (ipconf_iter->current->obj);
 	return has_next;
 }
 
 #define nm_ip_config_iter_ip6_address_for_each(iter, self, address) \
-    for (*(address) = NULL, nm_ip_config_iter_ip6_address_init ((iter), (self)); \
+    for (({ if (address) { *(((const NMPlatformIP6Address **) address)) = NULL; } }), nm_ip_config_iter_ip6_address_init ((iter), (self)); \
          nm_ip_config_iter_ip6_address_next ((iter), (address)); \
          )
 
 #define nm_ip_config_iter_ip6_route_for_each(iter, self, route) \
-    for (*(route) = NULL, nm_ip_config_iter_ip6_route_init ((iter), (self)); \
+    for (({ if (route) { *((const NMPlatformIP6Route **) (route)) = NULL; } }), nm_ip_config_iter_ip6_route_init ((iter), (self)); \
          nm_ip_config_iter_ip6_route_next ((iter), (route)); \
          )
 

commit d686636bf043a6f02e592658cad7ef6a091003d2

core: avoid compiler warning in nm_ip_config_iter_ip4_address_for_each() macro
  In file included from src/nm-ip6-config.c:24:0:
  src/nm-ip6-config.c: In function ‘nm_ip6_config_create_setting’:
  src/nm-ip6-config.c:734:62: error: the address of ‘address’ will always evaluate as ‘true’ [-Werror=address]
    nm_ip_config_iter_ip6_address_for_each (&ipconf_iter, self, &address) {
                                                                ^
  src/nm-ip6-config.h:60:17: note: in definition of macro ‘nm_ip_config_iter_ip6_address_for_each’
       for (({ if (address) { *(((const NMPlatformIP6Address **) address)) = NULL; } }), nm_ip_config_iter_ip6_address_init ((iter), (self)); \
                   ^

Fixes: 6e9aa9402a67bab563320fb0b37df242df15214c

shared/nm-utils/nm-macros-internal.h
@@ -687,6 +687,21 @@ nm_clear_g_cancellable (GCancellable **cancellable)
 
 /*****************************************************************************/
 
+/* Only useful inside a macro to avoid compiler warning.
+ *
+ * Otherwise compiler might warn for "if (ptr)"
+ *   the address of ‘a’ will always evaluate as ‘true’ [-Werror=address]
+ *
+ * The trivial function avoids the compiler warning.
+ */
+static inline int
+_nm_is_null (const void *ptr)
+{
+	return !!ptr;
+}
+
+/*****************************************************************************/
+
 /* Determine whether @x is a power of two (@x being an integer type).
  * Basically, this returns TRUE, if @x has exactly one bit set.
  * For negative values and zero, this always returns FALSE. */

src/nm-ip4-config.h
@@ -64,12 +64,12 @@ nm_ip_config_iter_ip4_route_next (NMDedupMultiIter *ipconf_iter, const NMPlatfor
 }
 
 #define nm_ip_config_iter_ip4_address_for_each(iter, self, address) \
-    for (({ if (address) { *((const NMPlatformIP4Address **) (address)) = NULL; } }), nm_ip_config_iter_ip4_address_init ((iter), (self)); \
+    for (({ if (!_nm_is_null (address)) { *((const NMPlatformIP4Address **) (address)) = NULL; } }), nm_ip_config_iter_ip4_address_init ((iter), (self)); \
          nm_ip_config_iter_ip4_address_next ((iter), (address)); \
          )
 
 #define nm_ip_config_iter_ip4_route_for_each(iter, self, route) \
-    for (({ if (route) { *((const NMPlatformIP4Route **) (route)) = NULL; } }), nm_ip_config_iter_ip4_route_init ((iter), (self)); \
+    for (({ if (!_nm_is_null (route)) { *((const NMPlatformIP4Route **) (route)) = NULL; } }), nm_ip_config_iter_ip4_route_init ((iter), (self)); \
          nm_ip_config_iter_ip4_route_next ((iter), (route)); \
          )
 

src/nm-ip6-config.h
@@ -57,12 +57,12 @@ nm_ip_config_iter_ip6_route_next (NMDedupMultiIter *ipconf_iter, const NMPlatfor
 }
 
 #define nm_ip_config_iter_ip6_address_for_each(iter, self, address) \
-    for (({ if (address) { *(((const NMPlatformIP6Address **) address)) = NULL; } }), nm_ip_config_iter_ip6_address_init ((iter), (self)); \
+    for (({ if (!_nm_is_null (address)) { *(((const NMPlatformIP6Address **) address)) = NULL; } }), nm_ip_config_iter_ip6_address_init ((iter), (self)); \
          nm_ip_config_iter_ip6_address_next ((iter), (address)); \
          )
 
 #define nm_ip_config_iter_ip6_route_for_each(iter, self, route) \
-    for (({ if (route) { *((const NMPlatformIP6Route **) (route)) = NULL; } }), nm_ip_config_iter_ip6_route_init ((iter), (self)); \
+    for (({ if (!_nm_is_null (route)) { *((const NMPlatformIP6Route **) (route)) = NULL; } }), nm_ip_config_iter_ip6_route_init ((iter), (self)); \
          nm_ip_config_iter_ip6_route_next ((iter), (route)); \
          )
 