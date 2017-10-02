b0f3dc0add9dc74ef1192f2996b420b159add9af
Lubomir Rintel <lkundrak@v3.sk>
2017-10-02 08:39:09 +0200

    introspection: add o.fd.NM.Device.OvsInterface interface

--- src/devices/nm-device.c
+++ src/devices/nm-device.c
@@ -1661,6 +1661,8 @@ _get_route_metric_default (NMDevice *self)
 		return 700;
 	case NM_DEVICE_TYPE_BT:
 		return 750;
+	case NM_DEVICE_TYPE_OVS_INTERFACE:
+		return 800;
 	case NM_DEVICE_TYPE_GENERIC:
 		return 950;
 	case NM_DEVICE_TYPE_UNKNOWN:
