NetworkManager/NetworkManager

commit ddfeed4530bb0ade4249025930da4f0456026d9f

dhcp: set @was_active flag for external activations
Set the @was_active flag for external activations with DHCP, so that
DHCP is retried multiple times in case of failure, as we do for
managed connections when the lease expires and for assumed
connections.

Fixes test: renewal_gw_after_dhcp_outage_for_assumed_var1
Fixes: e3113fdc4b01d79080911b7450fc032dc0a85108

src/devices/nm-device.c
@@ -5844,7 +5844,7 @@ dhcp4_start (NMDevice *self,
 
 	nm_device_add_pending_action (self, NM_PENDING_ACTION_DHCP4, TRUE);
 
-	if (nm_device_sys_iface_state_get (self) == NM_DEVICE_SYS_IFACE_STATE_ASSUME)
+	if (nm_device_sys_iface_state_is_external_or_assume (self))
 		priv->dhcp4.was_active = TRUE;
 
 	/* DHCP devices will be notified by the DHCP manager when stuff happens */
@@ -6645,8 +6645,8 @@ dhcp6_start_with_link_ready (NMDevice *self, NMConnection *connection)
 		                                             self);
 	}
 
-	if (nm_device_sys_iface_state_get (self) == NM_DEVICE_SYS_IFACE_STATE_ASSUME)
-		priv->dhcp6.was_active = TRUE;
+	if (nm_device_sys_iface_state_is_external_or_assume (self))
+		priv->dhcp4.was_active = TRUE;
 
 	return !!priv->dhcp6.client;
 }

commit 2776606aa35d9808880e64c40acbc7048f970957

dhcp: set @was_active on ipv6 when assuming a device with dhcp ipv6 conf
Due to a typo the flag was set on ipv4

Fixes: ddfeed4530bb0ade4249025930da4f0456026d9f

src/devices/nm-device.c
@@ -6903,7 +6903,7 @@ dhcp6_start_with_link_ready (NMDevice *self, NMConnection *connection)
 	}
 
 	if (nm_device_sys_iface_state_is_external_or_assume (self))
-		priv->dhcp4.was_active = TRUE;
+		priv->dhcp6.was_active = TRUE;
 
 	return !!priv->dhcp6.client;
 }