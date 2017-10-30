NetworkManager/NetworkManager

commit 956b3e8bd580da03473fb6930cede23f4e188640

shared: add nm_steal_fd() helper function

shared/nm-utils/nm-macros-internal.h
@@ -1151,6 +1151,19 @@ nm_decode_version (guint version, guint *major, guint *minor, guint *micro)
 
 /*****************************************************************************/
 
+static inline int
+nm_steal_fd (int *p_fd)
+{
+	int fd;
+
+	if (   p_fd
+	    && ((fd = *p_fd) > 0)) {
+		*p_fd = -1;
+		return fd;
+	}
+	return -1;
+}
+
 /**
  * nm_close:
  *

commit de65d3da912edcb5a4277aba676531e1951a790d

shared: fix nm_steal_fd() for handling zero file descriptor
Zero is a valid file descriptor.

Fixes: 956b3e8bd580da03473fb6930cede23f4e188640

shared/nm-utils/nm-macros-internal.h
@@ -1157,7 +1157,7 @@ nm_steal_fd (int *p_fd)
 	int fd;
 
 	if (   p_fd
-	    && ((fd = *p_fd) > 0)) {
+	    && ((fd = *p_fd) >= 0)) {
 		*p_fd = -1;
 		return fd;
 	}