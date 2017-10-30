NetworkManager/NetworkManager

commit 98afc76184502d8e19092d631efb07091be81af2

core: avoid coverity warning in device factory's _add_factory()
2. NetworkManager-1.9.2/src/devices/nm-device-factory.c:312:
returned_pointer: Assigning value from "g_slist_append(list,
g_object_ref(factory))" to "list" here, but that stored value is
overwritten before it can be used.

src/devices/nm-device-factory.c
@@ -309,7 +309,7 @@ _add_factory (NMDeviceFactory *factory,
 	for (i = 0; setting_types && setting_types[i]; i++) {
 		list = g_hash_table_lookup (factories_by_setting, (char *) setting_types[i]);
 		if (list)
-			list = g_slist_append (list, g_object_ref (factory));
+			g_slist_append (list, g_object_ref (factory));
 		else {
 			list = g_slist_append (list, g_object_ref (factory));
 			g_hash_table_insert (factories_by_setting, (char *) setting_types[i], list);

commit 449940af1d8e8c996e65e266407d857680fc4a9b

core: avoid coverity warning in device factory's _add_factory() (2)
2. NetworkManager-1.9.2/src/devices/nm-device-factory.c:312:
returned_pointer: Assigning value from "g_slist_append(list,
g_object_ref(factory))" to "list" here, but that stored value is
overwritten before it can be used.

Fixes: 98afc76184502d8e19092d631efb07091be81af2

src/devices/nm-device-factory.c
@@ -309,7 +309,7 @@ _add_factory (NMDeviceFactory *factory,
 	for (i = 0; setting_types && setting_types[i]; i++) {
 		list = g_hash_table_lookup (factories_by_setting, (char *) setting_types[i]);
 		if (list)
-			g_slist_append (list, g_object_ref (factory));
+			(void) g_slist_append (list, g_object_ref (factory));
 		else {
 			list = g_slist_append (list, g_object_ref (factory));
 			g_hash_table_insert (factories_by_setting, (char *) setting_types[i], list);