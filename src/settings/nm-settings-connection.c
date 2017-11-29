NetworkManager/NetworkManager

commit 36ac08c092afe7a14b0fe64ada8b6d6f43a1b8c7

policy: add nm_settings_connection_autoconnect_is_blocked() helper function

src/settings/nm-settings-connection.c
@@ -2646,6 +2646,15 @@ nm_settings_connection_autoconnect_blocked_reason_set_full (NMSettingsConnection
 	return TRUE;
 }
 
+gboolean
+nm_settings_connection_autoconnect_is_blocked (NMSettingsConnection *self)
+{
+	NMSettingsConnectionPrivate *priv = NM_SETTINGS_CONNECTION_GET_PRIVATE (self);
+
+	return    !priv->autoconnect_blocked_reason
+	       && priv->autoconnect_retries != 0;
+}
+
 /*****************************************************************************/
 
 /**

src/nm-policy.c
@@ -1222,8 +1222,7 @@ auto_activate_device (NMPolicy *self,
 		const char *permission;
 
 		if (   !nm_settings_connection_is_visible (candidate)
-		    || nm_settings_connection_autoconnect_retries_get (candidate) == 0
-		    || nm_settings_connection_autoconnect_blocked_reason_get (candidate, NM_SETTINGS_AUTO_CONNECT_BLOCKED_REASON_ALL))
+		    || nm_settings_connection_autoconnect_is_blocked (candidate))
 			continue;
 
 		s_con = nm_connection_get_setting_connection (NM_CONNECTION (candidate));

src/settings/nm-settings-connection.h
@@ -239,6 +239,8 @@ nm_settings_connection_autoconnect_blocked_reason_set (NMSettingsConnection *sel
 	return nm_settings_connection_autoconnect_blocked_reason_set_full (self, mask, set ? mask : NM_SETTINGS_AUTO_CONNECT_BLOCKED_REASON_NONE);
 }
 
+gboolean nm_settings_connection_autoconnect_is_blocked (NMSettingsConnection *self);
+
 gboolean nm_settings_connection_get_nm_generated (NMSettingsConnection *self);
 gboolean nm_settings_connection_get_volatile (NMSettingsConnection *self);
 

commit 249aff6349f6ff9068fd3168f7a75ef445b46e1f

core: fix broken autoconnect due to wrong nm_settings_connection_autoconnect_is_blocked()
Fixes: 36ac08c092afe7a14b0fe64ada8b6d6f43a1b8c7

src/settings/nm-settings-connection.c
@@ -2672,8 +2672,8 @@ nm_settings_connection_autoconnect_is_blocked (NMSettingsConnection *self)
 {
 	NMSettingsConnectionPrivate *priv = NM_SETTINGS_CONNECTION_GET_PRIVATE (self);
 
-	return    !priv->autoconnect_blocked_reason
-	       && priv->autoconnect_retries != 0;
+	return    priv->autoconnect_blocked_reason != NM_SETTINGS_AUTO_CONNECT_BLOCKED_REASON_NONE
+	       || priv->autoconnect_retries == 0;
 }
 
 /*****************************************************************************/