23b7f24d9e7d5d0a126226a576b5309856d71154
Lubomir Rintel <lkundrak@v3.sk>
2017-12-09 11:06:43 +0100

    platform/tests: drop bad comment

--- src/platform/tests/test-route.c
+++ src/platform/tests/test-route.c
@@ -1,6 +1,5 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
-/* NetworkManager audit support
- *
+/*
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
