16f8651908dbda630047230257802433580510a3
Lubomir Rintel <lkundrak@v3.sk>
2017-12-18 08:36:47 +0100

    libnm/vpn: fix secret comparison
    One setting may contain a superset or subset of the another. Be sure not
    to ignore the presence of extra secret properties.

--- libnm-core/nm-setting-vpn.c
+++ libnm-core/nm-setting-vpn.c
@@ -713,17 +713,15 @@ need_secrets (NMSetting *setting)
 }
 
 static gboolean
-compare_one_secret (NMSettingVpn *a,
-                    NMSettingVpn *b,
-                    NMSettingCompareFlags flags)
+_compare_secrets (NMSettingVpn *a,
+                  NMSettingVpn *b,
+                  NMSettingCompareFlags flags)
 {
-	GHashTable *a_secrets, *b_secrets;
+	GHashTable *a_secrets;
 	GHashTableIter iter;
 	const char *key, *val;
 
 	a_secrets = NM_SETTING_VPN_GET_PRIVATE (a)->secrets;
-	b_secrets = NM_SETTING_VPN_GET_PRIVATE (b)->secrets;
-
 	g_hash_table_iter_init (&iter, a_secrets);
 	while (g_hash_table_iter_next (&iter, (gpointer) &key, (gpointer) &val)) {
 		NMSettingSecretFlags a_secret_flags = NM_SETTING_SECRET_FLAG_NONE;
@@ -748,6 +746,19 @@ compare_one_secret (NMSettingVpn *a,
 		if (g_strcmp0 (val, nm_setting_vpn_get_secret (b, key)) != 0)
 			return FALSE;
 	}
+
+	return TRUE;
+}
+
+static gboolean
+compare_one_secret (NMSettingVpn *a,
+                    NMSettingVpn *b,
+                    NMSettingCompareFlags flags)
+{
+	if (!_compare_secrets (a, b, flags))
+		return FALSE;
+	if (!_compare_secrets (b, a, flags))
+		return FALSE;
 
 	return TRUE;
 }
