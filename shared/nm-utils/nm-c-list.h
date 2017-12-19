2aad517b0b3b29615d1afdc907107a24cca6e1f2
Thomas Haller <thaller@redhat.com>
2017-12-19 14:27:18 +0100

    shared: add NMCListElem
    Sometimes, we want to use CList to track a simple data item. But contrary
    to GList/GSList, we need to define a structure to hold the data pointer
    and the CList member.
    
    Add a generic NMCListElem type that can be used for such simple uses.
    
    Before you ask: why not use GList/GSList? Because even simple operations
    like g_list_append() is O(n), which kinda defeats the purpose of having
    a doubly linked list.
    
    This code is added to a new header file nm-c-list.h, the reason is that
    there is no other good place:
      - "nm-utils/c-list.h" is a clone of upstream, it should not deviate.
      - "nm-utils/c-list-util.h" contains our utils functions for c-list.h
        but should be plain C, independent of glib.
      - "nm-utils/nm-shared-utils.h" contains our glib related utilities,
        but it should not drag in "c-list.h".
    So, "nm-c-list.h" is a utility libray that extends "c-list.h" and
    requires glib.

---
+++ shared/nm-utils/nm-c-list.h
@@ -0,0 +1,74 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* NetworkManager -- Network link manager
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ * (C) Copyright 2014 Red Hat, Inc.
+ */
+
+#ifndef __NM_C_LIST_H__
+#define __NM_C_LIST_H__
+
+#include "c-list.h"
+
+/*****************************************************************************/
+
+typedef struct {
+	CList lst;
+	void *data;
+} NMCListElem;
+
+static inline NMCListElem *
+nm_c_list_elem_new_stale (void *data)
+{
+	NMCListElem *elem;
+
+	elem = g_slice_new (NMCListElem);
+	elem->data = data;
+	return elem;
+}
+
+static inline void *
+nm_c_list_elem_get (CList *lst)
+{
+	if (!lst)
+		return NULL;
+	return c_list_entry (lst, NMCListElem, lst)->data;
+}
+
+static inline void
+nm_c_list_elem_free (NMCListElem *elem)
+{
+	if (elem) {
+		c_list_unlink_stale (&elem->lst);
+		g_slice_free (NMCListElem, elem);
+	}
+}
+
+static inline void
+nm_c_list_elem_free_all (CList *head, GDestroyNotify free_fcn)
+{
+	NMCListElem *elem;
+
+	while ((elem = c_list_first_entry (head, NMCListElem, lst))) {
+		if (free_fcn)
+			free_fcn (elem->data);
+		c_list_unlink_stale (&elem->lst);
+		g_slice_free (NMCListElem, elem);
+	}
+}
+
+#endif /* __NM_C_LIST_H__ */
