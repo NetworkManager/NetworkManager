1443bf77e8cefc6f2d49f1e7f4140155ad73551b
Lubomir Rintel <lkundrak@v3.sk>
2018-01-02 15:41:05 +0100

    all: require jansson 2.5
    It was released Sep 19 2013 and Ubuntu 14.04 LTS (trusty) ships it.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=792323

--- shared/nm-utils/nm-jansson.h
+++ shared/nm-utils/nm-jansson.h
@@ -26,27 +26,6 @@
 
 #include <jansson.h>
 
-/* Added in Jansson v2.3 (released Jan 27 2012) */
-#ifndef json_object_foreach
-#define json_object_foreach(object, key, value) \
-    for(key = json_object_iter_key(json_object_iter(object)); \
-        key && (value = json_object_iter_value(json_object_iter_at (object, key) )); \
-        key = json_object_iter_key(json_object_iter_next(object, json_object_iter_at (object, key))))
-#endif
-
-/* Added in Jansson v2.4 (released Sep 23 2012), but travis.ci has v2.2. */
-#ifndef json_boolean
-#define json_boolean(val) ((val) ? json_true() : json_false())
-#endif
-
-/* Added in Jansson v2.5 (released Sep 19 2013), but travis.ci has v2.2. */
-#ifndef json_array_foreach
-#define json_array_foreach(array, index, value) \
-    for(index = 0; \
-        index < json_array_size(array) && (value = json_array_get(array, index)); \
-        index++)
-#endif
-
 /* Added in Jansson v2.7 */
 #ifndef json_boolean_value
 #define json_boolean_value json_is_true
@@ -54,21 +33,12 @@
 
 /* Added in Jansson v2.8 */
 #ifndef json_object_foreach_safe
-#if JANSSON_VERSION_HEX < 0x020300
-#define json_object_foreach_safe(object, n, key, value)     \
-    for (key = json_object_iter_key (json_object_iter (object)), \
-         n = json_object_iter_next (object, json_object_iter_at (object, key)); \
-         key && (value = json_object_iter_value (json_object_iter_at (object, key))); \
-         key = json_object_iter_key (n), \
-         n = json_object_iter_next (object, json_object_iter_at (object, key)))
-#else
 #define json_object_foreach_safe(object, n, key, value)     \
     for(key = json_object_iter_key(json_object_iter(object)), \
             n = json_object_iter_next(object, json_object_key_to_iter(key)); \
         key && (value = json_object_iter_value(json_object_key_to_iter(key))); \
         key = json_object_iter_key(n), \
             n = json_object_iter_next(object, json_object_key_to_iter(key)))
-#endif
 #endif
 
 #endif /* WITH_JANSON */
