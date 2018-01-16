9ef17869b5fa775bd7f1da14471f52775efe1017
Thomas Haller <thaller@redhat.com>
2018-01-16 15:42:43 +0100

    version: drop NM_VERSION_MAX_ALLOWED defines for internal build
    It already defaults to the right value. We only need to define
    NM_VERSION_MIN_REQUIRED, so that parts of our internal build
    can make use of deprecated API.

--- shared/nm-default.h
+++ shared/nm-default.h
@@ -115,7 +115,6 @@
 /* for internal compilation we don't want the deprecation macros
  * to be in effect. Define the widest range of versions to effectively
  * disable deprecation checks */
-#define NM_VERSION_MAX_ALLOWED   NM_VERSION_CUR_STABLE
 #define NM_VERSION_MIN_REQUIRED  NM_VERSION_0_9_8
 
 #ifndef NM_MORE_ASSERTS
