NetworkManager/NetworkManager

commit 8a46b25cfa8c33daa8af37bb8103ca02286001b1

all: require glib 2.40
RHEL 7.1 and Ubuntu 14.04 LTS both have this.

https://bugzilla.gnome.org/show_bug.cgi?id=792323

configure.ac
@@ -234,14 +234,14 @@ AC_SUBST([DL_LIBS], "$ac_cv_search_dlopen")
 PKG_CHECK_MODULES(GLIB, [gio-unix-2.0 >= 2.37.6 gmodule-2.0],
                   [AC_SUBST(LOG_DRIVER, '$(top_srcdir)/build-aux/tap-driver.sh')
                    AC_SUBST(AM_TESTS_FD_REDIRECT, '--tap')],
-                  [PKG_CHECK_MODULES(GLIB, gio-unix-2.0 >= 2.32 gmodule-2.0)
+                  [PKG_CHECK_MODULES(GLIB, gio-unix-2.0 >= 2.40 gmodule-2.0)
                    AC_SUBST(LOG_DRIVER, '$(top_srcdir)/build-aux/test-driver')])
 
 dnl GLIB_VERSION_MIN_REQUIRED should match the version above.
 dnl GLIB_VERSION_MAX_ALLOWED should be set to the same version;
 dnl nm-glib.h will cause it to be overridden for the functions
 dnl we have compat versions of.
-GLIB_CFLAGS="$GLIB_CFLAGS -DGLIB_VERSION_MIN_REQUIRED=GLIB_VERSION_2_32 -DGLIB_VERSION_MAX_ALLOWED=GLIB_VERSION_2_32"
+GLIB_CFLAGS="$GLIB_CFLAGS -DGLIB_VERSION_MIN_REQUIRED=GLIB_VERSION_2_40 -DGLIB_VERSION_MAX_ALLOWED=GLIB_VERSION_2_40"
 
 AC_SUBST(GLIB_CFLAGS)
 AC_SUBST(GLIB_LIBS)

docs/libnm-glib/Makefile.am
@@ -15,9 +15,6 @@ DOC_SOURCE_DIR=$(top_srcdir)/libnm-glib
 # Extra options to supply to gtkdoc-scan.
 SCAN_OPTIONS=
 
-# Extra options to supply to gtkdoc-scangobj.
-SCANGOBJ_OPTIONS=--type-init-func 'g_type_init();'
-
 # Extra options to supply to gtkdoc-mkdb.
 MKDB_OPTIONS=--sgml-mode --output-format=xml
 

docs/libnm-util/Makefile.am
@@ -17,9 +17,6 @@ DOC_SOURCE_DIR= \
 # Extra options to supply to gtkdoc-scan.
 SCAN_OPTIONS=--rebuild-types --rebuild-sections
 
-# Extra options to supply to gtkdoc-scangobj.
-SCANGOBJ_OPTIONS=--type-init-func 'g_type_init();'
-
 # Extra options to supply to gtkdoc-mkdb.
 MKDB_OPTIONS=--sgml-mode --output-format=xml
 

docs/libnm/Makefile.am
@@ -19,9 +19,6 @@ DOC_SOURCE_DIR= \
 # Extra options to supply to gtkdoc-scan.
 SCAN_OPTIONS=--rebuild-types --rebuild-sections
 
-# Extra options to supply to gtkdoc-scangobj.
-SCANGOBJ_OPTIONS=--type-init-func 'g_type_init();'
-
 # Extra options to supply to gtkdoc-mkdb.
 MKDB_OPTIONS=--sgml-mode --output-format=xml
 

clients/cli/nmcli.c
@@ -626,8 +626,6 @@ main (int argc, char *argv[])
 	textdomain (GETTEXT_PACKAGE);
 #endif
 
-	nm_g_type_init ();
-
 	/* Save terminal settings */
 	tcgetattr (STDIN_FILENO, &termios_orig);
 

clients/common/tests/test-general.c
@@ -86,7 +86,7 @@ test_client_meta_check (void)
 				g_assert (pi->setting_info == info);
 				g_assert (pi->property_name);
 
-				g_assert (nm_g_hash_table_add (property_names, (gpointer) pi->property_name));
+				g_assert (g_hash_table_add (property_names, (gpointer) pi->property_name));
 
 				g_assert_cmpstr (pi->property_name, ==, pi->meta_type->get_name ((const NMMetaAbstractInfo *) pi, FALSE));
 				g_assert_cmpstr (pi->property_name, ==, pi->meta_type->get_name ((const NMMetaAbstractInfo *) pi, TRUE));
@@ -107,7 +107,7 @@ test_client_meta_check (void)
 
 			for (i = 0; info->valid_parts[i]; i++) {
 				g_assert (info->valid_parts[i]->setting_info);
-				g_assert (nm_g_hash_table_add (dup, (gpointer) info->valid_parts[i]->setting_info));
+				g_assert (g_hash_table_add (dup, (gpointer) info->valid_parts[i]->setting_info));
 
 				if (i == 0) {
 					g_assert (info->valid_parts[i]->setting_info == &nm_meta_setting_infos_editor[NM_META_SETTING_TYPE_CONNECTION]);

clients/nm-online.c
@@ -244,8 +244,6 @@ main (int argc, char *argv[])
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
 
-	nm_g_type_init ();
-
 	data.start_timestamp_ms = _now_ms ();
 
 	opt_ctx = g_option_context_new (NULL);

clients/tui/newt/nmt-newt-utils.c
@@ -359,21 +359,11 @@ nmt_newt_edit_string (const char *data)
 		goto done;
 	}
 
-#if GLIB_CHECK_VERSION (2, 34, 0)
-	G_GNUC_BEGIN_IGNORE_DEPRECATIONS
 	if (!g_spawn_check_exit_status (status, &error)) {
 		nmt_newt_message_dialog (_("Editor failed: %s"), error->message);
 		g_error_free (error);
 		goto done;
 	}
-	G_GNUC_END_IGNORE_DEPRECATIONS
-#else
-	if (WIFEXITED (status)) {
-		if (WEXITSTATUS (status) != 0)
-			nmt_newt_message_dialog (_("Editor failed with status %d"), WEXITSTATUS (status));
-	} else if (WIFSIGNALED (status))
-		nmt_newt_message_dialog (_("Editor failed with signal %d"), WTERMSIG (status));
-#endif
 
 	if (!g_file_get_contents (filename, &new_data, NULL, &error)) {
 		nmt_newt_message_dialog (_("Could not re-read file: %s"), error->message);

commit 997cce7c90bf059f93fa9391bad591c56688e4ac

build: fix glib dependency to require at least 2.40
Fixes: 8a46b25cfa8c33daa8af37bb8103ca02286001b1

Makefile.am
@@ -164,14 +164,7 @@ DISTCLEANFILES += $(polkit_policy_DATA)
 noinst_LTLIBRARIES += \
 	introspection/libnmdbus.la
 
-# gdbus-codegen 2.38 will emit code that requires glib 2.38, which
-# will then cause availability warnings if we define
-# GLIB_VERSION_MAX_ALLOWED=GLIB_VERSION_2_32.
-#
-# This is fixed in GLib 2.40 (it emits code that takes
-# GLIB_VERSION_MAX_ALLOWED into account), so this workaround can go
-# away when we depend on that.
-introspection_libnmdbus_la_CPPFLAGS = $(filter-out -DGLIB_VERSION_MAX_ALLOWED%,$(GLIB_CFLAGS))
+introspection_libnmdbus_la_CPPFLAGS = $(GLIB_CFLAGS)
 
 introspection_sources = \
 	introspection/org.freedesktop.NetworkManager.AccessPoint.c \

configure.ac
@@ -243,10 +243,6 @@ PKG_CHECK_MODULES(GLIB, [gio-unix-2.0 >= 2.37.6 gmodule-2.0],
                   [PKG_CHECK_MODULES(GLIB, gio-unix-2.0 >= 2.40 gmodule-2.0)
                    AC_SUBST(LOG_DRIVER, '$(top_srcdir)/build-aux/test-driver')])
 
-dnl GLIB_VERSION_MIN_REQUIRED should match the version above.
-dnl GLIB_VERSION_MAX_ALLOWED should be set to the same version;
-dnl nm-glib.h will cause it to be overridden for the functions
-dnl we have compat versions of.
 GLIB_CFLAGS="$GLIB_CFLAGS -DGLIB_VERSION_MIN_REQUIRED=GLIB_VERSION_2_40 -DGLIB_VERSION_MAX_ALLOWED=GLIB_VERSION_2_40"
 
 AC_SUBST(GLIB_CFLAGS)

contrib/fedora/rpm/NetworkManager.spec
@@ -126,7 +126,7 @@ BuildRequires: dbus-glib-devel >= %{dbus_glib_version}
 %if 0%{?fedora}
 BuildRequires: wireless-tools-devel >= %{wireless_tools_version}
 %endif
-BuildRequires: glib2-devel >= 2.32.0
+BuildRequires: glib2-devel >= 2.40.0
 BuildRequires: gobject-introspection-devel >= 0.10.3
 BuildRequires: gettext-devel
 BuildRequires: pkgconfig

introspection/meson.build
@@ -74,17 +74,6 @@ install_data(
   install_dir: dbus_ifaces_dir
 )
 
-'''
-# gdbus-codegen 2.38 will emit code that requires glib 2.38, which
-# will then cause availability warnings if we define
-# GLIB_VERSION_MAX_ALLOWED=GLIB_VERSION_2_32.
-#
-# This is fixed in GLib 2.40 (it emits code that takes
-# GLIB_VERSION_MAX_ALLOWED into account), so this workaround can go
-# away when we depend on that.
-introspection_libnmdbus_la_CPPFLAGS = $(filter-out -DGLIB_VERSION_MAX_ALLOWED%,$(GLIB_CFLAGS))
-'''
-
 libnmdbus = static_library(
   'nmdbus',
   sources: sources,

meson.build
@@ -209,23 +209,18 @@ config_h.set10('HAVE_LIBSYSTEMD', libsystemd_dep.found())
 systemd_dep = dependency('systemd', required: false)
 have_systemd_200 = systemd_dep.found() and systemd_dep.version().version_compare('>= 200')
 
-gio_unix_dep = dependency('gio-unix-2.0', version: '>= 2.32')
+gio_unix_dep = dependency('gio-unix-2.0', version: '>= 2.40')
 
 log_driver = join_paths(meson.source_root(), 'build-aux', (gio_unix_dep.version().version_compare('>= 2.37.6') ? 'tap-driver.sh' : 'test-driver'))
 
-# FIXME: same version? which version?
-# GLIB_VERSION_MIN_REQUIRED should match the version above.
-# GLIB_VERSION_MAX_ALLOWED should be set to the same version;
-# nm-glib.h will cause it to be overridden for the functions
-# we have compat versions of.
 glib_dep = declare_dependency(
   dependencies: [
     gio_unix_dep,
     dependency('gmodule-2.0')
   ],
   compile_args: [
-    '-DGLIB_VERSION_MIN_REQUIRED=GLIB_VERSION_2_32',
-    '-DGLIB_VERSION_MAX_ALLOWED=GLIB_VERSION_2_32'
+    '-DGLIB_VERSION_MIN_REQUIRED=GLIB_VERSION_2_40',
+    '-DGLIB_VERSION_MAX_ALLOWED=GLIB_VERSION_2_40'
   ]
 )
 