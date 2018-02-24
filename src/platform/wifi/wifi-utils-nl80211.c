NetworkManager/NetworkManager

commit d7108d9362a3a87e59652b661213d1bd19beb10a

platform/wifi: drop an unused variable
  src/platform/wifi/wifi-utils-nl80211.c:192:31: error: unused variable 'msg_free' [-Werror,-Wunused-variable]
          nm_auto_nlmsg struct nl_msg *msg_free = msg;

Fixes: a7bda2ed12b958fd605e78a2512aa7246046983a

src/platform/wifi/wifi-utils-nl80211.c
@@ -189,7 +189,6 @@ _nl80211_send_and_recv (struct nl_sock *nl_sock,
                         int (*valid_handler) (struct nl_msg *, void *),
                         void *valid_data)
 {
-	nm_auto_nlmsg struct nl_msg *msg_free = msg;
 	int err;
 	int done = 0;
 	const struct nl_cb cb = {

commit a79e6b3b455c6aa7f3bd4f7abf6ec86bc9660284

platform/wifi: fix memleak in _nl80211_send_and_recv()
The callers expect _nl80211_send_and_recv() to free @msg.

This was broken by the previous commit, which wrongly removed
the nm_auto_nlmsg cleanup attribute.

Fix the compiler warning differently.

Fixes: d7108d9362a3a87e59652b661213d1bd19beb10a

src/platform/wifi/wifi-utils-nl80211.c
@@ -185,10 +185,11 @@ nl80211_alloc_msg (WifiDataNl80211 *nl80211, guint32 cmd, guint32 flags)
 /* NOTE: this function consumes 'msg' */
 static int
 _nl80211_send_and_recv (struct nl_sock *nl_sock,
-                        struct nl_msg *msg,
+                        struct nl_msg *inmsg,
                         int (*valid_handler) (struct nl_msg *, void *),
                         void *valid_data)
 {
+	nm_auto_nlmsg struct nl_msg *msg = inmsg;
 	int err;
 	int done = 0;
 	const struct nl_cb cb = {