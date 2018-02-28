NetworkManager/NetworkManager

commit 0f6baeef35550e7f151a817fb8e2988ae88198b7

core: verify secrets when adding connection via D-Bus (bgo #757843)
Fixes: dbbedce21f0dbb2849ed48bd2cee3b98e1ad2135
       libnm: don't check for valid passwords in NMSetting:verify()

https://bugzilla.gnome.org/show_bug.cgi?id=757843

src/settings/nm-settings.c
@@ -1407,7 +1407,11 @@ impl_settings_add_connection_helper (NMSettings *self,
 	GError *error = NULL;
 
 	connection = nm_simple_connection_new_from_dbus (settings, &error);
+
 	if (connection) {
+		if (!nm_connection_verify_secrets (connection, &error))
+			goto failure;
+
 		nm_settings_add_connection_dbus (self,
 		                                 connection,
 		                                 save_to_disk,
@@ -1415,10 +1419,12 @@ impl_settings_add_connection_helper (NMSettings *self,
 		                                 impl_settings_add_connection_add_cb,
 		                                 NULL);
 		g_object_unref (connection);
-	} else {
-		g_assert (error);
-		g_dbus_method_invocation_take_error (context, error);
+		return;
 	}
+
+failure:
+	g_assert (error);
+	g_dbus_method_invocation_take_error (context, error);
 }
 
 static void

commit 608dfacb0bddfa2a50da663db318b0ff0df0f6b1

core: fix leaking connection in impl_settings_add_connection_helper()
Fixes: 0f6baeef35550e7f151a817fb8e2988ae88198b7

src/settings/nm-settings.c
@@ -1337,7 +1337,7 @@ impl_settings_add_connection_helper (NMSettings *self,
                                      GVariant *settings,
                                      gboolean save_to_disk)
 {
-	NMConnection *connection;
+	gs_unref_object NMConnection *connection = NULL;
 	GError *error = NULL;
 
 	connection = _nm_simple_connection_new_from_dbus (settings,
@@ -1345,23 +1345,18 @@ impl_settings_add_connection_helper (NMSettings *self,
 	                                                  | NM_SETTING_PARSE_FLAGS_NORMALIZE,
 	                                                  &error);
 
-	if (connection) {
-		if (!nm_connection_verify_secrets (connection, &error))
-			goto failure;
-
-		nm_settings_add_connection_dbus (self,
-		                                 connection,
-		                                 save_to_disk,
-		                                 context,
-		                                 impl_settings_add_connection_add_cb,
-		                                 NULL);
-		g_object_unref (connection);
+	if (   !connection
+	    || !nm_connection_verify_secrets (connection, &error)) {
+		g_dbus_method_invocation_take_error (context, error);
 		return;
 	}
 
-failure:
-	g_assert (error);
-	g_dbus_method_invocation_take_error (context, error);
+	nm_settings_add_connection_dbus (self,
+	                                 connection,
+	                                 save_to_disk,
+	                                 context,
+	                                 impl_settings_add_connection_add_cb,
+	                                 NULL);
 }
 
 static void