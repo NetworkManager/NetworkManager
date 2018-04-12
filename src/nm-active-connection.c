NetworkManager/NetworkManager

commit c050fb7cd2160f0b74ba8a0760e717e3fe329066

devices, active-connection: port to gdbus

src/nm-active-connection.c
@@ -24,13 +24,13 @@
 #include "nm-default.h"
 #include "nm-active-connection.h"
 #include "nm-dbus-interface.h"
-#include "nm-dbus-glib-types.h"
 #include "nm-device.h"
 #include "nm-settings-connection.h"
 #include "nm-auth-utils.h"
 #include "nm-auth-subject.h"
 #include "NetworkManagerUtils.h"
-#include "nm-active-connection-glue.h"
+
+#include "nmdbus-active-connection.h"
 
 /* Base class for anything implementing the Connection.Active D-Bus interface */
 G_DEFINE_ABSTRACT_TYPE (NMActiveConnection, nm_active_connection, NM_TYPE_EXPORTED_OBJECT)
@@ -586,7 +586,7 @@ nm_active_connection_get_assumed (NMActiveConnection *self)
 static void
 auth_done (NMAuthChain *chain,
            GError *error,
-           DBusGMethodInvocation *unused,
+           GDBusMethodInvocation *unused,
            gpointer user_data)
 {
 	NMActiveConnection *self = NM_ACTIVE_CONNECTION (user_data);
@@ -720,10 +720,10 @@ set_property (GObject *object, guint prop_id,
 		nm_active_connection_set_master (NM_ACTIVE_CONNECTION (object), g_value_get_object (value));
 		break;
 	case PROP_SPECIFIC_OBJECT:
-		tmp = g_value_get_boxed (value);
+		tmp = g_value_get_string (value);
 		/* NM uses "/" to mean NULL */
 		if (g_strcmp0 (tmp, "/") != 0)
-			priv->specific_object = g_value_dup_boxed (value);
+			priv->specific_object = g_strdup (tmp);
 		break;
 	case PROP_DEFAULT:
 		priv->is_default = !!g_value_get_boolean (value);
@@ -752,7 +752,7 @@ get_property (GObject *object, guint prop_id,
 
 	switch (prop_id) {
 	case PROP_CONNECTION:
-		g_value_set_boxed (value, nm_connection_get_path (priv->connection));
+		g_value_set_string (value, nm_connection_get_path (priv->connection));
 		break;
 	case PROP_ID:
 		g_value_set_string (value, nm_connection_get_id (priv->connection));
@@ -764,13 +764,14 @@ get_property (GObject *object, guint prop_id,
 		g_value_set_string (value, nm_connection_get_connection_type (priv->connection));
 		break;
 	case PROP_SPECIFIC_OBJECT:
-		g_value_set_boxed (value, priv->specific_object ? priv->specific_object : "/");
+		g_value_set_string (value, priv->specific_object ? priv->specific_object : "/");
 		break;
 	case PROP_DEVICES:
-		devices = g_ptr_array_sized_new (1);
+		devices = g_ptr_array_sized_new (2);
 		if (priv->device && priv->state < NM_ACTIVE_CONNECTION_STATE_DEACTIVATED)
 			g_ptr_array_add (devices, g_strdup (nm_exported_object_get_path (NM_EXPORTED_OBJECT (priv->device))));
-		g_value_take_boxed (value, devices);
+		g_ptr_array_add (devices, NULL);
+		g_value_take_boxed (value, (char **) g_ptr_array_free (devices, FALSE));
 		break;
 	case PROP_STATE:
 		if (priv->state_set)
@@ -787,19 +788,19 @@ get_property (GObject *object, guint prop_id,
 		break;
 	case PROP_IP4_CONFIG:
 		/* The IP and DHCP config properties may be overridden by a subclass */
-		g_value_set_boxed (value, "/");
+		g_value_set_string (value, "/");
 		break;
 	case PROP_DHCP4_CONFIG:
-		g_value_set_boxed (value, "/");
+		g_value_set_string (value, "/");
 		break;
 	case PROP_DEFAULT6:
 		g_value_set_boolean (value, priv->is_default6);
 		break;
 	case PROP_IP6_CONFIG:
-		g_value_set_boxed (value, "/");
+		g_value_set_string (value, "/");
 		break;
 	case PROP_DHCP6_CONFIG:
-		g_value_set_boxed (value, "/");
+		g_value_set_string (value, "/");
 		break;
 	case PROP_VPN:
 		g_value_set_boolean (value, priv->vpn);
@@ -888,10 +889,10 @@ nm_active_connection_class_init (NMActiveConnectionClass *ac_class)
 	/* D-Bus exported properties */
 	g_object_class_install_property
 		(object_class, PROP_CONNECTION,
-		 g_param_spec_boxed (NM_ACTIVE_CONNECTION_CONNECTION, "", "",
-		                     DBUS_TYPE_G_OBJECT_PATH,
-		                     G_PARAM_READABLE |
-		                     G_PARAM_STATIC_STRINGS));
+		 g_param_spec_string (NM_ACTIVE_CONNECTION_CONNECTION, "", "",
+		                      NULL,
+		                      G_PARAM_READABLE |
+		                      G_PARAM_STATIC_STRINGS));
 
 	g_object_class_install_property
 		(object_class, PROP_ID,
@@ -916,15 +917,15 @@ nm_active_connection_class_init (NMActiveConnectionClass *ac_class)
 
 	g_object_class_install_property
 		(object_class, PROP_SPECIFIC_OBJECT,
-		 g_param_spec_boxed (NM_ACTIVE_CONNECTION_SPECIFIC_OBJECT, "", "",
-		                     DBUS_TYPE_G_OBJECT_PATH,
-		                     G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY |
-		                     G_PARAM_STATIC_STRINGS));
+		 g_param_spec_string (NM_ACTIVE_CONNECTION_SPECIFIC_OBJECT, "", "",
+		                      NULL,
+		                      G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY |
+		                      G_PARAM_STATIC_STRINGS));
 
 	g_object_class_install_property
 		(object_class, PROP_DEVICES,
 		 g_param_spec_boxed (NM_ACTIVE_CONNECTION_DEVICES, "", "",
-		                     DBUS_TYPE_G_ARRAY_OF_OBJECT_PATH,
+		                     G_TYPE_STRV,
 		                     G_PARAM_READABLE |
 		                     G_PARAM_STATIC_STRINGS));
 
@@ -946,17 +947,17 @@ nm_active_connection_class_init (NMActiveConnectionClass *ac_class)
 
 	g_object_class_install_property
 		(object_class, PROP_IP4_CONFIG,
-		 g_param_spec_boxed (NM_ACTIVE_CONNECTION_IP4_CONFIG, "", "",
-		                     DBUS_TYPE_G_OBJECT_PATH,
-		                     G_PARAM_READABLE |
-		                     G_PARAM_STATIC_STRINGS));
+		 g_param_spec_string (NM_ACTIVE_CONNECTION_IP4_CONFIG, "", "",
+		                      NULL,
+		                      G_PARAM_READABLE |
+		                      G_PARAM_STATIC_STRINGS));
 
 	g_object_class_install_property
 		(object_class, PROP_DHCP4_CONFIG,
-		 g_param_spec_boxed (NM_ACTIVE_CONNECTION_DHCP4_CONFIG, "", "",
-		                     DBUS_TYPE_G_OBJECT_PATH,
-		                     G_PARAM_READABLE |
-		                     G_PARAM_STATIC_STRINGS));
+		 g_param_spec_string (NM_ACTIVE_CONNECTION_DHCP4_CONFIG, "", "",
+		                      NULL,
+		                      G_PARAM_READABLE |
+		                      G_PARAM_STATIC_STRINGS));
 
 	g_object_class_install_property
 		(object_class, PROP_DEFAULT6,
@@ -967,17 +968,17 @@ nm_active_connection_class_init (NMActiveConnectionClass *ac_class)
 
 	g_object_class_install_property
 		(object_class, PROP_IP6_CONFIG,
-		 g_param_spec_boxed (NM_ACTIVE_CONNECTION_IP6_CONFIG, "", "",
-		                     DBUS_TYPE_G_OBJECT_PATH,
-		                     G_PARAM_READABLE |
-		                     G_PARAM_STATIC_STRINGS));
+		 g_param_spec_string (NM_ACTIVE_CONNECTION_IP6_CONFIG, "", "",
+		                      NULL,
+		                      G_PARAM_READABLE |
+		                      G_PARAM_STATIC_STRINGS));
 
 	g_object_class_install_property
 		(object_class, PROP_DHCP6_CONFIG,
-		 g_param_spec_boxed (NM_ACTIVE_CONNECTION_DHCP6_CONFIG, "", "",
-		                     DBUS_TYPE_G_OBJECT_PATH,
-		                     G_PARAM_READABLE |
-		                     G_PARAM_STATIC_STRINGS));
+		 g_param_spec_string (NM_ACTIVE_CONNECTION_DHCP6_CONFIG, "", "",
+		                      NULL,
+		                      G_PARAM_READABLE |
+		                      G_PARAM_STATIC_STRINGS));
 
 	g_object_class_install_property
 		(object_class, PROP_VPN,
@@ -988,10 +989,10 @@ nm_active_connection_class_init (NMActiveConnectionClass *ac_class)
 
 	g_object_class_install_property
 		(object_class, PROP_MASTER,
-		 g_param_spec_boxed (NM_ACTIVE_CONNECTION_MASTER, "", "",
-		                     DBUS_TYPE_G_OBJECT_PATH,
-		                     G_PARAM_READABLE |
-		                     G_PARAM_STATIC_STRINGS));
+		 g_param_spec_string (NM_ACTIVE_CONNECTION_MASTER, "", "",
+		                      NULL,
+		                      G_PARAM_READABLE |
+		                      G_PARAM_STATIC_STRINGS));
 
 	/* Internal properties */
 	g_object_class_install_property
@@ -1045,6 +1046,7 @@ nm_active_connection_class_init (NMActiveConnectionClass *ac_class)
 		              G_TYPE_NONE, 1, G_TYPE_UINT);
 
 	nm_exported_object_class_add_interface (NM_EXPORTED_OBJECT_CLASS (ac_class),
-	                                        &dbus_glib_nm_active_connection_object_info);
+	                                        NMDBUS_TYPE_ACTIVE_CONNECTION_SKELETON,
+	                                        NULL);
 }
 

introspection/nm-access-point.xml
@@ -12,6 +12,10 @@
       <tp:docstring>Flags describing the access point's capabilities according to the RSN (Robust Secure Network) protocol.</tp:docstring>
     </property>
     <property name="Ssid" type="ay" access="read">
+      <!-- gdbus-codegen assumes that "ay" means "non-UTF-8 string" and
+           won't deal with '\0' bytes correctly.
+      -->
+      <annotation name="org.gtk.GDBus.C.ForceGVariant" value="1"/>
       <tp:docstring>The Service Set Identifier identifying the access point.</tp:docstring>
     </property>
     <property name="Frequency" type="u" access="read">

introspection/nm-device-wifi.xml
@@ -5,7 +5,6 @@
     <annotation name="org.gtk.GDBus.C.Name" value="DeviceWifi"/>
 
     <method name="GetAccessPoints">
-      <annotation name="org.freedesktop.DBus.GLib.CSymbol" value="impl_device_get_access_points"/>
       <arg name="access_points" type="ao" direction="out">
         <tp:docstring>
           List of access point object paths.
@@ -20,7 +19,6 @@
     </method>
 
     <method name="GetAllAccessPoints">
-      <annotation name="org.freedesktop.DBus.GLib.CSymbol" value="impl_device_get_all_access_points"/>
       <arg name="access_points" type="ao" direction="out">
         <tp:docstring>
           List of access point object paths.
@@ -33,8 +31,6 @@
     </method>
 
     <method name="RequestScan">
-      <annotation name="org.freedesktop.DBus.GLib.CSymbol" value="impl_device_request_scan"/>
-      <annotation name="org.freedesktop.DBus.GLib.Async" value=""/>
       <arg name="options" type="a{sv}" direction="in">
         <tp:docstring>
           Options of scan (currently unused argument).

introspection/nm-device-wimax.xml
@@ -3,7 +3,6 @@
 <node name="/" xmlns:tp="http://telepathy.freedesktop.org/wiki/DbusSpec#extensions-v0">
   <interface name="org.freedesktop.NetworkManager.Device.WiMax">
     <method name="GetNspList">
-      <annotation name="org.freedesktop.DBus.GLib.CSymbol" value="impl_device_get_nsp_list"/>
       <arg name="nsps" type="ao" direction="out">
         <tp:docstring>
           List of NSP object paths

introspection/nm-device.xml
@@ -153,16 +153,12 @@
     </property>
 
     <method name="Disconnect">
-      <annotation name="org.freedesktop.DBus.GLib.CSymbol" value="impl_device_disconnect"/>
-      <annotation name="org.freedesktop.DBus.GLib.Async" value=""/>
       <tp:docstring>
         Disconnects a device and prevents the device from automatically activating further connections without user intervention.
       </tp:docstring>
     </method>
 
     <method name="Delete">
-      <annotation name="org.freedesktop.DBus.GLib.CSymbol" value="impl_device_delete"/>
-      <annotation name="org.freedesktop.DBus.GLib.Async" value=""/>
       <tp:docstring>
         Deletes a software device from NetworkManager and removes the interface from the system.
         The method returns an error when called for a hardware device.

src/devices/adsl/Makefile.am
@@ -7,13 +7,13 @@ AM_CPPFLAGS = \
 	-I${top_builddir}/src \
 	-I${top_srcdir}/src/devices \
 	-I${top_srcdir}/src/platform \
+	-I${top_builddir}/introspection \
 	-I${top_srcdir}/include \
 	-I${top_builddir}/libnm-core \
 	-I${top_srcdir}/libnm-core \
 	-DG_LOG_DOMAIN=\""NetworkManager-adsl"\" \
 	-DNETWORKMANAGER_COMPILATION=NM_NETWORKMANAGER_COMPILATION_INSIDE_DAEMON \
 	-DNM_VERSION_MAX_ALLOWED=NM_VERSION_NEXT_STABLE \
-	$(DBUS_CFLAGS) \
 	$(GUDEV_CFLAGS)
 
 GLIB_GENERATED = nm-adsl-enum-types.h nm-adsl-enum-types.c
@@ -21,10 +21,7 @@ GLIB_MKENUMS_H_FLAGS = --identifier-prefix NM
 GLIB_MKENUMS_C_FLAGS = --identifier-prefix NM
 nm_adsl_enum_types_sources = $(srcdir)/nm-device-adsl.h
 
-nm-device-adsl-glue.h: $(top_srcdir)/introspection/nm-device-adsl.xml
-	dbus-binding-tool --prefix=nm_device_adsl --mode=glib-server --output=$@ $<
-
-BUILT_SOURCES = $(GLIB_GENERATED) nm-device-adsl-glue.h
+BUILT_SOURCES = $(GLIB_GENERATED)
 
 pkglib_LTLIBRARIES = libnm-device-plugin-adsl.la
 
@@ -43,7 +40,7 @@ libnm_device_plugin_adsl_la_LDFLAGS = \
 	-Wl,--version-script=$(SYMBOL_VIS_FILE)
 
 libnm_device_plugin_adsl_la_LIBADD = \
-	$(DBUS_LIBS) \
+	$(top_builddir)/introspection/libnmdbus.la \
 	$(GUDEV_LIBS)
 
 CLEANFILES = $(BUILT_SOURCES)

src/devices/adsl/nm-device-adsl.c
@@ -43,7 +43,7 @@
 #include "nm-setting-adsl.h"
 #include "nm-utils.h"
 
-#include "nm-device-adsl-glue.h"
+#include "nmdbus-device-adsl.h"
 
 #include "nm-device-logging.h"
 _LOG_DECLARE_SELF (NMDeviceAdsl);
@@ -613,5 +613,6 @@ nm_device_adsl_class_init (NMDeviceAdslClass *klass)
 	parent_class->deactivate = deactivate;
 
 	nm_exported_object_class_add_interface (NM_EXPORTED_OBJECT_CLASS (klass),
-	                                        &dbus_glib_nm_device_adsl_object_info);
+	                                        NMDBUS_TYPE_DEVICE_ADSL_SKELETON,
+	                                        NULL);
 }

src/devices/bluetooth/Makefile.am
@@ -9,23 +9,21 @@ AM_CPPFLAGS = \
 	-I${top_srcdir}/src/settings \
 	-I${top_srcdir}/src/platform \
 	-I${top_srcdir}/src/devices/wwan \
+	-I${top_builddir}/introspection \
 	-I${top_srcdir}/include \
 	-I${top_builddir}/libnm-core \
 	-I${top_srcdir}/libnm-core \
 	-DG_LOG_DOMAIN=\""NetworkManager-bluetooth"\" \
 	-DNETWORKMANAGER_COMPILATION=NM_NETWORKMANAGER_COMPILATION_INSIDE_DAEMON \
 	-DNM_VERSION_MAX_ALLOWED=NM_VERSION_NEXT_STABLE \
-	$(DBUS_CFLAGS)
+	$(GLIB_CFLAGS)
 
 GLIB_GENERATED = nm-bt-enum-types.h nm-bt-enum-types.c
 GLIB_MKENUMS_H_FLAGS = --identifier-prefix NM
 GLIB_MKENUMS_C_FLAGS = --identifier-prefix NM
 nm_bt_enum_types_sources = $(srcdir)/nm-bt-error.h
 
-nm-device-bt-glue.h: $(top_srcdir)/introspection/nm-device-bt.xml
-	dbus-binding-tool --prefix=nm_device_bt --mode=glib-server --output=$@ $<
-
-BUILT_SOURCES = $(GLIB_GENERATED) nm-device-bt-glue.h
+BUILT_SOURCES = $(GLIB_GENERATED)
 
 pkglib_LTLIBRARIES = libnm-device-plugin-bluetooth.la
 
@@ -56,8 +54,9 @@ libnm_device_plugin_bluetooth_la_LDFLAGS = \
 	-Wl,--version-script=$(SYMBOL_VIS_FILE)
 
 libnm_device_plugin_bluetooth_la_LIBADD = \
+	$(top_builddir)/introspection/libnmdbus.la \
 	$(top_builddir)/src/devices/wwan/libnm-wwan.la \
-	$(DBUS_LIBS) \
+	$(GLIB_LIBS) \
 	$(GUDEV_LIBS)
 
 if WITH_BLUEZ5_DUN

commit 5f8c77501a284a2a5845fad95c7829c625c50b68

core: simplify NMActiveConnection.get_property to not create temporary GPtrArrray
Fixes: c050fb7cd2160f0b74ba8a0760e717e3fe329066

src/nm-active-connection.c
@@ -1172,7 +1172,7 @@ get_property (GObject *object, guint prop_id,
               GValue *value, GParamSpec *pspec)
 {
 	NMActiveConnectionPrivate *priv = NM_ACTIVE_CONNECTION_GET_PRIVATE ((NMActiveConnection *) object);
-	GPtrArray *devices;
+	char **strv;
 	NMDevice *master_device = NULL;
 
 	switch (prop_id) {
@@ -1202,11 +1202,10 @@ get_property (GObject *object, guint prop_id,
 		g_value_set_string (value, priv->specific_object ? priv->specific_object : "/");
 		break;
 	case PROP_DEVICES:
-		devices = g_ptr_array_sized_new (2);
+		strv = g_new0 (char *, 2);
 		if (priv->device && priv->state < NM_ACTIVE_CONNECTION_STATE_DEACTIVATED)
-			g_ptr_array_add (devices, g_strdup (nm_dbus_object_get_path (NM_DBUS_OBJECT (priv->device))));
-		g_ptr_array_add (devices, NULL);
-		g_value_take_boxed (value, (char **) g_ptr_array_free (devices, FALSE));
+			strv[0] = g_strdup (nm_dbus_object_get_path (NM_DBUS_OBJECT (priv->device)));
+		g_value_take_boxed (value, strv);
 		break;
 	case PROP_STATE:
 		if (priv->state_set)