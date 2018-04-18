NetworkManager/NetworkManager

commit d0563f0733ed293d67e9a0f6503e28c3f1c08f1b

auth-manager: don't process idle calls when the proxy creation finishes
The list of calls contains two kinds of elements: (1) calls that don't
need a D-Bus request and are only waiting for the asynchronous
invocation of the callback in an idle function; (2) calls that need a
D-Bus request and are waiting for the D-Bus proxy.

When the proxy creation finishes, only (2) calls must be canceled (if
the creation failed) or started (if the proxy was created).

Fixes: 798b2a7527bddadcec37b48183da313fbc961e45

https://bugzilla.redhat.com/show_bug.cgi?id=1567807

src/nm-auth-manager.c
@@ -517,8 +517,10 @@ _dbus_new_proxy_cb (GObject *source_object,
 		_LOGE ("could not create polkit proxy: %s", error->message);
 
 		while ((call_id = c_list_first_entry (&priv->calls_lst_head, NMAuthManagerCallId, calls_lst))) {
-			_LOG2T (call_id, "completed: failed due to no D-Bus proxy after startup");
-			_call_id_invoke_callback (call_id, FALSE, FALSE, error);
+			if (call_id->dbus_parameters) {
+				_LOG2T (call_id, "completed: failed due to no D-Bus proxy after startup");
+				_call_id_invoke_callback (call_id, FALSE, FALSE, error);
+			}
 		}
 		return;
 	}
@@ -536,8 +538,10 @@ _dbus_new_proxy_cb (GObject *source_object,
 	_log_name_owner (self, NULL);
 
 	while ((call_id = c_list_first_entry (&priv->calls_lst_head, NMAuthManagerCallId, calls_lst))) {
-		_LOG2T (call_id, "CheckAuthorization invoke now");
-		_call_check_authorize (call_id);
+		if (call_id->dbus_parameters) {
+			_LOG2T (call_id, "CheckAuthorization invoke now");
+			_call_check_authorize (call_id);
+		}
 	}
 
 	_emit_changed_signal (self);

commit a99d51cb501d4a4022273aeb0622e1a8eaac2657

auth-manager: fix potential issue iterating modified CList in _dbus_new_proxy_cb()
In the loop, we invoke callbacks. What the callbacks do, is out of control
of NMAuthManager. For example, they could cancel or schedule new
requests. Especially, cancelling invalidate the stored @safe pointer.

Fix that, by always iterate from the start of the list.

Fixes: d0563f0733ed293d67e9a0f6503e28c3f1c08f1b

src/nm-auth-manager.c
@@ -500,7 +500,7 @@ _dbus_new_proxy_cb (GObject *source_object,
 	NMAuthManagerPrivate *priv;
 	gs_free GError *error = NULL;
 	GDBusProxy *proxy;
-	NMAuthManagerCallId *call_id, *safe;
+	NMAuthManagerCallId *call_id;
 
 	proxy = g_dbus_proxy_new_for_bus_finish  (res, &error);
 
@@ -516,10 +516,12 @@ _dbus_new_proxy_cb (GObject *source_object,
 	if (!priv->proxy) {
 		_LOGE ("could not create polkit proxy: %s", error->message);
 
-		c_list_for_each_entry_safe (call_id, safe, &priv->calls_lst_head, calls_lst) {
+again:
+		c_list_for_each_entry (call_id, &priv->calls_lst_head, calls_lst) {
 			if (call_id->dbus_parameters) {
 				_LOG2T (call_id, "completed: failed due to no D-Bus proxy after startup");
 				_call_id_invoke_callback (call_id, FALSE, FALSE, error);
+				goto again;
 			}
 		}
 		return;