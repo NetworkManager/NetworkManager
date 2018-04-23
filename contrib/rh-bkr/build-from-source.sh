NetworkManager/NetworkManager

commit 2f67ac9eafde3f9979224ce6ce30de3d5f3e1862

build-from-source: also fetch refs for pull requests when building
When somebody creates a pull request against NetworkManager's github
repository, the github repository itself usually doesn't have a branch
that references the pull request.

Hence, the commit will not be fetched by default and checking out
the commit will fail.

Also add and fetch the refs for the pull request.

contrib/rh-bkr/build-from-source.sh
@@ -81,7 +81,14 @@ if ! timeout 10m git clone "$BUILD_REPO"; then
 fi
 
 cd "./NetworkManager/"
-git checkout "$BUILD_ID"
+
+# if we fetch from a github repository, we also care about the refs to the pull-requests
+# fetch them too.
+git config --add remote.origin.fetch '+refs/tags/*:refs/nmbuild-origin/tags/*'
+git config --add remote.origin.fetch '+refs/pull/*:refs/nmbuild-origin/pull/*'
+git fetch origin
+
+git checkout -B nmbuild "$BUILD_ID"
 echo "HEAD is $(git rev-parse HEAD)"
 
 if [[ "$DO_TEST_BUILD" == yes ]]; then

commit d8f99fe901fd528b1853f6ec043d7f5622b3456d

build-from-source: fix checking-out branches by name (2)
git doesn't like to fetch into a local branch which is currently
checked out.

  $ git fetch origin --prune
  fatal: Refusing to fetch into current branch refs/heads/master of non-bare repository

We need to first checkout a plain commit (detached HEAD).

Fixes: 2f67ac9eafde3f9979224ce6ce30de3d5f3e1862

contrib/rh-bkr/build-from-source.sh
@@ -87,9 +87,10 @@ cd "./NetworkManager/"
 git config --add remote.origin.fetch '+refs/heads/*:refs/heads/*'
 git config --add remote.origin.fetch '+refs/tags/*:refs/nmbuild-origin/tags/*'
 git config --add remote.origin.fetch '+refs/pull/*:refs/nmbuild-origin/pull/*'
+git checkout HEAD^{}
 git fetch origin --prune
-
 git checkout -B nmbuild "$BUILD_ID"
+
 echo "HEAD is $(git rev-parse HEAD)"
 
 if [[ "$DO_TEST_BUILD" == yes ]]; then