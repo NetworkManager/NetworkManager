NetworkManager/NetworkManager

commit 297d4985abcc7b571b8c090ee90622357fc60e16

core/dbus: rework D-Bus implementation to use lower layer GDBusConnection API
Previously, we used the generated GDBusInterfaceSkeleton types and glued
them via the NMExportedObject base class to our NM types. We also used
GDBusObjectManagerServer.

Don't do that anymore. The resulting code was more complicated despite (or
because?) using generated classes. It was hard to understand, complex, had
ordering-issues, and had a runtime and memory overhead.

This patch refactors this entirely and uses the lower layer API GDBusConnection
directly. It replaces the generated code, GDBusInterfaceSkeleton, and
GDBusObjectManagerServer. All this is now done by NMDbusObject and NMDBusManager
and static descriptor instances of type GDBusInterfaceInfo.

This adds a net plus of more then 1300 lines of hand written code. I claim
that this implementation is easier to understand. Note that previously we
also required extensive and complex glue code to bind our objects to the
generated skeleton objects. Instead, now glue our objects directly to
GDBusConnection. The result is more immediate and gets rid of layers of
code in between.
Now that the D-Bus glue us more under our control, we can address issus and
bottlenecks better, instead of adding code to bend the generated skeletons
to our needs.

Note that the current implementation now only supports one D-Bus connection.
That was effectively the case already, although there were places (and still are)
where the code pretends it could also support connections from a private socket.
We dropped private socket support mainly because it was unused, untested and
buggy, but also because GDBusObjectManagerServer could not export the same
objects on multiple connections. Now, it would be rather straight forward to
fix that and re-introduce ObjectManager on each private connection. But this
commit doesn't do that yet, and the new code intentionally supports only one
D-Bus connection.

src/main.c
@@ -48,7 +48,7 @@
 #include "settings/nm-settings.h"
 #include "nm-auth-manager.h"
 #include "nm-core-internal.h"
-#include "nm-exported-object.h"
+#include "nm-dbus-object.h"
 #include "nm-connectivity.h"
 #include "dns/nm-dns-manager.h"
 #include "systemd/nm-sd.h"
@@ -225,6 +225,7 @@ int
 main (int argc, char *argv[])
 {
 	gboolean success = FALSE;
+	NMManager *manager;
 	NMConfig *config;
 	GError *error = NULL;
 	gboolean wrote_pidfile = FALSE;
@@ -236,7 +237,7 @@ main (int argc, char *argv[])
 	 * https://bugzilla.gnome.org/show_bug.cgi?id=674885 */
 	g_type_ensure (G_TYPE_SOCKET);
 	g_type_ensure (G_TYPE_DBUS_CONNECTION);
-	g_type_ensure (NM_TYPE_BUS_MANAGER);
+	g_type_ensure (NM_TYPE_DBUS_MANAGER);
 
 	_nm_utils_is_manager_process = TRUE;
 
@@ -394,27 +395,22 @@ main (int argc, char *argv[])
 	                                                         NM_CONFIG_KEYFILE_KEY_MAIN_AUTH_POLKIT,
 	                                                         NM_CONFIG_DEFAULT_MAIN_AUTH_POLKIT_BOOL));
 
-	nm_manager_setup ();
+	manager = nm_manager_setup ();
 
-	if (!nm_bus_manager_get_connection (nm_bus_manager_get ())) {
-		nm_log_warn (LOGD_CORE, "Failed to connect to D-Bus; only private bus is available");
-	} else {
-		/* Start our DBus service */
-		if (!nm_bus_manager_start_service (nm_bus_manager_get ())) {
-			nm_log_err (LOGD_CORE, "failed to start the dbus service.");
-			goto done;
-		}
-	}
+	if (!nm_dbus_manager_start (nm_dbus_manager_get (),
+	                            nm_manager_dbus_set_property_handle,
+	                            manager))
+		goto done;
 
 #if WITH_CONCHECK
-	NM_UTILS_KEEP_ALIVE (nm_manager_get (), nm_connectivity_get (), "NMManager-depends-on-NMConnectivity");
+	NM_UTILS_KEEP_ALIVE (manager, nm_connectivity_get (), "NMManager-depends-on-NMConnectivity");
 #endif
 
 	nm_dispatcher_init ();
 
-	g_signal_connect (nm_manager_get (), NM_MANAGER_CONFIGURE_QUIT, G_CALLBACK (manager_configure_quit), config);
+	g_signal_connect (manager, NM_MANAGER_CONFIGURE_QUIT, G_CALLBACK (manager_configure_quit), config);
 
-	if (!nm_manager_start (nm_manager_get (), &error)) {
+	if (!nm_manager_start (manager, &error)) {
 		nm_log_err (LOGD_CORE, "failed to initialize: %s", error->message);
 		goto done;
 	}
@@ -448,11 +444,14 @@ done:
 	 * state here. We don't bother updating the state as devices
 	 * change during regular operation. If NM is killed with SIGKILL,
 	 * it misses to update the state. */
-	nm_manager_write_device_state (nm_manager_get ());
+	nm_manager_write_device_state (manager);
 
-	nm_exported_object_class_set_quitting ();
+	/* FIXME: we don't properly shut down on exit. That is a bug.
+	 * NMDBusObject have an assertion that they get unexported before disposing.
+	 * We need this workaround and disable the assertion during our leaky shutdown. */
+	nm_dbus_object_set_quitting ();
 
-	nm_manager_stop (nm_manager_get ());
+	nm_manager_stop (manager);
 
 	nm_config_state_set (config, TRUE, TRUE);
 

src/nm-dbus-manager.h
@@ -19,75 +19,89 @@
  * Copyright (C) 2006 - 2008 Novell, Inc.
  */
 
-#ifndef __NM_BUS_MANAGER_H__
-#define __NM_BUS_MANAGER_H__
+#ifndef __NM_DBUS_MANAGER_H__
+#define __NM_DBUS_MANAGER_H__
 
-#define NM_TYPE_BUS_MANAGER (nm_bus_manager_get_type ())
-#define NM_BUS_MANAGER(o) (G_TYPE_CHECK_INSTANCE_CAST ((o), NM_TYPE_BUS_MANAGER, NMBusManager))
-#define NM_BUS_MANAGER_CLASS(k) (G_TYPE_CHECK_CLASS_CAST((k), NM_TYPE_BUS_MANAGER, NMBusManagerClass))
-#define NM_IS_BUS_MANAGER(o) (G_TYPE_CHECK_INSTANCE_TYPE ((o), NM_TYPE_BUS_MANAGER))
-#define NM_IS_BUS_MANAGER_CLASS(k) (G_TYPE_CHECK_CLASS_TYPE ((k), NM_TYPE_BUS_MANAGER))
-#define NM_BUS_MANAGER_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), NM_TYPE_BUS_MANAGER, NMBusManagerClass))
+#include "nm-dbus-utils.h"
 
-#define NM_BUS_MANAGER_DBUS_CONNECTION_CHANGED          "dbus-connection-changed"
-#define NM_BUS_MANAGER_PRIVATE_CONNECTION_NEW           "private-connection-new"
-#define NM_BUS_MANAGER_PRIVATE_CONNECTION_DISCONNECTED  "private-connection-disconnected"
+#define NM_TYPE_DBUS_MANAGER (nm_dbus_manager_get_type ())
+#define NM_DBUS_MANAGER(o) (G_TYPE_CHECK_INSTANCE_CAST ((o), NM_TYPE_DBUS_MANAGER, NMDBusManager))
+#define NM_DBUS_MANAGER_CLASS(k) (G_TYPE_CHECK_CLASS_CAST((k), NM_TYPE_DBUS_MANAGER, NMDBusManagerClass))
+#define NM_IS_DBUS_MANAGER(o) (G_TYPE_CHECK_INSTANCE_TYPE ((o), NM_TYPE_DBUS_MANAGER))
+#define NM_IS_DBUS_MANAGER_CLASS(k) (G_TYPE_CHECK_CLASS_TYPE ((k), NM_TYPE_DBUS_MANAGER))
+#define NM_DBUS_MANAGER_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), NM_TYPE_DBUS_MANAGER, NMDBusManagerClass))
 
-typedef struct _NMBusManagerClass NMBusManagerClass;
+#define NM_DBUS_MANAGER_PRIVATE_CONNECTION_NEW           "private-connection-new"
+#define NM_DBUS_MANAGER_PRIVATE_CONNECTION_DISCONNECTED  "private-connection-disconnected"
 
-GType nm_bus_manager_get_type (void);
+typedef struct _NMDBusManagerClass NMDBusManagerClass;
 
-NMBusManager * nm_bus_manager_get       (void);
-void           nm_bus_manager_setup     (NMBusManager *instance);
+GType nm_dbus_manager_get_type (void);
 
-gboolean nm_bus_manager_start_service    (NMBusManager *self);
+NMDBusManager *nm_dbus_manager_get (void);
 
-GDBusConnection * nm_bus_manager_get_connection (NMBusManager *self);
+typedef void (*NMDBusManagerSetPropertyHandler) (NMDBusObject *obj,
+                                                 const NMDBusInterfaceInfoExtended *interface_info,
+                                                 const NMDBusPropertyInfoExtended *property_info,
+                                                 GDBusConnection *connection,
+                                                 const char *sender,
+                                                 GDBusMethodInvocation *invocation,
+                                                 GVariant *value,
+                                                 gpointer user_data);
 
-gboolean nm_bus_manager_get_caller_info (NMBusManager *self,
-                                         GDBusMethodInvocation *context,
-                                         char **out_sender,
-                                         gulong *out_uid,
-                                         gulong *out_pid);
+gboolean nm_dbus_manager_start (NMDBusManager *self,
+                                NMDBusManagerSetPropertyHandler handler,
+                                gpointer handler_data);
 
-gboolean nm_bus_manager_ensure_uid (NMBusManager          *self,
-                                    GDBusMethodInvocation *context,
-                                    gulong uid,
-                                    GQuark error_domain,
-                                    int error_code);
+GDBusConnection *nm_dbus_manager_get_connection (NMDBusManager *self);
 
-const char *nm_bus_manager_connection_get_private_name (NMBusManager *self,
-                                                        GDBusConnection *connection);
+NMDBusObject *nm_dbus_manager_lookup_object (NMDBusManager *self, const char *path);
 
-gboolean nm_bus_manager_get_unix_user (NMBusManager *self,
-                                       const char *sender,
-                                       gulong *out_uid);
+void _nm_dbus_manager_obj_export (NMDBusObject *obj);
+void _nm_dbus_manager_obj_unexport (NMDBusObject *obj);
+void _nm_dbus_manager_obj_notify (NMDBusObject *obj,
+                                  guint n_pspecs,
+                                  const GParamSpec *const*pspecs);
+void _nm_dbus_manager_obj_emit_signal (NMDBusObject *obj,
+                                       const NMDBusInterfaceInfoExtended *interface_info,
+                                       const GDBusSignalInfo *signal_info,
+                                       GVariant *args);
 
-gboolean nm_bus_manager_get_caller_info_from_message (NMBusManager *self,
-                                                      GDBusConnection *connection,
-                                                      GDBusMessage *message,
-                                                      char **out_sender,
-                                                      gulong *out_uid,
-                                                      gulong *out_pid);
+gboolean nm_dbus_manager_get_caller_info (NMDBusManager *self,
+                                          GDBusMethodInvocation *context,
+                                          char **out_sender,
+                                          gulong *out_uid,
+                                          gulong *out_pid);
 
-void nm_bus_manager_register_object (NMBusManager *self,
-                                     GDBusObjectSkeleton *object);
+gboolean nm_dbus_manager_ensure_uid (NMDBusManager          *self,
+                                     GDBusMethodInvocation *context,
+                                     gulong uid,
+                                     GQuark error_domain,
+                                     int error_code);
 
-void nm_bus_manager_unregister_object (NMBusManager *self,
-                                       GDBusObjectSkeleton *object);
+const char *nm_dbus_manager_connection_get_private_name (NMDBusManager *self,
+                                                         GDBusConnection *connection);
 
-GDBusObjectSkeleton *nm_bus_manager_get_registered_object (NMBusManager *self,
-                                                           const char *path);
+gboolean nm_dbus_manager_get_unix_user (NMDBusManager *self,
+                                        const char *sender,
+                                        gulong *out_uid);
 
-void nm_bus_manager_private_server_register (NMBusManager *self,
-                                             const char *path,
-                                             const char *tag);
+gboolean nm_dbus_manager_get_caller_info_from_message (NMDBusManager *self,
+                                                       GDBusConnection *connection,
+                                                       GDBusMessage *message,
+                                                       char **out_sender,
+                                                       gulong *out_uid,
+                                                       gulong *out_pid);
 
-GDBusProxy *nm_bus_manager_new_proxy (NMBusManager *self,
-                                      GDBusConnection *connection,
-                                      GType proxy_type,
-                                      const char *name,
-                                      const char *path,
-                                      const char *iface);
+void nm_dbus_manager_private_server_register (NMDBusManager *self,
+                                              const char *path,
+                                              const char *tag);
 
-#endif /* __NM_BUS_MANAGER_H__ */
+GDBusProxy *nm_dbus_manager_new_proxy (NMDBusManager *self,
+                                       GDBusConnection *connection,
+                                       GType proxy_type,
+                                       const char *name,
+                                       const char *path,
+                                       const char *iface);
+
+#endif /* __NM_DBUS_MANAGER_H__ */

commit c30d2ab01629f9fa70188d5c35aadd27d7c8b661

core: fix bus initialization order
We currently start the bus manager only after the creation of a
NMManager because the NMManager is needed to handle set-property bus
calls. However, objects created by NMManager
(e.g. NMDnsSystemdResolved) need a bus connection and so their
initialization currently fail.

To fix this, split nm_dbus_manager_start() in two parts: first only
create the connection and acquire the bus.  After this step the
NMManager can be set up. In the second step, set NMManager as the
set-property handler and start exporting objects on the bus.

Fixes: 297d4985abcc7b571b8c090ee90622357fc60e16

src/main.c
@@ -225,7 +225,7 @@ int
 main (int argc, char *argv[])
 {
 	gboolean success = FALSE;
-	NMManager *manager;
+	NMManager *manager = NULL;
 	NMConfig *config;
 	GError *error = NULL;
 	gboolean wrote_pidfile = FALSE;
@@ -395,12 +395,13 @@ main (int argc, char *argv[])
 	                                                         NM_CONFIG_KEYFILE_KEY_MAIN_AUTH_POLKIT,
 	                                                         NM_CONFIG_DEFAULT_MAIN_AUTH_POLKIT_BOOL));
 
-	manager = nm_manager_setup ();
+	if (!nm_dbus_manager_acquire_bus (nm_dbus_manager_get ()))
+		goto done_no_manager;
 
-	if (!nm_dbus_manager_start (nm_dbus_manager_get (),
-	                            nm_manager_dbus_set_property_handle,
-	                            manager))
-		goto done;
+	manager = nm_manager_setup ();
+	nm_dbus_manager_start (nm_dbus_manager_get(),
+	                       nm_manager_dbus_set_property_handle,
+	                       manager);
 
 	nm_dispatcher_init ();
 
@@ -453,6 +454,7 @@ done:
 
 	nm_dns_manager_stop (nm_dns_manager_get ());
 
+done_no_manager:
 	if (global_opt.pidfile && wrote_pidfile)
 		unlink (global_opt.pidfile);
 

src/nm-dbus-manager.c
@@ -83,6 +83,7 @@ typedef struct {
 	GDBusConnection *connection;
 	GDBusProxy *proxy;
 	guint objmgr_registration_id;
+	gboolean started;
 } NMDBusManagerPrivate;
 
 struct _NMDBusManager {
@@ -930,6 +931,7 @@ _obj_register (NMDBusManager *self,
 
 	nm_assert (c_list_is_empty (&obj->internal.registration_lst_head));
 	nm_assert (priv->connection);
+	nm_assert (priv->started);
 
 	n_klasses = 0;
 	gtype = G_OBJECT_TYPE (obj);
@@ -1107,7 +1109,7 @@ _nm_dbus_manager_obj_export (NMDBusObject *obj)
 		nm_assert_not_reached ();
 	c_list_link_tail (&priv->objects_lst_head, &obj->internal.objects_lst);
 
-	if (priv->connection)
+	if (priv->connection && priv->started)
 		_obj_register (self, obj);
 }
 
@@ -1306,7 +1308,7 @@ _nm_dbus_manager_obj_emit_signal (NMDBusObject *obj,
 	self = obj->internal.bus_manager;
 	priv = NM_DBUS_MANAGER_GET_PRIVATE (self);
 
-	if (!priv->connection) {
+	if (!priv->connection || !priv->started) {
 		nm_g_variant_unref_floating (args);
 		return;
 	}
@@ -1453,10 +1455,28 @@ static const GDBusInterfaceInfo interface_info_objmgr = NM_DEFINE_GDBUS_INTERFAC
 
 /*****************************************************************************/
 
-gboolean
+void
 nm_dbus_manager_start (NMDBusManager *self,
                        NMDBusManagerSetPropertyHandler set_property_handler,
                        gpointer set_property_handler_data)
+{
+	NMDBusManagerPrivate *priv;
+	NMDBusObject *obj;
+
+	g_return_if_fail (NM_IS_DBUS_MANAGER (self));
+	priv = NM_DBUS_MANAGER_GET_PRIVATE (self);
+	g_return_if_fail (priv->connection);
+
+	priv->set_property_handler = set_property_handler;
+	priv->set_property_handler_data = set_property_handler_data;
+	priv->started = TRUE;
+
+	c_list_for_each_entry (obj, &priv->objects_lst_head, internal.objects_lst)
+		_obj_register (self, obj);
+}
+
+gboolean
+nm_dbus_manager_acquire_bus (NMDBusManager *self)
 {
 	NMDBusManagerPrivate *priv;
 	gs_free_error GError *error = NULL;
@@ -1465,16 +1485,10 @@ nm_dbus_manager_start (NMDBusManager *self,
 	gs_unref_object GDBusProxy *proxy = NULL;
 	guint32 result;
 	guint registration_id;
-	NMDBusObject *obj;
 
 	g_return_val_if_fail (NM_IS_DBUS_MANAGER (self), FALSE);
 
 	priv = NM_DBUS_MANAGER_GET_PRIVATE (self);
-
-	priv->set_property_handler = set_property_handler;
-	priv->set_property_handler_data = set_property_handler_data;
-
-	g_return_val_if_fail (!priv->connection, FALSE);
 
 	/* we will create the D-Bus connection and registering the name synchronously.
 	 * The reason why that is necessary is because:
@@ -1556,9 +1570,6 @@ nm_dbus_manager_start (NMDBusManager *self,
 	priv->proxy = g_steal_pointer (&proxy);
 
 	_LOGI ("acquired D-Bus service \"%s\"", NM_DBUS_SERVICE);
-
-	c_list_for_each_entry (obj, &priv->objects_lst_head, internal.objects_lst)
-		_obj_register (self, obj);
 
 	return TRUE;
 }

src/nm-dbus-manager.h
@@ -49,9 +49,11 @@ typedef void (*NMDBusManagerSetPropertyHandler) (NMDBusObject *obj,
                                                  GVariant *value,
                                                  gpointer user_data);
 
-gboolean nm_dbus_manager_start (NMDBusManager *self,
-                                NMDBusManagerSetPropertyHandler handler,
-                                gpointer handler_data);
+gboolean nm_dbus_manager_acquire_bus (NMDBusManager *self);
+
+void nm_dbus_manager_start (NMDBusManager *self,
+                            NMDBusManagerSetPropertyHandler set_property_handler,
+                            gpointer set_property_handler_data);
 
 GDBusConnection *nm_dbus_manager_get_connection (NMDBusManager *self);
 