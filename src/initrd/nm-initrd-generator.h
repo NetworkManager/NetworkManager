c24e505b6acc3f837e9ed342b60525ac6c3b6f94
Lubomir Rintel <lkundrak@v3.sk>
2018-05-03 09:13:32 +0200

    initrd: add iBFT reader
    This is loosely based on nms-ibft-reader, but with some significant
    changes. Notably, it parses /sys/firmware/ibft directly instead of
    iscsiadm output.
    
    iscsiadm is not available on early boot (perhaps it's too large) and
    turns out that parsing sysfs directly is easier and more
    straightforwared anyways. A win-win situation.
    
    It is not useful alone, it's in a separate commit just for the sake of
    easier review.
    
    (cherry picked from commit b544f7243da7b3ca862c2e48c6220a0134b4bf15)

---
+++ src/initrd/nm-initrd-generator.h
@@ -0,0 +1,45 @@
+/* NetworkManager initrd configuration generator
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Copyright (C) 2014, 2018 Red Hat, Inc.
+ */
+
+#ifndef __NM_INITRD_GENERATOR_H__
+#define __NM_INITRD_GENERATOR_H__
+
+#include "nm-connection.h"
+#include "nm-utils.h"
+
+static inline gboolean
+guess_ip_address_family (const char *str)
+{
+	if (str == NULL)
+		return AF_UNSPEC;
+	else if (strchr (str, '.'))
+		return AF_INET;
+	else if (strchr (str, ':'))
+		return AF_INET6;
+	else
+		return AF_UNSPEC;
+}
+
+GHashTable *nmi_ibft_read (const char *sysfs_dir);
+
+gboolean nmi_ibft_update_connection_from_nic (NMConnection *connection, GHashTable *nic, GError **error);
+
+GHashTable *nmi_cmdline_reader_parse (const char *sysfs_dir, char **argv);
+
+#endif  /* __NM_INITRD_GENERATOR_H__ */
