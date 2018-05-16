NetworkManager/NetworkManager

commit c86d0b47229c036e1c1c39e81d7d125dcd8e74b9

dispatcher: extra sanitize names of environment variables
The DHCP options should already be sanitized. Still, make sure we don't
create a bogus environment.

dispatcher/nm-dispatcher-utils.c
@@ -33,6 +33,26 @@
 
 #include "nm-dispatcher-utils.h"
 
+static char *
+_validate_var_name (const char *key)
+{
+	char *sanitized = NULL;
+	nm_assert (key);
+
+	if (!key[0])
+		return NULL;
+
+	sanitized = g_ascii_strup (key, -1);
+	if (!NM_STRCHAR_ALL (sanitized, ch,    (ch >= 'A' && ch <= 'Z')
+	                                    || (ch >= '0' && ch <= '9')
+	                                    || NM_IN_SET (ch, '_'))) {
+		g_free (sanitized);
+		return NULL;
+	}
+
+	return sanitized;
+}
+
 static GSList *
 construct_basic_items (GSList *list,
                        const char *uuid,
@@ -244,7 +264,9 @@ construct_device_dhcp4_items (GSList *items, GVariant *dhcp4_config)
 
 	g_variant_iter_init (&iter, dhcp4_config);
 	while (g_variant_iter_next (&iter, "{&sv}", &key, &val)) {
-		ucased = g_ascii_strup (key, -1);
+		ucased = _validate_var_name (key);
+		if (!ucased)
+			continue;
 		tmp = g_variant_get_string (val, NULL);
 		items = g_slist_prepend (items, g_strdup_printf ("DHCP4_%s=%s", ucased, tmp));
 		g_free (ucased);
@@ -349,7 +371,9 @@ construct_device_dhcp6_items (GSList *items, GVariant *dhcp6_config)
 
 	g_variant_iter_init (&iter, dhcp6_config);
 	while (g_variant_iter_next (&iter, "{&sv}", &key, &val)) {
-		ucased = g_ascii_strup (key, -1);
+		ucased = _validate_var_name (key);
+		if (!ucased)
+			continue;
 		tmp = g_variant_get_string (val, NULL);
 		items = g_slist_prepend (items, g_strdup_printf ("DHCP6_%s=%s", ucased, tmp));
 		g_free (ucased);

commit 641c111277802da604de419a000ed212085c7282

dispatcher: fix leak in construct_device_dhcp4_items()
Fixes: c86d0b47229c036e1c1c39e81d7d125dcd8e74b9

dispatcher/nm-dispatcher-utils.c
@@ -265,11 +265,11 @@ construct_device_dhcp4_items (GSList *items, GVariant *dhcp4_config)
 	g_variant_iter_init (&iter, dhcp4_config);
 	while (g_variant_iter_next (&iter, "{&sv}", &key, &val)) {
 		ucased = _validate_var_name (key);
-		if (!ucased)
-			continue;
-		tmp = g_variant_get_string (val, NULL);
-		items = g_slist_prepend (items, g_strdup_printf ("DHCP4_%s=%s", ucased, tmp));
-		g_free (ucased);
+		if (ucased) {
+			tmp = g_variant_get_string (val, NULL);
+			items = g_slist_prepend (items, g_strdup_printf ("DHCP4_%s=%s", ucased, tmp));
+			g_free (ucased);
+		}
 		g_variant_unref (val);
 	}
 	return items;
@@ -372,11 +372,11 @@ construct_device_dhcp6_items (GSList *items, GVariant *dhcp6_config)
 	g_variant_iter_init (&iter, dhcp6_config);
 	while (g_variant_iter_next (&iter, "{&sv}", &key, &val)) {
 		ucased = _validate_var_name (key);
-		if (!ucased)
-			continue;
-		tmp = g_variant_get_string (val, NULL);
-		items = g_slist_prepend (items, g_strdup_printf ("DHCP6_%s=%s", ucased, tmp));
-		g_free (ucased);
+		if (ucased) {
+			tmp = g_variant_get_string (val, NULL);
+			items = g_slist_prepend (items, g_strdup_printf ("DHCP6_%s=%s", ucased, tmp));
+			g_free (ucased);
+		}
 		g_variant_unref (val);
 	}
 	return items;