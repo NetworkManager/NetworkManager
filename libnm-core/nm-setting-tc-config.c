d3db846d95b11d75eb8bba45f503a888ac26233f
Beniamino Galvani <bgalvani@redhat.com>
2018-05-28 18:13:42 +0200

    ifcfg-rh: add missing NM-only annotations
    Add the (+) annotation to NM-only variables.

--- libnm-core/nm-setting-tc-config.c
+++ libnm-core/nm-setting-tc-config.c
@@ -1610,7 +1610,7 @@ nm_setting_tc_config_class_init (NMSettingTCConfigClass *setting_class)
 	 **/
 	/* ---ifcfg-rh---
 	 * property: qdiscs
-	 * variable: QDISC1, QDISC2, ...
+	 * variable: QDISC1(+), QDISC2(+), ...
 	 * description: Queueing disciplines
 	 * example: QDISC1=ingress, QDISC2="root handle 1234: fq_codel"
 	 * ---end---
@@ -1637,7 +1637,7 @@ nm_setting_tc_config_class_init (NMSettingTCConfigClass *setting_class)
 	 **/
 	/* ---ifcfg-rh---
 	 * property: qdiscs
-	 * variable: FILTER1, FILTER2, ...
+	 * variable: FILTER1(+), FILTER2(+), ...
 	 * description: Traffic filters
 	 * example: FILTER1="parent ffff: matchall action simple sdata Input", ...
 	 * ---end---
