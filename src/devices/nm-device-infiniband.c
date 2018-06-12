ef0b76a0ec3bc4b6499712c41527cef5817a5297
Beniamino Galvani <bgalvani@redhat.com>
2018-06-12 14:51:28 +0200

    device: introduce nm_device_get_configured_mtu_from_connection()
    Deduplicate similar code from devices.
    
    (cherry picked from commit d9df1f1d05f7c4fe7c6a4ff357fb8b7f2dbdcdce)

--- src/devices/nm-device-infiniband.c
+++ src/devices/nm-device-infiniband.c
@@ -117,27 +117,9 @@ act_stage1_prepare (NMDevice *device, NMDeviceStateReason *out_failure_reason)
 static guint32
 get_configured_mtu (NMDevice *device, gboolean *out_is_user_config)
 {
-	NMSettingInfiniband *setting;
-	gint64 mtu_default;
-	guint32 mtu;
-
-	nm_assert (NM_IS_DEVICE (device));
-	nm_assert (out_is_user_config);
-
-	setting = NM_SETTING_INFINIBAND (nm_device_get_applied_setting (device, NM_TYPE_SETTING_INFINIBAND));
-	if (!setting)
-		g_return_val_if_reached (0);
-
-	mtu = nm_setting_infiniband_get_mtu (setting);
-	if (mtu == 0) {
-		mtu_default = nm_device_get_configured_mtu_from_connection_default (device, "infiniband.mtu");
-		if (mtu_default >= 0) {
-			*out_is_user_config = TRUE;
-			return (guint32) mtu_default;
-		}
-	}
-	*out_is_user_config = (mtu != 0);
-	return mtu;
+	return nm_device_get_configured_mtu_from_connection (device,
+	                                                     NM_TYPE_SETTING_INFINIBAND,
+	                                                     out_is_user_config);
 }
 
 static gboolean
