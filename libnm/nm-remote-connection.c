9bc6ca96f62d9658fa2ac3e87f003de521e28b7a
Thomas Haller <thaller@redhat.com>
2018-06-21 16:06:01 +0200

    libnm: fix leaking filename in NMRemoteConnection
    Fixes: bd6fe1781565eb8d7f2fd87f5256ec15dd51a6df

--- libnm/nm-remote-connection.c
+++ libnm/nm-remote-connection.c
@@ -62,7 +62,7 @@ typedef struct {
 
 	gboolean unsaved;
 	guint32 flags;
-	const char *filename;
+	char *filename;
 
 	gboolean visible;
 } NMRemoteConnectionPrivate;
@@ -939,6 +939,7 @@ dispose (GObject *object)
 	NMRemoteConnectionPrivate *priv = NM_REMOTE_CONNECTION_GET_PRIVATE (object);
 
 	g_clear_object (&priv->proxy);
+	nm_clear_g_free (&priv->filename);
 
 	G_OBJECT_CLASS (nm_remote_connection_parent_class)->dispose (object);
 }
