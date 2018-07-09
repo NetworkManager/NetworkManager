NetworkManager/NetworkManager

commit 1cdb36b8de5ad942fed979c8838e5df63a1edcb0

ppp-manager: use configured pppd path
Path to pppd can be set via configure flag but the source code ignores it.

Let's use PPPD_PATH like other calls of nm_utils_find_helper do.

https://bugzilla.gnome.org/show_bug.cgi?id=796752

src/ppp/nm-ppp-manager.c
@@ -809,7 +809,7 @@ create_pppd_cmd_line (NMPPPManager *self,
 
 	g_return_val_if_fail (setting != NULL, NULL);
 
-	pppd_binary = nm_utils_find_helper ("pppd", NULL, err);
+	pppd_binary = nm_utils_find_helper ("pppd", PPPD_PATH, err);
 	if (!pppd_binary)
 		return NULL;
 

commit 530b82a372181b00a202b102fe28f34071803491

build/meson: fix meson build without pppd
Fixes: 1cdb36b8de5ad942fed979c8838e5df63a1edcb0

src/ppp/nm-ppp-manager.c
@@ -809,6 +809,10 @@ create_pppd_cmd_line (NMPPPManager *self,
 
 	g_return_val_if_fail (setting != NULL, NULL);
 
+#ifndef PPPD_PATH
+#define PPPD_PATH NULL
+#endif
+
 	pppd_binary = nm_utils_find_helper ("pppd", PPPD_PATH, err);
 	if (!pppd_binary)
 		return NULL;