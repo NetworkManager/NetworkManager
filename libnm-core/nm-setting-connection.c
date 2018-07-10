NetworkManager/NetworkManager

commit 4199c976dac21c22773e0c133c42c3b738ca76d2

libnm: fix normalizing and verifying OVS connections
Normalizing can be complicated, as settings depend on each other and possibly
conflict.

That is, because verify() must exactly anticipate whether normalization will
succeed and how the result will look like. That is because we only want to
modify the connection, if we are sure that the result will verify.

Hence, verify() and normalize() are strongly related. The implementation
should not be spread out between NMSettingOvsInterface:verify(),
NMSettingOvsPatch:verify() and _normalize_ovs_interface_type().

Also, add some unit-tests.

libnm-core/nm-setting-connection.c
@@ -859,6 +859,8 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 	gboolean is_slave;
 	const char *slave_setting_type;
 	NMSetting *normerr_base_type = NULL;
+	const char *type;
+	const char *slave_type;
 	const char *normerr_slave_setting_type = NULL;
 	const char *normerr_missing_slave_type = NULL;
 	const char *normerr_missing_slave_type_port = NULL;
@@ -904,8 +906,10 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 		}
 	}
 
-	if (!priv->type) {
-		if (!connection || !(normerr_base_type = _nm_connection_find_base_type_setting (connection))) {
+	type = priv->type;
+	if (!type) {
+		if (   !connection
+		    || !(normerr_base_type = _nm_connection_find_base_type_setting (connection))) {
 			g_set_error_literal (error,
 			                     NM_CONNECTION_ERROR,
 			                     NM_CONNECTION_ERROR_MISSING_PROPERTY,
@@ -913,10 +917,11 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 			g_prefix_error (error, "%s.%s: ", NM_SETTING_CONNECTION_SETTING_NAME, NM_SETTING_CONNECTION_TYPE);
 			return FALSE;
 		}
+		type = nm_setting_get_name (normerr_base_type);
 	} else {
 		GType base_type;
 
-		if (!priv->type[0]) {
+		if (!type[0]) {
 			g_set_error_literal (error,
 			                     NM_CONNECTION_ERROR,
 			                     NM_CONNECTION_ERROR_INVALID_PROPERTY,
@@ -925,21 +930,21 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 			return FALSE;
 		}
 
-		base_type = nm_setting_lookup_type (priv->type);
+		base_type = nm_setting_lookup_type (type);
 		if (   base_type == G_TYPE_INVALID
 		    || _nm_setting_type_get_base_type_priority (base_type) == NM_SETTING_PRIORITY_INVALID) {
 			g_set_error (error,
 			             NM_CONNECTION_ERROR,
 			             NM_CONNECTION_ERROR_INVALID_PROPERTY,
 			             _("connection type '%s' is not valid"),
-			             priv->type);
+			             type);
 			g_prefix_error (error, "%s.%s: ", NM_SETTING_CONNECTION_SETTING_NAME, NM_SETTING_CONNECTION_TYPE);
 			return FALSE;
 		}
 
 		/* Make sure the corresponding 'type' item is present */
 		if (   connection
-		    && !nm_connection_get_setting_by_name (connection, priv->type)) {
+		    && !nm_connection_get_setting_by_name (connection, type)) {
 			NMSetting *s_base;
 			NMConnection *connection2;
 
@@ -952,7 +957,7 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 			g_object_unref (connection2);
 
 			if (!normerr_base_setting) {
-				_set_error_missing_base_setting (error, priv->type);
+				_set_error_missing_base_setting (error, type);
 				return FALSE;
 			}
 		}
@@ -960,13 +965,14 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 
 	is_slave = FALSE;
 	slave_setting_type = NULL;
-	if (priv->slave_type) {
-		is_slave = _nm_setting_slave_type_is_valid (priv->slave_type, &slave_setting_type);
+	slave_type = priv->slave_type;
+	if (slave_type) {
+		is_slave = _nm_setting_slave_type_is_valid (slave_type, &slave_setting_type);
 		if (!is_slave) {
 			g_set_error (error,
 			             NM_CONNECTION_ERROR,
 			             NM_CONNECTION_ERROR_INVALID_PROPERTY,
-			             _("Unknown slave type '%s'"), priv->slave_type);
+			             _("Unknown slave type '%s'"), slave_type);
 			g_prefix_error (error, "%s.%s: ", NM_SETTING_CONNECTION_SETTING_NAME, NM_SETTING_CONNECTION_SLAVE_TYPE);
 			return FALSE;
 		}
@@ -986,8 +992,8 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 		    && !nm_connection_get_setting_by_name (connection, slave_setting_type))
 			normerr_slave_setting_type = slave_setting_type;
 	} else {
+		nm_assert (!slave_type);
 		if (priv->master) {
-			const char *slave_type;
 			NMSetting *s_port;
 
 			if (   connection
@@ -1006,8 +1012,8 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 		}
 	}
 
-	if (   g_strcmp0 (priv->type, NM_SETTING_OVS_PORT_SETTING_NAME) != 0
-	    && g_strcmp0 (priv->slave_type, NM_SETTING_OVS_BRIDGE_SETTING_NAME) == 0) {
+	if (   nm_streq0 (type, NM_SETTING_OVS_PORT_SETTING_NAME)
+	    && !nm_streq0 (slave_type, NM_SETTING_OVS_BRIDGE_SETTING_NAME)) {
 		g_set_error (error,
 		             NM_CONNECTION_ERROR,
 		             NM_CONNECTION_ERROR_MISSING_PROPERTY,

libnm-core/nm-connection-private.h
@@ -34,4 +34,10 @@ const char *_nm_connection_detect_bluetooth_type (NMConnection *self);
 gboolean    _nm_connection_verify_required_interface_name (NMConnection *connection,
                                                            GError **error);
 
+int _nm_setting_ovs_interface_verify_interface_type (NMSettingOvsInterface *self,
+                                                     NMConnection *connection,
+                                                     gboolean normalize,
+                                                     gboolean *out_modified,
+                                                     GError **error);
+
 #endif  /* __NM_CONNECTION_PRIVATE_H__ */

libnm-core/nm-connection.c
@@ -1114,33 +1114,21 @@ static gboolean
 _normalize_ovs_interface_type (NMConnection *self, GHashTable *parameters)
 {
 	NMSettingOvsInterface *s_ovs_interface = nm_connection_get_setting_ovs_interface (self);
-	NMSettingConnection *s_con = nm_connection_get_setting_connection (self);
-	const char *interface_type;
+	gboolean modified;
+	int v;
 
-	if (strcmp (nm_connection_get_connection_type (self), NM_SETTING_OVS_INTERFACE_SETTING_NAME) == 0) {
-		/* OpenVSwitch managed interface. */
-		if (nm_connection_get_setting_ovs_patch (self))
-			interface_type = "patch";
-		else
-			interface_type = "internal";
-	} else if (g_strcmp0 (nm_setting_connection_get_slave_type (s_con), NM_SETTING_OVS_PORT_SETTING_NAME) == 0) {
-		/* A regular device enslaved to a port. */
-		interface_type = "";
-	} else {
-		/* Something else. */
+	if (!s_ovs_interface)
 		return FALSE;
-	}
 
-	if (!s_ovs_interface) {
-		s_ovs_interface = NM_SETTING_OVS_INTERFACE (nm_setting_ovs_interface_new ());
-		nm_connection_add_setting (self, NM_SETTING (s_ovs_interface));
-	}
+	v = _nm_setting_ovs_interface_verify_interface_type (s_ovs_interface,
+	                                                     self,
+	                                                     TRUE,
+	                                                     &modified,
+	                                                     NULL);
+	if (v != TRUE)
+		g_return_val_if_reached (modified);
 
-	g_object_set (s_ovs_interface,
-	              NM_SETTING_OVS_INTERFACE_TYPE, interface_type,
-	              NULL);
-
-	return TRUE;
+	return modified;
 }
 
 static gboolean
@@ -1465,6 +1453,7 @@ nm_connection_normalize (NMConnection *connection,
 			                     NM_CONNECTION_ERROR_FAILED,
 			                     _("Unexpected failure to normalize the connection"));
 		}
+		g_warning ("connection did not verify after normalization: %s", error ? (*error)->message : "??");
 		g_return_val_if_reached (FALSE);
 	}
 

libnm-core/nm-setting-ovs-interface.c
@@ -78,6 +78,173 @@ nm_setting_ovs_interface_get_interface_type (NMSettingOvsInterface *self)
 
 /*****************************************************************************/
 
+int
+_nm_setting_ovs_interface_verify_interface_type (NMSettingOvsInterface *self,
+                                                 NMConnection *connection,
+                                                 gboolean normalize,
+                                                 gboolean *out_modified,
+                                                 GError **error)
+{
+	gboolean has_patch;
+	const char *type;
+	const char *connection_type;
+	gboolean is_ovs_connection_type;
+	gboolean missing_patch_setting = FALSE;
+
+	g_return_val_if_fail (NM_IS_SETTING_OVS_INTERFACE (self), FALSE);
+	if (normalize) {
+		g_return_val_if_fail (NM_IS_CONNECTION (connection), FALSE);
+		nm_assert (self == nm_connection_get_setting_ovs_interface (connection));
+	} else
+		g_return_val_if_fail (!connection || NM_IS_CONNECTION (connection), FALSE);
+
+	NM_SET_OUT (out_modified, FALSE);
+
+	type = self ? self->type : NULL;
+
+	if (   type
+	    && !NM_IN_STRSET (type,
+	                      "internal",
+	                      "system",
+	                      "patch")) {
+		g_set_error (error,
+		             NM_CONNECTION_ERROR,
+		             NM_CONNECTION_ERROR_INVALID_PROPERTY,
+		             _("'%s' is not a valid interface type"),
+		             type);
+		g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
+		return FALSE;
+	}
+
+	if (!connection)
+		return TRUE;
+
+	connection_type = nm_connection_get_connection_type (connection);
+	if (!connection_type) {
+		/* if we have an ovs-interface, then the connection type must be either
+		 * "ovs-interface" (for non "system" type) or anything else (for "system" type).
+		 *
+		 * The connection type usually can be normalized based on the presence of a
+		 * base setting. However, in this case, if the connection type is missing,
+		 * that is too complicate to guess what the user wanted.
+		 *
+		 * Require the use to be explicit and fail. */
+		g_set_error (error,
+		             NM_CONNECTION_ERROR,
+		             NM_CONNECTION_ERROR_INVALID_PROPERTY,
+		             _("A connection with a '%s' setting needs connection.type explicitly set"),
+		             NM_SETTING_OVS_INTERFACE_SETTING_NAME);
+		g_prefix_error (error, "%s.%s: ", NM_SETTING_CONNECTION_SETTING_NAME, NM_SETTING_CONNECTION_TYPE);
+		return FALSE;
+	}
+
+	if (nm_streq (connection_type, NM_SETTING_OVS_INTERFACE_SETTING_NAME)) {
+		if (   type
+		    && nm_streq (type, "system")) {
+			g_set_error (error,
+			             NM_CONNECTION_ERROR,
+			             NM_CONNECTION_ERROR_INVALID_PROPERTY,
+			             _("A connection of type '%s' cannot have ovs-interface.type \"system\""),
+			             NM_SETTING_OVS_INTERFACE_SETTING_NAME);
+			g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
+			return FALSE;
+		}
+		is_ovs_connection_type = TRUE;
+	} else {
+		if (   type
+		    && !nm_streq (type, "system")) {
+			g_set_error (error,
+			             NM_CONNECTION_ERROR,
+			             NM_CONNECTION_ERROR_INVALID_PROPERTY,
+			             _("A connection of type '%s' cannot have an ovs-interface.type \"%s\""),
+			             connection_type,
+			             type);
+			g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
+			return FALSE;
+		}
+		is_ovs_connection_type = FALSE;
+	}
+
+	has_patch = !!nm_connection_get_setting_by_name (connection, NM_SETTING_OVS_PATCH_SETTING_NAME);
+
+	if (has_patch) {
+		if (!is_ovs_connection_type) {
+			g_set_error (error,
+			             NM_CONNECTION_ERROR,
+			             NM_CONNECTION_ERROR_INVALID_PROPERTY,
+			             _("A connection with '%s' setting must be of connection.type \"ovs-interface\" but is \"%s\""),
+			             NM_SETTING_OVS_PATCH_SETTING_NAME,
+			             connection_type);
+			g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
+			return FALSE;
+		}
+		if (type) {
+			if (!nm_streq (type, "patch")) {
+				g_set_error (error,
+				             NM_CONNECTION_ERROR,
+				             NM_CONNECTION_ERROR_INVALID_PROPERTY,
+				             _("A connection with '%s' setting needs to be of 'patch' interface type, not '%s'"),
+				             NM_SETTING_OVS_PATCH_SETTING_NAME,
+				             type);
+				g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
+				return FALSE;
+			}
+			return TRUE;
+		}
+		type = "patch";
+		goto normalize;
+	} else {
+		if (nm_streq0 (type, "patch")) {
+			g_set_error (error,
+			             NM_CONNECTION_ERROR,
+			             NM_CONNECTION_ERROR_MISSING_SETTING,
+			             _("A connection with ovs-interface.type '%s' setting a 'ovs-patch' setting"),
+			             type);
+			g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
+			return FALSE;
+		}
+	}
+
+	if (type)
+		return TRUE;
+
+	if (is_ovs_connection_type)
+		type = "internal";
+	else
+		type = "system";
+normalize:
+	if (!normalize) {
+		if (!self) {
+			g_set_error (error,
+			             NM_CONNECTION_ERROR,
+			             NM_CONNECTION_ERROR_MISSING_SETTING,
+			             _("Missing ovs interface setting"));
+			g_prefix_error (error, "%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME);
+		} else {
+			g_set_error (error,
+			             NM_CONNECTION_ERROR,
+			             NM_CONNECTION_ERROR_MISSING_PROPERTY,
+			             _("Missing ovs interface type"));
+			g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
+		}
+		if (missing_patch_setting) {
+		}
+		return NM_SETTING_VERIFY_NORMALIZABLE_ERROR;
+	}
+
+	if (!self) {
+		self = NM_SETTING_OVS_INTERFACE (nm_setting_ovs_interface_new ());
+		nm_connection_add_setting (connection, NM_SETTING (self));
+	}
+	g_object_set (self,
+	              NM_SETTING_OVS_INTERFACE_TYPE, type,
+	              NULL);
+	NM_SET_OUT (out_modified, TRUE);
+
+	return TRUE;
+}
+
+
 static int
 verify (NMSetting *setting, NMConnection *connection, GError **error)
 {
@@ -109,7 +276,7 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 
 		slave_type = nm_setting_connection_get_slave_type (s_con);
 		if (   slave_type
-		    && strcmp (slave_type, NM_SETTING_OVS_PORT_SETTING_NAME)) {
+		    && !nm_streq (slave_type, NM_SETTING_OVS_PORT_SETTING_NAME)) {
 			g_set_error (error,
 			             NM_CONNECTION_ERROR,
 			             NM_CONNECTION_ERROR_INVALID_PROPERTY,
@@ -122,30 +289,11 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 		}
 	}
 
-	if (!NM_IN_STRSET (self->type, "internal", "system", "patch", "", NULL)) {
-		g_set_error (error,
-		             NM_CONNECTION_ERROR,
-		             NM_CONNECTION_ERROR_INVALID_PROPERTY,
-		             _("'%s' is not a valid interface type"),
-		             self->type);
-		g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
-		return FALSE;
-	}
-
-	if (connection) {
-		if (   g_strcmp0 (self->type, "patch") == 0
-		    && !nm_connection_get_setting_ovs_patch (connection)) {
-			g_set_error (error,
-			             NM_CONNECTION_ERROR,
-			             NM_CONNECTION_ERROR_INVALID_PROPERTY,
-			             _("A connection of 'patch' interface type needs a '%s' setting."),
-			             NM_SETTING_OVS_PATCH_SETTING_NAME);
-			g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
-			return FALSE;
-		}
-	}
-
-	return TRUE;
+	return _nm_setting_ovs_interface_verify_interface_type (self,
+	                                                        connection,
+	                                                        FALSE,
+	                                                        NULL,
+	                                                        error);
 }
 
 /*****************************************************************************/

libnm-core/nm-setting-ovs-patch.c
@@ -115,37 +115,6 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 		return FALSE;
 	}
 
-
-	if (connection) {
-		NMSettingOvsInterface *s_ovs_interface;
-		const char *interface_type = NULL;
-
-		s_ovs_interface = nm_connection_get_setting_ovs_interface (connection);
-		if (s_ovs_interface)
-			interface_type = nm_setting_ovs_interface_get_interface_type (s_ovs_interface);
-
-		if (interface_type && strcmp (interface_type, "patch") != 0) {
-			g_set_error (error,
-			             NM_CONNECTION_ERROR,
-			             NM_CONNECTION_ERROR_INVALID_PROPERTY,
-			             _("A connection with '%s' setting needs to be of 'patch' interface type, not '%s'"),
-			            NM_SETTING_OVS_PATCH_SETTING_NAME,
-			            interface_type);
-			g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
-			return FALSE;
-		}
-
-		if (!interface_type) {
-			g_set_error (error,
-			             NM_CONNECTION_ERROR,
-			             NM_CONNECTION_ERROR_INVALID_PROPERTY,
-			             _("A connection with '%s' setting needs to be of 'patch' interface type"),
-			            NM_SETTING_OVS_PATCH_SETTING_NAME);
-			g_prefix_error (error, "%s.%s: ", NM_SETTING_OVS_INTERFACE_SETTING_NAME, NM_SETTING_OVS_INTERFACE_TYPE);
-			return NM_SETTING_VERIFY_NORMALIZABLE_ERROR;
-		}
-	}
-
 	return TRUE;
 }
 

libnm-core/nm-setting.c
@@ -219,7 +219,7 @@ _nm_setting_get_base_type_priority (NMSetting *setting)
 GType
 nm_setting_lookup_type (const char *name)
 {
-	SettingInfo *info;
+	const SettingInfo *info;
 
 	g_return_val_if_fail (name, G_TYPE_INVALID);
 

commit 354140e8d3a9f0b72ba6da3011cc6c506c607f4f

setting-connection: fix ovs-port parent setting verification
  $ nmcli c add type ovs-port ifname ovsport0
  Error: Failed to add 'ovs-port-ovsport0' connection: connection.type:
      Only 'ovs-port' connections can be enslaved to 'ovs-bridge'

nm_streq0() is not good here. It fails (with a wrong error message) even
when the slave_type is not set, which it shouldn't since slave_type can
be normalized. The real problem is the lack of the master property.

This fixes the condition:

  $ nmcli c add type ovs-port ifname ovsport0
  Error: Failed to add 'ovs-port-ovsport0' connection: connection.master:
    A connection with a 'ovs-port' setting must have a master.

Corrects the error message:

  $ nmcli c add con-name br0 type bridge
  $ nmcli c add type ovs-port ifname ovsport0 parent br0
  Error: Failed to add 'bridge-slave-ovsport0' connection: connection.slave-type:
    'ovs-port' connections must be enslaved to 'ovs-bridge', not 'bridge'

And gets rid of a confusing nm_streq0 use when comparing the type, since
at that point type must not be NULL anymore.

Fixes: 4199c976dac21c22773e0c133c42c3b738ca76d2

libnm-core/nm-setting-connection.c
@@ -1050,15 +1050,17 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 		}
 	}
 
-	if (   nm_streq0 (type, NM_SETTING_OVS_PORT_SETTING_NAME)
-	    && !nm_streq0 (slave_type, NM_SETTING_OVS_BRIDGE_SETTING_NAME)) {
+	if (   strcmp (type, NM_SETTING_OVS_PORT_SETTING_NAME) == 0
+	    && slave_type
+	    && strcmp (slave_type, NM_SETTING_OVS_BRIDGE_SETTING_NAME) != 0) {
 		g_set_error (error,
 		             NM_CONNECTION_ERROR,
 		             NM_CONNECTION_ERROR_MISSING_PROPERTY,
-		             _("Only '%s' connections can be enslaved to '%s'"),
+		             _("'%s' connections must be enslaved to '%s', not '%s'"),
 		             NM_SETTING_OVS_PORT_SETTING_NAME,
-		             NM_SETTING_OVS_BRIDGE_SETTING_NAME);
-		g_prefix_error (error, "%s.%s: ", NM_SETTING_CONNECTION_SETTING_NAME, NM_SETTING_CONNECTION_TYPE);
+		             NM_SETTING_OVS_BRIDGE_SETTING_NAME,
+		             slave_type);
+		g_prefix_error (error, "%s.%s: ", NM_SETTING_CONNECTION_SETTING_NAME, NM_SETTING_CONNECTION_SLAVE_TYPE);
 		return FALSE;
 	}
 