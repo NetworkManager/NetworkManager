NetworkManager/NetworkManager

commit 8d09660481e1ff8b513aa60b2c40a4982ccc57cd

libnm: add accessors for ovs port/bridge slaves
(cherry picked from commit 5f30a2b525dd42e300e7ab69497f28348090bee8)

libnm-core/nm-version.h
@@ -160,4 +160,10 @@
 # define NM_AVAILABLE_IN_1_12
 #endif
 
+#if NM_VERSION_MAX_ALLOWED < NM_VERSION_1_12_2
+# define NM_AVAILABLE_IN_1_12_2          G_UNAVAILABLE(1,12,2)
+#else
+# define NM_AVAILABLE_IN_1_12_2
+#endif
+
 #endif  /* NM_VERSION_H */

libnm/libnm.ver
@@ -1376,4 +1376,6 @@ libnm_1_12_2 {
 	nm_connection_get_setting_ovs_patch;
 	nm_connection_get_setting_ovs_port;
 	nm_connection_get_setting_tun;
+	nm_device_ovs_bridge_get_slaves;
+	nm_device_ovs_port_get_slaves;
 } libnm_1_12_0;

libnm/nm-device-ovs-bridge.c
@@ -14,7 +14,7 @@
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301 USA.
  *
- * Copyright 2017 Red Hat, Inc.
+ * Copyright 2017,2018 Red Hat, Inc.
  */
 
 #include "nm-default.h"
@@ -26,12 +26,21 @@
 #include "nm-setting-ovs-bridge.h"
 #include "nm-setting-ovs-port.h"
 #include "nm-setting-connection.h"
+#include "nm-core-internal.h"
+
+enum {
+	PROP_0,
+	PROP_SLAVES,
+
+	LAST_PROP
+};
 
 /**
  * NMDeviceOvsBridge:
  */
 struct _NMDeviceOvsBridge {
 	NMDevice parent;
+	GPtrArray *slaves;
 };
 
 struct _NMDeviceOvsBridgeClass {
@@ -41,6 +50,26 @@ struct _NMDeviceOvsBridgeClass {
 G_DEFINE_TYPE (NMDeviceOvsBridge, nm_device_ovs_bridge, NM_TYPE_DEVICE)
 
 /*****************************************************************************/
+
+/**
+ * nm_device_ovs_bridge_get_slaves:
+ * @device: a #NMDeviceOvsBridge
+ *
+ * Gets the ports currently enslaved to @device.
+ *
+ * Returns: (element-type NMDevice): the #GPtrArray containing
+ * #NMDevices that are slaves of @device. This is the internal
+ * copy used by the device, and must not be modified.
+ *
+ * Since: 1.12.2
+ **/
+const GPtrArray *
+nm_device_ovs_bridge_get_slaves (NMDeviceOvsBridge *device)
+{
+	g_return_val_if_fail (NM_IS_DEVICE_OVS_BRIDGE (device), FALSE);
+
+	return device->slaves;
+}
 
 static const char *
 get_type_description (NMDevice *device)
@@ -81,14 +110,65 @@ get_setting_type (NMDevice *device)
 /*****************************************************************************/
 
 static void
+init_dbus (NMObject *object)
+{
+	NMDeviceOvsBridge *device = NM_DEVICE_OVS_BRIDGE (object);
+	const NMPropertiesInfo property_info[] = {
+		{ NM_DEVICE_OVS_BRIDGE_SLAVES, &device->slaves, NULL, NM_TYPE_DEVICE },
+		{ NULL },
+	};
+
+	NM_OBJECT_CLASS (nm_device_ovs_bridge_parent_class)->init_dbus (object);
+
+	_nm_object_register_properties (object,
+	                                NM_DBUS_INTERFACE_DEVICE_OVS_BRIDGE,
+	                                property_info);
+}
+
+static void
+get_property (GObject *object,
+              guint prop_id,
+              GValue *value,
+              GParamSpec *pspec)
+{
+	NMDeviceOvsBridge *device = NM_DEVICE_OVS_BRIDGE (object);
+
+	switch (prop_id) {
+	case PROP_SLAVES:
+		g_value_take_boxed (value, _nm_utils_copy_object_array (nm_device_ovs_bridge_get_slaves (device)));
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static void
 nm_device_ovs_bridge_init (NMDeviceOvsBridge *device)
 {
 }
 
 static void
+dispose (GObject *object)
+{
+	NMDeviceOvsBridge *device = NM_DEVICE_OVS_BRIDGE (object);
+
+	g_clear_pointer (&device->slaves, g_ptr_array_unref);
+
+	G_OBJECT_CLASS (nm_device_ovs_bridge_parent_class)->dispose (object);
+}
+
+static void
 nm_device_ovs_bridge_class_init (NMDeviceOvsBridgeClass *ovs_bridge_class)
 {
+	GObjectClass *object_class = G_OBJECT_CLASS (ovs_bridge_class);
+	NMObjectClass *nm_object_class = NM_OBJECT_CLASS (ovs_bridge_class);
 	NMDeviceClass *device_class = NM_DEVICE_CLASS (ovs_bridge_class);
+
+	object_class->get_property = get_property;
+	object_class->dispose = dispose;
+
+	nm_object_class->init_dbus = init_dbus;
 
 	device_class->get_type_description = get_type_description;
 	device_class->connection_compatible = connection_compatible;

libnm/nm-device-ovs-bridge.h
@@ -14,7 +14,7 @@
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301 USA.
  *
- * Copyright 2017 Red Hat, Inc.
+ * Copyright 2017,2018 Red Hat, Inc.
  */
 
 #ifndef __NM_DEVICE_OVS_BRIDGE_H__
@@ -35,10 +35,15 @@ G_BEGIN_DECLS
 #define NM_IS_DEVICE_OVS_BRIDGE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), NM_TYPE_DEVICE_OVS_BRIDGE))
 #define NM_DEVICE_OVS_BRIDGE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), NM_TYPE_DEVICE_OVS_BRIDGE, NMDeviceOvsBridgeClass))
 
+#define NM_DEVICE_OVS_BRIDGE_SLAVES "slaves"
+
 typedef struct _NMDeviceOvsBridgeClass NMDeviceOvsBridgeClass;
 
 NM_AVAILABLE_IN_1_10
 GType nm_device_ovs_bridge_get_type (void);
+
+NM_AVAILABLE_IN_1_12_2
+const GPtrArray *nm_device_ovs_bridge_get_slaves (NMDeviceOvsBridge *device);
 
 G_END_DECLS
 

libnm/nm-device-ovs-port.c
@@ -14,7 +14,7 @@
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301 USA.
  *
- * Copyright 2017 Red Hat, Inc.
+ * Copyright 2017,2018 Red Hat, Inc.
  */
 
 #include "nm-default.h"
@@ -26,12 +26,21 @@
 #include "nm-setting-ovs-port.h"
 #include "nm-setting-ovs-port.h"
 #include "nm-setting-connection.h"
+#include "nm-core-internal.h"
+
+enum {
+	PROP_0,
+	PROP_SLAVES,
+
+	LAST_PROP
+};
 
 /**
  * NMDeviceOvsPort:
  */
 struct _NMDeviceOvsPort {
 	NMDevice parent;
+	GPtrArray *slaves;
 };
 
 struct _NMDeviceOvsPortClass {
@@ -41,6 +50,26 @@ struct _NMDeviceOvsPortClass {
 G_DEFINE_TYPE (NMDeviceOvsPort, nm_device_ovs_port, NM_TYPE_DEVICE)
 
 /*****************************************************************************/
+
+/**
+ * nm_device_ovs_port_get_slaves:
+ * @device: a #NMDeviceOvsPort
+ *
+ * Gets the interfaces currently enslaved to @device.
+ *
+ * Returns: (element-type NMDevice): the #GPtrArray containing
+ * #NMDevices that are slaves of @device. This is the internal
+ * copy used by the device, and must not be modified.
+ *
+ * Since: 1.12.2
+ **/
+const GPtrArray *
+nm_device_ovs_port_get_slaves (NMDeviceOvsPort *device)
+{
+	g_return_val_if_fail (NM_IS_DEVICE_OVS_PORT (device), FALSE);
+
+	return device->slaves;
+}
 
 static const char *
 get_type_description (NMDevice *device)
@@ -80,14 +109,65 @@ get_setting_type (NMDevice *device)
 /*****************************************************************************/
 
 static void
+init_dbus (NMObject *object)
+{
+	NMDeviceOvsPort *device = NM_DEVICE_OVS_PORT (object);
+	const NMPropertiesInfo property_info[] = {
+		{ NM_DEVICE_OVS_PORT_SLAVES, &device->slaves, NULL, NM_TYPE_DEVICE },
+		{ NULL },
+	};
+
+	NM_OBJECT_CLASS (nm_device_ovs_port_parent_class)->init_dbus (object);
+
+	_nm_object_register_properties (object,
+	                                NM_DBUS_INTERFACE_DEVICE_OVS_PORT,
+	                                property_info);
+}
+
+static void
+get_property (GObject *object,
+              guint prop_id,
+              GValue *value,
+              GParamSpec *pspec)
+{
+	NMDeviceOvsPort *device = NM_DEVICE_OVS_PORT (object);
+
+	switch (prop_id) {
+	case PROP_SLAVES:
+		g_value_take_boxed (value, _nm_utils_copy_object_array (nm_device_ovs_port_get_slaves (device)));
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static void
 nm_device_ovs_port_init (NMDeviceOvsPort *device)
 {
 }
 
 static void
+dispose (GObject *object)
+{
+	NMDeviceOvsPort *device = NM_DEVICE_OVS_PORT (object);
+
+	g_clear_pointer (&device->slaves, g_ptr_array_unref);
+
+	G_OBJECT_CLASS (nm_device_ovs_port_parent_class)->dispose (object);
+}
+
+static void
 nm_device_ovs_port_class_init (NMDeviceOvsPortClass *ovs_port_class)
 {
+	GObjectClass *object_class = G_OBJECT_CLASS (ovs_port_class);
+	NMObjectClass *nm_object_class = NM_OBJECT_CLASS (ovs_port_class);
 	NMDeviceClass *device_class = NM_DEVICE_CLASS (ovs_port_class);
+
+	object_class->get_property = get_property;
+	object_class->dispose = dispose;
+
+	nm_object_class->init_dbus = init_dbus;
 
 	device_class->get_type_description = get_type_description;
 	device_class->connection_compatible = connection_compatible;

libnm/nm-device-ovs-port.h
@@ -14,7 +14,7 @@
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301 USA.
  *
- * Copyright 2017 Red Hat, Inc.
+ * Copyright 2017,2018 Red Hat, Inc.
  */
 
 #ifndef __NM_DEVICE_OVS_PORT_H__
@@ -35,10 +35,15 @@ G_BEGIN_DECLS
 #define NM_IS_DEVICE_OVS_PORT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), NM_TYPE_DEVICE_OVS_PORT))
 #define NM_DEVICE_OVS_PORT_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), NM_TYPE_DEVICE_OVS_PORT, NMDeviceOvsPortClass))
 
+#define NM_DEVICE_OVS_PORT_SLAVES "slaves"
+
 typedef struct _NMDeviceOvsPortClass NMDeviceOvsPortClass;
 
 NM_AVAILABLE_IN_1_10
 GType nm_device_ovs_port_get_type (void);
+
+NM_AVAILABLE_IN_1_12_2
+const GPtrArray *nm_device_ovs_port_get_slaves (NMDeviceOvsPort *device);
 
 G_END_DECLS
 

commit 0a3755c1799d3a4dc1875d4c59c7c568a64c8456

version: fix compile error due to NM_AVAILABLE_IN_1_12_2 macro
Fixes: 8d09660481e1ff8b513aa60b2c40a4982ccc57cd

https://mail.gnome.org/archives/networkmanager-list/2018-July/msg00023.html

libnm-core/nm-version.h
@@ -161,7 +161,7 @@
 #endif
 
 #if NM_VERSION_MAX_ALLOWED < NM_VERSION_1_12_2
-# define NM_AVAILABLE_IN_1_12_2          G_UNAVAILABLE(1,12,2)
+# define NM_AVAILABLE_IN_1_12_2          G_UNAVAILABLE(1,12.2)
 #else
 # define NM_AVAILABLE_IN_1_12_2
 #endif