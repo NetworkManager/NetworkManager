aca73150add69447233e911aff0e5f54e4b22f1a
Thomas Haller <thaller@redhat.com>
2018-08-03 13:38:01 +0200

    libnm/docs: fix examples for NMSetting:name values

--- libnm-core/nm-setting.c
+++ libnm-core/nm-setting.c
@@ -2092,7 +2092,7 @@ nm_setting_class_init (NMSettingClass *setting_class)
 	 *
 	 * The setting's name, which uniquely identifies the setting within the
 	 * connection.  Each setting type has a name unique to that type, for
-	 * example "ppp" or "wireless" or "wired".
+	 * example "ppp" or "802-11-wireless" or "802-3-ethernet".
 	 **/
 	g_object_class_install_property
 		(object_class, PROP_NAME,
