fdcc7e5be9805aa1448699f6e0e8b54342ade2d3
Thomas Haller <thaller@redhat.com>
2018-08-29 13:27:00 +0200

    libnm/crypto: adjust argument types for crypto_md5_hash()
    There should be a clear distinction between whether an array
    is a NUL terminated string or binary with a length.
    
    crypto_md5_hash() is already complicated enough. Adjust it's
    API to only support binary arguments, and thus have "guint8 *" type.

--- libnm-core/tests/test-crypto.c
+++ libnm-core/tests/test-crypto.c
@@ -417,15 +417,16 @@ test_md5 (void)
 
 	for (i = 0; i < G_N_ELEMENTS (md5_tests); i++) {
 		memset (digest, 0, sizeof (digest));
-		crypto_md5_hash (md5_tests[i].salt,
+		crypto_md5_hash ((const guint8 *) md5_tests[i].salt,
 		                 /* crypto_md5_hash() used to clamp salt_len to 8.  It
 		                  * doesn't any more, so we need to do it here now to
 		                  * get output that matches md5_tests[i].result.
 		                  */
 		                 md5_tests[i].salt ? 8 : 0,
-		                 md5_tests[i].password,
+		                 (const guint8 *) md5_tests[i].password,
 		                 strlen (md5_tests[i].password),
-		                 digest, md5_tests[i].digest_size);
+		                 (guint8 *) digest,
+		                 md5_tests[i].digest_size);
 
 		hex = nm_utils_bin2hexstr (digest, md5_tests[i].digest_size, -1);
 		g_assert_cmpstr (hex, ==, md5_tests[i].result);
