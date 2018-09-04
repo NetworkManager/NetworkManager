NetworkManager/NetworkManager

commit 5ef81dc0fbbe41b85abe7929563d41b03eaf6989

contrib/rpm: change default DHCP plugin to internal
... but keep "dhclient" on Fedora and RHEL <= 7.

https://bugzilla.redhat.com/show_bug.cgi?id=1571655

contrib/fedora/rpm/NetworkManager.spec
@@ -101,6 +101,12 @@
 %global with_modem_manager_1 0
 %endif
 
+%if 0%{fedora} || 0%{?rhel} <= 7
+%global dhcp_default dhclient
+%else
+%global dhcp_default internal
+%endif
+
 ###############################################################################
 
 Name: NetworkManager
@@ -464,7 +470,7 @@ intltoolize --automake --copy --force
 	--with-dhclient=yes \
 	--with-dhcpcd=no \
 	--with-dhcpcanon=no \
-	--with-config-dhcp-default=dhclient \
+	--with-config-dhcp-default=%{dhcp_default} \
 %if %{with crypto_gnutls}
 	--with-crypto=gnutls \
 %else

commit c25d54d0fd24a1d0c7d8f32c51b6ebc88c94faba

contrib/rpm: fix invalid %if condition for building on RHEL
Fixes: 5ef81dc0fbbe41b85abe7929563d41b03eaf6989

contrib/fedora/rpm/NetworkManager.spec
@@ -101,7 +101,7 @@
 %global with_modem_manager_1 0
 %endif
 
-%if 0%{fedora} || 0%{?rhel} <= 7
+%if 0%{?fedora} || 0%{?rhel} <= 7
 %global dhcp_default dhclient
 %else
 %global dhcp_default internal