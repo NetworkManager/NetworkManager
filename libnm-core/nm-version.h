57cfc6f19da7d3cd290ff39e3403acaae65f0878
Thomas Haller <thaller@redhat.com>
2018-09-08 10:24:20 +0200

    release: bump version to 1.15.0 (development)

--- libnm-core/nm-version.h
+++ libnm-core/nm-version.h
@@ -174,4 +174,18 @@
 # define NM_AVAILABLE_IN_1_14
 #endif
 
+#if NM_VERSION_MIN_REQUIRED >= NM_VERSION_1_16
+# define NM_DEPRECATED_IN_1_16           G_DEPRECATED
+# define NM_DEPRECATED_IN_1_16_FOR(f)    G_DEPRECATED_FOR(f)
+#else
+# define NM_DEPRECATED_IN_1_16
+# define NM_DEPRECATED_IN_1_16_FOR(f)
+#endif
+
+#if NM_VERSION_MAX_ALLOWED < NM_VERSION_1_16
+# define NM_AVAILABLE_IN_1_16            G_UNAVAILABLE(1,16)
+#else
+# define NM_AVAILABLE_IN_1_16
+#endif
+
 #endif  /* NM_VERSION_H */
