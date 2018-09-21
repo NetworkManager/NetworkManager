NetworkManager/NetworkManager

commit 95b006c244978fecec9463690477e8b64f743202

dns: refactor create_resolv_conf() to use GString for constructing content

src/dns/nm-dns-manager.c
@@ -586,49 +586,42 @@ create_resolv_conf (char **searches,
                     char **nameservers,
                     char **options)
 {
-	gs_free char *searches_str = NULL;
-	gs_free char *nameservers_str = NULL;
-	gs_free char *options_str = NULL;
-	char *tmp_str;
 	GString *str;
-	int i;
+	gsize i;
 
-	if (searches) {
-		tmp_str = g_strjoinv (" ", searches);
-		searches_str = g_strconcat ("search ", tmp_str, "\n", NULL);
-		g_free (tmp_str);
+	str = g_string_new_len ("# Generated by NetworkManager\n", 245);
+
+	if (searches && searches[0]) {
+		g_string_append (str, "search");
+		for (i = 0; searches[i]; i++) {
+			g_string_append_c (str, ' ');
+			g_string_append (str, searches[i]);
+		}
+		g_string_append_c (str, '\n');
 	}
 
-	if (options) {
-		tmp_str = g_strjoinv (" ", options);
-		options_str = g_strconcat ("options ", tmp_str, "\n", NULL);
-		g_free (tmp_str);
-	}
-
-	if (nameservers) {
-		int num = g_strv_length (nameservers);
-
-		str = g_string_new ("");
-		for (i = 0; i < num; i++) {
+	if (nameservers && nameservers[0]) {
+		for (i = 0; nameservers[i]; i++) {
 			if (i == 3) {
-				g_string_append (str, "# ");
-				g_string_append (str, "NOTE: the libc resolver may not support more than 3 nameservers.");
-				g_string_append (str, "\n# ");
-				g_string_append (str, "The nameservers listed below may not be recognized.");
-				g_string_append_c (str, '\n');
+				g_string_append (str, "# NOTE: the libc resolver may not support more than 3 nameservers.\n");
+				g_string_append (str, "# The nameservers listed below may not be recognized.\n");
 			}
-
 			g_string_append (str, "nameserver ");
 			g_string_append (str, nameservers[i]);
 			g_string_append_c (str, '\n');
 		}
-		nameservers_str = g_string_free (str, FALSE);
 	}
 
-	return g_strdup_printf ("# Generated by NetworkManager\n%s%s%s",
-	                        searches_str ?: "",
-	                        nameservers_str ?: "",
-	                        options_str ?: "");
+	if (options && options[0]) {
+		g_string_append (str, "options");
+		for (i = 0; options[i]; i++) {
+			g_string_append_c (str, ' ');
+			g_string_append (str, options[i]);
+		}
+		g_string_append_c (str, '\n');
+	}
+
+	return g_string_free (str, FALSE);
 }
 
 static gboolean

commit 511709c54df5ecdc96453cb8af9796a54d901aa8

dns: fix creating resolv.conf content
g_string_new_len() allocates the buffer with length
bytes. Maybe it should be obvious (wasn't to me), but
if a init argument is given, that is taken as containing
length bytes.

So,

    str = g_string_new_len (init, len);

is more like

    str = g_string_new_len (NULL, len);
    g_string_append_len (str, init, len);

and not (how I wrongly thought)

    str = g_string_new_len (NULL, len);
    g_string_append (str, init);

Fixes: 95b006c244978fecec9463690477e8b64f743202

src/dns/nm-dns-manager.c
@@ -589,7 +589,9 @@ create_resolv_conf (char **searches,
 	GString *str;
 	gsize i;
 
-	str = g_string_new_len ("# Generated by NetworkManager\n", 245);
+	str = g_string_new_len (NULL, 245);
+
+	g_string_append (str, "# Generated by NetworkManager\n");
 
 	if (searches && searches[0]) {
 		g_string_append (str, "search");