b132ab7c7578a174b92d6a1b895651279ba61929
Thomas Haller <thaller@redhat.com>
2018-10-11 14:57:38 +0200

    ndisc: fix updating address lifetime on Router Announcement according to RFC4862
    This is a denial-of-service protection, where a malicious router
    advertisement can expire the addresses.
    
    See-also: https://github.com/systemd/systemd/commit/6554550f35a7976f9110aff94743d3576d5f02dd
    See-also: https://tools.ietf.org/search/rfc4862#section-5.5.3
    
    https://bugs.launchpad.net/ubuntu/+source/network-manager/+bug/1796622

--- src/ndisc/nm-lndp-ndisc.c
+++ src/ndisc/nm-lndp-ndisc.c
@@ -222,7 +222,7 @@ receive_ra (struct ndp *ndp, struct ndp_msg *msg, gpointer user_data)
 			};
 
 			if (address.preferred <= address.lifetime) {
-				if (nm_ndisc_complete_and_add_address (ndisc, &address))
+				if (nm_ndisc_complete_and_add_address (ndisc, &address, now))
 					changed |= NM_NDISC_CONFIG_ADDRESSES;
 			}
 		}
