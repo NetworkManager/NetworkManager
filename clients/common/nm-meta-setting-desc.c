NetworkManager/NetworkManager

commit 9a68123827a8c4fe1eeaaf003d365429441d97e9

clients: implement serial.parity as enum property
Previously we would only consider the first character of
value. For example, "Nonsense" was also allowed. Don't do
that anymore.

clients/common/nm-meta-setting-desc.c
@@ -3648,44 +3648,6 @@ _validate_fcn_proxy_pac_script (const char *value, char **out_to_free, GError **
 	RETURN_STR_TO_FREE (script);
 }
 
-static gconstpointer
-_get_fcn_serial_parity (ARGS_GET_FCN)
-{
-	NMSettingSerial *s_serial = NM_SETTING_SERIAL (setting);
-
-	RETURN_UNSUPPORTED_GET_TYPE ();
-
-	switch (nm_setting_serial_get_parity (s_serial)) {
-	case NM_SETTING_SERIAL_PARITY_EVEN:
-		return "even";
-	case NM_SETTING_SERIAL_PARITY_ODD:
-		return "odd";
-	default:
-	case NM_SETTING_SERIAL_PARITY_NONE:
-		return "none";
-	}
-}
-
-static gboolean
-_set_fcn_serial_parity (ARGS_SET_FCN)
-{
-	NMSettingSerialParity parity;
-
-	if (value[0] == 'E' || value[0] == 'e')
-		parity = NM_SETTING_SERIAL_PARITY_EVEN;
-	else if (value[0] == 'O' || value[0] == 'o')
-		parity = NM_SETTING_SERIAL_PARITY_ODD;
-	else if (value[0] == 'N' || value[0] == 'n')
-		parity = NM_SETTING_SERIAL_PARITY_NONE;
-	else {
-		g_set_error (error, 1, 0, _("'%s' is not valid; use [e, o, n]"), value);
-		return FALSE;
-	}
-
-	g_object_set (setting, property_info->property_name, parity, NULL);
-	return TRUE;
-}
-
 static gboolean
 _set_fcn_team_config (ARGS_SET_FCN)
 {
@@ -5954,9 +5916,39 @@ static const NMMetaPropertyInfo *const property_infos_SERIAL[] = {
 		.property_type =                &_pt_gobject_int,
 	),
 	PROPERTY_INFO_WITH_DESC (NM_SETTING_SERIAL_PARITY,
-		.property_type = DEFINE_PROPERTY_TYPE (
-			.get_fcn =                  _get_fcn_serial_parity,
-			.set_fcn =                  _set_fcn_serial_parity,
+		.property_type =                &_pt_gobject_enum,
+		.property_typ_data = DEFINE_PROPERTY_TYP_DATA (
+			PROPERTY_TYP_DATA_SUBTYPE (gobject_enum,
+				.get_gtype =            nm_setting_serial_parity_get_type,
+				.value_infos =          ENUM_VALUE_INFOS (
+					{
+						.value = NM_SETTING_SERIAL_PARITY_EVEN,
+						.nick = "E",
+					},
+					{
+						.value = NM_SETTING_SERIAL_PARITY_EVEN,
+						.nick = "e",
+					},
+					{
+						.value = NM_SETTING_SERIAL_PARITY_ODD,
+						.nick = "O",
+					},
+					{
+						.value = NM_SETTING_SERIAL_PARITY_ODD,
+						.nick = "o",
+					},
+					{
+						.value = NM_SETTING_SERIAL_PARITY_NONE,
+						.nick = "N",
+					},
+					{
+						.value = NM_SETTING_SERIAL_PARITY_NONE,
+						.nick = "n",
+					}
+				),
+			),
+			.typ_flags =                  NM_META_PROPERTY_TYP_FLAG_ENUM_GET_PARSABLE_TEXT
+			                            | NM_META_PROPERTY_TYP_FLAG_ENUM_GET_PRETTY_TEXT,
 		),
 	),
 	PROPERTY_INFO_WITH_DESC (NM_SETTING_SERIAL_STOPBITS,

commit 200f5356e6579ef31208a3e37345a85ca83ad731

cli: fix setting "serial.parity" enum
The meta data type descriptor must set .get_gtype only for
GObject properties which are of type int or uint. That is, when
the enum type cannot be automatically detected.

However, NM_SETTING_SERIAL_PARITY is a g_param_spec_enum()
of type NM_TYPE_SETTING_SERIAL_PARITY, so setting the get_gtype()
hook is wrong and leads to a crash

   $ /bin/nmcli connection add type gsm autoconnect no con-name t ifname '*' apn xyz serial.parity 5

   (process:11086): libnmc-CRITICAL **: 15:04:35.180: file clients/common/nm-meta-setting-desc.c: line 1283 (_set_fcn_gobject_enum): should not be reached
   Segmentation fault (core dumped)

That is because the enum property setter does:

    »···if (   has_gtype
    »···    && NM_IN_SET (gtype_prop,
    »···                  G_TYPE_INT,
    »···                  G_TYPE_UINT)
    »···    && G_TYPE_IS_CLASSED (gtype)
    »···    && (gtype_class = g_type_class_ref (gtype))
    »···    && (   (is_flags = G_IS_FLAGS_CLASS (gtype_class))
    »···        || G_IS_ENUM_CLASS (gtype_class))) {
    »···»···/* valid */

meaning, it only allows "has_gtype" if the native "gtype_prop" is
G_TYPE_INT or G_TYPE_UINT.

Fixes: 9a68123827a8c4fe1eeaaf003d365429441d97e9

clients/common/nm-meta-setting-desc.c
@@ -7152,7 +7152,6 @@ static const NMMetaPropertyInfo *const property_infos_SERIAL[] = {
 		.property_type =                &_pt_gobject_enum,
 		.property_typ_data = DEFINE_PROPERTY_TYP_DATA (
 			PROPERTY_TYP_DATA_SUBTYPE (gobject_enum,
-				.get_gtype =            nm_setting_serial_parity_get_type,
 				.value_infos =          ENUM_VALUE_INFOS (
 					{
 						.value = NM_SETTING_SERIAL_PARITY_EVEN,