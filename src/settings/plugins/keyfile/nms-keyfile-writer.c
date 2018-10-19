7d3fba9366b6b5bf52f793569dd6a73db82503fb
Thomas Haller <thaller@redhat.com>
2018-10-19 15:12:34 +0200

    keyfile: also add ".nmconnection" extension when writing keyfiles in /etc
    This is a change in behavior regarding the filename that we choose when
    writing files to "/etc/NetworkManager/system-connections/".
    
    (cherry picked from commit d37ad15f12bafd91cf724cda50aea7093e04bf7a)

--- src/settings/plugins/keyfile/nms-keyfile-writer.c
+++ src/settings/plugins/keyfile/nms-keyfile-writer.c
@@ -352,19 +352,16 @@ nms_keyfile_writer_connection (NMConnection *connection,
                                GError **error)
 {
 	const char *keyfile_dir;
-	gboolean with_extension = FALSE;
 
 	if (save_to_disk)
 		keyfile_dir = nms_keyfile_utils_get_path ();
-	else {
+	else
 		keyfile_dir = NM_CONFIG_KEYFILE_PATH_IN_MEMORY;
-		with_extension = TRUE;
-	}
 
 	return _internal_write_connection (connection,
 	                                   keyfile_dir,
 	                                   nms_keyfile_utils_get_path (),
-	                                   with_extension,
+	                                   TRUE,
 	                                   0,
 	                                   0,
 	                                   existing_path,
