e11ee4582aad43d3bcc8a2ba9eaf9f14e976075a
Michael Biebl <biebl@debian.org>
2018-10-25 06:10:18 +0200

    docs: fix bashism in tools/check-docs.sh
    [thaller@redhat.com: fixed issue in original patch]

--- tools/check-docs.sh
+++ tools/check-docs.sh
@@ -13,7 +13,7 @@ fi
 # Check that files that define types that are in public libnm API are included in libnm documentation.
 # Don't complain about readability or I'll rewrite this in Perl.
 if (sed -n 's/.*<xi:include href="\(xml\/.*\.xml\)".*/\1\n\1/p' $SOURCEDIR''docs/libnm/libnm-docs.xml;
-    grep -lE "$(sed -n 's/^[\t ]*\(.*_get_type\);/\1/p' $SOURCEDIR''libnm/libnm.ver |xargs echo |sed 's/ /|/g')" $SOURCEDIR''libnm{,-core}/*.h |
+    grep -lE "$(sed -n 's/^[\t ]*\(.*_get_type\);/\1/p' $SOURCEDIR''libnm/libnm.ver |xargs echo |sed 's/ /|/g')" $SOURCEDIR''libnm/*.h $SOURCEDIR''libnm-core/*.h |
     sed 's,.*/,xml/,;s/\.h$/.xml/') |sort |uniq -u| grep . >&2; then
 	echo "*** Error: libnm classes not included in docs/libnm/libnm-docs.xml ***" >&2
 	exit 1
