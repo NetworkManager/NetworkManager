NetworkManager/NetworkManager

commit 7a59cd274485e4c0345c563d48e516967630d7f0

docs: rework check-docs test script
Try to make check-docs.sh script more readable.

Also, previously the script would check that one side was a subset
of the other side. Tighten this check up, now both sides of the
comparison must agree and yield the same lines.

tools/check-docs.sh
@@ -1,22 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 
-SOURCEDIR=$1
+set -e
+
+die() {
+    printf '%s\n' "$@" >&2
+    exit 1
+}
+
+word_regex() {
+    tr '\n|<>\\' ' ' \
+    | sed -e 's, *$,\\>,' \
+          -e 's,^ *,\\<,' \
+          -e 's, \+,\\>\\|\\<,g'
+}
+
+same_lines() {
+    diff <(printf "%s\n" "$1"          | sed '/^$/d' | sort) \
+         <(printf "%s\n%s\n" "$2" "$3" | sed '/^$/d' | sort) >&2
+}
+
+SOURCEDIR="$1"
 [ -n "$SOURCEDIR" ] && SOURCEDIR="$SOURCEDIR/"
 
 # Check that the D-Bus API docs contain all known interfaces
-if (sed -n 's/.*<xi:include href="dbus-\(.*\.xml\)".*/\1\n\1/p' $SOURCEDIR''docs/api/network-manager-docs.xml;
-    cd $SOURCEDIR''introspection; ls *.xml) |sort |uniq -u| grep . >&2; then
-	echo "*** Error: D-Bus interfaces not included in docs/api/network-manager-docs.xml ***" >&2
-	exit 1
+F1="$(sed -n 's,^      <xi:include href="dbus-\([^"]*\.xml\)"/>$,\1,p' "$SOURCEDIR"docs/api/network-manager-docs.xml)"
+F2="$(cd "$SOURCEDIR"introspection; ls -1 *.xml)"
+if ! same_lines "$F1" "$F2" ; then
+    die "*** Error: D-Bus interfaces not included in docs/api/network-manager-docs.xml ***"
 fi
 
 # Check that files that define types that are in public libnm API are included in libnm documentation.
-# Don't complain about readability or I'll rewrite this in Perl.
-if (sed -n 's/.*<xi:include href="\(xml\/.*\.xml\)".*/\1\n\1/p' $SOURCEDIR''docs/libnm/libnm-docs.xml;
-    grep -lE "$(sed -n 's/^[\t ]*\(.*_get_type\);/\1/p' $SOURCEDIR''libnm/libnm.ver |xargs echo |sed 's/ /|/g')" $SOURCEDIR''libnm/*.h $SOURCEDIR''libnm-core/*.h |
-    sed 's,.*/,xml/,;s/\.h$/.xml/') |sort |uniq -u| grep . >&2; then
-	echo "*** Error: libnm classes not included in docs/libnm/libnm-docs.xml ***" >&2
-	exit 1
+F1="$(sed -n 's/.*<xi:include href="xml\/\([^"]*\)\.xml".*/\1/p' "$SOURCEDIR"docs/libnm/libnm-docs.xml)"
+F2="$(grep -l "$(sed -n 's/^[\t ]*\(.*_get_type\);/\1/p' "$SOURCEDIR"libnm/libnm.ver | word_regex)" \
+           "$SOURCEDIR"libnm/*.h \
+           "$SOURCEDIR"libnm-core/*.h \
+      | sed 's,.*/\([^/]\+\)\.h$,\1,')"
+F2_EXTRA="
+annotation-glossary
+api-index-full
+nm-dbus-interface
+nm-errors
+nm-utils
+nm-version
+"
+if ! same_lines "$F1" "$F2" "$F2_EXTRA"; then
+    die "*** Error: libnm classes not included in docs/libnm/libnm-docs.xml ***"
 fi
-
-exit 0

commit 168e8b9b6fccf0145dd2fac784612953d94285d4

build: fix check-docs.sh for out-of-tree builds
Fixes: 7a59cd274485e4c0345c563d48e516967630d7f0

Makefile.am
@@ -462,7 +462,7 @@ EXTRA_DIST += \
 	introspection/meson.build
 
 check-docs:
-	$(srcdir)/tools/check-docs.sh "$(srcdir)"
+	$(srcdir)/tools/check-docs.sh "$(srcdir)" "$(builddir)"
 
 check_local += check-docs
 

docs/api/network-manager-docs.xml
@@ -257,15 +257,6 @@
       <title>Types</title>
       <xi:include href="../../libnm-core/nm-dbus-types.xml"/>
     </chapter>
-
-    <!--
-      These are obsolete and intentionally omitted from the manual since they can't
-      appear on the D-Bus API of the present version of NetworkManager.
-      Keep them here so that "make check" won't think that they were skipped accidentally .
-      <xi:include href="dbus-org.freedesktop.NetworkManager.Device.WiMax.xml"/>
-      <xi:include href="dbus-org.freedesktop.NetworkManager.WiMax.Nsp.xml"/>
-    -->
-
   </part>
 
   <part id="secret-agents">

docs/libnm/libnm-docs.xml
@@ -294,13 +294,6 @@ print ("NetworkManager version " + client.get_version())]]></programlisting></in
     <xi:include href="xml/nm-vpn-plugin-old.xml"/>
   </chapter>
 
-  <!--
-    These don't contain any useful documentation. Keep them here,
-    so that tools/check-docs.sh knows that we did omit them intentionally.
-    <xi:include href="xml/nm-core-enum-types.xml"/>
-    <xi:include href="xml/nm-enum-types.xml"/>
-  -->
-
   <chapter id="object-tree">
     <title>Object Hierarchy</title>
      <xi:include href="xml/tree_index.sgml"/>

docs/meson.build
@@ -14,5 +14,5 @@ endif
 test(
   'check-docs',
   find_program(join_paths(meson.source_root(), 'tools', 'check-docs.sh')),
-  args: [meson.source_root()]
+  args: [meson.source_root(), meson.build_root()]
 )

tools/check-docs.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export LANG=C
+
 die() {
     printf '%s\n' "$@" >&2
     exit 1
@@ -15,25 +17,48 @@ word_regex() {
 }
 
 same_lines() {
-    diff <(printf "%s\n" "$1"          | sed '/^$/d' | sort) \
-         <(printf "%s\n%s\n" "$2" "$3" | sed '/^$/d' | sort) >&2
+    diff <(printf "%s\n" "$1" | sed '/^$/d' | sort) \
+         <(printf "%s\n" "$2" | sed '/^$/d' | sort) >&2
 }
 
+libnm_headers() {
+    (
+        ls -1 "$1"libnm/*.h "$1"libnm-core/*.h
+        if [ -n "$2" ]; then
+            ls -1 "$2"/libnm/*.h "$2"/libnm-core/*.h
+        fi
+    ) | sort | uniq
+}
+
+
 SOURCEDIR="$1"
+BUILDDIR="$2"
+if test "$SOURCEDIR" == "$BUILDDIR"; then
+    BUILDDIR=
+fi
 [ -n "$SOURCEDIR" ] && SOURCEDIR="$SOURCEDIR/"
+
 
 # Check that the D-Bus API docs contain all known interfaces
 F1="$(sed -n 's,^      <xi:include href="dbus-\([^"]*\.xml\)"/>$,\1,p' "$SOURCEDIR"docs/api/network-manager-docs.xml)"
+F1_EXTRA="
+org.freedesktop.NetworkManager.Device.WiMax.xml
+org.freedesktop.NetworkManager.WiMax.Nsp.xml
+"
 F2="$(cd "$SOURCEDIR"introspection; ls -1 *.xml)"
-if ! same_lines "$F1" "$F2" ; then
+if ! same_lines "$F1"$'\n'"$F1_EXTRA" "$F2" ; then
     die "*** Error: D-Bus interfaces not included in docs/api/network-manager-docs.xml ***"
 fi
 
+
 # Check that files that define types that are in public libnm API are included in libnm documentation.
 F1="$(sed -n 's/.*<xi:include href="xml\/\([^"]*\)\.xml".*/\1/p' "$SOURCEDIR"docs/libnm/libnm-docs.xml)"
+F1_EXTRA="
+nm-core-enum-types
+nm-enum-types
+"
 F2="$(grep -l "$(sed -n 's/^[\t ]*\(.*_get_type\);/\1/p' "$SOURCEDIR"libnm/libnm.ver | word_regex)" \
-           "$SOURCEDIR"libnm/*.h \
-           "$SOURCEDIR"libnm-core/*.h \
+           $(libnm_headers "$SOURCEDIR" "$BUILDDIR") \
       | sed 's,.*/\([^/]\+\)\.h$,\1,')"
 F2_EXTRA="
 annotation-glossary
@@ -43,6 +68,6 @@ nm-errors
 nm-utils
 nm-version
 "
-if ! same_lines "$F1" "$F2" "$F2_EXTRA"; then
+if ! same_lines "$F1"$'\n'"$F1_EXTRA" "$F2"$'\n'"$F2_EXTRA"; then
     die "*** Error: libnm classes not included in docs/libnm/libnm-docs.xml ***"
 fi