f330b198b18e9502d8c9ade9e33fa5ff8030da99
Beniamino Galvani <bgalvani@redhat.com>
2018-11-06 10:27:34 +0100

    modem: fix crash when disconnecting
    Fixes: 9b935fad9b108ee10299f2bd56f27db59ee34637

--- src/devices/wwan/nm-modem-broadband.c
+++ src/devices/wwan/nm-modem-broadband.c
@@ -1118,7 +1118,7 @@ static void
 disconnect_context_complete_on_idle (gpointer user_data,
                                      GCancellable *cancellable)
 {
-	DisconnectContext *ctx = NULL;
+	DisconnectContext *ctx = user_data;
 	gs_free_error GError *cancelled_error = NULL;
 
 	g_cancellable_set_error_if_cancelled (cancellable, &cancelled_error);
