NetworkManager/NetworkManager

commit eb883e34a5889812d35e5c2d7bc6051723001a3a

core: Add option to AddAndActivateConnection2 to bind the lifetime
This allows binding the lifetime of the created connection to the
existance of the requesting dbus client. This feature is useful if one
has a service specific connection (e.g. P2P wireless) which will not be
useful without the specific service.

This is simply a mechanism to ensure proper connection cleanup if the
requesting service has a failure.

introspection/org.freedesktop.NetworkManager.xml
@@ -129,6 +129,7 @@
         parameters. At this time the following options are supported:
 
          * persist: A string value of either "disk" (default), "memory" or "volatile". If "memory" is passed, the connection will not be saved to disk. If "volatile" is passed, the connection will not be saved to disk and will be destroyed when disconnected.
+         * bind: Bind the connections lifetime. Set to "dbus-name" to automatically disconnect when the requesting process disappears from the bus. The default of "none" means the connection is kept alive normally. If restricted, then persist must be set to "volatile".
     -->
     <method name="AddAndActivateConnection2">
       <arg name="connection" type="a{sa{sv}}" direction="in"/>

src/nm-manager.c
@@ -5224,7 +5224,7 @@ impl_manager_add_and_activate_connection (NMDBusObject *obj,
 	NMManager *self = NM_MANAGER (obj);
 	NMManagerPrivate *priv = NM_MANAGER_GET_PRIVATE (self);
 	gs_unref_object NMConnection *incompl_conn = NULL;
-	NMActiveConnection *active = NULL;
+	gs_unref_object NMActiveConnection *active = NULL;
 	gs_unref_object NMAuthSubject *subject = NULL;
 	GError *error = NULL;
 	NMDevice *device = NULL;
@@ -5235,6 +5235,7 @@ impl_manager_add_and_activate_connection (NMDBusObject *obj,
 	const char *specific_object_path;
 	gs_free NMConnection **conns = NULL;
 	NMSettingsConnectionPersistMode persist = NM_SETTINGS_CONNECTION_PERSIST_MODE_DISK;
+	const char *bind_lifetime = "none";
 
 	if (g_strcmp0 (method_info->parent.name, "AddAndActivateConnection2") == 0)
 		g_variant_get (parameters, "(@a{sa{sv}}&o&o@a{sv})", &settings, &device_path, &specific_object_path, &options);
@@ -5269,6 +5270,19 @@ impl_manager_add_and_activate_connection (NMDBusObject *obj,
 					                             "Option \"persist\" must be one of \"volatile\", \"memory\" or \"disk\".");
 					goto error;
 				}
+
+			} else if ((g_strcmp0 (option_name, "bind") == 0) &&
+			    g_variant_is_of_type (option_value, G_VARIANT_TYPE_STRING)) {
+				s = g_variant_get_string (option_value, NULL);
+
+				if (!NM_IN_STRSET (s, "dbus-client", "none")) {
+					error = g_error_new_literal (NM_MANAGER_ERROR,
+					                             NM_MANAGER_ERROR_INVALID_ARGUMENTS,
+					                             "Option \"bind\" must be one of \"dbus-client\" or \"none\".");
+					goto error;
+				}
+
+				bind_lifetime = s;
 
 			} else {
 				/* Unknown argument */
@@ -5349,6 +5363,13 @@ impl_manager_add_and_activate_connection (NMDBusObject *obj,
 	                                 &error);
 	if (!active)
 		goto error;
+
+	if (g_strcmp0 (bind_lifetime, "dbus-client") == 0) {
+		if (persist != NM_SETTINGS_CONNECTION_PERSIST_MODE_VOLATILE_ONLY)
+			goto error;
+
+		nm_active_connection_bind_dbus_client (active, dbus_connection, sender);
+	}
 
 	nm_active_connection_authorize (active,
 	                                incompl_conn,

src/nm-active-connection.c
@@ -1014,6 +1014,23 @@ nm_active_connection_set_parent (NMActiveConnection *self, NMActiveConnection *p
 	g_object_weak_ref ((GObject *) priv->parent, parent_destroyed, self);
 }
 
+/**
+ * nm_active_connection_bind_dbus_client:
+ * @self: the #NMActiveConnection
+ * @dbus_client: The dbus client to watch.
+ *
+ * Binds the lifetime of this active connection to the given dbus client. If
+ * the dbus client disappears, then the connection will be disconnected.
+ */
+void
+nm_active_connection_bind_dbus_client (NMActiveConnection *self, GDBusConnection *dbus_con, const char *dbus_client)
+{
+	NMActiveConnectionPrivate *priv = NM_ACTIVE_CONNECTION_GET_PRIVATE (self);
+
+	nm_keep_alive_set_dbus_client_watch (priv->keep_alive, dbus_con, dbus_client, NULL);
+	nm_keep_alive_sink (priv->keep_alive);
+}
+
 /*****************************************************************************/
 
 static void

src/nm-active-connection.h
@@ -190,4 +190,8 @@ gboolean      nm_active_connection_get_keep_alive (NMActiveConnection *self);
 
 void          nm_active_connection_clear_secrets (NMActiveConnection *self);
 
+void          nm_active_connection_bind_dbus_client (NMActiveConnection *self,
+                                                     GDBusConnection *dbus_con,
+                                                     const char *dbus_name);
+
 #endif /* __NETWORKMANAGER_ACTIVE_CONNECTION_H__ */

commit 7420ae831493f451c6601b888685c9cd54de7f34

all: rename "bind" option for AddAndActivateConnection2 to "bind-activation"
"bind" specifically binds the lifetime of the activation (NMActiveConnection).
In combination with "persist=volatile", the lifetime of the NMSettingsConnection
is indirectly bound to the NMActiveConnection. But still these concepts make sense
independently.
In the future, it may make sense to also bind the lifetime of the NMSettingsConnection
to the D-Bus client. Hence, rename the option to allow for the distinction.

Also, belatedly fix libnm comment about "bind" only working with
"persist" "volatile".

Fixes: eb883e34a5889812d35e5c2d7bc6051723001a3a

introspection/org.freedesktop.NetworkManager.xml
@@ -129,7 +129,7 @@
         parameters. At this time the following options are supported:
 
          * persist: A string value of either "disk" (default), "memory" or "volatile". If "memory" is passed, the connection will not be saved to disk. If "volatile" is passed, the connection will not be saved to disk and will be destroyed when disconnected.
-         * bind: Bind the activation lifetime. Set to "dbus-name" to automatically disconnect when the requesting process disappears from the bus. The default of "none" means the connection is kept activated normally.
+         * bind-activation: Bind the activation lifetime. Set to "dbus-name" to automatically disconnect when the requesting process disappears from the bus. The default of "none" means the connection is kept activated normally.
     -->
     <method name="AddAndActivateConnection2">
       <arg name="connection" type="a{sa{sv}}" direction="in"/>

libnm/nm-client.c
@@ -1304,12 +1304,10 @@ nm_client_add_and_activate_connection_finish (NMClient *client,
  *  * "persist": A string describing how the connection should be stored.
  *               The default is "disk", but it can be modified to "memory" (until
  *               the daemon quits) or "volatile" (will be deleted on disconnect).
- *  * "bind": Bind the connection lifetime to something. The default is "none",
+ *  * "bind-activation": Bind the connection lifetime to something. The default is "none",
  *            meaning an explicit disconnect is needed. The value "dbus-client"
- *            means the connection will automatically be closed when the calling
+ *            means the connection will automatically be deactivated when the calling
  *            DBus client disappears from the system bus.
- *            A non-default "bind" option must always be used together with
- *            "persist" set to "volatile".
  *
  * Since: 1.16
  **/

src/nm-manager.c
@@ -5270,7 +5270,7 @@ impl_manager_add_and_activate_connection (NMDBusObject *obj,
 					                             "Option \"persist\" must be one of \"volatile\", \"memory\" or \"disk\"");
 					goto error;
 				}
-			} else if (   nm_streq (option_name, "bind")
+			} else if (   nm_streq (option_name, "bind-activation")
 			           && g_variant_is_of_type (option_value, G_VARIANT_TYPE_STRING)) {
 				s = g_variant_get_string (option_value, NULL);
 
@@ -5281,7 +5281,7 @@ impl_manager_add_and_activate_connection (NMDBusObject *obj,
 				else {
 					error = g_error_new_literal (NM_MANAGER_ERROR,
 					                             NM_MANAGER_ERROR_INVALID_ARGUMENTS,
-					                             "Option \"bind\" must be one of \"dbus-client\" or \"none\"");
+					                             "Option \"bind-activation\" must be one of \"dbus-client\" or \"none\"");
 					goto error;
 				}
 			} else {