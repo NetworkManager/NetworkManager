NetworkManager/NetworkManager

commit 9664f284a1d8575798daa9abf9cd7f49a19b48d9

connectivity: allow limiting the connectivity check to a specified AF
Nothing changes practically, as the NMDevice still starts this with
AF_UNSPEC. That is going to change in the following commit.

The ugly part:

priv->concheck_x[0] in few places. I believe we shouldn't be using union
aliasing here, and instead of indexing the v4/v6 arrays by a boolean it
should be an enum. I'm not fixing it here, but I eventually plan to if
this gets an ACK.

src/nm-connectivity.c
@@ -68,6 +68,7 @@ struct _NMConnectivityCheckHandle {
 	gpointer user_data;
 
 	char *ifspec;
+	int addr_family;
 
 #if WITH_CONCHECK
 	struct {
@@ -145,9 +146,10 @@ NM_DEFINE_SINGLETON_GETTER (NMConnectivity, nm_connectivity_get, NM_TYPE_CONNECT
             _nm_log (__level, _NMLOG2_DOMAIN, 0, \
                      (cb_data->ifspec ? &cb_data->ifspec[3] : NULL), \
                      NULL, \
-                     "connectivity: (%s) " \
+                     "connectivity: (%s,AF_INET%s) " \
                      _NM_UTILS_MACRO_FIRST (__VA_ARGS__), \
-                     (cb_data->ifspec ? &cb_data->ifspec[3] : "") \
+                     (cb_data->ifspec ? &cb_data->ifspec[3] : ""), \
+                     (cb_data->addr_family == AF_INET6 ? "6" : "") \
                      _NM_UTILS_MACRO_REST (__VA_ARGS__)); \
         } \
     } G_STMT_END
@@ -572,11 +574,27 @@ do_curl_request (NMConnectivityCheckHandle *cb_data)
 {
 	NMConnectivityPrivate *priv = NM_CONNECTIVITY_GET_PRIVATE (cb_data->self);
 	CURL *ehandle;
+	long resolve;
 
 	ehandle = curl_easy_init ();
 	if (!ehandle) {
 		cb_data_free (cb_data, NM_CONNECTIVITY_ERROR, NULL, "curl error");
 		return;
+	}
+
+	switch (cb_data->addr_family) {
+	case AF_INET:
+		resolve = CURL_IPRESOLVE_V4;
+		break;
+	case AF_INET6:
+		resolve = CURL_IPRESOLVE_V6;
+		break;
+	case AF_UNSPEC:
+		resolve = CURL_IPRESOLVE_WHATEVER;
+		break;
+	default:
+		resolve = CURL_IPRESOLVE_WHATEVER;
+		g_warn_if_reached ();
 	}
 
 	cb_data->concheck.response = g_strdup (priv->response);
@@ -593,6 +611,7 @@ do_curl_request (NMConnectivityCheckHandle *cb_data)
 	curl_easy_setopt (ehandle, CURLOPT_HTTPHEADER, cb_data->concheck.request_headers);
 	curl_easy_setopt (ehandle, CURLOPT_INTERFACE, cb_data->ifspec);
 	curl_easy_setopt (ehandle, CURLOPT_RESOLVE, cb_data->concheck.hosts);
+	curl_easy_setopt (ehandle, CURLOPT_IPRESOLVE, resolve);
 
 	curl_multi_add_handle (priv->concheck.curl_mhandle, ehandle);
 }
@@ -607,7 +626,7 @@ resolve_cb (GObject *object, GAsyncResult *res, gpointer user_data)
 	GVariant *addresses;
 	gsize no_addresses;
 	int ifindex;
-	int family;
+	int addr_family;
 	GVariant *address = NULL;
 	const guchar *address_buf;
 	gsize len = 0;
@@ -635,14 +654,13 @@ resolve_cb (GObject *object, GAsyncResult *res, gpointer user_data)
 	g_variant_unref (result);
 
 	for (i = 0; i < no_addresses; i++) {
-		g_variant_get_child (addresses, i, "(ii@ay)", &ifindex, &family, &address);
+		g_variant_get_child (addresses, i, "(ii@ay)", &ifindex, &addr_family, &address);
 		address_buf = g_variant_get_fixed_array (address, &len, 1);
 
-		if (   (family == AF_INET && len == sizeof (struct in_addr))
-		    || (family == AF_INET6 && len == sizeof (struct in6_addr))) {
-			inet_ntop (family, address_buf, str, sizeof (str));
-			host = g_strdup_printf ("%s:%s:%s", priv->host,
-						priv->port ? priv->port : "80", str);
+		if (   (addr_family == AF_INET && len == sizeof (struct in_addr))
+		    || (addr_family == AF_INET6 && len == sizeof (struct in6_addr))) {
+			inet_ntop (addr_family, address_buf, str, sizeof (str));
+			host = g_strdup_printf ("%s:%s:%s", priv->host, priv->port ?: "80", str);
 			cb_data->concheck.hosts = curl_slist_append (cb_data->concheck.hosts, host);
 			_LOG2T ("adding '%s' to curl resolve list", host);
 			g_free (host);
@@ -685,7 +703,7 @@ resolved_proxy_created (GObject *object, GAsyncResult *res, gpointer user_data)
 	                   g_variant_new ("(isit)",
 	                                  cb_data->concheck.ifindex,
 	                                  priv->host,
-	                                  (gint32) AF_UNSPEC,
+	                                  (gint32) cb_data->addr_family,
 	                                  (guint64) SD_RESOLVED_DNS),
 	                   G_DBUS_CALL_FLAGS_NONE,
 	                   -1,
@@ -699,6 +717,7 @@ resolved_proxy_created (GObject *object, GAsyncResult *res, gpointer user_data)
 
 NMConnectivityCheckHandle *
 nm_connectivity_check_start (NMConnectivity *self,
+                             int addr_family,
                              int ifindex,
                              const char *iface,
                              NMConnectivityCheckCallback callback,
@@ -719,6 +738,7 @@ nm_connectivity_check_start (NMConnectivity *self,
 	cb_data->callback = callback;
 	cb_data->user_data = user_data;
 	cb_data->completed_state = NM_CONNECTIVITY_UNKNOWN;
+	cb_data->addr_family = addr_family;
 
 	if (iface)
 		cb_data->ifspec = g_strdup_printf ("if!%s", iface);

src/devices/nm-device.h
@@ -818,6 +818,7 @@ typedef void (*NMDeviceConnectivityCallback) (NMDevice *self,
 void nm_device_check_connectivity_update_interval (NMDevice *self);
 
 NMDeviceConnectivityHandle *nm_device_check_connectivity (NMDevice *self,
+                                                          int addr_family,
                                                           NMDeviceConnectivityCallback callback,
                                                           gpointer user_data);
 

src/nm-connectivity.h
@@ -58,6 +58,7 @@ typedef void (*NMConnectivityCheckCallback) (NMConnectivity *self,
                                              gpointer user_data);
 
 NMConnectivityCheckHandle *nm_connectivity_check_start (NMConnectivity *self,
+                                                        int family,
                                                         int ifindex,
                                                         const char *iface,
                                                         NMConnectivityCheckCallback callback,

src/nm-manager.c
@@ -2812,6 +2812,7 @@ device_realized (NMDevice *device,
 
 static void
 device_connectivity_changed (NMDevice *device,
+                             GParamSpec *pspec,
                              NMManager *self)
 {
 	NMManagerPrivate *priv = NM_MANAGER_GET_PRIVATE (self);
@@ -6041,6 +6042,12 @@ check_connectivity_auth_done_cb (NMAuthChain *chain,
 
 	c_list_for_each_entry (device, &priv->devices_lst_head, devices_lst) {
 		if (nm_device_check_connectivity (device,
+		                                  AF_INET,
+		                                  device_connectivity_done,
+		                                  data))
+			data->remaining++;
+		if (nm_device_check_connectivity (device,
+		                                  AF_INET6,
 		                                  device_connectivity_done,
 		                                  data))
 			data->remaining++;

commit 44ac97225245679da9192ab53d236364658cda07

connectivity: use 443 port for https URIs
If the URI does not specify a port, we always assumed "80". That is
wrong for https. Arguably, https is discouraged for connectivity checking,
but we still shouldn't break it.

Fixes: 9664f284a1d8575798daa9abf9cd7f49a19b48d9

src/nm-connectivity.c
@@ -936,12 +936,14 @@ update_config (NMConnectivity *self, NMConfigData *config_data)
 		new_uri_valid = (new_uri && *new_uri);
 		if (new_uri_valid) {
 			gs_free char *scheme = g_uri_parse_scheme (new_uri);
+			gboolean is_https = FALSE;
 
 			if (!scheme) {
 				_LOGE ("invalid URI '%s' for connectivity check.", new_uri);
 				new_uri_valid = FALSE;
 			} else if (g_ascii_strcasecmp (scheme, "https") == 0) {
 				_LOGW ("use of HTTPS for connectivity checking is not reliable and is discouraged (URI: %s)", new_uri);
+				is_https = TRUE;
 			} else if (g_ascii_strcasecmp (scheme, "http") != 0) {
 				_LOGE ("scheme of '%s' uri doesn't use a scheme that is allowed for connectivity check.", new_uri);
 				new_uri_valid = FALSE;
@@ -951,7 +953,8 @@ update_config (NMConnectivity *self, NMConfigData *config_data)
 				if (!host_and_port_from_uri (new_uri, &new_host, &new_port)) {
 					_LOGE ("cannot parse host and port from '%s'", new_uri);
 					new_uri_valid = FALSE;
-				}
+				} else if (!new_port && is_https)
+					new_port = g_strdup ("443");
 			}
 		}
 