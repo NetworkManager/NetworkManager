NetworkManager/NetworkManager

commit 6ae1f64351f326a18c9654b4b8cf5c0dfedb0951

device: reset SR-IOV VFs on deactivation
If the connection has a sriov setting we configure SR-IOV VFs on
activation. We should also clear resources when the connection
deactivates.

(cherry picked from commit 529533a50c01fec36e91134c1c44c6b00ef25fd4)

src/devices/nm-device.c
@@ -14566,6 +14566,7 @@ _set_state_full (NMDevice *self,
 	NMActRequest *req;
 	gboolean no_firmware = FALSE;
 	NMSettingsConnection *sett_conn;
+	NMSettingSriov *s_sriov;
 
 	g_return_if_fail (NM_IS_DEVICE (self));
 
@@ -14762,6 +14763,12 @@ _set_state_full (NMDevice *self,
 		}
 		break;
 	case NM_DEVICE_STATE_DEACTIVATING:
+		if (   (s_sriov = nm_device_get_applied_setting (self, NM_TYPE_SETTING_SRIOV))
+		    && priv->ifindex > 0) {
+			nm_platform_link_set_sriov_params (nm_device_get_platform (self),
+			                                   priv->ifindex, 0, 1);
+		}
+
 		_cancel_activation (self);
 
 		/* We cache the ignore_carrier state to not react on config-reloads while the connection

commit 87095393e0d3bf1e00dae2ec99f26044dc933333

device: fix compile error in _set_State_full() for missing cast
    src/devices/nm-device.c: In function '_set_state_full':
    src/devices/nm-device.c:14766:19: error: assignment from incompatible pointer type [-Werror]
       if (   (s_sriov = nm_device_get_applied_setting (self, NM_TYPE_SETTING_SRIOV))
                       ^

Fixes: 6ae1f64351f326a18c9654b4b8cf5c0dfedb0951

src/devices/nm-device.c
@@ -14763,7 +14763,7 @@ _set_state_full (NMDevice *self,
 		}
 		break;
 	case NM_DEVICE_STATE_DEACTIVATING:
-		if (   (s_sriov = nm_device_get_applied_setting (self, NM_TYPE_SETTING_SRIOV))
+		if (   (s_sriov = (NMSettingSriov *) nm_device_get_applied_setting (self, NM_TYPE_SETTING_SRIOV))
 		    && priv->ifindex > 0) {
 			nm_platform_link_set_sriov_params (nm_device_get_platform (self),
 			                                   priv->ifindex, 0, NM_TERNARY_TRUE);