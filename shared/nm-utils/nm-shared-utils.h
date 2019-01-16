NetworkManager/NetworkManager

commit 8c2d58b23746babf021e546449ea7b1c7549f6ba

shared/tests: add test for nm_strdup_int() macro

shared/nm-utils/nm-shared-utils.h
@@ -304,6 +304,8 @@ _nm_strndup_a_step (char *s, const char *str, gsize len)
 #if _NM_CC_SUPPORT_GENERIC
 #define nm_strdup_int(val) \
 	_Generic ((val), \
+	          char:    g_strdup_printf ("%d",                (int)     (val)), \
+	          \
 	          gint8:   g_strdup_printf ("%d",                (int)     (val)), \
 	          gint16:  g_strdup_printf ("%d",                (int)     (val)), \
 	          gint32:  g_strdup_printf ("%d",                (int)     (val)), \

shared/nm-utils/tests/test-shared-general.c
@@ -104,6 +104,54 @@ test_make_strv (void)
 
 /*****************************************************************************/
 
+typedef enum {
+	TEST_NM_STRDUP_ENUM_m1 = -1,
+	TEST_NM_STRDUP_ENUM_3  = 3,
+} TestNMStrdupIntEnum;
+
+static void
+test_nm_strdup_int (void)
+{
+#define _NM_STRDUP_INT_TEST(num, str) \
+	G_STMT_START { \
+		gs_free char *_s1 = NULL; \
+		\
+		_s1 = nm_strdup_int ((num)); \
+		\
+		g_assert (_s1); \
+		g_assert_cmpstr (_s1, ==, str); \
+	} G_STMT_END
+
+#define _NM_STRDUP_INT_TEST_TYPED(type, num) \
+	G_STMT_START { \
+		type _num = ((type) num); \
+		\
+		_NM_STRDUP_INT_TEST (_num, G_STRINGIFY (num)); \
+	} G_STMT_END
+
+	_NM_STRDUP_INT_TEST_TYPED (char, 0);
+	_NM_STRDUP_INT_TEST_TYPED (char, 1);
+	_NM_STRDUP_INT_TEST_TYPED (guint8, 0);
+	_NM_STRDUP_INT_TEST_TYPED (gint8, 25);
+	_NM_STRDUP_INT_TEST_TYPED (char, 47);
+	_NM_STRDUP_INT_TEST_TYPED (short, 47);
+	_NM_STRDUP_INT_TEST_TYPED (int, 47);
+	_NM_STRDUP_INT_TEST_TYPED (long, 47);
+	_NM_STRDUP_INT_TEST_TYPED (unsigned char, 47);
+	_NM_STRDUP_INT_TEST_TYPED (unsigned short, 47);
+	_NM_STRDUP_INT_TEST_TYPED (unsigned, 47);
+	_NM_STRDUP_INT_TEST_TYPED (unsigned long, 47);
+	_NM_STRDUP_INT_TEST_TYPED (gint64, 9223372036854775807);
+	_NM_STRDUP_INT_TEST_TYPED (gint64, -9223372036854775807);
+	_NM_STRDUP_INT_TEST_TYPED (guint64, 0);
+	_NM_STRDUP_INT_TEST_TYPED (guint64, 9223372036854775807);
+
+	_NM_STRDUP_INT_TEST (TEST_NM_STRDUP_ENUM_m1, "-1");
+	_NM_STRDUP_INT_TEST (TEST_NM_STRDUP_ENUM_3,  "3");
+}
+
+/*****************************************************************************/
+
 NMTST_DEFINE ();
 
 int main (int argc, char **argv)
@@ -113,6 +161,7 @@ int main (int argc, char **argv)
 	g_test_add_func ("/general/test_monotonic_timestamp", test_monotonic_timestamp);
 	g_test_add_func ("/general/test_nmhash", test_nmhash);
 	g_test_add_func ("/general/test_nm_make_strv", test_make_strv);
+	g_test_add_func ("/general/test_nm_strdup_int", test_nm_strdup_int);
 
 	return g_test_run ();
 }

commit e7e0100062aa404d82181c8b509af6ff1beb5f65

shared: fix generic selection of integers in nm_strdup_int()
This fixes a test error, which aims to convert "unsigned long int" type,
but the generic type may not have been covered.

Don't select based on the gint32-like typedefs, but on the basic C
integer types.

Fixes: 8c2d58b23746babf021e546449ea7b1c7549f6ba

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/issues/108

shared/nm-utils/nm-shared-utils.h
@@ -304,17 +304,19 @@ _nm_strndup_a_step (char *s, const char *str, gsize len)
 #if _NM_CC_SUPPORT_GENERIC
 #define nm_strdup_int(val) \
 	_Generic ((val), \
-	          char:    g_strdup_printf ("%d",                (int)     (val)), \
+	          char:               g_strdup_printf ("%d",   (int)                (val)), \
 	          \
-	          gint8:   g_strdup_printf ("%d",                (int)     (val)), \
-	          gint16:  g_strdup_printf ("%d",                (int)     (val)), \
-	          gint32:  g_strdup_printf ("%d",                (int)     (val)), \
-	          gint64:  g_strdup_printf ("%"G_GINT64_FORMAT,  (gint64)  (val)), \
+	          signed char:        g_strdup_printf ("%d",   (signed)             (val)), \
+	          signed short:       g_strdup_printf ("%d",   (signed)             (val)), \
+	          signed:             g_strdup_printf ("%d",   (signed)             (val)), \
+	          signed long:        g_strdup_printf ("%ld",  (signed long)        (val)), \
+	          signed long long:   g_strdup_printf ("%lld", (signed long long)   (val)), \
 	          \
-	          guint8:  g_strdup_printf ("%u",                (guint)   (val)), \
-	          guint16: g_strdup_printf ("%u",                (guint)   (val)), \
-	          guint32: g_strdup_printf ("%u",                (guint)   (val)), \
-	          guint64: g_strdup_printf ("%"G_GUINT64_FORMAT, (guint64) (val))  \
+	          unsigned char:      g_strdup_printf ("%u",   (unsigned)           (val)), \
+	          unsigned short:     g_strdup_printf ("%u",   (unsigned)           (val)), \
+	          unsigned:           g_strdup_printf ("%u",   (unsigned)           (val)), \
+	          unsigned long:      g_strdup_printf ("%lu",  (unsigned long)      (val)), \
+	          unsigned long long: g_strdup_printf ("%llu", (unsigned long long) (val))  \
 	)
 #else
 #define nm_strdup_int(val) \