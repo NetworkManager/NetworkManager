NetworkManager/NetworkManager

commit d81e10942fda8b6cb163fc1d631cd44eb35e3b91

all: fix misspellings
https://gitlab.freedesktop.org/NetworkManager/NetworkManager/merge_requests/64

libnm-core/nm-setting-connection.c
@@ -1633,7 +1633,7 @@ nm_setting_connection_class_init (NMSettingConnectionClass *klass)
 	 * at runtime. Currently supported are "${CONNECTION}", "${DEVICE}", "${MAC}",
 	 * "${BOOT}", "${RANDOM}".
 	 * These effectively create unique IDs per-connection, per-device, per-boot,
-	 * or every time. Note that "${DEVICE}" corresponds the the interface name of the
+	 * or every time. Note that "${DEVICE}" corresponds the interface name of the
 	 * device and "${MAC}" is the permanent MAC address of the device.
 	 * Any unrecognized patterns following '$' are treated verbatim, however
 	 * are reserved for future use. You are thus advised to avoid '$' or

clients/cli/nmcli.c
@@ -256,7 +256,7 @@ usage (void)
 	              "  -p, --pretty                             pretty output\n"
 	              "  -s, --show-secrets                       allow displaying passwords\n"
 	              "  -t, --terse                              terse output\n"
-	              "  -v, --version                            how program version\n"
+	              "  -v, --version                            show program version\n"
 	              "  -w, --wait <seconds>                     set timeout waiting for finishing operations\n"
 	              "\n"
 	              "OBJECT\n"

contrib/test/modemu.pl
@@ -252,7 +252,7 @@ Same as above, with a modem name different from default.
 =head1 BUGS
 
 Only works on machines with a PCI bus. ModemManager is picky about platform
-devices and accepts PCI and USB busses easily. Which is why pretent to have
+devices and accepts PCI and USB buses easily. Which is why pretent to have
 our tty on the PCI root device.
 
 Terminates after a single PPP session. C<pppd> seems to hang up the PTY.

libnm-core/nm-setting-wireless-security.h
@@ -100,7 +100,7 @@ typedef enum {
  * Configure the use of WPS by a connection while it activates.
  *
  * Note: prior to 1.16, this was a GEnum type instead of a GFlags type
- * altough, with the same numeric values.
+ * although, with the same numeric values.
  *
  * Since: 1.10
  **/

libnm-core/nm-setting.c
@@ -1303,7 +1303,7 @@ _nm_setting_should_compare_secret_property (NMSetting *setting,
 	 *   - @other also has the secret flat to be ignored.
 	 *
 	 * This makes the check symmetric (aside the fact that @setting must
-	 * have the secret while @other may not -- which is asymetric). */
+	 * have the secret while @other may not -- which is asymmetric). */
 	if (   NM_FLAGS_HAS (flags, NM_SETTING_COMPARE_FLAG_IGNORE_AGENT_OWNED_SECRETS)
 	    && NM_FLAGS_HAS (a_secret_flags, NM_SETTING_SECRET_FLAG_AGENT_OWNED)
 	    && (   !other
@@ -1602,7 +1602,7 @@ nm_setting_diff (NMSetting *a,
 				 *
 				 * Note that compare_property() called with two settings will ignore secrets
 				 * based on the flags, but it will do so if *both* settings have the flag we
-				 * look for. So that is symetric behavior and good.
+				 * look for. So that is symmetric behavior and good.
 				 *
 				 * But for the purpose of diff(), we do a asymmetric comparison because and
 				 * we want to skip testing the property if setting @a alone indicates to do
@@ -2490,7 +2490,7 @@ _nm_setting_gendata_reset_from_hash (NMSetting *setting,
 	}
 
 	/* let's not bother to find out whether the new hash has any different
-	 * content the the current gendata. Just replace it. */
+	 * content the current gendata. Just replace it. */
 	g_hash_table_remove_all (gendata->hash);
 	if (num > 0) {
 		g_hash_table_iter_init (&iter, new);

libnm/nm-checkpoint.c
@@ -91,7 +91,7 @@ nm_checkpoint_get_created (NMCheckpoint *checkpoint)
  * nm_checkpoint_get_rollback_timeout:
  * @checkpoint: a #NMCheckpoint
  *
- * Gets the the timeout in seconds for automatic rollback.
+ * Gets the timeout in seconds for automatic rollback.
  *
  * Returns: the rollback timeout.
  *

man/nmcli.xml
@@ -1445,7 +1445,7 @@
 
         <listitem>
           <para>Connect to a Wi-Fi network specified by SSID or BSSID. The command
-          finds a matching connnection or creates one and then activates it on a device.
+          finds a matching connection or creates one and then activates it on a device.
           This is a command-line counterpart of clicking an SSID in a GUI client. If
           a connection for the network already exists, it is possible to bring up
           (activate) the existing profile as follows:

shared/nm-utils/nm-hash-utils.c
@@ -71,7 +71,7 @@ again:
 		 * the first guint has only the entropy that nm_utils_random_bytes()
 		 * generated for the first 4 bytes and relies on a good random generator.
 		 *
-		 * The first int is especially intersting for nm_hash_static() below, and we
+		 * The first int is especially interesting for nm_hash_static() below, and we
 		 * want to have it all the entropy of t_arr. */
 		c_siphash_init (&siph_state, t_arr.v8);
 		c_siphash_append (&siph_state, (const guint8 *) &t_arr, sizeof (t_arr));

commit 4ecd4f8cf93737e17ea877bcff8594515afcc045

libnm-core: fix gtk-doc comment
Fixes: d81e10942fda8b6cb163fc1d631cd44eb35e3b91

libnm-core/nm-setting-connection.c
@@ -1633,7 +1633,7 @@ nm_setting_connection_class_init (NMSettingConnectionClass *klass)
 	 * at runtime. Currently supported are "${CONNECTION}", "${DEVICE}", "${MAC}",
 	 * "${BOOT}", "${RANDOM}".
 	 * These effectively create unique IDs per-connection, per-device, per-boot,
-	 * or every time. Note that "${DEVICE}" corresponds the interface name of the
+	 * or every time. Note that "${DEVICE}" corresponds to the interface name of the
 	 * device and "${MAC}" is the permanent MAC address of the device.
 	 * Any unrecognized patterns following '$' are treated verbatim, however
 	 * are reserved for future use. You are thus advised to avoid '$' or