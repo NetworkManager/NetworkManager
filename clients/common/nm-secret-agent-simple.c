NetworkManager/NetworkManager

commit 1a0fc8d437b04641b41c86cd1e9a35c48b5b2c67

clients/secret-agent: fix leaks in request_secrets_from_ui()
Fixes: 3bda3fb60c104114192e4e3c9c4ba0bef84d3a00

clients/common/nm-secret-agent-simple.c
@@ -728,11 +728,11 @@ try_spawn_vpn_auth_helper (RequestData *request,
 static void
 request_secrets_from_ui (RequestData *request)
 {
-	GPtrArray *secrets;
+	gs_unref_ptrarray GPtrArray *secrets = NULL;
 	NMSecretAgentSimplePrivate *priv;
 	NMSecretAgentSimpleSecret *secret;
 	const char *title;
-	char *msg;
+	gs_free char *msg = NULL;
 	gboolean ok = TRUE;
 
 	priv = NM_SECRET_AGENT_SIMPLE_GET_PRIVATE (request->self);
@@ -881,7 +881,6 @@ request_secrets_from_ui (RequestData *request)
 		                     request->request_id,
 		                     nm_connection_get_connection_type (request->connection));
 		_request_data_complete (request, NULL, error, NULL);
-		g_ptr_array_unref (secrets);
 		return;
 	}
 

commit b57a3a4cc62e3e74c5c0660834449f967236ee17

clients: fix double free
@secrets is unreferenced at the end of request_secrets_from_ui() and
so try_spawn_vpn_auth_helper() must take a reference to it.

Fixes: 1a0fc8d437b04641b41c86cd1e9a35c48b5b2c67

clients/common/nm-secret-agent-simple.c
@@ -799,7 +799,7 @@ try_spawn_vpn_auth_helper (RequestData *request,
 		.auth_dialog_response = g_string_new_len (NULL, sizeof (data->read_buf)),
 		.auth_dialog_pid = auth_dialog_pid,
 		.request = request,
-		.secrets = secrets,
+		.secrets = g_ptr_array_ref (secrets),
 	};
 
 	g_output_stream_write_async (auth_dialog_in,