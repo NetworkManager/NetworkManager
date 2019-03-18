6632c7709492182c795895c7d32ce2542a8d9e9a
Lubomir Rintel <lkundrak@v3.sk>
2019-03-18 10:53:20 +0100

    wwan: add service-providers.xml parser
    This allows up to look up a default APN if the user doesn't pick one.

---
+++ src/devices/wwan/nm-service-providers.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: LGPL-2.1+
+/*
+ * Copyright (C) 2019 Red Hat, Inc.
+ */
+
+#ifndef __NETWORKMANAGER_SERVICE_PROVIDERS_H__
+#define __NETWORKMANAGER_SERVICE_PROVIDERS_H__
+
+typedef void (*NMServiceProvidersGsmApnCallback) (const char *apn,
+                                                  const char *username,
+                                                  const char *password,
+                                                  const char *gateway,
+                                                  const char *auth_method,
+                                                  const GSList *dns,
+                                                  GError *error,
+                                                  gpointer user_data);
+
+void nm_service_providers_find_gsm_apn (const char *service_providers,
+                                        const char *mccmnc,
+                                        GCancellable *cancellable,
+                                        NMServiceProvidersGsmApnCallback callback,
+                                        gpointer user_data);
+
+#endif /* __NETWORKMANAGER_SERVICE_PROVIDERS_H__ */
