7a25f67af79c1732e3b450e4e274630327832e09
Thomas Haller <thaller@redhat.com>
2019-04-21 10:11:16 +0200

    shared/tests: add tests for libnm-core-aux
    These tests cannot (easily) be under "shared/nm-libnm-core-aux/tests"
    because libnm/libnm-core requires code under shared while
    "nm-libnm-core-aux" requires libnm/libnm-core. With autotools that is
    not problem, but with meson we include sub directories in a particular
    order and there is no way to foward declare stuff (AFAIK). To avoid
    the circular dependency, add the tests to "clients/common/tests", which
    is always built last.

---
+++ clients/common/tests/test-libnm-core-aux.c
@@ -0,0 +1,35 @@
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ * Copyright 2019 Red Hat, Inc.
+ */
+
+#include "nm-default.h"
+
+#include "nm-libnm-core-aux/nm-libnm-core-aux.h"
+
+#include "nm-utils/nm-test-utils.h"
+
+/*****************************************************************************/
+
+NMTST_DEFINE ();
+
+int main (int argc, char **argv)
+{
+	nmtst_init (&argc, &argv, TRUE);
+
+	return g_test_run ();
+}
