efccc2a53c3288034d1bd3809c9ee52fa98604c9
Thomas Haller <thaller@redhat.com>
2019-05-09 14:25:10 +0200

    shared: add nm_json_aux_gstr_append_*() helper

--- shared/nm-glib-aux/nm-json-aux.h
+++ shared/nm-glib-aux/nm-json-aux.h
@@ -20,4 +20,40 @@
 #ifndef __NM_JSON_AUX_H__
 #define __NM_JSON_AUX_H__
 
+/*****************************************************************************/
+
+static inline GString *
+nm_json_aux_gstr_append_delimiter (GString *gstr)
+{
+	g_string_append (gstr, ", ");
+	return gstr;
+}
+
+void nm_json_aux_gstr_append_string_len (GString *gstr,
+                                         const char *str,
+                                         gsize n);
+
+void nm_json_aux_gstr_append_string (GString *gstr,
+                                     const char *str);
+
+static inline void
+nm_json_aux_gstr_append_bool (GString *gstr,
+                              gboolean v)
+{
+	g_string_append (gstr, v ? "true" : "false");
+}
+
+static inline void
+nm_json_aux_gstr_append_int64 (GString *gstr,
+                               gint64 v)
+{
+	g_string_append_printf (gstr, "%"G_GINT64_FORMAT, v);
+}
+
+void nm_json_aux_gstr_append_obj_name (GString *gstr,
+                                       const char *key,
+                                       char start_container);
+
+/*****************************************************************************/
+
 #endif  /* __NM_JSON_AUX_H__ */
