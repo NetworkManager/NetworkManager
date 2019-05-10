848a80598e2d8496d258241990f050dba6a66e62
Thomas Haller <thaller@redhat.com>
2019-05-10 10:33:29 +0200

    shared: add JSON helper functions for NMValueType

--- shared/nm-glib-aux/nm-json-aux.h
+++ shared/nm-glib-aux/nm-json-aux.h
@@ -56,4 +56,28 @@ void nm_json_aux_gstr_append_obj_name (GString *gstr,
 
 /*****************************************************************************/
 
+#ifdef NM_VALUE_TYPE_DEFINE_FUNCTIONS
+#include "nm-value-type.h"
+static inline void
+nm_value_type_to_json (NMValueType value_type,
+                       GString *gstr,
+                       gconstpointer p_field)
+{
+	nm_assert (p_field);
+	nm_assert (gstr);
+
+	switch (value_type) {
+	case NM_VALUE_TYPE_BOOL:   nm_json_aux_gstr_append_bool   (gstr, *((const bool        *) p_field)); return;
+	case NM_VALUE_TYPE_INT32:  nm_json_aux_gstr_append_int64  (gstr, *((const gint32      *) p_field)); return;
+	case NM_VALUE_TYPE_INT:    nm_json_aux_gstr_append_int64  (gstr, *((const int         *) p_field)); return;
+	case NM_VALUE_TYPE_STRING: nm_json_aux_gstr_append_string (gstr, *((const char *const *) p_field)); return;
+	case NM_VALUE_TYPE_UNSPEC:
+		break;
+	}
+	nm_assert_not_reached ();
+}
+#endif
+
+/*****************************************************************************/
+
 #endif  /* __NM_JSON_AUX_H__ */
