NetworkManager/NetworkManager

commit ed552c732c76c6f0f222a71400d601a0c967be83

logging: log device and connection along with the message

src/nm-logging.c
@@ -594,6 +594,8 @@ _nm_log_impl (const char *file,
               NMLogLevel level,
               NMLogDomain domain,
               int error,
+              const char *ifname,
+              const char *conn_uuid,
               const char *fmt,
               ...)
 {
@@ -642,7 +644,7 @@ _nm_log_impl (const char *file,
 #define _NUM_MAX_FIELDS_SYSLOG_FACILITY 10
 			struct iovec iov_data[12 + _NUM_MAX_FIELDS_SYSLOG_FACILITY];
 			struct iovec *iov = iov_data;
-			gpointer iov_free_data[3];
+			gpointer iov_free_data[5];
 			gpointer *iov_free = iov_free_data;
 			nm_auto_free_gstring GString *s_domain_all = NULL;
 
@@ -704,6 +706,10 @@ _nm_log_impl (const char *file,
 			_iovec_set_format_a (iov++, 60, "TIMESTAMP_BOOTTIME=%lld.%06lld", (long long) (boottime / NM_UTILS_NS_PER_SECOND), (long long) ((boottime % NM_UTILS_NS_PER_SECOND) / 1000));
 			if (error != 0)
 				_iovec_set_format_a (iov++, 30, "ERRNO=%d", error);
+			if (ifname)
+				_iovec_set_format (iov++, iov_free++, "NM_DEVICE=%s", ifname);
+			if (conn_uuid)
+				_iovec_set_format (iov++, iov_free++, "NM_CONNECTION=%s", conn_uuid);
 
 			nm_assert (iov <= &iov_data[G_N_ELEMENTS (iov_data)]);
 			nm_assert (iov_free <= &iov_free_data[G_N_ELEMENTS (iov_free_data)]);

src/devices/nm-arping-manager.c
@@ -82,7 +82,7 @@ G_DEFINE_TYPE (NMArpingManager, nm_arping_manager, G_TYPE_OBJECT)
     G_STMT_START { \
         char _sbuf[64]; \
         \
-        nm_log ((level), _NMLOG_DOMAIN, \
+        nm_log ((level), _NMLOG_DOMAIN, NULL, NULL, \
                 "%s%s: " _NM_UTILS_MACRO_FIRST (__VA_ARGS__), \
                 _NMLOG_PREFIX_NAME, \
                 self ? nm_sprintf_buf (_sbuf, "[%p,%d]", \

src/devices/nm-device-logging.h
@@ -34,7 +34,7 @@ _nm_device_log_self_to_device (t *self) \
 #undef  _NMLOG_ENABLED
 #define _NMLOG_ENABLED(level, domain) ( nm_logging_enabled ((level), (domain)) )
 #define _NMLOG(level, domain, ...) \
-    nm_log_obj ((level), (domain), (self), "device", \
+    nm_log_obj ((level), (domain), NULL, NULL, (self), "device", \
                 "(%s): " _NM_UTILS_MACRO_FIRST(__VA_ARGS__), \
                 (self) ? (nm_device_get_iface (_nm_device_log_self_to_device (self)) ?: "(null)") : "(none)" \
                 _NM_UTILS_MACRO_REST(__VA_ARGS__))

src/devices/nm-lldp-listener.c
@@ -127,7 +127,7 @@ typedef struct {
             char _sbuf[64]; \
             int _ifindex = (self) ? NM_LLDP_LISTENER_GET_PRIVATE (self)->ifindex : 0; \
             \
-            _nm_log (_level, _NMLOG_DOMAIN, 0, \
+            _nm_log (_level, _NMLOG_DOMAIN, 0, NULL, NULL, \
                      "%s%s: " _NM_UTILS_MACRO_FIRST (__VA_ARGS__), \
                      _NMLOG_PREFIX_NAME, \
                      ((_ifindex > 0) \

src/devices/wwan/nm-modem-broadband.c
@@ -119,7 +119,7 @@ G_DEFINE_TYPE (NMModemBroadband, nm_modem_broadband, NM_TYPE_MODEM)
             char __prefix_name[128]; \
             const char *__uid; \
             \
-            _nm_log (_level, (_NMLOG_DOMAIN), 0, \
+            _nm_log (_level, (_NMLOG_DOMAIN), 0, NULL, NULL, \
                      "%s%s: " _NM_UTILS_MACRO_FIRST(__VA_ARGS__), \
                      _NMLOG_PREFIX_NAME, \
                      (__self \

src/devices/wwan/nm-modem-ofono.c
@@ -83,7 +83,7 @@ G_DEFINE_TYPE (NMModemOfono, nm_modem_ofono, NM_TYPE_MODEM)
             char __prefix_name[128]; \
             const char *__uid; \
             \
-            _nm_log (_level, (_NMLOG_DOMAIN), 0, \
+            _nm_log (_level, (_NMLOG_DOMAIN), 0, NULL, NULL, \
                      "%s%s: " _NM_UTILS_MACRO_FIRST(__VA_ARGS__), \
                      _NMLOG_PREFIX_NAME, \
                      (__self \

src/dhcp/nm-dhcp-client-logging.h
@@ -42,7 +42,7 @@
                                             ? LOGD_DHCP \
                                             : (nm_dhcp_client_get_ipv6 (_self) ? LOGD_DHCP6 : LOGD_DHCP4); \
             \
-            nm_log (_level, _domain, \
+            nm_log (_level, _domain, NULL, NULL, \
                     "%s%s%s%s%s: " _NM_UTILS_MACRO_FIRST (__VA_ARGS__), \
                     _NMLOG_PREFIX_NAME, \
                     (_domain == LOGD_DHCP4 ? "4" : (_domain == LOGD_DHCP6 ? "6" : "")), \
@@ -65,7 +65,7 @@
         if (nm_logging_enabled (_level, _domain)) { \
             const char *__ifname = (ifname); \
             \
-            nm_log (_level, _domain, \
+            nm_log (_level, _domain, NULL, NULL, \
                     "%s%s%s%s%s: " _NM_UTILS_MACRO_FIRST (__VA_ARGS__), \
                     _NMLOG_PREFIX_NAME, \
                     (_domain == LOGD_DHCP4 ? "4" : (_domain == LOGD_DHCP6 ? "6" : "")), \

src/dhcp/nm-dhcp-listener.c
@@ -92,7 +92,7 @@ NM_DEFINE_SINGLETON_GETTER (NMDhcpListener, nm_dhcp_listener_get, NM_TYPE_DHCP_L
         const NMDhcpListener *_self = (self); \
         char _prefix[64]; \
         \
-        nm_log ((level), (_NMLOG_DOMAIN), \
+        nm_log ((level), (_NMLOG_DOMAIN), NULL, NULL, \
                 "%s: " _NM_UTILS_MACRO_FIRST(__VA_ARGS__), \
                 (_self != singleton_instance \
                     ? nm_sprintf_buf (_prefix, "%s[%p]", _NMLOG_PREFIX_NAME, _self) \

commit 138c187376cff1c14c66a3fdc502d260178081bb

logging: fix stack overflow in logging for iov_data array
This overflow could only happen when we would try to log a message
with "NM_DEVICE=", "NM_CONNECTION=", and more than 8 logging domains
(_NUM_MAX_FIELDS_SYSLOG_FACILITY - 2).

The latter is never the case. While we sometimes log messages with
more than one logging domain, there are no logging statements that
use most as 8 different logging domains. So, this overflow is not
actually reachable from current code (I think).

Fixes: ed552c732c76 ('logging: log device and connection along with the message'):

src/nm-logging.c
@@ -747,7 +747,7 @@ _nm_log_impl (const char *file,
 		{
 			gint64 now, boottime;
 #define _NUM_MAX_FIELDS_SYSLOG_FACILITY 10
-			struct iovec iov_data[12 + _NUM_MAX_FIELDS_SYSLOG_FACILITY];
+			struct iovec iov_data[14 + _NUM_MAX_FIELDS_SYSLOG_FACILITY];
 			struct iovec *iov = iov_data;
 			gpointer iov_free_data[5];
 			gpointer *iov_free = iov_free_data;