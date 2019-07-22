NetworkManager/NetworkManager

commit 8bbda5cdff0f7f04d3b5863edfdba335dab4c7f8

Currently if we have two paired bluetooth devices, nm_device_disambiguate_names() will disambiguate them by their hardware address. This is not very helpful, so detect this case and use the Bluetooth device name instead.
This function is used by System Settings when showing the list of
network devices.

https://bugzilla.gnome.org/show_bug.cgi?id=740553

(Port of a libnm-gtk patch written by Ryan Lortie.)

libnm/nm-device.c
@@ -1813,6 +1813,25 @@ nm_device_disambiguate_names (NMDevice **devices,
 	if (!find_duplicates (names, duplicates, num_devices))
 		goto done;
 
+	/* If dealing with Bluetooth devices, try to distinguish them by
+	 * device name.
+	 */
+	for (i = 0; i < num_devices; i++) {
+		if (duplicates[i] && NM_IS_DEVICE_BT (devices[i])) {
+			const char *devname = nm_device_bt_get_name (NM_DEVICE_BT (devices[i]));
+
+			if (!devname)
+				continue;
+
+			g_free (names[i]);
+			names[i] = g_strdup_printf ("%s (%s)",
+			                            get_device_type_name_with_iface (devices[i]),
+			                            devname);
+		}
+	}
+	if (!find_duplicates (names, duplicates, num_devices))
+		goto done;
+
 	/* We have multiple identical network cards, so we have to differentiate
 	 * them by interface name.
 	 */

commit b621aba5c213734d6bf672340efea8576d026ccc

device: free temporary typename with iface variable
To create a disambiguated name for some Bluetooth devices we use its type name
with iface, however this value is allocated but never free'd when passed to
g_strdup_printf.

So use instead a temporary variable and free it once done.

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/merge_requests/208

Fixes: 8bbda5cdff0f7f04d3b5863edfdba335dab4c7f8

libnm/nm-device.c
@@ -1840,14 +1840,15 @@ nm_device_disambiguate_names (NMDevice **devices,
 	for (i = 0; i < num_devices; i++) {
 		if (duplicates[i] && NM_IS_DEVICE_BT (devices[i])) {
 			const char *devname = nm_device_bt_get_name (NM_DEVICE_BT (devices[i]));
+			char *name;
 
 			if (!devname)
 				continue;
 
 			g_free (names[i]);
-			names[i] = g_strdup_printf ("%s (%s)",
-			                            get_device_type_name_with_iface (devices[i]),
-			                            devname);
+			name = get_device_type_name_with_iface (devices[i]);
+			names[i] = g_strdup_printf ("%s (%s)", name, devname);
+			g_free (name);
 		}
 	}
 	if (!find_duplicates (names, duplicates, num_devices))