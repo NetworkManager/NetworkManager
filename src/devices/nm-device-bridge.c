57e3734b6cc1bb453216c7e2150a698114507a46
Beniamino Galvani <bgalvani@redhat.com>
2019-07-31 11:40:35 +0200

    device: fix releasing slaves
    Not all masters type have a platform link and so it's wrong to check
    for it to decide whether the slave should be really released. Move the
    check to master devices that need it (bond, bridge and team).
    
    OVS ports don't need the check because they don't call to platform to
    remove a slave.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1733709

--- src/devices/nm-device-bridge.c
+++ src/devices/nm-device-bridge.c
@@ -626,6 +626,12 @@ release_slave (NMDevice *device,
 	NMDeviceBridge *self = NM_DEVICE_BRIDGE (device);
 	gboolean success;
 	int ifindex_slave;
+	int ifindex;
+
+	ifindex = nm_device_get_ifindex (device);
+	if (   ifindex <= 0
+	    || !nm_platform_link_get (nm_device_get_platform (device), ifindex))
+		configure = FALSE;
 
 	ifindex_slave = nm_device_get_ip_ifindex (slave);
 
