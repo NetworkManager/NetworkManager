24028a22467275671df71cc6a8054036b37d8f03
Lubomir Rintel <lkundrak@v3.sk>
2019-09-10 11:19:01 +0200

    all: SPDX header conversion
      $ find * -type f |xargs perl contrib/scripts/spdx.pl
      $ git rm contrib/scripts/spdx.pl

--- libnm/nm-device-ppp.h
+++ libnm/nm-device-ppp.h
@@ -1,12 +1,5 @@
+// SPDX-License-Identifier: LGPL-2.1+
 /*
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
  * Copyright 2017 Red Hat, Inc.
  */
 
