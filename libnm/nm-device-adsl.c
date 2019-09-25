9683521521b0ad65fa6da171627b6f7afb415fc6
Thomas Haller <thaller@redhat.com>
2019-09-25 13:17:11 +0200

    libnm: spell "Author:" comment with upper case
    Like it's done at all other places.

--- libnm/nm-device-adsl.c
+++ libnm/nm-device-adsl.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: LGPL-2.1+
 /*
- * author: Pantelis Koukousoulas <pktoss@gmail.com>
+ * Author: Pantelis Koukousoulas <pktoss@gmail.com>
  * Copyright 2009 - 2011 Red Hat, Inc.
  */
 
