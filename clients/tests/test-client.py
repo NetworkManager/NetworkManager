5e4e328aaa8ef785b8b9e1cdfffb9e8556c44eb5
Thomas Haller <thaller@redhat.com>
2019-09-26 13:45:15 +0200

    clients/tests: don't include the source line number with client tests output
    The client tests compare the test output with a .expected file that is
    commit to git and that contains the expected output.
    
    The expected output contains data like
    
        size: 395
        location: clients/tests/test-client.py:842:test_001()/1
        cmd: $NMCLI
        lang: C
        returncode: 0
        stdout: 277 bytes
        >>>
        ...
    
    Note that there is the line number (clients/tests/test-client.py:842) of
    the source code where nmcli is called. This is to help correlate the output
    with the test code.
    
    However, Python 3.8 changes behavior and for function calls that span multiple
    lines, frame.f_lineno will give now the starting line (previously, it gave the last
    line) (see [1]).
    
    No longer include the line number, as it is not stable accross Python versions.
    
    If you really care, you can set NM_TEST_WITH_LINENO to get the line numbers back.
    Of course, then the expected output won't match anymore, and you'd have to regenerate
    it first. This is only useful if you debug tests, and want to have it easier to
    correlate output with the tests while developing them.
    
    [1] https://bugs.python.org/issue38283
    
    https://gitlab.freedesktop.org/NetworkManager/NetworkManager/merge_requests/292
    (cherry picked from commit ad3ef326aa7a645d6a81808115c894ef1b882cae)
    (cherry picked from commit 275a7c46a1504dbc51cf0e18e92f2b34330bfe00)

--- clients/tests/test-client.py
+++ clients/tests/test-client.py
@@ -61,6 +61,13 @@ ENV_NM_TEST_CLIENT_CHECK_L10N = 'NM_TEST_CLIENT_CHECK_L10N'
 # on disk with the expected output.
 ENV_NM_TEST_REGENERATE        = 'NM_TEST_REGENERATE'
 
+# whether the file location should include the line number. That is useful
+# only for debugging, to correlate the expected output with the test.
+# Obviously, since the expected output is commited to git without line numbers,
+# you'd have to first NM_TEST_REGENERATE the test expected data, with line
+# numbers enabled.
+ENV_NM_TEST_WITH_LINENO       = 'NM_TEST_WITH_LINENO'
+
 #
 ###############################################################################
 
@@ -287,6 +294,8 @@ class Configuration:
             # which we assert. That is useful, if there are intentional changes and
             # we want to regenerate the expected output.
             v = (os.environ.get(ENV_NM_TEST_REGENERATE, '0') == '1')
+        elif name == ENV_NM_TEST_WITH_LINENO:
+            v = (os.environ.get(ENV_NM_TEST_WITH_LINENO, '0') == '1')
         else:
             raise Exception()
         self._values[name] = v
@@ -561,7 +570,10 @@ class TestNmcli(NmTestBase):
         # the file, so that the user can easier find the source (when looking at the .expected files)
         self.assertTrue(os.path.abspath(frame.f_code.co_filename).endswith('/'+PathConfiguration.canonical_script_filename()))
 
-        calling_location = '%s:%d:%s()/%d' % (PathConfiguration.canonical_script_filename(), frame.f_lineno, frame.f_code.co_name, calling_num)
+        if conf.get(ENV_NM_TEST_WITH_LINENO):
+            calling_location = '%s:%d:%s()/%d' % (PathConfiguration.canonical_script_filename(), frame.f_lineno, frame.f_code.co_name, calling_num)
+        else:
+            calling_location = '%s:%s()/%d' % (PathConfiguration.canonical_script_filename(), frame.f_code.co_name, calling_num)
 
         if lang is None or lang == 'C':
             lang = 'C'
@@ -1105,13 +1117,6 @@ class TestNmcli(NmTestBase):
 
 def main():
     global dbus_session_inited
-
-    if sys.version_info.major == 3 and sys.version_info.minor == 8 and sys.version_info.releaselevel == 'beta':
-        # 3.8-beta changed behavior for the line numbers, which
-        # breaks the tests (https://bugs.python.org/issue38283)
-        # skip the test for now.
-        print("WARNING: skip client test with python 3.8-beta")
-        sys.exit(0)
 
     if len(sys.argv) >= 2 and sys.argv[1] == '--started-with-dbus-session':
         dbus_session_inited = True
