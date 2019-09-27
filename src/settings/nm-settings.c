NetworkManager/NetworkManager

commit 3e39d2a5861a162cd6afb92ff60bee71fb0b2517

settings: shortcut check for whether to create auto-default wired connection
This check is only useful for devices that implement new_default_connection.
We can shortcut the possibly expensive checks like have_connection_for_device(),
which need to iterate all profiles.

src/settings/nm-settings.c
@@ -3435,7 +3435,8 @@ device_realized (NMDevice *device, GParamSpec *pspec, NMSettings *self)
 	/* If the device isn't managed or it already has a default wired connection,
 	 * ignore it.
 	 */
-	if (   !nm_device_get_managed (device, FALSE)
+	if (   !NM_DEVICE_GET_CLASS (self)->new_default_connection
+	    || !nm_device_get_managed (device, FALSE)
 	    || g_object_get_qdata (G_OBJECT (device), _default_wired_connection_quark ())
 	    || have_connection_for_device (self, device)
 	    || nm_config_get_no_auto_default_for_device (priv->config, device))

commit 7df769600bad1447d20aee942f1867641bd6bb32

settings: fix creation of default-wired-connection in device_realized()
Fixes-test: @allow_wired_connections

Fixes: 3e39d2a5861a ('settings: shortcut check for whether to create auto-default wired connection')

src/settings/nm-settings.c
@@ -3429,7 +3429,7 @@ device_realized (NMDevice *device, GParamSpec *pspec, NMSettings *self)
 	/* If the device isn't managed or it already has a default wired connection,
 	 * ignore it.
 	 */
-	if (   !NM_DEVICE_GET_CLASS (self)->new_default_connection
+	if (   !NM_DEVICE_GET_CLASS (device)->new_default_connection
 	    || !nm_device_get_managed (device, FALSE)
 	    || g_object_get_qdata (G_OBJECT (device), _default_wired_connection_quark ()))
 		return;