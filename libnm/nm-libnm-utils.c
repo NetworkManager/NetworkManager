a5b2ba02ea138737ab9f04c4e5a3e62eed9bc380
Thomas Haller <thaller@redhat.com>
2019-10-14 08:06:33 +0200

    libnm: add logging NML_DBUS_LOG*() for debugging D-Bus for NMClient
    Commonly, a library (like libnm) is not supposed to log anything.
    Logging is not a suitable way to notify the calling application
    about anything. When something of importance happens, then the
    application must be notified via the library's API.
    
    However, logging can be very useful for debugging to see what is going
    on. Add a logging macro that by default does nothing, but can be turned
    on via an environment variable "LIBNM_CLIENT_DEBUG=debug".
    
    Another point is that libnm relies on the server side NetworkManager
    D-Bus interface to be in an expected manner. For example, we require a
    D-Bus object "org.freedesktop.NetworkManager" to be present and certain
    D-Bus interfaces implemented.
    
    However libnm should treat NetworkManager as external and untrusted component.
    That means, we cannot assert against the expectations we have. There are two
    reasons for this:
    
      - a bug in NetworkManager, dbus-daemon or else may cause such errors.
        This must not trigger an assertion failure in the client
        application, at least not unless requested.
    
      - libnm must be forward and backward compatible against a different
        NetworkManager server version. That is only possibly by ignoring
        anything that is unexpected. Asserting by default might prevent
        to implement API changes, both on libnm and server side.
    
    Note that we also don't notify the calling application via dedicated
    API. On the one hand, these things *can* happen. On the other hand, what
    would the calling appication do about it anyway? libnm by default must
    just behave gracefully and pretend all is good.
    
    For testing, development and debugging that is however not useful. We
    want the user to opt in to strict API validation. The user will be able
    to do that by setting "LIBNM_CLIENT_DEBUG=warning", which causes API
    violations being logged with g_warning(). These are assertions when
    running with G_DEBUG=fatal-warnings.
    
    This is inspired by GDBus' G_DBUS_DEBUG variable.
    
    Note that LIBNM_CLIENT_DEBUG environment variables is undocumented, unstable
    API. It's used for debugging and testing of the current libnm version at hand.
    There is no guaranteed stable behavior how a different libnm version
    might behave.

--- libnm/nm-libnm-utils.c
+++ libnm/nm-libnm-utils.c
@@ -10,6 +10,85 @@
 
 /*****************************************************************************/
 
+volatile int _nml_dbus_log_level = 0;
+
+int
+_nml_dbus_log_level_init (void)
+{
+	const GDebugKey keys[] = {
+		{ "trace",   _NML_DBUS_LOG_LEVEL_TRACE },
+		{ "debug",   _NML_DBUS_LOG_LEVEL_DEBUG },
+		{ "warning", _NML_DBUS_LOG_LEVEL_WARN },
+		{ "error",   _NML_DBUS_LOG_LEVEL_ERROR },
+	};
+	int l;
+
+	l =   _NML_DBUS_LOG_LEVEL_INITIALIZED
+	    | nm_utils_parse_debug_string (g_getenv ("LIBNM_CLIENT_DEBUG"),
+	                                   keys,
+	                                   G_N_ELEMENTS (keys));
+
+	if (!g_atomic_int_compare_and_exchange (&_nml_dbus_log_level, 0, l))
+		l = g_atomic_int_get (&_nml_dbus_log_level);
+
+	nm_assert (l & _NML_DBUS_LOG_LEVEL_INITIALIZED);
+	return l;
+}
+
+void
+_nml_dbus_log (NMLDBusLogLevel level,
+               const char *fmt,
+               ...) {
+	NMLDBusLogLevel configured_log_level;
+	gs_free char *msg = NULL;
+	va_list args;
+	const char *prefix = "";
+
+	/* we only call _nml_dbus_log() after nml_dbus_log_enabled(), which already does
+	 * an atomic access to the variable. Since the value is only initialized once and
+	 * never changes, we can just access it without additional locking. */
+	configured_log_level = _nml_dbus_log_level;
+
+	nm_assert (level & configured_log_level);
+
+	va_start (args, fmt);
+	msg = g_strdup_vprintf (fmt, args);
+	va_end (args);
+
+	switch (level) {
+	case NML_DBUS_LOG_LEVEL_TRACE:
+		prefix = "<trace> ";
+		break;
+	case NML_DBUS_LOG_LEVEL_DEBUG:
+		prefix = "<debug> ";
+		break;
+	case NML_DBUS_LOG_LEVEL_WARN:
+		prefix = "<warn > ";
+		if (NM_FLAGS_HAS (configured_log_level, _NML_DBUS_LOG_LEVEL_WARN)) {
+			g_warning ("libnm-dbus: %s%s", prefix, msg);
+			return;
+		}
+		break;
+	case NML_DBUS_LOG_LEVEL_ERROR:
+		prefix = "<error> ";
+		if (NM_FLAGS_HAS (configured_log_level, _NML_DBUS_LOG_LEVEL_ERROR)) {
+			g_critical ("libnm-dbus: %s%s", prefix, msg);
+			return;
+		}
+		if (NM_FLAGS_HAS (configured_log_level, _NML_DBUS_LOG_LEVEL_WARN)) {
+			g_warning ("libnm-dbus: %s%s", prefix, msg);
+			return;
+		}
+		break;
+	default:
+		break;
+	}
+
+	g_printerr ("libnm-dbus: %s%s\n", prefix, msg);
+}
+
+/*****************************************************************************/
+
 static char *
 _fixup_string (const char *desc,
                const char *const *ignored_phrases,
