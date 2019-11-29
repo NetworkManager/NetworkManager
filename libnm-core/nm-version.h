0c06a0f3687672a227c934a30d56f306e925f8b1
Thomas Haller <thaller@redhat.com>
2019-11-29 15:46:22 +0100

    release: bump version to 1.23.0 (development)

--- libnm-core/nm-version.h
+++ libnm-core/nm-version.h
@@ -215,6 +215,20 @@
 # define NM_AVAILABLE_IN_1_22
 #endif
 
+#if NM_VERSION_MIN_REQUIRED >= NM_VERSION_1_24
+# define NM_DEPRECATED_IN_1_24           G_DEPRECATED
+# define NM_DEPRECATED_IN_1_24_FOR(f)    G_DEPRECATED_FOR(f)
+#else
+# define NM_DEPRECATED_IN_1_24
+# define NM_DEPRECATED_IN_1_24_FOR(f)
+#endif
+
+#if NM_VERSION_MAX_ALLOWED < NM_VERSION_1_24
+# define NM_AVAILABLE_IN_1_24            G_UNAVAILABLE(1,24)
+#else
+# define NM_AVAILABLE_IN_1_24
+#endif
+
 /*
  * Synchronous API for calling D-Bus in libnm is deprecated. See
  * https://developer.gnome.org/libnm/stable/usage.html#sync-api
