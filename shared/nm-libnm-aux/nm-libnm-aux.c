3843e0c87d1351f8fb2ecfb37a7a33172988aa04
Thomas Haller <thaller@redhat.com>
2019-12-06 13:40:39 +0100

    shared: add "shared/nm-libnm-aux" static library
    We have "shared/nm-libnm-core-aux", which is shared code that can be used
    by anybody (including libnm-core, src, libnm and clients).
    
    We have "clients/common", which are helper function for clients. But
    that implies that the code is inside "clients". I think it would be
    useful to have auxiliary code that extends libnm, but is not only
    usable by code in "clients". In other words, "shared/nm-libnm-aux"
    is a better place than "clients/common", and I think most of the
    functionality form "clients/common" should move there.

---
+++ shared/nm-libnm-aux/nm-libnm-aux.c
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: LGPL-2.1+
+
+#include "nm-default.h"
+
+#include "nm-libnm-aux.h"
---
+++ shared/nm-libnm-aux/nm-libnm-aux.h
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: LGPL-2.1+
+
+#ifndef __NM_LIBNM_AUX_H__
+#define __NM_LIBNM_AUX_H__
+
+#endif /* __NM_LIBNM_AUX_H__ */
