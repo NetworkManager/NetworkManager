NetworkManager/NetworkManager

commit 0c6cd07ec81d1bd5584549bef72c30adc8c79c9f

nm-manager: remove lease file if copying dhclient lease fails
- also use nm_auto_close and nm_close().

src/nm-manager.c
@@ -2698,26 +2698,29 @@ get_existing_connection (NMManager *self,
 static gboolean
 copy_lease (const char *src, const char *dst)
 {
-	int src_fd, dst_fd;
-	ssize_t res, size = SSIZE_MAX;
+	nm_auto_close int src_fd = -1;
+	int dst_fd;
+	ssize_t res;
 
 	src_fd = open (src, O_RDONLY|O_CLOEXEC);
 	if (src_fd < 0)
 		return FALSE;
 
 	dst_fd = open (dst, O_CREAT|O_EXCL|O_CLOEXEC|O_WRONLY, 0644);
-	if (dst_fd < 0) {
-		close (src_fd);
+	if (dst_fd < 0)
+		return FALSE;
+
+	while ((res = sendfile (dst_fd, src_fd, NULL, G_MAXSSIZE)) > 0) {
+	}
+
+	nm_close (dst_fd);
+
+	if (res != 0) {
+		unlink (dst);
 		return FALSE;
 	}
 
-	while ((res = sendfile (dst_fd, src_fd, NULL, size)) > 0)
-		size -= res;
-
-	close (src_fd);
-	close (dst_fd);
-
-	return !res;
+	return TRUE;
 }
 
 static gboolean

commit 70ebdc77307d9710673d83e52ea5a13480f8988a

nm-manager: restore passing correct size to sendfile in copy_lease()
Otherwise sendfile() fails with EINVAL when the file offset
is greater than zero (pos + size > max), always on the second
iteration.

Fixes: 0c6cd07ec81d ('nm-manager: remove lease file if copying dhclient lease fails')

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/merge_requests/359

src/nm-manager.c
@@ -2700,7 +2700,7 @@ copy_lease (const char *src, const char *dst)
 {
 	nm_auto_close int src_fd = -1;
 	int dst_fd;
-	ssize_t res;
+	ssize_t res, size = SSIZE_MAX;
 
 	src_fd = open (src, O_RDONLY|O_CLOEXEC);
 	if (src_fd < 0)
@@ -2710,8 +2710,8 @@ copy_lease (const char *src, const char *dst)
 	if (dst_fd < 0)
 		return FALSE;
 
-	while ((res = sendfile (dst_fd, src_fd, NULL, G_MAXSSIZE)) > 0) {
-	}
+	while ((res = sendfile (dst_fd, src_fd, NULL, size)) > 0)
+		size -= res;
 
 	nm_close (dst_fd);
 