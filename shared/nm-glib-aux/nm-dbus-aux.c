NetworkManager/NetworkManager

commit ce36494c0a483b51275b9818d58e1ad150e4efbc

shared: add nm_dbus_error_is() helper

shared/nm-glib-aux/nm-dbus-aux.c
@@ -341,3 +341,26 @@ nm_dbus_connection_call_finish_variant_strip_dbus_error_cb (GObject *source,
 {
 	_call_finish_cb (source, result, user_data, FALSE, TRUE);
 }
+
+/*****************************************************************************/
+
+gboolean
+_nm_dbus_error_is (GError *error, ...)
+{
+	gs_free char *dbus_error = NULL;
+	const char *name;
+	va_list ap;
+
+	dbus_error = g_dbus_error_get_remote_error (error);
+	if (!dbus_error)
+		return FALSE;
+
+	va_start (ap, error);
+	while ((name = va_arg (ap, const char *))) {
+		if (nm_streq (dbus_error, name))
+			return TRUE;
+	}
+	va_end (ap);
+
+	return FALSE;
+}

shared/nm-glib-aux/nm-dbus-aux.h
@@ -192,4 +192,17 @@ void nm_dbus_connection_call_finish_variant_strip_dbus_error_cb (GObject *source
 
 /*****************************************************************************/
 
+gboolean _nm_dbus_error_is (GError *error, ...) G_GNUC_NULL_TERMINATED;
+
+#define nm_dbus_error_is(error, ...) \
+	({ \
+		GError *const _error = (error); \
+		\
+		_error && _nm_dbus_error_is (_error, __VA_ARGS__, NULL); \
+	})
+
+#define NM_DBUS_ERROR_NAME_UNKNOWN_METHOD "org.freedesktop.DBus.Error.UnknownMethod"
+
+/*****************************************************************************/
+
 #endif /* __NM_DBUS_AUX_H__ */

commit 0de4fd6ebc4c3909838087e254a24359e816d0be

shared: add missing va_end() to _nm_dbus_error_is()
Found by covscan:

   NetworkManager-1.22.0/shared/nm-glib-aux/nm-dbus-aux.c:361:
   missing_va_end: va_end was not called for "ap".

Fixes: ce36494c0a48 ('shared: add nm_dbus_error_is() helper')

shared/nm-glib-aux/nm-dbus-aux.c
@@ -357,8 +357,10 @@ _nm_dbus_error_is (GError *error, ...)
 
 	va_start (ap, error);
 	while ((name = va_arg (ap, const char *))) {
-		if (nm_streq (dbus_error, name))
+		if (nm_streq (dbus_error, name)) {
+			va_end (ap);
 			return TRUE;
+		}
 	}
 	va_end (ap);
 