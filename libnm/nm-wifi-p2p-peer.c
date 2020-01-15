NetworkManager/NetworkManager

commit ce0e898fb476df740a1c337ac9910f504251c466

libnm: refactor caching of D-Bus objects in NMClient
No longer use GDBusObjectMangaerClient and gdbus-codegen generated classes
for the NMClient cache. Instead, use GDBusConnection directly and a
custom implementation (NMLDBusObject) for caching D-Bus' ObjectManager
data.

CHANGES
-------

- This is a complete rework. I think the previous implementation was
difficult to understand. There were unfixed bugs and nobody understood
the code well enough to fix them. Maybe somebody out there understood the
code, but I certainly did not. At least nobody provided patches to fix those
issues. I do believe that this implementation is more straightforward and
easier to understand. It removes a lot of layers of code. Whether this claim
of simplicity is true, each reader must decide for himself/herself. Note
that it is still fairly complex.

- There was a lingering performance issue with large number of D-Bus
objects. The patch tries hard that the implementation scales well. Of
course, when we cache N objects that have N-to-M references to other,
we still are fundamentally O(N*M) for runtime and memory consumption (with
M being the number of references between objects). But each part should behave
efficiently and well.

- Play well with GMainContext. libnm code (NMClient) is generally not
thread safe. However, it should work to use multiple instances in
parallel, as long as each access to a NMClient is through the caller's
GMainContext. This follows glib's style and effectively allows to use NMClient
in a multi threaded scenario. This implies to stick to a main context
upon construction and ensure that callbacks are only invoked when
iterating that context. Also, NMClient itself shall never iterate the
caller's context. This also means, libnm must never use g_idle_add() or
g_timeout_add(), as those enqueue sources in the g_main_context_default()
context.

- Get ordering of messages right. All events are consistently enqueued

libnm/nm-wifi-p2p-peer.c
@@ -30,20 +30,15 @@ NM_GOBJECT_PROPERTIES_DEFINE_BASE (
 );
 
 typedef struct {
+	GBytes *wfd_ies;
 	char *name;
 	char *manufacturer;
 	char *model;
 	char *model_number;
 	char *serial;
-
-	GBytes *wfd_ies;
-
 	char *hw_address;
-
-	int last_seen;
-
-	NM80211ApFlags flags;
-
+	gint32 last_seen;
+	guint32 flags;
 	guint8 strength;
 } NMWifiP2PPeerPrivate;
 
@@ -340,33 +335,6 @@ nm_wifi_p2p_peer_filter_connections (NMWifiP2PPeer *peer, const GPtrArray *conne
 /*****************************************************************************/
 
 static void
-init_dbus (NMObject *object)
-{
-	NMWifiP2PPeerPrivate *priv = NM_WIFI_P2P_PEER_GET_PRIVATE (object);
-	const NMPropertiesInfo property_info[] = {
-		{ NM_WIFI_P2P_PEER_FLAGS,        &priv->flags },
-		{ NM_WIFI_P2P_PEER_NAME,         &priv->name },
-		{ NM_WIFI_P2P_PEER_MANUFACTURER, &priv->manufacturer },
-		{ NM_WIFI_P2P_PEER_MODEL,        &priv->model },
-		{ NM_WIFI_P2P_PEER_MODEL_NUMBER, &priv->model_number },
-		{ NM_WIFI_P2P_PEER_SERIAL,       &priv->serial },
-		{ NM_WIFI_P2P_PEER_WFD_IES,      &priv->wfd_ies },
-		{ NM_WIFI_P2P_PEER_HW_ADDRESS,   &priv->hw_address },
-		{ NM_WIFI_P2P_PEER_STRENGTH,     &priv->strength },
-		{ NM_WIFI_P2P_PEER_LAST_SEEN,    &priv->last_seen },
-		{ NULL },
-	};
-
-	NM_OBJECT_CLASS (nm_wifi_p2p_peer_parent_class)->init_dbus (object);
-
-	_nm_object_register_properties (object,
-	                                NM_DBUS_INTERFACE_WIFI_P2P_PEER,
-	                                property_info);
-}
-
-/*****************************************************************************/
-
-static void
 get_property (GObject *object,
               guint prop_id,
               GValue *value,
@@ -429,7 +397,6 @@ finalize (GObject *object)
 	g_free (priv->model);
 	g_free (priv->model_number);
 	g_free (priv->serial);
-
 	g_free (priv->hw_address);
 
 	g_bytes_unref (priv->wfd_ies);
@@ -437,16 +404,30 @@ finalize (GObject *object)
 	G_OBJECT_CLASS (nm_wifi_p2p_peer_parent_class)->finalize (object);
 }
 
+const NMLDBusMetaIface _nml_dbus_meta_iface_nm_wifip2ppeer = NML_DBUS_META_IFACE_INIT_PROP (
+	NM_DBUS_INTERFACE_WIFI_P2P_PEER,
+	nm_wifi_p2p_peer_get_type,
+	NML_DBUS_META_INTERFACE_PRIO_INSTANTIATE_HIGH,
+	NML_DBUS_META_IFACE_DBUS_PROPERTIES (
+		NML_DBUS_META_PROPERTY_INIT_U  ("Flags",        PROP_FLAGS,        NMWifiP2PPeer, _priv.flags        ),
+		NML_DBUS_META_PROPERTY_INIT_S  ("HwAddress",    PROP_HW_ADDRESS,   NMWifiP2PPeer, _priv.hw_address   ),
+		NML_DBUS_META_PROPERTY_INIT_I  ("LastSeen",     PROP_LAST_SEEN,    NMWifiP2PPeer, _priv.last_seen    ),
+		NML_DBUS_META_PROPERTY_INIT_S  ("Manufacturer", PROP_MANUFACTURER, NMWifiP2PPeer, _priv.manufacturer ),
+		NML_DBUS_META_PROPERTY_INIT_S  ("Model",        PROP_MODEL,        NMWifiP2PPeer, _priv.model        ),
+		NML_DBUS_META_PROPERTY_INIT_S  ("ModelNumber",  PROP_MODEL_NUMBER, NMWifiP2PPeer, _priv.model_number ),
+		NML_DBUS_META_PROPERTY_INIT_S  ("Serial",       PROP_SERIAL,       NMWifiP2PPeer, _priv.serial       ),
+		NML_DBUS_META_PROPERTY_INIT_Y  ("Strength",     PROP_STRENGTH,     NMWifiP2PPeer, _priv.strength     ),
+		NML_DBUS_META_PROPERTY_INIT_AY ("WfdIEs",       PROP_WFD_IES,      NMWifiP2PPeer, _priv.wfd_ies      ),
+	),
+);
+
 static void
 nm_wifi_p2p_peer_class_init (NMWifiP2PPeerClass *klass)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS (klass);
-	NMObjectClass *nm_object_class = NM_OBJECT_CLASS (klass);
 
 	object_class->get_property = get_property;
 	object_class->finalize     = finalize;
-
-	nm_object_class->init_dbus = init_dbus;
 
 	/**
 	 * NMWifiP2PPeer:flags:
@@ -579,5 +560,5 @@ nm_wifi_p2p_peer_class_init (NMWifiP2PPeerClass *klass)
 	                      G_PARAM_READABLE |
 	                      G_PARAM_STATIC_STRINGS);
 
-	g_object_class_install_properties (object_class, _PROPERTY_ENUMS_LAST, obj_properties);
+	_nml_dbus_meta_class_init_with_properties (object_class, &_nml_dbus_meta_iface_nm_wifip2ppeer);
 }

Makefile.am
@@ -1274,15 +1274,17 @@ libnm_lib_h_priv = \
 	libnm/nm-dns-manager.h \
 	libnm/nm-ip4-config.h \
 	libnm/nm-ip6-config.h \
-	libnm/nm-manager.h \
 	libnm/nm-object-private.h \
 	libnm/nm-remote-connection-private.h \
-	libnm/nm-remote-settings.h
+	$(NULL)
 libnm_lib_c_real = \
-	libnm/nm-access-point.c \
-	libnm/nm-active-connection.c \
-	libnm/nm-checkpoint.c \
 	libnm/nm-client.c \
+	libnm/nm-object.c \
+	libnm/nm-device.c \
+	libnm/nm-active-connection.c \
+	\
+	libnm/nm-access-point.c \
+	libnm/nm-checkpoint.c \
 	libnm/nm-dbus-helpers.c \
 	libnm/nm-device-6lowpan.c \
 	libnm/nm-device-adsl.c \
@@ -1311,7 +1313,6 @@ libnm_lib_c_real = \
 	libnm/nm-device-wimax.c \
 	libnm/nm-device-wireguard.c \
 	libnm/nm-device-wpan.c \
-	libnm/nm-device.c \
 	libnm/nm-dhcp-config.c \
 	libnm/nm-dhcp4-config.c \
 	libnm/nm-dhcp6-config.c \
@@ -1320,10 +1321,7 @@ libnm_lib_c_real = \
 	libnm/nm-ip4-config.c \
 	libnm/nm-ip6-config.c \
 	libnm/nm-libnm-utils.c \
-	libnm/nm-manager.c \
-	libnm/nm-object.c \
 	libnm/nm-remote-connection.c \
-	libnm/nm-remote-settings.c \
 	libnm/nm-secret-agent-old.c \
 	libnm/nm-vpn-connection.c \
 	libnm/nm-vpn-editor.c \

docs/libnm/Makefile.am
@@ -49,7 +49,6 @@ IGNORE_HFILES= \
 	nm-object-private.h \
 	nm-property-compare.h \
 	nm-remote-connection-private.h \
-	nm-remote-settings.h \
 	nm-setting-private.h \
 	nm-utils-private.h \
 	nm-core-tests-enum-types.h \

docs/libnm/meson.build
@@ -20,7 +20,6 @@ private_headers = [
   'nm-object-private.h',
   'nm-property-compare.h',
   'nm-remote-connection-private.h',
-  'nm-remote-settings.h',
   'nm-setting-private.h',
   'nm-utils-private.h',
   'nm-core-tests-enum-types.h',

libnm/libnm.ver
@@ -1638,6 +1638,7 @@ libnm_1_22_0 {
 global:
 	nm_client_get_dbus_connection;
 	nm_client_get_dbus_name_owner;
+	nm_client_get_metered;
 	nm_client_reload;
 	nm_client_reload_finish;
 	nm_device_get_interface_flags;

libnm/meson.build
@@ -119,10 +119,8 @@ libnm_sources = files(
   'nm-ip4-config.c',
   'nm-ip6-config.c',
   'nm-libnm-utils.c',
-  'nm-manager.c',
   'nm-object.c',
   'nm-remote-connection.c',
-  'nm-remote-settings.c',
   'nm-secret-agent-old.c',
   'nm-vpn-connection.c',
   'nm-vpn-editor.c',

libnm/nm-access-point.c
@@ -19,7 +19,7 @@
 
 /*****************************************************************************/
 
-NM_GOBJECT_PROPERTIES_DEFINE_BASE (
+NM_GOBJECT_PROPERTIES_DEFINE (NMAccessPoint,
 	PROP_FLAGS,
 	PROP_WPA_FLAGS,
 	PROP_RSN_FLAGS,
@@ -34,16 +34,16 @@ NM_GOBJECT_PROPERTIES_DEFINE_BASE (
 );
 
 typedef struct {
-	NM80211ApFlags flags;
-	NM80211ApSecurityFlags wpa_flags;
-	NM80211ApSecurityFlags rsn_flags;
 	GBytes *ssid;
-	guint32 frequency;
 	char *bssid;
-	NM80211Mode mode;
+	guint32 flags;
+	guint32 wpa_flags;
+	guint32 rsn_flags;
+	guint32 frequency;
+	guint32 mode;
 	guint32 max_bitrate;
+	gint32 last_seen;
 	guint8 strength;
-	int last_seen;
 } NMAccessPointPrivate;
 
 struct _NMAccessPoint {
@@ -127,9 +127,7 @@ nm_access_point_get_ssid (NMAccessPoint *ap)
 	g_return_val_if_fail (NM_IS_ACCESS_POINT (ap), NULL);
 
 	priv = NM_ACCESS_POINT_GET_PRIVATE (ap);
-	if (!priv->ssid || g_bytes_get_size (priv->ssid) == 0)
-		return NULL;
-
+	nm_assert (!priv->ssid || g_bytes_get_size (priv->ssid) > 0);
 	return priv->ssid;
 }
 
@@ -263,6 +261,9 @@ nm_access_point_connection_valid (NMAccessPoint *ap, NMConnection *connection)
 	const char *setting_band;
 	guint32 ap_freq, setting_chan, ap_chan;
 
+	g_return_val_if_fail (NM_IS_ACCESS_POINT (ap), FALSE);
+	g_return_val_if_fail (NM_IS_CONNECTION (connection), FALSE);
+
 	s_con = nm_connection_get_setting_connection (connection);
 	if (!s_con)
 		return FALSE;
@@ -368,7 +369,12 @@ GPtrArray *
 nm_access_point_filter_connections (NMAccessPoint *ap, const GPtrArray *connections)
 {
 	GPtrArray *filtered;
-	int i;
+	guint i;
+
+	g_return_val_if_fail (NM_IS_ACCESS_POINT (ap), NULL);
+
+	if (!connections)
+		return NULL;
 
 	filtered = g_ptr_array_new_with_free_func (g_object_unref);
 	for (i = 0; i < connections->len; i++) {
@@ -379,6 +385,24 @@ nm_access_point_filter_connections (NMAccessPoint *ap, const GPtrArray *connecti
 	}
 
 	return filtered;
+}
+
+/*****************************************************************************/
+
+static NMLDBusNotifyUpdatePropFlags
+_notify_update_prop_hw_address (NMClient *client,
+                                NMLDBusObject *dbobj,
+                                const NMLDBusMetaIface *meta_iface,
+                                guint dbus_property_idx,
+                                GVariant *value)
+{
+	NMAccessPoint *self = NM_ACCESS_POINT (dbobj->nmobj);
+	NMAccessPointPrivate *priv = NM_ACCESS_POINT_GET_PRIVATE (self);
+
+	g_free (priv->bssid);
+	priv->bssid = value ? g_variant_dup_string (value, NULL) : 0u;
+	_notify (self, PROP_HW_ADDRESS);
+	return NML_DBUS_NOTIFY_UPDATE_PROP_FLAGS_NOTIFY;
 }
 
 /*****************************************************************************/
@@ -396,7 +420,6 @@ finalize (GObject *object)
 
 	if (priv->ssid)
 		g_bytes_unref (priv->ssid);
-
 	g_free (priv->bssid);
 
 	G_OBJECT_CLASS (nm_access_point_parent_class)->finalize (object);
@@ -450,42 +473,31 @@ get_property (GObject *object,
 	}
 }
 
-static void
-init_dbus (NMObject *object)
-{
-	NMAccessPointPrivate *priv = NM_ACCESS_POINT_GET_PRIVATE (object);
-	const NMPropertiesInfo property_info[] = {
-		{ NM_ACCESS_POINT_FLAGS,       &priv->flags },
-		{ NM_ACCESS_POINT_WPA_FLAGS,   &priv->wpa_flags },
-		{ NM_ACCESS_POINT_RSN_FLAGS,   &priv->rsn_flags },
-		{ NM_ACCESS_POINT_SSID,        &priv->ssid },
-		{ NM_ACCESS_POINT_FREQUENCY,   &priv->frequency },
-		/* The D-Bus property is HwAddress, but the GObject property is "bssid" */
-		{ NM_ACCESS_POINT_HW_ADDRESS,  &priv->bssid },
-		{ NM_ACCESS_POINT_MODE,        &priv->mode },
-		{ NM_ACCESS_POINT_MAX_BITRATE, &priv->max_bitrate },
-		{ NM_ACCESS_POINT_STRENGTH,    &priv->strength },
-		{ NM_ACCESS_POINT_LAST_SEEN,   &priv->last_seen },
-		{ NULL },
-	};
-
-	NM_OBJECT_CLASS (nm_access_point_parent_class)->init_dbus (object);
-
-	_nm_object_register_properties (object,
-	                                NM_DBUS_INTERFACE_ACCESS_POINT,
-	                                property_info);
-}
+const NMLDBusMetaIface _nml_dbus_meta_iface_nm_accesspoint = NML_DBUS_META_IFACE_INIT_PROP (
+	NM_DBUS_INTERFACE_ACCESS_POINT,
+	nm_access_point_get_type,
+	NML_DBUS_META_INTERFACE_PRIO_INSTANTIATE_HIGH,
+	NML_DBUS_META_IFACE_DBUS_PROPERTIES (
+		NML_DBUS_META_PROPERTY_INIT_U   ("Flags",      PROP_FLAGS,       NMAccessPoint, _priv.flags                    ),
+		NML_DBUS_META_PROPERTY_INIT_U   ("Frequency",  PROP_FREQUENCY,   NMAccessPoint, _priv.frequency                ),
+		NML_DBUS_META_PROPERTY_INIT_FCN ("HwAddress",  PROP_BSSID,       "s",           _notify_update_prop_hw_address ),
+		NML_DBUS_META_PROPERTY_INIT_I   ("LastSeen",   PROP_LAST_SEEN,   NMAccessPoint, _priv.last_seen                ),
+		NML_DBUS_META_PROPERTY_INIT_U   ("MaxBitrate", PROP_MAX_BITRATE, NMAccessPoint, _priv.max_bitrate              ),
+		NML_DBUS_META_PROPERTY_INIT_U   ("Mode",       PROP_MODE,        NMAccessPoint, _priv.mode                     ),
+		NML_DBUS_META_PROPERTY_INIT_U   ("RsnFlags",   PROP_RSN_FLAGS,   NMAccessPoint, _priv.rsn_flags                ),
+		NML_DBUS_META_PROPERTY_INIT_AY  ("Ssid",       PROP_SSID,        NMAccessPoint, _priv.ssid                     ),
+		NML_DBUS_META_PROPERTY_INIT_Y   ("Strength",   PROP_STRENGTH,    NMAccessPoint, _priv.strength                 ),
+		NML_DBUS_META_PROPERTY_INIT_U   ("WpaFlags",   PROP_WPA_FLAGS,   NMAccessPoint, _priv.wpa_flags                ),
+	),
+);
 
 static void
 nm_access_point_class_init (NMAccessPointClass *ap_class)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS (ap_class);
-	NMObjectClass *nm_object_class = NM_OBJECT_CLASS (ap_class);
 
 	object_class->get_property = get_property;
 	object_class->finalize     = finalize;
-
-	nm_object_class->init_dbus = init_dbus;
 
 	/**
 	 * NMAccessPoint:flags:
@@ -620,5 +632,5 @@ nm_access_point_class_init (NMAccessPointClass *ap_class)
 	                      G_PARAM_READABLE |
 	                      G_PARAM_STATIC_STRINGS);
 
-	g_object_class_install_properties (object_class, _PROPERTY_ENUMS_LAST, obj_properties);
+	_nml_dbus_meta_class_init_with_properties (object_class, &_nml_dbus_meta_iface_nm_accesspoint);
 }

commit e978eca644f03675bed0f56e10ef0cae0bd2f427

libnm: fix exposing nm_wifi_p2p_peer_get_name() from D-Bus property
Fixes: ce0e898fb476 ('libnm: refactor caching of D-Bus objects in NMClient')

libnm/nm-wifi-p2p-peer.c
@@ -415,6 +415,7 @@ const NMLDBusMetaIface _nml_dbus_meta_iface_nm_wifip2ppeer = NML_DBUS_META_IFACE
 		NML_DBUS_META_PROPERTY_INIT_S  ("Manufacturer", PROP_MANUFACTURER, NMWifiP2PPeer, _priv.manufacturer ),
 		NML_DBUS_META_PROPERTY_INIT_S  ("Model",        PROP_MODEL,        NMWifiP2PPeer, _priv.model        ),
 		NML_DBUS_META_PROPERTY_INIT_S  ("ModelNumber",  PROP_MODEL_NUMBER, NMWifiP2PPeer, _priv.model_number ),
+		NML_DBUS_META_PROPERTY_INIT_S  ("Name",         PROP_NAME,         NMWifiP2PPeer, _priv.name         ),
 		NML_DBUS_META_PROPERTY_INIT_S  ("Serial",       PROP_SERIAL,       NMWifiP2PPeer, _priv.serial       ),
 		NML_DBUS_META_PROPERTY_INIT_Y  ("Strength",     PROP_STRENGTH,     NMWifiP2PPeer, _priv.strength     ),
 		NML_DBUS_META_PROPERTY_INIT_AY ("WfdIEs",       PROP_WFD_IES,      NMWifiP2PPeer, _priv.wfd_ies      ),