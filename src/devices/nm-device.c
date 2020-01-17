11c989bb04da3cf0433b097d5e55a9ac103a6851
Antonio Cardace <acardace@redhat.com>
2020-01-17 15:14:53 +0100

    nm-device: add new pending action to keep the device busy when in between states
    Add a 'in-state-change' pending action to be sure the device always has a
    pending when transitioning between states (this prevents callbacks to mark
    startup as complete while running _set_state_full()).
    
    This is needed as during the 'failed'->'disconnected' the pending action 'activation-*'
    for the device is removed resulting in an empty pending_actions list which then
    triggers 'check_if_startup_complete()' that will find no pending action and mark
    startup as complete even if the device could have been activated with another connection.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1759956
    (cherry picked from commit f583aec8066cefabf2f53b99ea55f3650f785b93)

--- src/devices/nm-device.c
+++ src/devices/nm-device.c
@@ -15515,6 +15515,12 @@ _set_state_full (NMDevice *self,
 	       reason_to_string_a (reason),
 	       _sys_iface_state_to_str (priv->sys_iface_state));
 
+	/* in order to prevent triggering any callback caused
+	 * by the device not having any pending action anymore
+	 * we add one here that gets removed at the end of the function */
+	nm_device_add_pending_action (self,
+	                              NM_PENDING_ACTION_IN_STATE_CHANGE,
+	                              TRUE);
 	priv->in_state_changed = TRUE;
 
 	priv->state = state;
@@ -15821,6 +15827,9 @@ _set_state_full (NMDevice *self,
 		g_object_unref (req);
 
 	priv->in_state_changed = FALSE;
+	nm_device_remove_pending_action (self,
+	                                 NM_PENDING_ACTION_IN_STATE_CHANGE,
+	                                 TRUE);
 
 	if ((old_state > NM_DEVICE_STATE_UNMANAGED) != (state > NM_DEVICE_STATE_UNMANAGED))
 		_notify (self, PROP_MANAGED);
