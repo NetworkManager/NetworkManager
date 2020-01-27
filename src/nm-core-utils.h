cc002ed3c1ec651b4dc52026e44340880fe643d6
Thomas Haller <thaller@redhat.com>
2020-01-27 20:16:22 +0100

    wifi: move nm_wifi_utils_parse_ies() to "src/nm-core-utils.h"
    We will need it in "src/supplicant".

--- src/nm-core-utils.h
+++ src/nm-core-utils.h
@@ -473,6 +473,11 @@ const char *nm_utils_parse_dns_domain (const char *domain, gboolean *is_routing)
 
 /*****************************************************************************/
 
+void nm_wifi_utils_parse_ies (const guint8 *bytes,
+                              gsize len,
+                              guint32 *out_max_rate,
+                              gboolean *out_metered);
+
 guint8 nm_wifi_utils_level_to_quality (int val);
 
 /*****************************************************************************/
