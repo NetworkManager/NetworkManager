ff7545704ee347fa4e79e5bea0b68d264e180f9d
Beniamino Galvani <bgalvani@redhat.com>
2020-02-06 14:52:12 +0100

    n-dhcp4: fix initialization of the 'secs' DHCP header field
    Due to wrong type conversions, the value was always zero.
    
    https://gitlab.freedesktop.org/NetworkManager/NetworkManager/issues/341
    (cherry picked from commit df6129d93a8445c649126cebffac3cb1192de771)

--- shared/n-dhcp4/src/n-dhcp4-private.h
+++ shared/n-dhcp4/src/n-dhcp4-private.h
@@ -477,7 +477,7 @@ int n_dhcp4_outgoing_append_lifetime(NDhcp4Outgoing *message, uint32_t lifetime)
 int n_dhcp4_outgoing_append_server_identifier(NDhcp4Outgoing *message, struct in_addr addr);
 int n_dhcp4_outgoing_append_requested_ip(NDhcp4Outgoing *message, struct in_addr addr);
 
-void n_dhcp4_outgoing_set_secs(NDhcp4Outgoing *message, uint32_t secs);
+void n_dhcp4_outgoing_set_secs(NDhcp4Outgoing *message, uint16_t secs);
 void n_dhcp4_outgoing_set_xid(NDhcp4Outgoing *message, uint32_t xid);
 void n_dhcp4_outgoing_set_yiaddr(NDhcp4Outgoing *message, struct in_addr yiaddr);
 
