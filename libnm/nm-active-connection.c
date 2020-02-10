NetworkManager/NetworkManager

commit ce0e898fb476df740a1c337ac9910f504251c466

libnm: refactor caching of D-Bus objects in NMClient
No longer use GDBusObjectMangaerClient and gdbus-codegen generated classes
for the NMClient cache. Instead, use GDBusConnection directly and a
custom implementation (NMLDBusObject) for caching D-Bus' ObjectManager
data.

CHANGES
-------

- This is a complete rework. I think the previous implementation was
difficult to understand. There were unfixed bugs and nobody understood
the code well enough to fix them. Maybe somebody out there understood the
code, but I certainly did not. At least nobody provided patches to fix those
issues. I do believe that this implementation is more straightforward and
easier to understand. It removes a lot of layers of code. Whether this claim
of simplicity is true, each reader must decide for himself/herself. Note
that it is still fairly complex.

- There was a lingering performance issue with large number of D-Bus
objects. The patch tries hard that the implementation scales well. Of
course, when we cache N objects that have N-to-M references to other,
we still are fundamentally O(N*M) for runtime and memory consumption (with
M being the number of references between objects). But each part should behave
efficiently and well.

- Play well with GMainContext. libnm code (NMClient) is generally not
thread safe. However, it should work to use multiple instances in
parallel, as long as each access to a NMClient is through the caller's
GMainContext. This follows glib's style and effectively allows to use NMClient
in a multi threaded scenario. This implies to stick to a main context
upon construction and ensure that callbacks are only invoked when
iterating that context. Also, NMClient itself shall never iterate the
caller's context. This also means, libnm must never use g_idle_add() or
g_timeout_add(), as those enqueue sources in the g_main_context_default()
context.

- Get ordering of messages right. All events are consistently enqueued

libnm/nm-active-connection.c
@@ -21,8 +21,6 @@
 #include "nm-ip6-config.h"
 #include "nm-remote-connection.h"
 
-#include "introspection/org.freedesktop.NetworkManager.Connection.Active.h"
-
 /*****************************************************************************/
 
 NM_GOBJECT_PROPERTIES_DEFINE (NMActiveConnection,
@@ -50,26 +48,34 @@ enum {
 	LAST_SIGNAL
 };
 
-static guint signals[LAST_SIGNAL] = { 0 };
+static guint signals[LAST_SIGNAL];
+
+enum {
+	PROPERTY_O_IDX_CONNECTION,
+	PROPERTY_O_IDX_MASTER,
+	PROPERTY_O_IDX_IP4_CONFIG,
+	PROPERTY_O_IDX_IP6_CONFIG,
+	PROPERTY_O_IDX_DHCP4_CONFIG,
+	PROPERTY_O_IDX_DHCP6_CONFIG,
+	_PROPERTY_O_IDX_NUM,
+};
 
 typedef struct _NMActiveConnectionPrivate {
-	NMRemoteConnection *connection;
+	NMLDBusPropertyO property_o[_PROPERTY_O_IDX_NUM];
+	NMLDBusPropertyAO devices;
+	NMRefString *specific_object_path;
 	char *id;
 	char *uuid;
 	char *type;
-	char *specific_object_path;
-	GPtrArray *devices;
-	NMActiveConnectionState state;
-	guint state_flags;
-	gboolean is_default;
-	NMIPConfig *ip4_config;
-	NMDhcpConfig *dhcp4_config;
-	gboolean is_default6;
-	NMIPConfig *ip6_config;
-	NMDhcpConfig *dhcp6_config;
-	gboolean is_vpn;
-	NMDevice *master;
-	NMActiveConnectionStateReason reason;
+
+	guint32 state;
+	guint32 state_flags;
+
+	bool is_default;
+	bool is_default6;
+	bool is_vpn;
+
+	guint32 reason;
 } NMActiveConnectionPrivate;
 
 G_DEFINE_TYPE (NMActiveConnection, nm_active_connection, NM_TYPE_OBJECT);
@@ -92,7 +98,7 @@ nm_active_connection_get_connection (NMActiveConnection *connection)
 {
 	g_return_val_if_fail (NM_IS_ACTIVE_CONNECTION (connection), NULL);
 
-	return NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->connection;
+	return nml_dbus_property_o_get_obj (&NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->property_o[PROPERTY_O_IDX_CONNECTION]);
 }
 
 /**
@@ -166,7 +172,7 @@ nm_active_connection_get_specific_object_path (NMActiveConnection *connection)
 {
 	g_return_val_if_fail (NM_IS_ACTIVE_CONNECTION (connection), NULL);
 
-	return NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->specific_object_path;
+	return _nml_coerce_property_object_path (NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->specific_object_path);
 }
 
 /**
@@ -183,7 +189,7 @@ nm_active_connection_get_devices (NMActiveConnection *connection)
 {
 	g_return_val_if_fail (NM_IS_ACTIVE_CONNECTION (connection), NULL);
 
-	return NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->devices;
+	return nml_dbus_property_ao_get_objs_as_ptrarray (&NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->devices);
 }
 
 /**
@@ -269,7 +275,7 @@ nm_active_connection_get_ip4_config (NMActiveConnection *connection)
 {
 	g_return_val_if_fail (NM_IS_ACTIVE_CONNECTION (connection), NULL);
 
-	return NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->ip4_config;
+	return nml_dbus_property_o_get_obj (&NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->property_o[PROPERTY_O_IDX_IP4_CONFIG]);
 }
 
 /**
@@ -288,7 +294,7 @@ nm_active_connection_get_dhcp4_config (NMActiveConnection *connection)
 {
 	g_return_val_if_fail (NM_IS_ACTIVE_CONNECTION (connection), NULL);
 
-	return NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->dhcp4_config;
+	return nml_dbus_property_o_get_obj (&NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->property_o[PROPERTY_O_IDX_DHCP4_CONFIG]);
 }
 
 /**
@@ -322,7 +328,7 @@ nm_active_connection_get_ip6_config (NMActiveConnection *connection)
 {
 	g_return_val_if_fail (NM_IS_ACTIVE_CONNECTION (connection), NULL);
 
-	return NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->ip6_config;
+	return nml_dbus_property_o_get_obj (&NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->property_o[PROPERTY_O_IDX_IP6_CONFIG]);
 }
 
 /**
@@ -341,7 +347,7 @@ nm_active_connection_get_dhcp6_config (NMActiveConnection *connection)
 {
 	g_return_val_if_fail (NM_IS_ACTIVE_CONNECTION (connection), NULL);
 
-	return NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->dhcp6_config;
+	return nml_dbus_property_o_get_obj (&NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->property_o[PROPERTY_O_IDX_DHCP6_CONFIG]);
 }
 
 /**
@@ -373,8 +379,54 @@ nm_active_connection_get_master (NMActiveConnection *connection)
 {
 	g_return_val_if_fail (NM_IS_ACTIVE_CONNECTION (connection), NULL);
 
-	return NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->master;
+	return nml_dbus_property_o_get_obj (&NM_ACTIVE_CONNECTION_GET_PRIVATE (connection)->property_o[PROPERTY_O_IDX_MASTER]);
 }
+
+/*****************************************************************************/
+
+static void
+_notify_event_state_changed (NMClient *client,
+                             NMClientNotifyEventWithPtr *notify_event)
+{
+	gs_unref_object NMActiveConnection *self = notify_event->user_data;
+	NMActiveConnectionPrivate *priv = NM_ACTIVE_CONNECTION_GET_PRIVATE (self);
+
+	/* we expose here the value cache in @priv. In practice, this is the same
+	 * value as we received from the signal. In the unexpected case where they
+	 * differ, the cached value of the current instance would still be more correct. */
+	g_signal_emit (self,
+	               signals[STATE_CHANGED],
+	               0,
+	               (guint) priv->state,
+	               (guint) priv->reason);
+}
+
+void
+_nm_active_connection_state_changed_commit (NMActiveConnection *self,
+                                            guint32 state,
+                                            guint32 reason)
+{
+	NMClient *client;
+	NMActiveConnectionPrivate *priv = NM_ACTIVE_CONNECTION_GET_PRIVATE (self);
+
+	client = _nm_object_get_client (self);
+
+	if (priv->state != state) {
+		priv->state = state;
+		_nm_client_queue_notify_object (client,
+		                                self,
+		                                obj_properties[PROP_STATE]);
+	}
+
+	priv->reason = reason;
+
+	_nm_client_notify_event_queue_with_ptr (client,
+	                                        NM_CLIENT_NOTIFY_EVENT_PRIO_GPROP + 1,
+	                                        _notify_event_state_changed,
+	                                        g_object_ref (self));
+}
+
+/*****************************************************************************/
 
 static void
 nm_active_connection_init (NMActiveConnection *self)
@@ -384,60 +436,6 @@ nm_active_connection_init (NMActiveConnection *self)
 	priv = G_TYPE_INSTANCE_GET_PRIVATE (self, NM_TYPE_ACTIVE_CONNECTION, NMActiveConnectionPrivate);
 
 	self->_priv = priv;
-
-	priv->devices = g_ptr_array_new ();
-}
-
-static void
-state_changed_proxy (NMDBusActiveConnectionProxy *proxy,
-                     NMActiveConnectionState state,
-                     NMActiveConnectionStateReason reason,
-                     gpointer user_data)
-{
-	NMActiveConnection *connection = NM_ACTIVE_CONNECTION (user_data);
-	NMActiveConnectionPrivate *priv = NM_ACTIVE_CONNECTION_GET_PRIVATE (connection);
-
-	priv->state = state;
-	priv->reason = reason;
-	g_signal_emit (connection, signals[STATE_CHANGED], 0, state, reason);
-	_notify (connection, PROP_STATE);
-}
-
-static void
-constructed (GObject *object)
-{
-	GDBusProxy *proxy;
-
-	proxy = _nm_object_get_proxy (NM_OBJECT (object), NM_DBUS_INTERFACE_ACTIVE_CONNECTION);
-	g_signal_connect (proxy, "state-changed",
-	                  G_CALLBACK (state_changed_proxy), object);
-	g_object_unref (proxy);
-
-	G_OBJECT_CLASS (nm_active_connection_parent_class)->constructed (object);
-}
-
-static void
-dispose (GObject *object)
-{
-	NMActiveConnectionPrivate *priv = NM_ACTIVE_CONNECTION_GET_PRIVATE (object);
-	GDBusProxy *proxy;
-
-	g_clear_pointer (&priv->devices, g_ptr_array_unref);
-
-	g_clear_object (&priv->connection);
-	g_clear_object (&priv->master);
-	g_clear_object (&priv->ip4_config);
-	g_clear_object (&priv->dhcp4_config);
-	g_clear_object (&priv->ip6_config);
-	g_clear_object (&priv->dhcp6_config);
-
-	proxy = _nm_object_get_proxy (NM_OBJECT (object), NM_DBUS_INTERFACE_ACTIVE_CONNECTION);
-	if (proxy) {
-		g_signal_handlers_disconnect_by_data (proxy, object);
-		g_object_unref (proxy);
-	}
-
-	G_OBJECT_CLASS (nm_active_connection_parent_class)->dispose (object);
 }
 
 static void
@@ -448,7 +446,7 @@ finalize (GObject *object)
 	g_free (priv->id);
 	g_free (priv->uuid);
 	g_free (priv->type);
-	g_free (priv->specific_object_path);
+	nm_ref_string_unref (priv->specific_object_path);
 
 	G_OBJECT_CLASS (nm_active_connection_parent_class)->finalize (object);
 }
@@ -516,73 +514,46 @@ get_property (GObject *object,
 	}
 }
 
-static gboolean
-demarshal_specific_object_path (NMObject *object, GParamSpec *pspec, GVariant *value, gpointer field)
-{
-	const char *v;
-	char **param = (char **) field;
-
-	/* We have to demarshal this manually, because the D-Bus property name
-	 * ("SpecificObject"), doesn't match the object property name
-	 * ("specific-object-path"). (The name "specific-object" is reserved for
-	 * future use as an NMObject-valued property.)
-	 */
-	if (!g_variant_is_of_type (value, G_VARIANT_TYPE_OBJECT_PATH))
-		return FALSE;
-
-	v = g_variant_get_string (value, NULL);
-
-	g_free (*param);
-	*param = nm_streq0 (v, "/") ? NULL : g_strdup (v);
-	return TRUE;
-}
-
-static void
-init_dbus (NMObject *object)
-{
-	NMActiveConnectionPrivate *priv = NM_ACTIVE_CONNECTION_GET_PRIVATE (object);
-	const NMPropertiesInfo property_info[] = {
-		{ NM_ACTIVE_CONNECTION_CONNECTION,           &priv->connection, NULL, NM_TYPE_REMOTE_CONNECTION },
-		{ NM_ACTIVE_CONNECTION_ID,                   &priv->id },
-		{ NM_ACTIVE_CONNECTION_UUID,                 &priv->uuid },
-		{ NM_ACTIVE_CONNECTION_TYPE,                 &priv->type },
-		{ "specific-object",                         &priv->specific_object_path, demarshal_specific_object_path },
-		{ NM_ACTIVE_CONNECTION_DEVICES,              &priv->devices, NULL, NM_TYPE_DEVICE },
-		{ NM_ACTIVE_CONNECTION_STATE,                &priv->state },
-		{ NM_ACTIVE_CONNECTION_STATE_FLAGS,          &priv->state_flags },
-		{ NM_ACTIVE_CONNECTION_DEFAULT,              &priv->is_default },
-		{ NM_ACTIVE_CONNECTION_IP4_CONFIG,           &priv->ip4_config, NULL, NM_TYPE_IP4_CONFIG },
-		{ NM_ACTIVE_CONNECTION_DHCP4_CONFIG,         &priv->dhcp4_config, NULL, NM_TYPE_DHCP4_CONFIG },
-		{ NM_ACTIVE_CONNECTION_DEFAULT6,             &priv->is_default6 },
-		{ NM_ACTIVE_CONNECTION_IP6_CONFIG,           &priv->ip6_config, NULL, NM_TYPE_IP6_CONFIG },
-		{ NM_ACTIVE_CONNECTION_DHCP6_CONFIG,         &priv->dhcp6_config, NULL, NM_TYPE_DHCP6_CONFIG },
-		{ NM_ACTIVE_CONNECTION_VPN,                  &priv->is_vpn },
-		{ NM_ACTIVE_CONNECTION_MASTER,               &priv->master, NULL, NM_TYPE_DEVICE },
-
-		{ NULL },
-	};
-
-	NM_OBJECT_CLASS (nm_active_connection_parent_class)->init_dbus (object);
-
-	_nm_object_register_properties (object,
-	                                NM_DBUS_INTERFACE_ACTIVE_CONNECTION,
-	                                property_info);
-}
+const NMLDBusMetaIface _nml_dbus_meta_iface_nm_connection_active = NML_DBUS_META_IFACE_INIT_PROP (
+	NM_DBUS_INTERFACE_ACTIVE_CONNECTION,
+	nm_active_connection_get_type,
+	NML_DBUS_META_INTERFACE_PRIO_INSTANTIATE_LOW,
+	NML_DBUS_META_IFACE_DBUS_PROPERTIES (
+		NML_DBUS_META_PROPERTY_INIT_O_PROP  ("Connection",     PROP_CONNECTION,           NMActiveConnectionPrivate, property_o[PROPERTY_O_IDX_CONNECTION],   nm_remote_connection_get_type ),
+		NML_DBUS_META_PROPERTY_INIT_B       ("Default",        PROP_DEFAULT,              NMActiveConnectionPrivate, is_default                                                             ),
+		NML_DBUS_META_PROPERTY_INIT_B       ("Default6",       PROP_DEFAULT6,             NMActiveConnectionPrivate, is_default6                                                            ),
+		NML_DBUS_META_PROPERTY_INIT_AO_PROP ("Devices",        PROP_DEVICES,              NMActiveConnectionPrivate, devices,                                 nm_device_get_type            ),
+		NML_DBUS_META_PROPERTY_INIT_O_PROP  ("Dhcp4Config",    PROP_DHCP4_CONFIG,         NMActiveConnectionPrivate, property_o[PROPERTY_O_IDX_DHCP4_CONFIG], nm_dhcp4_config_get_type      ),
+		NML_DBUS_META_PROPERTY_INIT_O_PROP  ("Dhcp6Config",    PROP_DHCP6_CONFIG,         NMActiveConnectionPrivate, property_o[PROPERTY_O_IDX_DHCP6_CONFIG], nm_dhcp6_config_get_type      ),
+		NML_DBUS_META_PROPERTY_INIT_S       ("Id",             PROP_ID,                   NMActiveConnectionPrivate, id                                                                     ),
+		NML_DBUS_META_PROPERTY_INIT_O_PROP  ("Ip4Config",      PROP_IP4_CONFIG,           NMActiveConnectionPrivate, property_o[PROPERTY_O_IDX_IP4_CONFIG],   nm_ip4_config_get_type        ),
+		NML_DBUS_META_PROPERTY_INIT_O_PROP  ("Ip6Config",      PROP_IP6_CONFIG,           NMActiveConnectionPrivate, property_o[PROPERTY_O_IDX_IP6_CONFIG],   nm_ip6_config_get_type        ),
+		NML_DBUS_META_PROPERTY_INIT_O_PROP  ("Master",         PROP_MASTER,               NMActiveConnectionPrivate, property_o[PROPERTY_O_IDX_MASTER],       nm_device_get_type            ),
+		NML_DBUS_META_PROPERTY_INIT_O       ("SpecificObject", PROP_SPECIFIC_OBJECT_PATH, NMActiveConnectionPrivate, specific_object_path                                                   ),
+		NML_DBUS_META_PROPERTY_INIT_U       ("State",          PROP_STATE,                NMActiveConnectionPrivate, state                                                                  ),
+		NML_DBUS_META_PROPERTY_INIT_U       ("StateFlags",     PROP_STATE_FLAGS,          NMActiveConnectionPrivate, state_flags                                                            ),
+		NML_DBUS_META_PROPERTY_INIT_S       ("Type",           PROP_TYPE,                 NMActiveConnectionPrivate, type                                                                   ),
+		NML_DBUS_META_PROPERTY_INIT_S       ("Uuid",           PROP_UUID,                 NMActiveConnectionPrivate, uuid                                                                   ),
+		NML_DBUS_META_PROPERTY_INIT_B       ("Vpn",            PROP_VPN,                  NMActiveConnectionPrivate, is_vpn                                                                 ),
+	),
+	.base_struct_offset = G_STRUCT_OFFSET (NMActiveConnection, _priv),
+);
 
 static void
-nm_active_connection_class_init (NMActiveConnectionClass *ap_class)
+nm_active_connection_class_init (NMActiveConnectionClass *klass)
 {
-	GObjectClass *object_class = G_OBJECT_CLASS (ap_class);
-	NMObjectClass *nm_object_class = NM_OBJECT_CLASS (ap_class);
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	NMObjectClass *nm_object_class = NM_OBJECT_CLASS (klass);
 
-	g_type_class_add_private (ap_class, sizeof (NMActiveConnectionPrivate));
+	g_type_class_add_private (klass, sizeof (NMActiveConnectionPrivate));
 
 	object_class->get_property = get_property;
-	object_class->constructed  = constructed;
-	object_class->dispose      = dispose;
 	object_class->finalize     = finalize;
 
-	nm_object_class->init_dbus = init_dbus;
+	_NM_OBJECT_CLASS_INIT_PRIV_PTR_INDIRECT (nm_object_class, NMActiveConnection);
+
+	_NM_OBJECT_CLASS_INIT_PROPERTY_O_FIELDS_N (nm_object_class, NMActiveConnectionPrivate, property_o);
+	_NM_OBJECT_CLASS_INIT_PROPERTY_AO_FIELDS_1 (nm_object_class, NMActiveConnectionPrivate, devices);
 
 	/**
 	 * NMActiveConnection:connection:
@@ -765,7 +736,13 @@ nm_active_connection_class_init (NMActiveConnectionClass *ap_class)
 	                         G_PARAM_READABLE |
 	                         G_PARAM_STATIC_STRINGS);
 
-	g_object_class_install_properties (object_class, _PROPERTY_ENUMS_LAST, obj_properties);
+	_nml_dbus_meta_class_init_with_properties (object_class, &_nml_dbus_meta_iface_nm_connection_active);
+
+	/* TODO: the state reason should also be exposed as a property in libnm's NMActiveConnection,
+	 * like done for NMDevice's state reason. */
+
+	/* TODO: the D-Bus API should also expose the state-reason as a property instead of
+	 * a "StateChanged" signal. Like done for Device's "StateReason".  */
 
 	/**
 	 * NMActiveConnection::state-changed:

Makefile.am
@@ -1274,15 +1274,17 @@ libnm_lib_h_priv = \
 	libnm/nm-dns-manager.h \
 	libnm/nm-ip4-config.h \
 	libnm/nm-ip6-config.h \
-	libnm/nm-manager.h \
 	libnm/nm-object-private.h \
 	libnm/nm-remote-connection-private.h \
-	libnm/nm-remote-settings.h
+	$(NULL)
 libnm_lib_c_real = \
-	libnm/nm-access-point.c \
-	libnm/nm-active-connection.c \
-	libnm/nm-checkpoint.c \
 	libnm/nm-client.c \
+	libnm/nm-object.c \
+	libnm/nm-device.c \
+	libnm/nm-active-connection.c \
+	\
+	libnm/nm-access-point.c \
+	libnm/nm-checkpoint.c \
 	libnm/nm-dbus-helpers.c \
 	libnm/nm-device-6lowpan.c \
 	libnm/nm-device-adsl.c \
@@ -1311,7 +1313,6 @@ libnm_lib_c_real = \
 	libnm/nm-device-wimax.c \
 	libnm/nm-device-wireguard.c \
 	libnm/nm-device-wpan.c \
-	libnm/nm-device.c \
 	libnm/nm-dhcp-config.c \
 	libnm/nm-dhcp4-config.c \
 	libnm/nm-dhcp6-config.c \
@@ -1320,10 +1321,7 @@ libnm_lib_c_real = \
 	libnm/nm-ip4-config.c \
 	libnm/nm-ip6-config.c \
 	libnm/nm-libnm-utils.c \
-	libnm/nm-manager.c \
-	libnm/nm-object.c \
 	libnm/nm-remote-connection.c \
-	libnm/nm-remote-settings.c \
 	libnm/nm-secret-agent-old.c \
 	libnm/nm-vpn-connection.c \
 	libnm/nm-vpn-editor.c \

docs/libnm/Makefile.am
@@ -49,7 +49,6 @@ IGNORE_HFILES= \
 	nm-object-private.h \
 	nm-property-compare.h \
 	nm-remote-connection-private.h \
-	nm-remote-settings.h \
 	nm-setting-private.h \
 	nm-utils-private.h \
 	nm-core-tests-enum-types.h \

docs/libnm/meson.build
@@ -20,7 +20,6 @@ private_headers = [
   'nm-object-private.h',
   'nm-property-compare.h',
   'nm-remote-connection-private.h',
-  'nm-remote-settings.h',
   'nm-setting-private.h',
   'nm-utils-private.h',
   'nm-core-tests-enum-types.h',

libnm/libnm.ver
@@ -1638,6 +1638,7 @@ libnm_1_22_0 {
 global:
 	nm_client_get_dbus_connection;
 	nm_client_get_dbus_name_owner;
+	nm_client_get_metered;
 	nm_client_reload;
 	nm_client_reload_finish;
 	nm_device_get_interface_flags;

libnm/meson.build
@@ -119,10 +119,8 @@ libnm_sources = files(
   'nm-ip4-config.c',
   'nm-ip6-config.c',
   'nm-libnm-utils.c',
-  'nm-manager.c',
   'nm-object.c',
   'nm-remote-connection.c',
-  'nm-remote-settings.c',
   'nm-secret-agent-old.c',
   'nm-vpn-connection.c',
   'nm-vpn-editor.c',

commit 425412a363e8404926fbe6391846141ea174ac5c

libnm: hide NMActiveConnection until NMRemoteConnection is ready
Generally, libnm's NMClient cache only wants to expose NMObjects that
are fully initalized. Most objects don't require anything special,
except NMRemoteConnection which waits for the GetSettings() call to complete.

NMObjects reference each other. For example, NMActiveConnection
references NMDevice and NMRemoteConnection. There is a desire that an
object is only ready, if the objects that it references are ready too.
In practice that is not done, because usually every objects references
other objects, that means all objects would be declared as non-ready
as long as any of them is still initializing. That does not seem
desirable. Instead, most objects (except NMRemoteConnection and now
NMActiveConnection) are considered ready and visible, once their first
notification completes. In case the objects reference any object that is
not yet ready, the references is NULL (but the source object is visible
already). This is also done this way, to cope with cycles where
objects reference each other. In practice, such cycles should not be
exposed by NetworkManager. However, libnm should be robust against that.

This has the undesired effect that when you call AddAndActivate(), then
the NMActiveConnection might already be visible while its
NMRemoteConnection isn't. That means, ac.get_connection() will
initially return NULL, until the remote connection becomes ready.
Also add a special handling that NMActiveConnection waits for their
NMRemoteConnection to be ready, before being ready itself.

Fixes: ce0e898fb476 ('libnm: refactor caching of D-Bus objects in NMClient')

libnm/nm-active-connection.c
@@ -425,6 +425,33 @@ _nm_active_connection_state_changed_commit (NMActiveConnection *self,
 	                                        _notify_event_state_changed,
 	                                        g_object_ref (self));
 }
+/*****************************************************************************/
+
+static gboolean
+is_ready (NMObject *nmobj)
+{
+	NMActiveConnectionPrivate *priv = NM_ACTIVE_CONNECTION_GET_PRIVATE (nmobj);
+
+	/* Usually, we don't want to expose our NMObject instances until they are fully initialized.
+	 * For NMRemoteSetting this means to wait until GetSettings() returns.
+	 *
+	 * Note that most object types reference each other (directly or indirectly). E.g. the
+	 * NMActiveConnection refers to the NMRemoteConnection and the NMDevice instance. So,
+	 * we don't want to hide them too long, otherwise basically the entire set of objects
+	 * will be hidden until they are all initialized. So, usually, when a NMObject references
+	 * objects that are not yet initialized, that reference will just be NULL but the object
+	 * will be considered ready already.
+	 *
+	 * For NMActiveConnection referencing a NMRemoteConnection don't do that. Here we wait for the
+	 * NMRemoteConnection to be ready as well. This is somewhat arbitrary special casing, but
+	 * the effect is that when nm_client_add_and_activate*() returns, the NMActiveConnection already
+	 * references a initialized NMRemoteConnection.
+	 */
+	if (!nml_dbus_property_o_is_ready_fully (&priv->property_o[PROPERTY_O_IDX_CONNECTION]))
+		return FALSE;
+
+	return NM_OBJECT_CLASS (nm_active_connection_parent_class)->is_ready (nmobj);
+}
 
 /*****************************************************************************/
 
@@ -549,6 +576,8 @@ nm_active_connection_class_init (NMActiveConnectionClass *klass)
 
 	object_class->get_property = get_property;
 	object_class->finalize     = finalize;
+
+	nm_object_class->is_ready = is_ready;
 
 	_NM_OBJECT_CLASS_INIT_PRIV_PTR_INDIRECT (nm_object_class, NMActiveConnection);
 

libnm/nm-client.c
@@ -1594,6 +1594,14 @@ nml_dbus_property_o_is_ready (const NMLDBusPropertyO *pr_o)
 	       || !pr_o->owner_dbobj;
 }
 
+gboolean
+nml_dbus_property_o_is_ready_fully (const NMLDBusPropertyO *pr_o)
+{
+	return    !pr_o->owner_dbobj
+	       || !pr_o->obj_watcher
+	       || pr_o->nmobj;
+}
+
 static void
 nml_dbus_property_o_notify_changed (NMLDBusPropertyO *pr_o,
                                     NMClient *self)

libnm/nm-libnm-utils.h
@@ -260,6 +260,8 @@ gpointer nml_dbus_property_o_get_obj (NMLDBusPropertyO *pr_o);
 
 gboolean nml_dbus_property_o_is_ready (const NMLDBusPropertyO *pr_o);
 
+gboolean nml_dbus_property_o_is_ready_fully (const NMLDBusPropertyO *pr_o);
+
 void nml_dbus_property_o_clear (NMLDBusPropertyO *pr_o,
                                 NMClient *client);
 