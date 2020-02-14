9b82d29f5f576da81ad75ac827ebad73499af2dd
Thomas Haller <thaller@redhat.com>
2020-02-14 15:44:44 +0100

    ifcfg-rh: fix potential crash with variadic argument make_ip6_setting()
    It is undefined behavior and can lead to crashes or memory corruption.
    In practice, this only had an issue on Big Endian systems.
    
    Fixes: fdbf4ae5e6ae ('ifcfg-rh: add IPV4_DHCP_TIMEOUT key for ipv4.dhcp-timeout property')

--- src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-reader.c
+++ src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-reader.c
@@ -1734,7 +1734,7 @@ make_ip4_setting (shvarFile *ifcfg,
 
 	g_object_set (s_ip4,
 	              NM_SETTING_IP_CONFIG_DHCP_SEND_HOSTNAME, svGetValueBoolean (ifcfg, "DHCP_SEND_HOSTNAME", TRUE),
-	              NM_SETTING_IP_CONFIG_DHCP_TIMEOUT, svGetValueInt64 (ifcfg, "IPV4_DHCP_TIMEOUT", 10, 0, G_MAXINT32, 0),
+	              NM_SETTING_IP_CONFIG_DHCP_TIMEOUT, (int) svGetValueInt64 (ifcfg, "IPV4_DHCP_TIMEOUT", 10, 0, G_MAXINT32, 0),
 	              NULL);
 
 	nm_clear_g_free (&value);
