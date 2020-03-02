NetworkManager/NetworkManager

commit 190a8ed425c11ccb72d0fc90ab107a23b42fa8db

shared: add nm_ref_string_equals_str() helper

shared/nm-glib-aux/nm-ref-string.h
@@ -50,6 +50,18 @@ nm_ref_string_get_len (NMRefString *rstr)
 }
 
 static inline gboolean
+nm_ref_string_equals_str (NMRefString *rstr, const char *s)
+{
+	/* Note that rstr->len might be greater than strlen(rstr->str). This function does
+	 * not cover that and would ignore everything after the first NUL byte. If you need
+	 * that distinction, this function is not for you. */
+
+	return   rstr
+	       ? nm_streq (rstr->str, s)
+	       : (s == NULL);
+}
+
+static inline gboolean
 NM_IS_REF_STRING (const NMRefString *rstr)
 {
 #if NM_MORE_ASSERTS > 10

commit 8b63fd8cc0c1631ea7c1936f696a0078f4597acf

shared: fix handling %NULL argument in nm_ref_string_equals_str()
Fixes: 190a8ed425c1 ('shared: add nm_ref_string_equals_str() helper')

shared/nm-glib-aux/nm-ref-string.h
@@ -57,7 +57,7 @@ nm_ref_string_equals_str (NMRefString *rstr, const char *s)
 	 * that distinction, this function is not for you. */
 
 	return   rstr
-	       ? nm_streq (rstr->str, s)
+	       ? (s && nm_streq (rstr->str, s))
 	       : (s == NULL);
 }
 