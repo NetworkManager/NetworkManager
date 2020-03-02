NetworkManager/NetworkManager

commit f4d12f7b59b682dbc73af2602f3bb7f5e91417ce

shared: add NM_UTILS_STRING_TABLE_LOOKUP_STRUCT_DEFINE() macro for lookup of structs

shared/nm-glib-aux/nm-shared-utils.h
@@ -1536,35 +1536,32 @@ guint8 *nm_utils_hexstr2bin_alloc (const char *hexstr,
 
 /*****************************************************************************/
 
-#define NM_UTILS_STRING_TABLE_LOOKUP_DEFINE(fcn_name, \
-                                            result_type, \
-                                            entry_cmd, \
-                                            unknown_val_cmd, \
-                                            ...) \
-result_type \
+#define _NM_UTILS_STRING_TABLE_LOOKUP_DEFINE(fcn_name, \
+                                             value_type, \
+                                             value_type_result, \
+                                             entry_cmd, \
+                                             unknown_val_cmd, \
+                                             get_operator, \
+                                             ...) \
+value_type_result \
 fcn_name (const char *name) \
 { \
 	static const struct { \
 		const char *name; \
-		result_type value; \
+		value_type value; \
 	} LIST[] = { \
 		__VA_ARGS__ \
 	}; \
 	\
 	{ entry_cmd; } \
 	\
-	if (NM_MORE_ASSERTS > 5) { \
-		static gboolean checked = FALSE; \
+	if (NM_MORE_ASSERT_ONCE (5)) { \
 		int i; \
 		\
-		if (!checked) { \
-			checked = TRUE; \
-			\
-			for (i = 0; i < G_N_ELEMENTS (LIST); i++) { \
-				nm_assert (LIST[i].name); \
-				if (i > 0) \
-					nm_assert (strcmp (LIST[i - 1].name, LIST[i].name) < 0); \
-			} \
+		for (i = 0; i < G_N_ELEMENTS (LIST); i++) { \
+			nm_assert (LIST[i].name); \
+			if (i > 0) \
+				nm_assert (strcmp (LIST[i - 1].name, LIST[i].name) < 0); \
 		} \
 	} \
 	\
@@ -1579,7 +1576,7 @@ fcn_name (const char *name) \
 			const int cmp = strcmp (LIST[imid].name, name); \
 			\
 			if (G_UNLIKELY (cmp == 0)) \
-				return LIST[imid].value; \
+				return get_operator (LIST[imid].value); \
 			\
 			if (cmp < 0) \
 				imin = imid + 1u; \
@@ -1596,6 +1593,32 @@ fcn_name (const char *name) \
 	\
 	{ unknown_val_cmd; } \
 }
+
+#define NM_UTILS_STRING_TABLE_LOOKUP_STRUCT_DEFINE(fcn_name, \
+                                                   result_type, \
+                                                   entry_cmd, \
+                                                   unknown_val_cmd, \
+                                                   ...) \
+	_NM_UTILS_STRING_TABLE_LOOKUP_DEFINE (fcn_name, \
+	                                      result_type, \
+	                                      const result_type *, \
+	                                      entry_cmd, \
+	                                      unknown_val_cmd, \
+	                                      &, \
+	                                      __VA_ARGS__)
+
+#define NM_UTILS_STRING_TABLE_LOOKUP_DEFINE(fcn_name, \
+                                            result_type, \
+                                            entry_cmd, \
+                                            unknown_val_cmd, \
+                                            ...) \
+	_NM_UTILS_STRING_TABLE_LOOKUP_DEFINE (fcn_name, \
+	                                      result_type, \
+	                                      result_type, \
+	                                      entry_cmd, \
+	                                      unknown_val_cmd, \
+	                                      , \
+	                                      __VA_ARGS__)
 
 /*****************************************************************************/
 

commit 4087024a9b1dcad5c819cde0d2902b94f6beeef4

shared: move assertion in _NM_UTILS_STRING_TABLE_LOOKUP_DEFINE()
Move the assertion for valid LIST first. It only checks static data,
and regardless of the entry_cmd, it should be done first.

Fixes: f4d12f7b59b6 ('shared: add NM_UTILS_STRING_TABLE_LOOKUP_STRUCT_DEFINE() macro for lookup of structs')

shared/nm-glib-aux/nm-shared-utils.h
@@ -1564,8 +1564,6 @@ fcn_name (const char *name) \
 		__VA_ARGS__ \
 	}; \
 	\
-	{ entry_cmd; } \
-	\
 	if (NM_MORE_ASSERT_ONCE (5)) { \
 		int i; \
 		\
@@ -1575,6 +1573,8 @@ fcn_name (const char *name) \
 				nm_assert (strcmp (LIST[i - 1].name, LIST[i].name) < 0); \
 		} \
 	} \
+	\
+	{ entry_cmd; } \
 	\
 	if (G_LIKELY (name)) { \
 		G_STATIC_ASSERT (G_N_ELEMENTS (LIST) > 1); \