NetworkManager/NetworkManager

commit 17d9b852c80ceefa37478eea03e844d0d057c4de

shared: explicitly implement binary search in NM_UTILS_STRING_TABLE_LOOKUP_DEFINE*()
When looking at nm_utils_array_find_binary_search(), we see that binary
search really isn't complicated. In nm_utils_array_find_binary_search()
it looks complicated, because that is our general purpose function which
accepts arbitrary lists, uses an opaque compare function, accepts a user
data argument, and returns the insertion position.

This is unnecessary for the narrow purpose in NM_UTILS_STRING_TABLE_LOOKUP_DEFINE*().
When we inline the binary search, it can be simplified, and the remaining
parts is simple enough to prefer this duplication of binary search over
using our general purpose function.

Also, this gives the compiler more chance for optimization. For
example, we can use "unsigned" as index type instead of gssize, because
we know (at compile time), that this type will always be large enough
for our LIST. Also, we can directly call strcmp().

The result is that the macro's implementation is also fast in the best
case (where the needle is found with only one strcmp()) and in the cases
where there is a small number of items to search.
It thus alleviates concerns that using the macro might be slower than
an optimized implementation.

The binary size of the defined function increases slightly (from 112
bytes to 192 bytes, on x86_64, GCC, -O2). But according to my tests it
is slightly and measurably faster.

shared/nm-glib-aux/nm-shared-utils.h
@@ -1565,8 +1565,6 @@ fcn_name (const char *name) \
 		static gboolean checked = FALSE; \
 		int i; \
 		\
-		G_STATIC_ASSERT (G_N_ELEMENTS (LIST) > 1); \
-		\
 		if (!checked) { \
 			checked = TRUE; \
 			\
@@ -1579,16 +1577,29 @@ fcn_name (const char *name) \
 	} \
 	\
 	if (G_LIKELY (name)) { \
-		gssize idx; \
+		G_STATIC_ASSERT (G_N_ELEMENTS (LIST) > 1); \
+		G_STATIC_ASSERT (G_N_ELEMENTS (LIST) < G_MAXUINT / 2u - 10u); \
+		unsigned imin = 0; \
+		unsigned imax = (G_N_ELEMENTS (LIST) - 1); \
+		unsigned imid = (G_N_ELEMENTS (LIST) - 1) / 2; \
 		\
-		idx = nm_utils_array_find_binary_search (LIST, \
-		                                         sizeof (LIST[0]), \
-		                                         G_N_ELEMENTS (LIST), \
-		                                         &name, \
-		                                         nm_strcmp_p_with_data, \
-		                                         NULL); \
-		if (G_LIKELY (idx >= 0)) \
-			return LIST[idx].value; \
+		for (;;) { \
+			const int cmp = strcmp (LIST[imid].name, name); \
+			\
+			if (G_UNLIKELY (cmp == 0)) \
+				return LIST[imid].value; \
+			\
+			if (cmp < 0) \
+				imin = imid + 1u; \
+			else \
+				imax = imid - 1u; \
+			\
+			if (G_UNLIKELY (imin > imax)) \
+				break; \
+			\
+			/* integer overflow cannot happen, because LIST is shorter than G_MAXUINT/2. */ \
+			imid = (imin + imax) / 2u;\
+		} \
 	} \
 	\
 	{ unknown_val_cmd; } \

commit be8be0f0918e62e72d37d79e8ae095bbf7c08710

shared: fix crash in _NM_UTILS_STRING_TABLE_LOOKUP_DEFINE()
If you have a LIST with 7 elements, and you lookup a value that
is not in the (sorted) list and would lie before the first element,
the binary search will dig down to imin=0, imid=0, imax=0 and
strcmp will give positive cmp value (indicating that the searched
value is sorted before).

Then, we would do "imax = imid - 1;", which wrapped to G_MAXUINT,
and the following "if (G_UNLIKELY (imin > imax))" would not hit,
resulting in an out of bound access next.

The easy fix is to not used unsigned integers.

The binary search was adapted from nm_utils_array_find_binary_search()
and nm_utils_ptrarray_find_binary_search(), which already used signed
integers to avoid this problem.

Fixes: 17d9b852c80c ('shared: explicitly implement binary search in NM_UTILS_STRING_TABLE_LOOKUP_DEFINE*()')

shared/nm-glib-aux/nm-shared-utils.h
@@ -1736,10 +1736,10 @@ fcn_name (const char *name) \
 	\
 	if (G_LIKELY (name)) { \
 		G_STATIC_ASSERT (G_N_ELEMENTS (LIST) > 1); \
-		G_STATIC_ASSERT (G_N_ELEMENTS (LIST) < G_MAXUINT / 2u - 10u); \
-		unsigned imin = 0; \
-		unsigned imax = (G_N_ELEMENTS (LIST) - 1); \
-		unsigned imid = (G_N_ELEMENTS (LIST) - 1) / 2; \
+		G_STATIC_ASSERT (G_N_ELEMENTS (LIST) < G_MAXINT / 2 - 10); \
+		int imin = 0; \
+		int imax = (G_N_ELEMENTS (LIST) - 1); \
+		int imid = (G_N_ELEMENTS (LIST) - 1) / 2; \
 		\
 		for (;;) { \
 			const int cmp = strcmp (LIST[imid].name, name); \
@@ -1748,15 +1748,15 @@ fcn_name (const char *name) \
 				return get_operator (LIST[imid].value); \
 			\
 			if (cmp < 0) \
-				imin = imid + 1u; \
+				imin = imid + 1; \
 			else \
-				imax = imid - 1u; \
+				imax = imid - 1; \
 			\
 			if (G_UNLIKELY (imin > imax)) \
 				break; \
 			\
-			/* integer overflow cannot happen, because LIST is shorter than G_MAXUINT/2. */ \
-			imid = (imin + imax) / 2u;\
+			/* integer overflow cannot happen, because LIST is shorter than G_MAXINT/2. */ \
+			imid = (imin + imax) / 2;\
 		} \
 	} \
 	\