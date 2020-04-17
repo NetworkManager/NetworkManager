NetworkManager/NetworkManager

commit 81e6fe963e42bf81704d11f4725f05cd7b6cf497

ifcfg-rh: add functions to detect well-known ifcfg-rh keys
This adds a lot of meta-data about how we handle ifcfg-rh.

We will use this to prune/delete all variables that are not explicitly
set (dirty) but also well-known.

We could now easily emit a warning when an ifcfg-rh file contains
unused key.

We also could add more meta-data for each key. For example, we write
different files (ifcfg- and keys- files). We could add flags to indicate
that variables are valid in certain files. Currently that's not done.
Also, for simple properties we could associate the key with the
NMSetting property, and treat does generically, like keyfile does.

Anyway, there are potentials. For now, we will use this to clear dirty
variables.

src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-utils.h
@@ -11,6 +11,45 @@
 
 #include "shvar.h"
 
+/*****************************************************************************/
+
+typedef enum {
+	NMS_IFCFG_KEY_TYPE_UNKNOWN         = 0,
+	NMS_IFCFG_KEY_TYPE_WELL_KNOWN      = (1u << 0),
+
+	NMS_IFCFG_KEY_TYPE_IS_PLAIN        = (1u << 1),
+	NMS_IFCFG_KEY_TYPE_IS_NUMBERED     = (1u << 2),
+	NMS_IFCFG_KEY_TYPE_IS_PREFIX       = (1u << 3),
+
+	/* by default, well knowns keys that are not explicitly set
+	 * by the writer (the unvisited, dirty ones) are removed.
+	 * With this flag, such keys are kept if they are present. */
+	NMS_IFCFG_KEY_TYPE_KEEP_WHEN_DIRTY = (1u << 4),
+
+} NMSIfcfgKeyTypeFlags;
+
+typedef struct {
+	const char *key_name;
+	NMSIfcfgKeyTypeFlags key_flags;
+} NMSIfcfgKeyTypeInfo;
+
+const NMSIfcfgKeyTypeInfo nms_ifcfg_well_known_keys[225];
+
+const NMSIfcfgKeyTypeInfo *nms_ifcfg_well_known_key_find_info (const char *key, gssize *out_idx);
+
+static inline NMSIfcfgKeyTypeFlags
+nms_ifcfg_well_known_key_find_info_flags (const char *key)
+{
+	const NMSIfcfgKeyTypeInfo *ti;
+
+	ti = nms_ifcfg_well_known_key_find_info (key, NULL);
+	if (!ti)
+		return NMS_IFCFG_KEY_TYPE_UNKNOWN;
+	return ti->key_flags;
+}
+
+/*****************************************************************************/
+
 gboolean nms_ifcfg_rh_utils_parse_unhandled_spec (const char *unhandled_spec,
                                                   const char **out_unmanaged_spec,
                                                   const char **out_unrecognized_spec);
@@ -51,6 +90,12 @@ _nms_ifcfg_rh_utils_numbered_tag (char *buf, gsize buf_len, const char *tag_name
 {
 	gsize l;
 
+#if NM_MORE_ASSERTS > 5
+	nm_assert (NM_FLAGS_ALL (nms_ifcfg_well_known_key_find_info_flags (tag_name),
+	                           NMS_IFCFG_KEY_TYPE_WELL_KNOWN
+	                         | NMS_IFCFG_KEY_TYPE_IS_NUMBERED));
+#endif
+
 	l = g_strlcpy (buf, tag_name, buf_len);
 	nm_assert (l < buf_len);
 	if (which != -1) {
@@ -87,6 +132,10 @@ nms_ifcfg_rh_utils_is_numbered_tag (const char *key,
 
 #define NMS_IFCFG_RH_UTIL_IS_NUMBERED_TAG(key, tag, out_idx) \
 	nms_ifcfg_rh_utils_is_numbered_tag_impl (key, tag, NM_STRLEN (tag), out_idx)
+
+/*****************************************************************************/
+
+const NMSIfcfgKeyTypeInfo *nms_ifcfg_rh_utils_is_well_known_key (const char *key);
 
 /*****************************************************************************/
 

src/settings/plugins/ifcfg-rh/shvar.c
@@ -62,6 +62,19 @@ struct _shvarFile {
 
 /*****************************************************************************/
 
+#define ASSERT_key_is_well_known(key) \
+	nm_assert ( ({ \
+		const char *_key = (key); \
+		gboolean _is_wellknown = TRUE; \
+		\
+		if (!nms_ifcfg_rh_utils_is_well_known_key (_key)) { \
+			_is_wellknown = FALSE; \
+			g_critical ("ifcfg-rh key \"%s\" is not well-known", _key); \
+		} \
+		\
+		_is_wellknown; \
+	}) )
+
 /**
  * svParseBoolean:
  * @value: the input string
@@ -1017,6 +1030,8 @@ _svGetValue (shvarFile *s, const char *key, char **to_free)
 	nm_assert (_shell_is_name (key, -1));
 	nm_assert (to_free);
 
+	ASSERT_key_is_well_known (key);
+
 	line = NULL;
 	c_list_for_each (current, &s->lst_head) {
 		l = c_list_entry (current, shvarLine, lst);
@@ -1251,6 +1266,8 @@ svSetValue (shvarFile *s, const char *key, const char *value)
 	g_return_val_if_fail (key, FALSE);
 
 	nm_assert (_shell_is_name (key, -1));
+
+	ASSERT_key_is_well_known (key);
 
 	line = NULL;
 	c_list_for_each (current, &s->lst_head) {

src/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-wifi-wpa-psk
@@ -16,4 +16,4 @@ CIPHER_GROUP="TKIP CCMP WEP40 WEP104"
 KEY_MGMT=WPA-PSK
 WPA_ALLOW_WPA=yes
 WPA_ALLOW_WPA2=yes
-LAST_ENTRY=no-newline
+CTCPROT=no-newline

src/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-write-unknown-4
@@ -5,32 +5,32 @@
 # don't consider only line-by-line, thus this is
 # expected.
 #
-# Also note that setting NAME will replace the last
+# Also note that setting IPADDR will replace the last
 # occurrence, and delete all previous once.
 
 #L1
-NAME=l2
+IPADDR=l2
 
 #L2
-NAME=l3
+IPADDR=l3
 
-some_key1=''
-some_key2=$'\U0x'
-some_key3=$'x\U0'
+METRIC1=''
+METRIC2=$'\U0x'
+METRIC3=$'x\U0'
 
 #L4
-NAME='
-NAME=l4x
+IPADDR='
+IPADDR=l4x
 '
 
 #Lx-1
-NAME2=not-visible
+IPADDR2=not-visible
 #Lx-2
-NAME2='invalid
+IPADDR2='invalid
 #Lx-3
 
 #Ly-1
-NAME3='invalid
+IPADDR3='invalid
 #Ly-2
-NAME3=name3-value
+IPADDR3=name3-value
 #Ly-3

src/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-write-unknown-4.expected
@@ -5,27 +5,27 @@
 # don't consider only line-by-line, thus this is
 # expected.
 #
-# Also note that setting NAME will replace the last
+# Also note that setting IPADDR will replace the last
 # occurrence, and delete all previous once.
 
 #L1
 
 #L2
 
-some_key1=''
-some_key2=$'\U0x'
-some_key3=$'x\U0'
+METRIC1=''
+METRIC2=$'\U0x'
+METRIC3=$'x\U0'
 
 #L4
-NAME=set-by-test1
+IPADDR=set-by-test1
 #NM: '
 
 #Lx-1
 #Lx-2
-NAME2=set-by-test2
+IPADDR2=set-by-test2
 #Lx-3
 
 #Ly-1
 #Ly-2
-NAME3=set-by-test3
+IPADDR3=set-by-test3
 #Ly-3

src/settings/plugins/ifcfg-rh/tests/test-ifcfg-rh.c
@@ -2868,7 +2868,7 @@ test_ifcfg_no_trailing_newline (void)
 	shvarFile *sv;
 
 	sv = _svOpenFile (TEST_IFCFG_DIR"/ifcfg-test-wifi-wpa-psk");
-	_svGetValue_check (sv, "LAST_ENTRY", "no-newline");
+	_svGetValue_check (sv, "CTCPROT", "no-newline");
 	svCloseFile (sv);
 }
 
@@ -9781,23 +9781,23 @@ test_write_unknown (gconstpointer test_data)
 	_nmtst_svFileSetModified (sv);
 
 	if (g_str_has_suffix (testfile, "ifcfg-test-write-unknown-4")) {
-		_svGetValue_check (sv, "NAME", "l4x");
-		_svGetValue_check (sv, "NAME2", "");
-		_svGetValue_check (sv, "NAME3", "name3-value");
+		_svGetValue_check (sv, "IPADDR", "l4x");
+		_svGetValue_check (sv, "IPADDR2", "");
+		_svGetValue_check (sv, "IPADDR3", "name3-value");
 
-		svSetValue (sv, "NAME", "set-by-test1");
-		svSetValue (sv, "NAME2", NULL);
-		svSetValue (sv, "NAME2", "set-by-test2");
-		svSetValue (sv, "NAME3", "set-by-test3");
+		svSetValue (sv, "IPADDR", "set-by-test1");
+		svSetValue (sv, "IPADDR2", NULL);
+		svSetValue (sv, "IPADDR2", "set-by-test2");
+		svSetValue (sv, "IPADDR3", "set-by-test3");
 
-		_svGetValue_check (sv, "some_key", NULL);
-		_svGetValue_check (sv, "some_key1", "");
-		_svGetValue_check (sv, "some_key2", "");
-		_svGetValue_check (sv, "some_key3", "x");
+		_svGetValue_check (sv, "METRIC", NULL);
+		_svGetValue_check (sv, "METRIC1", "");
+		_svGetValue_check (sv, "METRIC2", "");
+		_svGetValue_check (sv, "METRIC3", "x");
 
-		_svGetValue_check (sv, "NAME", "set-by-test1");
-		_svGetValue_check (sv, "NAME2", "set-by-test2");
-		_svGetValue_check (sv, "NAME3", "set-by-test3");
+		_svGetValue_check (sv, "IPADDR", "set-by-test1");
+		_svGetValue_check (sv, "IPADDR2", "set-by-test2");
+		_svGetValue_check (sv, "IPADDR3", "set-by-test3");
 	}
 
 	success = svWriteFile (sv, 0644, &error);
@@ -10242,6 +10242,69 @@ test_tc_write (void)
 
 /*****************************************************************************/
 
+static void
+test_well_known_keys (void)
+{
+	gsize i;
+
+	for (i = 0; i < G_N_ELEMENTS (nms_ifcfg_well_known_keys); i++) {
+		const NMSIfcfgKeyTypeInfo *ti = &nms_ifcfg_well_known_keys[i];
+
+		g_assert (ti->key_name);
+		g_assert (ti->key_name[0]);
+		g_assert (NM_FLAGS_HAS (ti->key_flags, NMS_IFCFG_KEY_TYPE_WELL_KNOWN));
+		g_assert (nm_utils_is_power_of_two (ti->key_flags & (  NMS_IFCFG_KEY_TYPE_IS_PLAIN
+		                                                     | NMS_IFCFG_KEY_TYPE_IS_NUMBERED
+		                                                     | NMS_IFCFG_KEY_TYPE_IS_PREFIX)));
+	}
+
+	for (i = 1; i < G_N_ELEMENTS (nms_ifcfg_well_known_keys); i++) {
+		const NMSIfcfgKeyTypeInfo *ti_prev = &nms_ifcfg_well_known_keys[i - 1];
+		const NMSIfcfgKeyTypeInfo *ti = &nms_ifcfg_well_known_keys[i];
+
+		g_assert_cmpstr (ti_prev->key_name, <, ti->key_name);
+	}
+
+	for (i = 0; i < G_N_ELEMENTS (nms_ifcfg_well_known_keys); i++) {
+		const NMSIfcfgKeyTypeInfo *ti = &nms_ifcfg_well_known_keys[i];
+		gs_free char *key_name = NULL;
+		gssize idx;
+
+		g_assert (ti == nms_ifcfg_well_known_key_find_info (ti->key_name, &idx));
+		g_assert_cmpint (i, ==, idx);
+
+		key_name = g_strdup (ti->key_name);
+		g_assert (ti == nms_ifcfg_well_known_key_find_info (key_name, &idx));
+		g_assert_cmpint (i, ==, idx);
+	}
+
+#define _test_well_known(key, expected) \
+	G_STMT_START { \
+		const NMSIfcfgKeyTypeInfo *_ti; \
+		const char *_expected = (expected); \
+		\
+		_ti = nms_ifcfg_rh_utils_is_well_known_key (""key""); \
+		if (!_expected) { \
+			g_assert (!_ti); \
+		} else { \
+			g_assert (_ti); \
+			g_assert_cmpstr (_ti->key_name, ==, _expected); \
+		} \
+	} G_STMT_END
+
+#define _test_well_known_plain(key) \
+	_test_well_known (""key"", ""key"")
+
+	_test_well_known_plain ("ONBOOT");
+	_test_well_known ("NM_USER_",  NULL);
+	_test_well_known ("NM_USER_x", "NM_USER_");
+	_test_well_known ("IPADDR",    "IPADDR");
+	_test_well_known ("IPADDR1",   "IPADDR");
+	_test_well_known ("IPADDRx",   NULL);
+}
+
+/*****************************************************************************/
+
 #define TPATH "/settings/plugins/ifcfg-rh/"
 
 #define TEST_IFCFG_WIFI_OPEN_SSID_LONG_QUOTED TEST_IFCFG_DIR"/ifcfg-test-wifi-open-ssid-long-quoted"
@@ -10538,6 +10601,7 @@ int main (int argc, char **argv)
 
 	g_test_add_func (TPATH "tc/read", test_tc_read);
 	g_test_add_func (TPATH "tc/write", test_tc_write);
+	g_test_add_func (TPATH "utils/test_well_known_keys", test_well_known_keys);
 
 	return g_test_run ();
 }

commit 70916a11838077b9e36a29794b95678c001aca95

ifcfg-rh: add PKEY_ID to well-known keys
Fixes: 81e6fe963e42 ('ifcfg-rh: add functions to detect well-known ifcfg-rh keys')

src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-utils.c
@@ -910,6 +910,7 @@ const NMSIfcfgKeyTypeInfo nms_ifcfg_well_known_keys[] = {
 	_KEY_TYPE ("PEERROUTES",                                  NMS_IFCFG_KEY_TYPE_IS_PLAIN ),
 	_KEY_TYPE ("PHYSDEV",                                     NMS_IFCFG_KEY_TYPE_IS_PLAIN ),
 	_KEY_TYPE ("PKEY",                                        NMS_IFCFG_KEY_TYPE_IS_PLAIN ),
+	_KEY_TYPE ("PKEY_ID",                                     NMS_IFCFG_KEY_TYPE_IS_PLAIN ),
 	_KEY_TYPE ("PMF",                                         NMS_IFCFG_KEY_TYPE_IS_PLAIN ),
 	_KEY_TYPE ("PORTNAME",                                    NMS_IFCFG_KEY_TYPE_IS_PLAIN ),
 	_KEY_TYPE ("POWERSAVE",                                   NMS_IFCFG_KEY_TYPE_IS_PLAIN ),

src/settings/plugins/ifcfg-rh/nms-ifcfg-rh-utils.h
@@ -33,7 +33,7 @@ typedef struct {
 	NMSIfcfgKeyTypeFlags key_flags;
 } NMSIfcfgKeyTypeInfo;
 
-extern const NMSIfcfgKeyTypeInfo nms_ifcfg_well_known_keys[231];
+extern const NMSIfcfgKeyTypeInfo nms_ifcfg_well_known_keys[232];
 
 const NMSIfcfgKeyTypeInfo *nms_ifcfg_well_known_key_find_info (const char *key, gssize *out_idx);
 