NetworkManager/NetworkManager

commit cedcea5ee812ce99a62d47b400518401e018bf74

libnm: fix verification of connection:mud-url property
For one, the setters sd_dhcp_client_set_mud_url() and sd_dhcp6_client_set_request_mud_url()
assert that the value honors these settings. So, we must never pass such values to the
function. Also, before calling n_dhcp4_client_probe_config_append_option()
the code doesn't check whether the URL is short enough. That would be
a bug (unless we ensure that the property is valid from the beginning).

In general, it is necessary to strictly validate the parameter.

Also, returning NM_SETTING_VERIFY_NORMALIZABLE_ERROR for a property that does
not get normalized is a bug.

libnm-core/nm-setting-connection.c
@@ -16,6 +16,7 @@
 #include "nm-setting-bridge.h"
 #include "nm-setting-team.h"
 #include "nm-setting-vlan.h"
+#include "systemd/nm-sd-utils-shared.h"
 
 /**
  * SECTION:nm-setting-connection
@@ -1230,6 +1231,27 @@ after_interface_name:
 		return FALSE;
 	}
 
+	if (priv->mud_url) {
+		if (!priv->mud_url[0]) {
+			g_set_error_literal (error, NM_CONNECTION_ERROR, NM_CONNECTION_ERROR_INVALID_PROPERTY,
+			                     _("property is empty"));
+			g_prefix_error (error, "%s.%s: ", nm_setting_get_name (setting), NM_SETTING_CONNECTION_MUD_URL);
+			return FALSE;
+		}
+		if (strlen (priv->mud_url) > 255) {
+			g_set_error_literal (error, NM_CONNECTION_ERROR, NM_CONNECTION_ERROR_INVALID_PROPERTY,
+			                     _("DHCP option cannot be longer than 255 characters"));
+			g_prefix_error (error, "%s.%s: ", nm_setting_get_name (setting), NM_SETTING_CONNECTION_MUD_URL);
+			return FALSE;
+		}
+		if (!nm_sd_http_url_is_valid (priv->mud_url)) {
+			g_set_error_literal (error, NM_CONNECTION_ERROR, NM_CONNECTION_ERROR_INVALID_PROPERTY,
+			                     _("MUD URL is not a valid URL"));
+			g_prefix_error (error, "%s.%s: ", nm_setting_get_name (setting), NM_SETTING_CONNECTION_MUD_URL);
+			return FALSE;
+		}
+	}
+
 	/* *** errors above here should be always fatal, below NORMALIZABLE_ERROR *** */
 
 	if (!priv->uuid) {
@@ -1238,13 +1260,6 @@ after_interface_name:
 		                     NM_CONNECTION_ERROR_MISSING_PROPERTY,
 		                     _("property is missing"));
 		g_prefix_error (error, "%s.%s: ", NM_SETTING_CONNECTION_SETTING_NAME, NM_SETTING_CONNECTION_UUID);
-		return NM_SETTING_VERIFY_NORMALIZABLE_ERROR;
-	}
-
-	if (priv->mud_url && !*priv->mud_url) {
-		g_set_error_literal (error, NM_CONNECTION_ERROR, NM_CONNECTION_ERROR_INVALID_PROPERTY,
-		                     _("property is empty"));
-		g_prefix_error (error, "%s.%s: ", nm_setting_get_name (setting), NM_SETTING_CONNECTION_MUD_URL);
 		return NM_SETTING_VERIFY_NORMALIZABLE_ERROR;
 	}
 
@@ -1493,8 +1508,8 @@ get_property (GObject *object, guint prop_id,
 	case PROP_WAIT_DEVICE_TIMEOUT:
 		g_value_set_int (value, priv->wait_device_timeout);
 		break;
-        case PROP_MUD_URL:
-		g_value_set_string (value, nm_setting_connection_get_mud_url(setting));
+	case PROP_MUD_URL:
+		g_value_set_string (value, priv->mud_url);
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);

commit dec1678fecadfcbf25ed21e4929bfd36d8008f66

dhcp: enforce MUD URL to use "https://" scheme
nm_sd_http_url_is_valid_https() is rather clunky, but it is
this way, because we must not disagree with systemd code
about what makes a valid URL.

RFC 8520 says "MUD URLs MUST use the "https" scheme".

See-also: https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/463#note_476190

Fixes: cedcea5ee812 ('libnm: fix verification of connection:mud-url property')

libnm-core/nm-setting-connection.c
@@ -1244,7 +1244,7 @@ after_interface_name:
 			g_prefix_error (error, "%s.%s: ", nm_setting_get_name (setting), NM_SETTING_CONNECTION_MUD_URL);
 			return FALSE;
 		}
-		if (!nm_sd_http_url_is_valid (priv->mud_url)) {
+		if (!nm_sd_http_url_is_valid_https (priv->mud_url)) {
 			g_set_error_literal (error, NM_CONNECTION_ERROR, NM_CONNECTION_ERROR_INVALID_PROPERTY,
 			                     _("MUD URL is not a valid URL"));
 			g_prefix_error (error, "%s.%s: ", nm_setting_get_name (setting), NM_SETTING_CONNECTION_MUD_URL);

shared/systemd/nm-sd-utils-shared.c
@@ -99,13 +99,14 @@ gboolean nm_sd_hostname_is_valid (const char *s, bool allow_trailing_dot)
 /*****************************************************************************/
 
 static gboolean
-_http_url_is_valid (const char *url)
+_http_url_is_valid (const char *url, gboolean only_https)
 {
 	if (   !url
 	    || !url[0])
 		return FALSE;
 
-	if (NM_STR_HAS_PREFIX (url, "http://"))
+	if (   !only_https
+	    && NM_STR_HAS_PREFIX (url, "http://"))
 		url += NM_STRLEN ("http://");
 	else if (NM_STR_HAS_PREFIX (url, "https://"))
 		url += NM_STRLEN ("https://");
@@ -119,16 +120,20 @@ _http_url_is_valid (const char *url)
 }
 
 gboolean
-nm_sd_http_url_is_valid (const char *url)
+nm_sd_http_url_is_valid_https (const char *url)
 {
-	gboolean v;
-
-	/* http_url_is_valid() is part of our API, as we use it to validate connection
-	 * properties. That means, it's behavior must remain stable (or only change
-	 * with care).
+	/* We use this function to verify connection:mud-url property, it must thus
+	 * not change behavior.
 	 *
-	 * Thus, reimplement it, and make sure that our implementation agrees. */
-	v = _http_url_is_valid (url);
-	nm_assert (v == http_url_is_valid (url));
-	return v;
+	 * Note that sd_dhcp_client_set_mud_url() and sd_dhcp6_client_set_request_mud_url()
+	 * assert with http_url_is_valid() that the argument is valid. We thus must make
+	 * sure to only pass URLs that are valid according to http_url_is_valid().
+	 *
+	 * This is given, because our nm_sd_http_url_is_valid_https() is more strict
+	 * than http_url_is_valid().
+	 *
+	 * We only must make sure that this is also correct in the future, when we
+	 * re-import systemd code. */
+	nm_assert (_http_url_is_valid (url, FALSE) == http_url_is_valid (url));
+	return _http_url_is_valid (url, TRUE);
 }

shared/systemd/nm-sd-utils-shared.h
@@ -36,6 +36,6 @@ gboolean nm_sd_hostname_is_valid(const char *s, bool allow_trailing_dot);
 
 /*****************************************************************************/
 
-gboolean nm_sd_http_url_is_valid (const char *url);
+gboolean nm_sd_http_url_is_valid_https (const char *url);
 
 #endif /* __NM_SD_UTILS_SHARED_H__ */