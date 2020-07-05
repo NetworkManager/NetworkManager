19613016e23fc891fb4d993e5d75a919a3b8a179
Thomas Haller <thaller@redhat.com>
2020-07-05 10:16:54 +0200

    shared: add "nm-std-aux/nm-std-utils.[hc]"
    General purpose utilities for nm-std-aux. Contrary to
    "nm-std-aux.h", this is not header only.

---
+++ shared/nm-std-aux/nm-std-utils.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: LGPL-2.1+
+
+#include "nm-default.h"
+
+#include "nm-std-utils.h"
+
---
+++ shared/nm-std-aux/nm-std-utils.h
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: LGPL-2.1+
+
+#ifndef __NM_STD_UTILS_H__
+#define __NM_STD_UTILS_H__
+
+#endif /* __NM_STD_UTILS_H__ */
