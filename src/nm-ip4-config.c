NetworkManager/NetworkManager

commit 5d0d13f57010b4eadcd547a29ec5a1f88ac07668

platform: add support for local routes
Also update unit tests.

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/407
https://bugzilla.redhat.com/show_bug.cgi?id=1821787

src/nm-ip4-config.c
@@ -860,8 +860,26 @@ _nm_ip_config_merge_route_attributes (int addr_family,
 			(dst) = (dflt); \
 	} G_STMT_END
 
+	if (   (variant = nm_ip_route_get_attribute (s_route, NM_IP_ROUTE_ATTRIBUTE_TYPE))
+	    && g_variant_is_of_type (variant, G_VARIANT_TYPE_STRING)) {
+		guint8 type;
+
+		type = nm_utils_route_type_by_name (g_variant_get_string (variant, NULL));
+		nm_assert (NM_IN_SET (type,
+		                      RTN_UNICAST,
+		                      RTN_LOCAL));
+
+		r->type_coerced = nm_platform_route_type_coerce (type);
+	} else
+		r->type_coerced = nm_platform_route_type_coerce (RTN_UNICAST);
+
 	GET_ATTR (NM_IP_ROUTE_ATTRIBUTE_TABLE, table, UINT32, uint32, 0);
-	r->table_coerced = nm_platform_route_table_coerce (table ?: (route_table ?: RT_TABLE_MAIN));
+
+	if (   !table
+	    && r->type_coerced == nm_platform_route_type_coerce (RTN_LOCAL))
+		r->table_coerced = nm_platform_route_table_coerce (RT_TABLE_LOCAL);
+	else
+		r->table_coerced = nm_platform_route_table_coerce (table ?: (route_table ?: RT_TABLE_MAIN));
 
 	if (addr_family == AF_INET) {
 		guint8 scope;

libnm-core/nm-core-internal.h
@@ -347,7 +347,7 @@ const char **_nm_ip_address_get_attribute_names (const NMIPAddress *addr, gboole
 
 void _nm_setting_wired_clear_s390_options (NMSettingWired *setting);
 
-gboolean _nm_ip_route_attribute_validate_all (const NMIPRoute *route);
+gboolean _nm_ip_route_attribute_validate_all (const NMIPRoute *route, GError **error);
 const char **_nm_ip_route_get_attribute_names (const NMIPRoute *route, gboolean sorted, guint *out_length);
 GHashTable *_nm_ip_route_get_attributes (NMIPRoute *route);
 

libnm-core/nm-libnm-core-intern/nm-libnm-core-utils.c
@@ -6,6 +6,8 @@
 
 #include "nm-common-macros.h"
 
+#include <linux/rtnetlink.h>
+
 /*****************************************************************************/
 
 gboolean
@@ -178,3 +180,31 @@ nm_client_permission_result_to_string (NMClientPermissionResult permission)
 	nm_assert_not_reached ();
 	return NULL;
 }
+
+NM_UTILS_STRING_TABLE_LOOKUP_DEFINE (
+	nm_utils_route_type_by_name,
+	guint8,
+	{ nm_assert (name); },
+	{ return RTN_UNSPEC; },
+	{ "blackhole",   RTN_BLACKHOLE   },
+	{ "broadcast",   RTN_BROADCAST   },
+	{ "local",       RTN_LOCAL       },
+	{ "multicast",   RTN_MULTICAST   },
+	{ "nat",         RTN_NAT         },
+	{ "prohibit",    RTN_PROHIBIT    },
+	{ "throw",       RTN_THROW       },
+	{ "unicast",     RTN_UNICAST     },
+	{ "unreachable", RTN_UNREACHABLE },
+);
+
+NM_UTILS_ENUM2STR_DEFINE (nm_utils_route_type2str, guint8,
+	NM_UTILS_ENUM2STR (RTN_BLACKHOLE, "blackhole"),
+	NM_UTILS_ENUM2STR (RTN_BROADCAST, "broadcast"),
+	NM_UTILS_ENUM2STR (RTN_LOCAL, "local"),
+	NM_UTILS_ENUM2STR (RTN_MULTICAST, "multicast"),
+	NM_UTILS_ENUM2STR (RTN_NAT, "nat"),
+	NM_UTILS_ENUM2STR (RTN_PROHIBIT, "prohibit"),
+	NM_UTILS_ENUM2STR (RTN_THROW, "throw"),
+	NM_UTILS_ENUM2STR (RTN_UNICAST, "unicast"),
+	NM_UTILS_ENUM2STR (RTN_UNREACHABLE, "unreachable"),
+);

libnm-core/nm-libnm-core-intern/nm-libnm-core-utils.h
@@ -112,4 +112,8 @@ NMClientPermission nm_auth_permission_from_string (const char *str);
 NMClientPermissionResult nm_client_permission_result_from_string (const char *nm);
 const char *nm_client_permission_result_to_string (NMClientPermissionResult permission);
 
+guint8 nm_utils_route_type_by_name (const char *name);
+
+const char *nm_utils_route_type2str (guint8 val, char *buf, gsize len);
+
 #endif /* __NM_LIBNM_SHARED_UTILS_H__ */

libnm-core/nm-setting-ip-config.c
@@ -1214,6 +1214,7 @@ static const NMVariantAttributeSpec *const ip_route_attribute_spec[] = {
 	NM_VARIANT_ATTRIBUTE_SPEC_DEFINE (NM_IP_ROUTE_ATTRIBUTE_SRC,           G_VARIANT_TYPE_STRING,  .v4 = TRUE, .v6 = TRUE, .str_type = 'a', ),
 	NM_VARIANT_ATTRIBUTE_SPEC_DEFINE (NM_IP_ROUTE_ATTRIBUTE_TABLE,         G_VARIANT_TYPE_UINT32,  .v4 = TRUE, .v6 = TRUE,                  ),
 	NM_VARIANT_ATTRIBUTE_SPEC_DEFINE (NM_IP_ROUTE_ATTRIBUTE_TOS,           G_VARIANT_TYPE_BYTE,    .v4 = TRUE,                              ),
+	NM_VARIANT_ATTRIBUTE_SPEC_DEFINE (NM_IP_ROUTE_ATTRIBUTE_TYPE,          G_VARIANT_TYPE_STRING,  .v4 = TRUE, .v6 = TRUE, .str_type = 'T', ),
 	NM_VARIANT_ATTRIBUTE_SPEC_DEFINE (NM_IP_ROUTE_ATTRIBUTE_WINDOW,        G_VARIANT_TYPE_UINT32,  .v4 = TRUE, .v6 = TRUE,                  ),
 	NULL,
 };
@@ -1339,6 +1340,18 @@ nm_ip_route_attribute_validate  (const char *name,
 			}
 			break;
 		}
+		case 'T': /* route type. */
+			if (!NM_IN_SET (nm_utils_route_type_by_name (string),
+			                RTN_UNICAST,
+			                RTN_LOCAL)) {
+				g_set_error (error,
+				             NM_CONNECTION_ERROR,
+				             NM_CONNECTION_ERROR_INVALID_PROPERTY,
+				             _("%s is not a valid route type"),
+				             string);
+				return FALSE;
+			}
+			break;
 		default:
 			break;
 		}
@@ -1348,22 +1361,48 @@ nm_ip_route_attribute_validate  (const char *name,
 }
 
 gboolean
-_nm_ip_route_attribute_validate_all (const NMIPRoute *route)
+_nm_ip_route_attribute_validate_all (const NMIPRoute *route, GError **error)
 {
 	GHashTableIter iter;
 	const char *key;
 	GVariant *val;
+	guint8 u8;
 
 	g_return_val_if_fail (route, FALSE);
+	g_return_val_if_fail (!error || !*error, FALSE);
 
 	if (!route->attributes)
 		return TRUE;
 
 	g_hash_table_iter_init (&iter, route->attributes);
 	while (g_hash_table_iter_next (&iter, (gpointer *) &key, (gpointer *) &val)) {
-		if (!nm_ip_route_attribute_validate (key, val, route->family, NULL, NULL))
+		if (!nm_ip_route_attribute_validate (key, val, route->family, NULL, error))
 			return FALSE;
 	}
+
+	if ((val = g_hash_table_lookup (route->attributes,
+	                                NM_IP_ROUTE_ATTRIBUTE_TYPE))) {
+		nm_assert (g_variant_is_of_type (val, G_VARIANT_TYPE_STRING));
+		u8 = nm_utils_route_type_by_name (g_variant_get_string (val, NULL));
+
+		if (   u8 == RTN_LOCAL
+		    && route->family == AF_INET
+		    && (val = g_hash_table_lookup (route->attributes, NM_IP_ROUTE_ATTRIBUTE_SCOPE))) {
+			nm_assert (g_variant_is_of_type (val, G_VARIANT_TYPE_BYTE));
+			u8 = g_variant_get_byte (val);
+
+			if (!NM_IN_SET(u8,
+			               RT_SCOPE_HOST,
+			               RT_SCOPE_NOWHERE)) {
+				g_set_error (error,
+				             NM_CONNECTION_ERROR,
+				             NM_CONNECTION_ERROR_INVALID_PROPERTY,
+				             _("route scope is invalid"));
+				return FALSE;
+			}
+		}
+	}
+
 	return TRUE;
 }
 

libnm-core/nm-setting-ip-config.h
@@ -164,6 +164,7 @@ gboolean     nm_ip_route_attribute_validate  (const char *name,
 #define NM_IP_ROUTE_ATTRIBUTE_SRC            "src"
 #define NM_IP_ROUTE_ATTRIBUTE_TABLE          "table"
 #define NM_IP_ROUTE_ATTRIBUTE_TOS            "tos"
+#define NM_IP_ROUTE_ATTRIBUTE_TYPE           "type"
 #define NM_IP_ROUTE_ATTRIBUTE_WINDOW         "window"
 
 /*****************************************************************************/

libnm-core/tests/test-general.c
@@ -2020,6 +2020,11 @@ test_setting_ip_route_attributes (void)
 	TEST_ATTR ("src", string, "1.2.3.0/24", AF_INET,  FALSE, TRUE);
 	TEST_ATTR ("src", string, "fd01::12",   AF_INET6, TRUE,  TRUE);
 
+	TEST_ATTR ("type", string, "local", AF_INET, TRUE, TRUE);
+	TEST_ATTR ("type", string, "local", AF_INET6, TRUE, TRUE);
+	TEST_ATTR ("type", string, "unicast", AF_INET, TRUE, TRUE);
+	TEST_ATTR ("type", string, "unicast", AF_INET6, TRUE, TRUE);
+
 #undef TEST_ATTR
 }
 

src/platform/nm-linux-platform.c
@@ -3257,14 +3257,16 @@ _new_from_nl_route (struct nlmsghdr *nlh, gboolean id_only)
 	rtm = nlmsg_data (nlh);
 
 	/*****************************************************************
-	 * only handle ~normal~ routes.
+	 * only handle ~supported~ routes.
 	 *****************************************************************/
 
 	if (!NM_IN_SET (rtm->rtm_family, AF_INET, AF_INET6))
 		return NULL;
 
-	if (rtm->rtm_type != RTN_UNICAST)
-		return NULL;
+	if (!NM_IN_SET (rtm->rtm_type,
+	                RTN_UNICAST,
+	                RTN_LOCAL))
+	    return NULL;
 
 	if (nlmsg_parse_arr (nlh,
 	                     sizeof (struct rtmsg),
@@ -4491,7 +4493,7 @@ _nl_msg_new_route (int nlmsg_type,
 		.rtm_scope = is_v4
 		             ? nm_platform_route_scope_inv (obj->ip4_route.scope_inv)
 		             : RT_SCOPE_NOWHERE,
-		.rtm_type = RTN_UNICAST,
+		.rtm_type = nm_platform_route_type_uncoerce (NMP_OBJECT_CAST_IP_ROUTE (obj)->type_coerced),
 		.rtm_flags = obj->ip_route.r_rtm_flags & ((unsigned) (RTNH_F_ONLINK)),
 		.rtm_dst_len = obj->ip_route.plen,
 		.rtm_src_len = is_v4

commit 5035687a7be4c67e32b4e08319f2e66c76b42212

core: only expose "type unicast" routes on D-Bus
Currently, we would not mark non-unicast routes with their type, so they
would wrongly appear as unicast routes in the D-Bus API.

That is wrong. For now, just hide them.

Fixes: 5d0d13f57010 ('platform: add support for local routes')

src/nm-ip4-config.c
@@ -3227,6 +3227,9 @@ out_addresses_cached:
 
 			nm_assert (_route_valid (route));
 
+			if (route->type_coerced != nm_platform_route_type_coerce (RTN_UNICAST))
+				continue;
+
 			g_variant_builder_init (&route_builder, G_VARIANT_TYPE ("a{sv}"));
 			g_variant_builder_add (&route_builder, "{sv}",
 			                       "dest",

src/nm-ip6-config.c
@@ -2682,6 +2682,9 @@ out_addresses_cached:
 
 			nm_assert (_route_valid (route));
 
+			if (route->type_coerced != nm_platform_route_type_coerce (RTN_UNICAST))
+				continue;
+
 			g_variant_builder_init (&route_builder, G_VARIANT_TYPE ("a{sv}"));
 			g_variant_builder_add (&route_builder, "{sv}",
 			                       "dest",