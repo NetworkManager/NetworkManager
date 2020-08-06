d7ff226ceb6044445788c403686f5a4262d4ebae
Beniamino Galvani <bgalvani@redhat.com>
2020-08-06 10:49:07 +0200

    systemd: dhcp6: parse the FQDN option
    Parse option 39 (Client Fully Qualified Domain Name, RFC 4704) from the DHCP
    reply, which specifies the FQDN assigned by the server to the client.
    
    https://github.com/systemd/systemd/commit/c43eea9f2effbb066901a61eafef473558d37b0f
    (cherry picked from commit 813fb7d64ee4cb0f935a3a15b9f5b8f5771655da)
    (cherry picked from commit 504a3486ed05f1d1a50fb3338f0617f7386a9359)

--- src/systemd/src/systemd/sd-dhcp6-lease.h
+++ src/systemd/src/systemd/sd-dhcp6-lease.h
@@ -44,6 +44,7 @@ int sd_dhcp6_lease_get_domains(sd_dhcp6_lease *lease, char ***domains);
 int sd_dhcp6_lease_get_ntp_addrs(sd_dhcp6_lease *lease,
                                  struct in6_addr **addrs);
 int sd_dhcp6_lease_get_ntp_fqdn(sd_dhcp6_lease *lease, char ***ntp_fqdn);
+int sd_dhcp6_lease_get_fqdn(sd_dhcp6_lease *lease, const char **fqdn);
 
 sd_dhcp6_lease *sd_dhcp6_lease_ref(sd_dhcp6_lease *lease);
 sd_dhcp6_lease *sd_dhcp6_lease_unref(sd_dhcp6_lease *lease);
