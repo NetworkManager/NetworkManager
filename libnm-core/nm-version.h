00132cecb59f4931b4c5a86e23b979d0a0dc7e51
Antonio Cardace <acardace@redhat.com>
2020-08-26 11:24:45 +0200

    version: add NM_VERSION_1_26_4/NM_AVAILABLE_IN_1_26_4 macros
    Signed-off-by: Antonio Cardace <acardace@redhat.com>

--- libnm-core/nm-version.h
+++ libnm-core/nm-version.h
@@ -243,6 +243,12 @@
 # define NM_AVAILABLE_IN_1_26
 #endif
 
+#if NM_VERSION_MAX_ALLOWED < NM_VERSION_1_26_4
+# define NM_AVAILABLE_IN_1_26_4          G_UNAVAILABLE(1,26.4)
+#else
+# define NM_AVAILABLE_IN_1_26_4
+#endif
+
 /*
  * Synchronous API for calling D-Bus in libnm is deprecated. See
  * https://developer.gnome.org/libnm/stable/usage.html#sync-api
