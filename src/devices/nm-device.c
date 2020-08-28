c24888be5113f459a2d0af6e49998b52b2b7196c
Thomas Haller <thaller@redhat.com>
2020-08-28 12:35:43 +0200

    device: fix casting pointer to enum for sriov_reset_on_deactivate_cb()
    Avoids a compiler warning:
    
        ../src/devices/nm-device.c:16079:26: error: cast to smaller integer type 'NMDeviceStateReason' from 'gpointer' (aka 'void *') [-Werror,-Wvoid-pointer-to-enum-cast]
                deactivate_ready (self, (NMDeviceStateReason) reason);
                                        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Fixes: 121c58f0c48d ('core: set number of SR-IOV VFs asynchronously')
    (cherry picked from commit 918ebd600acba97ea64af7d1039339983fa44af4)
    (cherry picked from commit 32641b9fca45f29372d76f06fcb9330e14820b15)

--- src/devices/nm-device.c
+++ src/devices/nm-device.c
@@ -15669,7 +15669,7 @@ sriov_deactivate_cb (GError *error, gpointer user_data)
 	if (nm_utils_error_is_cancelled_or_disposing (error))
 		return;
 
-	deactivate_ready (self, (NMDeviceStateReason) reason);
+	deactivate_ready (self, GPOINTER_TO_INT (reason));
 }
 
 static void
@@ -16002,7 +16002,7 @@ _set_state_full (NMDevice *self,
 				                0,
 				                NM_TERNARY_TRUE,
 				                sriov_deactivate_cb,
-				                nm_utils_user_data_pack (self, (gpointer) reason));
+				                nm_utils_user_data_pack (self, GINT_TO_POINTER (reason)));
 			}
 		}
 
