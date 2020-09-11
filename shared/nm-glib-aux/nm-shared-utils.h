NetworkManager/NetworkManager

commit fb6e9795b7cf8a2d3e94a3460c009a2451d18db8

shared: add nm_g_array_append_new() helper

shared/nm-glib-aux/nm-shared-utils.h
@@ -1519,6 +1519,18 @@ nm_g_array_len (const GArray *arr)
 	return arr ? arr->len : 0u;
 }
 
+#define nm_g_array_append_new(arr, type) \
+	({ \
+		GArray *_arr = (arr); \
+		gsize _l; \
+		\
+		nm_assert (_arr); \
+		_l = ((gsize) _arr->len) + 1u; \
+		nm_assert (_l > _arr->len); \
+		g_array_set_size (_arr, _l); \
+		&g_array_index (arr, type, _l); \
+	})
+
 /*****************************************************************************/
 
 static inline guint

commit ff37c961ff1006a40c843ed64129557cb4194fd6

shared: fix out of bounds for nm_g_array_append_new() macro
Fixes: fb6e9795b7cf ('shared: add nm_g_array_append_new() helper')

shared/nm-glib-aux/nm-shared-utils.h
@@ -1564,14 +1564,14 @@ nm_g_array_len (const GArray *arr)
 
 #define nm_g_array_append_new(arr, type) \
 	({ \
-		GArray *_arr = (arr); \
-		gsize _l; \
+		GArray *const _arr = (arr); \
+		guint _len; \
 		\
 		nm_assert (_arr); \
-		_l = ((gsize) _arr->len) + 1u; \
-		nm_assert (_l > _arr->len); \
-		g_array_set_size (_arr, _l); \
-		&g_array_index (arr, type, _l); \
+		_len = _arr->len; \
+		nm_assert (_len < G_MAXUINT); \
+		g_array_set_size (_arr, _len + 1u); \
+		&g_array_index (arr, type, _len); \
 	})
 
 /*****************************************************************************/