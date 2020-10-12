NetworkManager/NetworkManager

commit 21d4a26188687e49704bf33bea760720d45b721e

core: remove code for unused NM_WIFI_P2P_PEER_GROUPS property

src/devices/wifi/nm-wifi-p2p-peer.c
@@ -28,7 +28,6 @@ NM_GOBJECT_PROPERTIES_DEFINE (NMWifiP2PPeer,
 	PROP_MODEL_NUMBER,
 	PROP_SERIAL,
 	PROP_WFD_IES,
-	PROP_GROUPS,
 	PROP_HW_ADDRESS,
 	PROP_STRENGTH,
 	PROP_LAST_SEEN,
@@ -314,26 +313,6 @@ nm_wifi_p2p_peer_get_groups (const NMWifiP2PPeer *peer)
 	return (const char * const*) NM_WIFI_P2P_PEER_GET_PRIVATE (peer)->groups;
 }
 
-static gboolean
-nm_wifi_p2p_peer_set_groups (NMWifiP2PPeer *peer, const char** groups)
-{
-	NMWifiP2PPeerPrivate *priv;
-
-	g_return_val_if_fail (NM_IS_WIFI_P2P_PEER (peer), FALSE);
-	g_return_val_if_fail (groups != NULL, FALSE);
-
-	priv = NM_WIFI_P2P_PEER_GET_PRIVATE (peer);
-
-	if (_nm_utils_strv_equal (priv->groups, (char **) groups))
-		return FALSE;
-
-	g_strfreev (priv->groups);
-	priv->groups = g_strdupv ((char**) groups);
-
-	_notify (peer, PROP_GROUPS);
-	return TRUE;
-}
-
 const char *
 nm_wifi_p2p_peer_get_address (const NMWifiP2PPeer *peer)
 {
@@ -434,7 +413,6 @@ nm_wifi_p2p_peer_update_from_properties (NMWifiP2PPeer *peer,
 	GVariant *v;
 	gsize len;
 	const char *s;
-	const char **sv;
 	gint32 i32;
 	gboolean changed = FALSE;
 
@@ -483,16 +461,6 @@ nm_wifi_p2p_peer_update_from_properties (NMWifiP2PPeer *peer,
 		changed |= nm_wifi_p2p_peer_set_wfd_ies (peer, b);
 		g_variant_unref (v);
 	}
-
-	v = g_variant_lookup_value (properties, "Groups", G_VARIANT_TYPE_OBJECT_PATH_ARRAY);
-	if (v) {
-		sv = g_variant_get_objv (v, NULL);
-		changed |= nm_wifi_p2p_peer_set_groups (peer, sv);
-		g_free (sv);
-	}
-
-	/*if (max_rate)
-		changed |= nm_wifi_p2p_peer_set_max_bitrate (peer, max_rate / 1000);*/
 
 	if (!priv->supplicant_path) {
 		priv->supplicant_path = g_strdup (supplicant_path);
@@ -600,12 +568,6 @@ get_property (GObject *object, guint prop_id,
 		break;
 	case PROP_WFD_IES:
 		g_value_take_variant (value, nm_utils_gbytes_to_variant_ay (priv->wfd_ies));
-		break;
-	case PROP_GROUPS:
-		g_value_set_variant (value,
-		                      g_variant_new_strv (   (const char*const*) priv->groups
-		                                          ?: NM_PTRARRAY_EMPTY (const char *),
-		                                          -1));
 		break;
 	case PROP_HW_ADDRESS:
 		g_value_set_string (value, priv->address);
@@ -753,12 +715,6 @@ nm_wifi_p2p_peer_class_init (NMWifiP2PPeerClass *klass)
 	obj_properties[PROP_WFD_IES] =
 	    g_param_spec_variant (NM_WIFI_P2P_PEER_WFD_IES, "", "",
 	                          G_VARIANT_TYPE ("ay"),
-	                          NULL,
-	                          G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
-
-	obj_properties[PROP_GROUPS] =
-	    g_param_spec_variant (NM_WIFI_P2P_PEER_GROUPS, "", "",
-	                          G_VARIANT_TYPE ("as"),
 	                          NULL,
 	                          G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
 

src/devices/wifi/nm-wifi-p2p-peer.h
@@ -24,7 +24,6 @@
 #define NM_WIFI_P2P_PEER_MODEL_NUMBER         "model-number"
 #define NM_WIFI_P2P_PEER_SERIAL               "serial"
 #define NM_WIFI_P2P_PEER_WFD_IES              "wfd-ies"
-#define NM_WIFI_P2P_PEER_GROUPS               "groups"
 #define NM_WIFI_P2P_PEER_HW_ADDRESS           "hw-address"
 #define NM_WIFI_P2P_PEER_STRENGTH             "strength"
 #define NM_WIFI_P2P_PEER_LAST_SEEN            "last-seen"

commit dc54a946acb968c73f8d55e01c61efedb4ae6cc1

wifi: re-add code for tracking a peers groups
The code to track the property was accidentally removed in commit
21d4a2618868 ('core: remove code for unused NM_WIFI_P2P_PEER_GROUPS property')
causing all P2P connections to fail after 5 seconds.

Fixes: 21d4a2618868 ('core: remove code for unused NM_WIFI_P2P_PEER_GROUPS property')

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/551

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/650

src/devices/wifi/nm-wifi-p2p-peer.c
@@ -47,7 +47,8 @@ struct _NMWifiP2PPeerPrivate {
     char *address;
 
     GBytes *wfd_ies;
-    char ** groups;
+
+    const char **groups;
 
     guint8 strength;
 
@@ -281,7 +282,7 @@ nm_wifi_p2p_peer_get_groups(const NMWifiP2PPeer *peer)
 {
     g_return_val_if_fail(NM_IS_WIFI_P2P_PEER(peer), NULL);
 
-    return (const char *const *) NM_WIFI_P2P_PEER_GET_PRIVATE(peer)->groups;
+    return NM_WIFI_P2P_PEER_GET_PRIVATE(peer)->groups;
 }
 
 const char *
@@ -403,6 +404,14 @@ nm_wifi_p2p_peer_update_from_properties(NMWifiP2PPeer *peer, const NMSupplicantP
 
     changed |= nm_wifi_p2p_peer_set_wfd_ies(peer, peer_info->ies);
     changed |= nm_wifi_p2p_peer_set_last_seen(peer, peer_info->last_seen_msec / 1000u);
+
+    /* We currently only use the groups information internally to check if
+     * the peer is still joined. */
+    if (!_nm_utils_strv_equal((char **) priv->groups, (char **) peer_info->groups)) {
+        g_free(priv->groups);
+        priv->groups = nm_utils_strv_dup_packed(peer_info->groups, -1);
+        changed |= TRUE;
+    }
 
     g_object_thaw_notify(G_OBJECT(peer));
 
@@ -566,7 +575,7 @@ finalize(GObject *object)
     g_free(priv->serial);
     g_free(priv->address);
     g_bytes_unref(priv->wfd_ies);
-    g_strfreev(priv->groups);
+    g_free(priv->groups);
 
     G_OBJECT_CLASS(nm_wifi_p2p_peer_parent_class)->finalize(object);
 }

src/supplicant/nm-supplicant-interface.c
@@ -833,6 +833,7 @@ _peer_info_destroy(NMSupplicantPeerInfo *peer_info)
     g_free(peer_info->model);
     g_free(peer_info->model_number);
     g_free(peer_info->serial);
+    g_free(peer_info->groups);
     g_bytes_unref(peer_info->ies);
 
     nm_ref_string_unref(peer_info->peer_path);
@@ -856,6 +857,7 @@ _peer_info_properties_changed(NMSupplicantInterface *self,
 {
     GVariant *    v_v;
     const char *  v_s;
+    const char ** v_strv;
     gint32        v_i32;
     const guint8 *arr_data;
     gsize         arr_len;
@@ -879,6 +881,13 @@ _peer_info_properties_changed(NMSupplicantInterface *self,
 
     if (nm_g_variant_lookup(properties, "Serial", "&s", &v_s))
         nm_utils_strdup_reset(&peer_info->serial, v_s);
+
+    if (nm_g_variant_lookup(properties, "Groups", "^a&o", &v_strv)) {
+        g_free(peer_info->groups);
+        peer_info->groups = nm_utils_strv_dup_packed(v_strv, -1);
+
+        g_free(v_strv);
+    }
 
     v_v = nm_g_variant_lookup_value(properties, "DeviceAddress", G_VARIANT_TYPE_BYTESTRING);
     if (v_v) {

src/supplicant/nm-supplicant-types.h
@@ -185,6 +185,8 @@ typedef struct _NMSupplicantPeerInfo {
     char *model_number;
     char *serial;
 
+    const char **groups;
+
     GBytes *ies;
 
     gint64 last_seen_msec;