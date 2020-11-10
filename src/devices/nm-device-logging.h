NetworkManager/NetworkManager

commit cc35dc3bdf5f76cac5ed37127ca89eef18bb9998

device: improve "nm-device-logging.h" to support a self pointer of NMDevice type
"nm-device-logging.h" defines logging macros for a NMDevice instance.
It also expects a "self" variable in the call environment, and that
variable had to be in the type of NMDevice or the NMDevice subclass.

Extend the macro foo, so that @self can be either a NMDevice* pointer
or a NMDevice$SUBTYPE.

Of course, that would have always been possible, if we would simply cast
to "(NMDevice *)" where we need it. The trick is that the macro only
works if @self is one of the two expected types, and not some arbitrary
unrelated type.

src/devices/nm-device-logging.h
@@ -8,36 +8,40 @@
 
 #include "nm-device.h"
 
-#define _LOG_DECLARE_SELF(t)                                                  \
-    _nm_unused static inline NMDevice *_nm_device_log_self_to_device(t *self) \
-    {                                                                         \
-        return (NMDevice *) self;                                             \
-    }
+#if !_NM_CC_SUPPORT_GENERIC
+    #define _NM_DEVICE_CAST(self) ((NMDevice *) (self))
+#elif !defined(_NMLOG_DEVICE_TYPE)
+    #define _NM_DEVICE_CAST(self) _NM_ENSURE_TYPE(NMDevice *, self)
+#else
+    #define _NM_DEVICE_CAST(self)                    \
+        _Generic((self), _NMLOG_DEVICE_TYPE *        \
+                 : ((NMDevice *) (self)), NMDevice * \
+                 : ((NMDevice *) (self)))
+#endif
 
 #undef _NMLOG_ENABLED
 #define _NMLOG_ENABLED(level, domain) (nm_logging_enabled((level), (domain)))
-#define _NMLOG(level, domain, ...)                                               \
-    G_STMT_START                                                                 \
-    {                                                                            \
-        const NMLogLevel  _level  = (level);                                     \
-        const NMLogDomain _domain = (domain);                                    \
-                                                                                 \
-        if (nm_logging_enabled(_level, _domain)) {                               \
-            typeof(*self) *const _self = (self);                                 \
-            const char *const    _ifname =                                       \
-                _nm_device_get_iface(_nm_device_log_self_to_device(_self));      \
-                                                                                 \
-            nm_log_obj(_level,                                                   \
-                       _domain,                                                  \
-                       _ifname,                                                  \
-                       NULL,                                                     \
-                       _self,                                                    \
-                       "device",                                                 \
-                       "%s%s%s: " _NM_UTILS_MACRO_FIRST(__VA_ARGS__),            \
-                       NM_PRINT_FMT_QUOTED(_ifname, "(", _ifname, ")", "[null]") \
-                           _NM_UTILS_MACRO_REST(__VA_ARGS__));                   \
-        }                                                                        \
-    }                                                                            \
+#define _NMLOG(level, domain, ...)                                                       \
+    G_STMT_START                                                                         \
+    {                                                                                    \
+        const NMLogLevel  _level  = (level);                                             \
+        const NMLogDomain _domain = (domain);                                            \
+                                                                                         \
+        if (nm_logging_enabled(_level, _domain)) {                                       \
+            typeof(*self) *const _self   = (self);                                       \
+            const char *const    _ifname = _nm_device_get_iface(_NM_DEVICE_CAST(_self)); \
+                                                                                         \
+            nm_log_obj(_level,                                                           \
+                       _domain,                                                          \
+                       _ifname,                                                          \
+                       NULL,                                                             \
+                       _self,                                                            \
+                       "device",                                                         \
+                       "%s%s%s: " _NM_UTILS_MACRO_FIRST(__VA_ARGS__),                    \
+                       NM_PRINT_FMT_QUOTED(_ifname, "(", _ifname, ")", "[null]")         \
+                           _NM_UTILS_MACRO_REST(__VA_ARGS__));                           \
+        }                                                                                \
+    }                                                                                    \
     G_STMT_END
 
 #endif /* __NETWORKMANAGER_DEVICE_LOGGING_H__ */

src/devices/adsl/nm-device-adsl.c
@@ -23,8 +23,8 @@
 #include "nm-setting-adsl.h"
 #include "nm-utils.h"
 
+#define _NMLOG_DEVICE_TYPE NMDeviceAdsl
 #include "devices/nm-device-logging.h"
-_LOG_DECLARE_SELF(NMDeviceAdsl);
 
 /*****************************************************************************/
 

src/devices/bluetooth/nm-device-bt.c
@@ -30,8 +30,8 @@
 #include "devices/wwan/nm-modem-manager.h"
 #include "devices/wwan/nm-modem.h"
 
+#define _NMLOG_DEVICE_TYPE NMDeviceBt
 #include "devices/nm-device-logging.h"
-_LOG_DECLARE_SELF(NMDeviceBt);
 
 /*****************************************************************************/
 

src/devices/nm-device-6lowpan.c
@@ -14,8 +14,8 @@
 #include "nm-setting-6lowpan.h"
 #include "nm-utils.h"
 
+#define _NMLOG_DEVICE_TYPE NMDevice6Lowpan
 #include "nm-device-logging.h"
-_LOG_DECLARE_SELF(NMDevice6Lowpan);
 
 /*****************************************************************************/
 

src/devices/nm-device-bond.c
@@ -17,8 +17,8 @@
 #include "nm-core-internal.h"
 #include "nm-ip4-config.h"
 
+#define _NMLOG_DEVICE_TYPE NMDeviceBond
 #include "nm-device-logging.h"
-_LOG_DECLARE_SELF(NMDeviceBond);
 
 /*****************************************************************************/
 

src/devices/nm-device-bridge.c
@@ -15,8 +15,8 @@
 #include "nm-device-factory.h"
 #include "nm-core-internal.h"
 
+#define _NMLOG_DEVICE_TYPE NMDeviceBridge
 #include "nm-device-logging.h"
-_LOG_DECLARE_SELF(NMDeviceBridge);
 
 /*****************************************************************************/
 

src/devices/nm-device-dummy.c
@@ -18,8 +18,8 @@
 #include "nm-setting-dummy.h"
 #include "nm-core-internal.h"
 
+#define _NMLOG_DEVICE_TYPE NMDeviceDummy
 #include "nm-device-logging.h"
-_LOG_DECLARE_SELF(NMDeviceDummy);
 
 /*****************************************************************************/
 

src/devices/nm-device-ethernet.c
@@ -35,8 +35,8 @@
 #include "NetworkManagerUtils.h"
 #include "nm-udev-aux/nm-udev-utils.h"
 
+#define _NMLOG_DEVICE_TYPE NMDeviceEthernet
 #include "nm-device-logging.h"
-_LOG_DECLARE_SELF(NMDeviceEthernet);
 
 /*****************************************************************************/
 

commit 71eeec8c78b176b890d99f7e6c4d95f9ac65e8ab

device: fix _Generic() types for _NM_DEVICE_CAST() macro
clang (x86_64, 3.4.2-9.el7) fails:

    ../src/devices/nm-device-6lowpan.c:161:9: error: controlling expression type 'typeof (*self) *const' (aka 'struct _NMDevice6Lowpan *const') not compatible with any generic association type
            _LOGW(LOGD_DEVICE, "could not get 6lowpan properties");
            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../shared/nm-glib-aux/nm-logging-fwd.h:165:20: note: expanded from macro '_LOGW'
    #define _LOGW(...) _NMLOG(_LOGL_WARN, __VA_ARGS__)
                       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../src/devices/nm-device-logging.h:32:81: note: expanded from macro '_NMLOG'
                const char *const    _ifname = _nm_device_get_iface(_NM_DEVICE_CAST(_self)); \
                                                                                    ^~~~~
    ../src/devices/nm-device-logging.h:17:19: note: expanded from macro '_NM_DEVICE_CAST'
            _Generic((self), _NMLOG_DEVICE_TYPE *        \
                      ^

Fixes: cc35dc3bdf5f ('device: improve "nm-device-logging.h" to support a self pointer of NMDevice type')

src/devices/nm-device-logging.h
@@ -13,10 +13,12 @@
 #elif !defined(_NMLOG_DEVICE_TYPE)
     #define _NM_DEVICE_CAST(self) _NM_ENSURE_TYPE(NMDevice *, self)
 #else
-    #define _NM_DEVICE_CAST(self)                    \
-        _Generic((self), _NMLOG_DEVICE_TYPE *        \
-                 : ((NMDevice *) (self)), NMDevice * \
-                 : ((NMDevice *) (self)))
+    #define _NM_DEVICE_CAST(self) \
+        _Generic((self), \
+                 _NMLOG_DEVICE_TYPE *      : ((NMDevice *) (self)), \
+                 _NMLOG_DEVICE_TYPE * const: ((NMDevice *) (self)), \
+                 NMDevice *                : ((NMDevice *) (self)), \
+                 NMDevice *           const: ((NMDevice *) (self)))
 #endif
 
 #undef _NMLOG_ENABLED