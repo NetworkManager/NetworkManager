NetworkManager/NetworkManager

commit fbf1683c1a75c6eee002b5ed2eaa81a3f7126c84

dns: more debug logging for DNS settings in rebuild_domain_lists()

src/dns/nm-dns-manager.c
@@ -1557,6 +1557,21 @@ rebuild_domain_lists(NMDnsManager *self)
             has_default_route_explicit || (priority < 0 && has_default_route_auto);
         ip_data->domains.has_default_route =
             ip_data->domains.has_default_route_exclusive || has_default_route_auto;
+
+        {
+            gs_free char *str1 = NULL;
+            gs_free char *str2 = NULL;
+
+            _LOGT("plugin: settings: ifindex=%d, priority=%d, default-route=%d%s, search=%s, reverse=%s",
+                  ip_data->data->ifindex,
+                  priority,
+                  ip_data->domains.has_default_route,
+                  ip_data->domains.has_default_route_explicit
+                      ? " (explicit)"
+                      : (ip_data->domains.has_default_route_exclusive ? " (exclusive)" : ""),
+                  (str1 = g_strjoinv(",", (char **) ip_data->domains.search)),
+                  (str2 = g_strjoinv(",", ip_data->domains.reverse)));
+        }
     }
 }
 

commit 937c8a466928600390d221da233b6b3eed82c3a6

dns: fix accessing NULL domains.reverse array in rebuild_domain_lists()
Fixes: fbf1683c1a75 ('dns: more debug logging for DNS settings in rebuild_domain_lists()')

src/dns/nm-dns-manager.c
@@ -1571,7 +1571,8 @@ rebuild_domain_lists(NMDnsManager *self)
                       ? " (explicit)"
                       : (ip_data->domains.has_default_route_exclusive ? " (exclusive)" : ""),
                   (str1 = g_strjoinv(",", (char **) ip_data->domains.search)),
-                  (str2 = g_strjoinv(",", ip_data->domains.reverse)));
+                  (ip_data->domains.reverse ? (str2 = g_strjoinv(",", ip_data->domains.reverse))
+                                            : ""));
         }
     }
 }