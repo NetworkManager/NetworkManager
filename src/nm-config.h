d615b902d8ded3f5de36704a3f3f00680899b40b
Thomas Haller <thaller@redhat.com>
2020-11-27 09:51:09 +0100

    core: recognize "wifi.scan-generate-mac-address-mask" as valid "NetworkManager.conf" option
    Fixes: 32f4abe90b72 ('config: warn about unknown keys in config files')
    
    https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/595

--- src/nm-config.h
+++ src/nm-config.h
@@ -85,8 +85,10 @@
 #define NM_CONFIG_KEYFILE_KEY_DEVICE_SRIOV_NUM_VFS              "sriov-num-vfs"
 #define NM_CONFIG_KEYFILE_KEY_DEVICE_WIFI_BACKEND               "wifi.backend"
 #define NM_CONFIG_KEYFILE_KEY_DEVICE_WIFI_SCAN_RAND_MAC_ADDRESS "wifi.scan-rand-mac-address"
-#define NM_CONFIG_KEYFILE_KEY_DEVICE_CARRIER_WAIT_TIMEOUT       "carrier-wait-timeout"
-#define NM_CONFIG_KEYFILE_KEY_DEVICE_WIFI_IWD_AUTOCONNECT       "wifi.iwd.autoconnect"
+#define NM_CONFIG_KEYFILE_KEY_DEVICE_WIFI_SCAN_GENERATE_MAC_ADDRESS_MASK \
+    "wifi.scan-generate-mac-address-mask"
+#define NM_CONFIG_KEYFILE_KEY_DEVICE_CARRIER_WAIT_TIMEOUT "carrier-wait-timeout"
+#define NM_CONFIG_KEYFILE_KEY_DEVICE_WIFI_IWD_AUTOCONNECT "wifi.iwd.autoconnect"
 
 #define NM_CONFIG_KEYFILE_KEY_MATCH_DEVICE "match-device"
 #define NM_CONFIG_KEYFILE_KEY_STOP_MATCH   "stop-match"
