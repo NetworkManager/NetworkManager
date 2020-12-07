dcd0066b104850d6da4035ac942f4044ec6979ef
Thomas Haller <thaller@redhat.com>
2020-12-07 22:14:03 +0100

    libnm: add debug logging for creating/destroying NMObject

--- libnm/nm-object.c
+++ libnm/nm-object.c
@@ -248,6 +248,10 @@ nm_object_init(NMObject *object)
     self->_priv = priv;
 
     c_list_init(&self->obj_base.queue_notify_lst);
+
+    NML_DBUS_LOG(_NML_NMCLIENT_LOG_LEVEL_COERCE(NML_DBUS_LOG_LEVEL_TRACE),
+                 "nmobj[" NM_HASH_OBFUSCATE_PTR_FMT "]: creating",
+                 NM_HASH_OBFUSCATE_PTR(self));
 }
 
 static void
@@ -255,6 +259,12 @@ dispose(GObject *object)
 {
     NMObject *       self = NM_OBJECT(object);
     NMObjectPrivate *priv = NM_OBJECT_GET_PRIVATE(self);
+
+    if (!self->obj_base.is_disposing) {
+        NML_DBUS_LOG(_NML_NMCLIENT_LOG_LEVEL_COERCE(NML_DBUS_LOG_LEVEL_TRACE),
+                     "nmobj[" NM_HASH_OBFUSCATE_PTR_FMT "]: disposing",
+                     NM_HASH_OBFUSCATE_PTR(self));
+    }
 
     self->obj_base.is_disposing = TRUE;
 
