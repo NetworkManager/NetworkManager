NetworkManager/NetworkManager

commit ec7d8ddb295b888a17b5e0a6e85181d6ea512877

ovs: fix leaks
Fixes: 1eeca3c606ab ('core/ovs: track external-ids for cached ovsdb objects')

src/devices/ovs/nm-ovsdb.c
@@ -1770,7 +1770,7 @@ ovsdb_got_update(NMOvsdb *self, json_t *msg)
             nm_assert(nm_streq0(ovs_port->name, name));
 
             changed |= nm_utils_strdup_reset(&ovs_port->name, name);
-            changed |= nm_utils_strdup_reset(&ovs_port->connection_uuid, g_strdup(connection_uuid));
+            changed |= nm_utils_strdup_reset(&ovs_port->connection_uuid, connection_uuid);
             if (nm_strv_ptrarray_cmp(ovs_port->interfaces, interfaces) != 0) {
                 NM_SWAP(&ovs_port->interfaces, &interfaces);
                 changed = TRUE;
@@ -1876,7 +1876,7 @@ ovsdb_got_update(NMOvsdb *self, json_t *msg)
 
             changed = nm_utils_strdup_reset(&ovs_bridge->name, name);
             changed =
-                nm_utils_strdup_reset(&ovs_bridge->connection_uuid, g_strdup(connection_uuid));
+                nm_utils_strdup_reset(&ovs_bridge->connection_uuid, connection_uuid);
             if (nm_strv_ptrarray_cmp(ovs_bridge->ports, ports) != 0) {
                 NM_SWAP(&ovs_bridge->ports, &ports);
                 changed = TRUE;

commit 2fa8ef9fb9c7fe0cc2d9523eed6c5a3749b05175

ovs: fix indentation
Fixes: ec7d8ddb295b ('ovs: fix leaks')

src/devices/ovs/nm-ovsdb.c
@@ -1875,8 +1875,7 @@ ovsdb_got_update(NMOvsdb *self, json_t *msg)
             nm_assert(nm_streq0(ovs_bridge->name, name));
 
             changed = nm_utils_strdup_reset(&ovs_bridge->name, name);
-            changed =
-                nm_utils_strdup_reset(&ovs_bridge->connection_uuid, connection_uuid);
+            changed = nm_utils_strdup_reset(&ovs_bridge->connection_uuid, connection_uuid);
             if (nm_strv_ptrarray_cmp(ovs_bridge->ports, ports) != 0) {
                 NM_SWAP(&ovs_bridge->ports, &ports);
                 changed = TRUE;