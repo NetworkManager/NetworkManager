d2464c260fbb7ce8c2b45485b2e080cbcec172e5
Thomas Haller <thaller@redhat.com>
2021-01-10 11:33:17 +0100

    core: move NMIPConfigSource from "src/nm-types.h" to "shared/nm-base"

--- shared/nm-base/nm-base.h
+++ shared/nm-base/nm-base.h
@@ -162,6 +162,43 @@ typedef enum {
     _NM_SETTING_WIRED_WAKE_ON_LAN_EXCLUSIVE_FLAGS = 0x8001,
 } _NMSettingWiredWakeOnLan;
 
+/*****************************************************************************/
+
+typedef enum {
+    /* In priority order; higher number == higher priority */
+
+    NM_IP_CONFIG_SOURCE_UNKNOWN = 0,
+
+    /* for routes, the source is mapped to the uint8 field rtm_protocol.
+     * Reserve the range [1,0x100] for native RTPROT values. */
+
+    NM_IP_CONFIG_SOURCE_RTPROT_UNSPEC   = 1 + 0,
+    NM_IP_CONFIG_SOURCE_RTPROT_REDIRECT = 1 + 1,
+    NM_IP_CONFIG_SOURCE_RTPROT_KERNEL   = 1 + 2,
+    NM_IP_CONFIG_SOURCE_RTPROT_BOOT     = 1 + 3,
+    NM_IP_CONFIG_SOURCE_RTPROT_STATIC   = 1 + 4,
+    NM_IP_CONFIG_SOURCE_RTPROT_RA       = 1 + 9,
+    NM_IP_CONFIG_SOURCE_RTPROT_DHCP     = 1 + 16,
+    _NM_IP_CONFIG_SOURCE_RTPROT_LAST    = 1 + 0xFF,
+
+    NM_IP_CONFIG_SOURCE_KERNEL,
+    NM_IP_CONFIG_SOURCE_SHARED,
+    NM_IP_CONFIG_SOURCE_IP4LL,
+    NM_IP_CONFIG_SOURCE_IP6LL,
+    NM_IP_CONFIG_SOURCE_PPP,
+    NM_IP_CONFIG_SOURCE_WWAN,
+    NM_IP_CONFIG_SOURCE_VPN,
+    NM_IP_CONFIG_SOURCE_DHCP,
+    NM_IP_CONFIG_SOURCE_NDISC,
+    NM_IP_CONFIG_SOURCE_USER,
+} NMIPConfigSource;
+
+static inline gboolean
+NM_IS_IP_CONFIG_SOURCE_RTPROT(NMIPConfigSource source)
+{
+    return source > NM_IP_CONFIG_SOURCE_UNKNOWN && source <= _NM_IP_CONFIG_SOURCE_RTPROT_LAST;
+}
+
 /****************************************************************************/
 
 #endif /* __NM_LIBNM_BASE_H__ */
