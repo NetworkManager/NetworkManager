NetworkManager/NetworkManager

commit cd0cf9229d49a05c4e8afbb9b87b5d621c23ea00

veth: add support to configure veth interfaces
NetworkManager is now able to configure veth interfaces throught the
NMSettingVeth. Veth interfaces only have "peer" property.

In order to support Veth interfaces in NetworkManager the design need
to pass the following requirements:

 * Veth setting only has "peer" attribute.
 * Ethernet profiles must be applicable to Veth interfaces.
 * When creating a veth interface, the peer will be managed by
   NetworkManager but will not have a profile.
 * Veth connection can reapply only if the peer has not been modified.
 * In order to modify the veth peer, NetworkManager must deactivate the
   connection and create a new one with peer modified.

In general, it should support the basis of veth interfaces but without
breaking any existing feature or use case. The users that are using veth
interfaces as ethernet should not notice anything changed unless they
specified the veth peer setting.

Creating a Veth interface in NetworkManager is useful even without the
support for namespaces for some use cases, e.g "connecting one side of
the veth to an OVS bridge and the other side to a Linux bridge" this is
done when using OVN kubernetes [1][2]. In addition, it would provide
persistent configuration and rollback support for Veth interfaces.

[1] https://bugzilla.redhat.com/show_bug.cgi?id=1885605
[2] https://bugzilla.redhat.com/show_bug.cgi?id=1894139

Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

src/devices/nm-device-ethernet.c
@@ -34,6 +34,7 @@
 #include "nm-core-internal.h"
 #include "NetworkManagerUtils.h"
 #include "nm-udev-aux/nm-udev-utils.h"
+#include "nm-device-veth.h"
 
 #define _NMLOG_DEVICE_TYPE NMDeviceEthernet
 #include "nm-device-logging.h"
@@ -347,7 +348,9 @@ check_connection_compatible(NMDevice *device, NMConnection *connection, GError *
              ->check_connection_compatible(device, connection, error))
         return FALSE;
 
-    if (nm_connection_is_type(connection, NM_SETTING_PPPOE_SETTING_NAME)) {
+    if (nm_connection_is_type(connection, NM_SETTING_PPPOE_SETTING_NAME)
+        || (nm_connection_is_type(connection, NM_SETTING_VETH_SETTING_NAME)
+            && NM_IS_DEVICE_VETH(device))) {
         s_wired = nm_connection_get_setting_wired(connection);
     } else {
         s_wired =
@@ -1360,6 +1363,9 @@ wake_on_lan_enable(NMDevice *device)
 
     s_wired = nm_device_get_applied_setting(device, NM_TYPE_SETTING_WIRED);
 
+    if (NM_IS_DEVICE_VETH(device))
+        return FALSE;
+
     if (s_wired) {
         wol      = nm_setting_wired_get_wake_on_lan(s_wired);
         password = nm_setting_wired_get_wake_on_lan_password(s_wired);
@@ -1493,6 +1499,12 @@ act_stage3_ip_config_start(NMDevice *           device,
 {
     NMSettingConnection *s_con;
     const char *         connection_type;
+    int                  ifindex;
+
+    ifindex = nm_device_get_ifindex(device);
+
+    if (ifindex <= 0)
+        return NM_ACT_STAGE_RETURN_FAILURE;
 
     if (addr_family == AF_INET) {
         s_con = nm_device_get_applied_setting(device, NM_TYPE_SETTING_CONNECTION);
@@ -1803,7 +1815,7 @@ static void
 link_changed(NMDevice *device, const NMPlatformLink *pllink)
 {
     NM_DEVICE_CLASS(nm_device_ethernet_parent_class)->link_changed(device, pllink);
-    if (pllink->initialized)
+    if (!NM_IS_DEVICE_VETH(device) && pllink->initialized)
         _update_s390_subchannels((NMDeviceEthernet *) device);
 }
 

src/devices/nm-device-veth.c
@@ -7,11 +7,13 @@
 
 #include <stdlib.h>
 
+#include "nm-core-internal.h"
 #include "nm-device-veth.h"
 #include "nm-device-private.h"
 #include "nm-manager.h"
 #include "platform/nm-platform.h"
 #include "nm-device-factory.h"
+#include "nm-setting-veth.h"
 
 #define _NMLOG_DEVICE_TYPE NMDeviceVeth
 #include "nm-device-logging.h"
@@ -42,9 +44,10 @@ update_properties(NMDevice *device)
 
     ifindex = nm_device_get_ifindex(device);
 
-    if (!nm_platform_link_veth_get_properties(nm_device_get_platform(device),
-                                              ifindex,
-                                              &peer_ifindex))
+    if (ifindex <= 0
+        || !nm_platform_link_veth_get_properties(nm_device_get_platform(device),
+                                                 ifindex,
+                                                 &peer_ifindex))
         peer_ifindex = 0;
 
     nm_device_parent_set_ifindex(device, peer_ifindex);
@@ -69,6 +72,81 @@ link_changed(NMDevice *device, const NMPlatformLink *pllink)
 {
     NM_DEVICE_CLASS(nm_device_veth_parent_class)->link_changed(device, pllink);
     update_properties(device);
+}
+
+static gboolean
+create_and_realize(NMDevice *             device,
+                   NMConnection *         connection,
+                   NMDevice *             parent,
+                   const NMPlatformLink **out_plink,
+                   GError **              error)
+{
+    const char *   iface = nm_device_get_iface(device);
+    NMSettingVeth *s_veth;
+    int            r;
+
+    s_veth = _nm_connection_get_setting(connection, NM_TYPE_SETTING_VETH);
+    if (!s_veth) {
+        g_set_error(error,
+                    NM_DEVICE_ERROR,
+                    NM_DEVICE_ERROR_CREATION_FAILED,
+                    "Profile %s (%s) is not a suitable veth profile",
+                    nm_connection_get_id(connection),
+                    nm_connection_get_uuid(connection));
+        return FALSE;
+    }
+
+    r = nm_platform_link_veth_add(nm_device_get_platform(device),
+                                  iface,
+                                  nm_setting_veth_get_peer(s_veth),
+                                  out_plink);
+    if (r < 0) {
+        g_set_error(error,
+                    NM_DEVICE_ERROR,
+                    NM_DEVICE_ERROR_CREATION_FAILED,
+                    "Failed to create veth interface '%s' for '%s': %s",
+                    iface,
+                    nm_connection_get_id(connection),
+                    nm_strerror(r));
+        return FALSE;
+    }
+    return TRUE;
+}
+
+/*****************************************************************************/
+
+static NMDeviceCapabilities
+get_generic_capabilities(NMDevice *device)
+{
+    return NM_DEVICE_CAP_CARRIER_DETECT | NM_DEVICE_CAP_IS_SOFTWARE;
+}
+
+static gboolean
+complete_connection(NMDevice *           device,
+                    NMConnection *       connection,
+                    const char *         specific_object,
+                    NMConnection *const *existing_connections,
+                    GError **            error)
+{
+    NMSettingVeth *s_veth;
+
+    nm_utils_complete_generic(nm_device_get_platform(device),
+                              connection,
+                              NM_SETTING_VETH_SETTING_NAME,
+                              existing_connections,
+                              NULL,
+                              _("Veth connection"),
+                              "veth",
+                              NULL,
+                              TRUE);
+
+    s_veth = _nm_connection_get_setting(connection, NM_TYPE_SETTING_VETH);
+    if (!s_veth) {
+        s_veth = (NMSettingVeth *) nm_setting_veth_new();
+        nm_connection_add_setting(connection, NM_SETTING(s_veth));
+    }
+
+    return TRUE;
 }
 
 /*****************************************************************************/
@@ -136,6 +214,9 @@ nm_device_veth_class_init(NMDeviceVethClass *klass)
     device_class->can_unmanaged_external_down = can_unmanaged_external_down;
     device_class->link_changed                = link_changed;
     device_class->parent_changed_notify       = parent_changed_notify;
+    device_class->create_and_realize          = create_and_realize;
+    device_class->complete_connection         = complete_connection;
+    device_class->get_generic_capabilities    = get_generic_capabilities;
 
     obj_properties[PROP_PEER] = g_param_spec_string(NM_DEVICE_VETH_PEER,
                                                     "",
@@ -171,8 +252,10 @@ create_device(NMDeviceFactory *     factory,
                         NULL);
 }
 
-NM_DEVICE_FACTORY_DEFINE_INTERNAL(VETH,
-                                  Veth,
-                                  veth,
-                                  NM_DEVICE_FACTORY_DECLARE_LINK_TYPES(NM_LINK_TYPE_VETH),
-                                  factory_class->create_device = create_device;);
+NM_DEVICE_FACTORY_DEFINE_INTERNAL(
+    VETH,
+    Veth,
+    veth,
+    NM_DEVICE_FACTORY_DECLARE_LINK_TYPES(NM_LINK_TYPE_VETH)
+        NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES(NM_SETTING_VETH_SETTING_NAME),
+    factory_class->create_device = create_device;);

libnm/nm-device-ethernet.c
@@ -12,8 +12,10 @@
 #include "nm-setting-connection.h"
 #include "nm-setting-wired.h"
 #include "nm-setting-pppoe.h"
+#include "nm-setting-veth.h"
 #include "nm-utils.h"
 #include "nm-object-private.h"
+#include "nm-device-veth.h"
 
 /*****************************************************************************/
 
@@ -173,9 +175,12 @@ connection_compatible(NMDevice *device, NMConnection *connection, GError **error
              ->connection_compatible(device, connection, error))
         return FALSE;
 
-    if (nm_connection_is_type(connection, NM_SETTING_PPPOE_SETTING_NAME)) {
+    if (nm_connection_is_type(connection, NM_SETTING_PPPOE_SETTING_NAME)
+        || nm_connection_is_type(connection, NM_SETTING_WIRED_SETTING_NAME)
+        || (nm_connection_is_type(connection, NM_SETTING_VETH_SETTING_NAME)
+            && NM_IS_DEVICE_VETH(device))) {
         /* NOP */
-    } else if (!nm_connection_is_type(connection, NM_SETTING_WIRED_SETTING_NAME)) {
+    } else {
         g_set_error_literal(error,
                             NM_DEVICE_ERROR,
                             NM_DEVICE_ERROR_INCOMPATIBLE_CONNECTION,
@@ -306,13 +311,6 @@ get_property(GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
         break;
     }
 }
-
-/* TODO: implemented Veth. */
-const NMLDBusMetaIface _nml_dbus_meta_iface_nm_device_veth = NML_DBUS_META_IFACE_INIT(
-    NM_DBUS_INTERFACE_DEVICE_VETH,
-    NULL,
-    NML_DBUS_META_INTERFACE_PRIO_NONE,
-    NML_DBUS_META_IFACE_DBUS_PROPERTIES(NML_DBUS_META_PROPERTY_INIT_TODO("Peer", "o"), ), );
 
 const NMLDBusMetaIface _nml_dbus_meta_iface_nm_device_wired = NML_DBUS_META_IFACE_INIT_PROP(
     NM_DBUS_INTERFACE_DEVICE_WIRED,

libnm/nm-device-veth.c
@@ -1,0 +1,129 @@
+/* SPDX-License-Identifier: LGPL-2.1+ */
+/*
+ * Copyright (C) 2020 Red Hat, Inc.
+ */
+
+#include "nm-default.h"
+
+#include "nm-device-veth.h"
+
+#include "nm-setting-connection.h"
+#include "nm-setting-veth.h"
+#include "nm-setting-wired.h"
+#include "nm-utils.h"
+#include "nm-device-ethernet.h"
+#include "nm-object-private.h"
+
+/*****************************************************************************/
+
+NM_GOBJECT_PROPERTIES_DEFINE_BASE(PROP_PEER, );
+
+typedef struct {
+    char *peer;
+} NMDeviceVethPrivate;
+
+struct _NMDeviceVeth {
+    NMDeviceEthernet    parent;
+    NMDeviceVethPrivate _priv;
+};
+
+struct _NMDeviceVethClass {
+    NMDeviceEthernetClass parent;
+};
+
+G_DEFINE_TYPE(NMDeviceVeth, nm_device_veth, NM_TYPE_DEVICE_ETHERNET)
+
+#define NM_DEVICE_VETH_GET_PRIVATE(self) \
+    _NM_GET_PRIVATE(self, NMDeviceVeth, NM_IS_DEVICE_VETH, NMObject, NMDevice)
+
+/*****************************************************************************/
+
+/**
+ * nm_device_veth_get_peer:
+ * @device: a #NMDeviceVeth
+ *
+ * Returns: the device's peer name
+ *
+ * Since: 1.30
+ **/
+const char *
+nm_device_veth_get_peer(NMDeviceVeth *device)
+{
+    g_return_val_if_fail(NM_IS_DEVICE_VETH(device), NULL);
+
+    return _nml_coerce_property_str_not_empty(NM_DEVICE_VETH_GET_PRIVATE(device)->peer);
+}
+
+static GType
+get_setting_type(NMDevice *device)
+{
+    return NM_TYPE_SETTING_VETH;
+}
+
+/*****************************************************************************/
+
+static void
+get_property(GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
+{
+    NMDeviceVeth *device = NM_DEVICE_VETH(object);
+
+    switch (prop_id) {
+    case PROP_PEER:
+        g_value_set_string(value, nm_device_veth_get_peer(device));
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+        break;
+    }
+}
+
+static void
+nm_device_veth_init(NMDeviceVeth *device)
+{}
+
+static void
+finalize(GObject *object)
+{
+    NMDeviceVethPrivate *priv = NM_DEVICE_VETH_GET_PRIVATE(object);
+
+    g_free(priv->peer);
+
+    G_OBJECT_CLASS(nm_device_veth_parent_class)->finalize(object);
+}
+
+const NMLDBusMetaIface _nml_dbus_meta_iface_nm_device_veth = NML_DBUS_META_IFACE_INIT_PROP(
+    NM_DBUS_INTERFACE_DEVICE_VETH,
+    nm_device_veth_get_type,
+    NML_DBUS_META_INTERFACE_PRIO_INSTANTIATE_20,
+    NML_DBUS_META_IFACE_DBUS_PROPERTIES(
+        NML_DBUS_META_PROPERTY_INIT_S("peer", PROP_PEER, NMDeviceVeth, _priv.peer), ), );
+
+static void
+nm_device_veth_class_init(NMDeviceVethClass *klass)
+{
+    GObjectClass * object_class    = G_OBJECT_CLASS(klass);
+    NMObjectClass *nm_object_class = NM_OBJECT_CLASS(klass);
+    NMDeviceClass *device_class    = NM_DEVICE_CLASS(klass);
+
+    object_class->get_property = get_property;
+    object_class->finalize     = finalize;
+
+    _NM_OBJECT_CLASS_INIT_PRIV_PTR_DIRECT(nm_object_class, NMDeviceVeth);
+
+    device_class->get_setting_type = get_setting_type;
+
+    /**
+     * NMDeviceVeth:peer:
+     *
+     * The device's peer name.
+     *
+     * Since: 1.30
+     **/
+    obj_properties[PROP_PEER] = g_param_spec_string(NM_DEVICE_VETH_PEER,
+                                                    "",
+                                                    "",
+                                                    NULL,
+                                                    G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+    _nml_dbus_meta_class_init_with_properties(object_class, &_nml_dbus_meta_iface_nm_device_veth);
+}

Makefile.am
@@ -969,6 +969,7 @@ libnm_core_lib_h_pub_real = \
 	libnm-core/nm-setting-team.h \
 	libnm-core/nm-setting-tun.h \
 	libnm-core/nm-setting-user.h \
+	libnm-core/nm-setting-veth.h \
 	libnm-core/nm-setting-vlan.h \
 	libnm-core/nm-setting-vpn.h \
 	libnm-core/nm-setting-vrf.h \
@@ -1044,6 +1045,7 @@ libnm_core_lib_c_settings_real = \
 	libnm-core/nm-setting-team.c \
 	libnm-core/nm-setting-tun.c \
 	libnm-core/nm-setting-user.c \
+	libnm-core/nm-setting-veth.c \
 	libnm-core/nm-setting-vlan.c \
 	libnm-core/nm-setting-vpn.c \
 	libnm-core/nm-setting-vrf.c \
@@ -1363,6 +1365,7 @@ libnm_lib_h_pub_real = \
 	libnm/nm-device-ppp.h \
 	libnm/nm-device-team.h \
 	libnm/nm-device-tun.h \
+	libnm/nm-device-veth.h \
 	libnm/nm-device-vlan.h \
 	libnm/nm-device-vrf.h \
 	libnm/nm-device-vxlan.h \
@@ -1427,6 +1430,7 @@ libnm_lib_c_real = \
 	libnm/nm-device-ppp.c \
 	libnm/nm-device-team.c \
 	libnm/nm-device-tun.c \
+	libnm/nm-device-veth.c \
 	libnm/nm-device-vlan.c \
 	libnm/nm-device-vrf.c \
 	libnm/nm-device-vxlan.c \

clients/cli/connections.c
@@ -858,30 +858,30 @@ const NmcMetaGenericInfo
 
 /*****************************************************************************/
 
-#define NMC_FIELDS_SETTINGS_NAMES_ALL                                                    \
-    NM_SETTING_CONNECTION_SETTING_NAME                                                   \
-    "," NM_SETTING_MATCH_SETTING_NAME "," NM_SETTING_WIRED_SETTING_NAME                  \
-    "," NM_SETTING_802_1X_SETTING_NAME "," NM_SETTING_WIRELESS_SETTING_NAME              \
-    "," NM_SETTING_WIRELESS_SECURITY_SETTING_NAME "," NM_SETTING_IP4_CONFIG_SETTING_NAME \
-    "," NM_SETTING_IP6_CONFIG_SETTING_NAME "," NM_SETTING_SERIAL_SETTING_NAME            \
-    "," NM_SETTING_WIFI_P2P_SETTING_NAME "," NM_SETTING_PPP_SETTING_NAME                 \
-    "," NM_SETTING_PPPOE_SETTING_NAME "," NM_SETTING_ADSL_SETTING_NAME                   \
-    "," NM_SETTING_GSM_SETTING_NAME "," NM_SETTING_CDMA_SETTING_NAME                     \
-    "," NM_SETTING_BLUETOOTH_SETTING_NAME "," NM_SETTING_OLPC_MESH_SETTING_NAME          \
-    "," NM_SETTING_VPN_SETTING_NAME "," NM_SETTING_INFINIBAND_SETTING_NAME               \
-    "," NM_SETTING_BOND_SETTING_NAME "," NM_SETTING_VLAN_SETTING_NAME                    \
-    "," NM_SETTING_BRIDGE_SETTING_NAME "," NM_SETTING_BRIDGE_PORT_SETTING_NAME           \
-    "," NM_SETTING_TEAM_SETTING_NAME "," NM_SETTING_TEAM_PORT_SETTING_NAME               \
-    "," NM_SETTING_OVS_BRIDGE_SETTING_NAME "," NM_SETTING_OVS_INTERFACE_SETTING_NAME     \
-    "," NM_SETTING_OVS_PATCH_SETTING_NAME "," NM_SETTING_OVS_PORT_SETTING_NAME           \
-    "," NM_SETTING_DCB_SETTING_NAME "," NM_SETTING_TUN_SETTING_NAME                      \
-    "," NM_SETTING_IP_TUNNEL_SETTING_NAME "," NM_SETTING_MACSEC_SETTING_NAME             \
-    "," NM_SETTING_MACVLAN_SETTING_NAME "," NM_SETTING_VXLAN_SETTING_NAME                \
-    "," NM_SETTING_VRF_SETTING_NAME "," NM_SETTING_WPAN_SETTING_NAME                     \
-    "," NM_SETTING_6LOWPAN_SETTING_NAME "," NM_SETTING_WIREGUARD_SETTING_NAME            \
-    "," NM_SETTING_PROXY_SETTING_NAME "," NM_SETTING_TC_CONFIG_SETTING_NAME              \
-    "," NM_SETTING_SRIOV_SETTING_NAME "," NM_SETTING_ETHTOOL_SETTING_NAME                \
-    "," NM_SETTING_OVS_DPDK_SETTING_NAME                                                 \
+#define NMC_FIELDS_SETTINGS_NAMES_ALL                                                  \
+    NM_SETTING_CONNECTION_SETTING_NAME                                                 \
+    "," NM_SETTING_MATCH_SETTING_NAME "," NM_SETTING_WIRED_SETTING_NAME                \
+    "," NM_SETTING_VETH_SETTING_NAME "," NM_SETTING_802_1X_SETTING_NAME                \
+    "," NM_SETTING_WIRELESS_SETTING_NAME "," NM_SETTING_WIRELESS_SECURITY_SETTING_NAME \
+    "," NM_SETTING_IP4_CONFIG_SETTING_NAME "," NM_SETTING_IP6_CONFIG_SETTING_NAME      \
+    "," NM_SETTING_SERIAL_SETTING_NAME "," NM_SETTING_WIFI_P2P_SETTING_NAME            \
+    "," NM_SETTING_PPP_SETTING_NAME "," NM_SETTING_PPPOE_SETTING_NAME                  \
+    "," NM_SETTING_ADSL_SETTING_NAME "," NM_SETTING_GSM_SETTING_NAME                   \
+    "," NM_SETTING_CDMA_SETTING_NAME "," NM_SETTING_BLUETOOTH_SETTING_NAME             \
+    "," NM_SETTING_OLPC_MESH_SETTING_NAME "," NM_SETTING_VPN_SETTING_NAME              \
+    "," NM_SETTING_INFINIBAND_SETTING_NAME "," NM_SETTING_BOND_SETTING_NAME            \
+    "," NM_SETTING_VLAN_SETTING_NAME "," NM_SETTING_BRIDGE_SETTING_NAME                \
+    "," NM_SETTING_BRIDGE_PORT_SETTING_NAME "," NM_SETTING_TEAM_SETTING_NAME           \
+    "," NM_SETTING_TEAM_PORT_SETTING_NAME "," NM_SETTING_OVS_BRIDGE_SETTING_NAME       \
+    "," NM_SETTING_OVS_INTERFACE_SETTING_NAME "," NM_SETTING_OVS_PATCH_SETTING_NAME    \
+    "," NM_SETTING_OVS_PORT_SETTING_NAME "," NM_SETTING_DCB_SETTING_NAME               \
+    "," NM_SETTING_TUN_SETTING_NAME "," NM_SETTING_IP_TUNNEL_SETTING_NAME              \
+    "," NM_SETTING_MACSEC_SETTING_NAME "," NM_SETTING_MACVLAN_SETTING_NAME             \
+    "," NM_SETTING_VXLAN_SETTING_NAME "," NM_SETTING_VRF_SETTING_NAME                  \
+    "," NM_SETTING_WPAN_SETTING_NAME "," NM_SETTING_6LOWPAN_SETTING_NAME               \
+    "," NM_SETTING_WIREGUARD_SETTING_NAME "," NM_SETTING_PROXY_SETTING_NAME            \
+    "," NM_SETTING_TC_CONFIG_SETTING_NAME "," NM_SETTING_SRIOV_SETTING_NAME            \
+    "," NM_SETTING_ETHTOOL_SETTING_NAME "," NM_SETTING_OVS_DPDK_SETTING_NAME           \
     "," NM_SETTING_HOSTNAME_SETTING_NAME /* NM_SETTING_DUMMY_SETTING_NAME NM_SETTING_WIMAX_SETTING_NAME */
 
 const NmcMetaGenericInfo *const nmc_fields_con_active_details_groups[] = {

clients/cli/generate-docs-nm-settings-nmcli.xml.in
@@ -986,6 +986,11 @@
     </setting>
     <setting name="user" >
     </setting>
+    <setting name="veth" >
+        <property name="peer"
+                  alias="peer"
+                  description="This property specifies the peer interface name of the veth. This property is mandatory." />
+    </setting>
     <setting name="vlan" >
         <property name="parent"
                   alias="dev"

clients/common/nm-meta-setting-desc.c
@@ -7085,6 +7085,19 @@ static const NMMetaPropertyInfo *const property_infos_TUN[] = {
 };
 
 #undef  _CURRENT_NM_META_SETTING_TYPE
+#define _CURRENT_NM_META_SETTING_TYPE NM_META_SETTING_TYPE_VETH
+static const NMMetaPropertyInfo *const property_infos_VETH[] = {
+    PROPERTY_INFO_WITH_DESC (NM_SETTING_VETH_PEER,
+        .is_cli_option =                TRUE,
+        .property_alias =               "peer",
+        .inf_flags =                    NM_META_PROPERTY_INF_FLAG_REQD,
+        .prompt =                       N_("veth peer"),
+        .property_type =                &_pt_gobject_string,
+    ),
+    NULL
+};
+
+#undef  _CURRENT_NM_META_SETTING_TYPE
 #define _CURRENT_NM_META_SETTING_TYPE NM_META_SETTING_TYPE_VLAN
 static const NMMetaPropertyInfo *const property_infos_VLAN[] = {
     PROPERTY_INFO_WITH_DESC (NM_SETTING_VLAN_PARENT,
@@ -7984,6 +7997,7 @@ _setting_init_fcn_wireless (ARGS_SETTING_INIT_FCN)
 #define SETTING_PRETTY_NAME_TEAM_PORT           N_("Team port")
 #define SETTING_PRETTY_NAME_TUN                 N_("Tun device")
 #define SETTING_PRETTY_NAME_USER                N_("User settings")
+#define SETTING_PRETTY_NAME_VETH                N_("Veth connection")
 #define SETTING_PRETTY_NAME_VLAN                N_("VLAN connection")
 #define SETTING_PRETTY_NAME_VPN                 N_("VPN connection")
 #define SETTING_PRETTY_NAME_VRF                 N_("VRF connection")
@@ -8224,6 +8238,14 @@ const NMMetaSettingInfoEditor nm_meta_setting_infos_editor[] = {
         .setting_init_fcn =             _setting_init_fcn_tun,
     ),
     SETTING_INFO_EMPTY (USER),
+    SETTING_INFO (VETH,
+        .valid_parts = NM_META_SETTING_VALID_PARTS (
+            NM_META_SETTING_VALID_PART_ITEM (CONNECTION,            TRUE),
+            NM_META_SETTING_VALID_PART_ITEM (VETH,                  TRUE),
+            NM_META_SETTING_VALID_PART_ITEM (WIRED,                 FALSE),
+            NM_META_SETTING_VALID_PART_ITEM (ETHTOOL,               FALSE),
+        ),
+    ),
     SETTING_INFO (VLAN,
         .valid_parts = NM_META_SETTING_VALID_PARTS (
             NM_META_SETTING_VALID_PART_ITEM (CONNECTION,            TRUE),

commit 7c05ff16322c5b5d5c8e382f94aefb09657aebdb

device: fix complete-connection for veth devices
Otherwise,

    $ nmcli device connect veth0

fails with

    Error: Failed to add/activate new connection: veth.peer: property is not specified

In complete_connection(), we should by default complete ethernet
connections, unless the caller already indicated to want a veth
profile.

Fixes: cd0cf9229d49 ('veth: add support to configure veth interfaces')

src/devices/nm-device-ethernet.c
@@ -1595,6 +1595,60 @@ complete_connection(NMDevice *           device,
     NMSettingWired *s_wired;
     NMSettingPppoe *s_pppoe;
 
+    if (nm_streq0(nm_connection_get_connection_type(connection), NM_SETTING_VETH_SETTING_NAME)) {
+        NMSettingVeth *s_veth;
+        const char *   peer_name     = NULL;
+        const char *   con_peer_name = NULL;
+        int            ifindex;
+
+        nm_utils_complete_generic(nm_device_get_platform(device),
+                                  connection,
+                                  NM_SETTING_VETH_SETTING_NAME,
+                                  existing_connections,
+                                  NULL,
+                                  _("Veth connection"),
+                                  "veth",
+                                  NULL,
+                                  TRUE);
+
+        s_veth = _nm_connection_get_setting(connection, NM_TYPE_SETTING_VETH);
+        if (!s_veth) {
+            s_veth = (NMSettingVeth *) nm_setting_veth_new();
+            nm_connection_add_setting(connection, NM_SETTING(s_veth));
+        }
+
+        ifindex = nm_device_get_ip_ifindex(device);
+        if (ifindex > 0) {
+            const NMPlatformLink *pllink;
+
+            pllink = nm_platform_link_get(nm_device_get_platform(device), ifindex);
+            if (pllink && pllink->type == NM_LINK_TYPE_VETH && pllink->parent > 0) {
+                pllink = nm_platform_link_get(nm_device_get_platform(device), pllink->parent);
+
+                if (pllink && pllink->type == NM_LINK_TYPE_VETH) {
+                    peer_name = pllink->name;
+                }
+            }
+        }
+
+        if (!peer_name) {
+            nm_utils_error_set(error, NM_UTILS_ERROR_UNKNOWN, "cannot find peer for veth device");
+            return FALSE;
+        }
+
+        con_peer_name = nm_setting_veth_get_peer(s_veth);
+        if (con_peer_name) {
+            nm_utils_error_set(error,
+                               NM_UTILS_ERROR_UNKNOWN,
+                               "mismatching veth peer \"%s\"",
+                               con_peer_name);
+            return FALSE;
+        } else
+            g_object_set(s_veth, NM_SETTING_VETH_PEER, peer_name, NULL);
+
+        return TRUE;
+    }
+
     s_pppoe = nm_connection_get_setting_pppoe(connection);
 
     /* We can't telepathically figure out the service name or username, so if

src/devices/nm-device-veth.c
@@ -121,34 +121,6 @@ get_generic_capabilities(NMDevice *device)
     return NM_DEVICE_CAP_CARRIER_DETECT | NM_DEVICE_CAP_IS_SOFTWARE;
 }
 
-static gboolean
-complete_connection(NMDevice *           device,
-                    NMConnection *       connection,
-                    const char *         specific_object,
-                    NMConnection *const *existing_connections,
-                    GError **            error)
-{
-    NMSettingVeth *s_veth;
-
-    nm_utils_complete_generic(nm_device_get_platform(device),
-                              connection,
-                              NM_SETTING_VETH_SETTING_NAME,
-                              existing_connections,
-                              NULL,
-                              _("Veth connection"),
-                              "veth",
-                              NULL,
-                              TRUE);
-
-    s_veth = _nm_connection_get_setting(connection, NM_TYPE_SETTING_VETH);
-    if (!s_veth) {
-        s_veth = (NMSettingVeth *) nm_setting_veth_new();
-        nm_connection_add_setting(connection, NM_SETTING(s_veth));
-    }
-
-    return TRUE;
-}
-
 /*****************************************************************************/
 
 static void
@@ -215,7 +187,6 @@ nm_device_veth_class_init(NMDeviceVethClass *klass)
     device_class->link_changed                = link_changed;
     device_class->parent_changed_notify       = parent_changed_notify;
     device_class->create_and_realize          = create_and_realize;
-    device_class->complete_connection         = complete_connection;
     device_class->get_generic_capabilities    = get_generic_capabilities;
 
     obj_properties[PROP_PEER] = g_param_spec_string(NM_DEVICE_VETH_PEER,