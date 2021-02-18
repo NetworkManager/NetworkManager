b4b2c71193d4ed4cca91e493992e73319c957555
Thomas Haller <thaller@redhat.com>
2021-02-18 07:57:46 +0100

    shared: rename "libnm-systemd-logging-stub.la" to "libnm-log-null.la"
    This is really just a different implementation of
    "nm-glib-aux/nm-logging-fwd.h", that parallels libnm-log-core.
    
    It's also not only useful to shared/systemd, but also share/nm-platform,
    which also requires linking with a logging backend.

--- shared/nm-log-null/nm-logging-null.c
+++ shared/nm-log-null/nm-logging-null.c
@@ -3,7 +3,7 @@
  * Copyright (C) 2018 Red Hat, Inc.
  */
 
-#include "shared/systemd/nm-default-systemd-shared.h"
+#include "nm-glib-aux/nm-default-glib-i18n-lib.h"
 
 #include "nm-glib-aux/nm-logging-fwd.h"
 
