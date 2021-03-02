abe4f0f6ec148e39934c0edd3fbbd1aef6f1fa1a
Thomas Haller <thaller@redhat.com>
2021-03-02 21:02:12 +0100

    platform: move "linux/nl802154.h" to "src/linux-headers/"

--- src/core/platform/wpan/nm-wpan-utils.c
+++ src/core/platform/wpan/nm-wpan-utils.c
@@ -10,7 +10,7 @@
 #include <linux/if.h>
 
 #include "libnm-platform/nm-netlink.h"
-#include "platform/linux/nl802154.h"
+#include "linux-headers/nl802154.h"
 #include "libnm-platform/nm-platform-utils.h"
 
 #define _NMLOG_PREFIX_NAME "wpan-nl802154"
