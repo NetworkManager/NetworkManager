23dbd4df7f96e2a6d2e79746ef284d5cc86dd061
Jagadeesh Kotra <jagadeesh@stdin.top>
2021-03-19 16:54:00 +0530

    example: importing vpn with libnm

---
+++ examples/C/glib/vpn-import-libnm.c
@@ -0,0 +1,77 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2011, 2014 Red Hat, Inc.
+ */
+
+/*
+ * The example shows how to import VPN connection from a file.
+ * 
+ * @author: Jagadeesh Kotra <jagadeesh@stdin.top>
+ *
+ * Compile with:
+ *   gcc -Wall vpn-import-libnm.c -o vpn-import-libnm `pkg-config --cflags --libs libnm`
+ */
+
+#include <glib.h>
+#include <NetworkManager.h>
+#include <stdlib.h>
+
+static void
+add_cb(NMClient *client, GAsyncResult *result, GMainLoop *loop)
+{
+    GError *err = NULL;
+    nm_client_add_connection_finish(client, result, &err);
+    if (err != NULL) {
+        g_print("Error: %s\n", err->message);
+    } else {
+        g_print("Connection Added.\n");
+    }
+
+    g_main_loop_quit(loop);
+}
+
+int
+main(int argc, char **argv)
+{
+    GMainLoop *   loop = g_main_loop_new(NULL, FALSE);
+    GSList *plugins;
+    NMVpnEditorPlugin *editor;
+    NMClient *client;
+    GError *err = NULL;
+    NMConnection *conn = NULL;
+
+    if (argc < 2) {
+        g_print("program takes exactly one(1) argument.\n");
+        exit(1);
+    }
+
+    plugins = nm_vpn_plugin_info_list_load();
+
+    while (plugins != NULL) {
+        NMVpnPluginInfo *plugin      = plugins->data;
+        const char *     plugin_name = nm_vpn_plugin_info_get_name(plugin);
+        g_print("Trying Plugin: %s\n", plugin_name);
+
+        //try to load plugin
+        editor = nm_vpn_plugin_info_load_editor_plugin(plugin, NULL);
+
+        conn = nm_vpn_editor_plugin_import(editor, argv[1], &err);
+        if (err != NULL) {
+            g_print("Error: %s\n", err->message);
+            g_error_free(err);
+            err = NULL;
+        } else {
+            g_print("%s imported with %s plugin.\n", argv[1], plugin_name);
+            break;
+        }
+
+        plugins = plugins->next;
+    }
+
+    g_assert(conn != NULL);
+
+    client = nm_client_new(NULL, NULL);
+
+    nm_client_add_connection_async(client, conn, TRUE, NULL, (GAsyncReadyCallback) add_cb, loop);
+    g_main_loop_run(loop);
+}
