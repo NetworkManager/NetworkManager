NetworkManager/NetworkManager

commit 5bc511203e02dda24a7f45a373e44d5861b5f088

all: make nm_steal_pointer() and g_steal_pointer() more typesafe using typeof()
The previous code would always cast the argument to "void *", and thus
loose some type information.

For example,

  gulong variable = 0;

  g_steal_pointer(&variable);

would compile, when it shouldn't.

In general, we try to avoid redefining glib macros and headers. But we
really want those extra compile time checks that we can get, so let's do
it.

src/libnm-glib-aux/nm-glib.h
@@ -407,30 +407,19 @@ _nm_g_hash_table_get_keys_as_array(GHashTable *hash_table, guint *length)
 
 /*****************************************************************************/
 
-static inline gpointer
-_nm_g_steal_pointer(gpointer pp)
-{
-    gpointer *ptr = (gpointer *) pp;
-    gpointer  ref;
-
-    ref  = *ptr;
-    *ptr = NULL;
-
-    return ref;
-}
-
-#if !GLIB_CHECK_VERSION(2, 44, 0)
-static inline gpointer
-g_steal_pointer(gpointer pp)
-{
-    return _nm_g_steal_pointer(pp);
-}
-#endif
-
 #ifdef g_steal_pointer
     #undef g_steal_pointer
 #endif
-#define g_steal_pointer(pp) ((typeof(*(pp))) _nm_g_steal_pointer(pp))
+
+#define g_steal_pointer(pp)                                \
+    ({                                                     \
+        typeof(*(pp)) *const         _pp           = (pp); \
+        typeof(**_pp) *const         _p            = *_pp; \
+        _nm_unused const void *const _p_type_check = _p;   \
+                                                           \
+        *_pp = NULL;                                       \
+        _p;                                                \
+    })
 
 /*****************************************************************************/
 

src/libnm-std-aux/nm-std-aux.h
@@ -753,18 +753,15 @@ _nm_auto_fclose(FILE **pfd)
 
 /*****************************************************************************/
 
-static inline void *
-_nm_steal_pointer(void *pp)
-{
-    void **ptr = (void **) pp;
-    void * ref;
-
-    ref  = *ptr;
-    *ptr = NULL;
-    return ref;
-}
-
-#define nm_steal_pointer(pp) ((typeof(*(pp))) _nm_steal_pointer(pp))
+#define nm_steal_pointer(pp)                               \
+    ({                                                     \
+        typeof(*(pp)) *const         _pp           = (pp); \
+        typeof(**_pp) *const         _p            = *_pp; \
+        _nm_unused const void *const _p_type_check = _p;   \
+                                                           \
+        *_pp = NULL;                                       \
+        _p;                                                \
+    })
 
 /**
  * nm_steal_int:

commit 5de552893d2ab1e5ab0342aa8b2efa3facb0f9f9

glib-aux: fix compile error for g_steal_pointer() macro
Some compiler versions don't like to dereference and opaque
pointer for typeof:

gcc 8.3.1-5.1.el8 on RHEL:

    In file included from ./src/libnm-glib-aux/nm-macros-internal.h:41,
                     from ./src/libnm-glib-aux/nm-default-glib.h:68,
                     from ./src/libnm-glib-aux/nm-default-glib-i18n-lib.h:13,
                     from ./src/libnm-core-impl/nm-default-libnm-core.h:11,
                     from src/libnm-core-impl/nm-setting-8021x.c:7:
    src/libnm-core-impl/nm-setting-8021x.c: In function '_nm_setting_802_1x_cert_value_to_bytes':
    ./src/libnm-glib-aux/nm-glib.h:417:16: error: dereferencing pointer to incomplete type 'GBytes' {aka 'struct _GBytes'}
             typeof(**_pp) *const         _p            = *_pp; \
                    ^~~~~
    src/libnm-core-impl/nm-setting-8021x.c:361:12: note: in expansion of macro 'g_steal_pointer'
         return g_steal_pointer(&bytes);
                ^~~~~~~~~~~~~~~
    ./src/libnm-glib-aux/nm-glib.h:417:54: error: initialization of 'int * const' from incompatible pointer type 'GBytes *' {aka 'struct _GBytes *'} [-Werror=incompatible-pointer-types]
             typeof(**_pp) *const         _p            = *_pp; \
                                                          ^
    src/libnm-core-impl/nm-setting-8021x.c:361:12: note: in expansion of macro 'g_steal_pointer'
         return g_steal_pointer(&bytes);
                ^~~~~~~~~~~~~~~
    ./src/libnm-glib-aux/nm-glib.h:415:6: error: returning 'int * const' from a function with incompatible return type 'GBytes *' {aka 'struct _GBytes *'} [-Werror=incompatible-pointer-types]
         ({                                                     \
         ~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             typeof(*(pp)) *const         _pp           = (pp); \
             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             typeof(**_pp) *const         _p            = *_pp; \
             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             _nm_unused const void *const _p_type_check = _p;   \
             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                                                                \
                                                                ~
             *_pp = NULL;                                       \
             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             _p;                                                \
             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         })
         ~~
    src/libnm-core-impl/nm-setting-8021x.c:361:12: note: in expansion of macro 'g_steal_pointer'
         return g_steal_pointer(&bytes);
                ^~~~~~~~~~~~~~~

Fixes: 5bc511203e02

src/libnm-glib-aux/nm-glib.h
@@ -413,8 +413,8 @@ _nm_g_hash_table_get_keys_as_array(GHashTable *hash_table, guint *length)
 
 #define g_steal_pointer(pp)                                \
     ({                                                     \
-        typeof(*(pp)) *const         _pp           = (pp); \
-        typeof(**_pp) *const         _p            = *_pp; \
+        typeof(*(pp)) *const _pp                   = (pp); \
+        typeof(*_pp) _p                            = *_pp; \
         _nm_unused const void *const _p_type_check = _p;   \
                                                            \
         *_pp = NULL;                                       \

src/libnm-std-aux/nm-std-aux.h
@@ -755,8 +755,8 @@ _nm_auto_fclose(FILE **pfd)
 
 #define nm_steal_pointer(pp)                               \
     ({                                                     \
-        typeof(*(pp)) *const         _pp           = (pp); \
-        typeof(**_pp) *const         _p            = *_pp; \
+        typeof(*(pp)) *const _pp                   = (pp); \
+        typeof(*_pp) _p                            = *_pp; \
         _nm_unused const void *const _p_type_check = _p;   \
                                                            \
         *_pp = NULL;                                       \