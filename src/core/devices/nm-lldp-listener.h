07e58362b8346dda73ff24972f62577cdb7e8ef5
Thomas Haller <thaller@redhat.com>
2021-04-14 09:39:54 +0200

    device/lldp: simplify NMLldpListener API
    NMLldpListener API was a (refcounted) GObject with start/stop methods.
    That means, a listener instance itself had state, namely whether it was
    running and which ifindex was used. And this was not only internal
    state, but the user had to care about this.
    
    That is all entirely unnecessary. Beside requiring more code and having
    more overhead (of a GObject), it is also harder to use. NMDevice not
    only need to care whether priv->listener is set, it also needs to care
    whether it is running.
    
    Simplify this. The NMLldpListener is no longer ref-counted. As such, the
    notify callback is set in the constructor, and the user will stop
    receiving notifications by destroying the instance. Furthermore, the instance
    can only use one ifindex, that is determined at construct time too.
    
    The state that NMLldpListener now represents is simpler. This simplifies
    the usage from NMDevice, which now only call lldp_setup() to enable and
    disable the listener.
    
    There is also no need to restart the LLDP listener. The only exception
    is, if the ifindex changes. In that case, we throw away the old instance
    and create a new one. Otherwise, the LLDP listener is itself responsible
    to keep running. There is no excuse for it to fail, and if it does, it needs
    to autorecover as good as it can.

--- src/core/devices/nm-lldp-listener.h
+++ src/core/devices/nm-lldp-listener.h
@@ -6,27 +6,20 @@
 #ifndef __NM_LLDP_LISTENER__
 #define __NM_LLDP_LISTENER__
 
-#define NM_TYPE_LLDP_LISTENER (nm_lldp_listener_get_type())
-#define NM_LLDP_LISTENER(obj) \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj), NM_TYPE_LLDP_LISTENER, NMLldpListener))
-#define NM_LLDP_LISTENER_CLASS(klass) \
-    (G_TYPE_CHECK_CLASS_CAST((klass), NM_TYPE_LLDP_LISTENER, NMLldpListenerClass))
-#define NM_IS_LLDP_LISTENER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), NM_TYPE_LLDP_LISTENER))
-#define NM_IS_LLDP_LISTENER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), NM_TYPE_LLDP_LISTENER))
-#define NM_LLDP_LISTENER_GET_CLASS(obj) \
-    (G_TYPE_INSTANCE_GET_CLASS((obj), NM_TYPE_LLDP_LISTENER, NMLldpListenerClass))
+/*****************************************************************************/
 
-#define NM_LLDP_LISTENER_NEIGHBORS "neighbors"
+typedef void (*NMLldpListenerNotify)(NMLldpListener *self, gpointer user_data);
 
-typedef struct _NMLldpListenerClass NMLldpListenerClass;
+NMLldpListener *nm_lldp_listener_new(int                  ifindex,
+                                     NMLldpListenerNotify notify_callback,
+                                     gpointer             notify_user_data,
+                                     GError **            error);
+void            nm_lldp_listener_destroy(NMLldpListener *self);
 
-GType           nm_lldp_listener_get_type(void);
-NMLldpListener *nm_lldp_listener_new(void);
-gboolean        nm_lldp_listener_start(NMLldpListener *self, int ifindex, GError **error);
-void            nm_lldp_listener_stop(NMLldpListener *self);
-gboolean        nm_lldp_listener_is_running(NMLldpListener *self);
-
+int       nm_lldp_listener_get_ifindex(NMLldpListener *self);
 GVariant *nm_lldp_listener_get_neighbors(NMLldpListener *self);
+
+/*****************************************************************************/
 
 GVariant *nmtst_lldp_parse_from_raw(const guint8 *raw_data, gsize raw_len);
 
