e9c1d2a9dd8905a0c5655086d791dce4e7316de6
Thomas Haller <thaller@redhat.com>
2021-05-05 22:39:28 +0200

    firewall: add new "nm-firewall-utils.[ch]" module

---
+++ src/core/nm-firewall-utils.c
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2004 - 2016 Red Hat, Inc.
+ * Copyright (C) 2005 - 2008 Novell, Inc.
+ */
+
+#include "src/core/nm-default-daemon.h"
+
+#include "nm-firewall-utils.h"
---
+++ src/core/nm-firewall-utils.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2004 - 2016 Red Hat, Inc.
+ * Copyright (C) 2005 - 2008 Novell, Inc.
+ */
+
+#ifndef __NM_FIREWALL_UTILS_H__
+#define __NM_FIREWALL_UTILS_H__
+
+#endif /* __NM_FIREWALL_UTILS_H__ */
