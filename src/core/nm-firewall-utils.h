1da1ad9c9995f6454ef39da3d8ba85b0a23e919f
Thomas Haller <thaller@redhat.com>
2021-05-06 16:50:25 +0200

    firewall: make firewall-backend configurable via "NetworkManager.conf"
    "iptables" and "nftables" will be supported. Currently, the code is
    unused and only "iptables" is supported.

--- src/core/nm-firewall-utils.h
+++ src/core/nm-firewall-utils.h
@@ -7,6 +7,16 @@
 #ifndef __NM_FIREWALL_UTILS_H__
 #define __NM_FIREWALL_UTILS_H__
 
+typedef enum {
+    NM_FIREWALL_BACKEND_UNKNOWN,
+    NM_FIREWALL_BACKEND_IPTABLES,
+    NM_FIREWALL_BACKEND_NFTABLES,
+} NMFirewallBackend;
+
+NMFirewallBackend nm_firewall_utils_get_backend(void);
+
+/*****************************************************************************/
+
 typedef struct _NMFirewallConfig NMFirewallConfig;
 
 NMFirewallConfig *nm_firewall_config_new(const char *ip_iface, in_addr_t addr, guint8 plen);
