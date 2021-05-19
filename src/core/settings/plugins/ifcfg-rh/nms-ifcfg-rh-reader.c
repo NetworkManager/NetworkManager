NetworkManager/NetworkManager

commit d946aa0c503efe3f5dbf5c38a76b5b7625775bde

wired-setting: add support to accept-all-mac-addresses
This patch is introducing the wired setting accept-all-mac-addresses
property. The value corresponds to the kernel flag IFF_PROMISC.

When accept-all-mac-address is enabled, the interface will accept all
the packets without checking the destination mac address.

Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-reader.c
@@ -5179,6 +5179,11 @@ make_wired_setting(shvarFile *ifcfg, const char *file, NMSetting8021x **s_8021x,
     }
     nm_clear_g_free(&value);
 
+    g_object_set(s_wired,
+                 NM_SETTING_WIRED_ACCEPT_ALL_MAC_ADDRESSES,
+                 svGetValueTernary(ifcfg, "ACCEPT_ALL_MAC_ADDRESSES"),
+                 NULL);
+
     if (!found) {
         g_set_error(error,
                     NM_UTILS_ERROR,

src/core/devices/nm-device.c
@@ -693,6 +693,8 @@ typedef struct _NMDevicePrivate {
     } stats;
 
     bool mtu_force_set_done : 1;
+
+    NMOptionBool promisc_reset;
 } NMDevicePrivate;
 
 G_DEFINE_ABSTRACT_TYPE(NMDevice, nm_device, NM_TYPE_DBUS_OBJECT)
@@ -8435,8 +8437,10 @@ activate_stage2_device_config(NMDevice *self)
     NMDevicePrivate *priv = NM_DEVICE_GET_PRIVATE(self);
     NMDeviceClass *  klass;
     NMActStageReturn ret;
+    NMSettingWired * s_wired;
     gboolean         no_firmware = FALSE;
     CList *          iter;
+    NMTernary        accept_all_mac_addresses;
 
     nm_device_state_changed(self, NM_DEVICE_STATE_CONFIG, NM_DEVICE_STATE_REASON_NONE);
 
@@ -8492,6 +8496,25 @@ activate_stage2_device_config(NMDevice *self)
         else if (priv->act_request.obj && nm_device_sys_iface_state_is_external(self)
                  && slave_state <= NM_DEVICE_STATE_DISCONNECTED)
             nm_device_queue_recheck_assume(info->slave);
+    }
+
+    s_wired = nm_device_get_applied_setting(self, NM_TYPE_SETTING_WIRED);
+    accept_all_mac_addresses =
+        s_wired ? nm_setting_wired_get_accept_all_mac_addresses(s_wired) : NM_TERNARY_DEFAULT;
+    if (accept_all_mac_addresses != NM_TERNARY_DEFAULT) {
+        int ifi_flags;
+
+        ifi_flags = nm_platform_link_get_ifi_flags(nm_device_get_platform(self),
+                                                   nm_device_get_ip_ifindex(self),
+                                                   IFF_PROMISC);
+        if (ifi_flags >= 0 && ((!!ifi_flags) != (!!accept_all_mac_addresses))) {
+            nm_platform_link_change_flags(nm_device_get_platform(self),
+                                          nm_device_get_ip_ifindex(self),
+                                          IFF_PROMISC,
+                                          !!accept_all_mac_addresses);
+            if (priv->promisc_reset == NM_OPTION_BOOL_DEFAULT)
+                priv->promisc_reset = !accept_all_mac_addresses;
+        }
     }
 
     lldp_setup(self, NM_TERNARY_DEFAULT);
@@ -16007,6 +16030,14 @@ nm_device_cleanup(NMDevice *self, NMDeviceStateReason reason, CleanupType cleanu
 
     _ethtool_state_reset(self);
 
+    if (priv->promisc_reset != NM_OPTION_BOOL_DEFAULT && ifindex > 0) {
+        nm_platform_link_change_flags(nm_device_get_platform(self),
+                                      ifindex,
+                                      IFF_PROMISC,
+                                      !!priv->promisc_reset);
+        priv->promisc_reset = NM_OPTION_BOOL_DEFAULT;
+    }
+
     _cleanup_generic_post(self, cleanup_type);
 }
 
@@ -18212,6 +18243,8 @@ nm_device_init(NMDevice *self)
 
     priv->v4_commit_first_time = TRUE;
     priv->v6_commit_first_time = TRUE;
+
+    priv->promisc_reset = NM_OPTION_BOOL_DEFAULT;
 }
 
 static GObject *

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-utils.c
@@ -800,6 +800,7 @@ nms_ifcfg_rh_utils_is_numbered_tag_impl(const char *key,
     }
 
 const NMSIfcfgKeyTypeInfo nms_ifcfg_well_known_keys[] = {
+    _KEY_TYPE("ACCEPT_ALL_MAC_ADDRESSES", NMS_IFCFG_KEY_TYPE_IS_PLAIN),
     _KEY_TYPE("ACD_TIMEOUT", NMS_IFCFG_KEY_TYPE_IS_PLAIN),
     _KEY_TYPE("ADDRESS", NMS_IFCFG_KEY_TYPE_IS_NUMBERED),
     _KEY_TYPE("AP_ISOLATION", NMS_IFCFG_KEY_TYPE_IS_PLAIN),

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-utils.h
@@ -33,7 +33,7 @@ typedef struct {
     NMSIfcfgKeyTypeFlags key_flags;
 } NMSIfcfgKeyTypeInfo;
 
-extern const NMSIfcfgKeyTypeInfo nms_ifcfg_well_known_keys[247];
+extern const NMSIfcfgKeyTypeInfo nms_ifcfg_well_known_keys[248];
 
 const NMSIfcfgKeyTypeInfo *nms_ifcfg_well_known_key_find_info(const char *key, gssize *out_idx);
 

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-writer.c
@@ -1170,6 +1170,10 @@ write_wired_setting(NMConnection *connection, shvarFile *ifcfg, GError **error)
 
     svSetValueStr(ifcfg, "TYPE", TYPE_ETHERNET);
 
+    svSetValueTernary(ifcfg,
+                      "ACCEPT_ALL_MAC_ADDRESSES",
+                      nm_setting_wired_get_accept_all_mac_addresses(s_wired));
+
     return TRUE;
 }
 

src/libnm-client-impl/libnm.ver
@@ -1788,4 +1788,5 @@ global:
 libnm_1_32_0 {
 global:
 	nm_setting_match_new;
+	nm_setting_wired_get_accept_all_mac_addresses;
 } libnm_1_30_0;

src/libnm-core-impl/nm-setting-wired.c
@@ -44,7 +44,8 @@ NM_GOBJECT_PROPERTIES_DEFINE(NMSettingWired,
                              PROP_S390_NETTYPE,
                              PROP_S390_OPTIONS,
                              PROP_WAKE_ON_LAN,
-                             PROP_WAKE_ON_LAN_PASSWORD, );
+                             PROP_WAKE_ON_LAN_PASSWORD,
+                             PROP_ACCEPT_ALL_MAC_ADDRESSES, );
 
 typedef struct {
     struct {
@@ -62,6 +63,7 @@ typedef struct {
     char *                  s390_nettype;
     char *                  wol_password;
     NMSettingWiredWakeOnLan wol;
+    NMTernary               accept_all_mac_addresses;
     guint32                 speed;
     guint32                 mtu;
     bool                    auto_negotiate : 1;
@@ -735,6 +737,22 @@ nm_setting_wired_get_wake_on_lan_password(NMSettingWired *setting)
     return NM_SETTING_WIRED_GET_PRIVATE(setting)->wol_password;
 }
 
+/**
+ * nm_setting_wired_get_accept_all_mac_addresses:
+ * @setting: the #NMSettingWired
+ *
+ * Returns: the #NMSettingWired:accept-all-mac-addresses property of the setting
+ *
+ * Since: 1.32
+ **/
+NMTernary
+nm_setting_wired_get_accept_all_mac_addresses(NMSettingWired *setting)
+{
+    g_return_val_if_fail(NM_IS_SETTING_WIRED(setting), NM_TERNARY_DEFAULT);
+
+    return NM_SETTING_WIRED_GET_PRIVATE(setting)->accept_all_mac_addresses;
+}
+
 static gboolean
 verify(NMSetting *setting, NMConnection *connection, GError **error)
 {
@@ -1039,6 +1057,9 @@ get_property(GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
     case PROP_WAKE_ON_LAN_PASSWORD:
         g_value_set_string(value, priv->wol_password);
         break;
+    case PROP_ACCEPT_ALL_MAC_ADDRESSES:
+        g_value_set_enum(value, priv->accept_all_mac_addresses);
+        break;
     default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
         break;
@@ -1174,6 +1195,9 @@ set_property(GObject *object, guint prop_id, const GValue *value, GParamSpec *ps
         g_free(priv->wol_password);
         priv->wol_password = g_value_dup_string(value);
         break;
+    case PROP_ACCEPT_ALL_MAC_ADDRESSES:
+        priv->accept_all_mac_addresses = g_value_get_enum(value);
+        break;
     default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
         break;
@@ -1191,7 +1215,8 @@ nm_setting_wired_init(NMSettingWired *setting)
     priv->mac_address_blacklist = g_array_new(TRUE, FALSE, sizeof(char *));
     g_array_set_clear_func(priv->mac_address_blacklist, (GDestroyNotify) clear_blacklist_item);
 
-    priv->wol = NM_SETTING_WIRED_WAKE_ON_LAN_DEFAULT;
+    priv->wol                      = NM_SETTING_WIRED_WAKE_ON_LAN_DEFAULT;
+    priv->accept_all_mac_addresses = NM_TERNARY_DEFAULT;
 }
 
 /**
@@ -1684,6 +1709,29 @@ nm_setting_wired_class_init(NMSettingWiredClass *klass)
                             "",
                             NULL,
                             G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+    /**
+     * NMSettingWired:accept-all-mac-addresses:
+     *
+     * When %TRUE, setup the interface to accept packets for all MAC addresses.
+     * This is enabling the kernel interface flag IFF_PROMISC.
+     * When %FALSE, the interface will only accept the packets with the
+     * interface destination mac address or broadcast.
+     *
+     * Since: 1.32
+     **/
+    /* ---ifcfg-rh---
+     * property: accept-all-mac-addresses
+     * variable: ACCEPT_ALL_MAC_ADDRESSES
+     * description: Enforce the interface to accept all the packets.
+     * ---end---
+     */
+    obj_properties[PROP_ACCEPT_ALL_MAC_ADDRESSES] =
+        g_param_spec_enum(NM_SETTING_WIRED_ACCEPT_ALL_MAC_ADDRESSES,
+                          "",
+                          "",
+                          NM_TYPE_TERNARY,
+                          NM_TERNARY_DEFAULT,
+                          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
 
     g_object_class_install_properties(object_class, _PROPERTY_ENUMS_LAST, obj_properties);
 

src/libnm-core-impl/tests/test-general.c
@@ -3841,6 +3841,7 @@ test_connection_diff_a_only(void)
              {NM_SETTING_WIRED_S390_OPTIONS, NM_SETTING_DIFF_RESULT_IN_A},
              {NM_SETTING_WIRED_WAKE_ON_LAN, NM_SETTING_DIFF_RESULT_IN_A},
              {NM_SETTING_WIRED_WAKE_ON_LAN_PASSWORD, NM_SETTING_DIFF_RESULT_IN_A},
+             {NM_SETTING_WIRED_ACCEPT_ALL_MAC_ADDRESSES, NM_SETTING_DIFF_RESULT_IN_A},
              {NULL, NM_SETTING_DIFF_RESULT_UNKNOWN},
          }},
         {NM_SETTING_IP4_CONFIG_SETTING_NAME,

commit 38246b18021cff44bb5cc2230c70523293a131e7

ifcfg: fix wired reader for ACCEPT_ALL_MAC_ADDRESSES key
When the ACCEPT_ALL_MAC_ADDRESSES key is found by the wired reader, the
wired setting was not being created.

Fixes: d946aa0c503e ('wired-setting: add support to accept-all-mac-addresses')
Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-reader.c
@@ -5022,6 +5022,7 @@ make_wired_setting(shvarFile *ifcfg, const char *file, NMSetting8021x **s_8021x,
     const char *                    cvalue;
     gs_free char *                  value = NULL;
     gboolean                        found = FALSE;
+    NMTernary                       accept_all_mac_addresses;
 
     s_wired = NM_SETTING_WIRED(nm_setting_wired_new());
 
@@ -5180,10 +5181,14 @@ make_wired_setting(shvarFile *ifcfg, const char *file, NMSetting8021x **s_8021x,
     }
     nm_clear_g_free(&value);
 
-    g_object_set(s_wired,
-                 NM_SETTING_WIRED_ACCEPT_ALL_MAC_ADDRESSES,
-                 svGetValueTernary(ifcfg, "ACCEPT_ALL_MAC_ADDRESSES"),
-                 NULL);
+    accept_all_mac_addresses = svGetValueTernary(ifcfg, "ACCEPT_ALL_MAC_ADDRESSES");
+    if (accept_all_mac_addresses != NM_TERNARY_DEFAULT) {
+        g_object_set(s_wired,
+                     NM_SETTING_WIRED_ACCEPT_ALL_MAC_ADDRESSES,
+                     accept_all_mac_addresses,
+                     NULL);
+        found = TRUE;
+    }
 
     if (!found) {
         g_set_error(error,