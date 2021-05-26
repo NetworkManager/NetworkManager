NetworkManager/NetworkManager

commit 02dbba49d6d316077885c832fba57369cadd4195

libnm: fix leak in nm_vpn_service_plugin_read_vpn_details()
Found by Coverity:

  Error: RESOURCE_LEAK (CWE-772):
  NetworkManager-1.31.3/src/libnm-client-impl/nm-vpn-service-plugin.c:814: alloc_fn: Storage is returned from allocation function "g_string_new".
  NetworkManager-1.31.3/src/libnm-client-impl/nm-vpn-service-plugin.c:814: var_assign: Assigning: "key" = storage returned from "g_string_new(line->str + strlen("DATA_KEY="))".
  NetworkManager-1.31.3/src/libnm-client-impl/nm-vpn-service-plugin.c:815: var_assign: Assigning: "str" = "key".
  NetworkManager-1.31.3/src/libnm-client-impl/nm-vpn-service-plugin.c:855: leaked_storage: Variable "str" going out of scope leaks the storage it points to.
  NetworkManager-1.31.3/src/libnm-client-impl/nm-vpn-service-plugin.c:855: leaked_storage: Variable "key" going out of scope leaks the storage it points to.
  #  853|           NM_SET_OUT(out_secrets, g_steal_pointer(&secrets));
  #  854|       }
  #  855|->     return success;
  #  856|   }
  #  857|

Fixes: 3dfb72b92687 ('service-plugin: allow continuations in the auth-dialog protocol')

src/libnm-client-impl/nm-vpn-service-plugin.c
@@ -755,8 +755,9 @@ nm_vpn_service_plugin_read_vpn_details(int fd, GHashTable **out_data, GHashTable
     gs_unref_hashtable GHashTable *secrets = NULL;
     gboolean                       success = FALSE;
     GHashTable *                   hash    = NULL;
-    GString *                      key = NULL, *val = NULL;
-    nm_auto_free_gstring GString *line = NULL;
+    nm_auto_free_gstring GString *key      = NULL;
+    nm_auto_free_gstring GString *val      = NULL;
+    nm_auto_free_gstring GString *line     = NULL;
     char                          c;
 
     GString *str = NULL;

commit c64ef1b5c1b0292db9666dcb189ef715a3642766

libnm: fix logic and double free in nm_vpn_service_plugin_read_vpn_details()
"val" and "key" are now marked as nm_auto. They are freed at the end,
and we should not free them before breaking the loop (at least not,
without also clearing the variables).

Fixes: 02dbba49d6d3 ('libnm: fix leak in nm_vpn_service_plugin_read_vpn_details()')

src/libnm-client-impl/nm-vpn-service-plugin.c
@@ -759,8 +759,7 @@ nm_vpn_service_plugin_read_vpn_details(int fd, GHashTable **out_data, GHashTable
     nm_auto_free_gstring GString *val      = NULL;
     nm_auto_free_gstring GString *line     = NULL;
     char                          c;
-
-    GString *str = NULL;
+    GString *                     str = NULL;
 
     if (out_data)
         g_return_val_if_fail(*out_data == NULL, FALSE);
@@ -808,16 +807,12 @@ nm_vpn_service_plugin_read_vpn_details(int fd, GHashTable **out_data, GHashTable
             /* finish marker */
             break;
         } else if (strncmp(line->str, DATA_KEY_TAG, strlen(DATA_KEY_TAG)) == 0) {
-            if (key != NULL) {
+            if (nm_clear_g_string(&key))
                 g_warning("a value expected");
-                g_string_free(key, TRUE);
-            }
             key  = g_string_new(line->str + strlen(DATA_KEY_TAG));
             str  = key;
             hash = data;
         } else if (strncmp(line->str, DATA_VAL_TAG, strlen(DATA_VAL_TAG)) == 0) {
-            if (val != NULL)
-                g_string_free(val, TRUE);
             if (val || !key || hash != data) {
                 g_warning("%s not preceded by %s", DATA_VAL_TAG, DATA_KEY_TAG);
                 break;
@@ -825,16 +820,12 @@ nm_vpn_service_plugin_read_vpn_details(int fd, GHashTable **out_data, GHashTable
             val = g_string_new(line->str + strlen(DATA_VAL_TAG));
             str = val;
         } else if (strncmp(line->str, SECRET_KEY_TAG, strlen(SECRET_KEY_TAG)) == 0) {
-            if (key != NULL) {
+            if (nm_clear_g_string(&key))
                 g_warning("a value expected");
-                g_string_free(key, TRUE);
-            }
             key  = g_string_new(line->str + strlen(SECRET_KEY_TAG));
             str  = key;
             hash = secrets;
         } else if (strncmp(line->str, SECRET_VAL_TAG, strlen(SECRET_VAL_TAG)) == 0) {
-            if (val != NULL)
-                g_string_free(val, TRUE);
             if (val || !key || hash != secrets) {
                 g_warning("%s not preceded by %s", SECRET_VAL_TAG, SECRET_KEY_TAG);
                 break;