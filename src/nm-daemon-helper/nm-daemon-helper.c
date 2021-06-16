NetworkManager/NetworkManager

commit 9b802ff7f2d888d8abe8e6c2f83b8d0875e7cf2d

nm-daemon-helper: fix build with musl libc
NSS is not available in musl libc:

./src/nm-daemon-helper/nm-daemon-helper.c:9:10: fatal error: nss.h: No such file or directory
    9 | #include <nss.h>
      |          ^~~~~~~

src/nm-daemon-helper/nm-daemon-helper.c
@@ -6,7 +6,9 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#if defined(__GLIBC__)
 #include <nss.h>
+#endif
 
 #include "libnm-std-aux/nm-std-aux.h"
 
@@ -68,7 +70,9 @@ cmd_resolve_address(void)
         return RETURN_INVALID_ARGS;
 
     memset(&sockaddr, 0, sizeof(sockaddr));
+#if defined(__GLIBC__)
     __nss_configure_lookup("hosts", "dns");
+#endif
 
     if (inet_pton(AF_INET, address, &sockaddr.in.sin_addr) == 1) {
         sockaddr.in.sin_family = AF_INET;

commit e3e0afb0110c23b6f4b43ff4b8927244bc6a4356

nm-daemon-helper: fix indentation
Ops.

Fixes: 9b802ff7f2d8 ('nm-daemon-helper: fix build with musl libc')

src/nm-daemon-helper/nm-daemon-helper.c
@@ -7,7 +7,7 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #if defined(__GLIBC__)
-#include <nss.h>
+    #include <nss.h>
 #endif
 
 #include "libnm-std-aux/nm-std-aux.h"