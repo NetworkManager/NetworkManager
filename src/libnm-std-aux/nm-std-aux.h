309eba10780f83001f34b0fc8ea2955f5109233a
Thomas Haller <thaller@redhat.com>
2021-06-25 09:47:38 +0200

    std-aux/glib-aux: move NM_AUTO_PROTECT_ERRNO() to libnm-std-aux
    (cherry picked from commit 2b55408cc79831860a6b6585b2d4d8ac4f6cd20f)

--- src/libnm-std-aux/nm-std-aux.h
+++ src/libnm-std-aux/nm-std-aux.h
@@ -695,6 +695,14 @@ nm_close(int fd)
 NM_AUTO_DEFINE_FCN_VOID0(void *, _nm_auto_free_impl, free);
 #define nm_auto_free nm_auto(_nm_auto_free_impl)
 
+static inline void
+_nm_auto_protect_errno(const int *p_saved_errno)
+{
+    errno = *p_saved_errno;
+}
+#define NM_AUTO_PROTECT_ERRNO(errsv_saved) \
+    nm_auto(_nm_auto_protect_errno) _nm_unused const int errsv_saved = (errno)
+
 /*****************************************************************************/
 
 static inline void
