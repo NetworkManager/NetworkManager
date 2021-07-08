NetworkManager/NetworkManager

commit e874c5bf6b7819adc1c1bb7ae2cec60f8ec511a2

random: Provide missing gettid() declaration
Provide missing declaration of gettid() function necessary for entropy
generation for hash table in the _rand_init_seed() function in the
src/libnm-glib-aux/nm-random-utils.c.

Fixes: 3649efe2b574 ('glib-aux: put more effort into seeding GRand fallback for nm_utils_random_bytes()')

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/754

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/918

src/libnm-glib-aux/nm-shared-utils.h
@@ -30,6 +30,16 @@ nm_is_ascii(char ch)
 
 /*****************************************************************************/
 
+#include <sys/syscall.h>
+static inline pid_t
+_nm_gettid(void)
+{
+    return (pid_t) syscall(SYS_gettid);
+}
+#define gettid() _nm_gettid()
+
+/*****************************************************************************/
+
 pid_t nm_utils_gettid(void);
 
 gboolean _nm_assert_on_main_thread(void);

src/libnm-systemd-shared/sd-adapt-shared/nm-sd-adapt-shared.h
@@ -75,13 +75,6 @@ raw_getpid(void)
     #endif
 }
 
-static inline pid_t
-_nm_gettid(void)
-{
-    return (pid_t) syscall(SYS_gettid);
-}
-    #define gettid() _nm_gettid()
-
     /* we build with C11 and thus <uchar.h> provides char32_t,char16_t. */
     #define HAVE_CHAR32_T 1
     #define HAVE_CHAR16_T 1

commit 0c4a65929c8b63280498a3f9b1952ad42cd0b846

glib-aux: use nm_utils_gettid() instead of gettid()
I missed that we already have a gettid() wrapper. Drop the duplicated
again and use nm_utils_gettid().

Fixes: e874c5bf6b78 ('random: Provide missing gettid() declaration')

Makefile.am
@@ -4436,7 +4436,9 @@ src_core_tests_test_systemd_LDFLAGS = \
 src_core_tests_test_systemd_LDADD = \
 	src/libnm-systemd-core/libnm-systemd-core.la \
 	src/libnm-systemd-shared/libnm-systemd-shared.la \
+	src/libnm-glib-aux/libnm-glib-aux.la \
 	src/libnm-log-null/libnm-log-null.la \
+	src/libnm-std-aux/libnm-std-aux.la \
 	src/c-siphash/libc-siphash.la \
 	$(GLIB_LIBS) \
 	$(CODE_COVERAGE_LDFLAGS) \

src/libnm-glib-aux/nm-random-utils.c
@@ -61,7 +61,7 @@ _rand_init_seed(guint32 seed_array[static SEED_ARRAY_SIZE], GRand *rand)
     seed_array[seed_idx++] = ((guint64) now_nsec);
     seed_array[seed_idx++] = _pid_hash(getpid());
     seed_array[seed_idx++] = _pid_hash(getppid());
-    seed_array[seed_idx++] = _pid_hash(gettid());
+    seed_array[seed_idx++] = _pid_hash(nm_utils_gettid());
 
     nm_assert(seed_idx == SEED_ARRAY_SIZE);
 }

src/libnm-glib-aux/nm-shared-utils.h
@@ -30,16 +30,6 @@ nm_is_ascii(char ch)
 
 /*****************************************************************************/
 
-#include <sys/syscall.h>
-static inline pid_t
-_nm_gettid(void)
-{
-    return (pid_t) syscall(SYS_gettid);
-}
-#define gettid() _nm_gettid()
-
-/*****************************************************************************/
-
 pid_t nm_utils_gettid(void);
 
 gboolean _nm_assert_on_main_thread(void);

src/libnm-systemd-shared/sd-adapt-shared/nm-sd-adapt-shared.h
@@ -75,6 +75,8 @@ raw_getpid(void)
     #endif
 }
 
+#define gettid() nm_utils_gettid()
+
     /* we build with C11 and thus <uchar.h> provides char32_t,char16_t. */
     #define HAVE_CHAR32_T 1
     #define HAVE_CHAR16_T 1

src/core/tests/meson.build
@@ -44,7 +44,9 @@ exe = executable(
   link_with: [
     libnm_systemd_shared,
     libnm_systemd_core,
+    libnm_glib_aux,
     libnm_log_null,
+    libnm_std_aux,
     libc_siphash,
   ],
 )