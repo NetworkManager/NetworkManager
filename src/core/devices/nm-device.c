3dd854eb1ba5d13d62d890073fad1e0ec1205ea7
Thomas Haller <thaller@redhat.com>
2022-01-21 10:47:31 +0100

    device: initialize nm_auto variable in _ethtool_features_reset()
    The compiler is often adament to warn about maybe-uninitialized.

--- src/core/devices/nm-device.c
+++ src/core/devices/nm-device.c
@@ -2183,7 +2183,7 @@ _prop_get_x_generate_mac_address_mask(NMDevice *self, NMConnection *connection, 
 static void
 _ethtool_features_reset(NMDevice *self, NMPlatform *platform, EthtoolState *ethtool_state)
 {
-    gs_free NMEthtoolFeatureStates *features;
+    gs_free NMEthtoolFeatureStates *features = NULL;
 
     features = g_steal_pointer(&ethtool_state->features);
 
