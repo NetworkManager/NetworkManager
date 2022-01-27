NetworkManager/NetworkManager

commit ac8c3a711148d327bf6ee4fa99fc99eb1c8ca160

glib-aux: improve nm_ref_string_equals_str() to work for non-C-strings
A NMRefString tracks the length seprately, it thus may not be a NUL terminated
string (although, there is always a NUL character at the end of the buffer).

As such, the previous implementation did not work correctly in when comparing
for example NMRefString("a\0b") with "a". There was even a comment hinting
to that fact. Instead of making obscure comments, fix the implementation to
behave always correctly.

src/libnm-glib-aux/nm-ref-string.h
@@ -95,13 +95,15 @@ nm_ref_string_get_len(NMRefString *rstr)
 }
 
 static inline gboolean
-nm_ref_string_equals_str(NMRefString *rstr, const char *s)
+nm_ref_string_equals_str(NMRefString *rstr, const char *str)
 {
-    /* Note that rstr->len might be greater than strlen(rstr->str). This function does
-     * not cover that and would ignore everything after the first NUL byte. If you need
-     * that distinction, this function is not for you. */
+    if (!str)
+        return (!!rstr);
 
-    return rstr ? (s && nm_streq(rstr->str, s)) : (s == NULL);
+    if (!rstr)
+        return FALSE;
+
+    return rstr->len == strlen(str) && (rstr->str == str || memcmp(rstr->str, str, rstr->len) == 0);
 }
 
 static inline gboolean

commit b755539aa6ff56f9b7164db77cc0f9bc979ef984

glib-aux: fix nm_ref_string_equal_str()
Fix comparison with a NULL string.

The only current caller is nm-supplicant-interface.c, and this bug
causes a missing cleanup of the CurrentBSS property on disconnect.

Fixes: ac8c3a711148 ('glib-aux: improve nm_ref_string_equals_str() to work for non-C-strings')

https://bugzilla.redhat.com/show_bug.cgi?id=1983735
https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1073

src/libnm-glib-aux/nm-ref-string.h
@@ -131,7 +131,7 @@ static inline gboolean
 nm_ref_string_equal_str(NMRefString *rstr, const char *str)
 {
     if (!str)
-        return (!!rstr);
+        return !rstr;
 
     if (!rstr)
         return FALSE;