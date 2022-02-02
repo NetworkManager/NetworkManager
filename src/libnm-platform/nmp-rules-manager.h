ea4f6d7994af3820eb4646ea2bbe915fa27b09a2
Thomas Haller <thaller@redhat.com>
2022-02-02 15:43:04 +0100

    platform: rename NMPRulesManager API to NMPRouteManager
    Routes of type blackhole, unreachable, prohibit don't have an
    ifindex/device. They are thus in many ways similar to routing rules,
    as they are global. We need a mediator to keep track which routes
    to configure.
    
    This will be very similar to what NMPRulesManager already does for
    routing rules. Rename the API, so that it also can be used for routes.
    
    Renaming the file will be done next, so that git's rename detection
    doesn't get too confused.

--- src/libnm-platform/nmp-rules-manager.h
+++ src/libnm-platform/nmp-rules-manager.h
@@ -1,53 +1,53 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#ifndef __NMP_RULES_MANAGER_H__
-#define __NMP_RULES_MANAGER_H__
+#ifndef __NMP_ROUTE_MANAGER_H__
+#define __NMP_ROUTE_MANAGER_H__
 
 #include "nm-platform.h"
 
 /*****************************************************************************/
 
-#define NMP_RULES_MANAGER_EXTERN_WEAKLY_TRACKED_USER_TAG ((const void *) nmp_rules_manager_new)
+#define NMP_ROUTE_MANAGER_EXTERN_WEAKLY_TRACKED_USER_TAG ((const void *) nmp_route_manager_new)
 
-typedef struct _NMPRulesManager NMPRulesManager;
+typedef struct _NMPRouteManager NMPRouteManager;
 
-NMPRulesManager *nmp_rules_manager_new(NMPlatform *platform);
+NMPRouteManager *nmp_route_manager_new(NMPlatform *platform);
 
-void nmp_rules_manager_ref(NMPRulesManager *self);
-void nmp_rules_manager_unref(NMPRulesManager *self);
+void nmp_route_manager_ref(NMPRouteManager *self);
+void nmp_route_manager_unref(NMPRouteManager *self);
 
-#define nm_auto_unref_rules_manager nm_auto(_nmp_rules_manager_unref)
-NM_AUTO_DEFINE_FCN0(NMPRulesManager *, _nmp_rules_manager_unref, nmp_rules_manager_unref);
+#define nm_auto_unref_route_manager nm_auto(_nmp_route_manager_unref)
+NM_AUTO_DEFINE_FCN0(NMPRouteManager *, _nmp_route_manager_unref, nmp_route_manager_unref);
 
-void nmp_rules_manager_track(NMPRulesManager             *self,
-                             const NMPlatformRoutingRule *routing_rule,
-                             gint32                       track_priority,
-                             gconstpointer                user_tag,
-                             gconstpointer                user_tag_untrack);
+void nmp_route_manager_track_rule(NMPRouteManager             *self,
+                                  const NMPlatformRoutingRule *routing_rule,
+                                  gint32                       track_priority,
+                                  gconstpointer                user_tag,
+                                  gconstpointer                user_tag_untrack);
 
-void nmp_rules_manager_track_default(NMPRulesManager *self,
-                                     int              addr_family,
-                                     gint32           track_priority,
-                                     gconstpointer    user_tag);
+void nmp_route_manager_track_rule_default(NMPRouteManager *self,
+                                          int              addr_family,
+                                          gint32           track_priority,
+                                          gconstpointer    user_tag);
 
-void nmp_rules_manager_track_from_platform(NMPRulesManager *self,
-                                           NMPlatform      *platform,
-                                           int              addr_family,
-                                           gint32           tracking_priority,
-                                           gconstpointer    user_tag);
+void nmp_route_manager_track_rule_from_platform(NMPRouteManager *self,
+                                                NMPlatform      *platform,
+                                                int              addr_family,
+                                                gint32           tracking_priority,
+                                                gconstpointer    user_tag);
 
-void nmp_rules_manager_untrack(NMPRulesManager             *self,
-                               const NMPlatformRoutingRule *routing_rule,
-                               gconstpointer                user_tag);
+void nmp_route_manager_untrack_rule(NMPRouteManager             *self,
+                                    const NMPlatformRoutingRule *routing_rule,
+                                    gconstpointer                user_tag);
 
-void nmp_rules_manager_set_dirty(NMPRulesManager *self, gconstpointer user_tag);
+void nmp_route_manager_set_dirty(NMPRouteManager *self, gconstpointer user_tag);
 
-void nmp_rules_manager_untrack_all(NMPRulesManager *self,
+void nmp_route_manager_untrack_all(NMPRouteManager *self,
                                    gconstpointer    user_tag,
                                    gboolean         all /* or only dirty */);
 
-void nmp_rules_manager_sync(NMPRulesManager *self, gboolean keep_deleted_rules);
+void nmp_route_manager_sync_rules(NMPRouteManager *self, gboolean keep_deleted_rules);
 
 /*****************************************************************************/
 
-#endif /* __NMP_RULES_MANAGER_H__ */
+#endif /* __NMP_ROUTE_MANAGER_H__ */
