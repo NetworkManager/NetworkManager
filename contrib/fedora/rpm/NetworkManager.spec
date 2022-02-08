NetworkManager/NetworkManager

commit d68ab6b8f02a8de68ce6bb1207cfba4e995fd756

nm-sudo: rename to nm-priv-helper
The name "nm-sudo" reminds of the "sudo" tool, and this is a bit
confusing because it's not related. Rename the service to
"nm-priv-helper", which stands for "NM privileged helper".

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/938

.gitignore
@@ -71,8 +71,8 @@ test-*.trs
 /src/nm-dispatcher/org.freedesktop.nm_dispatcher.service
 /src/nm-dispatcher/tests/test-dispatcher-envp
 
-/src/nm-sudo/nm-sudo
-/src/nm-sudo/org.freedesktop.nm.sudo.service
+/src/nm-priv-helper/nm-priv-helper
+/src/nm-priv-helper/org.freedesktop.nm-priv-helper.service
 
 /data/NetworkManager.service
 /data/NetworkManager-wait-online.service
@@ -82,6 +82,7 @@ test-*.trs
 /data/org.freedesktop.NetworkManager.policy
 /data/org.freedesktop.NetworkManager.policy.in
 /data/nm-sudo.service
+/data/nm-priv-helper.service
 
 /docs/api/version.xml
 /docs/api/settings-spec.html
@@ -436,6 +437,8 @@ test-*.trs
 /src/ndisc/tests/test-ndisc-linux
 /src/nm-daemon-helper/nm-daemon-helper
 /src/nm-iface-helper
+/src/nm-sudo/nm-sudo
+/src/nm-sudo/org.freedesktop.nm.sudo.service
 /src/platform/tests/dump
 /src/platform/tests/monitor
 /src/platform/tests/platform

Makefile.am
@@ -506,8 +506,8 @@ src_libnm_base_libnm_base_la_SOURCES = \
 	src/libnm-base/nm-ethtool-utils-base.h \
 	src/libnm-base/nm-net-aux.c \
 	src/libnm-base/nm-net-aux.h \
-	src/libnm-base/nm-sudo-utils.c \
-	src/libnm-base/nm-sudo-utils.h \
+	src/libnm-base/nm-priv-helper-utils.c \
+	src/libnm-base/nm-priv-helper-utils.h \
 	$(NULL)
 
 src_libnm_base_libnm_base_la_LDFLAGS = \
@@ -2590,8 +2590,8 @@ src_core_libNetworkManager_la_SOURCES = \
 	src/core/nm-rfkill-manager.h \
 	src/core/nm-session-monitor.c \
 	src/core/nm-session-monitor.h \
-	src/core/nm-sudo-call.c \
-	src/core/nm-sudo-call.h \
+	src/core/nm-priv-helper-call.c \
+	src/core/nm-priv-helper-call.h \
 	src/core/nm-keep-alive.c \
 	src/core/nm-keep-alive.h \
 	src/core/nm-sleep-monitor.c \
@@ -4555,16 +4555,16 @@ EXTRA_DIST += \
 	$(NULL)
 
 ###############################################################################
-# src/nm-sudo
+# src/nm-priv-helper
 ###############################################################################
 
-libexec_PROGRAMS += src/nm-sudo/nm-sudo
+libexec_PROGRAMS += src/nm-priv-helper/nm-priv-helper
 
-src_nm_sudo_nm_sudo_SOURCES = \
-	src/nm-sudo/nm-sudo.c \
+src_nm_priv_helper_nm_priv_helper_SOURCES = \
+	src/nm-priv-helper/nm-priv-helper.c \
 	$(NULL)
 
-src_nm_sudo_nm_sudo_CPPFLAGS = \
+src_nm_priv_helper_nm_priv_helper_CPPFLAGS = \
 	$(dflt_cppflags) \
 	-I$(builddir)/src/libnm-core-public \
 	-I$(srcdir)/src/libnm-core-public \
@@ -4575,12 +4575,12 @@ src_nm_sudo_nm_sudo_CPPFLAGS = \
 	$(GLIB_CFLAGS) \
 	$(NULL)
 
-src_nm_sudo_nm_sudo_LDFLAGS = \
+src_nm_priv_helper_nm_priv_helper_LDFLAGS = \
 	-Wl,--version-script="$(srcdir)/linker-script-binary.ver" \
 	$(SANITIZER_EXEC_LDFLAGS) \
 	$(NULL)
 
-src_nm_sudo_nm_sudo_LDADD = \
+src_nm_priv_helper_nm_priv_helper_LDADD = \
 	src/libnm-base/libnm-base.la \
 	src/libnm-glib-aux/libnm-glib-aux.la \
 	src/libnm-std-aux/libnm-std-aux.la \
@@ -4588,20 +4588,20 @@ src_nm_sudo_nm_sudo_LDADD = \
 	$(GLIB_LIBS) \
 	$(NULL)
 
-src/nm-sudo/org.freedesktop.nm.sudo.service: $(srcdir)/src/nm-sudo/org.freedesktop.nm.sudo.service.in
+src/nm-priv-helper/org.freedesktop.nm-priv-helper.service: $(srcdir)/src/nm-priv-helper/org.freedesktop.nm-priv-helper.service.in
 	@sed \
 	-e 's|@libexecdir[@]|$(libexecdir)|g' \
 	$< >$@
 
-dbusactivation_DATA += src/nm-sudo/org.freedesktop.nm.sudo.service
-CLEANFILES += src/nm-sudo/org.freedesktop.nm.sudo.service
+dbusactivation_DATA += src/nm-priv-helper/org.freedesktop.nm-priv-helper.service
+CLEANFILES += src/nm-priv-helper/org.freedesktop.nm-priv-helper.service
 
-dbusservice_DATA += src/nm-sudo/nm-sudo.conf
+dbusservice_DATA += src/nm-priv-helper/nm-priv-helper.conf
 
 EXTRA_DIST += \
-	src/nm-sudo/nm-sudo.conf \
-	src/nm-sudo/org.freedesktop.nm.sudo.service.in \
-	src/nm-sudo/meson.build \
+	src/nm-priv-helper/nm-priv-helper.conf \
+	src/nm-priv-helper/org.freedesktop.nm-priv-helper.service.in \
+	src/nm-priv-helper/meson.build \
 	$(NULL)
 
 ###############################################################################
@@ -5294,7 +5294,7 @@ systemdsystemunit_DATA += \
 	data/NetworkManager.service \
 	data/NetworkManager-wait-online.service \
 	data/NetworkManager-dispatcher.service \
-	data/nm-sudo.service \
+	data/nm-priv-helper.service \
 	$(NULL)
 
 data/NetworkManager.service: $(srcdir)/data/NetworkManager.service.in
@@ -5311,7 +5311,7 @@ endif
 data/NetworkManager-dispatcher.service: $(srcdir)/data/NetworkManager-dispatcher.service.in
 	$(AM_V_GEN) $(data_edit) $< >$@
 
-data/nm-sudo.service: $(srcdir)/data/nm-sudo.service.in
+data/nm-priv-helper.service: $(srcdir)/data/nm-priv-helper.service.in
 	$(AM_V_GEN) $(data_edit) $< >$@
 
 endif
@@ -5343,7 +5343,7 @@ EXTRA_DIST += \
 	data/NetworkManager-wait-online-systemd-pre200.service.in \
 	data/NetworkManager-wait-online.service.in \
 	data/NetworkManager.service.in \
-	data/nm-sudo.service.in \
+	data/nm-priv-helper.service.in \
 	data/meson.build \
 	data/nm-shared.xml \
 	data/server.conf.in \
@@ -5353,7 +5353,7 @@ CLEANFILES += \
 	data/NetworkManager-dispatcher.service \
 	data/NetworkManager-wait-online.service \
 	data/NetworkManager.service \
-	data/nm-sudo.service \
+	data/nm-priv-helper.service \
 	data/server.conf \
 	$(NULL)
 

contrib/fedora/rpm/NetworkManager.spec
@@ -40,7 +40,7 @@
 
 %global real_version_major %(printf '%s' '%{real_version}' | sed -n 's/^\\([1-9][0-9]*\\.[0-9][0-9]*\\)\\.[0-9][0-9]*$/\\1/p')
 
-%global systemd_units NetworkManager.service NetworkManager-wait-online.service NetworkManager-dispatcher.service nm-sudo.service
+%global systemd_units NetworkManager.service NetworkManager-wait-online.service NetworkManager-dispatcher.service nm-priv-helper.service
 
 %global systemd_units_cloud_setup nm-cloud-setup.service nm-cloud-setup.timer
 
@@ -943,7 +943,7 @@ if [ $1 -eq 0 ]; then
 
     /usr/sbin/update-alternatives --remove ifup %{_libexecdir}/nm-ifup >/dev/null 2>&1 || :
 fi
-%systemd_preun NetworkManager-wait-online.service NetworkManager-dispatcher.service nm-sudo.service
+%systemd_preun NetworkManager-wait-online.service NetworkManager-dispatcher.service nm-priv-helper.service
 
 
 %if %{with nm_cloud_setup}
@@ -977,7 +977,7 @@ fi
 %files
 %{dbus_sys_dir}/org.freedesktop.NetworkManager.conf
 %{dbus_sys_dir}/nm-dispatcher.conf
-%{dbus_sys_dir}/nm-sudo.conf
+%{dbus_sys_dir}/nm-priv-helper.conf
 %{dbus_sys_dir}/nm-ifcfg-rh.conf
 %{_sbindir}/%{name}
 %{_bindir}/nmcli
@@ -1002,7 +1002,7 @@ fi
 %{_libexecdir}/nm-dispatcher
 %{_libexecdir}/nm-initrd-generator
 %{_libexecdir}/nm-daemon-helper
-%{_libexecdir}/nm-sudo
+%{_libexecdir}/nm-priv-helper
 %dir %{_libdir}/%{name}
 %dir %{nmplugindir}
 %{nmplugindir}/libnm-settings-plugin*.so
@@ -1026,7 +1026,7 @@ fi
 %dir %{_localstatedir}/lib/NetworkManager
 %dir %{_sysconfdir}/sysconfig/network-scripts
 %{_datadir}/dbus-1/system-services/org.freedesktop.nm_dispatcher.service
-%{_datadir}/dbus-1/system-services/org.freedesktop.nm.sudo.service
+%{_datadir}/dbus-1/system-services/org.freedesktop.nm-priv-helper.service
 %{_datadir}/polkit-1/actions/*.policy
 %{_prefix}/lib/udev/rules.d/*.rules
 %if %{with firewalld_zone}
@@ -1036,7 +1036,7 @@ fi
 %{systemd_dir}/NetworkManager.service
 %{systemd_dir}/NetworkManager-wait-online.service
 %{systemd_dir}/NetworkManager-dispatcher.service
-%{systemd_dir}/nm-sudo.service
+%{systemd_dir}/nm-priv-helper.service
 %dir %{_datadir}/doc/NetworkManager/examples
 %{_datadir}/doc/NetworkManager/examples/server.conf
 %doc NEWS AUTHORS README CONTRIBUTING.md TODO

data/nm-priv-helper.service.in
@@ -1,0 +1,79 @@
+[Unit]
+Description=NetworkManager Privileged Helper
+
+#
+# nm-priv-helper exists for privilege separation. It allows to run
+# NetworkManager without certain capabilities, and ask nm-priv-helper
+# for special operations where more privileges are required.
+#
+
+# While nm-priv-helper has privileges that NetworkManager has not, it
+# does not mean that itself should run totally unconstrained. On the
+# contrary, it also should only have permissions it requires.
+#
+# nm-priv-helper rejects all requests that come from any other than the
+# name owner of "org.freedesktop.NetworkManager" (that is,
+# NetworkManager process itself). It is thus only an implementation
+# detail and provides no public API to the user.
+
+[Service]
+Type=dbus
+BusName=org.freedesktop.nm-priv-helper
+ExecStart=@libexecdir@/nm-priv-helper
+NotifyAccess=main
+
+# Extra configuration options. Set via `systemctl edit
+# nm-priv-helper.service`:
+#
+# FOR TESTING ONLY: disable authentication to allow requests from
+# everybody. Don't set this outside of testing!
+#Environment=NM_PRIV_HELPER_NO_AUTH_FOR_TESTING=1
+#
+# The logging level for debug messages (to stdout).
+#Environment=NM_PRIV_HELPER_LOG=TRACE
+#
+# nm-priv-helper will exit on idle after timeout. Set timeout here or
+# set to 2147483647 for infinity.
+
+#Environment=NM_PRIV_HELPER_IDLE_TIMEOUT_MSEC=10000
+
+
+# Restrict:
+AmbientCapabilities=
+CapabilityBoundingSet=
+PrivateDevices=true
+PrivateMounts=true
+PrivateNetwork=true
+PrivateTmp=true
+ProtectClock=true
+ProtectControlGroups=true
+ProtectHome=true
+ProtectHostname=true
+ProtectKernelLogs=true
+ProtectKernelModules=true
+ProtectKernelTunables=true
+ProtectSystem=strict
+RestrictAddressFamilies=
+RestrictNamespaces=true
+SystemCallFilter=~@clock
+SystemCallFilter=~@cpu-emulation
+SystemCallFilter=~@debug
+SystemCallFilter=~@module
+SystemCallFilter=~@mount
+SystemCallFilter=~@obsolete
+SystemCallFilter=~@privileged
+SystemCallFilter=~@raw-io
+SystemCallFilter=~@reboot
+SystemCallFilter=~@swap
+NoNewPrivileges=true
+SupplementaryGroups=
+
+# Grant:
+CapabilityBoundingSet=CAP_DAC_OVERRIDE
+PrivateUsers=no
+RestrictAddressFamilies=AF_UNIX
+SystemCallFilter=@resources
+
+
+[Install]
+Alias=dbus-org.freedesktop.nm-priv-helper.service

src/libnm-base/nm-priv-helper-utils.h
@@ -1,0 +1,23 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#ifndef __NM_PRIV_HELPER_UTILS_H__
+#define __NM_PRIV_HELPER_UTILS_H__
+
+/*****************************************************************************/
+
+#define NM_PRIV_HELPER_DBUS_BUS_NAME    "org.freedesktop.nm-priv-helper"
+#define NM_PRIV_HELPER_DBUS_OBJECT_PATH "/org/freedesktop/nm-priv-helper"
+#define NM_PRIV_HELPER_DBUS_IFACE_NAME  "org.freedesktop.nm-priv-helper"
+
+/*****************************************************************************/
+
+#define NM_OVSDB_SOCKET RUNSTATEDIR "/openvswitch/db.sock"
+
+typedef enum {
+    NM_PRIV_HELPER_GET_FD_TYPE_NONE         = 0,
+    NM_PRIV_HELPER_GET_FD_TYPE_OVSDB_SOCKET = 1,
+} NMPrivHelperGetFDType;
+
+int nm_priv_helper_utils_open_fd(NMPrivHelperGetFDType fd_type, GError **error);
+
+#endif /* __NM_PRIV_HELPER_UTILS_H__ */

src/nm-priv-helper/meson.build
@@ -1,20 +1,20 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 configure_file(
-  input: 'org.freedesktop.nm.sudo.service.in',
+  input: 'org.freedesktop.nm-priv-helper.service.in',
   output: '@BASENAME@',
   install_dir: dbus_system_bus_services_dir,
   configuration: data_conf,
 )
 
 install_data(
-  'nm-sudo.conf',
+  'nm-priv-helper.conf',
   install_dir: dbus_conf_dir,
 )
 
 executable(
-  'nm-sudo',
-  'nm-sudo.c',
+  'nm-priv-helper',
+  'nm-priv-helper.c',
   include_directories : [
     src_inc,
     top_inc,

src/nm-priv-helper/nm-priv-helper.conf
@@ -3,11 +3,11 @@
  "http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
 <busconfig>
     <policy user="root">
-        <allow own="org.freedesktop.nm.sudo"/>
-        <allow send_destination="org.freedesktop.nm.sudo"/>
+        <allow own="org.freedesktop.nm.priv-helper"/>
+        <allow send_destination="org.freedesktop.nm.priv-helper"/>
     </policy>
     <policy context="default">
-        <deny own="org.freedesktop.nm.sudo"/>
-        <deny send_destination="org.freedesktop.nm.sudo"/>
+        <deny own="org.freedesktop.nm.priv-helper"/>
+        <deny send_destination="org.freedesktop.nm.priv-helper"/>
     </policy>
 </busconfig>

src/nm-priv-helper/org.freedesktop.nm-priv-helper.service.in
@@ -1,0 +1,5 @@
+[D-BUS Service]
+Name=org.freedesktop.nm-priv-helper
+Exec=@libexecdir@/nm-priv-helper
+User=root
+SystemdService=dbus-org.freedesktop.nm-priv-helper.service

commit c33067e29531f8ee037ad23e36fd4bd78778ad86

priv-helper: fix D-Bus patch to not contain forbidden character '-'
"-" is not allowed as D-Bus path and interface name, and discouraged as
bus name. This cause nm-priv-helper to crash, because GDBus asserts the
the object path is valid.

Replace the '-' with '_'. This way, it's consistent with
"nm_dispatcher".

Fixes: d68ab6b8f02a ('nm-sudo: rename to nm-priv-helper')

.gitignore
@@ -72,7 +72,7 @@ test-*.trs
 /src/nm-dispatcher/tests/test-dispatcher-envp
 
 /src/nm-priv-helper/nm-priv-helper
-/src/nm-priv-helper/org.freedesktop.nm-priv-helper.service
+/src/nm-priv-helper/org.freedesktop.nm_priv_helper.service
 
 /data/NetworkManager.service
 /data/NetworkManager-wait-online.service
@@ -437,6 +437,7 @@ test-*.trs
 /src/ndisc/tests/test-ndisc-linux
 /src/nm-daemon-helper/nm-daemon-helper
 /src/nm-iface-helper
+/src/nm-priv-helper/org.freedesktop.nm-priv-helper.service
 /src/nm-sudo/nm-sudo
 /src/nm-sudo/org.freedesktop.nm.sudo.service
 /src/platform/tests/dump

Makefile.am
@@ -4595,19 +4595,19 @@ src_nm_priv_helper_nm_priv_helper_LDADD = \
 	$(GLIB_LIBS) \
 	$(NULL)
 
-src/nm-priv-helper/org.freedesktop.nm-priv-helper.service: $(srcdir)/src/nm-priv-helper/org.freedesktop.nm-priv-helper.service.in
+src/nm-priv-helper/org.freedesktop.nm_priv_helper.service: $(srcdir)/src/nm-priv-helper/org.freedesktop.nm_priv_helper.service.in
 	@sed \
 	-e 's|@libexecdir[@]|$(libexecdir)|g' \
 	$< >$@
 
-dbusactivation_DATA += src/nm-priv-helper/org.freedesktop.nm-priv-helper.service
-CLEANFILES += src/nm-priv-helper/org.freedesktop.nm-priv-helper.service
+dbusactivation_DATA += src/nm-priv-helper/org.freedesktop.nm_priv_helper.service
+CLEANFILES += src/nm-priv-helper/org.freedesktop.nm_priv_helper.service
 
 dbusservice_DATA += src/nm-priv-helper/nm-priv-helper.conf
 
 EXTRA_DIST += \
 	src/nm-priv-helper/nm-priv-helper.conf \
-	src/nm-priv-helper/org.freedesktop.nm-priv-helper.service.in \
+	src/nm-priv-helper/org.freedesktop.nm_priv_helper.service.in \
 	src/nm-priv-helper/meson.build \
 	$(NULL)
 

contrib/fedora/rpm/NetworkManager.spec
@@ -1047,7 +1047,7 @@ fi
 %dir %{_localstatedir}/lib/NetworkManager
 %dir %{_sysconfdir}/sysconfig/network-scripts
 %{_datadir}/dbus-1/system-services/org.freedesktop.nm_dispatcher.service
-%{_datadir}/dbus-1/system-services/org.freedesktop.nm-priv-helper.service
+%{_datadir}/dbus-1/system-services/org.freedesktop.nm_priv_helper.service
 %{_datadir}/polkit-1/actions/*.policy
 %{_prefix}/lib/udev/rules.d/*.rules
 %if %{with firewalld_zone}

data/nm-priv-helper.service.in
@@ -18,7 +18,7 @@ Description=NetworkManager Privileged Helper
 
 [Service]
 Type=dbus
-BusName=org.freedesktop.nm-priv-helper
+BusName=org.freedesktop.nm_priv_helper
 ExecStart=@libexecdir@/nm-priv-helper
 NotifyAccess=main
 
@@ -76,4 +76,4 @@ SystemCallFilter=@resources
 
 
 [Install]
-Alias=dbus-org.freedesktop.nm-priv-helper.service
+Alias=dbus-org.freedesktop.nm_priv_helper.service

src/libnm-base/nm-priv-helper-utils.h
@@ -5,9 +5,9 @@
 
 /*****************************************************************************/
 
-#define NM_PRIV_HELPER_DBUS_BUS_NAME    "org.freedesktop.nm-priv-helper"
-#define NM_PRIV_HELPER_DBUS_OBJECT_PATH "/org/freedesktop/nm-priv-helper"
-#define NM_PRIV_HELPER_DBUS_IFACE_NAME  "org.freedesktop.nm-priv-helper"
+#define NM_PRIV_HELPER_DBUS_BUS_NAME    "org.freedesktop.nm_priv_helper"
+#define NM_PRIV_HELPER_DBUS_OBJECT_PATH "/org/freedesktop/nm_priv_helper"
+#define NM_PRIV_HELPER_DBUS_IFACE_NAME  "org.freedesktop.nm_priv_helper"
 
 /*****************************************************************************/
 

src/nm-priv-helper/meson.build
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 configure_file(
-  input: 'org.freedesktop.nm-priv-helper.service.in',
+  input: 'org.freedesktop.nm_priv_helper.service.in',
   output: '@BASENAME@',
   install_dir: dbus_system_bus_services_dir,
   configuration: data_conf,

src/nm-priv-helper/nm-priv-helper.conf
@@ -3,11 +3,11 @@
  "http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
 <busconfig>
     <policy user="root">
-        <allow own="org.freedesktop.nm.priv-helper"/>
-        <allow send_destination="org.freedesktop.nm.priv-helper"/>
+        <allow own="org.freedesktop.nm_priv_helper"/>
+        <allow send_destination="org.freedesktop.nm_priv_helper"/>
     </policy>
     <policy context="default">
-        <deny own="org.freedesktop.nm.priv-helper"/>
-        <deny send_destination="org.freedesktop.nm.priv-helper"/>
+        <deny own="org.freedesktop.nm_priv_helper"/>
+        <deny send_destination="org.freedesktop.nm_priv_helper"/>
     </policy>
 </busconfig>

src/nm-priv-helper/org.freedesktop.nm-priv-helper.service.in
@@ -1,5 +1,0 @@
-[D-BUS Service]
-Name=org.freedesktop.nm-priv-helper
-Exec=@libexecdir@/nm-priv-helper
-User=root
-SystemdService=dbus-org.freedesktop.nm-priv-helper.service

src/nm-priv-helper/org.freedesktop.nm_priv_helper.service.in
@@ -1,0 +1,5 @@
+[D-BUS Service]
+Name=org.freedesktop.nm_priv_helper
+Exec=@libexecdir@/nm-priv-helper
+User=root
+SystemdService=dbus-org.freedesktop.nm_priv_helper.service