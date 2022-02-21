NetworkManager/NetworkManager

commit 40032f4614153d1775744267302dd79287f73b84

cli: fix resetting values via property alias
Property aliases should really just be shortcuts for one fully spelled
out property (sometimes, they do more like "master").

Anyway, we must also handle resetting the value, otherwise:

  $ nmcli connection add type gsm apn ""

will still result in "gsm.apn=internet", unlike

  $ nmcli connection add type gsm gsm.apn ""

src/nmcli/connections.c
@@ -4182,23 +4182,17 @@ set_option(NmCli *                   nmc,
                        NULL);
     if (option && option->check_and_set) {
         return option->check_and_set(nmc, connection, option, value, error);
-    } else if (value) {
-        return set_property(nmc->client,
-                            connection,
-                            setting_name,
-                            property_name,
-                            value,
-                            inf_flags & NM_META_PROPERTY_INF_FLAG_MULTI
-                                ? NM_META_ACCESSOR_MODIFIER_ADD
-                                : NM_META_ACCESSOR_MODIFIER_SET,
-                            error);
-    } else if (inf_flags & NM_META_PROPERTY_INF_FLAG_REQD) {
-        g_set_error(error,
-                    NMCLI_ERROR,
-                    NMC_RESULT_ERROR_USER_INPUT,
-                    _("Error: '%s' is mandatory."),
-                    option_name);
-        return FALSE;
+    } else {
+        set_property(nmc->client,
+                     connection,
+                     setting_name,
+                     property_name,
+                     value,
+                     !value ? NM_META_ACCESSOR_MODIFIER_DEL
+                            : (inf_flags & NM_META_PROPERTY_INF_FLAG_MULTI
+                                   ? NM_META_ACCESSOR_MODIFIER_ADD
+                                   : NM_META_ACCESSOR_MODIFIER_SET),
+                     error);
     }
 
     return TRUE;

commit 5b4ce608d4db331cad0a891976a0bbce975e61f0

cli: don't reset default values in interactive add
Since commit 40032f461415 ('cli: fix resetting values via property
alias'), nmcli sets NULL properties during interactive add (nmcli -a
connection add) when the user leaves the field blank. This can lead to
an invalid connection for properties that can't be empty like
infiniband.transport-mode; they should be left to the default value in
case of no value entered.

Fixes: 40032f461415 ('cli: fix resetting values via property alias')
Fixes-test: @inf_create_port_novice_mode
https://bugzilla.redhat.com/show_bug.cgi?id=2053603
https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1111

src/nmcli/connections.c
@@ -4189,6 +4189,7 @@ set_option(NmCli                    *nmc,
            NMConnection             *connection,
            const NMMetaAbstractInfo *abstract_info,
            const char               *value,
+           gboolean                  allow_reset,
            GError                  **error)
 {
     const char            *setting_name, *property_name, *option_name;
@@ -4209,7 +4210,7 @@ set_option(NmCli                    *nmc,
                        NULL);
     if (option && option->check_and_set) {
         return option->check_and_set(nmc, connection, option, value, error);
-    } else {
+    } else if (value || allow_reset) {
         set_property(nmc->client,
                      connection,
                      setting_name,
@@ -5199,7 +5200,7 @@ nmc_process_connection_properties(NmCli              *nmc,
         if (!*argc && nmc->complete)
             complete_option(nmc, chosen, value ?: "", connection);
 
-        if (!set_option(nmc, connection, chosen, value, error))
+        if (!set_option(nmc, connection, chosen, value, TRUE, error))
             return FALSE;
 
     } while (*argc);
@@ -5410,7 +5411,7 @@ again:
     if (multi && !value)
         return;
 
-    if (!set_option(nmc, connection, abstract_info, value, &error)) {
+    if (!set_option(nmc, connection, abstract_info, value, FALSE, &error)) {
         g_printerr("%s\n", error->message);
         g_clear_error(&error);
         goto again;