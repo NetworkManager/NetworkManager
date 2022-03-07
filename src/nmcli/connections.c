NetworkManager/NetworkManager

commit 40032f4614153d1775744267302dd79287f73b84

cli: fix resetting values via property alias
Property aliases should really just be shortcuts for one fully spelled
out property (sometimes, they do more like "master").

Anyway, we must also handle resetting the value, otherwise:

  $ nmcli connection add type gsm apn ""

will still result in "gsm.apn=internet", unlike

  $ nmcli connection add type gsm gsm.apn ""

src/nmcli/connections.c
@@ -4182,23 +4182,17 @@ set_option(NmCli *                   nmc,
                        NULL);
     if (option && option->check_and_set) {
         return option->check_and_set(nmc, connection, option, value, error);
-    } else if (value) {
-        return set_property(nmc->client,
-                            connection,
-                            setting_name,
-                            property_name,
-                            value,
-                            inf_flags & NM_META_PROPERTY_INF_FLAG_MULTI
-                                ? NM_META_ACCESSOR_MODIFIER_ADD
-                                : NM_META_ACCESSOR_MODIFIER_SET,
-                            error);
-    } else if (inf_flags & NM_META_PROPERTY_INF_FLAG_REQD) {
-        g_set_error(error,
-                    NMCLI_ERROR,
-                    NMC_RESULT_ERROR_USER_INPUT,
-                    _("Error: '%s' is mandatory."),
-                    option_name);
-        return FALSE;
+    } else {
+        set_property(nmc->client,
+                     connection,
+                     setting_name,
+                     property_name,
+                     value,
+                     !value ? NM_META_ACCESSOR_MODIFIER_DEL
+                            : (inf_flags & NM_META_PROPERTY_INF_FLAG_MULTI
+                                   ? NM_META_ACCESSOR_MODIFIER_ADD
+                                   : NM_META_ACCESSOR_MODIFIER_SET),
+                     error);
     }
 
     return TRUE;

commit 904913493db5ac8c2ae36f5ac6d9da28868cc8a6

cli: don't ignore errors from setting property aliases
Before, we would just ignore the errors when we passed an invalid value
to a property alias:

  $ nmcli c add type ethernet mac Hello
  Connection 'ethernet-1' (242eec76-7147-411a-a50b-336cf5bc8137) successfully added.
  $ nmcli c show 242eec76-7147-411a-a50b-336cf5bc8137 |grep 802-3-ethernet.mac-address:
  802-3-ethernet.mac-address:             --

...or crash, because the GError would still be around:

  $ nmcli c add type ethernet mac Hello ethernet.mac-address World
  (process:734670): GLib-WARNING **: 14:52:51.436: GError set over the top of a previous GError or uninitialized memory.
  This indicates a bug in someone's code. You must ensure an error is NULL before it's set.
  The overwriting error message was: Error: failed to modify 802-3-ethernet.mac-address: 'World' is not a valid Ethernet MAC.
  Error: failed to modify 802-3-ethernet.mac-address: 'Hello' is not a valid Ethernet MAC.

Now we catch it early enough:

  $ nmcli c add type ethernet mac Hello
  Error: failed to modify 802-3-ethernet.mac-address: 'Hello' is not a valid Ethernet MAC.

Fixes: 40032f461415 ('cli: fix resetting values via property alias')

src/nmcli/connections.c
@@ -4211,16 +4211,16 @@ set_option(NmCli                    *nmc,
     if (option && option->check_and_set) {
         return option->check_and_set(nmc, connection, option, value, error);
     } else if (value || allow_reset) {
-        set_property(nmc->client,
-                     connection,
-                     setting_name,
-                     property_name,
-                     value,
-                     !value ? NM_META_ACCESSOR_MODIFIER_DEL
-                            : (inf_flags & NM_META_PROPERTY_INF_FLAG_MULTI
-                                   ? NM_META_ACCESSOR_MODIFIER_ADD
-                                   : NM_META_ACCESSOR_MODIFIER_SET),
-                     error);
+        return set_property(nmc->client,
+                            connection,
+                            setting_name,
+                            property_name,
+                            value,
+                            !value ? NM_META_ACCESSOR_MODIFIER_DEL
+                                   : (inf_flags & NM_META_PROPERTY_INF_FLAG_MULTI
+                                          ? NM_META_ACCESSOR_MODIFIER_ADD
+                                          : NM_META_ACCESSOR_MODIFIER_SET),
+                            error);
     }
 
     return TRUE;