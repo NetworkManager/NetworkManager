ad0362f70cc07108bbdde4c6acc732112ad86048
Thomas Haller <thaller@redhat.com>
2022-03-30 08:47:34 +0200

    all: avoid using global string buffer for to-string methods
    These string functions allow to omit the string buffer. This is for
    convenience, to use a global (thread-local) buffer. I think that is
    error prone and we should drop that "convenience" feature.
    
    At various places, pass a stack allocated buffer.
    
    (cherry picked from commit b87afac8e8b8fe93a07da9eb6cc69bc0673b67bb)

--- src/core/dnsmasq/nm-dnsmasq-manager.c
+++ src/core/dnsmasq/nm-dnsmasq-manager.c
@@ -67,9 +67,12 @@ dm_watch_cb(GPid pid, int status, gpointer user_data)
     guint                    err;
 
     if (WIFEXITED(status)) {
+        char sbuf[NM_UTILS_TO_STRING_BUFFER_SIZE];
+
         err = WEXITSTATUS(status);
         if (err != 0) {
-            _LOGW("dnsmasq exited with error: %s", nm_utils_dnsmasq_status_to_string(err, NULL, 0));
+            _LOGW("dnsmasq exited with error: %s",
+                  nm_utils_dnsmasq_status_to_string(err, sbuf, sizeof(sbuf)));
         }
     } else if (WIFSTOPPED(status)) {
         _LOGW("dnsmasq stopped unexpectedly with signal %d", WSTOPSIG(status));
