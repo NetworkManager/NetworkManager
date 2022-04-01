55ee796c3b14cb6bb7ff4916d6580202a95a00d1
Beniamino Galvani <bgalvani@redhat.com>
2022-04-01 15:32:34 +0200

    supplicant: add BIP interface capability
    Introduce a new capability indicating whether the interface supports
    any of the BIP ciphers that can be used for 802.11w (PMF).
    
    (cherry picked from commit cd1e0193abcf26f523bd52d83af5aab086ceaa92)

--- src/core/supplicant/nm-supplicant-types.h
+++ src/core/supplicant/nm-supplicant-types.h
@@ -48,6 +48,9 @@ typedef enum {
     NM_SUPPL_CAP_TYPE_FAST,
     NM_SUPPL_CAP_TYPE_WFD,
     NM_SUPPL_CAP_TYPE_SUITEB192,
+    NM_SUPPL_CAP_TYPE_BIP,
+    /* Note: if you're adding a capability here, log its presence at the
+     * bottom of _dbus_get_capabilities_cb(). */
     _NM_SUPPL_CAP_TYPE_NUM,
 } NMSupplCapType;
 
@@ -75,6 +78,7 @@ typedef enum {
     _NM_SUPPL_CAP_MASK_DEFINE(FT),
     _NM_SUPPL_CAP_MASK_DEFINE(SAE),
     _NM_SUPPL_CAP_MASK_DEFINE(SHA384),
+    _NM_SUPPL_CAP_MASK_DEFINE(BIP),
 #undef _NM_SUPPL_CAP_MASK_DEFINE
 } NMSupplCapMask;
 
