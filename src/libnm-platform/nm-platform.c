NetworkManager/NetworkManager

commit 528a63d9cc4da678482e05de3c41dfdfe649dcfe

platform: avoid unnecessary configuration of IP address in nm_platform_ip_address_sync()
We call sync many times. Often there is nothing to update. Check the
cache first, before (re) adding it.

Note that many addresses have a limited lifetime, that is, a lifetime
that keeps counting down with seconds granularity. For those (common)
cases we will only avoid the call to kernel if there are two syncs
within less than a second.

src/libnm-platform/nm-platform.c
@@ -4348,6 +4348,7 @@ next_plat:;
      * priority.
      */
     for (i_know = 0; i_know < known_addresses->len; i_know++) {
+        const NMPObject            *plat_obj;
         const NMPObject            *known_obj;
         const NMPlatformIPXAddress *known_address;
         guint32                     lifetime;
@@ -4367,6 +4368,23 @@ next_plat:;
                                           &now,
                                           &preferred);
         nm_assert(lifetime > 0);
+
+        plat_obj = nm_platform_ip_address_get(self, addr_family, ifindex, known_address);
+        if (plat_obj
+            && nm_platform_vtable_address.vx[IS_IPv4].address_cmp(
+                   known_address,
+                   NMP_OBJECT_CAST_IPX_ADDRESS(plat_obj),
+                   NM_PLATFORM_IP_ADDRESS_CMP_TYPE_SEMANTICALLY)
+                   == 0) {
+            char sbuf[sizeof(_nm_utils_to_string_buffer)];
+
+            /* The object is already added. Skip update. */
+            _LOG3t(
+                "address: skip updating IPv%c address: %s",
+                nm_utils_addr_family_to_char(addr_family),
+                nmp_object_to_string(known_obj, NMP_OBJECT_TO_STRING_PUBLIC, sbuf, sizeof(sbuf)));
+            continue;
+        }
 
         if (IS_IPv4) {
             if (!nm_platform_ip4_address_add(

commit 7f427ac4e6711fafaee5a3ab76a006f92e122b00

platform: ensure the platform cache is up to date during nm_platform_ip_address_sync()
Since commit 528a63d9cc4d ('platform: avoid unnecessary configuration of
IP address in nm_platform_ip_address_sync()'), we no longer configure the
IP address if it is in the platform cache. But the cache might not be
up to date. Process any pending netlink events.

https://bugzilla.redhat.com/show_bug.cgi?id=2073926

Fixes: 528a63d9cc4d ('platform: avoid unnecessary configuration of IP address in nm_platform_ip_address_sync()')

src/libnm-platform/nm-platform.c
@@ -4112,6 +4112,9 @@ nm_platform_ip_address_sync(NMPlatform *self,
         }
     }
 
+    /* ensure we have the platform cache up to date. */
+    nm_platform_process_events(self);
+
     /* @plat_addresses for IPv6 must be sorted in decreasing priority order (highest priority addresses first).
      * IPv4 are probably unsorted or sorted with lowest priority first, but their order doesn't matter because
      * we check the "secondary" flag. */