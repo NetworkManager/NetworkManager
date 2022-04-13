f20ac6bdc7cd8ac8dc3ebc5ee338fdf2533a16f2
Beniamino Galvani <bgalvani@redhat.com>
2022-04-13 09:04:32 +0200

    dhcp: improve logging for DHCPv6 merged leases
    Instead of logging the event-id, which is composed from options that
    are already visible in the log, it's more interesting to log that the
    lease was merged.

--- src/core/dhcp/nm-dhcp-utils.c
+++ src/core/dhcp/nm-dhcp-utils.c
@@ -820,26 +820,6 @@ nm_dhcp_utils_get_leasefile_path(int         addr_family,
     return FALSE;
 }
 
-char *
-nm_dhcp_utils_get_dhcp6_event_id(GHashTable *lease)
-{
-    const char *start;
-    const char *iaid;
-
-    if (!lease)
-        return NULL;
-
-    iaid = g_hash_table_lookup(lease, "iaid");
-    if (!iaid)
-        return NULL;
-
-    start = g_hash_table_lookup(lease, "life_starts");
-    if (!start)
-        return NULL;
-
-    return g_strdup_printf("%s|%s", iaid, start);
-}
-
 gboolean
 nm_dhcp_utils_merge_new_dhcp6_lease(const NML3ConfigData  *l3cd_old,
                                     const NML3ConfigData  *l3cd_new,
