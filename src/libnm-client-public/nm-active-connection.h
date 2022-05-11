NetworkManager/NetworkManager

commit 649314ddaa4c34feca2d9bc5821edc331a890255

libnm: replace nm-types.h by defining the types in respective headers
The typedefs in nm-types.h confuse gtkdoc-scan. It generates a
libnm-sections.txt file like this:

  <SECTION>
  <FILE>nm-types</FILE>
  <TITLE>NMDeviceOvs</TITLE>
  NMAccessPoint
  NMActiveConnection
  NMCheckpoint
  NMClient
  NMDevice
  ...

Note the wrongly picked title and, more importantly, the object types in
a bogus section. This in turn makes gtkdoc-mkdb fail to include the
property and signal documentation in appropriate sections.

Without nm-types.h, we need to mind the header dependencies. This means
that we need to order the headers that define types before the ones that
use them. Also, we need to break the depencency loops in few palces.

src/libnm-client-public/nm-active-connection.h
@@ -43,9 +43,12 @@ G_BEGIN_DECLS
 #define NM_ACTIVE_CONNECTION_VPN                  "vpn"
 #define NM_ACTIVE_CONNECTION_MASTER               "master"
 
+typedef struct _NMDevice NMDevice;
+
 /**
  * NMActiveConnection:
  */
+typedef struct _NMActiveConnection      NMActiveConnection;
 typedef struct _NMActiveConnectionClass NMActiveConnectionClass;
 
 GType nm_active_connection_get_type(void);

src/libnm-client-public/nm-object.h
@@ -11,8 +11,6 @@
 #error "Only <NetworkManager.h> can be included directly."
 #endif
 
-#include "nm-types.h"
-
 G_BEGIN_DECLS
 
 #define NM_TYPE_OBJECT            (nm_object_get_type())
@@ -25,9 +23,12 @@ G_BEGIN_DECLS
 #define NM_OBJECT_PATH   "path"
 #define NM_OBJECT_CLIENT "client"
 
+typedef struct _NMClient NMClient;
+
 /**
  * NMObject:
  */
+typedef struct _NMObject      NMObject;
 typedef struct _NMObjectClass NMObjectClass;
 
 GType nm_object_get_type(void);

Makefile.am
@@ -1691,7 +1691,6 @@ libnm_lib_h_pub_real = \
 	src/libnm-client-public/nm-object.h \
 	src/libnm-client-public/nm-remote-connection.h \
 	src/libnm-client-public/nm-secret-agent-old.h \
-	src/libnm-client-public/nm-types.h \
 	src/libnm-client-public/nm-vpn-connection.h \
 	src/libnm-client-public/nm-vpn-editor.h \
 	src/libnm-client-public/nm-vpn-plugin-old.h \

src/libnm-client-impl/nm-client.c
@@ -20,6 +20,7 @@
 #include "nm-checkpoint.h"
 #include "libnm-core-intern/nm-core-internal.h"
 #include "nm-dbus-helpers.h"
+#include "nm-wifi-p2p-peer.h"
 #include "nm-device-6lowpan.h"
 #include "nm-device-adsl.h"
 #include "nm-device-bond.h"
@@ -33,7 +34,6 @@
 #include "nm-device-macsec.h"
 #include "nm-device-macvlan.h"
 #include "nm-device-modem.h"
-#include "nm-device-olpc-mesh.h"
 #include "nm-device-ovs-bridge.h"
 #include "nm-device-ovs-interface.h"
 #include "nm-device-ovs-port.h"
@@ -46,6 +46,7 @@
 #include "nm-device-wifi.h"
 #include "nm-device-wireguard.h"
 #include "nm-device-wpan.h"
+#include "nm-device-olpc-mesh.h"
 #include "nm-dhcp-config.h"
 #include "nm-dhcp4-config.h"
 #include "nm-dhcp6-config.h"
@@ -56,7 +57,6 @@
 #include "nm-remote-connection.h"
 #include "nm-utils.h"
 #include "nm-vpn-connection.h"
-#include "nm-wifi-p2p-peer.h"
 
 /*****************************************************************************/
 

src/libnm-client-impl/nm-default-libnm.h
@@ -16,6 +16,16 @@
 /*****************************************************************************/
 
 #include "nm-version.h"
+#include "nm-dbus-interface.h"
+#include "nm-dhcp-config.h"
+#include "nm-ip-config.h"
+#include "nm-connection.h"
+#include "nm-remote-connection.h"
+#include "nm-active-connection.h"
+#include "nm-device.h"
+#include "nm-checkpoint.h"
+#include "nm-client.h"
+#include "nm-vpn-connection.h"
 #include "nm-libnm-utils.h"
 
 /*****************************************************************************/

src/libnm-client-impl/nm-device-olpc-mesh.c
@@ -5,12 +5,13 @@
 
 #include "libnm-client-impl/nm-default-libnm.h"
 
+#include "nm-access-point.h"
+#include "nm-device-wifi.h"
 #include "nm-device-olpc-mesh.h"
 
 #include "nm-setting-connection.h"
 #include "nm-setting-olpc-mesh.h"
 #include "nm-object-private.h"
-#include "nm-device-wifi.h"
 
 /*****************************************************************************/
 

src/libnm-client-impl/nm-device-wifi-p2p.c
@@ -5,13 +5,13 @@
 
 #include "libnm-client-impl/nm-default-libnm.h"
 
+#include "nm-wifi-p2p-peer.h"
 #include "nm-device-wifi-p2p.h"
 
 #include "libnm-glib-aux/nm-dbus-aux.h"
 #include "nm-setting-connection.h"
 #include "nm-setting-wifi-p2p.h"
 #include "nm-utils.h"
-#include "nm-wifi-p2p-peer.h"
 #include "nm-object-private.h"
 #include "libnm-core-intern/nm-core-internal.h"
 #include "nm-dbus-helpers.h"

src/libnm-client-impl/nm-device-wifi.c
@@ -6,6 +6,7 @@
 
 #include "libnm-client-impl/nm-default-libnm.h"
 
+#include "nm-access-point.h"
 #include "nm-device-wifi.h"
 
 #include <linux/if_ether.h>
@@ -15,7 +16,6 @@
 #include "nm-setting-wireless.h"
 #include "nm-setting-wireless-security.h"
 #include "nm-utils.h"
-#include "nm-access-point.h"
 #include "nm-object-private.h"
 #include "libnm-core-intern/nm-core-internal.h"
 #include "nm-dbus-helpers.h"

commit a7854328e1068380c678275ee685af58faaa0f6e

libnm: avoid duplicate typedefs for NMClient/NMDevice
clang 3.4.2-9.el7 does not like this:

  $ clang -DHAVE_CONFIG_H -I. -I..  -I../src/libnm-core-public -I./src/libnm-core-public -I../src/libnm-client-public -I./src/libnm-client-public -pthread -I/usr/include/gio-unix-2.0/ -I/usr/include/glib-2.0 -I/usr/lib64/glib-2.0/include   -DGLIB_VERSION_MIN_REQUIRED=GLIB_VERSION_2_40 -DGLIB_VERSION_MAX_ALLOWED=GLIB_VERSION_2_40  -Wall -Werror -Wextra -Wdeclaration-after-statement -Wfloat-equal -Wformat-nonliteral -Wformat-security -Wimplicit-function-declaration -Winit-self -Wmissing-declarations -Wmissing-include-dirs -Wmissing-prototypes -Wpointer-arith -Wshadow -Wstrict-prototypes -Wundef -Wvla -Wno-duplicate-decl-specifier -Wno-format-y2k -Wno-missing-field-initializers -Wno-sign-compare -Wno-tautological-constant-out-of-range-compare -Wno-unknown-pragmas -Wno-unused-parameter  -Qunused-arguments -Wunknown-warning-option -Wtypedef-redefinition -Warray-bounds -Wparentheses-equality -Wunused-value -Wimplicit-fallthrough  -fno-strict-aliasing -fdata-sections -ffunction-sections -Wl,--gc-sections -g -O2 -MT examples/C/glib/examples_C_glib_add_connection_libnm-add-connection-libnm.o -MD -MP -MF examples/C/glib/.deps/examples_C_glib_add_connection_libnm-add-connection-libnm.Tpo -c -o examples/C/glib/examples_C_glib_add_connection_libnm-add-connection-libnm.o `test -f 'examples/C/glib/add-connection-libnm.c' || echo '../'`examples/C/glib/add-connection-libnm.c
  ...
  ../src/libnm-client-public/nm-client.h:149:31: error: redefinition of typedef 'NMClient' is a C11 feature [-Werror,-Wtypedef-redefinition]
  typedef struct _NMClient      NMClient;
                                ^

Our code base is C11 internally (actually "-std=gnu11"), but this problem
happens when we build the example. The warning is actually correct, because
our public headers should be more liberal (and possibly be C99 or even C89,
this is undefined).

Fixes: 649314ddaa4c

src/libnm-client-public/nm-active-connection.h
@@ -43,8 +43,6 @@ G_BEGIN_DECLS
 #define NM_ACTIVE_CONNECTION_VPN                  "vpn"
 #define NM_ACTIVE_CONNECTION_MASTER               "master"
 
-typedef struct _NMDevice NMDevice;
-
 /**
  * NMActiveConnection:
  */
@@ -64,14 +62,18 @@ NM_AVAILABLE_IN_1_10
 NMActivationStateFlags nm_active_connection_get_state_flags(NMActiveConnection *connection);
 NM_AVAILABLE_IN_1_8
 NMActiveConnectionStateReason nm_active_connection_get_state_reason(NMActiveConnection *connection);
-NMDevice                     *nm_active_connection_get_master(NMActiveConnection *connection);
-gboolean                      nm_active_connection_get_default(NMActiveConnection *connection);
-NMIPConfig                   *nm_active_connection_get_ip4_config(NMActiveConnection *connection);
-NMDhcpConfig                 *nm_active_connection_get_dhcp4_config(NMActiveConnection *connection);
-gboolean                      nm_active_connection_get_default6(NMActiveConnection *connection);
-NMIPConfig                   *nm_active_connection_get_ip6_config(NMActiveConnection *connection);
-NMDhcpConfig                 *nm_active_connection_get_dhcp6_config(NMActiveConnection *connection);
-gboolean                      nm_active_connection_get_vpn(NMActiveConnection *connection);
+
+struct _NMDevice;
+
+struct _NMDevice *nm_active_connection_get_master(NMActiveConnection *connection);
+
+gboolean      nm_active_connection_get_default(NMActiveConnection *connection);
+NMIPConfig   *nm_active_connection_get_ip4_config(NMActiveConnection *connection);
+NMDhcpConfig *nm_active_connection_get_dhcp4_config(NMActiveConnection *connection);
+gboolean      nm_active_connection_get_default6(NMActiveConnection *connection);
+NMIPConfig   *nm_active_connection_get_ip6_config(NMActiveConnection *connection);
+NMDhcpConfig *nm_active_connection_get_dhcp6_config(NMActiveConnection *connection);
+gboolean      nm_active_connection_get_vpn(NMActiveConnection *connection);
 
 G_END_DECLS
 

src/libnm-client-public/nm-object.h
@@ -23,8 +23,6 @@ G_BEGIN_DECLS
 #define NM_OBJECT_PATH   "path"
 #define NM_OBJECT_CLIENT "client"
 
-typedef struct _NMClient NMClient;
-
 /**
  * NMObject:
  */
@@ -35,8 +33,10 @@ GType nm_object_get_type(void);
 
 const char *nm_object_get_path(NMObject *object);
 
+struct _NMClient;
+
 NM_AVAILABLE_IN_1_24
-NMClient *nm_object_get_client(NMObject *object);
+struct _NMClient *nm_object_get_client(NMObject *object);
 
 G_END_DECLS
 