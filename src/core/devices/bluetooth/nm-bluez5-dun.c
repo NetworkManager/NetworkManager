NetworkManager/NetworkManager

commit dbf29c54508e7fb198b815e6fa6249b66005fcf3

platform: fix build with musl libc
Don't mix <net/ethernet.h> and <linux/if_ether.h>.

Fixes the following build error with musl libc:

  In file included from /usr/include/net/ethernet.h:10,
                   from ../src/libnm-platform/nm-linux-platform.c:17:
  /usr/include/netinet/if_ether.h:115:8: error: redefinition of 'struct ethhdr'
    115 | struct ethhdr {
        |        ^~~~~~
  In file included from ../src/linux-headers/ethtool.h:19,
                   from ../src/libnm-std-aux/nm-linux-compat.h:22,
                   from ../src/libnm-platform/nm-linux-platform.c:10:
  /usr/include/linux/if_ether.h:169:8: note: originally defined here
    169 | struct ethhdr {
        |        ^~~~~~

Fixes: dc98ab807ca8 ('platform: include "linux-headers" via "libnm-std-aux/nm-linux-compat.h"')

src/core/devices/bluetooth/nm-bluez5-dun.c
@@ -9,7 +9,6 @@
 #include <bluetooth/sdp.h>
 #include <bluetooth/sdp_lib.h>
 #include <bluetooth/rfcomm.h>
-#include <net/ethernet.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <fcntl.h>

src/core/devices/nm-lldp-listener.c
@@ -7,8 +7,6 @@
 
 #include "nm-lldp-listener.h"
 
-#include <net/ethernet.h>
-
 #include "NetworkManagerUtils.h"
 #include "libnm-core-public/nm-errors.h"
 #include "libnm-glib-aux/nm-time-utils.h"

src/libnm-platform/nm-linux-platform.c
@@ -14,7 +14,6 @@
 #include <endian.h>
 #include <fcntl.h>
 #include <libudev.h>
-#include <net/ethernet.h>
 #include <linux/fib_rules.h>
 #include <linux/ip.h>
 #include <linux/if.h>

src/libnm-platform/wifi/nm-wifi-utils-nl80211.c
@@ -12,7 +12,6 @@
 #include "libnm-std-aux/nm-linux-compat.h"
 
 #include <sys/ioctl.h>
-#include <net/ethernet.h>
 #include <unistd.h>
 #include <linux/nl80211.h>
 #include <linux/if.h>

src/libnm-platform/wifi/nm-wifi-utils.h
@@ -7,8 +7,6 @@
 #ifndef __WIFI_UTILS_H__
 #define __WIFI_UTILS_H__
 
-#include <net/ethernet.h>
-
 #include "libnm-platform/nm-netlink.h"
 #include "libnm-base/nm-base.h"
 

src/libnm-platform/wpan/nm-wpan-utils.h
@@ -6,8 +6,6 @@
 #ifndef __WPAN_UTILS_H__
 #define __WPAN_UTILS_H__
 
-#include <net/ethernet.h>
-
 #include "libnm-platform/nm-netlink.h"
 
 typedef struct NMWpanUtils NMWpanUtils;

commit 0cd3ffa7e931c5ca335b9f1993d46dba3adc20f6

build: fix compilation
Fixes: dbf29c54508e ('platform: fix build with musl libc')

src/core/devices/bluetooth/nm-bluez5-dun.c
@@ -9,6 +9,7 @@
 #include <bluetooth/sdp.h>
 #include <bluetooth/sdp_lib.h>
 #include <bluetooth/rfcomm.h>
+#include <net/ethernet.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <fcntl.h>

src/core/devices/nm-lldp-listener.c
@@ -7,6 +7,8 @@
 
 #include "nm-lldp-listener.h"
 
+#include <net/ethernet.h>
+
 #include "NetworkManagerUtils.h"
 #include "libnm-core-public/nm-errors.h"
 #include "libnm-glib-aux/nm-time-utils.h"