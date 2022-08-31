7dccb5f548c5e31c5e4527be73df3481a2829d4a
Vojtech Bubela <vbubela@redhat.com>
2022-08-31 11:32:12 +0200

    version: add 1.42 macros

--- src/libnm-core-public/nm-version.h
+++ src/libnm-core-public/nm-version.h
@@ -341,6 +341,20 @@
 #define NM_AVAILABLE_IN_1_40
 #endif
 
+#if NM_VERSION_MIN_REQUIRED >= NM_VERSION_1_42
+#define NM_DEPRECATED_IN_1_42        G_DEPRECATED
+#define NM_DEPRECATED_IN_1_42_FOR(f) G_DEPRECATED_FOR(f)
+#else
+#define NM_DEPRECATED_IN_1_42
+#define NM_DEPRECATED_IN_1_42_FOR(f)
+#endif
+
+#if NM_VERSION_MAX_ALLOWED < NM_VERSION_1_42
+#define NM_AVAILABLE_IN_1_42 G_UNAVAILABLE(1, 42)
+#else
+#define NM_AVAILABLE_IN_1_42
+#endif
+
 /*
  * Synchronous API for calling D-Bus in libnm is deprecated. See
  * https://networkmanager.dev/docs/libnm/latest/usage.html#sync-api
