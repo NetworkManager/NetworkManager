aea053db38ffd00c223e5cc1b5d3f2b342fc9870
Thomas Haller <thaller@redhat.com>
2022-09-20 12:38:03 +0200

    platform: add "nmp-plobj.[hc]"
    Our naming in libnm-platform is bad.
    
    We have NMPlatform, which is a cache of objects. Consequently we have
    platform methods like nm_platform_get_link().
    
    We also have various other types that share the NMPlatform prefix, like
    NMPlatformIP4Address. For those we have nm_platform_ip4_address_to_string().
    
    "methods" of a type should have the same prefix as the type,
    and we should not have types that share the same prefix.
    
    Also, "NMPlatformIP4Address" is a long name, and inconsistent with the
    strongly related NMPObjectIP4Address.
    
    Add new files to move and rename parts of the platform API.

---
+++ src/libnm-platform/nmp-plobj.c
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2015 - 2018 Red Hat, Inc.
+ */
+
+#include "libnm-glib-aux/nm-default-glib-i18n-lib.h"
+
+#include "nmp-plobj.h"
+
---
+++ src/libnm-platform/nmp-plobj.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2015 - 2018 Red Hat, Inc.
+ */
+
+#ifndef __NMP_PLOBJ_H__
+#define __NMP_PLOBJ_H__
+
+#endif /* __NMP_PLOBJ_H__ */
