NetworkManager/NetworkManager

commit 9958510f2872266b2f4d19567529a0c69070e573

bond: add support of queue_id of bond port
Introduced `NMSettingBondPort` to hold the new setting class with single
property `NM_SETTING_BOND_PORT_QUEUE_ID`.

For dbus interface, please use `bond-port` as setting name and
`queue-id` as property name.

Unit test cases for ifcfg reader and writer included.

Signed-off-by: Gris Ge <fge@redhat.com>

https://bugzilla.redhat.com/show_bug.cgi?id=1949127

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/952

src/libnm-core-impl/nm-setting-bond-port.c
@@ -1,0 +1,176 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2021 Red Hat, Inc.
+ */
+
+#include "libnm-core-impl/nm-default-libnm-core.h"
+
+#include "nm-connection-private.h"
+#include "nm-setting-bond-port.h"
+#include "nm-setting-bond.h"
+#include "nm-setting-connection.h"
+#include "nm-utils-private.h"
+#include "nm-utils.h"
+
+/**
+ * SECTION:nm-setting-bond-port
+ * @short_description: Describes connection properties for bond ports
+ *
+ * The #NMSettingBondPort object is a #NMSetting subclass that describes
+ * optional properties that apply to bond ports.
+ **/
+
+/*****************************************************************************/
+
+NM_GOBJECT_PROPERTIES_DEFINE(NMSettingBondPort, PROP_QUEUE_ID, );
+
+typedef struct {
+    guint32 queue_id;
+} NMSettingBondPortPrivate;
+
+/**
+ * NMSettingBondPort:
+ *
+ * Bond Port Settings
+ */
+struct _NMSettingBondPort {
+    NMSetting                parent;
+    NMSettingBondPortPrivate _priv;
+};
+
+struct _NMSettingBondPortClass {
+    NMSettingClass parent;
+};
+
+G_DEFINE_TYPE(NMSettingBondPort, nm_setting_bond_port, NM_TYPE_SETTING)
+
+#define NM_SETTING_BOND_PORT_GET_PRIVATE(self) \
+    _NM_GET_PRIVATE(self, NMSettingBondPort, NM_IS_SETTING_BOND_PORT, NMSetting)
+
+/*****************************************************************************/
+
+/**
+ * nm_setting_bond_port_get_queue_id:
+ * @setting: the #NMSettingBondPort
+ *
+ * Returns: the #NMSettingBondPort:queue_id property of the setting
+ *
+ * Since: 1.34
+ **/
+guint32
+nm_setting_bond_port_get_queue_id(NMSettingBondPort *setting)
+{
+    g_return_val_if_fail(NM_IS_SETTING_BOND_PORT(setting), 0);
+
+    return NM_SETTING_BOND_PORT_GET_PRIVATE(setting)->queue_id;
+}
+
+/*****************************************************************************/
+
+static gboolean
+verify(NMSetting *setting, NMConnection *connection, GError **error)
+{
+    if (connection) {
+        NMSettingConnection *s_con;
+        const char *         slave_type;
+
+        s_con = nm_connection_get_setting_connection(connection);
+        if (!s_con) {
+            g_set_error(error,
+                        NM_CONNECTION_ERROR,
+                        NM_CONNECTION_ERROR_MISSING_SETTING,
+                        _("missing setting"));
+            g_prefix_error(error, "%s: ", NM_SETTING_CONNECTION_SETTING_NAME);
+            return FALSE;
+        }
+
+        slave_type = nm_setting_connection_get_slave_type(s_con);
+        if (!nm_streq0(slave_type, NM_SETTING_BOND_SETTING_NAME)) {
+            g_set_error(error,
+                        NM_CONNECTION_ERROR,
+                        NM_CONNECTION_ERROR_INVALID_PROPERTY,
+                        _("A connection with a '%s' setting must have the slave-type set to '%s'. "
+                          "Instead it is '%s'"),
+                        NM_SETTING_BOND_PORT_SETTING_NAME,
+                        NM_SETTING_BOND_SETTING_NAME,
+                        slave_type);
+            g_prefix_error(error,
+                           "%s.%s: ",
+                           NM_SETTING_CONNECTION_SETTING_NAME,
+                           NM_SETTING_CONNECTION_SLAVE_TYPE);
+            return FALSE;
+        }
+    }
+
+    return TRUE;
+}
+
+/*****************************************************************************/
+
+static void
+nm_setting_bond_port_init(NMSettingBondPort *setting)
+{}
+
+/**
+ * nm_setting_bond_port_new:
+ *
+ * Creates a new #NMSettingBondPort object with default values.
+ *
+ * Returns: (transfer full): the new empty #NMSettingBondPort object
+ *
+ * Since: 1.34
+ **/
+NMSetting *
+nm_setting_bond_port_new(void)
+{
+    return g_object_new(NM_TYPE_SETTING_BOND_PORT, NULL);
+}
+
+static void
+nm_setting_bond_port_class_init(NMSettingBondPortClass *klass)
+{
+    GObjectClass *  object_class        = G_OBJECT_CLASS(klass);
+    NMSettingClass *setting_class       = NM_SETTING_CLASS(klass);
+    GArray *        properties_override = _nm_sett_info_property_override_create_array();
+
+    object_class->get_property = _nm_setting_property_get_property_direct;
+    object_class->set_property = _nm_setting_property_set_property_direct;
+
+    setting_class->verify          = verify;
+    setting_class->finalize_direct = TRUE;
+
+    /**
+     * NMSettingBondPort:queue-id:
+     *
+     * The queue ID of this bond port. The maximum value of queue ID is
+     * the number of TX queues currently active in device.
+     *
+     * Since: 1.34
+     **/
+    /* ---ifcfg-rh---
+     * property: queue-id
+     * variable: BONDING_OPTS: queue-id=
+     * values: 0 - 65535
+     * default: 0
+     * description: Queue ID.
+     * ---end---
+     */
+    _nm_setting_property_define_direct_uint32(properties_override,
+                                              obj_properties,
+                                              NM_SETTING_BOND_PORT_QUEUE_ID,
+                                              PROP_QUEUE_ID,
+                                              0,
+                                              G_MAXUINT16,
+                                              NM_BOND_PORT_QUEUE_ID_DEF,
+                                              NM_SETTING_PARAM_INFERRABLE,
+                                              NMSettingBondPort,
+                                              _priv.queue_id);
+
+    g_object_class_install_properties(object_class, _PROPERTY_ENUMS_LAST, obj_properties);
+
+    _nm_setting_class_commit(setting_class,
+                             NM_META_SETTING_TYPE_BOND_PORT,
+                             NULL,
+                             properties_override,
+                             0);
+}

Makefile.am
@@ -1172,6 +1172,7 @@ src_libnm_core_impl_lib_h_pub_real = \
 	src/libnm-core-public/nm-setting-8021x.h \
 	src/libnm-core-public/nm-setting-adsl.h \
 	src/libnm-core-public/nm-setting-bluetooth.h \
+	src/libnm-core-public/nm-setting-bond-port.h \
 	src/libnm-core-public/nm-setting-bond.h \
 	src/libnm-core-public/nm-setting-bridge-port.h \
 	src/libnm-core-public/nm-setting-bridge.h \
@@ -1253,6 +1254,7 @@ src_libnm_core_impl_lib_c_settings_real = \
 	src/libnm-core-impl/nm-setting-adsl.c \
 	src/libnm-core-impl/nm-setting-bluetooth.c \
 	src/libnm-core-impl/nm-setting-bond.c \
+	src/libnm-core-impl/nm-setting-bond-port.c \
 	src/libnm-core-impl/nm-setting-bridge-port.c \
 	src/libnm-core-impl/nm-setting-bridge.c \
 	src/libnm-core-impl/nm-setting-cdma.c \
@@ -3412,6 +3414,7 @@ EXTRA_DIST += \
 	src/core/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-bond-eth-type \
 	src/core/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-bond-main \
 	src/core/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-bond-mode-numeric \
+	src/core/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-bond-port \
 	src/core/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-bond-slave \
 	src/core/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-bond-slave-ib \
 	src/core/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-bridge-component \
@@ -5086,6 +5089,8 @@ src_nmtui_nmtui_SOURCES = \
 	src/nmtui/nmt-mtu-entry.h \
 	src/nmtui/nmt-page-bond.c \
 	src/nmtui/nmt-page-bond.h \
+	src/nmtui/nmt-page-bond-port.c \
+	src/nmtui/nmt-page-bond-port.h \
 	src/nmtui/nmt-page-bridge.c \
 	src/nmtui/nmt-page-bridge.h \
 	src/nmtui/nmt-page-bridge-port.c \

docs/libnm/libnm-docs.xml
@@ -315,6 +315,7 @@ print ("NetworkManager version " + client.get_version())]]></programlisting></in
     <xi:include href="xml/nm-setting-8021x.xml"/>
     <xi:include href="xml/nm-setting-adsl.xml"/>
     <xi:include href="xml/nm-setting-bluetooth.xml"/>
+    <xi:include href="xml/nm-setting-bond-port.xml"/>
     <xi:include href="xml/nm-setting-bond.xml"/>
     <xi:include href="xml/nm-setting-bridge-port.xml"/>
     <xi:include href="xml/nm-setting-bridge.xml"/>

po/POTFILES.in
@@ -83,6 +83,7 @@ src/libnm-core-impl/nm-setting-6lowpan.c
 src/libnm-core-impl/nm-setting-8021x.c
 src/libnm-core-impl/nm-setting-adsl.c
 src/libnm-core-impl/nm-setting-bluetooth.c
+src/libnm-core-impl/nm-setting-bond-port.c
 src/libnm-core-impl/nm-setting-bond.c
 src/libnm-core-impl/nm-setting-bridge-port.c
 src/libnm-core-impl/nm-setting-bridge.c
@@ -159,6 +160,7 @@ src/nmtui/nmt-edit-connection-list.c
 src/nmtui/nmt-editor-section.c
 src/nmtui/nmt-editor.c
 src/nmtui/nmt-mtu-entry.c
+src/nmtui/nmt-page-bond-port.c
 src/nmtui/nmt-page-bond.c
 src/nmtui/nmt-page-bridge-port.c
 src/nmtui/nmt-page-bridge.c

src/core/devices/nm-device-bond.c
@@ -17,6 +17,7 @@
 #include "libnm-core-aux-intern/nm-libnm-core-utils.h"
 #include "libnm-core-intern/nm-core-internal.h"
 #include "nm-ip4-config.h"
+#include "nm-setting-bond-port.h"
 
 #define _NMLOG_DEVICE_TYPE NMDeviceBond
 #include "nm-device-logging.h"
@@ -215,11 +216,33 @@ update_connection(NMDevice *device, NMConnection *connection)
 }
 
 static gboolean
-master_update_slave_connection(NMDevice *    self,
-                               NMDevice *    slave,
-                               NMConnection *connection,
-                               GError **     error)
+controller_update_port_connection(NMDevice *    self,
+                                  NMDevice *    port,
+                                  NMConnection *connection,
+                                  GError **     error)
 {
+    NMSettingBondPort *s_port;
+    int                ifindex_port = nm_device_get_ifindex(port);
+    uint               queue_id     = NM_BOND_PORT_QUEUE_ID_DEF;
+    gs_free char *     queue_id_str = NULL;
+
+    g_return_val_if_fail(ifindex_port > 0, FALSE);
+
+    s_port = _nm_connection_get_setting_bond_port(connection);
+    if (!s_port) {
+        s_port = NM_SETTING_BOND_PORT(nm_setting_bond_port_new());
+        nm_connection_add_setting(connection, NM_SETTING(s_port));
+    }
+
+    queue_id_str =
+        nm_platform_sysctl_slave_get_option(nm_device_get_platform(self), ifindex_port, "queue_id");
+    if (queue_id_str) {
+        queue_id =
+            _nm_utils_ascii_str_to_int64(queue_id_str, 10, 0, 65535, NM_BOND_PORT_QUEUE_ID_DEF);
+        g_object_set(s_port, NM_SETTING_BOND_PORT_QUEUE_ID, queue_id, NULL);
+    } else
+        _LOGW(LOGD_BOND, "failed to read bond port setting '%s'", NM_SETTING_BOND_PORT_QUEUE_ID);
+
     g_object_set(nm_connection_get_setting_connection(connection),
                  NM_SETTING_CONNECTION_MASTER,
                  nm_device_get_iface(self),
@@ -384,30 +407,63 @@ act_stage1_prepare(NMDevice *device, NMDeviceStateReason *out_failure_reason)
     return ret;
 }
 
-static gboolean
-enslave_slave(NMDevice *device, NMDevice *slave, NMConnection *connection, gboolean configure)
-{
-    NMDeviceBond *self = NM_DEVICE_BOND(device);
+/*
+ * The queue id format is '$interface_name:$queue_id'
+ * The max queue 65535 hold 5 chars.
+ */
+#define _MAX_QUEUE_ID_STR_LEN 5 + IFNAMSIZ + 1 + 1
 
-    nm_device_master_check_slave_physical_port(device, slave, LOGD_BOND);
+static void
+commit_port_options(NMDevice *bond_device, NMDevice *port, NMSettingBondPort *set_port)
+{
+    char queue_id_str[_MAX_QUEUE_ID_STR_LEN];
+
+    /*
+     * The queue-id of bond port is read only, we should modify bond interface using:
+     *    echo "eth1:2" > /sys/class/net/bond0/bonding/queue_id
+     * Kernel allows parital editing, so no need to care about other bond ports.
+     */
+    snprintf(queue_id_str,
+             _MAX_QUEUE_ID_STR_LEN,
+             "%s:%" G_GUINT32_FORMAT,
+             nm_device_get_iface(port),
+             set_port ? nm_setting_bond_port_get_queue_id(set_port) : NM_BOND_PORT_QUEUE_ID_DEF);
+
+    nm_platform_sysctl_master_set_option(nm_device_get_platform(bond_device),
+                                         nm_device_get_ifindex(bond_device),
+                                         "queue_id",
+                                         queue_id_str);
+}
+
+static gboolean
+enslave_slave(NMDevice *device, NMDevice *port, NMConnection *connection, gboolean configure)
+{
+    NMDeviceBond *     self = NM_DEVICE_BOND(device);
+    NMSettingBondPort *s_port;
+
+    nm_device_master_check_slave_physical_port(device, port, LOGD_BOND);
 
     if (configure) {
         gboolean success;
 
-        nm_device_take_down(slave, TRUE);
+        nm_device_take_down(port, TRUE);
         success = nm_platform_link_enslave(nm_device_get_platform(device),
                                            nm_device_get_ip_ifindex(device),
-                                           nm_device_get_ip_ifindex(slave));
-        nm_device_bring_up(slave, TRUE, NULL);
+                                           nm_device_get_ip_ifindex(port));
+        nm_device_bring_up(port, TRUE, NULL);
 
         if (!success) {
-            _LOGI(LOGD_BOND, "enslaved bond slave %s: failed", nm_device_get_ip_iface(slave));
+            _LOGI(LOGD_BOND, "assigning bond port %s: failed", nm_device_get_ip_iface(port));
             return FALSE;
         }
 
-        _LOGI(LOGD_BOND, "enslaved bond slave %s", nm_device_get_ip_iface(slave));
+        s_port = _nm_connection_get_setting_bond_port(connection);
+
+        commit_port_options(device, port, s_port);
+
+        _LOGI(LOGD_BOND, "assigned bond port %s", nm_device_get_ip_iface(port));
     } else
-        _LOGI(LOGD_BOND, "bond slave %s was enslaved", nm_device_get_ip_iface(slave));
+        _LOGI(LOGD_BOND, "bond port %s was assigned", nm_device_get_ip_iface(port));
 
     return TRUE;
 }
@@ -613,7 +669,7 @@ nm_device_bond_class_init(NMDeviceBondClass *klass)
     device_class->complete_connection      = complete_connection;
 
     device_class->update_connection              = update_connection;
-    device_class->master_update_slave_connection = master_update_slave_connection;
+    device_class->master_update_slave_connection = controller_update_port_connection;
 
     device_class->create_and_realize = create_and_realize;
     device_class->act_stage1_prepare = act_stage1_prepare;

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-reader.c
@@ -31,6 +31,7 @@
 #include "nm-setting-ethtool.h"
 #include "nm-setting-8021x.h"
 #include "nm-setting-bond.h"
+#include "nm-setting-bond-port.h"
 #include "nm-setting-team.h"
 #include "nm-setting-team-port.h"
 #include "nm-setting-bridge.h"
@@ -5493,6 +5494,37 @@ make_bond_setting(shvarFile *ifcfg, const char *file, GError **error)
     return (NMSetting *) s_bond;
 }
 
+static NMSetting *
+make_bond_port_setting(shvarFile *ifcfg)
+{
+    NMSetting *   s_port        = NULL;
+    gs_free char *value_to_free = NULL;
+    const char *  value;
+    guint32       queue_id = 0;
+
+    g_return_val_if_fail(ifcfg != NULL, FALSE);
+
+    value = svGetValueStr(ifcfg, "BOND_PORT_QUEUE_ID", &value_to_free);
+    if (value) {
+        s_port = nm_setting_bond_port_new();
+        queue_id =
+            _nm_utils_ascii_str_to_uint64(value, 10, 0, G_MAXUINT16, NM_BOND_PORT_QUEUE_ID_DEF);
+        if (errno != 0) {
+            PARSE_WARNING("Invalid bond port queue_id value '%s': error %d", value, errno);
+            nm_clear_g_free(&value_to_free);
+            return s_port;
+        } else {
+            nm_clear_g_free(&value_to_free);
+            nm_g_object_set_property_uint(G_OBJECT(s_port),
+                                          NM_SETTING_BOND_PORT_QUEUE_ID,
+                                          queue_id,
+                                          NULL);
+        }
+    }
+
+    return s_port;
+}
+
 static NMConnection *
 bond_connection_from_ifcfg(const char *file, shvarFile *ifcfg, GError **error)
 {
@@ -6657,6 +6689,10 @@ connection_from_file_full(const char *filename,
         nm_connection_add_setting(connection, setting);
 
     setting = make_bridge_port_setting(main_ifcfg);
+    if (setting)
+        nm_connection_add_setting(connection, setting);
+
+    setting = make_bond_port_setting(main_ifcfg);
     if (setting)
         nm_connection_add_setting(connection, setting);
 

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-utils.c
@@ -827,6 +827,7 @@ const NMSIfcfgKeyTypeInfo nms_ifcfg_well_known_keys[] = {
     _KEY_TYPE("BAND", NMS_IFCFG_KEY_TYPE_IS_PLAIN),
     _KEY_TYPE("BONDING_MASTER", NMS_IFCFG_KEY_TYPE_IS_PLAIN),
     _KEY_TYPE("BONDING_OPTS", NMS_IFCFG_KEY_TYPE_IS_PLAIN),
+    _KEY_TYPE("BOND_PORT_QUEUE_ID", NMS_IFCFG_KEY_TYPE_IS_PLAIN),
     _KEY_TYPE("BOOTPROTO", NMS_IFCFG_KEY_TYPE_IS_PLAIN),
     _KEY_TYPE("BRIDGE", NMS_IFCFG_KEY_TYPE_IS_PLAIN),
     _KEY_TYPE("BRIDGE_MACADDR", NMS_IFCFG_KEY_TYPE_IS_PLAIN),

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-utils.h
@@ -33,7 +33,7 @@ typedef struct {
     NMSIfcfgKeyTypeFlags key_flags;
 } NMSIfcfgKeyTypeInfo;
 
-extern const NMSIfcfgKeyTypeInfo nms_ifcfg_well_known_keys[251];
+extern const NMSIfcfgKeyTypeInfo nms_ifcfg_well_known_keys[252];
 
 const NMSIfcfgKeyTypeInfo *nms_ifcfg_well_known_key_find_info(const char *key, gssize *out_idx);
 

commit 23ce9cff99313b9cefb7770262347731161f4539

libnm-core: allow empty slave-type with a NMSettingBondPort
It is allowed to have a connection with empty connection.slave-type
and a NMSettingBondPort; the property will be set automatically during
normalization if a master is set, otherwise the setting will be removed.

With this change, it becomes possible to remove a port from a bond
from nmcli, turning it into a non-slave connection. Before, this used
to fail with:

  $ nmcli connection add type ethernet ifname test con-name test+ connection.master bond0 connection.slave-type bond
  $ nmcli connection modify test+ connection.master '' connection.slave-type ''
  Error: Failed to modify connection 'test+': connection.slave-type: A connection with a 'bond-port' setting must have the slave-type set to 'bond'

https://bugzilla.redhat.com/show_bug.cgi?id=2126262
https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1382

Fixes: 9958510f2872 ('bond: add support of queue_id of bond port')

src/libnm-core-impl/nm-setting-bond-port.c
@@ -85,13 +85,15 @@ verify(NMSetting *setting, NMConnection *connection, GError **error)
         }
 
         slave_type = nm_setting_connection_get_slave_type(s_con);
-        if (!nm_streq0(slave_type, NM_SETTING_BOND_SETTING_NAME)) {
+        if (slave_type && !nm_streq(slave_type, NM_SETTING_BOND_SETTING_NAME)) {
             g_set_error(error,
                         NM_CONNECTION_ERROR,
                         NM_CONNECTION_ERROR_INVALID_PROPERTY,
-                        _("A connection with a '%s' setting must have the slave-type set to '%s'"),
+                        _("A connection with a '%s' setting must have the slave-type set to '%s'. "
+                          "Instead it is '%s'"),
                         NM_SETTING_BOND_PORT_SETTING_NAME,
-                        NM_SETTING_BOND_SETTING_NAME);
+                        NM_SETTING_BOND_SETTING_NAME,
+                        slave_type);
             g_prefix_error(error,
                            "%s.%s: ",
                            NM_SETTING_CONNECTION_SETTING_NAME,