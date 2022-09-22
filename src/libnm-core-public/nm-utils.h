NetworkManager/NetworkManager

commit 08eff4c46ee618fcf00ddde28a425d10ba0df041

glib-aux: rename IP address related helpers from "nm-inet-utils.h"
- name things related to `in_addr_t`, `struct in6_addr`, `NMIPAddr` as
  `nm_ip4_addr_*()`, `nm_ip6_addr_*()`, `nm_ip_addr_*()`, respectively.

- we have a wrapper `nm_inet_ntop()` for `inet_ntop()`. This name
  of our wrapper is chosen to be familiar with the libc underlying
  function. With this, also name functions that are about string
  representations of addresses `nm_inet_*()`, `nm_inet4_*()`,
  `nm_inet6_*()`. For example, `nm_inet_parse_str()`,
  `nm_inet_is_normalized()`.

<<<<

  R() {
     git grep -l "$1" | xargs sed -i "s/\<$1\>/$2/g"
  }

  R NM_CMP_DIRECT_IN4ADDR_SAME_PREFIX          NM_CMP_DIRECT_IP4_ADDR_SAME_PREFIX
  R NM_CMP_DIRECT_IN6ADDR_SAME_PREFIX          NM_CMP_DIRECT_IP6_ADDR_SAME_PREFIX
  R NM_UTILS_INET_ADDRSTRLEN                   NM_INET_ADDRSTRLEN
  R _nm_utils_inet4_ntop                       nm_inet4_ntop
  R _nm_utils_inet6_ntop                       nm_inet6_ntop
  R _nm_utils_ip4_get_default_prefix           nm_ip4_addr_get_default_prefix
  R _nm_utils_ip4_get_default_prefix0          nm_ip4_addr_get_default_prefix0
  R _nm_utils_ip4_netmask_to_prefix            nm_ip4_addr_netmask_to_prefix
  R _nm_utils_ip4_prefix_to_netmask            nm_ip4_addr_netmask_from_prefix
  R nm_utils_inet4_ntop_dup                    nm_inet4_ntop_dup
  R nm_utils_inet6_ntop_dup                    nm_inet6_ntop_dup
  R nm_utils_inet_ntop                         nm_inet_ntop
  R nm_utils_inet_ntop_dup                     nm_inet_ntop_dup
  R nm_utils_ip4_address_clear_host_address    nm_ip4_addr_clear_host_address
  R nm_utils_ip4_address_is_link_local         nm_ip4_addr_is_link_local
  R nm_utils_ip4_address_is_loopback           nm_ip4_addr_is_loopback
  R nm_utils_ip4_address_is_zeronet            nm_ip4_addr_is_zeronet
  R nm_utils_ip4_address_same_prefix           nm_ip4_addr_same_prefix
  R nm_utils_ip4_address_same_prefix_cmp       nm_ip4_addr_same_prefix_cmp

src/libnm-core-public/nm-utils.h
@@ -171,12 +171,12 @@ gboolean nm_utils_is_valid_iface_name(const char *name, GError **error);
 gboolean nm_utils_is_uuid(const char *str);
 
 /**
- * NM_UTILS_INET_ADDRSTRLEN:
+ * NM_INET_ADDRSTRLEN:
  *
  * Defines the minimal length for a char buffer that is suitable as @dst argument
  * for both nm_utils_inet4_ntop() and nm_utils_inet6_ntop().
  **/
-#define NM_UTILS_INET_ADDRSTRLEN INET6_ADDRSTRLEN
+#define NM_INET_ADDRSTRLEN INET6_ADDRSTRLEN
 
 const char *nm_utils_inet4_ntop(guint32 inaddr, char *dst);
 

commit d1fd6eb53bff65807f97535713d5f8959dbb8cbc

libnm: fix API break in "nm-utils.h" for NM_UTILS_INET_ADDRSTRLEN
This is part of public API, and was wrongly renamed during some internal
refactoring.

Reported-by: Eivind Næss <eivnaes@yahoo.com>

Fixes: 08eff4c46ee6 ('glib-aux: rename IP address related helpers from "nm-inet-utils.h"')

src/libnm-core-public/nm-utils.h
@@ -171,12 +171,12 @@ gboolean nm_utils_is_valid_iface_name(const char *name, GError **error);
 gboolean nm_utils_is_uuid(const char *str);
 
 /**
- * NM_INET_ADDRSTRLEN:
+ * NM_UTILS_INET_ADDRSTRLEN:
  *
  * Defines the minimal length for a char buffer that is suitable as @dst argument
  * for both nm_utils_inet4_ntop() and nm_utils_inet6_ntop().
  **/
-#define NM_INET_ADDRSTRLEN INET6_ADDRSTRLEN
+#define NM_UTILS_INET_ADDRSTRLEN INET6_ADDRSTRLEN
 
 const char *nm_utils_inet4_ntop(guint32 inaddr, char *dst);
 