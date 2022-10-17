NetworkManager/NetworkManager

commit b784da83d27ef2b366fa2cd73ee653becac42793

nmcli: show sriov capability in "nmcli" output
This is sometimes interesting to know.

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1431

src/nmcli/general.c
@@ -1301,6 +1301,7 @@ device_overview(NmCli *nmc, NMDevice *device)
     GString         *outbuf = g_string_sized_new(80);
     char            *tmp;
     const GPtrArray *activatable;
+    NMDeviceCapabilities caps;
 
     activatable = nm_device_get_available_connections(device);
 
@@ -1378,10 +1379,13 @@ device_overview(NmCli *nmc, NMDevice *device)
         break;
     }
 
-    if (nm_device_is_software(device))
+    caps = nm_device_get_capabilities(device);
+    if (caps & NM_DEVICE_CAP_IS_SOFTWARE)
         g_string_append_printf(outbuf, "%s, ", _("sw"));
     else
         g_string_append_printf(outbuf, "%s, ", _("hw"));
+    if (caps & NM_DEVICE_CAP_SRIOV)
+        g_string_append_printf(outbuf, "%s, ", _("sriov"));
 
     if (!NM_IN_STRSET(nm_device_get_ip_iface(device), NULL, nm_device_get_iface(device)))
         g_string_append_printf(outbuf, "%s %s, ", _("iface"), nm_device_get_ip_iface(device));

commit 37582fc38420a9af7a18cf4010a8c29abdfc3d78

style: fix code formatting
Fixes: b784da83d27e ('nmcli: show sriov capability in "nmcli" output')

src/nmcli/general.c
@@ -1298,9 +1298,9 @@ client_state(NMClient *client, GParamSpec *param, NmCli *nmc)
 static void
 device_overview(NmCli *nmc, NMDevice *device)
 {
-    GString         *outbuf = g_string_sized_new(80);
-    char            *tmp;
-    const GPtrArray *activatable;
+    GString             *outbuf = g_string_sized_new(80);
+    char                *tmp;
+    const GPtrArray     *activatable;
     NMDeviceCapabilities caps;
 
     activatable = nm_device_get_available_connections(device);