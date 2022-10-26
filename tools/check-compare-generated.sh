139f4b4b2ee153e728f111143a55d126e7fd0e65
Thomas Haller <thaller@redhat.com>
2022-10-26 09:59:42 +0200

    build: pass both filenames to "tools/check-compare-generated.sh" script
    It just feels nicer to be explicit about the filenames and
    not rely on a specific naming.
    
    Also, in meson we can directly pass the target as argument, which
    expands to the filename but also adds a dependency.

--- tools/check-compare-generated.sh
+++ tools/check-compare-generated.sh
@@ -2,17 +2,16 @@
 
 set -e
 
-srcdir="$1"
-builddir="$2"
-doc_h="$3"
+f_commited="$1"
+f_generated="$2"
 
 [ -n "$NMTST_NO_CHECK_SETTINGS_DOCS" ] && exit 0
 
-cmp -s "${srcdir}/${doc_h}.in" "${builddir}/${doc_h}" && exit 0
+cmp -s "$f_commited" "$f_generated" && exit 0
 
 if [ "$NM_TEST_REGENERATE" = 1 ] ; then
-   cp -f "${builddir}/${doc_h}" "${srcdir}/${doc_h}.in"
+   cp -f "$f_generated" "$f_commited"
 else
-   echo "*** Error: the generated file '${builddir}/${doc_h}' differs from the source file '${srcdir}/${doc_h}.in'. You probably should copy the generated file over to the source file. You can skip this test by setting NMTST_NO_CHECK_SETTINGS_DOCS=yes. You can also automatically copy the file by rerunning the test with NM_TEST_REGENERATE=1"
+   echo "*** Error: the generated file '$f_generated' differs from the source file '$f_commited'. You probably should copy the generated file over to the source file. You can skip this test by setting NMTST_NO_CHECK_SETTINGS_DOCS=yes. You can also automatically copy the file by rerunning the test with NM_TEST_REGENERATE=1"
    exit 1
 fi
