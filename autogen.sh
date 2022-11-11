6afafec47840a65bc3d0d22df0717f03c985d660
Lubomir Rintel <lkundrak@v3.sk>
2022-11-11 20:45:21 +0100

    MONSTROSITY

--- autogen.sh
+++ autogen.sh
@@ -1,31 +1,81 @@
-#!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+#!/bin/bash
 
-set -e
+set -x
+echo ===================================
+env
 
-srcdir=`dirname $0`
-if test -z "$srcdir"; then
-    srcdir=.
-fi
+echo ===================================
+NOCONFIGURE=yes ./orig-autogen.sh
 
-olddir=`pwd`
+echo ===================================
+./configure \
+PYTHON="${PYTHON}" \
+--enable-maintainer-mode \
+--enable-more-warnings=error \
+--prefix=/opt/test \
+--sysconfdir=/etc \
+--enable-gtk-doc \
+--enable-more-asserts \
+--with-more-asserts=100 \
+--enable-more-logging \
+--enable-compile-warnings=yes\
+--with-valgrind=no \
+--enable-concheck \
+--enable-ifcfg-rh \
+--enable-ifcfg-suse \
+--enable-ifupdown \
+--enable-ifnet \
+--enable-vala=yes \
+--enable-polkit=yes \
+--with-nmtui=yes \
+--with-modem-manager-1 \
+--with-suspend-resume=systemd \
+--enable-teamdctl=yes \
+--enable-tests=root \
+--with-netconfig=/path/does/not/exist/netconfig \
+--with-resolvconf=/path/does/not/exist/resolvconf \
+--with-crypto=nss \
+--with-session-tracking=systemd \
+--with-consolekit=yes \
+--with-systemd-logind=yes \
+--with-consolekit=yes
 
-REQUIRED_AUTOMAKE_VERSION=1.9
-PKG_NAME=NetworkManager
+make -j20
+make check -k
 
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/src/core/main.c) || {
-    printf "**Error**: Directory "\`$srcdir\'" does not look like the" >&2
-    echo " top-level $PKG_NAME directory" >&2
-    exit 1
-}
+echo ===================================
+make check -k
 
-cd $srcdir
+echo ===================================
+src/libnm-client-impl/tests/test-nm-client
 
-gtkdocize
-autoreconf --verbose --force --install
+echo ===================================
+src/libnm-client-impl/tests/test-nm-client -p /libnm/activate-virtual
 
-cd $olddir
-if test -z "$NOCONFIGURE"; then
-    exec $srcdir/configure --enable-maintainer-mode --enable-more-warnings=error --enable-gtk-doc --enable-introspection "$@"
-fi
+
+echo ===================================
+env -i XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR src/libnm-client-impl/tests/test-nm-client
+
+echo ===================================
+env -i XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR src/libnm-client-impl/tests/test-nm-client -p /libnm/activate-virtual
+
+
+echo ===================================
+dnf -y install strace
+
+echo ===================================
+strace -s4096 -f -otest1 src/libnm-client-impl/tests/test-nm-client
+
+echo ===================================
+strace -s4096 -f -otest2 src/libnm-client-impl/tests/test-nm-client -p /libnm/activate-virtual
+
+echo ===================================
+strace -s4096 -f -otest3 env -i XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR src/libnm-client-impl/tests/test-nm-client
+
+echo ===================================
+strace -s4096 -f -otest4 env -i XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR src/libnm-client-impl/tests/test-nm-client -p /libnm/activate-virtual
+
+
+grep '' test?
+
+exit 1
---
+++ orig-autogen.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+set -e
+
+srcdir=`dirname $0`
+if test -z "$srcdir"; then
+    srcdir=.
+fi
+
+olddir=`pwd`
+
+REQUIRED_AUTOMAKE_VERSION=1.9
+PKG_NAME=NetworkManager
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/src/core/main.c) || {
+    printf "**Error**: Directory "\`$srcdir\'" does not look like the" >&2
+    echo " top-level $PKG_NAME directory" >&2
+    exit 1
+}
+
+cd $srcdir
+
+gtkdocize
+autoreconf --verbose --force --install
+
+cd $olddir
+if test -z "$NOCONFIGURE"; then
+    exec $srcdir/configure --enable-maintainer-mode --enable-more-warnings=error --enable-gtk-doc --enable-introspection "$@"
+fi
