NetworkManager/NetworkManager

commit e8618f03d7d8735946a82de8ce4257dd92328b20

support loopback interface
Support managing the loopback interface through NM as the users want to
set the proper mtu for loopback interface when forwarding the packets.
Additionally, the IP addresses, DNS, route and routing rules are also
allowed to configure for the loopback connection profiles.

https://bugzilla.redhat.com/show_bug.cgi?id=2060905

src/libnm-core-impl/nm-setting-loopback.c
@@ -1,0 +1,194 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2022 Red Hat, Inc.
+ */
+
+#include "libnm-core-impl/nm-default-libnm-core.h"
+
+#include "nm-setting-loopback.h"
+
+#include "nm-connection-private.h"
+#include "nm-setting-connection.h"
+#include "nm-setting-private.h"
+
+/**
+ * SECTION:nm-setting-loopback
+ * @short_description: Describes connection properties for loopback interfaces
+ *
+ * The #NMSettingLoopback object is a #NMSetting subclass that describes properties
+ * necessary for connection to loopback devices
+ **/
+
+/*****************************************************************************/
+
+NM_GOBJECT_PROPERTIES_DEFINE(NMSettingLoopback, PROP_MTU, );
+
+typedef struct {
+    guint32 mtu;
+} NMSettingLoopbackPrivate;
+
+/**
+ * NMSettingLoopback:
+ *
+ * Loopback Link Settings
+ *
+ * Since: 1.42
+ */
+struct _NMSettingLoopback {
+    NMSetting                parent;
+    NMSettingLoopbackPrivate _priv;
+};
+
+struct _NMSettingLoopbackClass {
+    NMSettingClass parent;
+};
+
+#define NM_SETTING_LOOPBACK_GET_PRIVATE(self) \
+    _NM_GET_PRIVATE(self, NMSettingLoopback, NM_IS_SETTING_LOOPBACK)
+
+G_DEFINE_TYPE(NMSettingLoopback, nm_setting_loopback, NM_TYPE_SETTING)
+
+/*****************************************************************************/
+
+/**
+ * nm_setting_loopback_get_mtu:
+ * @setting: the #NMSettingLoopback
+ *
+ * Returns: the #NMSettingLoopback:mtu property of the setting
+ *
+ * Since: 1.42
+ **/
+guint32
+nm_setting_loopback_get_mtu(NMSettingLoopback *setting)
+{
+    g_return_val_if_fail(NM_IS_SETTING_LOOPBACK(setting), 0);
+
+    return NM_SETTING_LOOPBACK_GET_PRIVATE(setting)->mtu;
+}
+
+static gboolean
+verify(NMSetting *setting, NMConnection *connection, GError **error)
+{
+    if (connection) {
+        NMSettingIPConfig *s_ip4;
+        NMSettingIPConfig *s_ip6;
+        const char        *method;
+
+        if ((s_ip4 = nm_connection_get_setting_ip4_config(connection))) {
+            if ((method = nm_setting_ip_config_get_method(s_ip4))
+                && !NM_IN_STRSET(method,
+                                 NM_SETTING_IP4_CONFIG_METHOD_AUTO,
+                                 NM_SETTING_IP4_CONFIG_METHOD_MANUAL)) {
+                g_set_error(error,
+                            NM_CONNECTION_ERROR,
+                            NM_CONNECTION_ERROR_INVALID_PROPERTY,
+                            _("ipv4 method \"%s\" is not supported for loopback"),
+                            method);
+                g_prefix_error(error,
+                               "%s.%s: ",
+                               NM_SETTING_IP4_CONFIG_SETTING_NAME,
+                               NM_SETTING_IP_CONFIG_METHOD);
+                return FALSE;
+            }
+            if (!NM_IN_SET(nm_setting_ip4_config_get_link_local(NM_SETTING_IP4_CONFIG(s_ip4)),
+                           NM_SETTING_IP4_LL_DEFAULT,
+                           NM_SETTING_IP4_LL_AUTO,
+                           NM_SETTING_IP4_LL_DISABLED)) {
+                g_set_error(error,
+                            NM_CONNECTION_ERROR,
+                            NM_CONNECTION_ERROR_INVALID_PROPERTY,
+                            _("ipv4.link-local cannot be enabled for loopback"));
+                g_prefix_error(error,
+                               "%s.%s: ",
+                               NM_SETTING_IP4_CONFIG_SETTING_NAME,
+                               NM_SETTING_IP4_CONFIG_LINK_LOCAL);
+                return FALSE;
+            }
+        }
+        if ((s_ip6 = nm_connection_get_setting_ip6_config(connection))) {
+            if ((method = nm_setting_ip_config_get_method(s_ip6))
+                && !NM_IN_STRSET(method,
+                                 NM_SETTING_IP6_CONFIG_METHOD_AUTO,
+                                 NM_SETTING_IP6_CONFIG_METHOD_MANUAL)) {
+                g_set_error(error,
+                            NM_CONNECTION_ERROR,
+                            NM_CONNECTION_ERROR_INVALID_PROPERTY,
+                            _("ipv6 method \"%s\" is not supported for loopback"),
+                            method);
+                g_prefix_error(error,
+                               "%s.%s: ",
+                               NM_SETTING_IP6_CONFIG_SETTING_NAME,
+                               NM_SETTING_IP_CONFIG_METHOD);
+                return FALSE;
+            }
+        }
+    }
+    return TRUE;
+}
+
+/*****************************************************************************/
+
+static void
+nm_setting_loopback_init(NMSettingLoopback *setting)
+{}
+
+/**
+ * nm_setting_loopback_new:
+ *
+ * Creates a new #NMSettingLoopback object with default values.
+ *
+ * Returns: (transfer full): the new empty #NMSettingLoopback object
+ *
+ * Since: 1.42
+ **/
+NMSetting *
+nm_setting_loopback_new(void)
+{
+    return g_object_new(NM_TYPE_SETTING_LOOPBACK, NULL);
+}
+
+static void
+nm_setting_loopback_class_init(NMSettingLoopbackClass *klass)
+{
+    GObjectClass   *object_class        = G_OBJECT_CLASS(klass);
+    NMSettingClass *setting_class       = NM_SETTING_CLASS(klass);
+    GArray         *properties_override = _nm_sett_info_property_override_create_array();
+
+    object_class->get_property = _nm_setting_property_get_property_direct;
+    object_class->set_property = _nm_setting_property_set_property_direct;
+
+    setting_class->verify = verify;
+
+    /**
+     * NMSettingLoopback:mtu:
+     *
+     * If non-zero, only transmit packets of the specified size or smaller,
+     * breaking larger packets up into multiple Ethernet frames.
+     *
+     * Since: 1.42
+     **/
+    /* ---ifcfg-rh---
+     * property: mtu
+     * variable: MTU
+     * description: MTU of the interface.
+     * ---end---
+     */
+    _nm_setting_property_define_direct_uint32(properties_override,
+                                              obj_properties,
+                                              NM_SETTING_LOOPBACK_MTU,
+                                              PROP_MTU,
+                                              0,
+                                              G_MAXUINT32,
+                                              0,
+                                              NM_SETTING_PARAM_FUZZY_IGNORE,
+                                              NMSettingLoopback,
+                                              _priv.mtu);
+
+    g_object_class_install_properties(object_class, _PROPERTY_ENUMS_LAST, obj_properties);
+
+    _nm_setting_class_commit(setting_class,
+                             NM_META_SETTING_TYPE_LOOPBACK,
+                             NULL,
+                             properties_override,
+                             0);
+}

Makefile.am
@@ -1024,6 +1024,8 @@ introspection_sources = \
 	introspection/org.freedesktop.NetworkManager.Device.IPTunnel.h \
 	introspection/org.freedesktop.NetworkManager.Device.Infiniband.c \
 	introspection/org.freedesktop.NetworkManager.Device.Infiniband.h \
+	introspection/org.freedesktop.NetworkManager.Device.Loopback.c \
+	introspection/org.freedesktop.NetworkManager.Device.Loopback.h \
 	introspection/org.freedesktop.NetworkManager.Device.Lowpan.c \
 	introspection/org.freedesktop.NetworkManager.Device.Lowpan.h \
 	introspection/org.freedesktop.NetworkManager.Device.Macsec.c \
@@ -1111,6 +1113,7 @@ DBUS_INTERFACE_DOCS = \
 	docs/api/dbus-org.freedesktop.NetworkManager.Device.Generic.xml \
 	docs/api/dbus-org.freedesktop.NetworkManager.Device.IPTunnel.xml \
 	docs/api/dbus-org.freedesktop.NetworkManager.Device.Infiniband.xml \
+	docs/api/dbus-org.freedesktop.NetworkManager.Device.Loopback.xml \
 	docs/api/dbus-org.freedesktop.NetworkManager.Device.Lowpan.xml \
 	docs/api/dbus-org.freedesktop.NetworkManager.Device.Macsec.xml \
 	docs/api/dbus-org.freedesktop.NetworkManager.Device.Macvlan.xml \
@@ -1179,6 +1182,7 @@ dbusinterfaces_DATA = \
 	introspection/org.freedesktop.NetworkManager.Device.Generic.xml \
 	introspection/org.freedesktop.NetworkManager.Device.IPTunnel.xml \
 	introspection/org.freedesktop.NetworkManager.Device.Infiniband.xml \
+	introspection/org.freedesktop.NetworkManager.Device.Loopback.xml \
 	introspection/org.freedesktop.NetworkManager.Device.Lowpan.xml \
 	introspection/org.freedesktop.NetworkManager.Device.Macsec.xml \
 	introspection/org.freedesktop.NetworkManager.Device.Macvlan.xml \
@@ -1260,6 +1264,7 @@ src_libnm_core_impl_lib_h_pub_real = \
 	src/libnm-core-public/nm-setting-ip-tunnel.h \
 	src/libnm-core-public/nm-setting-ip4-config.h \
 	src/libnm-core-public/nm-setting-ip6-config.h \
+	src/libnm-core-public/nm-setting-loopback.h \
 	src/libnm-core-public/nm-setting-macsec.h \
 	src/libnm-core-public/nm-setting-macvlan.h \
 	src/libnm-core-public/nm-setting-match.h \
@@ -1339,6 +1344,7 @@ src_libnm_core_impl_lib_c_settings_real = \
 	src/libnm-core-impl/nm-setting-ip-tunnel.c \
 	src/libnm-core-impl/nm-setting-ip4-config.c \
 	src/libnm-core-impl/nm-setting-ip6-config.c \
+	src/libnm-core-impl/nm-setting-loopback.c \
 	src/libnm-core-impl/nm-setting-macsec.c \
 	src/libnm-core-impl/nm-setting-macvlan.c \
 	src/libnm-core-impl/nm-setting-match.c \
@@ -1732,6 +1738,7 @@ libnm_lib_h_pub_real = \
 	src/libnm-client-public/nm-device-generic.h \
 	src/libnm-client-public/nm-device-infiniband.h \
 	src/libnm-client-public/nm-device-ip-tunnel.h \
+	src/libnm-client-public/nm-device-loopback.h \
 	src/libnm-client-public/nm-device-macsec.h \
 	src/libnm-client-public/nm-device-macvlan.h \
 	src/libnm-client-public/nm-device-modem.h \
@@ -1802,6 +1809,7 @@ libnm_lib_c_real = \
 	src/libnm-client-impl/nm-device-generic.c \
 	src/libnm-client-impl/nm-device-infiniband.c \
 	src/libnm-client-impl/nm-device-ip-tunnel.c \
+	src/libnm-client-impl/nm-device-loopback.c \
 	src/libnm-client-impl/nm-device-macsec.c \
 	src/libnm-client-impl/nm-device-macvlan.c \
 	src/libnm-client-impl/nm-device-modem.c \
@@ -2618,6 +2626,8 @@ src_core_libNetworkManager_la_SOURCES = \
 	src/core/devices/nm-device-infiniband.h \
 	src/core/devices/nm-device-ip-tunnel.c \
 	src/core/devices/nm-device-ip-tunnel.h \
+	src/core/devices/nm-device-loopback.c \
+	src/core/devices/nm-device-loopback.h \
 	src/core/devices/nm-device-macsec.c \
 	src/core/devices/nm-device-macsec.h \
 	src/core/devices/nm-device-macvlan.c \

docs/api/Makefile.am
@@ -41,6 +41,7 @@ content_files = \
 	dbus-org.freedesktop.NetworkManager.Device.Generic.xml \
 	dbus-org.freedesktop.NetworkManager.Device.IPTunnel.xml \
 	dbus-org.freedesktop.NetworkManager.Device.Infiniband.xml \
+	dbus-org.freedesktop.NetworkManager.Device.Loopback.xml \
 	dbus-org.freedesktop.NetworkManager.Device.Macsec.xml \
 	dbus-org.freedesktop.NetworkManager.Device.Macvlan.xml \
 	dbus-org.freedesktop.NetworkManager.Device.Modem.xml \

docs/api/network-manager-docs.xml
@@ -193,6 +193,7 @@
       <xi:include href="dbus-org.freedesktop.NetworkManager.Device.Generic.xml"/>
       <xi:include href="dbus-org.freedesktop.NetworkManager.Device.IPTunnel.xml"/>
       <xi:include href="dbus-org.freedesktop.NetworkManager.Device.Infiniband.xml"/>
+      <xi:include href="dbus-org.freedesktop.NetworkManager.Device.Loopback.xml"/>
       <xi:include href="dbus-org.freedesktop.NetworkManager.Device.Lowpan.xml"/>
       <xi:include href="dbus-org.freedesktop.NetworkManager.Device.Macsec.xml"/>
       <xi:include href="dbus-org.freedesktop.NetworkManager.Device.Macvlan.xml"/>

docs/libnm/libnm-docs.xml
@@ -332,6 +332,7 @@ print ("NetworkManager version " + client.get_version())]]></programlisting></in
     <xi:include href="xml/nm-setting-ip-tunnel.xml"/>
     <xi:include href="xml/nm-setting-ip4-config.xml"/>
     <xi:include href="xml/nm-setting-ip6-config.xml"/>
+    <xi:include href="xml/nm-setting-loopback.xml"/>
     <xi:include href="xml/nm-setting-macsec.xml"/>
     <xi:include href="xml/nm-setting-macvlan.xml"/>
     <xi:include href="xml/nm-setting-match.xml"/>
@@ -381,6 +382,7 @@ print ("NetworkManager version " + client.get_version())]]></programlisting></in
     <xi:include href="xml/nm-device-generic.xml"/>
     <xi:include href="xml/nm-device-infiniband.xml"/>
     <xi:include href="xml/nm-device-ip-tunnel.xml"/>
+    <xi:include href="xml/nm-device-loopback.xml"/>
     <xi:include href="xml/nm-device-macsec.xml"/>
     <xi:include href="xml/nm-device-macvlan.xml"/>
     <xi:include href="xml/nm-device-modem.xml"/>

introspection/meson.build
@@ -17,6 +17,7 @@ ifaces = [
   'org.freedesktop.NetworkManager.Device.Generic',
   'org.freedesktop.NetworkManager.Device.IPTunnel',
   'org.freedesktop.NetworkManager.Device.Infiniband',
+  'org.freedesktop.NetworkManager.Device.Loopback',
   'org.freedesktop.NetworkManager.Device.Lowpan',
   'org.freedesktop.NetworkManager.Device.Macsec',
   'org.freedesktop.NetworkManager.Device.Macvlan',

introspection/org.freedesktop.NetworkManager.Device.Loopback.xml
@@ -1,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<node name="/">
+  <!--
+      org.freedesktop.NetworkManager.Device.Loopback:
+      @short_description: Loopback Device.
+  -->
+  <interface name="org.freedesktop.NetworkManager.Device.Loopback"/>
+</node>

man/NetworkManager.conf.xml
@@ -1011,6 +1011,10 @@ ipv6.ip6-privacy=0
           </para></listitem>
         </varlistentry>
         <varlistentry>
+          <term><varname>loopback.mtu</varname></term>
+          <listitem><para>If configured explicitly to 0, the MTU is not reconfigured during device activation unless it is required due to IPv6 constraints. If left unspecified, a DHCP/IPv6 SLAAC provided value is used or the MTU is left unspecified on activation.</para></listitem>
+        </varlistentry>
+        <varlistentry>
           <term><varname>sriov.autoprobe-drivers</varname></term>
           <listitem><para>If left unspecified, drivers are autoprobed when the SR-IOV VF gets created.</para></listitem>
         </varlistentry>

commit 2eca11bcbab14703549d5f363a1aef5ea9a89bf1

loopback: reject setting "slave-type"/"master" for "loopback" profiles
A loopback interface cannot be attached to a controller interface (in kernel).

Also, we have special handling for the loopback address 127.0.0.1. It's
not clear how that should behave when the loopback device would be
attached to another interface.

Just reject such configuration as invalid.

Fixes: e8618f03d7d8 ('support loopback interface')

src/libnm-core-impl/nm-setting-loopback.c
@@ -70,9 +70,10 @@ static gboolean
 verify(NMSetting *setting, NMConnection *connection, GError **error)
 {
     if (connection) {
-        NMSettingIPConfig *s_ip4;
-        NMSettingIPConfig *s_ip6;
-        const char        *method;
+        NMSettingIPConfig   *s_ip4;
+        NMSettingIPConfig   *s_ip6;
+        NMSettingConnection *s_con;
+        const char          *method;
 
         if ((s_ip4 = nm_connection_get_setting_ip4_config(connection))) {
             if ((method = nm_setting_ip_config_get_method(s_ip4))
@@ -119,6 +120,23 @@ verify(NMSetting *setting, NMConnection *connection, GError **error)
                                "%s.%s: ",
                                NM_SETTING_IP6_CONFIG_SETTING_NAME,
                                NM_SETTING_IP_CONFIG_METHOD);
+                return FALSE;
+            }
+        }
+
+        if ((s_con = nm_connection_get_setting_connection(connection))) {
+            if (nm_setting_connection_get_slave_type(s_con)
+                || nm_setting_connection_get_master(s_con)) {
+                g_set_error(error,
+                            NM_CONNECTION_ERROR,
+                            NM_CONNECTION_ERROR_INVALID_PROPERTY,
+                            _("a loopback profile cannot be a port"));
+                g_prefix_error(error,
+                               "%s.%s: ",
+                               NM_SETTING_CONNECTION_SETTING_NAME,
+                               nm_setting_connection_get_slave_type(s_con)
+                                   ? NM_SETTING_CONNECTION_SLAVE_TYPE
+                                   : NM_SETTING_CONNECTION_MASTER);
                 return FALSE;
             }
         }