NetworkManager/NetworkManager

commit 3ce3ed4c92fb7d21da8f8c2f77b093fa71130455

examples: improve finding last checkpoint in "checkpoint.py"
This is a python example. We should do nice things, like
using max() for finding the maximum, instead of sorting.

examples/python/gi/checkpoint.py
@@ -71,11 +71,11 @@ def find_checkpoint(nmc, path):
 
 
 def find_checkpoint_last(nmc):
-    l = [c.get_path() for c in nmc.get_checkpoints()]
-    if not l:
-        return None
-    l.sort(key=checkpoint_path_to_num)
-    return l[-1]
+    return max(
+        nmc.get_checkpoints(),
+        key=lambda c: checkpoint_path_to_num(c.get_path()),
+        default=None,
+    )
 
 
 def validate_path(path, nmc):

commit 135bbcc0e4f587770ef059ef48cb5501ab72e318

examples: fix handling "--last" argument in "examples/python/gi/checkpoint.py"
Fixes: 3ce3ed4c92fb ('examples: improve finding last checkpoint in "checkpoint.py"')

examples/python/gi/checkpoint.py
@@ -80,10 +80,10 @@ def find_checkpoint_last(nmc):
 
 def validate_path(path, nmc):
     if path == "--last":
-        path = find_checkpoint_last(nmc)
-        if path is None:
+        c = find_checkpoint_last(nmc)
+        if c is None:
             sys.exit("Has no checkpoint")
-        return path
+        return c.get_path()
 
     try:
         num = int(path)