NetworkManager/NetworkManager

commit 8132045d5feb6bb855079b8476b119bfa8fbdab2

nmcli: fix typos in nmcli output
Spotted by lintian:
  I: network-manager: spelling-error-in-binary writting writing [usr/bin/nmcli]
  I: network-manager: spelling-error-in-binary wihout without [usr/lib/x86_64-linux-gnu/NetworkManager/1.40.12/libnm-device-plugin-wifi.so]

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1515

src/nmcli/connections.c
@@ -170,7 +170,7 @@ print_connection_done(GObject *source_object, GAsyncResult *res, gpointer user_d
     written = g_output_stream_write_finish(stream, res, &error);
     if (written == -1) {
         g_string_printf(nmc->return_text,
-                        _("Error: Error writting connection: %s"),
+                        _("Error: Error writing connection: %s"),
                         error->message);
         nmc->return_value = NMC_RESULT_ERROR_UNKNOWN;
         nmc->should_wait--;
@@ -230,7 +230,7 @@ nmc_print_connection_and_quit(NmCli *nmc, NMConnection *connection)
     return;
 
 error:
-    g_string_printf(nmc->return_text, _("Error: Error writting connection: %s"), error->message);
+    g_string_printf(nmc->return_text, _("Error: Error writing connection: %s"), error->message);
     nmc->return_value = NMC_RESULT_ERROR_UNKNOWN;
     nmc->should_wait--;
     quit();

src/core/devices/wifi/nm-wifi-utils.c
@@ -1004,7 +1004,7 @@ psk_setting_to_iwd_config(GKeyFile *file, NMSettingWirelessSecurity *s_wsec, GEr
         if (NM_FLAGS_ANY(psk_flags, SECRETS_DONT_STORE_FLAGS)) {
             nm_log_info(
                 LOGD_WIFI,
-                "IWD network config is being created wihout the PSK but IWD will save the PSK on "
+                "IWD network config is being created without the PSK but IWD will save the PSK on "
                 "successful activation not honoring the psk-flags property");
         }
         return TRUE;

commit 108ab677e4121c97bb641068f656502cbcd33dd3

nmcli/style: fix clang-format style
Fixes: 8132045d5feb ('nmcli: fix typos in nmcli output')

src/nmcli/connections.c
@@ -169,9 +169,7 @@ print_connection_done(GObject *source_object, GAsyncResult *res, gpointer user_d
 
     written = g_output_stream_write_finish(stream, res, &error);
     if (written == -1) {
-        g_string_printf(nmc->return_text,
-                        _("Error: Error writing connection: %s"),
-                        error->message);
+        g_string_printf(nmc->return_text, _("Error: Error writing connection: %s"), error->message);
         nmc->return_value = NMC_RESULT_ERROR_UNKNOWN;
         nmc->should_wait--;
         quit();