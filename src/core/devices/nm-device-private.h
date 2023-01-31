20f791d8fe30dc6906b024c760c7b45954fa8c94
Thomas Haller <thaller@redhat.com>
2023-01-31 12:15:50 +0100

    core: expose accessors to NMManager in NMSettings, NMSettingsConnection, NMDevice
    We should avoid using the NM_MANAGER_GET singleton. Everybody already
    has a manager instance. Expose it and allow to use it.

--- src/core/devices/nm-device-private.h
+++ src/core/devices/nm-device-private.h
@@ -29,10 +29,6 @@ enum NMActStageReturn {
 
 #define NM_DEVICE_CAP_INTERNAL_MASK 0xc0000000
 
-NMSettings *nm_device_get_settings(NMDevice *self);
-
-NMManager *nm_device_get_manager(NMDevice *self);
-
 gboolean nm_device_set_ip_ifindex(NMDevice *self, int ifindex);
 
 gboolean nm_device_set_ip_iface(NMDevice *self, const char *iface);
