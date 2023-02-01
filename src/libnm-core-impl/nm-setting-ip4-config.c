4ccca2b5bd8f7b350a1d354b6d2be7f2e247eac3
Thomas Haller <thaller@redhat.com>
2023-02-01 08:58:15 +0100

    platform,core: better handle onlink flag for ECMP routes
    The onlink flag is part of each next hop.
    
    When NetworkManager configures ECMP routes, we won't support that. All
    next hops of an ECMP route must share the same onlink flag. That is fine
    and fixed by this commit.
    
    What is not fine, is that we don't track the rtnh_flags flags in
    NMPlatformIP4RtNextHop, and consequently our nmp_object_id_cmp() is
    wrong.
    
    Fixes: 5b5ce4268211 ('nm-netns: track ECMP routes')
    (cherry picked from commit 6ed966258c0acc5bcf0c1dfa7d0cbcdb65b84b56)

--- src/libnm-core-impl/nm-setting-ip4-config.c
+++ src/libnm-core-impl/nm-setting-ip4-config.c
@@ -1143,11 +1143,15 @@ nm_setting_ip4_config_class_init(NMSettingIP4ConfigClass *klass)
      *          The default is <literal>unicast</literal>.</para>
      *      </listitem>
      *      <listitem>
-     *        <para><literal>"weight"</literal> - an unsigned 32 bit integer ranging from 0 to 256. This indicates
-     *          that the IPv4 route is a ECMP route and is only allowed for routes that specify a next-hop.
-     *          NetworkManager will automatically merge compatible routes into ECMP multi-hop routes.
-     *          Setting to zero or omitting the attribute configures single hop routes that won't get
-     *          merged. If the route finds no merge partner, it is configured as single hop route.</para>
+     *        <para><literal>"weight"</literal> - an unsigned 32 bit integer
+     *        ranging from 0 to 256. A non-zero weight indicates that the IPv4
+     *        route is an ECMP IPv4 route.  NetworkManager will automatically
+     *        merge compatible ECMP routes into multi-hop routes.  Setting to
+     *        zero or omitting the attribute configures single hop routes that
+     *        won't get merged. If the route finds no merge partner, it is
+     *        configured as single hop route.</para> <para>Note that in
+     *        NetworkManager, currently all nexthops of a ECMP route must share
+     *        the same "onlink" flag in order to be mergable.</para>
      *      </listitem>
      *      <listitem>
      *        <para><literal>"window"</literal> - an unsigned 32 bit integer.</para>
