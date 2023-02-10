NetworkManager/NetworkManager

commit 820f6f3a4a42702c7463e0cb4f7ef51b6c88dde5

contrib/rpm: sync obsoletes_{initscripts_updown,ifcfg_rh} version
Initially, when we obsoleted {initscripts_updown,ifcfg_rh}, the package
versions that we build from this upstream spec file differed from what
we put into Fedora/RHEL.

By now, this is long gone, and for upstream package builds we don't care
to accurately track the version, when using upstream/copr builds. Just
sync the version to what they are in Fedora/RHEL.

contrib/fedora/rpm/NetworkManager.spec
@@ -24,8 +24,8 @@
 
 %global obsoletes_device_plugins     1:0.9.9.95-1
 %global obsoletes_ppp_plugin         1:1.5.3
-%global obsoletes_initscripts_updown 1:1.35.4
-%global obsoletes_ifcfg_rh           1:1.37.1
+%global obsoletes_initscripts_updown 1:1.36.0-0.6
+%global obsoletes_ifcfg_rh           1:1.36.2
 
 %global nmlibdir %{_prefix}/lib/%{name}
 %global nmplugindir %{_libdir}/%{name}/%{version}-%{release}

commit 06dc84a563af95d87a5fa3634ec98a9e98f9635d

contrib/rpm: fix "--enable-ppp" flag to build without ppp
[thaller@redhat.com: I introduced this bug when taking the original patch].

Fixes: 820f6f3a4a42 ('contrib/rpm: sync obsoletes_{initscripts_updown,ifcfg_rh} version')

contrib/fedora/rpm/NetworkManager.spec
@@ -878,7 +878,7 @@ autoreconf --install --force
 	--with-pppd="%{_sbindir}/pppd" \
 	--with-pppd-plugin-dir="%{_libdir}/pppd/%{ppp_version}" \
 %else
-	--enable-ppp=yes \
+	--enable-ppp=no \
 %endif
 %if %{with firewalld_zone}
 	--enable-firewalld-zone=yes \