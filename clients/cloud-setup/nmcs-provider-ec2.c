5009cf5d54d41329822f32a43ca4eb4881b2743f
Lubomir Rintel <lkundrak@v3.sk>
2023-02-27 00:09:40 +0100

    cloud-setup: rename get/Get identifiers to req and Req
    We're going to extend those to issue methods other than GET.
    Also, "request" would've been too long, "req" looks nicer.
    
    Conflicts: code formatting, missing trivial commit in,
    provider-azure and gcp, missing 494819bbbf ("cloud-setup: move common
    code for get_config() to base class and improve cancellation")
    
    (cherry picked from commit 85ce088616aae3d61c9bc51eb395d1f59ed9a503)
    (cherry picked from commit 6e8cfbae32e22ba75693146039b53b516a4147e1)
    (cherry picked from commit 20cd11ee4974bb1690d523d5700492ad9c618688)
    (cherry picked from commit 9ce530fa7a5859d228255d131065047460290bd9)
    (cherry picked from commit d6d161a31de5bbd4b32d2c05ed328e53e6712c07)
    (cherry picked from commit 977fc2c8c5d604a4052ff2edb798d979b0827ed7)
    (cherry picked from commit 89ee76409bf58cb24a06f3fe6fa8f1c86d93ff02)
    (cherry picked from commit e3d23c7ee08c39df87d8ae007d769e9e89d6d6c3)
    (cherry picked from commit 3a0f3ff46f702c00cb17c8e8cf4d86875b787ca5)

--- clients/cloud-setup/nmcs-provider-ec2.c
+++ clients/cloud-setup/nmcs-provider-ec2.c
@@ -92,7 +92,7 @@ _detect_get_meta_data_done_cb (GObject *source,
 	gs_free_error GError *error = NULL;
 	gboolean success;
 
-	success = nm_http_client_poll_get_finish (NM_HTTP_CLIENT (source),
+	success = nm_http_client_poll_req_finish (NM_HTTP_CLIENT (source),
 	                                          result,
 	                                          NULL,
 	                                          NULL,
@@ -132,7 +132,7 @@ detect (NMCSProvider *provider,
 
 	http_client = nmcs_provider_get_http_client (provider);
 
-	nm_http_client_poll_get (http_client,
+	nm_http_client_poll_req (http_client,
 	                         (uri = _ec2_uri_concat ("latest/meta-data/")),
 	                         HTTP_TIMEOUT_MS,
 	                         256*1024,
@@ -200,7 +200,7 @@ _get_config_fetch_done_cb (NMHttpClient *http_client,
 
 	nm_utils_user_data_unpack (user_data, &iface_data, &hwaddr);
 
-	success = nm_http_client_poll_get_finish (http_client,
+	success = nm_http_client_poll_req_finish (http_client,
 	                                          result,
 	                                          NULL,
 	                                          &response_data,
@@ -316,7 +316,7 @@ _get_config_metadata_ready_cb (GObject *source,
 
 	nm_g_slice_free (metadata_data);
 
-	nm_http_client_poll_get_finish (NM_HTTP_CLIENT (source),
+	nm_http_client_poll_req_finish (NM_HTTP_CLIENT (source),
 	                                result,
 	                                NULL,
 	                                NULL,
@@ -387,7 +387,7 @@ _get_config_metadata_ready_cb (GObject *source,
 		_LOGD ("get-config: start fetching meta data for #%"G_GSSIZE_FORMAT", %s (%s)", config_iface_data->iface_idx, hwaddr, v_mac_data->path);
 
 		iface_data->n_pending++;
-		nm_http_client_poll_get (http_client,
+		nm_http_client_poll_req (http_client,
 		                         (uri1 = _ec2_uri_interfaces (v_mac_data->path,
 		                                                        NM_STR_HAS_SUFFIX (v_mac_data->path, "/")
 		                                                      ? ""
@@ -405,7 +405,7 @@ _get_config_metadata_ready_cb (GObject *source,
 		                         nm_utils_user_data_pack (iface_data, hwaddr));
 
 		iface_data->n_pending++;
-		nm_http_client_poll_get (http_client,
+		nm_http_client_poll_req (http_client,
 		                         (uri2 = _ec2_uri_interfaces (v_mac_data->path,
 		                                                        NM_STR_HAS_SUFFIX (v_mac_data->path, "/")
 		                                                      ? ""
@@ -526,7 +526,7 @@ get_config (NMCSProvider *provider,
 	 * MAC addresses, then we poll until we see them. They might not yet be
 	 * around from the start...
 	 */
-	nm_http_client_poll_get (nmcs_provider_get_http_client (provider),
+	nm_http_client_poll_req (nmcs_provider_get_http_client (provider),
 	                         (uri = _ec2_uri_interfaces ()),
 	                         HTTP_TIMEOUT_MS,
 	                         256 * 1024,
