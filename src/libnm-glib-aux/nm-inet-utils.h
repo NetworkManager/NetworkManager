NetworkManager/NetworkManager

commit 58e2ba0535359e2c0173215a1dc2f1d574061c56

glib-aux: drop ethernet fields from NMIPAddr union
It's not really used anyway.

The idea was, that the ethernet MAC address would fit from the size, and
we might at a few places use that for convenience. But it's more
confusing. Also, because there is already NMEtherAddr and `struct
ether_addr`.

src/libnm-glib-aux/nm-inet-utils.h
@@ -9,14 +9,7 @@ typedef struct _NMIPAddr {
         in_addr_t       addr4;
         struct in_addr  addr4_struct;
         struct in6_addr addr6;
-
-        /* NMIPAddr is really a union for IP addresses.
-         * However, as ethernet addresses fit in here nicely, use
-         * it also for an ethernet MAC address. */
-        guint8      ether_addr_octet[6 /*ETH_ALEN*/];
-        NMEtherAddr ether_addr;
-
-        guint8 array[sizeof(struct in6_addr)];
+        guint8          array[sizeof(struct in6_addr)];
     };
 } NMIPAddr;
 

commit d960ef823d49776bbb00f17e3f29b4ab790aaf61

glib-aux: ensure alignment for nm_ether_addr_zero
In practice, this does not cause an issue, because NMEtherAddr quite
quite obviously only contains uint8 values and has alignment 1.

It's still ugly to case nm_ip_addr_zero to NMEtherAddr if they are
entirely unrelated types.

Fixes: 58e2ba053535 ('glib-aux: drop ethernet fields from NMIPAddr union')

src/libnm-glib-aux/nm-inet-utils.h
@@ -8,6 +8,11 @@ typedef union _NMIPAddr {
     in_addr_t       addr4;
     struct in_addr  addr4_struct;
     struct in6_addr addr6;
+
+    /* This union field only exists, so that it's guaranteed that NMIPAddr has
+     * a suitable alignment. We use that with nm_ether_addr_zero macro, that
+     * aliases nm_ip_addr_zero. */
+    NMEtherAddr _ether_addr;
 } NMIPAddr;
 
 #define NM_IP_ADDR_INIT   \

src/libnm-glib-aux/nm-shared-utils.h
@@ -206,7 +206,7 @@ extern const union _NMIPAddr nm_ip_addr_zero;
 
 /* Let's reuse nm_ip_addr_zero also for nm_ether_addr_zero. It's a union that
  * also contains a NMEtherAddr field. */
-#define nm_ether_addr_zero (*((const NMEtherAddr *) ((gconstpointer) &nm_ip_addr_zero)))
+#define nm_ether_addr_zero (*((const NMEtherAddr *) &nm_ip_addr_zero))
 
 static inline int
 nm_ether_addr_cmp(const NMEtherAddr *a, const NMEtherAddr *b)