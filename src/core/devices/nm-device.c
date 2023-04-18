cab80c5129b293a9459d72bd4339f56a62b85eba
Beniamino Galvani <bgalvani@redhat.com>
2023-04-18 09:30:09 +0200

    device: emit dhcp-change dispatcher event also after a lease renewal
    Fixes: e1648d0665a0 ('core: commit l3cd asynchronously on DHCP bound event')
    Co-authored-by: Thomas Haller <thaller@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=2179537
    https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1609

--- src/core/devices/nm-device.c
+++ src/core/devices/nm-device.c
@@ -10500,14 +10500,14 @@ _dev_ipdhcpx_notify(NMDhcpClient *client, const NMDhcpClientNotifyData *notify_d
 
         if (notify_data->lease_update.accepted) {
             nm_manager_write_device_state(priv->manager, self, NULL);
+            nm_dispatcher_call_device(NM_DISPATCHER_ACTION_DHCP_CHANGE_X(IS_IPv4),
+                                      self,
+                                      NULL,
+                                      NULL,
+                                      NULL,
+                                      NULL);
             if (priv->ipdhcp_data_x[IS_IPv4].state != NM_DEVICE_IP_STATE_READY) {
                 _dev_ipdhcpx_set_state(self, addr_family, NM_DEVICE_IP_STATE_READY);
-                nm_dispatcher_call_device(NM_DISPATCHER_ACTION_DHCP_CHANGE_X(IS_IPv4),
-                                          self,
-                                          NULL,
-                                          NULL,
-                                          NULL,
-                                          NULL);
                 _dev_ip_state_check_async(self, addr_family);
             }
         }
