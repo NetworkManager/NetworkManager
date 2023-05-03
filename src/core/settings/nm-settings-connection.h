86b4953fad11a26700ddf24c416df932be867ad1
Thomas Haller <thaller@redhat.com>
2023-05-03 12:15:47 +0200

    core: add nm_settings_connection_get_setting() helper
    For efficiently and conveniently lookup an NMSetting from the
    NMConnection inside the NMSettingsConnection.
    
    Note that this uses the NMMetaSettingType as lookup key. That is a novel
    approach, compared to lookup by name (nm_connection_get_setting_by_name())
    or GType (nm_connection_get_setting()).
    
    Using the NMMetaSettingType enum is however faster, because it does not
    require resolving the name/GType first. This is perfecly fine internal API,
    we should use it.
    
    (cherry picked from commit 429cf416fd12f14d3a5639d7890f0d334c5e9328)

--- src/core/settings/nm-settings-connection.h
+++ src/core/settings/nm-settings-connection.h
@@ -7,6 +7,8 @@
 #ifndef __NETWORKMANAGER_SETTINGS_CONNECTION_H__
 #define __NETWORKMANAGER_SETTINGS_CONNECTION_H__
 
+#include "libnm-core-intern/nm-meta-setting-base.h"
+
 #include "nm-dbus-object.h"
 #include "nm-connection.h"
 
@@ -222,6 +224,8 @@ NMSettings *nm_settings_connection_get_settings(NMSettingsConnection *self);
 NMManager *nm_settings_connection_get_manager(NMSettingsConnection *self);
 
 NMConnection *nm_settings_connection_get_connection(NMSettingsConnection *self);
+gpointer      nm_settings_connection_get_setting(NMSettingsConnection *self,
+                                                 NMMetaSettingType     meta_type);
 
 void _nm_settings_connection_set_connection(NMSettingsConnection            *self,
                                             NMConnection                    *new_connection,
