526f701bcc1588ea14fcf6d5ac87db4cbe8370dc
Thomas Haller <thaller@redhat.com>
2023-05-22 13:03:43 +0200

    platform/tests: check errno on failure to umount() in test_netns_bind_to_path()
    The umount() call failed in a test. Rework the assertion, so
    we might see the errno of the problem.
    
    (cherry picked from commit 1a0fa8539737c784f17a5639e67a87a8636aa23b)
    (cherry picked from commit c66ae9341558e2e6cfa6a0beda3e0b0b0faf5af6)
    (cherry picked from commit edf23f434e36a32a93eee4e6c497336ff1b2c694)
    (cherry picked from commit 01134c6fe89035e8c24d8ebbe0b09a7040b38ee7)

--- src/core/platform/tests/test-link.c
+++ src/core/platform/tests/test-link.c
@@ -3209,7 +3209,9 @@ test_netns_bind_to_path(gpointer fixture, gconstpointer test_data)
     nm_auto_pop_netns NMPNetns *netns_pop  = NULL;
     NMPlatform                 *platforms[3];
     NMPNetns                   *netns;
+    int                         errsv;
     int                         i;
+    int                         r;
 
     if (_test_netns_check_skip())
         return;
@@ -3252,7 +3254,12 @@ test_netns_bind_to_path(gpointer fixture, gconstpointer test_data)
                     !=,
                     0);
 
-    g_assert_cmpint(umount(P_VAR_RUN), ==, 0);
+    r = umount(P_VAR_RUN);
+    if (r != 0) {
+        errsv = errno;
+        g_assert_cmpint(errsv, ==, 0);
+        g_assert_cmpint(r, ==, 0);
+    }
 }
 
 /*****************************************************************************/
