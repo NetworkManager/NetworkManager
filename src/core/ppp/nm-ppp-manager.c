84e21d8bbcbe14b3325fde51a91d0e16c803612f
Thomas Haller <thaller@redhat.com>
2023-06-07 08:51:54 +0200

    ppp: fix plugin name for "rp-pppoe.so" with ppp 2.5
    Between ppp 2.4.8 and 2.4.9, "rp-pppoe.so" was renamed to "pppoe.so" (and a
    symlink created). Between 2.4.9 and 2.5.0, the symlink was dropped.
    
    See-also: https://github.com/ppp-project/ppp/commit/b2c36e6c0e1655aea9b1b0a03a8160f42a26c884
    
    I guess, NetworkManager always meant to use ppp's "(rp-)pppoe.so"
    plugin, and never the library from the rp-pppoe project.
    If a user actually wants to use the plugin from rp-pppoe project, then
    this is going to break.
    
    https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/1312
    
    Fixes: afe80171b24d ('ppp: move ppp code to "nm-pppd-compat.c"')

--- src/core/ppp/nm-ppp-manager.c
+++ src/core/ppp/nm-ppp-manager.c
@@ -843,7 +843,7 @@ create_pppd_cmd_line(NMPPPManager   *self,
         const char *pppoe_service;
 
         nm_strv_ptrarray_add_string_dup(cmd, "plugin");
-        nm_strv_ptrarray_add_string_dup(cmd, "rp-pppoe.so");
+        nm_strv_ptrarray_add_string_dup(cmd, NM_PPPOE_PLUGIN_NAME);
 
         nm_strv_ptrarray_add_string_concat(cmd, "nic-", priv->parent_iface);
 
@@ -872,7 +872,7 @@ create_pppd_cmd_line(NMPPPManager   *self,
 
         } else if (!strcmp(protocol, NM_SETTING_ADSL_PROTOCOL_PPPOE)) {
             nm_strv_ptrarray_add_string_dup(cmd, "plugin");
-            nm_strv_ptrarray_add_string_dup(cmd, "rp-pppoe.so");
+            nm_strv_ptrarray_add_string_dup(cmd, NM_PPPOE_PLUGIN_NAME);
             nm_strv_ptrarray_add_string_dup(cmd, priv->parent_iface);
         }
 
