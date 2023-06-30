CodeLinaro-mirror/le_NetworkManager_NetworkManager

core: replace multiple arguments of nm_match_spec_device() with struct
Struct allow named arguments, which seems easier to maintain instead of
a function with many arguments. Also, adding a new parameter does not
require changes to most of the callers.

The real advantage of this is that we encode all the search parameters
in one argument. And we can add that argument to
_match_section_infos_lookup(), alongside lookup by NMDevice or
NMPlatformLink.

src/core/devices/nm-device.c
@@ -17294,14 +17294,15 @@ nm_device_spec_match_list_full(NMDevice *self, const GSList *specs, int no_match
         !nm_device_get_unmanaged_flags(self, NM_UNMANAGED_PLATFORM_INIT),
         &is_fake);
 
-    m = nm_match_spec_device(specs,
-                             nm_device_get_iface(self),
-                             nm_device_get_type_description(self),
-                             nm_device_get_driver(self),
-                             nm_device_get_driver_version(self),
-                             is_fake ? NULL : hw_address,
-                             klass->get_s390_subchannels ? klass->get_s390_subchannels(self) : NULL,
-                             nm_dhcp_manager_get_config(nm_dhcp_manager_get()));
+    m = nm_match_spec_device_v(
+        specs,
+        .interface_name   = nm_device_get_iface(self),
+        .device_type      = nm_device_get_type_description(self),
+        .driver           = nm_device_get_driver(self),
+        .driver_version   = nm_device_get_driver_version(self),
+        .hwaddr           = is_fake ? NULL : hw_address,
+        .s390_subchannels = klass->get_s390_subchannels ? klass->get_s390_subchannels(self) : NULL,
+        .dhcp_plugin      = nm_dhcp_manager_get_config(nm_dhcp_manager_get()), );
 
     return nm_match_spec_match_type_to_bool(m, no_match_value);
 }

NetworkManager/NetworkManager

(cherry picked from commit c47d6b17d580fa1466c5aa89e1c7c4717a676501)

src/core/devices/nm-device.c
@@ -17050,14 +17050,18 @@ nm_device_spec_match_list_full(NMDevice *self, const GSList *specs, int no_match
         !nm_device_get_unmanaged_flags(self, NM_UNMANAGED_PLATFORM_INIT),
         &is_fake);
 
-    m = nm_match_spec_device(specs,
-                             nm_device_get_iface(self),
-                             nm_device_get_type_description(self),
-                             nm_device_get_driver(self),
-                             nm_device_get_driver_version(self),
-                             is_fake ? NULL : hw_address,
-                             klass->get_s390_subchannels ? klass->get_s390_subchannels(self) : NULL,
-                             nm_dhcp_manager_get_config(nm_dhcp_manager_get()));
+    m = nm_match_spec_device(
+        specs,
+        &((const NMMatchSpecDeviceData){
+            .interface_name = nm_device_get_iface(self),
+            .device_type    = nm_device_get_type_description(self),
+            .driver         = nm_device_get_driver(self),
+            .driver_version = nm_device_get_driver_version(self),
+            .hwaddr         = is_fake ? NULL : hw_address,
+            .s390_subchannels =
+                klass->get_s390_subchannels ? klass->get_s390_subchannels(self) : NULL,
+            .dhcp_plugin = nm_dhcp_manager_get_config(nm_dhcp_manager_get()),
+        }));
 
     return nm_match_spec_match_type_to_bool(m, no_match_value);
 }