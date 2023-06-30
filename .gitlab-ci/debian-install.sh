NetworkManager/NetworkManager

commit d563b4e009d4d37a503d6930cbd8b9e623db96d4

gitlab-ci: fix setup of debian:9 container
Debian:9 (stretch) is archived. We need to patch the sources.list
for it to be usable.

Although it's end of life, we are still interested, whether we
are able to build with such old compiler. Fix the test.

.gitlab-ci.yml
@@ -59,9 +59,9 @@ variables:
   # ".default_tag".
   ALPINE_TAG:  'tag-d53edbf2ebdd'
   CENTOS_TAG:  'tag-c374af7add25'
-  DEBIAN_TAG:  'tag-2ded62f40aeb'
+  DEBIAN_TAG:  'tag-33f59276e980'
   FEDORA_TAG:  'tag-c374af7add25'
-  UBUNTU_TAG:  'tag-2ded62f40aeb'
+  UBUNTU_TAG:  'tag-33f59276e980'
 
   ALPINE_EXEC: 'bash .gitlab-ci/alpine-install.sh'
   CENTOS_EXEC: 'bash .gitlab-ci/fedora-install.sh'

.gitlab-ci/debian-install.sh
@@ -5,6 +5,15 @@ set -ex
 grep -q '^VERSION=.\(9 (stretch)\|18.04.[0-9]\+ LTS\)' /etc/os-release && IS_DEBIAN_9=1 || IS_DEBIAN_9=0
 
 if [ $IS_DEBIAN_9 = 1 ]; then
+    cat > /etc/apt/sources.list <<EOF
+deb http://archive.debian.org/debian/ stretch main non-free contrib
+deb-src http://archive.debian.org/debian/ stretch main non-free contrib
+deb http://archive.debian.org/debian-security/ stretch/updates main non-free contrib
+deb-src http://archive.debian.org/debian-security/ stretch/updates main non-free contrib
+EOF
+fi
+
+if [ $IS_DEBIAN_9 = 1 ]; then
     # pam is hosted on this release to the point chfn doesn't work.
     # It's okay on Ubuntu 16.04 and 20.04 though, so keep this version specific.
     #

commit ccb16013e0772b35fa41269541dce3839a6e116b

gitlab-ci: fix detection and handling of Ubuntu 18.04
Fixes: d563b4e009d4 ('gitlab-ci: fix setup of debian:9 container')

.gitlab-ci.yml
@@ -59,9 +59,9 @@ variables:
   # ".default_tag".
   ALPINE_TAG:  'tag-d53edbf2ebdd'
   CENTOS_TAG:  'tag-c374af7add25'
-  DEBIAN_TAG:  'tag-33f59276e980'
+  DEBIAN_TAG:  'tag-340ef1f25d8d'
   FEDORA_TAG:  'tag-c374af7add25'
-  UBUNTU_TAG:  'tag-33f59276e980'
+  UBUNTU_TAG:  'tag-340ef1f25d8d'
 
   ALPINE_EXEC: 'bash .gitlab-ci/alpine-install.sh'
   CENTOS_EXEC: 'bash .gitlab-ci/fedora-install.sh'

.gitlab-ci/debian-install.sh
@@ -2,7 +2,10 @@
 
 set -ex
 
-grep -q '^VERSION=.\(9 (stretch)\|18.04.[0-9]\+ LTS\)' /etc/os-release && IS_DEBIAN_9=1 || IS_DEBIAN_9=0
+IS_DEBIAN_9=0
+IS_UBUNTU_18_04=0
+grep -q '^VERSION=.\(9 (stretch)\)' /etc/os-release && IS_DEBIAN_9=1
+grep -q '^VERSION=.\(18.04.[0-9]\+ LTS\)' /etc/os-release && IS_UBUNTU_18_04=1
 
 if [ $IS_DEBIAN_9 = 1 ]; then
     cat > /etc/apt/sources.list <<EOF
@@ -13,7 +16,7 @@ deb-src http://archive.debian.org/debian-security/ stretch/updates main non-free
 EOF
 fi
 
-if [ $IS_DEBIAN_9 = 1 ]; then
+if [ $IS_DEBIAN_9 = 1 -o $IS_UBUNTU_18_04 = 1 ]; then
     # pam is hosted on this release to the point chfn doesn't work.
     # It's okay on Ubuntu 16.04 and 20.04 though, so keep this version specific.
     #