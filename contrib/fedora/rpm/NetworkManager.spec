NetworkManager/NetworkManager

commit 8c5aec7a1b32d0fca14575da734f5c942d89e86e

contrib/rpm: migrate to SPDX license
Fedora is moving to SPDX standard in naming a licenses.
See https://fedoraproject.org/wiki/Changes/SPDX_Licenses_Phase_2 .

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1668

contrib/fedora/rpm/NetworkManager.spec
@@ -194,7 +194,7 @@ Epoch: %{epoch_version}
 Version: %{rpm_version}
 Release: %{release_version}%{?snap}%{?dist}
 Group: System Environment/Base
-License: GPLv2+ and LGPLv2+
+License: GPL-2.0-or-later AND LGPL-2.1-or-later
 URL: https://networkmanager.dev/
 
 #Source: https://download.gnome.org/sources/NetworkManager/%{real_version_major}/%{name}-%{real_version}.tar.xz

commit ed44b86bcbf11fffb6a074570d68cb826d9953dd

contrib/rpm: libnm: migrate to SPDX license
The license identifier was updated for the main package, but not for
libnm which overrides it to LGPL 2.1 or later. Update it too.

Fixes: 8c5aec7a1b32 ('contrib/rpm: migrate to SPDX license')

contrib/fedora/rpm/NetworkManager.spec
@@ -488,7 +488,7 @@ This package contains NetworkManager support for PPP.
 Summary: Libraries for adding NetworkManager support to applications.
 Group: Development/Libraries
 Conflicts: NetworkManager-glib < 1:1.31.0
-License: LGPLv2+
+License: LGPL-2.1-or-later
 
 %description libnm
 This package contains the libraries that make it easier to use some
@@ -501,7 +501,7 @@ Group: Development/Libraries
 Requires: %{name}-libnm%{?_isa} = %{epoch}:%{version}-%{release}
 Requires: glib2-devel
 Requires: pkgconfig
-License: LGPLv2+
+License: LGPL-2.1-or-later
 
 %description libnm-devel
 This package contains the header and pkg-config files for development