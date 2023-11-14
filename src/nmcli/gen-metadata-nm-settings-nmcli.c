NetworkManager/NetworkManager

commit 925d4df801f7add9450c8c8dbb270df9015afb56

man nm-settings-nmcli: add "Valid values" field
Show a new field called "Valid values" in those properties that only
accept a limited set of values, like enums, ints with a valid range of
values, etc.

As there is some complex logic behind getting this information, this
logic has been put in nm-meta-setting-desc and nm-enum-utils so they can
be re-used, avoiding duplicity and errors. Some refactor has been done
in nm-meta-setting-desc in this direction, too.

src/nmcli/gen-metadata-nm-settings-nmcli.c
@@ -4,6 +4,7 @@
 
 #include "libnmc-setting/nm-meta-setting-desc.h"
 #include "libnmc-setting/nm-meta-setting-base.h"
+#include "libnm-glib-aux/nm-enum-utils.h"
 #include "nm-core-enum-types.h"
 #include <stdarg.h>
 #include <stdlib.h>
@@ -70,28 +71,12 @@ _property_get_gtype(const NMMetaPropertyInfo *prop_info)
 }
 
 static char *
-_get_enum_format(GType g_type)
+get_enum_format(GType g_type)
 {
     if (G_TYPE_IS_FLAGS(g_type))
         return g_strdup_printf("flags (%s)", g_type_name(g_type));
     else
         return g_strdup_printf("choice (%s)", g_type_name(g_type));
-}
-
-static char *
-get_enum_format(const NMMetaPropertyInfo *prop_info)
-{
-    GType                        gtype         = G_TYPE_INVALID;
-    const NMMetaPropertyTypData *prop_typ_data = prop_info->property_typ_data;
-
-    if (prop_typ_data && prop_typ_data->subtype.gobject_enum.get_gtype)
-        gtype = prop_typ_data->subtype.gobject_enum.get_gtype();
-    else
-        gtype = _property_get_gtype(prop_info);
-
-    prop_assert(gtype != G_TYPE_INVALID, prop_info, "unknown property's enum type");
-
-    return _get_enum_format(gtype);
 }
 
 static char *
@@ -185,11 +170,11 @@ get_property_format(const NMMetaPropertyInfo *prop_info)
     case NM_META_PROPERTY_TYPE_FORMAT_STRING:
         return g_strdup("string");
     case NM_META_PROPERTY_TYPE_FORMAT_ENUM:
-        return get_enum_format(prop_info);
+        return get_enum_format(nm_meta_property_enum_get_type(prop_info));
     case NM_META_PROPERTY_TYPE_FORMAT_SECRET_FLAGS:
-        return _get_enum_format(NM_TYPE_SETTING_SECRET_FLAGS);
+        return get_enum_format(NM_TYPE_SETTING_SECRET_FLAGS);
     case NM_META_PROPERTY_TYPE_FORMAT_DCB_FLAGS:
-        return _get_enum_format(NM_TYPE_SETTING_DCB_FLAGS);
+        return get_enum_format(NM_TYPE_SETTING_DCB_FLAGS);
     case NM_META_PROPERTY_TYPE_FORMAT_BOOL:
         return g_strdup("boolean");
     case NM_META_PROPERTY_TYPE_FORMAT_TERNARY:
@@ -224,6 +209,199 @@ get_property_format(const NMMetaPropertyInfo *prop_info)
     }
 }
 
+#define append_vals(to, ...)                          \
+    G_STMT_START                                      \
+    {                                                 \
+        size_t      i;                                \
+        const char *from[] = {__VA_ARGS__};           \
+        for (i = 0; i < NM_N_ELEMENTS(from); i++)     \
+            g_ptr_array_add((to), g_strdup(from[i])); \
+    }                                                 \
+    G_STMT_END
+
+static void
+append_connection_types(GPtrArray *valid_values)
+{
+    size_t i;
+    for (i = 0; i < _NM_META_SETTING_TYPE_NUM; i++) {
+        /* If the setting has a priority of a base-type, it's a valid value for connection.type */
+        NMSettingPriority pri =
+            _nm_setting_type_get_base_type_priority(nm_meta_setting_infos[i].get_setting_gtype());
+        if (pri != NM_SETTING_PRIORITY_INVALID)
+            g_ptr_array_add(valid_values, g_strdup(nm_meta_setting_infos[i].setting_name));
+    }
+}
+
+static void
+append_int_valid_range(const NMMetaPropertyInfo *prop_info, GPtrArray *valid_values)
+{
+    NMMetaSignUnsignInt64 min;
+    NMMetaSignUnsignInt64 max;
+
+    nm_meta_property_int_get_range(prop_info, &min, &max);
+    if (NM_IN_SET(_property_get_gtype(prop_info), G_TYPE_UINT, G_TYPE_UINT64))
+        g_ptr_array_add(valid_values, g_strdup_printf("%lu - %lu", min.u64, max.u64));
+    else
+        g_ptr_array_add(valid_values, g_strdup_printf("%ld - %ld", min.i64, max.i64));
+}
+
+static void
+_append_enum_valid_values(GType                       g_type,
+                          int                         min,
+                          int                         max,
+                          const NMUtilsEnumValueInfo *value_infos,
+                          GPtrArray                  *valid_values)
+{
+    size_t       i;
+    const char **alias;
+    GString     *names  = g_string_sized_new(64);
+    GArray      *values = _nm_utils_enum_get_values_full(g_type, min, max, value_infos);
+
+    for (i = 0; i < values->len; i++) {
+        NMUtilsEnumValueInfoFull *val = &g_array_index(values, NMUtilsEnumValueInfoFull, i);
+
+        g_string_assign(names, val->nick);
+
+        if (val->aliases) {
+            for (alias = val->aliases; *alias; alias++) {
+                g_string_append_c(names, '/');
+                g_string_append(names, *alias);
+            }
+        }
+
+        g_ptr_array_add(valid_values, g_strdup_printf("%s (%s)", names->str, val->value_str));
+    }
+
+    g_string_free(names, TRUE);
+    g_array_unref(values);
+}
+
+static void
+append_enum_valid_values(const NMMetaPropertyInfo *prop_info, GPtrArray *valid_values)
+{
+    const NMMetaPropertyTypData *prop_typ_data = prop_info->property_typ_data;
+    const NMUtilsEnumValueInfo  *value_infos;
+    GType                        gtype;
+    int                          min;
+    int                          max;
+
+    gtype = nm_meta_property_enum_get_type(prop_info);
+    nm_meta_property_enum_get_range(prop_info, &min, &max);
+    value_infos = prop_typ_data ? prop_typ_data->subtype.gobject_enum.value_infos : NULL;
+
+    _append_enum_valid_values(gtype, min, max, value_infos, valid_values);
+}
+
+static void
+append_ethtool_valid_values(const NMMetaPropertyInfo *prop_info, GPtrArray *valid_values)
+{
+    NMEthtoolID ethtool_id;
+
+    prop_assert(prop_info->property_typ_data, prop_info, "missing .property_typ_data");
+
+    ethtool_id = prop_info->property_typ_data->subtype.ethtool.ethtool_id;
+
+    if (nm_ethtool_id_is_coalesce(ethtool_id) || nm_ethtool_id_is_ring(ethtool_id))
+        g_ptr_array_add(valid_values, g_strdup_printf("0 - %u", G_MAXUINT32));
+    else if (nm_ethtool_id_is_feature(ethtool_id) || nm_ethtool_id_is_pause(ethtool_id))
+        append_vals(valid_values, "on", "off", "ignore");
+}
+
+static void
+append_dcb_valid_values(const NMMetaPropertyInfo *prop_info, GPtrArray *valid_values)
+{
+    guint max;
+    guint other;
+
+    prop_assert(prop_info->property_typ_data, prop_info, "missing .property_typ_data");
+
+    max   = prop_info->property_typ_data->subtype.dcb.max;
+    other = prop_info->property_typ_data->subtype.dcb.other;
+
+    if (max != 0)
+        g_ptr_array_add(valid_values, g_strdup_printf("0 - %u", max));
+    if (other != 0 && (!max || other > max))
+        g_ptr_array_add(valid_values, g_strdup_printf("%u", other));
+}
+
+static GPtrArray *
+get_property_valid_values(const NMMetaPropertyInfo *prop_info)
+{
+    const NMMetaPropertyType    *prop_type     = prop_info->property_type;
+    const NMMetaPropertyTypData *prop_typ_data = prop_info->property_typ_data;
+    NMMetaPropertyTypeFormat     fmt           = prop_type->doc_format;
+    GPtrArray                   *valid_values  = g_ptr_array_new_full(16, g_free);
+
+    /* connection.type is generated differently */
+    if (prop_info->setting_info->general->meta_type == NM_META_SETTING_TYPE_CONNECTION
+        && !nm_strcmp0(prop_info->property_name, "type")) {
+        append_connection_types(valid_values);
+        return valid_values;
+    }
+
+    /* If it's a list, get the format of the items, but only  if we can process it
+     * (prop_typ_data->subtype is of type multilist, so we can't process ints or enums) */
+    if (fmt == NM_META_PROPERTY_TYPE_FORMAT_MULTILIST && prop_typ_data) {
+        switch (prop_typ_data->list_items_doc_format) {
+        case NM_META_PROPERTY_TYPE_FORMAT_SECRET_FLAGS:
+        case NM_META_PROPERTY_TYPE_FORMAT_DCB_FLAGS:
+        case NM_META_PROPERTY_TYPE_FORMAT_BOOL:
+        case NM_META_PROPERTY_TYPE_FORMAT_DCB_BOOL:
+        case NM_META_PROPERTY_TYPE_FORMAT_TERNARY:
+        case NM_META_PROPERTY_TYPE_FORMAT_ETHTOOL:
+            fmt = prop_typ_data->list_items_doc_format;
+            break;
+        case NM_META_PROPERTY_TYPE_FORMAT_ENUM:
+            prop_warn(prop_info, "unknown enum type, can't get valid values");
+            break;
+        case NM_META_PROPERTY_TYPE_FORMAT_INT:
+            prop_warn(prop_info, "can't check for valid range in multilists");
+            break;
+        default:
+            /* Other types probably don't need to show "Valid values" */
+            break;
+        }
+    }
+
+    switch (fmt) {
+    case NM_META_PROPERTY_TYPE_FORMAT_INT:
+        append_int_valid_range(prop_info, valid_values);
+        break;
+    case NM_META_PROPERTY_TYPE_FORMAT_ENUM:
+        append_enum_valid_values(prop_info, valid_values);
+        break;
+    case NM_META_PROPERTY_TYPE_FORMAT_SECRET_FLAGS:
+        _append_enum_valid_values(NM_TYPE_SETTING_SECRET_FLAGS, 0, G_MAXUINT, NULL, valid_values);
+        break;
+    case NM_META_PROPERTY_TYPE_FORMAT_DCB_FLAGS:
+        _append_enum_valid_values(NM_TYPE_SETTING_DCB_FLAGS, 0, G_MAXUINT, NULL, valid_values);
+        break;
+    case NM_META_PROPERTY_TYPE_FORMAT_BOOL:
+    case NM_META_PROPERTY_TYPE_FORMAT_DCB_BOOL:
+        append_vals(valid_values, "true/yes/on", "false/no/off");
+        break;
+    case NM_META_PROPERTY_TYPE_FORMAT_TERNARY:
+        append_vals(valid_values, "true/yes/on", "false/no/off", "default/unknown");
+        break;
+    case NM_META_PROPERTY_TYPE_FORMAT_ETHTOOL:
+        append_ethtool_valid_values(prop_info, valid_values);
+        break;
+    case NM_META_PROPERTY_TYPE_FORMAT_DCB:
+        append_dcb_valid_values(prop_info, valid_values);
+        break;
+    default:
+        break;
+    }
+
+    if (prop_typ_data && prop_typ_data->values_static) {
+        const char *const *v;
+        for (v = prop_typ_data->values_static; *v; v++)
+            g_ptr_array_add(valid_values, g_strdup(*v));
+    }
+
+    return valid_values;
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -246,11 +424,13 @@ main(int argc, char *argv[])
         g_print(" >\n");
 
         for (i_property = 0; i_property < sett_info->properties_num; i_property++) {
-            const NMMetaPropertyInfo *prop_info = sett_info->properties[i_property];
-            gs_free char             *name      = NULL;
-            gs_free char             *alias     = NULL;
-            gs_free char             *descr     = NULL;
-            gs_free char             *fmt       = NULL;
+            const NMMetaPropertyInfo    *prop_info = sett_info->properties[i_property];
+            gs_free char                *name      = NULL;
+            gs_free char                *alias     = NULL;
+            gs_free char                *descr     = NULL;
+            gs_free char                *fmt       = NULL;
+            gs_unref_ptrarray GPtrArray *vals_arr  = NULL;
+            gs_free char                *vals_str  = NULL;
 
             g_print("%s<property", _indent_level(2 * INDENT));
             g_print(" name=%s", name = _xml_escape_attribute(prop_info->property_name));
@@ -280,6 +460,15 @@ main(int argc, char *argv[])
                 g_print("\n%sformat=%s",
                         _indent_level(2 * INDENT + 10),
                         _xml_escape_attribute(fmt));
+
+            vals_arr = get_property_valid_values(prop_info);
+            if (vals_arr->len) {
+                g_ptr_array_add(vals_arr, NULL);
+                vals_str = g_strjoinv(", ", (char **) vals_arr->pdata);
+                g_print("\n%svalues=%s",
+                        _indent_level(2 * INDENT + 10),
+                        _xml_escape_attribute(vals_str));
+            }
 
             g_print(" />\n");
         }

man/nm-settings-nmcli.xsl
@@ -176,6 +176,11 @@
             Format: <xsl:value-of select="@type"/>
           </para>
         </xsl:if>
+        <xsl:if test="@values">
+          <para>
+            Valid values: <xsl:value-of select="@values"/>
+          </para>
+        </xsl:if>
       </listitem>
     </varlistentry>
   </xsl:template>

src/libnm-glib-aux/nm-enum-utils.c
@@ -320,53 +320,117 @@ _nm_utils_enum_from_str_full(GType                       type,
 const char **
 _nm_utils_enum_get_values(GType type, int from, int to)
 {
-    GTypeClass *klass;
-    GPtrArray  *array;
-    int         i;
-    char        sbuf[64];
+    int        i;
+    GArray    *values_full = _nm_utils_enum_get_values_full(type, from, to, NULL);
+    GPtrArray *values      = g_ptr_array_sized_new(values_full->len + 1);
 
-    klass = g_type_class_ref(type);
-    array = g_ptr_array_new();
+    for (i = 0; i < values_full->len; i++) {
+        NMUtilsEnumValueInfoFull *v = &g_array_index(values_full, NMUtilsEnumValueInfoFull, i);
+        g_ptr_array_add(values, (gpointer) v->nick);
+    }
+
+    g_ptr_array_add(values, NULL);
+    g_array_unref(values_full);
+    return (const char **) g_ptr_array_free(values, FALSE);
+}
+
+static void
+_free_value_info_full(NMUtilsEnumValueInfoFull *value_info_full)
+{
+    g_free(value_info_full->aliases);
+}
+
+static void
+_init_value_info_full(NMUtilsEnumValueInfoFull *v, bool is_flag, const char *nick, int value)
+{
+    char        sbuf[64];
+    const char *value_str = is_flag ? g_intern_string(nm_sprintf_buf(sbuf, "0x%x", value))
+                                    : g_intern_string(nm_sprintf_buf(sbuf, "%d", value));
+
+    v->nick      = _enum_is_valid_enum_nick(nick) ? nick : value_str;
+    v->aliases   = NULL;
+    v->value_str = value_str;
+    v->value     = value;
+}
+
+/**
+ * _nm_utils_enum_get_values_full:
+ * @type: the enum or flags type
+ * @from: lowest value to return
+ * @to:   highest value to return
+ * @value_infos: (nullable): additional value aliases
+ * 
+ * Get the enum or flags values within the given range, putting together the
+ * value, name and aliases of each of them.
+ *
+ * If @value_infos is NULL, no memory will be allocated and deallocated for the
+ * aliases and #NMUtilsEnumValueInfoFull:aliases will be NULL in the returned
+ * data.
+ *
+ * The caller is responsible of releasing the container, but not the contained
+ * data. Only #NMUtilsEnumValueInfoFull:aliases can be stolen (and set to NULL),
+ * and then the caller becomes the responsible to release it.
+ * 
+ * Return: (transfer container): an array of #NMUtilsEnumValueInfoFull.
+ */
+GArray *
+_nm_utils_enum_get_values_full(GType                       type,
+                               int                         from,
+                               int                         to,
+                               const NMUtilsEnumValueInfo *value_infos)
+{
+    NMUtilsEnumValueInfoFull v;
+    GArray                  *array;
+    int                      i;
+
+    nm_auto_unref_gtypeclass GTypeClass *klass = g_type_class_ref(type);
+    g_return_val_if_fail(G_IS_ENUM_CLASS(klass) || G_IS_FLAGS_CLASS(klass), NULL);
+
+    _ASSERT_enum_values_info(type, value_infos);
+
+    array = g_array_new(FALSE, FALSE, sizeof(NMUtilsEnumValueInfoFull));
 
     if (G_IS_ENUM_CLASS(klass)) {
         GEnumClass *enum_class = G_ENUM_CLASS(klass);
-        GEnumValue *enum_value;
 
         for (i = 0; i < enum_class->n_values; i++) {
-            enum_value = &enum_class->values[i];
-            if (enum_value->value >= from && enum_value->value <= to) {
-                if (_enum_is_valid_enum_nick(enum_value->value_nick))
-                    g_ptr_array_add(array, (gpointer) enum_value->value_nick);
-                else
-                    g_ptr_array_add(
-                        array,
-                        (gpointer) g_intern_string(nm_sprintf_buf(sbuf, "%d", enum_value->value)));
-            }
-        }
-    } else if (G_IS_FLAGS_CLASS(klass)) {
-        GFlagsClass *flags_class = G_FLAGS_CLASS(klass);
-        GFlagsValue *flags_value;
+            GEnumValue *enum_val = &enum_class->values[i];
 
-        for (i = 0; i < flags_class->n_values; i++) {
-            flags_value = &flags_class->values[i];
-            if (flags_value->value >= (guint) from && flags_value->value <= (guint) to) {
-                if (_enum_is_valid_flags_nick(flags_value->value_nick))
-                    g_ptr_array_add(array, (gpointer) flags_value->value_nick);
-                else
-                    g_ptr_array_add(
-                        array,
-                        (gpointer) g_intern_string(
-                            nm_sprintf_buf(sbuf, "0x%x", (unsigned) flags_value->value)));
+            if (enum_val->value >= from && enum_val->value <= to) {
+                _init_value_info_full(&v, FALSE, enum_val->value_nick, enum_val->value);
+                g_array_append_val(array, v);
             }
         }
     } else {
-        g_type_class_unref(klass);
-        g_ptr_array_free(array, TRUE);
-        g_return_val_if_reached(NULL);
+        GFlagsClass *flags_class = G_FLAGS_CLASS(klass);
+
+        for (i = 0; i < flags_class->n_values; i++) {
+            GFlagsValue *flags_val = &flags_class->values[i];
+
+            if (flags_val->value >= (guint) from && flags_val->value <= (guint) to) {
+                _init_value_info_full(&v, TRUE, flags_val->value_nick, flags_val->value);
+                g_array_append_val(array, v);
+            }
+        }
     }
 
-    g_type_class_unref(klass);
-    g_ptr_array_add(array, NULL);
+    if (value_infos) {
+        g_array_set_clear_func(array, (GDestroyNotify) _free_value_info_full);
 
-    return (const char **) g_ptr_array_free(array, FALSE);
+        for (i = 0; i < array->len; i++) {
+            NMUtilsEnumValueInfoFull *vi_full = &g_array_index(array, NMUtilsEnumValueInfoFull, i);
+            GPtrArray                *aliases = g_ptr_array_new();
+
+            const NMUtilsEnumValueInfo *vi;
+            for (vi = value_infos; vi && vi->nick; vi++) {
+                if (vi->value == vi_full->value)
+                    g_ptr_array_add(aliases, (gpointer) vi->nick);
+            }
+
+            g_ptr_array_add(aliases, NULL);
+            vi_full->aliases = (const char **) g_ptr_array_free(aliases, FALSE);
+        }
+    }
+
+    return array;
 }

src/libnm-glib-aux/nm-enum-utils.h
@@ -15,6 +15,13 @@ typedef struct _NMUtilsEnumValueInfo {
     int         value;
 } NMUtilsEnumValueInfo;
 
+typedef struct _NMUtilsEnumValueInfoFull {
+    const char  *nick;
+    const char **aliases;
+    const char  *value_str;
+    int          value;
+} NMUtilsEnumValueInfoFull;
+
 char    *_nm_utils_enum_to_str_full(GType                       type,
                                     int                         value,
                                     const char                 *sep,
@@ -26,6 +33,11 @@ gboolean _nm_utils_enum_from_str_full(GType                       type,
                                       const NMUtilsEnumValueInfo *value_infos);
 
 const char **_nm_utils_enum_get_values(GType type, int from, int to);
+
+GArray *_nm_utils_enum_get_values_full(GType                       type,
+                                       int                         from,
+                                       int                         to,
+                                       const NMUtilsEnumValueInfo *value_infos);
 
 /*****************************************************************************/
 

commit 539d6f436ab2645b3aced2a9ad084b39654d9da4

gen-metadata-nm-settings-nmcli: fix some printf warnings on 32 bit platforms
The specifiers %ld and %lu are not correct for 64 bit integers on 32 bit
platforms, triggering a warning. Use instead the GLib constants to
correctly define them.

Fixes: 925d4df801f7 ('man nm-settings-nmcli: add "Valid values" field')
Fixes: 5c6ae44e006b ('man nm-settings-nmcli: add "Special values" field')

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1790

src/nmcli/gen-metadata-nm-settings-nmcli.c
@@ -31,7 +31,7 @@ static void G_GNUC_PRINTF(3, 4)
     msg_full = g_strdup_printf("gen-metadata-nm-settings-nmcli: %s.%s (type %d): %s\n",
                                prop_info->setting_info->general->setting_name,
                                prop_info->property_name,
-                               prop_info->property_type->doc_format,
+                               (int) prop_info->property_type->doc_format,
                                msg);
     if (fatal)
         g_error("%s", msg_full);
@@ -248,10 +248,15 @@ append_int_valid_range(const NMMetaPropertyInfo *prop_info, GPtrArray *valid_val
     NMMetaSignUnsignInt64 max;
 
     nm_meta_property_int_get_range(prop_info, &min, &max);
-    if (NM_IN_SET(_property_get_gtype(prop_info), G_TYPE_UINT, G_TYPE_UINT64))
-        g_ptr_array_add(valid_values, g_strdup_printf("%lu - %lu", min.u64, max.u64));
-    else
-        g_ptr_array_add(valid_values, g_strdup_printf("%ld - %ld", min.i64, max.i64));
+    if (NM_IN_SET(_property_get_gtype(prop_info), G_TYPE_UINT, G_TYPE_UINT64)) {
+        g_ptr_array_add(
+            valid_values,
+            g_strdup_printf("%" G_GUINT64_FORMAT " - %" G_GUINT64_FORMAT, min.u64, max.u64));
+    } else {
+        g_ptr_array_add(
+            valid_values,
+            g_strdup_printf("%" G_GINT64_FORMAT " - %" G_GINT64_FORMAT, min.i64, max.i64));
+    }
 }
 
 static void
@@ -442,11 +447,11 @@ append_int_special_values(const NMMetaPropertyInfo *prop_info, GPtrArray *specia
         for (; v->nick != NULL; v++) {
             char *v_str;
             if (base == 16)
-                v_str = g_strdup_printf("%s (0x%lx)", v->nick, v->value.u64);
+                v_str = g_strdup_printf("%s (0x%" G_GINT64_MODIFIER "x)", v->nick, v->value.u64);
             else if (is_uint)
-                v_str = g_strdup_printf("%s (%lu)", v->nick, v->value.u64);
+                v_str = g_strdup_printf("%s (%" G_GUINT64_FORMAT ")", v->nick, v->value.u64);
             else
-                v_str = g_strdup_printf("%s (%ld)", v->nick, v->value.i64);
+                v_str = g_strdup_printf("%s (%" G_GINT64_FORMAT ")", v->nick, v->value.i64);
 
             g_ptr_array_add(special_values, v_str);
         }