4600a811fb8ae4511767867f68a9860e84edc5a4
Thomas Haller <thaller@redhat.com>
2023-11-28 12:37:01 +0100

    platform: fix handling "weight" for IPv4 routes
    The hash/cmp functions were wrong with respect to IPv4 routes. Fix that.
    
    - the weight was cast to a guint8, which is too small to hold all
      values.
    
    - NM_PLATFORM_IP_ROUTE_CMP_TYPE_ID comparison would normalize a zero
      weight to one
    
        NM_CMP_DIRECT(NM_MAX(a->weight, 1u), NM_MAX(b->weight, 1u));
    
      That was very wrong.
    
    - the handling of the weight depends on the n_nexthops parameter.
      See _ip4_route_weight_normalize().
    
    The remarkable thing is that upper layers find it useful to track IPv4
    single-hop routes with a non-zero weight. Consequently, this is honored
    by NM_PLATFORM_IP_ROUTE_CMP_TYPE_ID to treat single-hop routes
    different, when their weight differs. However, adding such a route in
    kernel will not work. To kernel, single-hop routes have no weight. While
    the route exists as distinct in our hash tables, according to the
    implemented identity, it never exists in kernel (or NMPlatform cache).
    Well, you can call nm_platform_ip_route_add() with such a route, but the
    result will have a weight of zero (making it a different route). See also
    nm_platform_ip_route_normalize().
    
    This works all mostly fine. The only thing to take care is that you
    cannot look into the NMPlatform cache and ever find a IPv4 single-hop
    route with a non-zero weight. If you preform such a lookup, realize that
    such routes don't exist in platform. You can however normalize the
    weight to zero first (nm_platform_ip_route_normalize()) and look for a
    similar route with a zero weight.
    
    Fixes: 1bbdecf5e125 ('platform: manage ECMP routes')
    (cherry picked from commit bc53ad4976eb736a5e7781d0a8aeb0e5549c117a)

--- src/libnm-platform/nm-platform.h
+++ src/libnm-platform/nm-platform.h
@@ -437,18 +437,27 @@ struct _NMPlatformIP4Route {
      * pref_src must match, unless set to 0.0.0.0 to match any. */
     in_addr_t pref_src;
 
-    /* This is the weight of for the first next-hop, in case of n_nexthops > 1.
+    /* This is the weight of for the first next-hop.
      *
-     * If n_nexthops is zero, this value is undefined (should be zero).
-     * If n_nexthops is 1, this also doesn't matter, but it's usually set to
-     * zero.
-     * If n_nexthops is greater or equal to one, this is the weight of
-     * the first hop.
+     * For multi-hop routes (n_nexthops > 1) this is the weight of the first
+     * hop. Note that the valid range is from 1-256. Zero is treated the same
+     * as 1 (for NM_PLATFORM_IP_ROUTE_CMP_TYPE_ID comparison).
      *
-     * Note that upper layers (nm_utils_ip_route_attribute_to_platform()) use this flag to indicate
-     * whether this is a multihop route. Single-hop, non-ECMP routes will have a weight of zero.
+     * For routes without next-hop (e.g. blackhole type), the weight is
+     * meaningless. It should be set to zero. NM_PLATFORM_IP_ROUTE_CMP_TYPE_ID
+     * will treat it as zero.
      *
-     * The valid range for weight in kernel is 1-256. */
+     * For single-hop routes, in kernel they don't have a weight. That means,
+     * all routes in the platform cache have a weight of zero. For tracking
+     * purposes, we find it useful that upper layers have single-hop routes
+     * with a positive weight. Such routes can never exist in kernel. Trying
+     * to add such a route will somewhat work, because
+     * nm_platform_ip_route_normalize() normalizes the weight to zero
+     * (effectively adding another route, according to
+     * NM_PLATFORM_IP_ROUTE_CMP_TYPE_ID). A lookup in the platform cache with
+     * such a route will not yield a result. It does not exist there. If you
+     * want to find such a route, normalize it first.
+     */
     guint16 weight;
 
     /* rtm_tos (iproute2: tos)
@@ -697,13 +706,11 @@ typedef struct {
 typedef struct {
     int       ifindex;
     in_addr_t gateway;
-    /* The valid range for weight is 1-256. Single hop routes in kernel
-     * don't have a weight, we assign them weight zero (to indicate the
-     * weight is missing).
+
+    /* The weight of the next hop. The valid range for weight is 1-256.
      *
-     * Upper layers (nm_utils_ip_route_attribute_to_platform()) care about
-     * the distinction of unset weight (no-ECMP). They express no-ECMP as
-     * zero.
+     * Zero is allowed too, but treated as 1 (by
+     * NM_PLATFORM_IP_ROUTE_CMP_TYPE_ID comparison).
      */
     guint16 weight;
 
