NetworkManager/NetworkManager

commit 50f34217f919ed13bed60133cfd7ee9d88abd07e

main: use _nm_utils_ascii_str_to_int64 instead of strtol for reading pid
Also removes the errno check, since it's already done within _nm_utils_ascii_str_to_int64()

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1727

src/core/main-utils.c
@@ -158,7 +158,8 @@ nm_main_utils_ensure_not_running_pidfile(const char *pidfile)
     gs_free char *contents     = NULL;
     gs_free char *proc_cmdline = NULL;
     gsize         len          = 0;
-    long          pid;
+    pid_t         pid;
+    gint64        pid64;
     const char   *process_name;
     const char   *prgname = g_get_prgname();
 
@@ -173,12 +174,13 @@ nm_main_utils_ensure_not_running_pidfile(const char *pidfile)
         return;
 
     errno = 0;
-    pid   = strtol(contents, NULL, 10);
-    if (pid <= 0 || errno)
+    pid64 = _nm_utils_ascii_str_to_int64(contents, 10, 0, G_MAXINT64, -1);
+    pid   = (pid_t) pid64;
+    if (pid <= 0 || (gint64) pid != pid64)
         return;
 
     nm_clear_g_free(&contents);
-    proc_cmdline = g_strdup_printf("/proc/%ld/cmdline", pid);
+    proc_cmdline = g_strdup_printf("/proc/%" G_GINT64_FORMAT "/cmdline", (gint64) pid);
     if (!g_file_get_contents(proc_cmdline, &contents, &len, NULL))
         return;
 
@@ -190,7 +192,10 @@ nm_main_utils_ensure_not_running_pidfile(const char *pidfile)
     if (strcmp(process_name, prgname) == 0) {
         /* Check that the process exists */
         if (kill(pid, 0) == 0) {
-            fprintf(stderr, _("%s is already running (pid %ld)\n"), prgname, pid);
+            fprintf(stderr,
+                    _("%s is already running (pid %" G_GINT64_FORMAT ")\n"),
+                    prgname,
+                    (gint64) pid);
             exit(1);
         }
     }

commit c86f9e47fb3782f6f0fdb2500322c808be3962c9

core: Fix unextractable translation string
Glib format specifiers are not gettext friendly. It even emits a
warning: src/core/main-utils.c:196: warning: Although being used in a
format string position, the msgid is not a valid C format string."

One possible solution is to use the equivalent format specifiers from
<inttypes.h> like PRId64, available since C99.

Even simpler is to cast the value to a type that is big enough to hold
it according to C specs (i.e. for int64: long long).

Fixes: 50f34217f919 ('main: use _nm_utils_ascii_str_to_int64 instead of strtol for reading pid')

src/core/main-utils.c
@@ -192,10 +192,7 @@ nm_main_utils_ensure_not_running_pidfile(const char *pidfile)
     if (strcmp(process_name, prgname) == 0) {
         /* Check that the process exists */
         if (kill(pid, 0) == 0) {
-            fprintf(stderr,
-                    _("%s is already running (pid %" G_GINT64_FORMAT ")\n"),
-                    prgname,
-                    (gint64) pid);
+            fprintf(stderr, _("%s is already running (pid %lld)\n"), prgname, (long long) pid);
             exit(1);
         }
     }