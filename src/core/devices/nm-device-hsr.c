NetworkManager/NetworkManager

commit 5426bdf4a12276f12a387e9c6000910d0f0efa65

HSR: add support to HSR/PRP interface
This patch add support to HSR/PRP interface. Please notice that PRP
driver is represented as HSR too. They are different drivers but on
kernel they are integrated together.

HSR/PRP is a network protocol standard for Ethernet that provides
seamless failover against failure of any network component. It intends
to be transparent to the application. These protocols are useful for
applications that request high availability and short switchover time
e.g electrical substation or high power inverters.

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1791

src/core/devices/nm-device-hsr.c
@@ -1,0 +1,290 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2023 Red Hat, Inc.
+ */
+
+#include "src/core/nm-default-daemon.h"
+
+#include "nm-manager.h"
+#include "nm-device-hsr.h"
+
+#include <linux/if_ether.h>
+
+#include "libnm-core-intern/nm-core-internal.h"
+#include "nm-act-request.h"
+#include "nm-device-private.h"
+#include "nm-setting-hsr.h"
+#include "libnm-platform/nm-platform.h"
+#include "nm-device-factory.h"
+
+#define _NMLOG_DEVICE_TYPE NMDeviceHsr
+#include "nm-device-logging.h"
+
+/*****************************************************************************/
+
+NM_GOBJECT_PROPERTIES_DEFINE(NMDeviceHsr,
+                             PROP_PORT1,
+                             PROP_PORT2,
+                             PROP_SUPERVISION_ADDRESS,
+                             PROP_MULTICAST_SPEC,
+                             PROP_PRP, );
+
+typedef struct {
+    NMPlatformLnkHsr props;
+} NMDeviceHsrPrivate;
+
+struct _NMDeviceHsr {
+    NMDevice           parent;
+    NMDeviceHsrPrivate _priv;
+};
+
+struct _NMDeviceHsrClass {
+    NMDeviceClass parent;
+};
+
+G_DEFINE_TYPE(NMDeviceHsr, nm_device_hsr, NM_TYPE_DEVICE)
+
+#define NM_DEVICE_HSR_GET_PRIVATE(self) \
+    _NM_GET_PRIVATE(self, NMDeviceHsr, NM_IS_DEVICE_HSR, NMDevice)
+
+/*****************************************************************************/
+
+static NMDeviceCapabilities
+get_generic_capabilities(NMDevice *dev)
+{
+    return NM_DEVICE_CAP_CARRIER_DETECT | NM_DEVICE_CAP_IS_SOFTWARE;
+}
+
+static void
+update_properties(NMDevice *device)
+{
+    NMDeviceHsr            *self;
+    NMDeviceHsrPrivate     *priv;
+    const NMPlatformLink   *plink;
+    const NMPlatformLnkHsr *props;
+    int                     ifindex;
+
+    g_return_if_fail(NM_IS_DEVICE_HSR(device));
+    self = NM_DEVICE_HSR(device);
+    priv = NM_DEVICE_HSR_GET_PRIVATE(self);
+
+    ifindex = nm_device_get_ifindex(device);
+    g_return_if_fail(ifindex > 0);
+    props = nm_platform_link_get_lnk_hsr(nm_device_get_platform(device), ifindex, &plink);
+
+    if (!props) {
+        _LOGW(LOGD_PLATFORM, "could not get HSR properties");
+        return;
+    }
+
+    g_object_freeze_notify((GObject *) device);
+
+#define CHECK_PROPERTY_CHANGED(field, prop)      \
+    G_STMT_START                                 \
+    {                                            \
+        if (priv->props.field != props->field) { \
+            priv->props.field = props->field;    \
+            _notify(self, prop);                 \
+        }                                        \
+    }                                            \
+    G_STMT_END
+
+    CHECK_PROPERTY_CHANGED(port1, PROP_PORT1);
+    CHECK_PROPERTY_CHANGED(port2, PROP_PORT2);
+    CHECK_PROPERTY_CHANGED(multicast_spec, PROP_MULTICAST_SPEC);
+    CHECK_PROPERTY_CHANGED(prp, PROP_PRP);
+
+    if (!nm_ether_addr_equal(&priv->props.supervision_address, &props->supervision_address))
+        _notify(self, PROP_SUPERVISION_ADDRESS);
+
+    g_object_thaw_notify((GObject *) device);
+}
+
+static void
+link_changed(NMDevice *device, const NMPlatformLink *pllink)
+{
+    NM_DEVICE_CLASS(nm_device_hsr_parent_class)->link_changed(device, pllink);
+    update_properties(device);
+}
+
+static gboolean
+create_and_realize(NMDevice              *device,
+                   NMConnection          *connection,
+                   NMDevice              *parent,
+                   const NMPlatformLink **out_plink,
+                   GError               **error)
+{
+    const char      *iface = nm_device_get_iface(device);
+    NMSettingHsr    *s_hsr;
+    NMPlatformLnkHsr lnk = {};
+    int              r;
+
+    s_hsr = _nm_connection_get_setting(connection, NM_TYPE_SETTING_HSR);
+    nm_assert(s_hsr);
+
+    if (nm_setting_hsr_get_port1(s_hsr) != NULL)
+        lnk.port1 = nm_platform_link_get_ifindex(NM_PLATFORM_GET, nm_setting_hsr_get_port1(s_hsr));
+    if (nm_setting_hsr_get_port2(s_hsr) != NULL)
+        lnk.port2 = nm_platform_link_get_ifindex(NM_PLATFORM_GET, nm_setting_hsr_get_port2(s_hsr));
+    if (nm_setting_hsr_get_supervision_address(s_hsr) != NULL)
+        nm_ether_addr_from_string(&lnk.supervision_address,
+                                  nm_setting_hsr_get_supervision_address(s_hsr));
+    lnk.multicast_spec = nm_setting_hsr_get_multicast_spec(s_hsr);
+    lnk.prp            = nm_setting_hsr_get_prp(s_hsr);
+    r = nm_platform_link_hsr_add(nm_device_get_platform(device), iface, &lnk, out_plink);
+    if (r < 0) {
+        g_set_error(error,
+                    NM_DEVICE_ERROR,
+                    NM_DEVICE_ERROR_CREATION_FAILED,
+                    "Failed to create HSR interface '%s' for '%s': %s",
+                    iface,
+                    nm_connection_get_id(connection),
+                    nm_strerror(r));
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+/*****************************************************************************/
+
+static void
+get_property(GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
+{
+    NMDeviceHsr        *self = NM_DEVICE_HSR(object);
+    NMDeviceHsrPrivate *priv = NM_DEVICE_HSR_GET_PRIVATE(self);
+    NMDevice           *port;
+
+    switch (prop_id) {
+    case PROP_PORT1:
+        port = nm_manager_get_device_by_ifindex(NM_MANAGER_GET, priv->props.port1);
+        nm_dbus_utils_g_value_set_object_path(value, port);
+        break;
+    case PROP_PORT2:
+        port = nm_manager_get_device_by_ifindex(NM_MANAGER_GET, priv->props.port2);
+        nm_dbus_utils_g_value_set_object_path(value, port);
+        break;
+    case PROP_SUPERVISION_ADDRESS:
+        g_value_take_string(value, nm_ether_addr_to_string_dup(&priv->props.supervision_address));
+        break;
+    case PROP_MULTICAST_SPEC:
+        g_value_set_uchar(value, priv->props.multicast_spec);
+        break;
+    case PROP_PRP:
+        g_value_set_boolean(value, priv->props.prp);
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+        break;
+    }
+}
+
+static void
+nm_device_hsr_init(NMDeviceHsr *self)
+{}
+
+/*****************************************************************************/
+
+static const NMDBusInterfaceInfoExtended interface_info_device_hsr = {
+    .parent = NM_DEFINE_GDBUS_INTERFACE_INFO_INIT(
+        NM_DBUS_INTERFACE_DEVICE_HSR,
+        .properties = NM_DEFINE_GDBUS_PROPERTY_INFOS(
+            NM_DEFINE_DBUS_PROPERTY_INFO_EXTENDED_READABLE("Port1", "o", NM_DEVICE_HSR_PORT1),
+            NM_DEFINE_DBUS_PROPERTY_INFO_EXTENDED_READABLE("Port2", "o", NM_DEVICE_HSR_PORT2),
+            NM_DEFINE_DBUS_PROPERTY_INFO_EXTENDED_READABLE("SupervisionAddress",
+                                                           "s",
+                                                           NM_DEVICE_HSR_SUPERVISION_ADDRESS),
+            NM_DEFINE_DBUS_PROPERTY_INFO_EXTENDED_READABLE("MulticastSpec",
+                                                           "y",
+                                                           NM_DEVICE_HSR_MULTICAST_SPEC),
+            NM_DEFINE_DBUS_PROPERTY_INFO_EXTENDED_READABLE("Prp", "b", NM_DEVICE_HSR_PRP), ), ),
+};
+
+static void
+nm_device_hsr_class_init(NMDeviceHsrClass *klass)
+{
+    GObjectClass      *object_class      = G_OBJECT_CLASS(klass);
+    NMDBusObjectClass *dbus_object_class = NM_DBUS_OBJECT_CLASS(klass);
+    NMDeviceClass     *device_class      = NM_DEVICE_CLASS(klass);
+
+    object_class->get_property = get_property;
+
+    dbus_object_class->interface_infos = NM_DBUS_INTERFACE_INFOS(&interface_info_device_hsr);
+
+    device_class->connection_type_supported        = NM_SETTING_HSR_SETTING_NAME;
+    device_class->connection_type_check_compatible = NM_SETTING_HSR_SETTING_NAME;
+    device_class->link_types                       = NM_DEVICE_DEFINE_LINK_TYPES(NM_LINK_TYPE_HSR);
+
+    device_class->link_changed             = link_changed;
+    device_class->create_and_realize       = create_and_realize;
+    device_class->get_generic_capabilities = get_generic_capabilities;
+
+    obj_properties[PROP_PORT1] = g_param_spec_string(NM_DEVICE_HSR_PORT1,
+                                                     "",
+                                                     "",
+                                                     NULL,
+                                                     G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+    obj_properties[PROP_PORT2] = g_param_spec_string(NM_DEVICE_HSR_PORT2,
+                                                     "",
+                                                     "",
+                                                     NULL,
+                                                     G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+    obj_properties[PROP_SUPERVISION_ADDRESS] =
+        g_param_spec_string(NM_DEVICE_HSR_SUPERVISION_ADDRESS,
+                            "",
+                            "",
+                            NULL,
+                            G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+    obj_properties[PROP_MULTICAST_SPEC] =
+        g_param_spec_uchar(NM_DEVICE_HSR_MULTICAST_SPEC,
+                           "",
+                           "",
+                           0,
+                           G_MAXUINT8,
+                           0,
+                           G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+    obj_properties[PROP_PRP] = g_param_spec_boolean(NM_DEVICE_HSR_PRP,
+                                                    "",
+                                                    "",
+                                                    FALSE,
+                                                    G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+    g_object_class_install_properties(object_class, _PROPERTY_ENUMS_LAST, obj_properties);
+}
+
+/*****************************************************************************/
+
+#define NM_TYPE_HSR_DEVICE_FACTORY (nm_hsr_device_factory_get_type())
+#define NM_HSR_DEVICE_FACTORY(obj) \
+    (_NM_G_TYPE_CHECK_INSTANCE_CAST((obj), NM_TYPE_HSR_DEVICE_FACTORY, NMHsrDeviceFactory))
+
+static NMDevice *
+create_device(NMDeviceFactory      *factory,
+              const char           *iface,
+              const NMPlatformLink *plink,
+              NMConnection         *connection,
+              gboolean             *out_ignore)
+{
+    return g_object_new(NM_TYPE_DEVICE_HSR,
+                        NM_DEVICE_IFACE,
+                        iface,
+                        NM_DEVICE_TYPE_DESC,
+                        "hsr",
+                        NM_DEVICE_DEVICE_TYPE,
+                        NM_DEVICE_TYPE_HSR,
+                        NM_DEVICE_LINK_TYPE,
+                        NM_LINK_TYPE_HSR,
+                        NULL);
+}
+
+NM_DEVICE_FACTORY_DEFINE_INTERNAL(
+    HSR,
+    Hsr,
+    hsr,
+    NM_DEVICE_FACTORY_DECLARE_LINK_TYPES(NM_LINK_TYPE_HSR)
+        NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES(NM_SETTING_HSR_SETTING_NAME),
+    factory_class->create_device = create_device;)

src/libnm-client-impl/libnm.ver
@@ -1947,7 +1947,20 @@ global:
 libnm_1_46_0 {
 global:
 	nm_access_point_get_bandwidth;
+	nm_device_hsr_get_multicast_spec;
+	nm_device_hsr_get_port1;
+	nm_device_hsr_get_port2;
+	nm_device_hsr_get_prp;
+	nm_device_hsr_get_supervision_address;
+	nm_device_hsr_get_type;
 	nm_device_ip_tunnel_get_fwmark;
 	nm_ethtool_optname_is_channels;
 	nm_ethtool_optname_is_eee;
+	nm_setting_hsr_get_multicast_spec;
+	nm_setting_hsr_get_port1;
+	nm_setting_hsr_get_port2;
+	nm_setting_hsr_get_prp;
+	nm_setting_hsr_get_supervision_address;
+	nm_setting_hsr_get_type;
+	nm_setting_hsr_new;
 } libnm_1_44_0;

src/libnm-core-impl/gen-metadata-nm-settings-libnm-core.xml.in
@@ -1418,6 +1418,30 @@
                   gprop-type="gint"
                   />
     </setting>
+    <setting name="hsr"
+             gtype="NMSettingHsr"
+             >
+        <property name="multicast-spec"
+                  dbus-type="u"
+                  gprop-type="guint"
+                  />
+        <property name="port1"
+                  dbus-type="s"
+                  gprop-type="gchararray"
+                  />
+        <property name="port2"
+                  dbus-type="s"
+                  gprop-type="gchararray"
+                  />
+        <property name="prp"
+                  dbus-type="b"
+                  gprop-type="gboolean"
+                  />
+        <property name="supervision-address"
+                  dbus-type="s"
+                  gprop-type="gchararray"
+                  />
+    </setting>
     <setting name="infiniband"
              gtype="NMSettingInfiniband"
              >

src/libnm-core-impl/nm-keyfile.c
@@ -3045,6 +3045,10 @@ static const ParseInfoSetting *const parse_infos[_NM_META_SETTING_TYPE_NUM] = {
                                                   .parser = setting_alias_parser,
                                                   .writer = setting_alias_writer, ), ), ),
     PARSE_INFO_SETTING(
+        NM_META_SETTING_TYPE_HSR,
+        PARSE_INFO_PROPERTIES(PARSE_INFO_PROPERTY(NM_SETTING_HSR_SUPERVISION_ADDRESS,
+                                                  .parser = mac_address_parser_ETHER, ), ), ),
+    PARSE_INFO_SETTING(
         NM_META_SETTING_TYPE_INFINIBAND,
         PARSE_INFO_PROPERTIES(PARSE_INFO_PROPERTY(NM_SETTING_INFINIBAND_MAC_ADDRESS,
                                                   .parser = mac_address_parser_INFINIBAND, ), ), ),

commit 0e893593a95aac02b20d1015a009f414f996917c

hsr: drop supervision-address from HSR setting
The supervision address is read-only. It is constructed by kernel and
only the last byte can be modified by setting the multicast-spec as
documented indeed.

As 1.46 was not released yet, we still can drop the whole API for this
setting property. We are keeping the NMDeviceHsr property as it is a
nice to have for reading it.

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1823

Fixes: 5426bdf4a122 ('HSR: add support to HSR/PRP interface')

src/core/devices/nm-device-hsr.c
@@ -126,9 +126,6 @@ create_and_realize(NMDevice              *device,
         lnk.port1 = nm_platform_link_get_ifindex(NM_PLATFORM_GET, nm_setting_hsr_get_port1(s_hsr));
     if (nm_setting_hsr_get_port2(s_hsr) != NULL)
         lnk.port2 = nm_platform_link_get_ifindex(NM_PLATFORM_GET, nm_setting_hsr_get_port2(s_hsr));
-    if (nm_setting_hsr_get_supervision_address(s_hsr) != NULL)
-        nm_ether_addr_from_string(&lnk.supervision_address,
-                                  nm_setting_hsr_get_supervision_address(s_hsr));
     lnk.multicast_spec = nm_setting_hsr_get_multicast_spec(s_hsr);
     lnk.prp            = nm_setting_hsr_get_prp(s_hsr);
     r = nm_platform_link_hsr_add(nm_device_get_platform(device), iface, &lnk, out_plink);

src/libnm-client-impl/libnm.ver
@@ -1960,7 +1960,6 @@ global:
 	nm_setting_hsr_get_port1;
 	nm_setting_hsr_get_port2;
 	nm_setting_hsr_get_prp;
-	nm_setting_hsr_get_supervision_address;
 	nm_setting_hsr_get_type;
 	nm_setting_hsr_new;
 } libnm_1_44_0;

src/libnm-core-impl/gen-metadata-nm-settings-libnm-core.xml.in
@@ -1437,10 +1437,6 @@
                   dbus-type="b"
                   gprop-type="gboolean"
                   />
-        <property name="supervision-address"
-                  dbus-type="s"
-                  gprop-type="gchararray"
-                  />
     </setting>
     <setting name="infiniband"
              gtype="NMSettingInfiniband"

src/libnm-core-impl/nm-keyfile.c
@@ -3045,10 +3045,6 @@ static const ParseInfoSetting *const parse_infos[_NM_META_SETTING_TYPE_NUM] = {
                                                   .parser = setting_alias_parser,
                                                   .writer = setting_alias_writer, ), ), ),
     PARSE_INFO_SETTING(
-        NM_META_SETTING_TYPE_HSR,
-        PARSE_INFO_PROPERTIES(PARSE_INFO_PROPERTY(NM_SETTING_HSR_SUPERVISION_ADDRESS,
-                                                  .parser = mac_address_parser_ETHER, ), ), ),
-    PARSE_INFO_SETTING(
         NM_META_SETTING_TYPE_INFINIBAND,
         PARSE_INFO_PROPERTIES(PARSE_INFO_PROPERTY(NM_SETTING_INFINIBAND_MAC_ADDRESS,
                                                   .parser = mac_address_parser_INFINIBAND, ), ), ),

src/libnm-core-impl/nm-setting-hsr.c
@@ -23,17 +23,11 @@
 
 /*****************************************************************************/
 
-NM_GOBJECT_PROPERTIES_DEFINE(NMSettingHsr,
-                             PROP_PORT1,
-                             PROP_PORT2,
-                             PROP_SUPERVISION_ADDRESS,
-                             PROP_MULTICAST_SPEC,
-                             PROP_PRP, );
+NM_GOBJECT_PROPERTIES_DEFINE(NMSettingHsr, PROP_PORT1, PROP_PORT2, PROP_MULTICAST_SPEC, PROP_PRP, );
 
 typedef struct {
     char   *port1;
     char   *port2;
-    char   *supervision_address;
     guint32 multicast_spec;
     bool    prp;
 } NMSettingHsrPrivate;
@@ -89,22 +83,6 @@ nm_setting_hsr_get_port2(NMSettingHsr *setting)
     g_return_val_if_fail(NM_IS_SETTING_HSR(setting), NULL);
 
     return NM_SETTING_HSR_GET_PRIVATE(setting)->port2;
-}
-
-/**
- * nm_setting_hsr_get_supervision_address:
- * @setting: the #NMSettingHsr
- *
- * Returns: the #NMSettingHsr:supervision_address property of the setting
- *
- * Since: 1.46
- **/
-const char *
-nm_setting_hsr_get_supervision_address(NMSettingHsr *setting)
-{
-    g_return_val_if_fail(NM_IS_SETTING_HSR(setting), NULL);
-
-    return NM_SETTING_HSR_GET_PRIVATE(setting)->supervision_address;
 }
 
 /**
@@ -182,19 +160,6 @@ verify(NMSetting *setting, NMConnection *connection, GError **error)
         return FALSE;
     }
 
-    if (priv->supervision_address && !nm_utils_hwaddr_valid(priv->supervision_address, ETH_ALEN)) {
-        g_set_error(error,
-                    NM_CONNECTION_ERROR,
-                    NM_CONNECTION_ERROR_INVALID_PROPERTY,
-                    _("'%s' is not a valid MAC address"),
-                    priv->supervision_address);
-        g_prefix_error(error,
-                       "%s.%s: ",
-                       NM_SETTING_HSR_SETTING_NAME,
-                       NM_SETTING_HSR_SUPERVISION_ADDRESS);
-        return FALSE;
-    }
-
     return TRUE;
 }
 
@@ -260,21 +225,6 @@ nm_setting_hsr_class_init(NMSettingHsrClass *klass)
                                               NM_SETTING_PARAM_INFERRABLE,
                                               NMSettingHsr,
                                               _priv.port2);
-
-    /**
-      * NMSettingHsr:supervision-address:
-      *
-      * The supervision MAC address.
-      *
-      * Since: 1.46
-      **/
-    _nm_setting_property_define_direct_string(properties_override,
-                                              obj_properties,
-                                              NM_SETTING_HSR_SUPERVISION_ADDRESS,
-                                              PROP_SUPERVISION_ADDRESS,
-                                              NM_SETTING_PARAM_INFERRABLE,
-                                              NMSettingHsr,
-                                              _priv.supervision_address);
 
     /**
       * NMSettingHsr:multicast-spec:

src/libnm-core-public/nm-setting-hsr.h
@@ -23,11 +23,10 @@ G_BEGIN_DECLS
 
 #define NM_SETTING_HSR_SETTING_NAME "hsr"
 
-#define NM_SETTING_HSR_PORT1               "port1"
-#define NM_SETTING_HSR_PORT2               "port2"
-#define NM_SETTING_HSR_SUPERVISION_ADDRESS "supervision-address"
-#define NM_SETTING_HSR_MULTICAST_SPEC      "multicast-spec"
-#define NM_SETTING_HSR_PRP                 "prp"
+#define NM_SETTING_HSR_PORT1          "port1"
+#define NM_SETTING_HSR_PORT2          "port2"
+#define NM_SETTING_HSR_MULTICAST_SPEC "multicast-spec"
+#define NM_SETTING_HSR_PRP            "prp"
 
 typedef struct _NMSettingHsrClass NMSettingHsrClass;
 
@@ -40,8 +39,6 @@ NM_AVAILABLE_IN_1_46
 const char *nm_setting_hsr_get_port1(NMSettingHsr *setting);
 NM_AVAILABLE_IN_1_46
 const char *nm_setting_hsr_get_port2(NMSettingHsr *setting);
-NM_AVAILABLE_IN_1_46
-const char *nm_setting_hsr_get_supervision_address(NMSettingHsr *setting);
 NM_AVAILABLE_IN_1_46
 guint32 nm_setting_hsr_get_multicast_spec(NMSettingHsr *setting);
 NM_AVAILABLE_IN_1_46

src/libnm-platform/nm-linux-platform.c
@@ -5075,11 +5075,6 @@ _nl_msg_new_link_set_linkinfo(struct nl_msg *msg, NMLinkType link_type, gconstpo
 
         if (props->multicast_spec)
             NLA_PUT_U8(msg, IFLA_HSR_MULTICAST_SPEC, props->multicast_spec);
-        if (!nm_ether_addr_is_zero(&props->supervision_address))
-            NLA_PUT(msg,
-                    IFLA_HSR_SUPERVISION_ADDR,
-                    sizeof(props->supervision_address),
-                    &props->supervision_address);
 
         NLA_PUT_U8(msg, IFLA_HSR_PROTOCOL, props->prp);
         break;

src/libnmc-setting/nm-meta-setting-desc.c
@@ -6078,12 +6078,6 @@ static const NMMetaPropertyInfo *const property_infos_HSR[] = {
         .prompt =                       N_("hsr port2"),
         .property_type =                &_pt_gobject_string,
     ),
-    PROPERTY_INFO_WITH_DESC (NM_SETTING_HSR_SUPERVISION_ADDRESS,
-        .is_cli_option =                TRUE,
-        .property_alias =               "supervision-address",
-        .prompt =                       N_("hsr supervision address"),
-        .property_type =                &_pt_gobject_mac,
-    ),
     PROPERTY_INFO_WITH_DESC (NM_SETTING_HSR_MULTICAST_SPEC,
         .is_cli_option =                TRUE,
         .property_alias =               "multicast-spec",

src/libnmc-setting/settings-docs.h.in
@@ -449,7 +449,6 @@
 #define DESCRIBE_DOC_NM_SETTING_HSR_PORT1 N_("The port1 interface name of the HSR. This property is mandatory.")
 #define DESCRIBE_DOC_NM_SETTING_HSR_PORT2 N_("The port2 interface name of the HSR. This property is mandatory.")
 #define DESCRIBE_DOC_NM_SETTING_HSR_PRP N_("The protocol used by the interface, whether it is PRP or HSR.")
-#define DESCRIBE_DOC_NM_SETTING_HSR_SUPERVISION_ADDRESS N_("The supervision MAC address.")
 #define DESCRIBE_DOC_NM_SETTING_LINK_GRO_MAX_SIZE N_("The maximum size of a packet built by the Generic Receive Offload stack for this device. The value must be between 0 and 4294967295. When set to -1, the existing value is preserved.")
 #define DESCRIBE_DOC_NM_SETTING_LINK_GSO_MAX_SEGMENTS N_("The maximum segments of a Generic Segment Offload packet the device should accept. The value must be between 0 and 4294967295. When set to -1, the existing value is preserved.")
 #define DESCRIBE_DOC_NM_SETTING_LINK_GSO_MAX_SIZE N_("The maximum size of a Generic Segment Offload packet the device should accept. The value must be between 0 and 4294967295. When set to -1, the existing value is preserved.")

src/nmcli/gen-metadata-nm-settings-nmcli.xml.in
@@ -1144,10 +1144,6 @@
                   alias="port2"
                   nmcli-description="The port2 interface name of the HSR. This property is mandatory."
                   format="string" />
-        <property name="supervision-address"
-                  alias="supervision-address"
-                  nmcli-description="The supervision MAC address."
-                  format="MAC address" />
         <property name="multicast-spec"
                   alias="multicast-spec"
                   nmcli-description="The last byte of supervision address."