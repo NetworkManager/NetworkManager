NetworkManager/NetworkManager

commit 3e4a2ebb3c63fbf523f97d70d4027b7f9f9e37ea

all: use the new NMSettingConnection Controller property

src/core/NetworkManagerUtils.c
@@ -695,7 +695,8 @@ check_connection_controller(NMConnection *orig, NMConnection *candidate, GHashTa
 
     props = check_property_in_hash(settings,
                                    NM_SETTING_CONNECTION_SETTING_NAME,
-                                   NM_SETTING_CONNECTION_MASTER);
+                                   NM_SETTING_CONNECTION_CONTROLLER);
+
     if (!props)
         return TRUE;
 
@@ -721,7 +722,7 @@ check_connection_controller(NMConnection *orig, NMConnection *candidate, GHashTa
                 remove_from_hash(settings,
                                  props,
                                  NM_SETTING_CONNECTION_SETTING_NAME,
-                                 NM_SETTING_CONNECTION_MASTER);
+                                 NM_SETTING_CONNECTION_CONTROLLER);
                 return TRUE;
             } else {
                 return FALSE;

src/core/devices/nm-device-bond.c
@@ -248,7 +248,7 @@ controller_update_port_connection(NMDevice     *self,
                      NULL);
 
     g_object_set(nm_connection_get_setting_connection(connection),
-                 NM_SETTING_CONNECTION_MASTER,
+                 NM_SETTING_CONNECTION_CONTROLLER,
                  nm_connection_get_uuid(applied_connection),
                  NM_SETTING_CONNECTION_SLAVE_TYPE,
                  NM_SETTING_BOND_SETTING_NAME,

src/core/devices/nm-device-bridge.c
@@ -610,7 +610,7 @@ master_update_slave_connection(NMDevice     *device,
     }
 
     g_object_set(s_con,
-                 NM_SETTING_CONNECTION_MASTER,
+                 NM_SETTING_CONNECTION_CONTROLLER,
                  nm_connection_get_uuid(applied_connection),
                  NM_SETTING_CONNECTION_SLAVE_TYPE,
                  NM_SETTING_BRIDGE_SETTING_NAME,

src/core/devices/team/nm-device-team.c
@@ -290,7 +290,7 @@ master_update_slave_connection(NMDevice     *device,
     g_free(port_config);
 
     g_object_set(nm_connection_get_setting_connection(connection),
-                 NM_SETTING_CONNECTION_MASTER,
+                 NM_SETTING_CONNECTION_CONTROLLER,
                  nm_connection_get_uuid(applied_connection),
                  NM_SETTING_CONNECTION_SLAVE_TYPE,
                  NM_SETTING_TEAM_SETTING_NAME,

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-reader.c
@@ -306,7 +306,7 @@ check_if_bond_slave(shvarFile *ifcfg, NMSettingConnection *s_con)
         }
 
         g_object_set(s_con,
-                     NM_SETTING_CONNECTION_MASTER,
+                     NM_SETTING_CONNECTION_CONTROLLER,
                      v,
                      NM_SETTING_CONNECTION_SLAVE_TYPE,
                      NM_SETTING_BOND_SETTING_NAME,
@@ -340,7 +340,7 @@ check_if_team_slave(shvarFile *ifcfg, NMSettingConnection *s_con)
     }
 
     g_object_set(s_con,
-                 NM_SETTING_CONNECTION_MASTER,
+                 NM_SETTING_CONNECTION_CONTROLLER,
                  v,
                  NM_SETTING_CONNECTION_SLAVE_TYPE,
                  NM_SETTING_TEAM_SETTING_NAME,
@@ -512,7 +512,7 @@ make_connection_setting(const char *file,
                           old_value,
                           v);
         } else {
-            g_object_set(s_con, NM_SETTING_CONNECTION_MASTER, v, NULL);
+            g_object_set(s_con, NM_SETTING_CONNECTION_CONTROLLER, v, NULL);
             g_object_set(s_con,
                          NM_SETTING_CONNECTION_SLAVE_TYPE,
                          NM_SETTING_BRIDGE_SETTING_NAME,
@@ -535,7 +535,7 @@ make_connection_setting(const char *file,
                           old_value,
                           v);
         } else {
-            g_object_set(s_con, NM_SETTING_CONNECTION_MASTER, v, NULL);
+            g_object_set(s_con, NM_SETTING_CONNECTION_CONTROLLER, v, NULL);
             g_object_set(s_con,
                          NM_SETTING_CONNECTION_SLAVE_TYPE,
                          NM_SETTING_OVS_PORT_SETTING_NAME,
@@ -555,7 +555,7 @@ make_connection_setting(const char *file,
                           old_value,
                           v);
         } else {
-            g_object_set(s_con, NM_SETTING_CONNECTION_MASTER, v, NULL);
+            g_object_set(s_con, NM_SETTING_CONNECTION_CONTROLLER, v, NULL);
             g_object_set(s_con,
                          NM_SETTING_CONNECTION_SLAVE_TYPE,
                          NM_SETTING_VRF_SETTING_NAME,

src/core/settings/plugins/keyfile/tests/test-keyfile-settings.c
@@ -2046,7 +2046,7 @@ test_write_bridge_component(void)
                  TRUE,
                  NM_SETTING_CONNECTION_TYPE,
                  NM_SETTING_WIRED_SETTING_NAME,
-                 NM_SETTING_CONNECTION_MASTER,
+                 NM_SETTING_CONNECTION_CONTROLLER,
                  "br0",
                  NM_SETTING_CONNECTION_SLAVE_TYPE,
                  NM_SETTING_BRIDGE_SETTING_NAME,
@@ -2357,7 +2357,7 @@ test_read_minimal_slave(void)
                                                     NM_SETTING_WIRED_SETTING_NAME,
                                                     &s_con);
     g_object_set(s_con,
-                 NM_SETTING_CONNECTION_MASTER,
+                 NM_SETTING_CONNECTION_CONTROLLER,
                  "br0",
                  NM_SETTING_CONNECTION_SLAVE_TYPE,
                  "bridge",

src/core/tests/test-core.c
@@ -302,7 +302,7 @@ test_nm_utils_log_connection_diff(void)
     g_object_set(nm_connection_get_setting_connection(connection2),
                  NM_SETTING_CONNECTION_ID,
                  "id2",
-                 NM_SETTING_CONNECTION_MASTER,
+                 NM_SETTING_CONNECTION_CONTROLLER,
                  "master2",
                  NULL);
     nm_utils_log_connection_diff(connection,

src/libnm-core-impl/nm-setting-6lowpan.c
@@ -103,7 +103,7 @@ verify(NMSetting *setting, NMConnection *connection, GError **error)
                             NM_CONNECTION_ERROR_INVALID_PROPERTY,
                             _("'%s' value doesn't match '%s=%s'"),
                             priv->parent,
-                            NM_SETTING_CONNECTION_MASTER,
+                            NM_SETTING_CONNECTION_CONTROLLER,
                             master);
                 g_prefix_error(error,
                                "%s.%s: ",

commit b9f7b1a17b9df0508c3920fe9d96f21a272f6e3f

utils: drop both "master" and "controller" from the dictionary
If a generated connection matches a connection that uses interface name
as controller, we need to drop the existing value from the settings to
avoid conflicts. Therefore, both of them need to be dropped; controller
and master.

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1833

Fixes: 3e4a2ebb3c63 ('all: use the new NMSettingConnection Controller property')

Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

src/core/NetworkManagerUtils.c
@@ -722,6 +722,10 @@ check_connection_controller(NMConnection *orig, NMConnection *candidate, GHashTa
                 remove_from_hash(settings,
                                  props,
                                  NM_SETTING_CONNECTION_SETTING_NAME,
+                                 NM_SETTING_CONNECTION_MASTER);
+                remove_from_hash(settings,
+                                 props,
+                                 NM_SETTING_CONNECTION_SETTING_NAME,
                                  NM_SETTING_CONNECTION_CONTROLLER);
                 return TRUE;
             } else {