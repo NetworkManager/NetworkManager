f585128d681ac470951b4bf48e4296e5d0400a4f
Wen Liang <liangwen12year@gmail.com>
2024-03-05 11:33:19 -0500

    loggging 10
    Signed-off-by: Wen Liang <liangwen12year@gmail.com>

--- src/core/nm-policy.c
+++ src/core/nm-policy.c
@@ -330,17 +330,16 @@ ip6_remove_device_prefix_delegations(NMPolicy *self, NMDevice *device)
                       nm_device_get_applied_connection(device),
                       device,
                       subnet);
-                if (!g_hash_table_remove(delegation->subnets, subnet)) {
+                if (g_hash_table_remove(delegation->subnets, GINT_TO_POINTER(ifindex))) {
                     _LOGW(LOGD_DEVICE,
                           "---- %s:%d : down link subnet removed, connection on device %p, the "
                           "device "
-                          "is %p, the subnet is %p faillllllll",
+                          "is %p, the subnet is %p successssssss",
                           __func__,
                           __LINE__,
                           nm_device_get_applied_connection(device),
                           device,
                           subnet);
-                    subnet->preferred = 0;
                 }
             }
         }
