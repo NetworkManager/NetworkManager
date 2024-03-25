NetworkManager/NetworkManager

commit 1c41066a40d9cdb39904798c1caf220d6234e882

build: include documentation in meson dist
Previously, the tarball generated by `meson dist` did not contain the
autogenerated documentation due to the way meson works (packaging the
latest revision control commit). This introduces a dist script which
builds & copies the generated documentation into the distribution
tarball.

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1811

tools/meson-dist-data.sh
@@ -1,0 +1,35 @@
+#!/bin/bash
+set -e
+
+ensure_var_path() {
+  if [ "${!1}" = "" ]; then
+      echo "$0: Required variable $1 is not set, aborting" >&2
+      exit 1
+  fi
+
+  if [ ! -d "${!1}" ]; then
+      echo "$0: Path '${!1}' in $1 does not exist or is not directory, aborting" >&2
+      exit 1
+  fi
+}
+
+copy_from_build() {
+    cp -Tr "$MESON_BUILD_ROOT/$1" "$MESON_DIST_ROOT/$1"
+}
+
+if [ "$MESON_BUILD_ROOT" = "" ]; then
+    if [ "$1" = "--build-root" ]; then
+        MESON_BUILD_ROOT="$2"
+    fi
+fi
+
+ensure_var_path "MESON_DIST_ROOT"
+ensure_var_path "MESON_BUILD_ROOT"
+ensure_var_path "MESON_SOURCE_ROOT"
+
+ninja -C "$MESON_BUILD_ROOT" all libnm-doc NetworkManager-doc
+
+mkdir -p "$MESON_DIST_ROOT/docs/"
+copy_from_build /docs/api/
+copy_from_build /docs/libnm/
+copy_from_build /man/

meson.build
@@ -978,9 +978,13 @@ endif
 
 if enable_docs
   assert(enable_introspection, '-Ddocs=true requires -Dintrospection=true')
-  assert(meson.version().version_compare('>= 0.46.0'), '-Ddocs requires meson >= 0.46')
+  assert(meson.version().version_compare('>= 0.49.0'), '-Ddocs requires meson >= 0.49')
   subdir('man')
   subdir('docs')
+  meson.add_dist_script(
+    'tools/meson-dist-data.sh',
+    '--build-root', build_root
+  )
 endif
 
 configure_file(

commit 4efcea0e2b9e4ca5e3a25debe061eb4bab327fdd

meson: put the documentation included in the tarball in separate folder
We want to distribute the generated documentation when we generate the
tarball because we normally do it when we do a release, but `meson dist`
only includes files that are commited to the repository, so a script
meson-dist-data.sh was added with meson.add_dist_script in commit
1c41066a40d9 ('build: include documentation in meson dist').

This script was copying the whole documentation folders, including some
intermediate files that are not useful for users that wants to read the
docs. Get rid of them and copy only the files that are useful for users:
the generated html pages in docs/api and docs/libnm and the final man
pages.

Also, including these intermediate files caused at least one build
failure, although quite difficult to reproduce:
- Generate tarball with meson
- Untar the generated tarball
- Using the sources from the tarball, configure the project with
  autotools, but building to an out-of-tree folder, not building in
  the source dir (i.e. using a 'build' subfolder). This is called
  a "VPATH build" by autotools and Make. See:
  - https://www.gnu.org/software/make/manual/html_node/General-Search.html
  - https://www.gnu.org/software/automake/manual/html_node/VPATH-Builds.html
- Build

In that scenario, we get an error trying to generate any file under man/
because the man/ subdirectory has not been created. The reason of this
was that the man/ subdirectory is created by the Makefile when
generating the file man/common.ent. However, this file was present in
the source directory because it has been included in the tarball, so
Make detects it and doesn't run the rules to generate it. The result is
that out-of-tree-dir/man folder is not created.

Not including the intermediate files would solve this problem. Anyway,
it is also a good idea to save the generated docs to a new folder,
called docs-dist, that is not used in any part of the build process, so
we ensure even more that this kind of situations doesn't happen again.

Fixes: 1c41066a40d9

tools/meson-dist-data.sh
@@ -13,10 +13,6 @@ ensure_var_path() {
   fi
 }
 
-copy_from_build() {
-    cp -Tr "$MESON_BUILD_ROOT/$1" "$MESON_DIST_ROOT/$1"
-}
-
 if [ "$MESON_BUILD_ROOT" = "" ]; then
     if [ "$1" = "--build-root" ]; then
         MESON_BUILD_ROOT="$2"
@@ -29,7 +25,7 @@ ensure_var_path "MESON_SOURCE_ROOT"
 
 ninja -C "$MESON_BUILD_ROOT" all libnm-doc NetworkManager-doc
 
-mkdir -p "$MESON_DIST_ROOT/docs/"
-copy_from_build /docs/api/
-copy_from_build /docs/libnm/
-copy_from_build /man/
+mkdir -p "$MESON_DIST_ROOT/docs-dist/man"
+cp -Tr "$MESON_BUILD_ROOT/docs/api/html" "$MESON_DIST_ROOT/docs-dist/api"
+cp -Tr "$MESON_BUILD_ROOT/docs/libnm/html" "$MESON_DIST_ROOT/docs-dist/libnm"
+cp "$MESON_BUILD_ROOT/man/"*.[1-8] "$MESON_DIST_ROOT/docs-dist/man"