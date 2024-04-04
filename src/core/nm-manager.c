NetworkManager/NetworkManager

commit bd38a1983251f5edec7aad1dc8828387c3f8b290

connection: add support to down-on-poweroff
The new option at NMSettingConnection allow the user to specify if the
connection needs to be down when powering off the system. This is useful
for IP address removal prior powering off. In order to accomplish that,
we listen on "Shutdown" systemd DBus signal.

The option is set to FALSE by default, it can be specified globally on
configuration file or per profile.

src/core/nm-manager.c
@@ -7216,14 +7216,18 @@ device_sleep_cb(NMDevice *device, GParamSpec *pspec, NMManager *self)
 }
 
 static void
-_handle_device_takedown(NMManager *self, NMDevice *device, gboolean suspending)
+_handle_device_takedown(NMManager *self,
+                        NMDevice  *device,
+                        gboolean   suspending,
+                        gboolean   is_shutdown)
 {
     nm_device_notify_sleeping(device);
 
     if (nm_device_is_activating(device)
         || nm_device_get_state(device) == NM_DEVICE_STATE_ACTIVATED) {
         _LOGD(LOGD_SUSPEND,
-              "sleep: wait disconnection of device %s",
+              "%s: wait disconnection of device %s",
+              is_shutdown ? "shutdown" : "sleep",
               nm_device_get_ip_iface(device));
 
         if (sleep_devices_add(self, device, suspending))
@@ -7272,7 +7276,7 @@ do_sleep_wake(NMManager *self, gboolean sleeping_changed)
                 continue;
             }
 
-            _handle_device_takedown(self, device, suspending);
+            _handle_device_takedown(self, device, suspending, FALSE);
         }
     } else {
         _LOGD(LOGD_SUSPEND, "sleep: %s...", waking_from_suspend ? "waking up" : "re-enabling");
@@ -7450,6 +7454,41 @@ sleeping_cb(NMPowerMonitor *monitor, gboolean is_about_to_suspend, gpointer user
 
     _LOGT(LOGD_SUSPEND, "sleep: received %s signal", is_about_to_suspend ? "sleeping" : "resuming");
     _internal_sleep(self, is_about_to_suspend);
+}
+
+static void
+shutdown_cb(NMPowerMonitor *monitor, gpointer user_data)
+{
+    NMManager        *self = user_data;
+    NMManagerPrivate *priv = NM_MANAGER_GET_PRIVATE(self);
+    NMDevice         *device;
+
+    _LOGT(LOGD_SUSPEND, "shutdown: received shutdown signal");
+
+    c_list_for_each_entry (device, &priv->devices_lst_head, devices_lst) {
+        NMSettingConnection *s_con;
+        gboolean             take_down = FALSE;
+
+        s_con = nm_device_get_applied_setting(device, NM_META_SETTING_TYPE_CONNECTION);
+        if (!s_con)
+            continue;
+
+        if (nm_setting_connection_get_down_on_poweroff(s_con)
+            == NM_SETTING_CONNECTION_DOWN_ON_POWEROFF_YES)
+            take_down = TRUE;
+        else if (nm_setting_connection_get_down_on_poweroff(s_con)
+                 == NM_SETTING_CONNECTION_DOWN_ON_POWEROFF_DEFAULT)
+            take_down = nm_config_data_get_connection_default_int64(
+                NM_CONFIG_GET_DATA,
+                NM_CON_DEFAULT("connection.down-on-poweroff"),
+                device,
+                NM_SETTING_CONNECTION_DOWN_ON_POWEROFF_NO,
+                NM_SETTING_CONNECTION_DOWN_ON_POWEROFF_YES,
+                NM_SETTING_CONNECTION_DOWN_ON_POWEROFF_NO);
+
+        if (take_down)
+            _handle_device_takedown(self, device, FALSE, TRUE);
+    }
 }
 
 static void
@@ -8842,6 +8881,7 @@ nm_manager_init(NMManager *self)
     /* sleep/wake handling */
     priv->power_monitor = nm_power_monitor_new();
     g_signal_connect(priv->power_monitor, NM_POWER_MONITOR_SLEEPING, G_CALLBACK(sleeping_cb), self);
+    g_signal_connect(priv->power_monitor, NM_POWER_MONITOR_SHUTDOWN, G_CALLBACK(shutdown_cb), self);
 
     /* Listen for authorization changes */
     priv->auth_mgr = g_object_ref(nm_auth_manager_get());

man/NetworkManager.conf.xml
@@ -861,6 +861,10 @@ ipv6.ip6-privacy=0
           <term><varname>connection.autoconnect-slaves</varname></term>
         </varlistentry>
         <varlistentry>
+          <term><varname>connection.down-on-poweroff</varname></term>
+          <listitem><para>Whether the connection will be brought down before the system is powered off.</para></listitem>
+        </varlistentry>
+        <varlistentry>
           <term><varname>connection.mud-url</varname></term>
           <listitem><para>If unspecified, MUD URL defaults to <literal>"none"</literal>.</para></listitem>
         </varlistentry>

src/core/nm-power-monitor.c
@@ -47,6 +47,7 @@
 
 enum {
     SLEEPING,
+    SHUTDOWN,
     LAST_SIGNAL,
 };
 
@@ -66,6 +67,7 @@ struct _NMPowerMonitor {
 
     gulong sig_id_1;
     gulong sig_id_2;
+    gulong sig_id_3;
 };
 
 struct _NMPowerMonitorClass {
@@ -82,6 +84,8 @@ G_DEFINE_TYPE(NMPowerMonitor, nm_power_monitor, G_TYPE_OBJECT);
 /*****************************************************************************/
 
 static void sleep_signal(NMPowerMonitor *self, gboolean is_about_to_suspend);
+
+static void shutdown_signal(NMPowerMonitor *self);
 
 /*****************************************************************************/
 
@@ -166,6 +170,12 @@ prepare_for_sleep_cb(GDBusProxy *proxy, gboolean is_about_to_suspend, gpointer d
 }
 
 static void
+prepare_for_shutdown_cb(GDBusProxy *proxy, gpointer data)
+{
+    shutdown_signal(data);
+}
+
+static void
 name_owner_cb(GObject *object, GParamSpec *pspec, gpointer user_data)
 {
     GDBusProxy     *proxy = G_DBUS_PROXY(object);
@@ -196,6 +206,16 @@ sleep_signal(NMPowerMonitor *self, gboolean is_about_to_suspend)
 
     if (is_about_to_suspend)
         drop_inhibitor(self, FALSE);
+}
+
+static void
+shutdown_signal(NMPowerMonitor *self)
+{
+    g_return_if_fail(NM_IS_POWER_MONITOR(self));
+
+    _LOGD("received SHUTDOWN signal");
+
+    g_signal_emit(self, signals[SHUTDOWN], 0);
 }
 
 /**
@@ -268,6 +288,12 @@ on_proxy_acquired(GObject *object, GAsyncResult *res, NMPowerMonitor *self)
                                                    G_VARIANT_TYPE("(b)"),
                                                    G_CALLBACK(prepare_for_sleep_cb),
                                                    self);
+    self->sig_id_3 = _nm_dbus_proxy_signal_connect(self->proxy,
+                                                   "PrepareForShutdown",
+                                                   G_VARIANT_TYPE("(b)"),
+                                                   G_CALLBACK(prepare_for_shutdown_cb),
+                                                   self);
+
     {
         gs_free char *owner = NULL;
 
@@ -314,6 +340,7 @@ dispose(GObject *object)
     if (self->proxy) {
         nm_clear_g_signal_handler(self->proxy, &self->sig_id_1);
         nm_clear_g_signal_handler(self->proxy, &self->sig_id_2);
+        nm_clear_g_signal_handler(self->proxy, &self->sig_id_3);
         g_clear_object(&self->proxy);
     }
 
@@ -330,6 +357,16 @@ nm_power_monitor_class_init(NMPowerMonitorClass *klass)
     gobject_class->dispose = dispose;
 
     signals[SLEEPING] = g_signal_new(NM_POWER_MONITOR_SLEEPING,
+                                     NM_TYPE_POWER_MONITOR,
+                                     G_SIGNAL_RUN_LAST,
+                                     0,
+                                     NULL,
+                                     NULL,
+                                     g_cclosure_marshal_VOID__BOOLEAN,
+                                     G_TYPE_NONE,
+                                     1,
+                                     G_TYPE_BOOLEAN);
+    signals[SHUTDOWN] = g_signal_new(NM_POWER_MONITOR_SHUTDOWN,
                                      NM_TYPE_POWER_MONITOR,
                                      G_SIGNAL_RUN_LAST,
                                      0,

src/core/nm-power-monitor.h
@@ -18,6 +18,7 @@
 #define NM_IS_POWER_MONITOR_CLASS(k) (G_TYPE_CHECK_CLASS_TYPE((k), NM_TYPE_POWER_MONITOR))
 
 #define NM_POWER_MONITOR_SLEEPING "sleeping"
+#define NM_POWER_MONITOR_SHUTDOWN "shutdown"
 
 typedef struct _NMPowerMonitorClass NMPowerMonitorClass;
 

src/libnm-client-impl/libnm.ver
@@ -1958,6 +1958,7 @@ global:
 	nm_ethtool_optname_is_eee;
 	nm_setting_connection_get_autoconnect_ports;
 	nm_setting_connection_get_controller;
+	nm_setting_connection_get_down_on_poweroff;
 	nm_setting_connection_get_port_type;
 	nm_setting_generic_get_device_handler;
 	nm_setting_get_enum_property_type;
@@ -1977,3 +1978,9 @@ global:
 	nm_sriov_eswitch_inline_mode_get_type;
 	nm_sriov_eswitch_mode_get_type;
 } libnm_1_44_0;
+
+libnm_1_48_0 {
+global:
+	nm_setting_connection_down_on_poweroff_get_type;
+	nm_setting_connection_get_down_on_poweroff;
+} libnm_1_46_0;

src/libnm-core-impl/gen-metadata-nm-settings-libnm-core.xml.in
@@ -792,6 +792,10 @@
                   dbus-type="i"
                   gprop-type="gint"
                   />
+        <property name="down-on-poweroff"
+                  dbus-type="i"
+                  gprop-type="gint"
+                  />
         <property name="gateway-ping-timeout"
                   dbus-type="u"
                   gprop-type="guint"

src/libnm-core-impl/nm-setting-connection.c
@@ -73,7 +73,8 @@ NM_GOBJECT_PROPERTIES_DEFINE(NMSettingConnection,
                              PROP_AUTH_RETRIES,
                              PROP_WAIT_DEVICE_TIMEOUT,
                              PROP_MUD_URL,
-                             PROP_WAIT_ACTIVATION_DELAY, );
+                             PROP_WAIT_ACTIVATION_DELAY,
+                             PROP_DOWN_ON_POWEROFF, );
 
 typedef struct {
     GArray     *permissions;
@@ -89,6 +90,7 @@ typedef struct {
     char       *mud_url;
     guint64     timestamp;
     int         autoconnect_ports;
+    int         down_on_poweroff;
     int         metered;
     gint32      autoconnect_priority;
     gint32      autoconnect_retries;
@@ -826,6 +828,26 @@ nm_setting_connection_get_wait_activation_delay(NMSettingConnection *setting)
     g_return_val_if_fail(NM_IS_SETTING_CONNECTION(setting), -1);
 
     return NM_SETTING_CONNECTION_GET_PRIVATE(setting)->wait_activation_delay;
+}
+
+/**
+ * nm_setting_connection_get_down_on_poweroff:
+ * @setting: the #NMSettingConnection
+ *
+ * Returns the %NM_SETTING_CONNECTION_DOWN_ON_POWEROFF property.
+ *
+ * Returns: whether the connection will be brought down before the system
+ * is powered off.
+ *
+ * Since: 1.48
+ */
+NMSettingConnectionDownOnPoweroff
+nm_setting_connection_get_down_on_poweroff(NMSettingConnection *setting)
+{
+    g_return_val_if_fail(NM_IS_SETTING_CONNECTION(setting),
+                         NM_SETTING_CONNECTION_DOWN_ON_POWEROFF_DEFAULT);
+
+    return NM_SETTING_CONNECTION_GET_PRIVATE(setting)->down_on_poweroff;
 }
 
 /**
@@ -3157,6 +3179,29 @@ nm_setting_connection_class_init(NMSettingConnectionClass *klass)
                                              NM_SETTING_PARAM_NONE,
                                              NMSettingConnectionPrivate,
                                              wait_activation_delay);
+
+    /**
+     * NMSettingConnection:down-on-poweroff:
+     *
+     *
+     * Whether the connection will be brought down before the system is powered
+     * off.  The default value is %NM_SETTING_CONNECTION_DOWN_ON_POWEROFF_DEFAULT. When
+     * the default value is specified, then the global value from
+     * NetworkManager configuration is looked up, if not set, it is considered
+     * as %NM_SETTING_CONNECTION_DOWN_ON_POWEROFF_NO.
+     *
+     * Since: 1.48
+     **/
+    _nm_setting_property_define_direct_enum(properties_override,
+                                            obj_properties,
+                                            NM_SETTING_CONNECTION_DOWN_ON_POWEROFF,
+                                            PROP_DOWN_ON_POWEROFF,
+                                            NM_TYPE_SETTING_CONNECTION_DOWN_ON_POWEROFF,
+                                            NM_SETTING_CONNECTION_DOWN_ON_POWEROFF_DEFAULT,
+                                            NM_SETTING_PARAM_NONE,
+                                            NULL,
+                                            NMSettingConnectionPrivate,
+                                            down_on_poweroff);
 
     g_object_class_install_properties(object_class, _PROPERTY_ENUMS_LAST, obj_properties);
 

src/libnm-core-impl/tests/test-general.c
@@ -3962,7 +3962,7 @@ typedef struct {
 
 typedef struct {
     const char *name;
-    DiffKey     keys[32];
+    DiffKey     keys[33];
 } DiffSetting;
 
 #define ARRAY_LEN(a) (sizeof(a) / sizeof(a[0]))
@@ -4036,6 +4036,7 @@ test_connection_diff_a_only(void)
           {NM_SETTING_CONNECTION_MUD_URL, NM_SETTING_DIFF_RESULT_IN_A},
           {NM_SETTING_CONNECTION_WAIT_DEVICE_TIMEOUT, NM_SETTING_DIFF_RESULT_IN_A},
           {NM_SETTING_CONNECTION_WAIT_ACTIVATION_DELAY, NM_SETTING_DIFF_RESULT_IN_A},
+          {NM_SETTING_CONNECTION_DOWN_ON_POWEROFF, NM_SETTING_DIFF_RESULT_IN_A},
           {NULL, NM_SETTING_DIFF_RESULT_UNKNOWN}}},
         {NM_SETTING_WIRED_SETTING_NAME,
          {

commit 1b60dd9a9ef5e6324504c5802205179407bfaa9f

manager: fix assertion failure during shutdown
Fix the following:

  NetworkManager: file ../src/libnm-core-impl/nm-connection.c: line 321 (nm_connection_get_setting): should not be reached
  NetworkManager.service: Main process exited, code=dumped, status=5/TRAP

Fixes: bd38a1983251 ('connection: add support to down-on-poweroff')

src/core/nm-manager.c
@@ -7488,7 +7488,7 @@ shutdown_cb(NMPowerMonitor *monitor, gpointer user_data)
         NMSettingConnection *s_con;
         gboolean             take_down = FALSE;
 
-        s_con = nm_device_get_applied_setting(device, NM_META_SETTING_TYPE_CONNECTION);
+        s_con = nm_device_get_applied_setting(device, NM_TYPE_SETTING_CONNECTION);
         if (!s_con)
             continue;
 