NetworkManager/NetworkManager

commit c0ddcf2998eace0290d22ddffb83e2a954ccb834

wwan: Ensure we get existing objects on reset
When ModemManager become available, NetworkManager resets
GDBusObjectManagerClient object.

But there is a race condition if object-added is emitted before
modm_ensure_manager(), we need to check existing objects if we want to be
in sync with ModemManager.

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1957

src/core/devices/wwan/nm-modem-manager.c
@@ -265,9 +265,7 @@ modm_handle_name_owner_changed(MMManager *modem_manager, GParamSpec *pspec, NMMo
     modm_clear_manager(self);
     modm_ensure_manager(self);
 
-    /* Whenever GDBusObjectManagerClient is fixed, we can just do the following:
-     * modm_manager_available (self);
-     */
+    modm_manager_available (self);
 }
 
 static void

commit 8c800778058c6d083c3ff5443ff3d56671527c8b

wwan: fix code format
Fixes: c0ddcf2998ea ('wwan: Ensure we get existing objects on reset')

src/core/devices/wwan/nm-modem-manager.c
@@ -265,7 +265,7 @@ modm_handle_name_owner_changed(MMManager *modem_manager, GParamSpec *pspec, NMMo
     modm_clear_manager(self);
     modm_ensure_manager(self);
 
-    modm_manager_available (self);
+    modm_manager_available(self);
 }
 
 static void