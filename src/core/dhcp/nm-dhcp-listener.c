NetworkManager/NetworkManager

commit bc5d76ca820718af074af149b895a782987da6b0

core/dhcp: adjust client plugin priority
Prioritize internal, which is what most people should be using. Try
dhclient last, so that it's not attempted when not explicitly
configured or everything else fails.

src/core/dhcp/nm-dhcp-listener.c
@@ -30,18 +30,18 @@ const NMDhcpClientFactory *const _nm_dhcp_manager_factories[6] = {
 /* the order here matters, as we will try the plugins in this order to find
  * the first available plugin. */
 
+    &_nm_dhcp_client_factory_internal,
 #if WITH_DHCPCANON
     &_nm_dhcp_client_factory_dhcpcanon,
-#endif
-#if WITH_DHCLIENT
-    &_nm_dhcp_client_factory_dhclient,
 #endif
 #if WITH_DHCPCD
     &_nm_dhcp_client_factory_dhcpcd,
 #endif
-    &_nm_dhcp_client_factory_internal,
     &_nm_dhcp_client_factory_systemd,
     &_nm_dhcp_client_factory_nettools,
+#if WITH_DHCLIENT
+    &_nm_dhcp_client_factory_dhclient,
+#endif
 };
 
 /*****************************************************************************/

commit a51159f4e84fa97990d6ea134176e0d5eb8b4814

dhcp: fix indentation
Fixes: bc5d76ca8207 ('core/dhcp: adjust client plugin priority')

src/core/dhcp/nm-dhcp-listener.c
@@ -27,8 +27,8 @@
 
 const NMDhcpClientFactory *const _nm_dhcp_manager_factories[6] = {
 
-/* the order here matters, as we will try the plugins in this order to find
- * the first available plugin. */
+    /* the order here matters, as we will try the plugins in this order to find
+     * the first available plugin. */
 
     &_nm_dhcp_client_factory_internal,
 #if WITH_DHCPCANON