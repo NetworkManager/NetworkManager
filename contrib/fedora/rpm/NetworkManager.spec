NetworkManager/NetworkManager

commit 777418dfd786722d4dc16d4e60d2ca724f93b55c

rpm: disable ifcfg support in Fedora 41
https://fedoraproject.org/wiki/Changes/RemoveIfcfgSupportInNM
https://bugzilla.redhat.com/show_bug.cgi?id=2296272
https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1985

contrib/fedora/rpm/NetworkManager.spec
@@ -132,7 +132,7 @@
 %bcond_without default_ifcfg_rh
 %endif
 
-%if 0%{?rhel} >= 10
+%if 0%{?rhel} >= 10 || 0%{?fedora} >= 41
 %bcond_with ifcfg_rh
 %bcond_with split_ifcfg_rh
 %elif 0%{?fedora} >= 36
@@ -149,7 +149,7 @@
 %bcond_with ifcfg_warning
 %endif
 
-%if 0%{?fedora} >= 39
+%if %{with ifcfg_rh} && 0%{?fedora} >= 39
 %bcond_without ifcfg_migrate
 %else
 %bcond_with ifcfg_migrate

commit a367f8770be1db6b6ef5a1926a18fd1ce4fe6120

rpm: add "Obsoletes" for removed ifcfg packages in F41
Add the "Obsoletes" to the removed ifcfg packages, so that they are
properly removed on upgrade. Otherwise, the upgrade to the new NM
fails due to failed dependencies.

Fixes: 777418dfd786 ('rpm: disable ifcfg support in Fedora 41')

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/2001

contrib/fedora/rpm/NetworkManager.spec
@@ -231,6 +231,14 @@ Obsoletes: NetworkManager-initscripts-ifcfg-rh < 1:1.47.5-3
 Obsoletes: NetworkManager-dispatcher-routing-rules < 1:1.47.5-3
 %endif
 
+%if 0%{?fedora} >= 41
+%if %{without ifcfg_rh}
+Obsoletes: NetworkManager-initscripts-ifcfg-rh < 1:1.49-3.1
+Obsoletes: NetworkManager-dispatcher-routing-rules < 1:1.49.3-1
+Obsoletes: NetworkManager-initscripts-updown < 1:1.49.3-1
+%endif
+%endif
+
 Conflicts: NetworkManager-vpnc < 1:0.7.0.99-1
 Conflicts: NetworkManager-openvpn < 1:0.7.0.99-1
 Conflicts: NetworkManager-pptp < 1:0.7.0.99-1