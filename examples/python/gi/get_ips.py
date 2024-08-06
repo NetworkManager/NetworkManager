NetworkManager/NetworkManager

commit 797d9c4403cf170870e9d7d9b0ea22317c850b35

python: make dbus, gi examples, and debug-helper.py python3 ready
https://bugzilla.gnome.org/show_bug.cgi?id=791121

examples/python/gi/get_ips.py
@@ -116,8 +116,8 @@ if __name__ == "__main__":
     dev = c.get_device_by_iface(dev_iface)
     if dev is None:
         sys.exit('Device \'%s\' not found' % dev_iface)
-    print "Device: %s - %s" % (dev_iface, dev.get_device_type().value_name)
-    print "---------------------------------------"
+    print("Device: %s - %s" % (dev_iface, dev.get_device_type().value_name))
+    print("---------------------------------------")
 
     print("IPv4 addresses:")
     print("---------------")
@@ -134,7 +134,7 @@ if __name__ == "__main__":
     show_routes(dev, socket.AF_INET)
     print
 
-    print "IPv6 addresses:"
+    print("IPv6 addresses:")
     print("---------------")
     show_addresses(dev, socket.AF_INET6)
     print
@@ -144,17 +144,17 @@ if __name__ == "__main__":
     show_gateway(dev, socket.AF_INET6)
     print
 
-    print "IPv6 routes:"
+    print("IPv6 routes:")
     print("------------")
     show_routes(dev, socket.AF_INET6)
     print
 
-    print "IPv4 DNS:"
+    print("IPv4 DNS:")
     print("------------")
     show_dns(dev, socket.AF_INET)
     print
 
-    print "IPv6 DNS:"
+    print("IPv6 DNS:")
     print("------------")
     show_dns(dev, socket.AF_INET6)
     print

examples/python/dbus/add-connection-compat.py
@@ -42,7 +42,7 @@ s_con = dbus.Dictionary({
             'uuid': str(uuid.uuid4()),
             'id': 'MyConnectionExample'})
 
-addr1 = dbus.Array([ip_to_int("10.1.2.3"), dbus.UInt32(8L), ip_to_int("10.1.2.1")], signature=dbus.Signature('u'))
+addr1 = dbus.Array([ip_to_int("10.1.2.3"), dbus.UInt32(8), ip_to_int("10.1.2.1")], signature=dbus.Signature('u'))
 s_ip4 = dbus.Dictionary({
             'addresses': dbus.Array([addr1], signature=dbus.Signature('au')),
             'method': 'manual'})
@@ -56,7 +56,7 @@ con = dbus.Dictionary({
     'ipv6': s_ip6})
 
 
-print "Creating connection:", s_con['id'], "-", s_con['uuid']
+print("Creating connection:", s_con['id'], "-", s_con['uuid'])
 
 bus = dbus.SystemBus()
 proxy = bus.get_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager/Settings")

examples/python/dbus/add-connection.py
@@ -38,7 +38,7 @@ s_con = dbus.Dictionary({
 
 addr1 = dbus.Dictionary({
     'address': '10.1.2.3',
-    'prefix': dbus.UInt32(8L)})
+    'prefix': dbus.UInt32(8)})
 s_ip4 = dbus.Dictionary({
             'address-data': dbus.Array([addr1], signature=dbus.Signature('a{sv}')),
             'gateway': '10.1.2.1',
@@ -53,7 +53,7 @@ con = dbus.Dictionary({
     'ipv6': s_ip6})
 
 
-print "Creating connection:", s_con['id'], "-", s_con['uuid']
+print("Creating connection:", s_con['id'], "-", s_con['uuid'])
 
 bus = dbus.SystemBus()
 proxy = bus.get_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager/Settings")

examples/python/dbus/add-wifi-eap-connection.py
@@ -20,7 +20,7 @@
 import dbus, uuid
 
 def path_to_value(path):
-    return dbus.ByteArray("file://" + path + "\0")
+    return dbus.ByteArray("file://".encode("utf-8") + path.encode("utf-8") + "\0".encode("utf-8"))
 
 s_con = dbus.Dictionary({
     'type': '802-11-wireless',
@@ -28,7 +28,7 @@ s_con = dbus.Dictionary({
     'id': 'My Wifi'})
 
 s_wifi = dbus.Dictionary({
-    'ssid': dbus.ByteArray("homewifi"),
+    'ssid': dbus.ByteArray("homewifi".encode("utf-8")),
     'security': '802-11-wireless-security'})
 
 s_wsec = dbus.Dictionary({'key-mgmt': 'wpa-eap'})

examples/python/dbus/add-wifi-psk-connection.py
@@ -25,7 +25,7 @@ s_con = dbus.Dictionary({
     'id': 'My-WPA-PSK'})
 
 s_wifi = dbus.Dictionary({
-    'ssid': dbus.ByteArray("best-wifi"),
+    'ssid': dbus.ByteArray("best-wifi".encode("utf-8")),
     'mode': 'infrastructure',
 })
 

examples/python/dbus/create-bond.py
@@ -26,7 +26,7 @@
 
 import dbus, sys, uuid
 from dbus.mainloop.glib import DBusGMainLoop
-import gobject
+from gi.repository import GObject
 
 DBusGMainLoop(set_as_default=True)
 
@@ -54,7 +54,7 @@ def create_bond(bond_name):
         'connection': s_con,
         'ipv4': s_ip4,
         'ipv6': s_ip6})
-    print "Creating bond connection: %s" % bond_name
+    print("Creating bond connection: %s" % bond_name)
     return add_connection(con)
 
 def create_slave(device, master):
@@ -72,11 +72,11 @@ def create_slave(device, master):
     con = dbus.Dictionary({
         '802-3-ethernet': s_wired,
         'connection': s_con})
-    print "Creating slave connection: %s" % slave_name
+    print("Creating slave connection: %s" % slave_name)
     add_connection(con)
 
 def usage():
-    print "Usage: %s <bond_name> <ifname1> ..." % sys.argv[0]
+    print("Usage: %s <bond_name> <ifname1> ..." % sys.argv[0])
     sys.exit(0)
 
 
@@ -94,17 +94,17 @@ bus = dbus.SystemBus()
 proxy = bus.get_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager")
 manager = dbus.Interface(proxy, "org.freedesktop.NetworkManager")
 ac = manager.ActivateConnection(bond_path, "/", "/")
-print "Activating bond: %s (%s)" % (bond_name, ac)
+print("Activating bond: %s (%s)" % (bond_name, ac))
 
 # Monitor the active bond connection
-loop = gobject.MainLoop()
+loop = GObject.MainLoop()
 def properties_changed(props):
     if 'State' in props:
         if props['State'] == 2:
-            print "Succesfully connected"
+            print("Succesfully connected")
             loop.quit()
         if props['State'] == 3 or props['State'] == 4:
-            print "Bond activation failed"
+            print("Bond activation failed")
             loop.quit()
 
 obj = bus.get_object("org.freedesktop.NetworkManager", ac)

examples/python/dbus/get-active-connection-uuids.py
@@ -49,8 +49,8 @@ for a in active:
     c_proxy = bus.get_object("org.freedesktop.NetworkManager", connection_path)
     connection = dbus.Interface(c_proxy, "org.freedesktop.NetworkManager.Settings.Connection")
     settings = connection.GetSettings()
-    print "%s (%s) - %s" % (settings['connection']['id'], uuid, settings['connection']['type']) 
+    print("%s (%s) - %s" % (settings['connection']['id'], uuid, settings['connection']['type']))
 
 if len(active) == 0:
-    print "No active connections"
+    print("No active connections")
 

examples/python/dbus/is-wwan-default.py
@@ -36,7 +36,7 @@ bus = dbus.SystemBus()
 proxy = bus.get_object("org.freedesktop.DBus", "/org/freedesktop/DBus")
 busdaemon = dbus.Interface(proxy, "org.freedesktop.DBus")
 if not busdaemon.NameHasOwner(NM_SERVICE_NAME):
-    print "NetworkManager not running"
+    print("NetworkManager not running")
     sys.exit(1)
 
 # Get a proxy for the NetworkManager object
@@ -46,11 +46,11 @@ props = dbus.Interface(proxy, DBUS_PROPS_IFACE)
 
 def found_connection_type(ctype):
     if ctype == '':
-        print "No active connection"
+        print("No active connection")
     elif ctype in ["gsm", "cdma", "bluetooth"]:
-        print "WWAN is default"
+        print("WWAN is default")
     else:
-        print "WWAN is not default"
+        print("WWAN is not default")
         sys.exit(0)
 
 # Shortcut #1, for NM 1.0
@@ -94,7 +94,7 @@ for a in active:
         break
 
 if default_is_wwan:
-    print "WWAN is default"
+    print("WWAN is default")
 else:
-    print "WWAN is not default"
+    print("WWAN is not default")
 

commit 75350b3e7604c6f728b895b342e45425b14d9708

examples: fix print parenthesis place in get_ips.py
Fixes: 797d9c4403cf ('python: make dbus, gi examples, and debug-helper.py python3 ready')

examples/python/gi/get_ips.py
@@ -35,7 +35,7 @@ def show_addresses(dev, family):
         addr = nm_address.get_address()
         prefix = nm_address.get_prefix()
 
-        print("%s/%d") % (addr, prefix)
+        print("%s/%d" % (addr, prefix))
 
 
 def show_gateway(dev, family):
@@ -75,7 +75,7 @@ def show_routes(dev, family):
         next_hop = nm_route.get_next_hop()
         metric = nm_route.get_metric()
 
-        print("%s/%d  %s  %d") % (dest, prefix, next_hop, metric)
+        print("%s/%d  %s  %d" % (dest, prefix, next_hop, metric))
 
 
 def show_dns(dev, family):
@@ -88,11 +88,11 @@ def show_dns(dev, family):
         print("None")
         return
 
-    print("Nameservers: %s") % (ip_cfg.get_nameservers())
-    print("Domains: %s") % (ip_cfg.get_domains())
-    print("Searches: %s") % (ip_cfg.get_searches())
+    print("Nameservers: %s" % ip_cfg.get_nameservers())
+    print("Domains: %s" % ip_cfg.get_domains())
+    print("Searches: %s" % ip_cfg.get_searches())
     if family == socket.AF_INET:
-        print("WINS: %s") % (ip_cfg.get_wins_servers())
+        print("WINS: %s" % ip_cfg.get_wins_servers())
 
 
 if __name__ == "__main__":
@@ -110,39 +110,39 @@ if __name__ == "__main__":
     print("IPv4 addresses:")
     print("---------------")
     show_addresses(dev, socket.AF_INET)
-    print
+    print()
 
     print("IPv4 gateway:")
     print("-------------")
     show_gateway(dev, socket.AF_INET)
-    print
+    print()
 
     print("IPv4 routes:")
     print("------------")
     show_routes(dev, socket.AF_INET)
-    print
+    print()
 
     print("IPv6 addresses:")
     print("---------------")
     show_addresses(dev, socket.AF_INET6)
-    print
+    print()
 
     print("IPv6 gateway:")
     print("-------------")
     show_gateway(dev, socket.AF_INET6)
-    print
+    print()
 
     print("IPv6 routes:")
     print("------------")
     show_routes(dev, socket.AF_INET6)
-    print
+    print()
 
     print("IPv4 DNS:")
     print("------------")
     show_dns(dev, socket.AF_INET)
-    print
+    print()
 
     print("IPv6 DNS:")
     print("------------")
     show_dns(dev, socket.AF_INET6)
-    print
+    print()