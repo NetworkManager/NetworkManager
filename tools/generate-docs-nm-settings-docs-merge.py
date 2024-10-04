78585c27c004eecba482c2802d8fbdbb737f6bc3
Íñigo Huguet <ihuguet@redhat.com>
2024-10-04 11:51:33 +0200

    ip-config: Fix the nmcli docs for the renamed dhcp-send-hostname
    In nmcli we have renamed dhcp-send-hostname and dhcp-send-hostname-v2 to
    dhcp-send-hostname-deprecated and dhcp-send-hostname so users don't need
    to worry about the details of the weird workarounds that we sometimes
    need to do to expand and/or deprecate some properties.
    
    However, the autogenerated documentation didn't include this names. Add
    ---nmcli--- specific documentation, adding a new property-infos field
    called "rename" with the new name used in nmcli. This field can be used
    for more properties if we use the same strategy in the future.

--- tools/generate-docs-nm-settings-docs-merge.py
+++ tools/generate-docs-nm-settings-docs-merge.py
@@ -300,6 +300,7 @@ for setting_name in iter_keys_of_dicts(settings_roots, key_fcn_setting_name):
         node_set_attr(property_node, "special-values", properties_attrs)
         node_set_attr(property_node, "default", properties_attrs)
         node_set_attr(property_node, "alias", properties_attrs)
+        node_set_attr(property_node, "rename", properties_attrs)
 
         if description_docbook is not None:
             property_node.insert(0, description_docbook)
