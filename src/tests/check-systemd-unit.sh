f5a723ae5087775b316ae30986418c06a06a5cec
Lubomir Rintel <lkundrak@v3.sk>
2024-10-04 21:30:49 +0200

    test: check validity of systemd service files
    This spots errors like what commit a5314584564a ('cloud-setup: fix
    placement of directives in the systemd unit') fixes.
    
    This is what a failure would look like:
    
       5/74 check-nm-cloud-setup.service     FAIL     0.05s   exit status 1
      >>> ...  /bin/bash src/tests/check-systemd-service.sh
          build/src/nm-cloud-setup/nm-cloud-setup.service
      ―――――――――――――――――――――― ✀  ―――――――――――――――――――――――
      build/src/nm-cloud-setup/nm-cloud-setup.service:19:
          Unknown key 'StartLimitIntervalSec' in section [Service], ignoring.
    
    https://issues.redhat.com/browse/RHEL-56860
    https://issues.redhat.com/browse/RHEL-49694
    https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/pipelines/1284415

---
+++ src/tests/check-systemd-unit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+set -e
+set -o pipefail
+
+# We're using "security" as opposed to "verify" because (as of 2024)
+# the latter doesn't support --offline runs.
+#
+# The point is that if anything appears before the security report
+# header, there's an error or a warning while parsing the unit file.
+env -i systemd-analyze --offline=true security "$1" 2>&1 |awk '
+	/NAME.*DESCRIPTION.*EXPOSURE/ {suppress=1}
+	{if (!suppress) {print; failed++}}
+	END {exit failed}
+'
