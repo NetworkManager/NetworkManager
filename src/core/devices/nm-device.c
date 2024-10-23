f96707452172098f93fc22986bd1e102f07c0d96
Wen Liang <wenliang@redhat.com>
2024-10-23 16:22:37 -0400

    nm-device: add log indicating gateway ping started
    Since we are adding the ping check for the connection.ip-ping-addresses,
    it makes more sense to improve the logging to differentiate between the
    started ping operations for gateway and connection.ip-ping-addresses.

--- src/core/devices/nm-device.c
+++ src/core/devices/nm-device.c
@@ -15008,6 +15008,8 @@ nm_device_start_ip_check(NMDevice *self)
         const NMPObject      *gw;
         const NML3ConfigData *l3cd;
 
+        _LOGD(LOGD_DEVICE, "starting ping gateway...");
+
         l3cd = priv->l3cfg ? nm_l3cfg_get_combined_l3cd(priv->l3cfg, TRUE) : NULL;
         if (!l3cd) {
             /* pass */
