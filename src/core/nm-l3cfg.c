c6828a6e46bae219ba05538a0091d027055387f8
Íñigo Huguet <ihuguet@redhat.com>
2024-11-27 13:53:02 +0100

    l3cfg: remove routes added by NM on reapply
    By default, on reapply we were only syncing the main routes table. This
    causes that routes added by NM to other tables are not removed on
    reapply. This was done to preserve routes added externally, but routes
    added by NM itself should be removed.
    
    Add a new route table syncing mode "main + NM routes". This mode
    maintains the normal behaviour of syncing completely the main table,
    and for other tables removes only routes that were added by us, leaving
    the rest untouched. Use this mode by default, as this is what a user
    would expect on reapply.
    
    Note: this might not work if NM is restarted between the profile being
    modified and the reapply, because NM forgets what routes were added by
    itself because of the restart. This is a rare corner case, though.
    
    Use the D-Bus property "VersionInfo" to expose a capability flag
    indicating that this bug is fixed. It is the first capability that we
    expose in this way. However, it is convenient to do it this way as it's
    something that clients like nmstate needs to know, so they can decide
    whether a conn down is needed or not. It is not enough to decide that by
    version number because it might be fixed via a downstream patch in distros
    like RHEL.
    
    https://issues.redhat.com/browse/RHEL-67324
    https://issues.redhat.com/browse/RHEL-66262
    
    Fixes: e9c17fcc9b33 ('l3cfg: default to 'main' route table sync mode')
    (cherry picked from commit e330eb9c4a721d158641701cb48cd8094246d258)
    (cherry picked from commit 7f6e84b26eaaabcd046b5c547824fffcba706051)
    (cherry picked from commit 95064b80257e5d0b969e7c1899fe2b17ecb46e09)
    (cherry picked from commit 31575040620858662342713af188fab91421cb06)

--- src/core/nm-l3cfg.c
+++ src/core/nm-l3cfg.c
@@ -4995,7 +4995,7 @@ _l3_commit_one(NML3Cfg              *self,
     }
 
     if (route_table_sync == NM_IP_ROUTE_TABLE_SYNC_MODE_NONE)
-        route_table_sync = NM_IP_ROUTE_TABLE_SYNC_MODE_MAIN;
+        route_table_sync = NM_IP_ROUTE_TABLE_SYNC_MODE_MAIN_AND_NM_ROUTES;
 
     if (any_dirty)
         _obj_states_track_prune_dirty(self, TRUE);
@@ -5024,6 +5024,8 @@ _l3_commit_one(NML3Cfg              *self,
         }
 
         if (c_list_is_empty(&self->priv.p->blocked_lst_head_x[IS_IPv4])) {
+            gs_unref_ptrarray GPtrArray *routes_old = NULL;
+
             addresses_prune =
                 nm_platform_ip_address_get_prune_list(self->priv.platform,
                                                       addr_family,
@@ -5031,10 +5033,26 @@ _l3_commit_one(NML3Cfg              *self,
                                                       nm_g_array_data(ipv6_temp_addrs_keep),
                                                       nm_g_array_len(ipv6_temp_addrs_keep));
 
+            if (route_table_sync == NM_IP_ROUTE_TABLE_SYNC_MODE_MAIN_AND_NM_ROUTES) {
+                NMDedupMultiIter iter;
+                const NMPObject *rt_obj;
+
+                routes_old = g_ptr_array_new();
+                nm_l3_config_data_iter_obj_for_each (&iter,
+                                                     l3cd_old,
+                                                     &rt_obj,
+                                                     NMP_OBJECT_TYPE_IP_ROUTE(IS_IPv4))
+                    g_ptr_array_add(routes_old, (gpointer) rt_obj);
+
+                nm_platform_route_objs_sort(routes_old, NM_PLATFORM_IP_ROUTE_CMP_TYPE_SEMANTICALLY);
+            }
+
             routes_prune = nm_platform_ip_route_get_prune_list(self->priv.platform,
                                                                addr_family,
                                                                self->priv.ifindex,
-                                                               route_table_sync);
+                                                               route_table_sync,
+                                                               routes_old);
+
             _obj_state_zombie_lst_prune_all(self, addr_family);
         }
     } else {
