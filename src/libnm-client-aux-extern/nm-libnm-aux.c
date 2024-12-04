NetworkManager/NetworkManager

commit bc6098d441060c14cd816a0a62480d0dac698fae

libnm: add internal nmc_client_has_{version_info_v,version_info_capability,capability}() helper
In the end, it turned out I don't need them. They still seem useful,
because they show how to use this API. In particular for how the
bitfield should be parsed.

src/libnm-client-aux-extern/nm-libnm-aux.c
@@ -135,3 +135,64 @@ nmc_client_new_waitsync(GCancellable *cancellable,
     }
     return TRUE;
 }
+
+/*****************************************************************************/
+
+guint32
+nmc_client_has_version_info_v(NMClient *nmc)
+{
+    const guint32 *ver;
+    gsize          len;
+
+    ver = nm_client_get_version_info(nmc, &len);
+    if (len < 1)
+        return 0;
+    return ver[0];
+}
+
+gboolean
+nmc_client_has_version_info_capability(NMClient *nmc, NMVersionInfoCapability capability)
+{
+    const guint32 *ver;
+    gsize          len;
+    gsize          idx;
+    gsize          idx_hi;
+    gsize          idx_lo;
+
+    ver = nm_client_get_version_info(nmc, &len);
+
+    if (len < 2)
+        return FALSE;
+
+    len--;
+    ver++;
+
+    idx = (gsize) capability;
+    if (idx >= G_MAXSIZE - 31u)
+        return FALSE;
+
+    idx_hi = ((idx + 31u) / 32u);
+    idx_lo = (idx % 32u);
+
+    if (idx_hi > len)
+        return FALSE;
+
+    return NM_FLAGS_ANY(ver[idx_hi], (1ull << idx_lo));
+}
+
+gboolean
+nmc_client_has_capability(NMClient *nmc, NMCapability capability)
+{
+    const guint32 *caps;
+    gsize          len;
+    gsize          i;
+
+    caps = nm_client_get_capabilities(nmc, &len);
+
+    for (i = 0; i < len; i++) {
+        if (caps[i] == capability)
+            return TRUE;
+    }
+
+    return FALSE;
+}

src/libnm-client-aux-extern/nm-libnm-aux.h
@@ -21,4 +21,8 @@ gboolean nmc_client_new_waitsync(GCancellable *cancellable,
                                  const char   *first_property_name,
                                  ...);
 
+guint32  nmc_client_has_version_info_v(NMClient *nmc);
+gboolean nmc_client_has_version_info_capability(NMClient *nmc, NMVersionInfoCapability capability);
+gboolean nmc_client_has_capability(NMClient *nmc, NMCapability capability);
+
 #endif /* __NM_LIBNM_AUX_H__ */

commit 1dc78f80f92700ef7803ea64de8cede2b20be9c6

libnmc: fix bug checking VersionInfo's capabilities
Remove the `+ 31u` that was making that it would search for bit 1 at
array's element 1, instead of element 0. Fixed comparison >len that
shoudl be >=len. Fix a few typos.

Fixes: bc6098d44106 ('libnm: add internal nmc_client_has_{version_info_v,version_info_capability,capability}() helper')

src/libnm-client-aux-extern/nm-libnm-aux.c
@@ -169,14 +169,11 @@ nmc_client_has_version_info_capability(NMClient *nmc, NMVersionInfoCapability ca
     len--;
     ver++;
 
-    idx = (gsize) capability;
-    if (idx >= G_MAXSIZE - 31u)
-        return FALSE;
+    idx    = (gsize) capability;
+    idx_hi = idx / 32u;
+    idx_lo = idx % 32u;
 
-    idx_hi = ((idx + 31u) / 32u);
-    idx_lo = (idx % 32u);
-
-    if (idx_hi > len)
+    if (idx_hi >= len)
         return FALSE;
 
     return NM_FLAGS_ANY(ver[idx_hi], (1ull << idx_lo));

src/libnm-client-impl/nm-client.c
@@ -6316,7 +6316,7 @@ nm_client_get_capabilities(NMClient *client, gsize *length)
  *
  * If available, the first element in the array is NM_VERSION which
  * encodes the daemon version as "(major << 16 | minor << 8 | micro)".
- * The following elements are a bitfield of %NMVersionInfoCapabilities
+ * The following elements are a bitfield of %NMVersionInfoCapability
  * that indicate that the daemon supports a certain capability.
  *
  * Returns: (transfer none) (array length=length): the
@@ -8313,7 +8313,7 @@ nm_client_class_init(NMClientClass *client_class)
      * Expose version info and capabilities of NetworkManager. If non-empty,
      * the first element is NM_VERSION, which encodes the version of the
      * daemon as "(major << 16 | minor << 8 | micro)". The following elements
-     * is a bitfields of %NMVersionInfoCapabilities. If a bit is set, then
+     * is a bitfields of %NMVersionInfoCapability. If a bit is set, then
      * the running NetworkManager has the respective capability.
      *
      * Since: 1.42