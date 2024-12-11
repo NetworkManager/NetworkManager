b92a07713c17eb55fb3f0cfa4c757e379c432e17
Gris Ge <fge@redhat.com>
2024-12-11 22:22:59 +0800

    vpn: Place gateway route to table defined in ipvx.route-table
    Previously, NM create direct route to gateway to main(254) route table
    regardless `ipvx.route-table` value.
    
    Fixed by setting `NMPlatformIP4Route.table_any` to `TRUE`.
    
    Resolves: https://issues.redhat.com/browse/RHEL-69901
    
    Signed-off-by: Gris Ge <fge@redhat.com>
    (cherry picked from commit 6d06286f1db7421bef1c4dab5fada918c59daf87)
    (cherry picked from commit 29f23d3519dbb4dcffc9682fbdfb721cfc0b851c)
    (cherry picked from commit 0dc07c5ca4d32b5ea8e104cbad106da9bb5b096d)
    (cherry picked from commit 6a04a966c28dbe04e3bd608af06a66cf0af89d21)
    (cherry picked from commit 70060d84f268250fd0bead2928eba8739e3eb486)

--- src/core/vpn/nm-vpn-connection.c
+++ src/core/vpn/nm-vpn-connection.c
@@ -1241,6 +1241,7 @@ _parent_device_l3cd_add_gateway_route(NML3ConfigData *l3cd,
             .gateway    = parent_gw.addr4,
             .rt_source  = NM_IP_CONFIG_SOURCE_VPN,
             .metric_any = TRUE,
+            .table_any  = TRUE,
         };
     } else {
         route.r6 = (NMPlatformIP6Route){
@@ -1250,6 +1251,7 @@ _parent_device_l3cd_add_gateway_route(NML3ConfigData *l3cd,
             .gateway    = parent_gw.addr6,
             .rt_source  = NM_IP_CONFIG_SOURCE_VPN,
             .metric_any = TRUE,
+            .table_any  = TRUE,
         };
     }
     nm_l3_config_data_add_route(l3cd, addr_family, NULL, &route.rx);
@@ -1266,6 +1268,7 @@ _parent_device_l3cd_add_gateway_route(NML3ConfigData *l3cd,
                 .plen       = 32,
                 .rt_source  = NM_IP_CONFIG_SOURCE_VPN,
                 .metric_any = TRUE,
+                .table_any  = TRUE,
             };
         } else {
             route.r6 = (NMPlatformIP6Route){
@@ -1273,6 +1276,7 @@ _parent_device_l3cd_add_gateway_route(NML3ConfigData *l3cd,
                 .plen       = 128,
                 .rt_source  = NM_IP_CONFIG_SOURCE_VPN,
                 .metric_any = TRUE,
+                .table_any  = TRUE,
             };
         }
         nm_l3_config_data_add_route(l3cd, addr_family, NULL, &route.rx);
