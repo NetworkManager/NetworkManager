6ea03c8476e63e52e0d04f77be3a47b1af08259d
Beniamino Galvani <bgalvani@redhat.com>
2025-01-20 13:50:11 +0100

    libnm-glib-aux: fix build error with GCC 15
    Fix the following build error with GCC 15:
    
      ../src/libnm-glib-aux/nm-shared-utils.c:35:42: error: initializer-string for array of 'char' is too long [-Werror=unterminated-string-initialization]
         35 | const char _nm_hexchar_table_lower[16] = "0123456789abcdef";
            |                                          ^~~~~~~~~~~~~~~~~~
      ../src/libnm-glib-aux/nm-shared-utils.c:36:42: error: initializer-string for array of 'char' is too long [-Werror=unterminated-string-initialization]
         36 | const char _nm_hexchar_table_upper[16] = "0123456789ABCDEF";
            |                                          ^~~~~~~~~~~~~~~~~~
    
    Fixes: 069be33fbdc0 ('shared: add nm_hexchar() helper')

--- src/libnm-glib-aux/nm-shared-utils.c
+++ src/libnm-glib-aux/nm-shared-utils.c
@@ -32,8 +32,10 @@ G_STATIC_ASSERT(G_STRUCT_OFFSET(NMUtilsNamedValue, value_ptr) == sizeof(const ch
 
 /*****************************************************************************/
 
-const char _nm_hexchar_table_lower[16] = "0123456789abcdef";
-const char _nm_hexchar_table_upper[16] = "0123456789ABCDEF";
+const char _nm_hexchar_table_lower[16] =
+    {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+const char _nm_hexchar_table_upper[16] =
+    {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 const void *const _NM_PTRARRAY_EMPTY[1] = {NULL};
 
