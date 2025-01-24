NetworkManager/NetworkManager

commit cf6af54ffa4f637f62b1ad395566167ffd89d9d4

cloud-setup: allow VETH along with ETHERNET too
Pairs of veth devides are used for CI testing in place of real
ethernets. Use GLib types instead of NM numbers, since it's possible to
match them in hierarchical manner, with NMDeviceVeth being a subclass of
NMDeviceEthernet.

Fixes: 6ff4b9e57c ('cloud-setup: create VLANs for multiple VNICs on OCI')

src/nm-cloud-setup/main.c
@@ -295,7 +295,7 @@ _nmc_get_ethernet_hwaddrs(NMClient *nmc)
 }
 
 static NMDevice *
-_nmc_get_device_by_hwaddr(NMClient *nmc, NMDeviceType device_type, const char *hwaddr)
+_nmc_get_device_by_hwaddr(NMClient *nmc, const GType type_device, const char *hwaddr)
 {
     const GPtrArray *devices;
     guint            i;
@@ -307,7 +307,7 @@ _nmc_get_device_by_hwaddr(NMClient *nmc, NMDeviceType device_type, const char *h
         const char   *hwaddr_dev;
         gs_free char *s = NULL;
 
-        if (nm_device_get_device_type(device) != device_type)
+        if (!G_TYPE_CHECK_INSTANCE_TYPE(device, type_device))
             continue;
 
         hwaddr_dev = _device_get_hwaddr(device);
@@ -712,7 +712,7 @@ _config_ethernet(SigTermData                          *sigterm_data,
     gs_unref_object NMDevice *device = NULL;
 
     device = nm_g_object_ref(
-        _nmc_get_device_by_hwaddr(nmc, NM_DEVICE_TYPE_ETHERNET, config_data->hwaddr));
+        _nmc_get_device_by_hwaddr(nmc, NM_TYPE_DEVICE_ETHERNET, config_data->hwaddr));
     if (!device) {
         _LOGD("config device %s: skip because device not found", config_data->hwaddr);
         return FALSE;
@@ -895,14 +895,14 @@ _config_one(SigTermData                       *sigterm_data,
                                            config_data,
                                            nmc,
                                            result,
-                                           NM_DEVICE_TYPE_MACVLAN,
+                                           NM_TYPE_DEVICE_MACVLAN,
                                            NM_SETTING_MACVLAN_SETTING_NAME,
                                            config_data->priv.oci.parent_hwaddr);
         any_changes += _oci_config_vnic_dev(sigterm_data,
                                             config_data,
                                             nmc,
                                             result,
-                                            NM_DEVICE_TYPE_VLAN,
+                                            NM_TYPE_DEVICE_VLAN,
                                             NM_SETTING_VLAN_SETTING_NAME,
                                             config_data->hwaddr);
 

commit 6b8d7d54853fb9ecfd85d6af6e6ba9a1c4b1b167

cloud-setup: avoid accidental cast into a smaller type
This resulted in what looked like more significant bits of GType pointer
sometimes falling off the cliff, presumably because of a cast to
NMDeviceType enum (that probably ends up actually being a char).

This was silent enough to not emit compiler warnings and only occurring
with some very rare situations (needs GCC with LTO and some of the
optimization flags used by Fedora 41).

Fixes: cf6af54ffa ('cloud-setup: allow VETH along with ETHERNET too')
Fixes: 6ff4b9e57c ('cloud-setup: create VLANs for multiple VNICs on OCI')

src/nm-cloud-setup/main.c
@@ -833,7 +833,7 @@ _oci_config_vnic_dev(SigTermData                          *sigterm_data,
                      const NMCSProviderGetConfigIfaceData *config_data,
                      NMClient                             *nmc,
                      const NMCSProviderGetConfigResult    *result,
-                     NMDeviceType                          device_type,
+                     const GType                           device_type,
                      const char                           *connection_type,
                      const char                           *parent_hwaddr)
 {