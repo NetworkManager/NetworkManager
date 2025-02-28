NetworkManager/NetworkManager

commit 5409d4f5cd4088e40ee5b7077ffca7380fede731

fedora/REQUIRED_PACKAGES: speed up optional packages installation
Don't try to install one by one by calling dnf repeatedly.
It is slow. Instead, use --skip-broken option.

contrib/fedora/REQUIRED_PACKAGES
@@ -25,12 +25,6 @@ install() {
     fi
 }
 
-install_ignore_missing() {
-    for p; do
-        install "$p" || :
-    done
-}
-
 if test "$NM_NO_EXTRA" != 1; then
     # these packages are convenient for developing, but not necessary
     # for CI testing.
@@ -90,8 +84,9 @@ install \
     which \
     #end
 
-# some packages don't exist in certain distributions. Install them one-by-one, and ignore errors.
-install_ignore_missing \
+# Some packages don't exist in certain distributions. Ignore errors
+# installing them, but still drag them in when available.
+install --skip-unavailable \
     black \
     dbus-python \
     iproute-tc \

commit c6093e1dab2a84ae32ce938e0fc64306e143360e

fedora/REQUIRED_PACKAGES: use dnf, not yum
Dnf is available since many years ago, let's always use it. Moreover, we
are using now --skip-unavailable option, which yum doesn't have, so it
fails.

In the CI we were still using yum because we were passing NM_INSTALL=yum
from fedora-install.sh, causing the CI to fail. Change it.

Fixes: 5409d4f5cd40 ('fedora/REQUIRED_PACKAGES: speed up optional packages installation')

.gitlab-ci/fedora-install.sh
@@ -39,12 +39,12 @@ fi
 fi
 
 
-NM_NO_EXTRA=1 NM_INSTALL="yum install -y" ./contrib/fedora/REQUIRED_PACKAGES
-yum install -y glibc-langpack-pl ccache clang
+NM_NO_EXTRA=1 ./contrib/fedora/REQUIRED_PACKAGES
+dnf install -y glibc-langpack-pl ccache clang
 
 # containers have "tsflags=nodocs" in /etc/dnf/dnf.conf. We need /usr/shared/gtk-doc/html
 # to generate proper documentation.
-yum reinstall -y --setopt='tsflags=' glib2-doc
+dnf reinstall -y --setopt='tsflags=' glib2-doc
 
 if [ $IS_FEDORA = 1 ]; then
     FEDORA_VERSION=$(cat /etc/os-release | grep '^VERSION_ID=' | sed s\/"VERSION_ID="\/\/)

contrib/fedora/REQUIRED_PACKAGES
@@ -4,15 +4,12 @@
 # NetworkManager on Fedora and RHEL.
 #
 # Not all of these packages are available, depending
-# on your distribution/release. But yum will happily
-# skip them.
+# on your distribution/release. But the script will skip them.
 #
 # Not all of these packages are strictly speaking necessary.
 # This is a generous list of related packages.
 
 set -xe
-
-DNF="$(command -v dnf &>/dev/null && echo dnf || echo yum)"
 
 SUDO=
 [ "$EUID" -eq 0 ] || SUDO=sudo
@@ -21,7 +18,7 @@ install() {
     if [ "$NM_INSTALL" != "" ]; then
         $NM_INSTALL "$@"
     else
-        $SUDO "$DNF" install -y "$@"
+        $SUDO dnf install -y "$@"
     fi
 }
 