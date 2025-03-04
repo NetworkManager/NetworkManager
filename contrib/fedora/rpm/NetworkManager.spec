NetworkManager/NetworkManager

commit 91e36ea21493c0a7db692298c7bbf5e71309d843

spec: don't install *-initrd.services
They cannot be used yet as Dracut need changes. They are not really
tested, neither, so better not to install them as they might cause
confusion about their purpose.

contrib/fedora/rpm/NetworkManager.spec
@@ -43,7 +43,7 @@
 
 %global real_version_major %(printf '%s' '%{real_version}' | sed -n 's/^\\([1-9][0-9]*\\.[0-9][0-9]*\\)\\.[0-9][0-9]*$/\\1/p')
 
-%global systemd_units NetworkManager.service NetworkManager-wait-online.service NetworkManager-dispatcher.service nm-priv-helper.service NetworkManager-config-initrd.service NetworkManager-initrd.service NetworkManager-wait-online-initrd.service
+%global systemd_units NetworkManager.service NetworkManager-wait-online.service NetworkManager-dispatcher.service nm-priv-helper.service
 
 %global systemd_units_cloud_setup nm-cloud-setup.service nm-cloud-setup.timer
 
@@ -760,6 +760,11 @@ rm -f %{buildroot}%{_libdir}/*.la
 rm -f %{buildroot}%{_libdir}/pppd/%{ppp_version}/*.la
 rm -f %{buildroot}%{nmplugindir}/*.la
 
+# Don't use the *-initrd.service files yet, wait dracut to support them
+rm -f %{_unitdir}/NetworkManager-config-initrd.service
+rm -f %{_unitdir}/NetworkManager-initrd.service
+rm -f %{_unitdir}/NetworkManager-wait-online-initrd.service
+
 # Ensure the documentation timestamps are constant to avoid multilib conflicts
 find %{buildroot}%{_datadir}/gtk-doc -exec touch --reference meson.build '{}' \+
 
@@ -829,7 +834,7 @@ if [ $1 -eq 0 ]; then
     # Don't kill networking entirely just on package remove
     #/bin/systemctl stop NetworkManager.service >/dev/null 2>&1 || :
 fi
-%systemd_preun NetworkManager-wait-online.service NetworkManager-dispatcher.service nm-priv-helper.service NetworkManager-config-initrd.service NetworkManager-initrd.service NetworkManager-wait-online-initrd.service
+%systemd_preun NetworkManager-wait-online.service NetworkManager-dispatcher.service nm-priv-helper.service
 
 
 %if %{with ifcfg_rh}
@@ -927,9 +932,6 @@ fi
 %{_unitdir}/NetworkManager-wait-online.service
 %{_unitdir}/NetworkManager-dispatcher.service
 %{_unitdir}/nm-priv-helper.service
-%{_unitdir}/NetworkManager-config-initrd.service
-%{_unitdir}/NetworkManager-initrd.service
-%{_unitdir}/NetworkManager-wait-online-initrd.service
 %dir %{_datadir}/doc/NetworkManager/examples
 %{_datadir}/doc/NetworkManager/examples/server.conf
 %if %{with ifcfg_warning} || %{with ifcfg_migrate}

commit a60ca31fd9f9b9002b17291dacc7582ac46d907d

spec: specify paths to *-initrd.service files from the buildroot
Fixes: 91e36ea21493 ('spec: don't install *-initrd.services')

contrib/fedora/rpm/NetworkManager.spec
@@ -761,9 +761,9 @@ rm -f %{buildroot}%{_libdir}/pppd/%{ppp_version}/*.la
 rm -f %{buildroot}%{nmplugindir}/*.la
 
 # Don't use the *-initrd.service files yet, wait dracut to support them
-rm -f %{_unitdir}/NetworkManager-config-initrd.service
-rm -f %{_unitdir}/NetworkManager-initrd.service
-rm -f %{_unitdir}/NetworkManager-wait-online-initrd.service
+rm -f %{buildroot}%{_unitdir}/NetworkManager-config-initrd.service
+rm -f %{buildroot}%{_unitdir}/NetworkManager-initrd.service
+rm -f %{buildroot}%{_unitdir}/NetworkManager-wait-online-initrd.service
 
 # Ensure the documentation timestamps are constant to avoid multilib conflicts
 find %{buildroot}%{_datadir}/gtk-doc -exec touch --reference meson.build '{}' \+