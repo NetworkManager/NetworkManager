325ddf458d10b682226698cd5f03f4dd1d741562
Beniamino Galvani <bgalvani@redhat.com>
2025-03-24 21:16:47 +0100

    platform: replace EEE ethtool ioctl calls with netlink

--- src/libnm-platform/nmp-ethtool.h
+++ src/libnm-platform/nmp-ethtool.h
@@ -14,4 +14,13 @@ gboolean nmp_ethtool_set_pause(struct nl_sock            *genl_sock,
                                int                        ifindex,
                                const NMEthtoolPauseState *pause);
 
+gboolean nmp_ethtool_get_eee(struct nl_sock    *genl_sock,
+                             guint16            family_id,
+                             int                ifindex,
+                             NMEthtoolEEEState *eee);
+gboolean nmp_ethtool_set_eee(struct nl_sock          *genl_sock,
+                             guint16                  family_id,
+                             int                      ifindex,
+                             const NMEthtoolEEEState *eee);
+
 #endif /* __NMP_ETHTOOL_H__ */
