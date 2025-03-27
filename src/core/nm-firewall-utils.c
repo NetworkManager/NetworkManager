NetworkManager/NetworkManager

commit 4badc1f33acefc4d9ae5c3ca6e80f088da682001

firewall: fix signalling timeout error reason from _fw_nft_call()
During timeout we cancel the (internal) GCancellable. But the overall
error reason is not cancellation by the user, it's timeout. Fix
the error reason.

Fixes: 9ebdb967ded7 ('firewall: implement masquerading for shared mode with nftables')
(cherry picked from commit 897c6a5744b38adee2d6cf1e580e78220aad5215)

src/core/nm-firewall-utils.c
@@ -400,15 +400,21 @@ _fw_nft_call_communicate_cb(GObject *source, GAsyncResult *result, gpointer user
                                          &error)) {
         /* on any error, the process might still be running. We need to abort it in
          * the background... */
-        if (nm_utils_error_is_cancelled(error)) {
-            nm_log_dbg(LOGD_SHARING,
-                       "firewall: ntf[%s]: communication cancelled. Kill process",
-                       call_data->identifier);
-        } else {
+        if (!nm_utils_error_is_cancelled(error)) {
             nm_log_dbg(LOGD_SHARING,
                        "firewall: nft[%s]: communication failed: %s. Kill process",
                        call_data->identifier,
                        error->message);
+        } else if (!call_data->timeout_source) {
+            nm_log_dbg(LOGD_SHARING,
+                       "firewall: ntf[%s]: communication timed out. Kill process",
+                       call_data->identifier);
+            nm_clear_error(&error);
+            nm_utils_error_set(&error, NM_UTILS_ERROR_UNKNOWN, "timeout communicating with nft");
+        } else {
+            nm_log_dbg(LOGD_SHARING,
+                       "firewall: ntf[%s]: communication cancelled. Kill process",
+                       call_data->identifier);
         }
 
         {

commit 7968fcb7e6bcbfe8d60441a1f27a9ec2aa637321

firewall/utils: fix ntf -> nft typo
Fixes: 4badc1f33ace ('firewall: fix signalling timeout error reason from _fw_nft_call()')

src/core/nm-firewall-utils.c
@@ -466,13 +466,13 @@ _fw_nft_call_communicate_cb(GObject *source, GAsyncResult *result, gpointer user
                        error->message);
         } else if (!call_data->timeout_source) {
             nm_log_dbg(LOGD_SHARING,
-                       "firewall: ntf[%s]: communication timed out. Kill process",
+                       "firewall: nft[%s]: communication timed out. Kill process",
                        call_data->identifier);
             nm_clear_error(&error);
             nm_utils_error_set(&error, NM_UTILS_ERROR_UNKNOWN, "timeout communicating with nft");
         } else {
             nm_log_dbg(LOGD_SHARING,
-                       "firewall: ntf[%s]: communication cancelled. Kill process",
+                       "firewall: nft[%s]: communication cancelled. Kill process",
                        call_data->identifier);
         }
 