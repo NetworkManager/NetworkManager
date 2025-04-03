NetworkManager/NetworkManager

commit 19bed3121fb652dbe56a84780937edada3a7ea3f

ethtool: support Forward Error Correction(fec)
Introducing support of ethtool FEC mode:

D-BUS API: `fec-mode: uint32_t`.
Keyfile:

```
[ethtool]
fec-mode=<uint32_t>
```

nmcli: `ethtool.fec-mode` allowing values are any combination of:
 * auto
 * off
 * rs
 * baser
 * llrs

Unit test cases included.

Resolves: https://issues.redhat.com/browse/RHEL-24055

Signed-off-by: Gris Ge <fge@redhat.com>

src/core/devices/nm-device.c
@@ -315,6 +315,7 @@ typedef struct {
     NMEthtoolPauseState    *pause;
     NMEthtoolChannelsState *channels;
     NMEthtoolEEEState      *eee;
+    uint32_t                fec_mode;
 } EthtoolState;
 
 typedef enum {
@@ -2682,6 +2683,19 @@ _ethtool_features_reset(NMDevice *self, NMPlatform *platform, EthtoolState *etht
 }
 
 static void
+_ethtool_fec_reset(NMDevice *self, NMPlatform *platform, EthtoolState *ethtool_state)
+{
+    if (ethtool_state->fec_mode) {
+        if (!nm_platform_ethtool_set_fec_mode(platform,
+                                              ethtool_state->ifindex,
+                                              ethtool_state->fec_mode))
+            _LOGW(LOGD_DEVICE, "ethtool: failure resetting FEC");
+        else
+            _LOGD(LOGD_DEVICE, "ethtool: FEC successfully reset");
+    }
+}
+
+static void
 _ethtool_features_set(NMDevice         *self,
                       NMPlatform       *platform,
                       EthtoolState     *ethtool_state,
@@ -2710,6 +2724,55 @@ _ethtool_features_set(NMDevice         *self,
         _LOGD(LOGD_DEVICE, "ethtool: offload features successfully set");
 
     ethtool_state->features = g_steal_pointer(&features);
+}
+
+static void
+_ethtool_fec_set(NMDevice         *self,
+                 NMPlatform       *platform,
+                 EthtoolState     *ethtool_state,
+                 NMSettingEthtool *s_ethtool)
+{
+    uint32_t       old_fec_mode;
+    uint32_t       fec_mode = NM_SETTING_ETHTOOL_FEC_MODE_NONE;
+    GHashTable    *hash;
+    GHashTableIter iter;
+    const char    *name;
+    GVariant      *variant;
+
+    nm_assert(NM_IS_DEVICE(self));
+    nm_assert(NM_IS_PLATFORM(platform));
+    nm_assert(NM_IS_SETTING_ETHTOOL(s_ethtool));
+    nm_assert(ethtool_state);
+    nm_assert(!ethtool_state->fec_mode);
+
+    hash = _nm_setting_option_hash(NM_SETTING(s_ethtool), FALSE);
+    if (!hash)
+        return;
+
+    g_hash_table_iter_init(&iter, hash);
+    while (g_hash_table_iter_next(&iter, (gpointer *) &name, (gpointer *) &variant)) {
+        NMEthtoolID ethtool_id = nm_ethtool_id_get_by_name(name);
+
+        if (!nm_ethtool_id_is_fec(ethtool_id))
+            continue;
+
+        nm_assert(g_variant_is_of_type(variant, G_VARIANT_TYPE_UINT32));
+        fec_mode = g_variant_get_uint32(variant);
+    }
+
+    nm_platform_ethtool_get_fec_mode(platform, ethtool_state->ifindex, &old_fec_mode);
+
+    /* The NM_SETTING_ETHTOOL_FEC_MODE_NONE is query only value, hence do nothing. */
+    if (!fec_mode || fec_mode == NM_SETTING_ETHTOOL_FEC_MODE_NONE) {
+        return;
+    }
+
+    if (!nm_platform_ethtool_set_fec_mode(platform, ethtool_state->ifindex, fec_mode))
+        _LOGW(LOGD_DEVICE, "ethtool: failure setting FEC %d", fec_mode);
+    else {
+        _LOGD(LOGD_DEVICE, "ethtool: FEC %d successfully set", fec_mode);
+        ethtool_state->fec_mode = old_fec_mode;
+    }
 }
 
 static void
@@ -3193,6 +3256,7 @@ _ethtool_state_reset(NMDevice *self)
     _ethtool_pause_reset(self, platform, ethtool_state);
     _ethtool_channels_reset(self, platform, ethtool_state);
     _ethtool_eee_reset(self, platform, ethtool_state);
+    _ethtool_fec_reset(self, platform, ethtool_state);
 }
 
 static void
@@ -3229,9 +3293,11 @@ _ethtool_state_set(NMDevice *self)
     _ethtool_pause_set(self, platform, ethtool_state, s_ethtool);
     _ethtool_channels_set(self, platform, ethtool_state, s_ethtool);
     _ethtool_eee_set(self, platform, ethtool_state, s_ethtool);
+    _ethtool_fec_set(self, platform, ethtool_state, s_ethtool);
 
     if (ethtool_state->features || ethtool_state->coalesce || ethtool_state->ring
-        || ethtool_state->pause || ethtool_state->channels || ethtool_state->eee)
+        || ethtool_state->pause || ethtool_state->channels || ethtool_state->eee
+        || ethtool_state->fec_mode != 0)
         priv->ethtool_state = g_steal_pointer(&ethtool_state);
 }
 
@@ -8625,7 +8691,7 @@ port_state_changed(NMDevice           *port,
 /**
  * nm_device_controller_add_port:
  * @self: the controller device
- * @port: the port device to attach as port 
+ * @port: the port device to attach as port
  * @configure: pass %TRUE if the port should be configured by the controller, or
  * %FALSE if it is already configured outside NetworkManager
  *

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-writer.c
@@ -1454,6 +1454,15 @@ write_ethtool_setting(NMConnection *connection, shvarFile *ifcfg, GError **error
                 return FALSE;
             }
         }
+        if (ethtool_id == NM_ETHTOOL_ID_FEC_MODE) {
+            if (nm_setting_option_get_uint32(NM_SETTING(s_ethtool),
+                                             nm_ethtool_data[ethtool_id]->optname,
+                                             &u32)) {
+                nm_sprintf_buf(prop_name, "ethtool.%s", nm_ethtool_data[ethtool_id]->optname);
+                set_error_unsupported(error, connection, prop_name, FALSE);
+                return FALSE;
+            }
+        }
 
         if (!any_option) {
             /* Write an empty dummy "-A" option without arguments. This is to

src/core/settings/plugins/ifcfg-rh/tests/test-ifcfg-rh.c
@@ -3623,7 +3623,8 @@ test_roundtrip_ethtool(void)
             optname = nm_ethtool_data[ethtool_id]->optname;
             vtype   = nm_ethtool_id_get_variant_type(ethtool_id);
 
-            if (nm_ethtool_optname_is_channels(optname) || nm_ethtool_optname_is_eee(optname)) {
+            if (nm_ethtool_optname_is_channels(optname) || nm_ethtool_optname_is_eee(optname)
+                || nm_ethtool_optname_is_fec(optname)) {
                 /* Not supported */
                 continue;
             }

src/libnm-base/nm-base.h
@@ -135,7 +135,11 @@ typedef enum {
     NM_ETHTOOL_ID_CHANNELS_COMBINED,
     _NM_ETHTOOL_ID_CHANNELS_LAST = NM_ETHTOOL_ID_CHANNELS_COMBINED,
 
-    _NM_ETHTOOL_ID_LAST = _NM_ETHTOOL_ID_CHANNELS_LAST,
+    _NM_ETHTOOL_ID_FEC_FIRST = _NM_ETHTOOL_ID_CHANNELS_LAST + 1,
+    NM_ETHTOOL_ID_FEC_MODE   = _NM_ETHTOOL_ID_FEC_FIRST,
+    _NM_ETHTOOL_ID_FEC_LAST  = NM_ETHTOOL_ID_FEC_MODE,
+
+    _NM_ETHTOOL_ID_LAST = _NM_ETHTOOL_ID_FEC_LAST,
 
     _NM_ETHTOOL_ID_COALESCE_NUM =
         (_NM_ETHTOOL_ID_COALESCE_LAST - _NM_ETHTOOL_ID_COALESCE_FIRST + 1),
@@ -158,6 +162,7 @@ typedef enum {
     NM_ETHTOOL_TYPE_PAUSE,
     NM_ETHTOOL_TYPE_CHANNELS,
     NM_ETHTOOL_TYPE_EEE,
+    NM_ETHTOOL_TYPE_FEC,
 } NMEthtoolType;
 
 /****************************************************************************/
@@ -196,6 +201,12 @@ static inline gboolean
 nm_ethtool_id_is_eee(NMEthtoolID id)
 {
     return id >= _NM_ETHTOOL_ID_EEE_FIRST && id <= _NM_ETHTOOL_ID_EEE_LAST;
+}
+
+static inline gboolean
+nm_ethtool_id_is_fec(NMEthtoolID id)
+{
+    return id >= _NM_ETHTOOL_ID_FEC_FIRST && id <= _NM_ETHTOOL_ID_FEC_LAST;
 }
 
 /*****************************************************************************/

src/libnm-base/nm-ethtool-base.c
@@ -111,6 +111,7 @@ const NMEthtoolData *const nm_ethtool_data[_NM_ETHTOOL_ID_NUM + 1] = {
     ETHT_DATA(CHANNELS_TX),
     ETHT_DATA(CHANNELS_OTHER),
     ETHT_DATA(CHANNELS_COMBINED),
+    ETHT_DATA(FEC_MODE),
     [_NM_ETHTOOL_ID_NUM] = NULL,
 };
 
@@ -201,6 +202,7 @@ static const guint8 _by_name[_NM_ETHTOOL_ID_NUM] = {
     NM_ETHTOOL_ID_FEATURE_TX_UDP_TNL_SEGMENTATION,
     NM_ETHTOOL_ID_FEATURE_TX_VLAN_STAG_HW_INSERT,
     NM_ETHTOOL_ID_FEATURE_TXVLAN,
+    NM_ETHTOOL_ID_FEC_MODE,
     NM_ETHTOOL_ID_PAUSE_AUTONEG,
     NM_ETHTOOL_ID_PAUSE_RX,
     NM_ETHTOOL_ID_PAUSE_TX,
@@ -305,6 +307,8 @@ nm_ethtool_id_to_type(NMEthtoolID id)
         return NM_ETHTOOL_TYPE_CHANNELS;
     if (nm_ethtool_id_is_eee(id))
         return NM_ETHTOOL_TYPE_EEE;
+    if (nm_ethtool_id_is_fec(id))
+        return NM_ETHTOOL_TYPE_FEC;
 
     return NM_ETHTOOL_TYPE_UNKNOWN;
 }
@@ -319,6 +323,7 @@ nm_ethtool_id_get_variant_type(NMEthtoolID ethtool_id)
         return G_VARIANT_TYPE_BOOLEAN;
     case NM_ETHTOOL_TYPE_CHANNELS:
     case NM_ETHTOOL_TYPE_COALESCE:
+    case NM_ETHTOOL_TYPE_FEC:
     case NM_ETHTOOL_TYPE_RING:
         return G_VARIANT_TYPE_UINT32;
     case NM_ETHTOOL_TYPE_UNKNOWN:

src/libnm-base/nm-ethtool-utils-base.h
@@ -109,6 +109,8 @@ G_BEGIN_DECLS
 #define NM_ETHTOOL_OPTNAME_CHANNELS_OTHER    "channels-other"
 #define NM_ETHTOOL_OPTNAME_CHANNELS_COMBINED "channels-combined"
 
+#define NM_ETHTOOL_OPTNAME_FEC_MODE "fec-mode"
+
 #define NM_ETHTOOL_OPTNAME_EEE_ENABLED "eee-enabled"
 
 /*****************************************************************************/

src/libnm-client-impl/libnm.ver
@@ -2043,4 +2043,6 @@ global:
 	nm_setting_connection_clear_ip_ping_addresses;
 	nm_setting_connection_remove_ip_ping_address_by_value;
 	nm_setting_connection_get_ip_ping_addresses_require_all;
+	nm_setting_ethtool_fec_mode_get_type;
+	nm_ethtool_optname_is_fec;
 } libnm_1_50_0;

src/libnm-client-public/nm-ethtool-utils.h
@@ -109,6 +109,8 @@ G_BEGIN_DECLS
 #define NM_ETHTOOL_OPTNAME_CHANNELS_OTHER    "channels-other"
 #define NM_ETHTOOL_OPTNAME_CHANNELS_COMBINED "channels-combined"
 
+#define NM_ETHTOOL_OPTNAME_FEC_MODE "fec-mode"
+
 #define NM_ETHTOOL_OPTNAME_EEE_ENABLED "eee-enabled"
 
 /*****************************************************************************/

commit 245f0e0b35d385e966289080dbd2594e74a189b2

core: optimize hash table search in _ethtool_fec_set
Break the loop as soon as we've found the value.

Fixes: 19bed3121fb6 ('ethtool: support Forward Error Correction(fec)')

src/core/devices/nm-device.c
@@ -2788,13 +2788,11 @@ _ethtool_fec_set(NMDevice         *self,
 
     g_hash_table_iter_init(&iter, hash);
     while (g_hash_table_iter_next(&iter, (gpointer *) &name, (gpointer *) &variant)) {
-        NMEthtoolID ethtool_id = nm_ethtool_id_get_by_name(name);
-
-        if (!nm_ethtool_id_is_fec(ethtool_id))
-            continue;
-
-        nm_assert(g_variant_is_of_type(variant, G_VARIANT_TYPE_UINT32));
-        fec_mode = g_variant_get_uint32(variant);
+        if (nm_ethtool_id_is_fec(nm_ethtool_id_get_by_name(name))) {
+            nm_assert(g_variant_is_of_type(variant, G_VARIANT_TYPE_UINT32));
+            fec_mode = g_variant_get_uint32(variant);
+            break;
+        }
     }
 
     /* The NM_SETTING_ETHTOOL_FEC_MODE_NONE is query only value, hence do nothing. */