ca41810be1e4152715335e253b61950538c6a2c8
Íñigo Huguet <ihuguet@redhat.com>
2025-04-30 12:21:23 +0200

    Revert "policy: refresh IPv4 forwarding after connection activation and disconnection"
    This reverts commit 2ad5fbf0253d6f915a0e8815ca70ba4d9ec41648.
    
    It is actually a partial revert. The changes to documentation don't need
    to be reverted.
    
    Fixes: 2ad5fbf0253d ('policy: refresh IPv4 forwarding after connection activation and disconnection')

--- src/core/devices/nm-device.h
+++ src/core/devices/nm-device.h
@@ -852,14 +852,4 @@ void nm_routing_rules_sync(NMConnection *applied_connection,
                            NMDevice *self,
                            NMNetns  *netns);
 
-NMSettingIPConfigForwarding nm_device_get_ipv4_forwarding(NMDevice *self);
-
-const char *nm_device_get_effective_ip_config_method(NMDevice *self, int addr_family);
-
-char *nm_device_sysctl_ip_conf_get(NMDevice *self, int addr_family, const char *property);
-
-gboolean nm_device_get_refresh_forwarding_done(NMDevice *self);
-
-void nm_device_set_refresh_forwarding_done(NMDevice *self, gboolean is_refresh_forwarding_done);
-
 #endif /* __NETWORKMANAGER_DEVICE_H__ */
