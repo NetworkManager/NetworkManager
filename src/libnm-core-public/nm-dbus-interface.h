ea6af6d8066673e39e287c19aa3cbde47c707f47
Beniamino Galvani <bgalvani@redhat.com>
2025-06-30 22:33:29 +0200

    libnm-core: change version of ip4-forwarding capability
    NM_VERSION_INFO_CAPABILITY_IP4_FORWARDING was backported to branch
    nm-1-54 before the 1.54 release. Update the symbol version.

--- src/libnm-core-public/nm-dbus-interface.h
+++ src/libnm-core-public/nm-dbus-interface.h
@@ -100,7 +100,7 @@
  *   https://issues.redhat.com/browse/RHEL-66262
  *   https://issues.redhat.com/browse/RHEL-67324
  * @NM_VERSION_INFO_CAPABILITY_IP4_FORWARDING: Indicates that NetworkManager supports
- * configuring per-device IPv4 sysctl forwarding setting. Since: 1.56.
+ * configuring per-device IPv4 sysctl forwarding setting. Since: 1.54.
  *
  * The numeric values represent the bit index of the capability. These capabilities
  * can be queried in the "VersionInfo" D-Bus property.
