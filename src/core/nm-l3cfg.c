NetworkManager/NetworkManager

commit a09f9cc61629345bfadba0b0b564f704f870441c

l3cfg: ensure the probing timeout is initialized on probe start
When handling event TIMEOUT, "acd_data->probing_timeout_msec" needs to
be always initialized before jumping to "handle_start_probing:";
otherwise, an assertion failure is triggered at:

  static void
  _l3_acd_data_timeout_schedule_probing_restart(AcdData *acd_data, gint64 now_msec)
  {
    ...
    nm_assert(acd_data->probing_timeout_msec > 0);

Even if the ACD data is already in state PROBE, that doesn't mean that
the timeout is already initialized because the PROBE state can also be
reached from a INSTANCE_RESET event; and depending on the previous
state "acd_data->probing_timeout_msec" could be uninitialized.

Fixes-test: @iptunnel_restart
Fixes: b8f9d7b5dd11 ('l3cfg: rework ACD handling in NML3Cfg to support handling conflicts')

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/2023

src/core/nm-l3cfg.c
@@ -2752,7 +2752,9 @@ handle_init:
                     goto handle_start_defending;
                 }
 
-                log_reason = "retry probing on timeout";
+                acd_data->probing_timestamp_msec = (*p_now_msec);
+                acd_data->probing_timeout_msec   = acd_timeout_msec;
+                log_reason                       = "retry probing on timeout";
                 goto handle_start_probing;
             }
 

commit aad6a7490e1b12e66f26fef5df908572597c6029

l3cfg: don't reset the probe timestamp during timer events
acd_data->probing_timestamp_msec indicates when the probing
started. It is used in different places to calculate the timeout for
certain operations. In particular, it is used to detect that the probe
creation took too long when handling the ACD_STATE_CHANGE_MODE_TIMEOUT
event.

If we reset this timestamp at every timer event, we'll never hit the
probe creation timeout. Don't update it during a timeout event.

Fixes: a09f9cc61629 ('l3cfg: ensure the probing timeout is initialized on probe start')

src/core/nm-l3cfg.c
@@ -2751,9 +2751,8 @@ handle_init:
                     goto handle_start_defending;
                 }
 
-                acd_data->probing_timestamp_msec = (*p_now_msec);
-                acd_data->probing_timeout_msec   = acd_timeout_msec;
-                log_reason                       = "retry probing on timeout";
+                acd_data->probing_timeout_msec = acd_timeout_msec;
+                log_reason                     = "retry probing on timeout";
                 goto handle_start_probing;
             }
 