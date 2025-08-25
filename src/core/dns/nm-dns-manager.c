NetworkManager/NetworkManager

commit 1f0d1d78d2a28ef82764a801c344e22816b06f67

dns-manager: always apply options from [global-dns]
Currently, the use of [global-dns] section for setting DNS options is
conditioned on presence of a nameserver in a [global-dns-domain-*] section.
Attempt to use the section for options alone results in an error:

  [global-dns]
  options=timeout:1

Or via D-Bus API:

  # busctl set-property org.freedesktop.NetworkManager \
      /org/freedesktop/NetworkManager org.freedesktop.NetworkManager \
      GlobalDnsConfiguration 'a{sv}' 2 \
          "options" as 1 "timeout:1" \
          "domains" a{sv} 0
  ...
  Nov 24 13:15:21 zmok.local NetworkManager[501184]: <debug> [1669292121.3904]
      manager: set global DNS failed with error: Global
      DNS configuration is missing the default domain

The insistence on existence of [global-dns-domain-*] would make sense if
other [global-dns-domain-...] sections were present.

However, the user might only want to set the options in resolv.conf and
still use connection-provide nameservers for the actual resolving.

Lift the limitation by allowing the [global-dns] to be used alone, while
still insist on [global-dns-domain-*] being there in presence of other
domain-specific options.

https://bugzilla.redhat.com/show_bug.cgi?id=2019306

src/core/dns/nm-dns-manager.c
@@ -1201,7 +1201,8 @@ compute_hash(NMDnsManager *self, const NMGlobalDnsConfig *global, guint8 buffer[
 
     if (global)
         nm_global_dns_config_update_checksum(global, sum);
-    else {
+
+    if (!global || !nm_global_dns_config_lookup_domain(global, "*")) {
         const CList *head;
 
         /* FIXME(ip-config-checksum): this relies on the fact that an IP
@@ -1244,13 +1245,15 @@ merge_global_dns_config(NMResolvConfData *rc, NMGlobalDnsConfig *global_conf)
     }
 
     default_domain = nm_global_dns_config_lookup_domain(global_conf, "*");
-    nm_assert(default_domain);
+    if (!default_domain)
+        return TRUE;
 
     servers = nm_global_dns_domain_get_servers(default_domain);
-    if (servers) {
-        for (i = 0; servers[i]; i++)
-            add_string_item(rc->nameservers, servers[i], TRUE);
-    }
+    if (!servers)
+        return TRUE;
+
+    for (i = 0; servers[i]; i++)
+        add_string_item(rc->nameservers, servers[i], TRUE);
 
     return TRUE;
 }
@@ -1311,9 +1314,10 @@ _collect_resolv_conf_data(NMDnsManager      *self,
 
     priv = NM_DNS_MANAGER_GET_PRIVATE(self);
 
-    if (global_config) {
+    if (global_config)
         merge_global_dns_config(&rc, global_config);
-    } else {
+
+    if (!global_config || !nm_global_dns_config_lookup_domain(global_config, "*")) {
         nm_auto_str_buf NMStrBuf tmp_strbuf = NM_STR_BUF_INIT(0, FALSE);
         int                      first_prio = 0;
         const NMDnsConfigIPData *ip_data;
@@ -2556,14 +2560,12 @@ config_changed_cb(NMConfig           *config,
     }
 }
 
-static GVariant *
-_get_global_config_variant(NMGlobalDnsConfig *global)
+static void
+_get_global_config_variant(GVariantBuilder *builder, NMGlobalDnsConfig *global)
 {
     NMGlobalDnsDomain *domain;
-    GVariantBuilder    builder;
     guint              i, num;
 
-    g_variant_builder_init(&builder, G_VARIANT_TYPE("aa{sv}"));
     num = nm_global_dns_config_get_num_domains(global);
     for (i = 0; i < num; i++) {
         GVariantBuilder    conf_builder;
@@ -2599,10 +2601,8 @@ _get_global_config_variant(NMGlobalDnsConfig *global)
                               "priority",
                               g_variant_new_int32(NM_DNS_PRIORITY_DEFAULT_NORMAL));
 
-        g_variant_builder_add(&builder, "a{sv}", &conf_builder);
+        g_variant_builder_add(builder, "a{sv}", &conf_builder);
     }
-
-    return g_variant_ref_sink(g_variant_builder_end(&builder));
 }
 
 static GVariant *
@@ -2619,14 +2619,11 @@ _get_config_variant(NMDnsManager *self)
     if (priv->config_variant)
         return priv->config_variant;
 
-    global_config = nm_config_data_get_global_dns_config(nm_config_get_data(priv->config));
-    if (global_config) {
-        priv->config_variant = _get_global_config_variant(global_config);
-        _LOGT("current configuration: %s", (str = g_variant_print(priv->config_variant, TRUE)));
-        return priv->config_variant;
-    }
-
     g_variant_builder_init(&builder, G_VARIANT_TYPE("aa{sv}"));
+
+    global_config = nm_config_data_get_global_dns_config(nm_config_get_data(priv->config));
+    if (global_config)
+        _get_global_config_variant(&builder, global_config);
 
     head = _mgr_get_ip_data_lst_head(self);
     c_list_for_each_entry (ip_data, head, ip_data_lst) {

Makefile.am
@@ -4497,7 +4497,9 @@ EXTRA_DIST += \
 	src/core/tests/config/NetworkManager-warn.conf \
 	src/core/tests/config/NetworkManager.state \
 	src/core/tests/config/bad.conf \
+	src/core/tests/config/global-dns-good.conf \
 	src/core/tests/config/global-dns-invalid.conf \
+	src/core/tests/config/global-dns-options.conf \
 	src/core/tests/config/conf.d/00-overrides.conf \
 	src/core/tests/config/conf.d/10-more.conf \
 	src/core/tests/config/conf.d/20-config-enable-1.conf \

src/core/nm-config-data.c
@@ -1233,7 +1233,7 @@ load_global_dns(GKeyFile *keyfile, gboolean internal)
             default_found = TRUE;
     }
 
-    if (!default_found) {
+    if (!default_found && g_hash_table_size(dns_config->domains)) {
         nm_log_dbg(LOGD_CORE,
                    "%s global DNS configuration is missing default domain, ignore it",
                    internal ? "internal" : "user");

src/core/tests/config/global-dns-good.conf
@@ -1,0 +1,13 @@
+# Good configuration, since there is a default domain section
+
+[global-dns]
+searches=foo.com
+options=timeout:5
+
+[global-dns-domain-*]
+servers=4.5.6.7
+options=myoption1
+
+[global-dns-domain-test.com]
+servers=1.2.3.4
+options=myoption2

src/core/tests/config/global-dns-options.conf
@@ -1,0 +1,5 @@
+# Good configuration, since there is no domain section
+
+[global-dns]
+searches=foo.com
+options=timeout:5

src/core/tests/config/test-config.c
@@ -370,7 +370,21 @@ test_config_global_dns(void)
 
     g_object_unref(config);
 
-    /* Check that a file without a default domain section gives a NULL configuration */
+    /* Check that a file with a default domain section gives a good configuration */
+    config =
+        setup_config(NULL, TEST_DIR "/global-dns-good.conf", "", NULL, "/no/such/dir", "", NULL);
+    dns = nm_config_data_get_global_dns_config(nm_config_get_data_orig(config));
+    g_assert(dns);
+    g_object_unref(config);
+
+    /* Check that a file with options but no domains gives a good configuration */
+    config =
+        setup_config(NULL, TEST_DIR "/global-dns-options.conf", "", NULL, "/no/such/dir", "", NULL);
+    dns = nm_config_data_get_global_dns_config(nm_config_get_data_orig(config));
+    g_assert(dns);
+    g_object_unref(config);
+
+    /* Check that a file with a domain domain, but without a default one gives a NULL configuration */
     config =
         setup_config(NULL, TEST_DIR "/global-dns-invalid.conf", "", NULL, "/no/such/dir", "", NULL);
     dns = nm_config_data_get_global_dns_config(nm_config_get_data_orig(config));

commit 04811bab1f52c4768f43c1dc88ea83aac144c259

dns: add settings from global-dns with no global-dns-domain-*
The settings from global-dns are supposed to be always used and merged
with the settings from the connections. However, they were being ignored
if no global-dns-domain-* was defined, probably because in the past both
were mandatory. Fix it.

Fixes: 1f0d1d78d2a2 ('dns-manager: always apply options from [global-dns]')

src/core/dns/nm-dns-manager.c
@@ -542,10 +542,41 @@ add_string_item(GPtrArray *array, const char *str, gboolean dup)
 }
 
 static void
-add_dns_option_item(GPtrArray *array, const char *str)
+add_dns_nameservers(NMResolvConfData *rc, int addr_family, int ifindex, const NML3ConfigData *l3cd)
 {
-    if (_nm_utils_dns_option_find_idx((const char *const *) array->pdata, array->len, str) < 0)
-        g_ptr_array_add(array, g_strdup(str));
+    char               buf[NM_INET_ADDRSTRLEN + 50];
+    guint              num_nameservers;
+    guint              i;
+    const char *const *strarr;
+
+    nm_assert(ifindex == nm_l3_config_data_get_ifindex(l3cd));
+
+    strarr = nm_l3_config_data_get_nameservers(l3cd, addr_family, &num_nameservers);
+    for (i = 0; i < num_nameservers; i++) {
+        NMIPAddr a;
+
+        if (!nm_dns_uri_parse_plain(addr_family, strarr[i], NULL, &a))
+            continue;
+
+        if (addr_family == AF_INET)
+            nm_inet_ntop(addr_family, &a, buf);
+        else if (IN6_IS_ADDR_V4MAPPED(&a))
+            nm_inet4_ntop(a.addr6.s6_addr32[3], buf);
+        else {
+            nm_inet6_ntop(&a.addr6, buf);
+            if (IN6_IS_ADDR_LINKLOCAL(&a)) {
+                const char *ifname;
+
+                ifname = nm_platform_link_get_name(NM_PLATFORM_GET, ifindex);
+                if (ifname) {
+                    g_strlcat(buf, "%", sizeof(buf));
+                    g_strlcat(buf, ifname, sizeof(buf));
+                }
+            }
+        }
+
+        add_string_item(rc->nameservers, buf, TRUE);
+    }
 }
 
 static void
@@ -586,45 +617,23 @@ add_dns_domains(GPtrArray            *array,
 }
 
 static void
-merge_one_l3cd(NMResolvConfData *rc, int addr_family, int ifindex, const NML3ConfigData *l3cd)
+add_dns_option_item(GPtrArray *array, const char *str)
 {
-    char               buf[NM_INET_ADDRSTRLEN + 50];
-    gboolean           has_trust_ad;
+    if (_nm_utils_dns_option_find_idx((const char *const *) array->pdata, array->len, str) < 0)
+        g_ptr_array_add(array, g_strdup(str));
+}
+
+static void
+add_dns_options(NMResolvConfData     *rc,
+                int                   addr_family,
+                const NML3ConfigData *l3cd,
+                gboolean              has_global_nameservers)
+{
     guint              num_nameservers;
+    gboolean           has_trust_ad;
     guint              num;
     guint              i;
     const char *const *strarr;
-
-    nm_assert(ifindex == nm_l3_config_data_get_ifindex(l3cd));
-
-    strarr = nm_l3_config_data_get_nameservers(l3cd, addr_family, &num_nameservers);
-    for (i = 0; i < num_nameservers; i++) {
-        NMIPAddr a;
-
-        if (!nm_dns_uri_parse_plain(addr_family, strarr[i], NULL, &a))
-            continue;
-
-        if (addr_family == AF_INET)
-            nm_inet_ntop(addr_family, &a, buf);
-        else if (IN6_IS_ADDR_V4MAPPED(&a))
-            nm_inet4_ntop(a.addr6.s6_addr32[3], buf);
-        else {
-            nm_inet6_ntop(&a.addr6, buf);
-            if (IN6_IS_ADDR_LINKLOCAL(&a)) {
-                const char *ifname;
-
-                ifname = nm_platform_link_get_name(NM_PLATFORM_GET, ifindex);
-                if (ifname) {
-                    g_strlcat(buf, "%", sizeof(buf));
-                    g_strlcat(buf, ifname, sizeof(buf));
-                }
-            }
-        }
-
-        add_string_item(rc->nameservers, buf, TRUE);
-    }
-
-    add_dns_domains(rc->searches, addr_family, l3cd, FALSE, TRUE);
 
     has_trust_ad = FALSE;
     strarr       = nm_l3_config_data_get_dns_options(l3cd, addr_family, &num);
@@ -638,16 +647,27 @@ merge_one_l3cd(NMResolvConfData *rc, int addr_family, int ifindex, const NML3Con
         add_dns_option_item(rc->options, option);
     }
 
-    if (num_nameservers == 0) {
+    nm_l3_config_data_get_nameservers(l3cd, addr_family, &num_nameservers);
+    if (num_nameservers == 0 || has_global_nameservers) {
         /* If the @l3cd contributes no DNS servers, ignore whether trust-ad is set or unset
-         * for this @l3cd. */
+         * for this @l3cd. If there are global nameservers, those from this @l3cd has been
+         * ignored, so it count as not contributing. */
     } else if (has_trust_ad) {
         /* We only set has_trust_ad to TRUE, if all IP configs agree (or don't contribute).
          * Once set to FALSE, it doesn't get reset. */
         if (rc->has_trust_ad == NM_TERNARY_DEFAULT)
             rc->has_trust_ad = NM_TERNARY_TRUE;
-    } else
+    } else {
         rc->has_trust_ad = NM_TERNARY_FALSE;
+    }
+}
+
+static void
+add_nis_servers(NMResolvConfData *rc, int addr_family, const NML3ConfigData *l3cd)
+{
+    char  buf[NM_INET_ADDRSTRLEN + 50];
+    guint num;
+    guint i;
 
     if (addr_family == AF_INET) {
         const in_addr_t *nis_servers;
@@ -1264,6 +1284,9 @@ merge_global_dns_config(NMResolvConfData *rc, NMGlobalDnsConfig *global_conf)
     const char *const *servers;
     guint              i;
 
+    /* Global config must be processed before connections' config */
+    nm_assert(rc->nameservers->len == 0);
+
     if (!global_conf)
         return FALSE;
 
@@ -1342,56 +1365,63 @@ _collect_resolv_conf_data(NMDnsManager      *self,
                           char            ***out_nis_servers,
                           const char       **out_nis_domain)
 {
-    NMDnsManagerPrivate *priv;
-    NMResolvConfData     rc = {
-            .nameservers  = g_ptr_array_new(),
-            .searches     = g_ptr_array_new(),
-            .options      = g_ptr_array_new(),
-            .nis_domain   = NULL,
-            .nis_servers  = g_ptr_array_new(),
-            .has_trust_ad = NM_TERNARY_DEFAULT,
+    NMDnsManagerPrivate     *priv;
+    nm_auto_str_buf NMStrBuf tmp_strbuf             = NM_STR_BUF_INIT(0, FALSE);
+    int                      first_prio             = 0;
+    gboolean                 is_first               = TRUE;
+    gboolean                 has_global_nameservers = FALSE;
+    const NMDnsConfigIPData *ip_data;
+    const CList             *head;
+    NMResolvConfData         rc = {
+                .nameservers  = g_ptr_array_new(),
+                .searches     = g_ptr_array_new(),
+                .options      = g_ptr_array_new(),
+                .nis_domain   = NULL,
+                .nis_servers  = g_ptr_array_new(),
+                .has_trust_ad = NM_TERNARY_DEFAULT,
     };
 
     priv = NM_DNS_MANAGER_GET_PRIVATE(self);
 
-    if (global_config)
+    if (global_config) {
         merge_global_dns_config(&rc, global_config);
+        has_global_nameservers = rc.nameservers->len > 0;
+    }
 
-    if (!global_config || !nm_global_dns_config_lookup_domain(global_config, "*")) {
-        nm_auto_str_buf NMStrBuf tmp_strbuf = NM_STR_BUF_INIT(0, FALSE);
-        int                      first_prio = 0;
-        const NMDnsConfigIPData *ip_data;
-        const CList             *head;
-        gboolean                 is_first = TRUE;
+    head = _mgr_get_ip_data_lst_head(self);
+    c_list_for_each_entry (ip_data, head, ip_data_lst) {
+        gboolean skip = FALSE;
+        int      dns_priority;
 
-        head = _mgr_get_ip_data_lst_head(self);
-        c_list_for_each_entry (ip_data, head, ip_data_lst) {
-            gboolean skip = FALSE;
-            int      dns_priority;
+        _ASSERT_dns_config_ip_data(ip_data);
 
-            _ASSERT_dns_config_ip_data(ip_data);
+        if (!nm_l3_config_data_get_dns_priority(ip_data->l3cd, ip_data->addr_family, &dns_priority))
+            nm_assert_not_reached();
 
-            if (!nm_l3_config_data_get_dns_priority(ip_data->l3cd,
-                                                    ip_data->addr_family,
-                                                    &dns_priority))
-                nm_assert_not_reached();
+        if (is_first) {
+            is_first   = FALSE;
+            first_prio = dns_priority;
+        } else if (first_prio < 0 && first_prio != dns_priority)
+            skip = TRUE;
 
-            if (is_first) {
-                is_first   = FALSE;
-                first_prio = dns_priority;
-            } else if (first_prio < 0 && first_prio != dns_priority)
-                skip = TRUE;
+        _LOGT("config: %8d %-7s v%c %-5d %s: %s",
+              dns_priority,
+              _config_type_to_string(ip_data->ip_config_type),
+              nm_utils_addr_family_to_char(ip_data->addr_family),
+              ip_data->data->ifindex,
+              skip ? "<SKIP>" : "",
+              get_nameserver_list(ip_data->addr_family, ip_data->l3cd, &tmp_strbuf));
 
-            _LOGT("config: %8d %-7s v%c %-5d %s: %s",
-                  dns_priority,
-                  _config_type_to_string(ip_data->ip_config_type),
-                  nm_utils_addr_family_to_char(ip_data->addr_family),
-                  ip_data->data->ifindex,
-                  skip ? "<SKIP>" : "",
-                  get_nameserver_list(ip_data->addr_family, ip_data->l3cd, &tmp_strbuf));
-
-            if (!skip)
-                merge_one_l3cd(&rc, ip_data->addr_family, ip_data->data->ifindex, ip_data->l3cd);
+        if (!skip) {
+            if (!has_global_nameservers) {
+                add_dns_nameservers(&rc,
+                                    ip_data->addr_family,
+                                    ip_data->data->ifindex,
+                                    ip_data->l3cd);
+            }
+            add_dns_domains(rc.searches, ip_data->addr_family, ip_data->l3cd, FALSE, TRUE);
+            add_dns_options(&rc, ip_data->addr_family, ip_data->l3cd, has_global_nameservers);
+            add_nis_servers(&rc, ip_data->addr_family, ip_data->l3cd);
         }
     }
 