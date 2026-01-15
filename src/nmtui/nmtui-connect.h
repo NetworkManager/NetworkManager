221da3f8c0c4394e8e5b624bde207cb77e87d333
Rishabh Garg <rishabhgarg818@gmail.com>
2026-01-15 07:55:48 +0000

    nmtui: feature: wifi-rescan feature for the nmtui.

--- src/nmtui/nmtui-connect.h
+++ src/nmtui/nmtui-connect.h
@@ -6,6 +6,8 @@
 #ifndef NMTUI_CONNECT_H
 #define NMTUI_CONNECT_H
 
+#define RESCAN_TIMEOUT_MS 10000
+
 NmtNewtForm *nmtui_connect(gboolean is_top, int argc, char **argv);
 
 #endif /* NMTUI_CONNECT_H */
