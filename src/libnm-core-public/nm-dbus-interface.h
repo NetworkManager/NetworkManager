b9725dab739f35e941166e2a81269885c5a6672f
Íñigo Huguet <ihuguet@riseup.net>
2026-02-13 15:41:27 +0100

    core: device: allow to change the admin state of the device in SetManaged()
    Control it with a new NM_DEVICE_MANAGED_SET_ADMIN_STATE flag.
    This flag will make that, at the same time that the device is moved to
    managed/unmanaged, it's admin state is set to up/down. Many users want
    to have a way to have their devices in a DOWN admin state when they are
    not using them. Because of the complex activation process, NM wants to
    have its devices in UP state all the time. However, it is not a problem
    to have it DOWN if we are not managing it.

--- src/libnm-core-public/nm-dbus-interface.h
+++ src/libnm-core-public/nm-dbus-interface.h
@@ -1271,6 +1271,10 @@ typedef enum {
  * @NM_DEVICE_MANAGED_FLAGS_PERMANENT: to set the device managed state to the permanent (on disk) value.
  * @NM_DEVICE_MANAGED_FLAGS_BY_MAC: to match the device by MAC address, not by name.
  *   This option only makes sense together with %NM_DEVICE_MANAGED_FLAGS_PERMANENT.
+ * @NM_DEVICE_MANAGED_FLAGS_SET_ADMIN_STATE: to set the administrative state of the
+ *   device to up if the managed state is %NM_DEVICE_MANAGED_YES, and down if the managed state
+ *   is %NM_DEVICE_MANAGED_NO. If the flag is not set, the administrative state is not changed.
+ *   The flag is ignored for %NM_DEVICE_MANAGED_RESET.
  * @NM_DEVICE_MANAGED_FLAGS_ALL: all flags.
  *
  * Flags for the SetManaged() D-Bus call of a device and nm_device_set_managed_async().
@@ -1278,12 +1282,13 @@ typedef enum {
  * Since: 1.58
  */
 typedef enum /*< flags >*/ {
-    NM_DEVICE_MANAGED_FLAGS_NONE      = 0,
-    NM_DEVICE_MANAGED_FLAGS_RUNTIME   = 0x1,
-    NM_DEVICE_MANAGED_FLAGS_PERMANENT = 0x2,
-    NM_DEVICE_MANAGED_FLAGS_BY_MAC    = 0x4,
+    NM_DEVICE_MANAGED_FLAGS_NONE            = 0,
+    NM_DEVICE_MANAGED_FLAGS_RUNTIME         = 0x1,
+    NM_DEVICE_MANAGED_FLAGS_PERMANENT       = 0x2,
+    NM_DEVICE_MANAGED_FLAGS_BY_MAC          = 0x4,
+    NM_DEVICE_MANAGED_FLAGS_SET_ADMIN_STATE = 0x8,
 
-    NM_DEVICE_MANAGED_FLAGS_ALL = 0x7, /* <skip> */
+    NM_DEVICE_MANAGED_FLAGS_ALL = 0xF, /* <skip> */
 } NMDeviceManagedFlags;
 
 /**
