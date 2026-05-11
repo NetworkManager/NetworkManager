22ab14d20780648015582187d48170e03321acc9
Jan Vaclav <jvaclav@redhat.com>
2026-05-11 12:12:12 +0200

    libnm: add NM_VERSION_1_56_1

--- src/libnm-core-public/nm-version.h
+++ src/libnm-core-public/nm-version.h
@@ -453,6 +453,12 @@
 #define NM_AVAILABLE_IN_1_56
 #endif
 
+#if NM_VERSION_MAX_ALLOWED < NM_VERSION_1_56_1
+#define NM_AVAILABLE_IN_1_56_1 G_UNAVAILABLE(1, 56.1)
+#else
+#define NM_AVAILABLE_IN_1_56_1
+#endif
+
 /*
  * Synchronous API for calling D-Bus in libnm is deprecated. See
  * https://networkmanager.dev/docs/libnm/latest/usage.html#sync-api
