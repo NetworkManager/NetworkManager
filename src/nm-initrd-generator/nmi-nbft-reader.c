NetworkManager/NetworkManager

commit d38cbfb3d1ec43d953f484b39fcc6c6128e7bf3b

initrd: VLAN support for the NBFT parser
Creates additional connections for VLANs, which are in fact
separate HFI records in the NBFT table. Uses MAC address for
linking parent interface as the interface naming is defined
by an external service.

Signed-off-by: Tomas Bzatek <tbzatek@redhat.com>

src/nm-initrd-generator/nmi-nbft-reader.c
@@ -61,45 +61,57 @@ load_libnvme(void)
     return handle;
 }
 
-static NMConnection *
-parse_hfi(struct nbft_info_hfi *hfi, int iface_idx, char **hostname)
+static char *
+format_conn_name(const char *table_name, struct nbft_info_hfi *hfi, gboolean is_vlan)
 {
-    gs_unref_object NMConnection         *connection;
-    NMSetting                            *s_connection;
-    NMSetting                            *s_wired;
-    gs_free char                         *hwaddr = NULL;
-    gs_free char                         *ifname = NULL;
-    gs_unref_object NMSetting            *s_ip4  = NULL;
-    gs_unref_object NMSetting            *s_ip6  = NULL;
-    nm_auto_unref_ip_address NMIPAddress *ipaddr = NULL;
-    gs_free_error GError                 *error  = NULL;
-    int                                   family = AF_UNSPEC;
-    NMIPAddr                              addr_bin;
+    if (is_vlan) {
+        nm_assert(hfi->tcp_info.vlan > 0);
+        return g_strdup_printf("%s connection HFI %d VLAN %d",
+                               table_name,
+                               hfi->index,
+                               hfi->tcp_info.vlan);
+    } else
+        return g_strdup_printf("%s connection HFI %d", table_name, hfi->index);
+}
 
-    /* Pre-checks */
-    if (!nm_inet_parse_bin_full(family, FALSE, hfi->tcp_info.ipaddr, &family, &addr_bin)) {
-        _LOGW(LOGD_CORE, "NBFT: Malformed IP address: '%s'", hfi->tcp_info.ipaddr);
-        return NULL;
+static NMConnection *
+find_conn_for_wired_mac(GPtrArray *a, const char *hwaddr)
+{
+    guint i;
+
+    for (i = 0; i < a->len; i++) {
+        NMConnection   *con = a->pdata[i];
+        NMSettingWired *s_wired;
+
+        if (!nm_connection_is_type(con, NM_SETTING_WIRED_SETTING_NAME))
+            continue;
+        s_wired = nm_connection_get_setting_wired(con);
+        if (!s_wired)
+            continue;
+        if (nm_streq(hwaddr, nm_setting_wired_get_mac_address(s_wired)))
+            return con;
     }
+    return NULL;
+}
 
-    ifname = g_strdup_printf("NBFT connection %d", iface_idx);
-    hwaddr = g_strdup_printf("%02x:%02x:%02x:%02x:%02x:%02x",
-                             hfi->tcp_info.mac_addr[0],
-                             hfi->tcp_info.mac_addr[1],
-                             hfi->tcp_info.mac_addr[2],
-                             hfi->tcp_info.mac_addr[3],
-                             hfi->tcp_info.mac_addr[4],
-                             hfi->tcp_info.mac_addr[5]);
+static NMConnection *
+create_wired_conn(struct nbft_info_hfi *hfi,
+                  const char           *conn_name,
+                  const char           *hwaddr,
+                  gboolean              is_vlan)
+{
+    NMConnection *connection;
+    NMSetting    *s_connection;
+    NMSetting    *s_wired;
 
-    /* Create new connection */
     connection = nm_simple_connection_new();
 
     s_connection = nm_setting_connection_new();
     g_object_set(s_connection,
                  NM_SETTING_CONNECTION_TYPE,
-                 NM_SETTING_WIRED_SETTING_NAME,
+                 is_vlan ? NM_SETTING_VLAN_SETTING_NAME : NM_SETTING_WIRED_SETTING_NAME,
                  NM_SETTING_CONNECTION_ID,
-                 ifname,
+                 conn_name,
                  NM_SETTING_CONNECTION_AUTOCONNECT_PRIORITY,
                  NMI_AUTOCONNECT_PRIORITY_FIRMWARE,
                  NULL);
@@ -110,7 +122,80 @@ parse_hfi(struct nbft_info_hfi *hfi, int iface_idx, char **hostname)
     g_object_set(s_wired, NM_SETTING_WIRED_MAC_ADDRESS, hwaddr, NULL);
     nm_connection_add_setting(connection, s_wired);
 
-    /* TODO: hfi->tcp_info.vlan */
+    return connection;
+}
+
+static void
+parse_hfi(GPtrArray *a, struct nbft_info_hfi *hfi, const char *table_name, char **hostname)
+{
+    gs_unref_object NMConnection         *connection = NULL;
+    NMConnection                         *parent_connection;
+    NMSetting                            *s_vlan;
+    gs_free char                         *hwaddr    = NULL;
+    gs_free char                         *conn_name = NULL;
+    gs_unref_object NMSetting            *s_ip4     = NULL;
+    gs_unref_object NMSetting            *s_ip6     = NULL;
+    nm_auto_unref_ip_address NMIPAddress *ipaddr    = NULL;
+    guint                                 prefix;
+    gs_free_error GError                 *error  = NULL;
+    int                                   family = AF_UNSPEC;
+    NMIPAddr                              addr_bin;
+
+    /* Pre-checks */
+    if (!nm_inet_parse_bin_full(family, FALSE, hfi->tcp_info.ipaddr, &family, &addr_bin)) {
+        _LOGW(LOGD_CORE, "NBFT: Malformed IP address: '%s'", hfi->tcp_info.ipaddr);
+        return;
+    }
+
+    /* MAC address */
+    hwaddr = g_strdup_printf("%02X:%02X:%02X:%02X:%02X:%02X",
+                             hfi->tcp_info.mac_addr[0],
+                             hfi->tcp_info.mac_addr[1],
+                             hfi->tcp_info.mac_addr[2],
+                             hfi->tcp_info.mac_addr[3],
+                             hfi->tcp_info.mac_addr[4],
+                             hfi->tcp_info.mac_addr[5]);
+
+    /* First check if we need VLANs */
+    if (hfi->tcp_info.vlan > 0) {
+        parent_connection = find_conn_for_wired_mac(a, hwaddr);
+        if (!parent_connection) {
+            /* Create new parent wired connection */
+            conn_name         = format_conn_name(table_name, hfi, FALSE);
+            parent_connection = create_wired_conn(hfi, conn_name, hwaddr, FALSE);
+
+            s_ip4 = nm_setting_ip4_config_new();
+            s_ip6 = nm_setting_ip6_config_new();
+            g_object_set(s_ip4,
+                         NM_SETTING_IP_CONFIG_METHOD,
+                         NM_SETTING_IP4_CONFIG_METHOD_DISABLED,
+                         NULL);
+            g_object_set(s_ip6,
+                         NM_SETTING_IP_CONFIG_METHOD,
+                         NM_SETTING_IP6_CONFIG_METHOD_DISABLED,
+                         NULL);
+            nm_connection_add_setting(parent_connection, g_steal_pointer(&s_ip4));
+            nm_connection_add_setting(parent_connection, g_steal_pointer(&s_ip6));
+
+            if (!nm_connection_normalize(parent_connection, NULL, NULL, &error)) {
+                _LOGW(LOGD_CORE, "Generated an invalid connection: %s", error->message);
+                g_object_unref(parent_connection);
+                return;
+            }
+            g_ptr_array_add(a, parent_connection);
+        }
+
+        conn_name  = format_conn_name(table_name, hfi, TRUE);
+        connection = create_wired_conn(hfi, conn_name, hwaddr, TRUE);
+
+        s_vlan = nm_setting_vlan_new();
+        g_object_set(s_vlan, NM_SETTING_VLAN_ID, hfi->tcp_info.vlan, NULL);
+        nm_connection_add_setting(connection, s_vlan);
+    } else {
+        /* No VLANS */
+        conn_name  = format_conn_name(table_name, hfi, FALSE);
+        connection = create_wired_conn(hfi, conn_name, hwaddr, FALSE);
+    }
 
     /* IP addresses */
     s_ip4 = nm_setting_ip4_config_new();
@@ -149,7 +234,7 @@ parse_hfi(struct nbft_info_hfi *hfi, int iface_idx, char **hostname)
                       hfi->tcp_info.ipaddr,
                       hfi->tcp_info.subnet_mask_prefix,
                       error->message);
-                return NULL;
+                return;
             }
             nm_setting_ip_config_add_address(NM_SETTING_IP_CONFIG(s_ip4), ipaddr);
             if (is_valid_addr(AF_INET, hfi->tcp_info.gateway_ipaddr)) {
@@ -204,7 +289,7 @@ parse_hfi(struct nbft_info_hfi *hfi, int iface_idx, char **hostname)
                       hfi->tcp_info.ipaddr,
                       prefix,
                       error->message);
-                return NULL;
+                return;
             }
             nm_setting_ip_config_add_address(NM_SETTING_IP_CONFIG(s_ip6), ipaddr);
             if (is_valid_addr(AF_INET6, hfi->tcp_info.gateway_ipaddr)) {
@@ -246,9 +331,10 @@ parse_hfi(struct nbft_info_hfi *hfi, int iface_idx, char **hostname)
 
     if (!nm_connection_normalize(connection, NULL, NULL, &error)) {
         _LOGW(LOGD_CORE, "Generated an invalid connection: %s", error->message);
-        return NULL;
+        return;
     }
-    return g_steal_pointer(&connection);
+
+    g_ptr_array_add(a, g_steal_pointer(&connection));
 }
 
 NMConnection **
@@ -260,7 +346,6 @@ nmi_nbft_reader_parse(const char *sysfs_dir, char **hostname)
     GDir                             *dir;
     void                             *libnvme_handle = NULL;
     const char                       *entry_name;
-    int                               idx = 1;
 
     g_return_val_if_fail(sysfs_dir != NULL, NULL);
     path = g_build_filename(sysfs_dir, "firmware", "acpi", "tables", NULL);
@@ -293,9 +378,7 @@ nmi_nbft_reader_parse(const char *sysfs_dir, char **hostname)
             continue;
         }
 
-        for (hfi = nbft->hfi_list; hfi && *hfi; hfi++, idx++) {
-            NMConnection *connection;
-
+        for (hfi = nbft->hfi_list; hfi && *hfi; hfi++) {
             if (!nm_streq((*hfi)->transport, "tcp")) {
                 _LOGW(LOGD_CORE,
                       "NBFT table %s, HFI descriptor %d: unsupported transport type '%s'",
@@ -304,10 +387,7 @@ nmi_nbft_reader_parse(const char *sysfs_dir, char **hostname)
                       (*hfi)->transport);
                 continue;
             }
-
-            connection = parse_hfi(*hfi, idx, hostname);
-            if (connection)
-                g_ptr_array_add(a, connection);
+            parse_hfi(a, *hfi, entry_name, hostname);
         }
 
         _nvme_nbft_free(nbft);
@@ -316,7 +396,7 @@ nmi_nbft_reader_parse(const char *sysfs_dir, char **hostname)
     g_dir_close(dir);
     dlclose(libnvme_handle);
     g_ptr_array_add(a, NULL); /* trailing NULL-delimiter */
-    return (NMConnection **) g_ptr_array_free(a, FALSE);
+    return (NMConnection **) g_ptr_array_free(g_steal_pointer(&a), FALSE);
 }
 
 #else /* WITH_NBFT */

commit c5c81588d55b45798cc50b5c873613cde7835f92

nm-initrd-generator: set parent for NBFT vlan connection
If the NBFT contains VLAN connections, a race condition can occur between creation
of the VLAN interface and renaming of the parent interface. The VLAN
connection is set up on the interface before renaming it, and when
NetworkManager tries to set it up on the renamed interface again later,
the setup fails:

platform-linux: do-add-link[enp2s0.2/vlan]: failure 17 (File exists - 8021q: VLAN device already exists)

Fix this by adding an explicit "vlan.parent" setting to the VLAN interface
configuration in nm-initrd-generator.

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/work_items/1903

Fixes: d38cbfb3d1ec ('initrd: VLAN support for the NBFT parser')
Suggested-by: Jonathan Kang <jonathankang@gnome.org>
Signed-off-by: Martin Wilck <mwilck@suse.com>

src/nm-initrd-generator/nmi-nbft-reader.c
@@ -162,7 +162,9 @@ parse_hfi(GPtrArray *a, struct nbft_info_hfi *hfi, const char *table_name, char 
     gs_free char                         *conn_name = NULL;
     gs_unref_object NMSetting            *s_ip4     = NULL;
     gs_unref_object NMSetting            *s_ip6     = NULL;
+    NMSettingConnection                  *s_con     = NULL;
     nm_auto_unref_ip_address NMIPAddress *ipaddr    = NULL;
+    const char                           *parent_uuid;
     guint                                 prefix;
     gs_free_error GError                 *error  = NULL;
     int                                   family = AF_UNSPEC;
@@ -218,6 +220,10 @@ parse_hfi(GPtrArray *a, struct nbft_info_hfi *hfi, const char *table_name, char 
 
         s_vlan = nm_setting_vlan_new();
         g_object_set(s_vlan, NM_SETTING_VLAN_ID, hfi->tcp_info.vlan, NULL);
+        s_con = nm_connection_get_setting_connection(parent_connection);
+        nm_assert(s_con);
+        parent_uuid = nm_setting_connection_get_uuid(s_con);
+        g_object_set(s_vlan, NM_SETTING_VLAN_PARENT, parent_uuid, NULL);
         nm_connection_add_setting(connection, s_vlan);
     } else {
         /* No VLANS */

src/nm-initrd-generator/tests/test-nbft-reader.c
@@ -44,7 +44,10 @@ find_connection_for_mac(NMConnection **nbft_connections, const char *expected_ma
 }
 
 static void
-verify_connection(NMConnection *c, const char *expected_mac, guint32 expected_vlan_id)
+verify_connection(NMConnection *c,
+                  const char   *expected_mac,
+                  guint32       expected_vlan_id,
+                  const char   *expected_parent)
 {
     NMSettingConnection *s_con;
     NMSettingWired      *s_wired;
@@ -74,7 +77,7 @@ verify_connection(NMConnection *c, const char *expected_mac, guint32 expected_vl
         s_vlan = nm_connection_get_setting_vlan(c);
         g_assert(s_vlan);
         g_assert_cmpint(nm_setting_vlan_get_id(s_vlan), ==, expected_vlan_id);
-        g_assert_cmpstr(nm_setting_vlan_get_parent(s_vlan), ==, NULL);
+        g_assert_cmpstr(nm_setting_vlan_get_parent(s_vlan), ==, expected_parent);
     } else {
         g_assert_cmpstr(nm_setting_connection_get_connection_type(s_con),
                         ==,
@@ -208,42 +211,42 @@ test_read_nbft_ipv4_static(void)
     /* NBFT-multi HFI 1 */
     expected_mac_address = "52:54:00:72:c5:ae";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv4(connection, "192.168.122.158", 24, "192.168.122.1");
     verify_ipv6_disabled(connection);
 
     /* NBFT-multi HFI 2 */
     expected_mac_address = "52:54:00:72:c5:af";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv4_dhcp(connection);
     verify_ipv6_disabled(connection);
 
     /* NBFT-Dell.PowerEdge.R660-fw1.5.5-mpath+discovery HFI 1 */
     expected_mac_address = "00:62:0b:cb:eb:70";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv4(connection, "172.18.240.1", 24, NULL);
     verify_ipv6_disabled(connection);
 
     /* NBFT-Dell.PowerEdge.R660-fw1.5.5-mpath+discovery HFI 2 */
     expected_mac_address = "00:62:0b:cb:eb:71";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv4(connection, "172.18.230.2", 24, NULL);
     verify_ipv6_disabled(connection);
 
     /* NBFT-rhpoc */
     expected_mac_address = "ea:eb:d3:58:89:58";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv4(connection, "192.168.101.30", 24, NULL);
     verify_ipv6_disabled(connection);
 
     /* NBFT-static-ipv4 */
     expected_mac_address = "52:54:00:b8:19:b9";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv4(connection, "192.168.49.50", 24, NULL);
     verify_ipv6_disabled(connection);
 
@@ -265,14 +268,14 @@ test_read_nbft_ipv4_dhcp(void)
     /* NBFT-dhcp-ipv4 */
     expected_mac_address = "52:54:00:b8:19:b9";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv4_dhcp(connection);
     verify_ipv6_disabled(connection);
 
     /* NBFT-Dell.PowerEdge.R760 */
     expected_mac_address = "b0:26:28:e8:7c:0e";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv4_dhcp(connection);
     verify_ipv6_disabled(connection);
 
@@ -294,7 +297,7 @@ test_read_nbft_ipv6_static(void)
     /* NBFT-static-ipv6 */
     expected_mac_address = "52:54:00:9e:20:1a";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv6(connection, "fd09:9a46:b5c1:1fe::10", 64, NULL);
     verify_ipv4_disabled(connection);
 
@@ -316,21 +319,21 @@ test_read_nbft_ipv6_auto(void)
     /* NBFT-auto-ipv6 */
     expected_mac_address = "52:54:00:9e:20:1a";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv6(connection, "fd09:9a46:b5c1:1ff:5054:ff:fe9e:201a", 64, NULL);
     verify_ipv4_disabled(connection);
 
     /* NBFT-dhcp-ipv6 */
     expected_mac_address = "52:54:00:b8:19:b9";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv6_auto(connection);
     verify_ipv4_disabled(connection);
 
     /* NBFT-ipv6-noip+disc */
     expected_mac_address = "40:a6:b7:c0:8a:c9";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv6_auto(connection);
     verify_ipv4_disabled(connection);
 
@@ -340,10 +343,12 @@ test_read_nbft_ipv6_auto(void)
 static void
 test_read_nbft_vlan(void)
 {
-    NMConnection **nbft_connections;
-    NMConnection  *connection;
-    const char    *expected_mac_address;
-    gs_free char  *hostname = NULL;
+    NMConnection       **nbft_connections;
+    NMConnection        *connection;
+    NMSettingConnection *s_conn;
+    const char          *expected_mac_address;
+    gs_free char        *hostname = NULL;
+    const char          *parent;
 
     nbft_connections = nmi_nbft_reader_parse(TEST_INITRD_DIR "/nbft-vlan", &hostname);
     g_assert_cmpstr(hostname, ==, NULL);
@@ -352,28 +357,30 @@ test_read_nbft_vlan(void)
     /* NBFT-qemu-vlans-incomplete HFI 1 */
     expected_mac_address = "52:54:00:72:c5:ae";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv4(connection, "192.168.122.158", 24, "192.168.122.1");
     verify_ipv6_disabled(connection);
 
     /* NBFT-qemu-vlans-incomplete HFI 2 */
     expected_mac_address = "52:54:00:72:c5:af";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 0);
-    verify_connection(connection, expected_mac_address, 0);
+    s_conn               = nm_connection_get_setting_connection(connection);
+    parent               = nm_setting_connection_get_uuid(s_conn);
+    verify_connection(connection, expected_mac_address, 0, NULL);
     verify_ipv4_disabled(connection);
     verify_ipv6_disabled(connection);
 
     /* NBFT-qemu-vlans-incomplete HFI 2 VLAN 11 */
     expected_mac_address = "52:54:00:72:c5:af";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 11);
-    verify_connection(connection, expected_mac_address, 11);
+    verify_connection(connection, expected_mac_address, 11, parent);
     verify_ipv4(connection, "192.168.124.58", 24, NULL);
     verify_ipv6_disabled(connection);
 
     /* NBFT-qemu-vlans-incomplete HFI 2 VLAN 12 */
     expected_mac_address = "52:54:00:72:c5:af";
     connection           = find_connection_for_mac(nbft_connections, expected_mac_address, 12);
-    verify_connection(connection, expected_mac_address, 12);
+    verify_connection(connection, expected_mac_address, 12, parent);
     verify_ipv4(connection, "192.168.125.58", 24, NULL);
     verify_ipv6_disabled(connection);
 