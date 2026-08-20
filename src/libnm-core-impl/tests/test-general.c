NetworkManager/NetworkManager

commit 41291ef773f7628a179f19a271239fd2676ffa37

core/connection: ensure wired settings are around for bridges
Bridges are wired ethernet bridges, it makes sense for them to have
wired ethernet settings.

Ensuring they always exist makes reapplying the MTU changes more
convenient. The MTU for bridges is taken from wired settings, making it
impossible to change and reapply it for connections that lack them
(as reapply doesn't really cope well with addition and removal of
settings).

https://bugzilla.redhat.com/show_bug.cgi?id=2076131
https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/merge_requests/1208

src/libnm-core-impl/nm-connection.c
@@ -1708,7 +1708,7 @@ _normalize_required_settings(NMConnection *self)
     NMSetting          *s_bridge;
     gboolean            changed = FALSE;
 
-    if (nm_connection_get_setting_vlan(self)) {
+    if (nm_connection_get_setting_vlan(self) || nm_connection_get_setting_bridge(self)) {
         if (!nm_connection_get_setting_wired(self)) {
             nm_connection_add_setting(self, nm_setting_wired_new());
             changed = TRUE;

src/core/settings/plugins/keyfile/tests/test-keyfile-settings.c
@@ -1924,6 +1924,7 @@ test_write_bridge_main(void)
     gs_unref_object NMConnection *connection = NULL;
     NMSettingConnection          *s_con;
     NMSettingBridge              *s_bridge;
+    NMSettingWired               *s_wired;
     NMSettingIPConfig            *s_ip4;
     NMSettingIPConfig            *s_ip6;
 
@@ -1952,6 +1953,11 @@ test_write_bridge_main(void)
     s_bridge = (NMSettingBridge *) nm_setting_bridge_new();
     g_assert(s_bridge);
     nm_connection_add_setting(connection, NM_SETTING(s_bridge));
+
+    /* Ethernet setting */
+    s_wired = (NMSettingWired *) nm_setting_wired_new();
+    g_assert(s_wired);
+    nm_connection_add_setting(connection, NM_SETTING(s_wired));
 
     /* IP4 setting */
     s_ip4 = (NMSettingIPConfig *) nm_setting_ip4_config_new();

src/libnm-core-impl/nm-setting-bridge.c
@@ -1311,6 +1311,15 @@ verify(NMSetting *setting, NMConnection *connection, GError **error)
                                            NM_SETTING_BRIDGE_VLANS))
         return NM_SETTING_VERIFY_NORMALIZABLE;
 
+    if (connection && !nm_connection_get_setting_wired(connection)) {
+        g_set_error_literal(error,
+                            NM_CONNECTION_ERROR,
+                            NM_CONNECTION_ERROR_SETTING_NOT_FOUND,
+                            _("bridge connection should have a ethernet setting as well"));
+        g_prefix_error(error, "%s: ", NM_SETTING_BRIDGE_SETTING_NAME);
+        return NM_SETTING_VERIFY_NORMALIZABLE;
+    }
+
     return TRUE;
 }
 

commit 10426744f40edfc93ff5beeb00c6e97fac9a7e46

libnm: fix normalization order for Bluetooth NAP connections
_normalize_required_settings() added the ethernet setting a bridge needs
before adding the bridge setting a NAP profile needs, so the bridge it
synthesized for NAP never got one. Verification then failed as
normalizable and nm_connection_normalize() warned and hit
g_return_val_if_reached(), which users see on stderr when creating a
Bluetooth hotspot with the documented nmcli-examples(7) command.

The profile still got stored & nmcli discards the return value, and the
daemon's own normalize pass then adds the missing ethernet setting.

Fixes: 41291ef773f7 ('core/connection: ensure wired settings are around for bridges')
https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/1996

src/libnm-core-impl/nm-connection.c
@@ -1835,25 +1835,20 @@ _normalize_802_1x_empty_strings(NMConnection *self)
 static gboolean
 _normalize_required_settings(NMConnection *self)
 {
-    NMSettingBluetooth *s_bt = nm_connection_get_setting_bluetooth(self);
-    NMSetting          *s_bridge;
-    gboolean            changed = FALSE;
+    gboolean changed = FALSE;
 
-    if (nm_connection_get_setting_vlan(self) || nm_connection_get_setting_bridge(self)) {
-        if (!nm_connection_get_setting_wired(self)) {
-            nm_connection_add_setting(self, nm_setting_wired_new());
-            changed = TRUE;
-        }
+    if (_nm_connection_get_setting_bluetooth_for_nap(self)
+        && !nm_connection_get_setting_bridge(self)) {
+        nm_connection_add_setting(
+            self,
+            g_object_new(NM_TYPE_SETTING_BRIDGE, NM_SETTING_BRIDGE_STP, FALSE, NULL));
+        changed = TRUE;
     }
-    if (s_bt
-        && nm_streq0(nm_setting_bluetooth_get_connection_type(s_bt),
-                     NM_SETTING_BLUETOOTH_TYPE_NAP)) {
-        if (!nm_connection_get_setting_bridge(self)) {
-            s_bridge = nm_setting_bridge_new();
-            g_object_set(s_bridge, NM_SETTING_BRIDGE_STP, FALSE, NULL);
-            nm_connection_add_setting(self, s_bridge);
-            changed = TRUE;
-        }
+    /* Must run after the NAP block: its synthesized bridge needs a wired setting too. */
+    if ((nm_connection_get_setting_vlan(self) || nm_connection_get_setting_bridge(self))
+        && !nm_connection_get_setting_wired(self)) {
+        nm_connection_add_setting(self, nm_setting_wired_new());
+        changed = TRUE;
     }
     return changed;
 }

src/libnm-core-impl/tests/test-general.c
@@ -6571,6 +6571,41 @@ test_connection_normalize_shared_addresses(void)
 }
 
 static void
+test_connection_normalize_bluetooth_nap(void)
+{
+    gs_unref_object NMConnection *con = NULL;
+    NMSettingConnection          *s_con;
+    NMSettingBluetooth           *s_bt;
+
+    con = nmtst_create_minimal_connection("test-bt-nap",
+                                          NULL,
+                                          NM_SETTING_BLUETOOTH_SETTING_NAME,
+                                          &s_con);
+    g_object_set(s_con, NM_SETTING_CONNECTION_INTERFACE_NAME, "btnap0", NULL);
+
+    s_bt = nm_connection_get_setting_bluetooth(con);
+    g_object_set(s_bt, NM_SETTING_BLUETOOTH_TYPE, NM_SETTING_BLUETOOTH_TYPE_NAP, NULL);
+
+    nmtst_assert_connection_verifies_after_normalization(con,
+                                                         NM_CONNECTION_ERROR,
+                                                         NM_CONNECTION_ERROR_INVALID_SETTING);
+    nmtst_connection_normalize(con);
+    nmtst_assert_connection_has_settings(con,
+                                         NM_SETTING_CONNECTION_SETTING_NAME,
+                                         NM_SETTING_BLUETOOTH_SETTING_NAME,
+                                         NM_SETTING_BRIDGE_SETTING_NAME,
+                                         NM_SETTING_WIRED_SETTING_NAME,
+                                         NM_SETTING_IP4_CONFIG_SETTING_NAME,
+                                         NM_SETTING_IP6_CONFIG_SETTING_NAME,
+                                         NM_SETTING_PROXY_SETTING_NAME);
+    g_assert(!nm_setting_bridge_get_stp(nm_connection_get_setting_bridge(con)));
+    g_assert_cmpstr(
+        nm_setting_bluetooth_get_connection_type(nm_connection_get_setting_bluetooth(con)),
+        ==,
+        NM_SETTING_BLUETOOTH_TYPE_NAP);
+}
+
+static void
 test_connection_normalize_ovs_interface_type_system(gconstpointer test_data)
 {
     const guint                   TEST_CASE = GPOINTER_TO_UINT(test_data);
@@ -12073,6 +12108,8 @@ main(int argc, char **argv)
                     test_connection_normalize_may_fail);
     g_test_add_func("/core/general/test_connection_normalize_shared_addresses",
                     test_connection_normalize_shared_addresses);
+    g_test_add_func("/core/general/test_connection_normalize_bluetooth_nap",
+                    test_connection_normalize_bluetooth_nap);
     g_test_add_data_func("/core/general/test_connection_normalize_ovs_interface_type_system/1",
                          GUINT_TO_POINTER(1),
                          test_connection_normalize_ovs_interface_type_system);