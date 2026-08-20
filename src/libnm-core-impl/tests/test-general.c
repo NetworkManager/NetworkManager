NetworkManager/NetworkManager

commit 9b28c9ba91f6714561db843d51557a91494fb790

core: infer the bluetooth type from the presence of the supplemental settings
When the user sets a GSM or CDMA setting along with a Bluetooth setting
we know we're dealing with a DUN profile. No need to ask.

[thaller@redhat.com: verify() and normalize() must strongly agree whether a
connection is normalizable, and now to do it. That is, after verify()
determines the connection is normalizable, normalize() must fix it as
anticipated.

The reason is, we only want to modify the connection, if we are able
to create a valid result. Hence, after normalize() it *must* verify().

Try to simplify that by moving the logic of fixing the bt-type to a
common place _nm_connection_detect_bluetooth_type().]

Co-Authored-By: Thomas Haller <thaller@redhat.com>

libnm-core/nm-connection.c
@@ -684,6 +684,26 @@ _normalize_connection_type (NMConnection *self)
 }
 
 const char *
+_nm_connection_detect_bluetooth_type (NMConnection *self)
+{
+	NMSettingBluetooth *s_bt = nm_connection_get_setting_bluetooth (self);
+
+	if (   s_bt
+	    && nm_setting_bluetooth_get_connection_type (s_bt)) {
+		if (   nm_connection_get_setting_gsm (self)
+		    || nm_connection_get_setting_cdma (self))
+			return NM_SETTING_BLUETOOTH_TYPE_DUN;
+		if (nm_connection_get_setting_bridge (self))
+			return NM_SETTING_BLUETOOTH_TYPE_NAP;
+		return NM_SETTING_BLUETOOTH_TYPE_PANU;
+	}
+
+	/* NULL means the connection is not a bluetooth type, or it needs
+	 * no normalization, as the type is set explicitly. */
+	return NULL;
+}
+
+const char *
 _nm_connection_detect_slave_type (NMConnection *connection, NMSetting **out_s_port)
 {
 	NMConnectionPrivate *priv = NM_CONNECTION_GET_PRIVATE (connection);
@@ -1053,6 +1073,20 @@ _normalize_team_port_config (NMConnection *self, GHashTable *parameters)
 }
 
 static gboolean
+_normalize_bluetooth_type (NMConnection *self, GHashTable *parameters)
+{
+	const char *type = _nm_connection_detect_bluetooth_type (self);
+
+	if (type) {
+		g_object_set (nm_connection_get_setting_bluetooth (self),
+		              NM_SETTING_BLUETOOTH_TYPE, type,
+		              NULL);
+		return TRUE;
+	}
+	return FALSE;
+}
+
+static gboolean
 _normalize_required_settings (NMConnection *self, GHashTable *parameters)
 {
 	NMSettingBluetooth *s_bt = nm_connection_get_setting_bluetooth (self);
@@ -1353,6 +1387,7 @@ nm_connection_normalize (NMConnection *connection,
 	was_modified |= _normalize_wireless_mac_address_randomization (connection, parameters);
 	was_modified |= _normalize_team_config (connection, parameters);
 	was_modified |= _normalize_team_port_config (connection, parameters);
+	was_modified |= _normalize_bluetooth_type (connection, parameters);
 
 	/* Verify anew. */
 	success = _nm_connection_verify (connection, error);

libnm-core/nm-connection-private.h
@@ -29,6 +29,8 @@ NMSetting  *_nm_connection_find_base_type_setting         (NMConnection *connect
 const char *_nm_connection_detect_slave_type              (NMConnection *connection,
                                                            NMSetting **out_s_port);
 
+const char *_nm_connection_detect_bluetooth_type (NMConnection *self);
+
 gboolean    _nm_connection_verify_required_interface_name (NMConnection *connection,
                                                            GError **error);
 

libnm-core/nm-setting-bluetooth.c
@@ -113,6 +113,8 @@ static gboolean
 verify (NMSetting *setting, NMConnection *connection, GError **error)
 {
 	NMSettingBluetoothPrivate *priv = NM_SETTING_BLUETOOTH_GET_PRIVATE (setting);
+	const char *type;
+	gboolean missing_nap_bridge = FALSE;
 
 	if (priv->bdaddr && !nm_utils_hwaddr_valid (priv->bdaddr, ETH_ALEN)) {
 		g_set_error_literal (error,
@@ -123,28 +125,38 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 		return FALSE;
 	}
 
-	if (!priv->type) {
-		g_set_error_literal (error,
-		                     NM_CONNECTION_ERROR,
-		                     NM_CONNECTION_ERROR_MISSING_PROPERTY,
-		                     _("property is missing"));
-		g_prefix_error (error, "%s.%s: ", NM_SETTING_BLUETOOTH_SETTING_NAME, NM_SETTING_BLUETOOTH_TYPE);
-		return FALSE;
-	} else if (!g_str_equal (priv->type, NM_SETTING_BLUETOOTH_TYPE_DUN) &&
-	           !g_str_equal (priv->type, NM_SETTING_BLUETOOTH_TYPE_NAP) &&
-	           !g_str_equal (priv->type, NM_SETTING_BLUETOOTH_TYPE_PANU)) {
+	type = priv->type;
+	if (!type) {
+		if (connection) {
+			/* We may infer the type from the (non-)existence of gsm/cdma/bridge settings. */
+			type = _nm_connection_detect_bluetooth_type (connection);
+		}
+		if (!type) {
+			g_set_error_literal (error,
+			                     NM_CONNECTION_ERROR,
+			                     NM_CONNECTION_ERROR_MISSING_PROPERTY,
+			                     _("property is missing"));
+			g_prefix_error (error, "%s.%s: ", NM_SETTING_BLUETOOTH_SETTING_NAME, NM_SETTING_BLUETOOTH_TYPE);
+			return FALSE;
+		}
+	}
+
+	if (!NM_IN_STRSET (type, NM_SETTING_BLUETOOTH_TYPE_DUN,
+	                         NM_SETTING_BLUETOOTH_TYPE_NAP,
+	                         NM_SETTING_BLUETOOTH_TYPE_PANU)) {
+		nm_assert (priv->type == type);
 		g_set_error (error,
 		             NM_CONNECTION_ERROR,
 		             NM_CONNECTION_ERROR_INVALID_PROPERTY,
 		             _("'%s' is not a valid value for the property"),
-		             priv->type);
+		             type);
 		g_prefix_error (error, "%s.%s: ", NM_SETTING_BLUETOOTH_SETTING_NAME, NM_SETTING_BLUETOOTH_TYPE);
 		return FALSE;
 	}
 
 	/* Make sure the corresponding 'type' setting is present */
 	if (   connection
-	    && !strcmp (priv->type, NM_SETTING_BLUETOOTH_TYPE_DUN)) {
+	    && nm_streq (type, NM_SETTING_BLUETOOTH_TYPE_DUN)) {
 		gboolean gsm = FALSE, cdma = FALSE;
 
 		gsm = !!nm_connection_get_setting_gsm (connection);
@@ -170,19 +182,12 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 	 */
 
 	/* NAP mode needs a bridge setting, and a bridge needs a name. */
-	if (!strcmp (priv->type, NM_SETTING_BLUETOOTH_TYPE_NAP)) {
+	if (nm_streq (type, NM_SETTING_BLUETOOTH_TYPE_NAP)) {
 		if (!_nm_connection_verify_required_interface_name (connection, error))
 			return FALSE;
-		if (connection && !nm_connection_get_setting_bridge (connection)) {
-			g_set_error (error,
-			             NM_CONNECTION_ERROR,
-			             NM_CONNECTION_ERROR_INVALID_SETTING,
-			             _("'%s' connection requires '%s' setting"),
-			             NM_SETTING_BLUETOOTH_TYPE_NAP,
-			             NM_SETTING_BRIDGE_SETTING_NAME);
-			g_prefix_error (error, "%s: ", NM_SETTING_BLUETOOTH_SETTING_NAME);
-			return NM_SETTING_VERIFY_NORMALIZABLE_ERROR;
-		}
+		if (   connection
+		    && !nm_connection_get_setting_bridge (connection))
+			missing_nap_bridge = TRUE;
 	} else {
 		if (!priv->bdaddr) {
 			g_set_error_literal (error,
@@ -192,6 +197,30 @@ verify (NMSetting *setting, NMConnection *connection, GError **error)
 			g_prefix_error (error, "%s.%s: ", NM_SETTING_BLUETOOTH_SETTING_NAME, NM_SETTING_BLUETOOTH_BDADDR);
 			return FALSE;
 		}
+	}
+
+	/* errors form here are normalizable. */
+
+	if (!priv->type) {
+		/* as determined above, we can detect the bluetooth type. */
+		nm_assert (!missing_nap_bridge);
+		g_set_error_literal (error,
+		                     NM_CONNECTION_ERROR,
+		                     NM_CONNECTION_ERROR_MISSING_PROPERTY,
+		                     _("property is missing"));
+		g_prefix_error (error, "%s.%s: ", NM_SETTING_BLUETOOTH_SETTING_NAME, NM_SETTING_BLUETOOTH_TYPE);
+		return NM_SETTING_VERIFY_NORMALIZABLE;
+	}
+
+	if (missing_nap_bridge) {
+		g_set_error (error,
+		             NM_CONNECTION_ERROR,
+		             NM_CONNECTION_ERROR_INVALID_SETTING,
+		             _("'%s' connection requires '%s' setting"),
+		             NM_SETTING_BLUETOOTH_TYPE_NAP,
+		             NM_SETTING_BRIDGE_SETTING_NAME);
+		g_prefix_error (error, "%s: ", NM_SETTING_BLUETOOTH_SETTING_NAME);
+		return NM_SETTING_VERIFY_NORMALIZABLE_ERROR;
 	}
 
 	return TRUE;

commit 8b67c90c3c1d1afe8d755b6068c70a4d4a755ba9

libnm: fix inverted condition when detecting the Bluetooth type
_nm_connection_detect_bluetooth_type() inferred a type only when
bluetooth.type was already set, which is backwards. verify() calls it
exactly when the property is missing, so a profile carrying a gsm/cdma
or bridge setting was rejected as incomplete instead of normalized,
while a profile with an explicit type could get it overwritten by the
inferred one during normalization.

Fixes: 9b28c9ba91f6 ('core: infer the bluetooth type from the presence of the supplemental settings')

src/libnm-core-impl/nm-connection.c
@@ -1109,7 +1109,7 @@ _nm_connection_detect_bluetooth_type(NMConnection *self)
 {
     NMSettingBluetooth *s_bt = nm_connection_get_setting_bluetooth(self);
 
-    if (s_bt && nm_setting_bluetooth_get_connection_type(s_bt)) {
+    if (s_bt && !nm_setting_bluetooth_get_connection_type(s_bt)) {
         if (nm_connection_get_setting_gsm(self) || nm_connection_get_setting_cdma(self))
             return NM_SETTING_BLUETOOTH_TYPE_DUN;
         if (nm_connection_get_setting_bridge(self))

src/libnm-core-impl/tests/test-general.c
@@ -6606,6 +6606,61 @@ test_connection_normalize_bluetooth_nap(void)
 }
 
 static void
+test_connection_normalize_bluetooth_type(gconstpointer test_data)
+{
+    const guint                   TEST_CASE = GPOINTER_TO_UINT(test_data);
+    gs_unref_object NMConnection *con       = NULL;
+    NMSettingConnection          *s_con;
+    NMSettingBluetooth           *s_bt;
+    const char                   *expected_type;
+
+    con =
+        nmtst_create_minimal_connection("test-bt", NULL, NM_SETTING_BLUETOOTH_SETTING_NAME, &s_con);
+    s_bt = nm_connection_get_setting_bluetooth(con);
+    g_object_set(s_bt, NM_SETTING_BLUETOOTH_BDADDR, "11:22:33:44:55:66", NULL);
+
+    switch (TEST_CASE) {
+    case 1:
+        nm_connection_add_setting(con, nm_setting_gsm_new());
+        g_object_set(nm_connection_get_setting_gsm(con), NM_SETTING_GSM_APN, "internet", NULL);
+        expected_type = NM_SETTING_BLUETOOTH_TYPE_DUN;
+        break;
+    case 2:
+        nm_connection_add_setting(con, nm_setting_cdma_new());
+        g_object_set(nm_connection_get_setting_cdma(con), NM_SETTING_CDMA_NUMBER, "#777", NULL);
+        expected_type = NM_SETTING_BLUETOOTH_TYPE_DUN;
+        break;
+    case 3:
+        g_object_set(s_con, NM_SETTING_CONNECTION_INTERFACE_NAME, "btnap0", NULL);
+        nm_connection_add_setting(con, nm_setting_bridge_new());
+        expected_type = NM_SETTING_BLUETOOTH_TYPE_NAP;
+        break;
+    case 4:
+        expected_type = NM_SETTING_BLUETOOTH_TYPE_PANU;
+        break;
+    /* Cases 5 and 6: an explicit type wins over the one that would be inferred. */
+    case 5:
+        nm_connection_add_setting(con, nm_setting_gsm_new());
+        g_object_set(nm_connection_get_setting_gsm(con), NM_SETTING_GSM_APN, "internet", NULL);
+        g_object_set(s_bt, NM_SETTING_BLUETOOTH_TYPE, NM_SETTING_BLUETOOTH_TYPE_PANU, NULL);
+        expected_type = NM_SETTING_BLUETOOTH_TYPE_PANU;
+        break;
+    case 6:
+        g_object_set(s_con, NM_SETTING_CONNECTION_INTERFACE_NAME, "btnap0", NULL);
+        nm_connection_add_setting(con, nm_setting_bridge_new());
+        g_object_set(s_bt, NM_SETTING_BLUETOOTH_TYPE, NM_SETTING_BLUETOOTH_TYPE_PANU, NULL);
+        expected_type = NM_SETTING_BLUETOOTH_TYPE_PANU;
+        break;
+    default:
+        g_assert_not_reached();
+    }
+
+    nmtst_assert_connection_verifies_and_normalizable(con);
+    nmtst_connection_normalize(con);
+    g_assert_cmpstr(nm_setting_bluetooth_get_connection_type(s_bt), ==, expected_type);
+}
+
+static void
 test_connection_normalize_ovs_interface_type_system(gconstpointer test_data)
 {
     const guint                   TEST_CASE = GPOINTER_TO_UINT(test_data);
@@ -12110,6 +12165,24 @@ main(int argc, char **argv)
                     test_connection_normalize_shared_addresses);
     g_test_add_func("/core/general/test_connection_normalize_bluetooth_nap",
                     test_connection_normalize_bluetooth_nap);
+    g_test_add_data_func("/core/general/test_connection_normalize_bluetooth_type/1",
+                         GUINT_TO_POINTER(1),
+                         test_connection_normalize_bluetooth_type);
+    g_test_add_data_func("/core/general/test_connection_normalize_bluetooth_type/2",
+                         GUINT_TO_POINTER(2),
+                         test_connection_normalize_bluetooth_type);
+    g_test_add_data_func("/core/general/test_connection_normalize_bluetooth_type/3",
+                         GUINT_TO_POINTER(3),
+                         test_connection_normalize_bluetooth_type);
+    g_test_add_data_func("/core/general/test_connection_normalize_bluetooth_type/4",
+                         GUINT_TO_POINTER(4),
+                         test_connection_normalize_bluetooth_type);
+    g_test_add_data_func("/core/general/test_connection_normalize_bluetooth_type/5",
+                         GUINT_TO_POINTER(5),
+                         test_connection_normalize_bluetooth_type);
+    g_test_add_data_func("/core/general/test_connection_normalize_bluetooth_type/6",
+                         GUINT_TO_POINTER(6),
+                         test_connection_normalize_bluetooth_type);
     g_test_add_data_func("/core/general/test_connection_normalize_ovs_interface_type_system/1",
                          GUINT_TO_POINTER(1),
                          test_connection_normalize_ovs_interface_type_system);