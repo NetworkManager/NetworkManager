NetworkManager/NetworkManager

commit bb0a26e906619217238c81afb0c1cbb49b030708

wifi: add support for new "6GHz" band
Until now the Wi-Fi bands were named after the first 802.11 standard
that introduced them: "a" for 5GHz introduced in 802.11a and "bg" for
2.4GHz introduced in 802.11b/g. With new bands added, this naming
scheme doesn't sound very intuitive to remember for users. Furthermore
we have now 6GHz that is introduced by 802.11ax (Wi-Fi 6), but the
compatible devices can use all three the bands (2.4, 5, 6 GHz).

For the 6 GHz band, simply name it "6GHz".

Co-authored-by: Beniamino Galvani <bgalvani@redhat.com>

src/core/devices/wifi/nm-wifi-ap.c
@@ -574,16 +574,6 @@ nm_wifi_ap_to_string(const NMWifiAP *self, char *str_buf, gulong buf_len, gint64
     return str_buf;
 }
 
-static guint
-freq_to_band(guint32 freq)
-{
-    if (freq >= 4915 && freq <= 5825)
-        return 5;
-    else if (freq >= 2412 && freq <= 2484)
-        return 2;
-    return 0;
-}
-
 gboolean
 nm_wifi_ap_check_compatible(NMWifiAP *self, NMConnection *connection)
 {
@@ -631,12 +621,12 @@ nm_wifi_ap_check_compatible(NMWifiAP *self, NMConnection *connection)
 
     band = nm_setting_wireless_get_band(s_wireless);
     if (band) {
-        guint ap_band = freq_to_band(priv->freq);
+        const char *ap_band = nm_utils_wifi_freq_to_band(priv->freq);
 
-        if (!strcmp(band, "a") && ap_band != 5)
+        if (!nm_streq(band, ap_band))
             return FALSE;
-        else if (!strcmp(band, "bg") && ap_band != 2)
-            return FALSE;
+
+        return TRUE;
     }
 
     channel = nm_setting_wireless_get_channel(s_wireless);

man/nmcli.xml
@@ -1718,6 +1718,7 @@
             <group choice='req'>
               <arg choice='plain'>a</arg>
               <arg choice='plain'>bg</arg>
+              <arg choice='plain'>6GHz</arg>
             </group>
           </arg>
           <arg><option>channel</option> <replaceable>channel</replaceable></arg>

src/core/devices/wifi/nm-device-wifi.c
@@ -3257,8 +3257,15 @@ act_stage1_prepare(NMDevice *device, NMDeviceStateReason *out_failure_reason)
 static void
 ensure_hotspot_frequency(NMDeviceWifi *self, NMSettingWireless *s_wifi, NMWifiAP *ap)
 {
-    guint32     a_freqs[]  = {5180, 5200, 5220, 5745, 5765, 5785, 5805, 0};
-    guint32     bg_freqs[] = {2412, 2437, 2462, 2472, 0};
+    guint32     freqs_a[]    = {5180, 5200, 5220, 5745, 5765, 5785, 5805, 0};
+    guint32     freqs_bg[]   = {2412, 2437, 2462, 2472, 0};
+    guint32     freqs_6ghz[] = {5975, /* only U-NII-5 PSC channels, for better compatibility */
+                                6055,
+                                6135,
+                                6215,
+                                6295,
+                                6375,
+                                0};
     guint32    *rnd_freqs;
     guint       rnd_freqs_len;
     NMDevice   *device = NM_DEVICE(self);
@@ -3269,7 +3276,7 @@ ensure_hotspot_frequency(NMDeviceWifi *self, NMSettingWireless *s_wifi, NMWifiAP
     guint       l;
 
     nm_assert(ap);
-    nm_assert(NM_IN_STRSET(band, NULL, "a", "bg"));
+    nm_assert(NM_IN_STRSET(band, NULL, "a", "bg", "6GHz"));
 
     if (nm_wifi_ap_get_freq(ap))
         return;
@@ -3303,11 +3310,14 @@ ensure_hotspot_frequency(NMDeviceWifi *self, NMSettingWireless *s_wifi, NMWifiAP
     }
 
     if (nm_streq0(band, "a")) {
-        rnd_freqs     = a_freqs;
-        rnd_freqs_len = G_N_ELEMENTS(a_freqs) - 1;
+        rnd_freqs     = freqs_a;
+        rnd_freqs_len = G_N_ELEMENTS(freqs_a) - 1;
+    } else if (nm_streq0(band, "6GHz")) {
+        rnd_freqs     = freqs_6ghz;
+        rnd_freqs_len = G_N_ELEMENTS(freqs_6ghz) - 1;
     } else {
-        rnd_freqs     = bg_freqs;
-        rnd_freqs_len = G_N_ELEMENTS(bg_freqs) - 1;
+        rnd_freqs     = freqs_bg;
+        rnd_freqs_len = G_N_ELEMENTS(freqs_bg) - 1;
     }
 
     /* shuffle the frequencies (inplace). The idea is to choose

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-reader.c
@@ -4401,7 +4401,7 @@ make_wireless_setting(shvarFile *ifcfg, GError **error)
     NMSettingWireless         *s_wireless;
     const char                *cvalue;
     char                      *value = NULL;
-    gint64                     chan  = 0;
+    guint64                    chan  = 0;
     NMSettingMacRandomization  mac_randomization;
     NMSettingWirelessPowersave powersave = NM_SETTING_WIRELESS_POWERSAVE_DEFAULT;
     NMTernary                  ternary;
@@ -4502,7 +4502,7 @@ make_wireless_setting(shvarFile *ifcfg, GError **error)
 
     value = svGetValueStr_cp(ifcfg, "CHANNEL");
     if (value) {
-        chan = _nm_utils_ascii_str_to_int64(value, 10, 1, 196, 0);
+        chan = _nm_utils_ascii_str_to_int64(value, 10, 1, _NM_WIFI_CHANNEL_MAX, 0);
         if (chan == 0) {
             g_set_error(error,
                         NM_SETTINGS_ERROR,
@@ -4518,44 +4518,47 @@ make_wireless_setting(shvarFile *ifcfg, GError **error)
 
     value = svGetValueStr_cp(ifcfg, "BAND");
     if (value) {
-        if (!strcmp(value, "a")) {
-            if (chan && chan <= 14) {
-                g_set_error(error,
-                            NM_SETTINGS_ERROR,
-                            NM_SETTINGS_ERROR_INVALID_CONNECTION,
-                            "Band '%s' invalid for channel %u",
-                            value,
-                            (guint32) chan);
-                g_free(value);
-                goto error;
-            }
-        } else if (!strcmp(value, "bg")) {
-            if (chan && chan > 14) {
-                g_set_error(error,
-                            NM_SETTINGS_ERROR,
-                            NM_SETTINGS_ERROR_INVALID_CONNECTION,
-                            "Band '%s' invalid for channel %u",
-                            value,
-                            (guint32) chan);
-                g_free(value);
-                goto error;
-            }
-        } else {
+        if (!NM_IN_STRSET(value, "a", "bg", "6GHz")) {
             g_set_error(error,
                         NM_SETTINGS_ERROR,
                         NM_SETTINGS_ERROR_INVALID_CONNECTION,
-                        "Invalid wireless band '%s'",
+                        "Band '%s' invalid",
                         value);
             g_free(value);
             goto error;
         }
+
+        if (chan) {
+            if (!nm_utils_wifi_is_channel_valid(chan, value)) {
+                g_set_error(error,
+                            NM_SETTINGS_ERROR,
+                            NM_SETTINGS_ERROR_INVALID_CONNECTION,
+                            "Band '%s' invalid for channel %u",
+                            value,
+                            (guint32) chan);
+                g_free(value);
+                goto error;
+            }
+        }
+
         g_object_set(s_wireless, NM_SETTING_WIRELESS_BAND, value, NULL);
         g_free(value);
     } else if (chan > 0) {
-        if (chan > 14)
+        if (chan > _NM_WIFI_CHANNEL_MAX_5GHZ) {
+            g_set_error(error,
+                        NM_SETTINGS_ERROR,
+                        NM_SETTINGS_ERROR_INVALID_CONNECTION,
+                        "Setting channel without band is ambiguous and deprecated. Not supported "
+                        "for 6GHz.");
+            g_free(value);
+            goto error;
+        } else if (chan > _NM_WIFI_CHANNEL_MAX_2GHZ) {
+            PARSE_WARNING(
+                "Setting channel without band is ambiguous and deprecated. Assuming band 'a'.");
             g_object_set(s_wireless, NM_SETTING_WIRELESS_BAND, "a", NULL);
-        else
+        } else {
             g_object_set(s_wireless, NM_SETTING_WIRELESS_BAND, "bg", NULL);
+        }
     }
 
     value = svGetValueStr_cp(ifcfg, "MTU");

src/core/settings/plugins/ifcfg-rh/nms-ifcfg-rh-writer.c
@@ -849,7 +849,7 @@ write_wireless_setting(NMConnection *connection,
     GBytes            *ssid;
     const guint8      *ssid_data;
     gsize              ssid_len;
-    const char        *mode, *bssid;
+    const char        *mode, *bssid, *band;
     const char        *device_mac, *cloned_mac;
     guint32            mtu, chan, i;
     gboolean           adhoc = FALSE, hex_ssid = FALSE;
@@ -968,9 +968,11 @@ write_wireless_setting(NMConnection *connection,
     chan = nm_setting_wireless_get_channel(s_wireless);
     if (chan) {
         svSetValueInt64(ifcfg, "CHANNEL", chan);
-    } else {
-        /* Band only set if channel is not, since channel implies band */
-        svSetValueStr(ifcfg, "BAND", nm_setting_wireless_get_band(s_wireless));
+    }
+
+    band = nm_setting_wireless_get_band(s_wireless);
+    if (band) {
+        svSetValueStr(ifcfg, "BAND", band);
     }
 
     bssid = nm_setting_wireless_get_bssid(s_wireless);

src/core/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-Test_Write_WiFi_Band_6ghz.cexpected
@@ -1,9 +1,7 @@
-ESSID=MySSID
-MODE=Ap
-CHANNEL=52
-BAND=a
+ESSID="Test SSID"
+MODE=Managed
+BAND=6GHz
 MAC_ADDRESS_RANDOMIZATION=default
-AP_ISOLATION=yes
 TYPE=Wireless
 PROXY_METHOD=none
 BROWSER_ONLY=no
@@ -15,6 +13,6 @@ IPV6_AUTOCONF=yes
 IPV6_DEFROUTE=yes
 IPV6_FAILURE_FATAL=no
 IPV6_ADDR_GEN_MODE=default
-NAME="Test Write Wi-Fi AP Mode"
+NAME="Test Write Wi-Fi Band 6GHz"
 UUID=${UUID}
 ONBOOT=yes

src/core/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-Test_Write_WiFi_Band_a.cexpected
@@ -13,6 +13,6 @@ IPV6_AUTOCONF=yes
 IPV6_DEFROUTE=yes
 IPV6_FAILURE_FATAL=no
 IPV6_ADDR_GEN_MODE=default
-NAME="Test Write Wi-Fi Band A"
+NAME="Test Write Wi-Fi Band A - 5GHz"
 UUID=${UUID}
 ONBOOT=yes

src/core/settings/plugins/ifcfg-rh/tests/network-scripts/ifcfg-test-wifi-band-6ghz
@@ -1,0 +1,13 @@
+TYPE=Wireless
+DEVICE=eth2
+HWADDR=00:16:41:11:22:33
+NM_CONTROLLED=yes
+BOOTPROTO=dhcp
+ESSID=blahblah
+BAND=6GHz
+MODE=Managed
+RATE=auto
+ONBOOT=yes
+USERCTL=yes
+PEERDNS=yes
+IPV6INIT=no

commit 99bb1a780939806d038588d9a4ac47b69be870b5

wifi: fix crash matching band against an AP with unknown frequency
nm_wifi_ap_check_compatible() passes the AP band to nm_streq(), which
crashes on NULL when the AP frequency doesn't map to a known band. A
fake AP created for a hidden network without band and channel has no
frequency, so activating a hidden profile crashed the daemon as soon as
any profile with an explicit "band" was rechecked against it.

Fixes: bb0a26e90661 ('wifi: add support for new "6GHz" band')

https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/2005

src/core/devices/wifi/nm-wifi-ap.c
@@ -666,10 +666,8 @@ nm_wifi_ap_check_compatible(NMWifiAP *self, NMConnection *connection)
     if (band) {
         const char *ap_band = nm_wifi_freq_to_band_prop(priv->freq);
 
-        if (!nm_streq(band, ap_band))
+        if (!nm_streq0(band, ap_band))
             return FALSE;
-
-        return TRUE;
     }
 
     channel = nm_setting_wireless_get_channel(s_wireless);

src/core/devices/wifi/tests/test-devices-wifi.c
@@ -6,6 +6,7 @@
 #include "src/core/nm-default-daemon.h"
 
 #include "devices/wifi/nm-wifi-utils.h"
+#include "devices/wifi/nm-wifi-ap.h"
 #include "devices/wifi/nm-device-wifi.h"
 #include "libnm-core-intern/nm-core-internal.h"
 
@@ -1503,6 +1504,60 @@ test_wps_key_to_psk(void)
 
 /*****************************************************************************/
 
+static void
+test_ap_check_compatible_band(void)
+{
+    gs_unref_object NMWifiAP     *ap_no_freq    = NULL;
+    gs_unref_object NMWifiAP     *ap_open       = NULL;
+    gs_unref_object NMWifiAP     *ap_rsn        = NULL;
+    gs_unref_object NMConnection *conn_ap       = NULL;
+    gs_unref_object NMConnection *conn          = NULL;
+    const KeyData                 band_a[]      = {{NM_SETTING_WIRELESS_BAND, "a", 0}, {NULL}};
+    const KeyData                 band_bg[]     = {{NM_SETTING_WIRELESS_BAND, "bg", 0}, {NULL}};
+    const KeyData                 band_a_ch36[] = {{NM_SETTING_WIRELESS_BAND, "a", 0},
+                                                   {NM_SETTING_WIRELESS_CHANNEL, NULL, 36},
+                                                   {NULL}};
+    const KeyData                 band_a_ch40[] = {{NM_SETTING_WIRELESS_BAND, "a", 0},
+                                                   {NM_SETTING_WIRELESS_CHANNEL, NULL, 40},
+                                                   {NULL}};
+    const KeyData wsec_psk[] = {{NM_SETTING_WIRELESS_SECURITY_KEY_MGMT, "wpa-psk", 0},
+                                {NM_SETTING_WIRELESS_SECURITY_PROTO, "rsn", 0},
+                                {NULL}};
+
+    /* An AP with unknown frequency never matches a profile that requires a band. */
+    conn       = create_basic("hidden", NULL, _NM_802_11_MODE_INFRA);
+    ap_no_freq = nm_wifi_ap_new_fake_from_connection(conn);
+    g_assert(ap_no_freq);
+
+    fill_wifi(conn, band_a);
+    g_assert(!nm_wifi_ap_check_compatible(ap_no_freq, conn));
+
+    conn_ap = create_basic("hidden", NULL, _NM_802_11_MODE_INFRA);
+    fill_wifi(conn_ap, band_a_ch36);
+    ap_open = nm_wifi_ap_new_fake_from_connection(conn_ap);
+    g_assert(ap_open);
+
+    g_assert(nm_wifi_ap_check_compatible(ap_open, conn));
+
+    fill_wifi(conn, band_bg);
+    g_assert(!nm_wifi_ap_check_compatible(ap_open, conn));
+
+    /* A matching band must not skip the channel check. */
+    fill_wifi(conn, band_a_ch40);
+    g_assert(!nm_wifi_ap_check_compatible(ap_open, conn));
+
+    /* A matching band must not skip the security check. */
+    fill_wsec(conn_ap, wsec_psk);
+    ap_rsn = nm_wifi_ap_new_fake_from_connection(conn_ap);
+    g_assert(ap_rsn);
+
+    fill_wifi(conn, band_a_ch36);
+    g_assert(nm_wifi_ap_check_compatible(ap_open, conn));
+    g_assert(!nm_wifi_ap_check_compatible(ap_rsn, conn));
+}
+
+/*****************************************************************************/
+
 NMTST_DEFINE();
 
 int
@@ -1650,6 +1705,8 @@ main(int argc, char **argv)
     g_test_add_func("/wifi/ssids_options_to_ptrarray", test_ssids_options_to_ptrarray);
 
     g_test_add_func("/wifi/wps_key_to_psk", test_wps_key_to_psk);
+
+    g_test_add_func("/wifi/ap/check_compatible_band", test_ap_check_compatible_band);
 
     return g_test_run();
 }